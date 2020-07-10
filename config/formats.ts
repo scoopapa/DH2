178b71e4133aa9071de31095c171903703c0b558
scoopapa <pccompositor@aim.com>
2020-07-10 12:28:18 -0500

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1054,7 +1054,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			/**@type {{[k: string]: true}} */
 			let speciesTable = {};
 			for (const set of team) {
-				console.log( this );
+				console.log( this.dex.speciesCache[set.species] );
 				let template = this.dex.speciesCache[set.species];
 				if (speciesTable[template.species]) {
 					return ["You are limited to one of each Pokémon by Species Clause (except for different Rotom formes). ", "You have more than one " + template.baseSpecies + "."];
