fda127a19d2942a57169df2b32effb6cc235bc2b
scoopapa <pccompositor@aim.com>
2020-08-07 00:38:05 -0500

    Create moves.ts

---
+++ data/mods/ccapm2020/moves.ts
@@ -0,0 +1,21 @@
+export const BattleMovedex: {[k: string]: ModdedMoveData} = {
+	healorder: {
+		num: 456,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		desc: "The user restores 1/2 of its maximum HP, rounded half up.",
+		shortDesc: "Heals the user by 50% of its max HP.",
+		isNonstandard: "Past",
+		name: "Heal Order",
+		pp: 10,
+		priority: 0,
+		flags: {snatch: 1, heal: 1},
+		heal: [1, 2],
+		secondary: null,
+		target: "self",
+		type: "Bug",
+		zMove: {effect: 'clearnegativeboost'},
+		contestType: "Clever",
+	},
+};
