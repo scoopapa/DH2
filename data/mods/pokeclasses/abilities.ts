2dd71eb1013ff0f62e2c85c60c4dd516ed5d72d2
scoopapa <pccompositor@aim.com>
2020-08-10 10:50:22 -0500

    Update abilities.ts

--- data/mods/pokeclasses/abilities.ts
+++ data/mods/pokeclasses/abilities.ts
@@ -23,4 +23,38 @@ export const BattleAbilities: {[k: string]: ModdedAbilityData} = {
 			}
 		},
 	},
+	warrior: {
+		shortDesc: "This Pokemon's Attack is doubled.",
+		onModifyAtkPriority: 5,
+		onModifyAtk(stat) {
+			return this.chainModify(1.2);
+		},
+		onModifyDef(stat) {
+			return this.chainModify(1.2);
+		},
+		onModifySpA(stat) {
+			return this.chainModify(0.9);
+		},
+		onModifySpD(stat) {
+			return this.chainModify(0.9);
+		},
+		onBasePower(basePower, attacker, defender, move) {
+			if (move.flags['contact']) {
+				return this.chainModify(1.1);
+			}
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.flags['contact']) {
+				return this.chainModify(0.9);
+			}
+		},
+		onDamage(damage, target, source, effect) {
+			if (effect.name == 'Stealth Rock' or effect.name == 'Spikes') {
+				return this.chainModify(0.5);
+			}
+		},
+		name: "Huge Power",
+		rating: 5,
+		num: 37,
+	},
 };
