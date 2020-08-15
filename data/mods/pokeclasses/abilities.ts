f655cfd8ad6de28bdb03e4ab17a329da0fa911fe
scoopapa <pccompositor@aim.com>
2020-08-15 14:53:10 -0500

    PC

--- data/mods/pokeclasses/abilities.ts
+++ data/mods/pokeclasses/abilities.ts
@@ -49,11 +49,62 @@ export const BattleAbilities: {[k: string]: ModdedAbilityData} = {
 			}
 		},
 		onDamage(damage, target, source, effect) {
-			if (effect.name == 'Stealth Rock' or effect.name == 'Spikes') {
+			if (effect.name === 'Stealth Rock' || effect.name === 'Spikes') {
 				return this.chainModify(0.5);
 			}
 		},
-		name: "Huge Power",
+		onResidualOrder: 5,
+		onResidualSubOrder: 4,
+		onResidual(pokemon) {
+			if (pokemon.hp && pokemon.status && this.randomChance(1, 5)) {
+				this.debug('warrior');
+				this.add('-activate', pokemon, 'ability: Warrior');
+				pokemon.cureStatus();
+			}
+		},
+		onModifyPriority(priority, pokemon, target, move) {
+			if (move?.id === 'taunt') {
+				return priority + 1;
+			}
+		},
+		name: "warrior",
+		rating: 5,
+		num: 37,
+	},
+	warrior: {
+		shortDesc: "This Pokemon's Attack is doubled.",
+		onModifyAtkPriority: 5,
+		onModifyAtk(stat) {
+			return this.chainModify(0.7);
+		},
+		onModifyDef(stat) {
+			return this.chainModify(0.8);
+		},
+		onModifySpA(stat) {
+			return this.chainModify(1.2);
+		},
+		onModifySpD(stat) {
+			return this.chainModify(1.1);
+		},
+		onModifyMovePriority: -2,
+		onModifyMove(move) {
+			if (move.secondaries) {
+				for (const secondary of move.secondaries) {
+					if (secondary.chance) secondary.chance *= 1.5;
+				}
+			}
+		},
+		onSourceModifyAccuracy(accuracy, target, source, move) {
+			if (move.category === "Status") return accuracy * 1.2;
+		},
+		onTryHealPriority: 1,
+		onTryHeal(damage, target, source, effect) {
+			const heals = ['drain', 'leechseed', 'ingrain', 'aquaring', 'strengthsap'];
+			if (heals.includes(effect.id)) {
+				return this.chainModify([0x14CC, 0x1000]);
+			}
+		},
+		name: "mage",
 		rating: 5,
 		num: 37,
 	},
--- data/mods/pokeclasses/moves.ts
+++ data/mods/pokeclasses/moves.ts
@@ -19,4 +19,40 @@ export const BattleMovedex: {[k: string]: ModdedMoveData} = {
 		name: "Athletics",
 	},
 	//--------------------------------modified moves-------------------------------------------
+	reflect: {
+		inherit: true,
+		effect: {
+			duration: 5,
+			durationCallback(target, source, effect) {
+				if (source?.hasItem('lightclay')) {
+					return 8;
+				}
+				return 5;
+			},
+			onAnyModifyDamage(damage, source, target, move) {
+				if (target !== source && target.side === this.effectData.target && this.getCategory(move) === 'Physical') {
+					if (!target.getMoveHitData(move).crit && !move.infiltrates) {
+						this.debug('Reflect weaken');
+						if (target.side.active.length > 1) return this.chainModify([0xAAC, 0x1000]);
+						return this.chainModify(0.5);
+					}
+				}
+			},
+			onStart(side) {
+				this.add('-sidestart', side, 'Reflect');
+				this.effectData.sourceClass = 
+			},
+			onResidualOrder: 21,
+			onEnd(side) {
+				this.add('-sideend', side, 'Reflect');
+			},
+		},
+		secondary: null,
+		target: "allySide",
+		type: "Psychic",
+		zMove: {boost: {def: 1}},
+		contestType: "Clever",
+	},
+	
+	
 };
