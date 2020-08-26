65aaf8c92c0158b7ac313e7aaaa405c2cd6fdb4f
Petuuuhhh <peterswanton191@bellsouth.net>
2020-08-26 11:28:03 -0400

    m4a v6

---
+++ data/mods/m4av6/abilities.ts
@@ -0,0 +1,83 @@
+export const BattleAbilities: {[k: string]: ModdedAbilityData} = {
+	gravitas: {
+		shortDesc: "On switch-in, this Pokémon summons Gravity.",
+		onStart(source) {
+			this.field.addPseudoWeather('gravity');
+		},
+		name: "Gravitas",
+		rating: 4,
+		num: -1001,
+	},
+	ignite: {
+		desc: "This Pokémon's Normal-type moves become Fire-type moves and have their power multiplied by 1.2. This effect comes after other effects that change a move's type, but before Ion Deluge and Electrify's effects.",
+		shortDesc: "This Pokémon's Normal-type moves become Fire-type and have 1.2x power.",
+		onModifyTypePriority: -1,
+		onModifyType(move, pokemon) {
+			const noModifyType = [
+				'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
+			];
+			if (move.type === 'Normal' && !noModifyType.includes(move.id) && !(move.isZ && move.category !== 'Status')) {
+				move.type = 'Fire';
+				move.igniteBoosted = true;
+			}
+		},
+		onBasePowerPriority: 23,
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.igniteBoosted) return this.chainModify([0x1333, 0x1000]);
+		},
+		name: "Ignite",
+		rating: 4,
+		num: -1002,
+	},
+	grounded: {
+		desc: "While this Pokémon is active, the effects of terrains are disabled. (BUT ACTUALLY IT DOES NOTHING RIGHT NOW! WIP)",
+		shortDesc: "While this Pokémon is active, the effects of terrains are disabled.",
+		onStart(pokemon) {
+			this.add('-ability', pokemon, 'Grounded');
+		},
+		name: "Grounded",
+		rating: 2,
+		num: -1003,
+	},
+	arenarock: {
+		desc: "On switch-in, the field becomes Grassy Terrain. This terrain remains in effect until this Ability is no longer active for any Pokémon.",
+		shortDesc: "On switch-in, Grassy Terrain begins until this Ability is not active in battle.",
+		onStart(source) {
+			this.field.clearTerrain();
+			this.field.setTerrain('grassyterrain');
+		},
+		onAnyTerrainStart(target, source, terrain) {
+			if(target !== source){
+				this.field.clearTerrain();
+				this.field.setTerrain('grassyterrain');
+			}
+		},
+		onEnd(pokemon) {
+			if (this.field.terrainData.source !== pokemon) return;
+			for (const target of this.getAllActive()) {
+				if (target === pokemon) continue;
+				if (target.hasAbility('arenarock')) {
+					this.field.terrainData.source = target;
+					return;
+				}
+			}
+			this.field.clearTerrain();
+		},
+		name: "Arena Rock",
+		rating: 4.5,
+		num: -1004,
+	},
+	sharpstriker: {
+		desc: "This Pokémon's ballistic moves have their power multiplied by 1.5. Ballistic moves include Bullet Seed, Octazooka, Barrage, Rock Wrecker, Zap Cannon, Acid Spray, Aura Sphere, Focus Blast, and all moves with Ball or Bomb in their name.",
+		shortDesc: "This Pokémon's ballistic moves have 1.5x power (Shadow Ball, Sludge Bomb, Focus Blast, etc).",
+		onBasePowerPriority: 19,
+		onBasePower(basePower, attacker, defender, move) {
+			if (move.flags['bullet']) {
+				return this.chainModify(1.5);
+			}
+		},
+		name: "Sharp Striker",
+		rating: 3,
+		num: -1005,
+	},
+}
---
+++ data/mods/m4av6/moves.ts
@@ -0,0 +1,66 @@
+export const BattleMovedex: {[k: string]: ModdedMoveData} = {
+	grassyterrain: {
+		num: 580,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		desc: "For 5 turns, the terrain becomes Grassy Terrain. During the effect, the power of Grass-type attacks used by grounded Pokemon is multiplied by 1.3, the power of Bulldoze, Earthquake, and Magnitude used against grounded Pokemon is multiplied by 0.5, and grounded Pokemon have 1/16 of their maximum HP, rounded down, restored at the end of each turn, including the last turn. Camouflage transforms the user into a Grass type, Nature Power becomes Energy Ball, and Secret Power has a 30% chance to cause sleep. Fails if the current terrain is Grassy Terrain.",
+		shortDesc: "5 turns. Grounded: +Grass power, +1/16 max HP.",
+		name: "Grassy Terrain",
+		pp: 10,
+		priority: 0,
+		flags: {nonsky: 1},
+		terrain: 'grassyterrain',
+		effect: {
+			duration: 5,
+			durationCallback(source, effect) {
+                if (source?.hasItem('terrainextender')) {
+                    return 8;
+                }
+                if (source?.hasAbility('arenarock')) {
+                    return 0;
+                }
+                return 5;
+            },
+			onBasePowerPriority: 6,
+			onBasePower(basePower, attacker, defender, move) {
+				const weakenedMoves = ['earthquake', 'bulldoze', 'magnitude'];
+				if (weakenedMoves.includes(move.id)) {
+					this.debug('move weakened by grassy terrain');
+					return this.chainModify(0.5);
+				}
+				if (move.type === 'Grass' && attacker.isGrounded()) {
+					this.debug('grassy terrain boost');
+					return this.chainModify([0x14CD, 0x1000]);
+				}
+			},
+			onStart(battle, source, effect) {
+				if (effect?.effectType === 'Ability') {
+					this.add('-fieldstart', 'move: Grassy Terrain', '[from] ability: ' + effect, '[of] ' + source);
+				} else {
+					this.add('-fieldstart', 'move: Grassy Terrain');
+				}
+			},
+			onResidualOrder: 5,
+			onResidualSubOrder: 3,
+			onResidual() {
+				this.eachEvent('Terrain');
+			},
+			onTerrain(pokemon) {
+				if (pokemon.isGrounded() && !pokemon.isSemiInvulnerable()) {
+					this.debug('Pokemon is grounded, healing through Grassy Terrain.');
+					this.heal(pokemon.baseMaxhp / 16, pokemon, pokemon);
+				}
+			},
+			onEnd() {
+				if (!this.effectData.duration) this.eachEvent('Terrain');
+				this.add('-fieldend', 'move: Grassy Terrain');
+			},
+		},
+		secondary: null,
+		target: "all",
+		type: "Grass",
+		zMove: {boost: {def: 1}},
+		contestType: "Beautiful",
+	},
+}
