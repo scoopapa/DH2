62a67ed4c7920dad3f17e06bc1a0c969877bfc95
scoopapa <pccompositor@aim.com>
2020-08-27 11:19:25 -0500

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1469,7 +1469,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
   		name: "[Gen 7] Super Smash Mods",
   		desc: ["None Yet!",
 		      ],
-  		ruleset: ['Gen 7 [Ubers]', '+Past'],
+  		ruleset: ['Standard', '+Past'],
 		mod: 'smashmods',
 		//banlist: [],
 	},
