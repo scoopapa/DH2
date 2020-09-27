2e70f4c5db8787c24f37498e7895f6edaa7f7a45
scoopapa <pccompositor@aim.com>
2020-09-27 13:06:25 -0500

    Update scripts.ts

--- data/mods/m4av6/scripts.ts
+++ data/mods/m4av6/scripts.ts
@@ -38,68 +38,67 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'butterfree').learnset.taunt = ['7L1'];
 		this.modData('Learnsets', 'butterfree').learnset.earthpower = ['7L1'];
 	},
-};
-
-pokemon: {
-	setStatus(status, source, sourceEffect, ignoreImmunities) {
-		if (!this.hp) return false;
-		status = this.battle.dex.getEffect(status);
-		if (this.battle.event) {
-			if (!source) source = this.battle.event.source;
-			if (!sourceEffect) sourceEffect = this.battle.effect;
-		}
-		if (!source) source = this;
- 
-		if (this.status === status.id) {
-			if ((sourceEffect as Move)?.status === this.status) {
-				this.battle.add('-fail', this, this.status);
-			} else if ((sourceEffect as Move)?.status) {
-				this.battle.add('-fail', source);
-				this.battle.attrLastMove('[still]');
+	pokemon: {
+		setStatus: function(status, source = null, sourceEffect, ignoreImmunities) {
+			if (!this.hp) return false;
+			status = this.battle.dex.getEffect(status);
+			if (this.battle.event) {
+				if (!source) source = this.battle.event.source;
+				if (!sourceEffect) sourceEffect = this.battle.effect;
 			}
-			return false;
-		}
- 
-		if (!ignoreImmunities && status.id &&
-				!(source?.hasAbility('corrosion') && ['tox', 'psn'].includes(status.id)) &&
-				!(source?.hasAbility('dustscatter') && ['poisonpowder', 'stunspore'].includes(effect.id))) {
-			// the game currently never ignores immunities
-			if (!this.runStatusImmunity(status.id === 'tox' ? 'psn' : status.id)) {
-				this.battle.debug('immune to status');
-				if ((sourceEffect as Move)?.status) {
-					this.battle.add('-immune', this);
+			if (!source) source = this;
+	 
+			if (this.status === status.id) {
+				if ((sourceEffect as Move)?.status === this.status) {
+					this.battle.add('-fail', this, this.status);
+				} else if ((sourceEffect as Move)?.status) {
+					this.battle.add('-fail', source);
+					this.battle.attrLastMove('[still]');
 				}
 				return false;
 			}
-		}
-		const prevStatus = this.status;
-		const prevStatusData = this.statusData;
-		if (status.id) {
-			const result: boolean = this.battle.runEvent('SetStatus', this, source, sourceEffect, status);
-			if (!result) {
-				this.battle.debug('set status [' + status.id + '] interrupted');
-				return result;
+	 
+			if (!ignoreImmunities && status.id &&
+					!(source?.hasAbility('corrosion') && ['tox', 'psn'].includes(status.id)) &&
+					!(source?.hasAbility('dustscatter') && ['poisonpowder', 'stunspore'].includes(effect.id))) {
+				// the game currently never ignores immunities
+				if (!this.runStatusImmunity(status.id === 'tox' ? 'psn' : status.id)) {
+					this.battle.debug('immune to status');
+					if ((sourceEffect as Move)?.status) {
+						this.battle.add('-immune', this);
+					}
+					return false;
+				}
 			}
+			const prevStatus = this.status;
+			const prevStatusData = this.statusData;
+			if (status.id) {
+				const result: boolean = this.battle.runEvent('SetStatus', this, source, sourceEffect, status);
+				if (!result) {
+					this.battle.debug('set status [' + status.id + '] interrupted');
+					return result;
+				}
+			}
+	 
+			this.status = status.id;
+			this.statusData = {id: status.id, target: this};
+			if (source) this.statusData.source = source;
+			if (status.duration) this.statusData.duration = status.duration;
+			if (status.durationCallback) {
+				this.statusData.duration = status.durationCallback.call(this.battle, this, source, sourceEffect);
+			}
+	 
+			if (status.id && !this.battle.singleEvent('Start', status, this.statusData, this, source, sourceEffect)) {
+				this.battle.debug('status start [' + status.id + '] interrupted');
+				// cancel the setstatus
+				this.status = prevStatus;
+				this.statusData = prevStatusData;
+				return false;
+			}
+			if (status.id && !this.battle.runEvent('AfterSetStatus', this, source, sourceEffect, status)) {
+				return false;
+			}
+			return true;
 		}
- 
-		this.status = status.id;
-		this.statusData = {id: status.id, target: this};
-		if (source) this.statusData.source = source;
-		if (status.duration) this.statusData.duration = status.duration;
-		if (status.durationCallback) {
-			this.statusData.duration = status.durationCallback.call(this.battle, this, source, sourceEffect);
-		}
- 
-		if (status.id && !this.battle.singleEvent('Start', status, this.statusData, this, source, sourceEffect)) {
-			this.battle.debug('status start [' + status.id + '] interrupted');
-			// cancel the setstatus
-			this.status = prevStatus;
-			this.statusData = prevStatusData;
-			return false;
-		}
-		if (status.id && !this.battle.runEvent('AfterSetStatus', this, source, sourceEffect, status)) {
-			return false;
-		}
-		return true;
-	}
+	};
 };
