aee12635690edb7f32dfb6784933d53bdb1c2354
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-09-28 14:33:21 +0100

    Update scripts.ts
    Remove bugged Dust Scatter code and apply new move distribution from slate 11

--- data/mods/m4av6/scripts.ts
+++ data/mods/m4av6/scripts.ts
@@ -37,68 +37,10 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'flareon').learnset.burnup = ['7L1'];
 		this.modData('Learnsets', 'butterfree').learnset.taunt = ['7L1'];
 		this.modData('Learnsets', 'butterfree').learnset.earthpower = ['7L1'];
-	},
-	pokemon: {
-		setStatus: function(status, source = null, sourceEffect, ignoreImmunities) {
-			if (!this.hp) return false;
-			status = this.battle.dex.getEffect(status);
-			if (this.battle.event) {
-				if (!source) source = this.battle.event.source;
-				if (!sourceEffect) sourceEffect = this.battle.effect;
-			}
-			if (!source) source = this;
-	 
-			if (this.status === status.id) {
-				if ((sourceEffect as Move)?.status === this.status) {
-					this.battle.add('-fail', this, this.status);
-				} else if ((sourceEffect as Move)?.status) {
-					this.battle.add('-fail', source);
-					this.battle.attrLastMove('[still]');
-				}
-				return false;
-			}
-	 
-			if (!ignoreImmunities && status.id &&
-					!(source?.hasAbility('corrosion') && ['tox', 'psn'].includes(status.id)) &&
-					!(source?.hasAbility('dustscatter') && ['poisonpowder', 'stunspore'].includes(effect.id))) {
-				// the game currently never ignores immunities
-				if (!this.runStatusImmunity(status.id === 'tox' ? 'psn' : status.id)) {
-					this.battle.debug('immune to status');
-					if ((sourceEffect as Move)?.status) {
-						this.battle.add('-immune', this);
-					}
-					return false;
-				}
-			}
-			const prevStatus = this.status;
-			const prevStatusData = this.statusData;
-			if (status.id) {
-				const result: boolean = this.battle.runEvent('SetStatus', this, source, sourceEffect, status);
-				if (!result) {
-					this.battle.debug('set status [' + status.id + '] interrupted');
-					return result;
-				}
-			}
-	 
-			this.status = status.id;
-			this.statusData = {id: status.id, target: this};
-			if (source) this.statusData.source = source;
-			if (status.duration) this.statusData.duration = status.duration;
-			if (status.durationCallback) {
-				this.statusData.duration = status.durationCallback.call(this.battle, this, source, sourceEffect);
-			}
-	 
-			if (status.id && !this.battle.singleEvent('Start', status, this.statusData, this, source, sourceEffect)) {
-				this.battle.debug('status start [' + status.id + '] interrupted');
-				// cancel the setstatus
-				this.status = prevStatus;
-				this.statusData = prevStatusData;
-				return false;
-			}
-			if (status.id && !this.battle.runEvent('AfterSetStatus', this, source, sourceEffect, status)) {
-				return false;
-			}
-			return true;
-		}
+		this.modData('Learnsets', 'chandelure').learnset.mindblown = ['7L1'];
+		this.modData('Learnsets', 'gothitelle').learnset.wish = ['7L1'];
+		this.modData('Learnsets', 'gothitelle').learnset.teleport = ['7L1'];
+		this.modData('Learnsets', 'gothitelle').learnset.doomdesire = ['7L1'];
+		this.modData('Learnsets', 'gothitelle').learnset.flashcannon = ['7L1'];
 	},
 };
