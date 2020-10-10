bf104627fa707ce559bfca2aa9f052ffb8713e11
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-10-10 17:20:48 +0100

    Update scripts.ts
    Add new moves to Archeops and Talonflame as per the results of slate 14

--- data/mods/m4av6/scripts.ts
+++ data/mods/m4av6/scripts.ts
@@ -48,5 +48,9 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'reuniclus').learnset.photongeyser = ['7L1'];
 		this.modData('Learnsets', 'reuniclus').learnset.psychoboost = ['7L1'];
 		this.modData('Learnsets', 'boltund').learnset.pursuit = ['8L1'];
+		this.modData('Learnsets', 'archeops').learnset.fireblast = ['7L1'];
+		this.modData('Learnsets', 'archeops').learnset.dualwingbeat = ['7L1'];
+		this.modData('Learnsets', 'archeops').learnset.bravebird = ['7L1'];
+		this.modData('Learnsets', 'talonflame').learnset.scorchingsands = ['7L1'];
 	},
 };
