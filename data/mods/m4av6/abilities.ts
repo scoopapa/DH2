4417348ddef906e142db47cc2cc04fca0fe44afb
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-10-16 21:18:19 +0100

    Update abilities.ts
    why did I forget how this Ability worked until now
    this was such a silly error

--- data/mods/m4av6/abilities.ts
+++ data/mods/m4av6/abilities.ts
@@ -200,7 +200,8 @@ export const BattleAbilities: {[k: string]: ModdedAbilityData} = {
 			this.field.setTerrain('grassyterrain');
 		},
 		onAnyTerrainStart(target, source, terrain) {
-			if(this.field.isTerrain('grassyterrain') && !source.hasAbility('arenarock')){
+			if (source !=== this.effectData.target && !source.hasAbility('arenarock')) {
+				this.field.clearTerrain();
 				this.field.setTerrain('grassyterrain');
 			}
 		},
