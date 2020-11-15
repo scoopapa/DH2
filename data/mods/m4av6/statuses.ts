e300dfad5d7add60aeaaabe1101237804c5f5138
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-11-15 18:25:13 +0000

    Create statuses.ts
    Add Acidic Terrain properly (oops, the other effect had the wrong name because of how it was stored XP)

---
+++ data/mods/m4av6/statuses.ts
@@ -0,0 +1,57 @@
+'use strict';
+
+exports.BattleStatuses = {
+
+	acidicterrain: {
+		name: 'Acidic Terrain',
+		effectType: 'Terrain',
+		duration: 5,
+		durationCallback(source, effect) {
+			if (source?.hasItem('terrainextender')) {
+				return 8;
+			}
+			return 5;
+		},
+		onBasePowerPriority: 6,
+		onBasePower(basePower, attacker, defender, move) {
+			if (move.type === 'Poison' && attacker.isGrounded() && !attacker.isSemiInvulnerable()) {
+				for (const target of this.getAllActive()) {
+					if (target.hasAbility('downtoearth')) {
+						this.add('-message', `${target.name} suppresses the effects of the terrain!`);
+						return;
+					}
+				}
+				this.debug('acidic terrain boost');
+				return this.chainModify([0x14CD, 0x1000]);
+			}
+		},
+		onModifyMovePriority: -5,
+		onModifyMove(attacker, defender, target) {
+			if (move.type === 'Poison' && defender.isGrounded() && !defender.isSemiInvulnerable() && defender.hasType('Steel')) {
+				for (const target of this.getAllActive()) {
+					if (target.hasAbility('downtoearth')) {
+						this.add('-message', `${target.name} suppresses the effects of the terrain!`);
+						return;
+					}
+				}
+				if (!move.ignoreImmunity) move.ignoreImmunity = {};
+				if (move.ignoreImmunity !== true) {
+					move.ignoreImmunity['Steel'] = true;
+				}
+			}
+		},
+		onStart(battle, source, effect) {
+			if (effect?.effectType === 'Ability') {
+				this.add('-fieldstart', 'move: Acidic Terrain', '[from] ability: ' + effect, '[of] ' + source);
+			} else {
+				this.add('-fieldstart', 'move: Acidic Terrain');
+			}
+		},
+		onResidualOrder: 21,
+		onResidualSubOrder: 2,
+		onEnd() {
+			this.add('-fieldend', 'move: Acidic Terrain');
+		},
+  },
+
+};
