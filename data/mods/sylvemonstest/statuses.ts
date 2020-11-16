fa49478b43568b029d748a8d587f2c8df677ac26
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-11-16 14:53:28 +0000

    Update statuses.ts
    This?

--- data/mods/sylvemonstest/statuses.ts
+++ data/mods/sylvemonstest/statuses.ts
@@ -1,4 +1,29 @@
 export const BattleStatuses: {[k: string]: ModdedStatusData} = {
+
+	par: {
+		name: 'par',
+		effectType: 'Status',
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'par', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+			} else {
+				this.add('-status', target, 'par');
+			}
+		},
+		onModifySpe(spe, pokemon) {
+			if (!pokemon.hasAbility('quickfeet')) {
+				return this.chainModify(0.5);
+			}
+		},
+		onBeforeMovePriority: 1,
+		onBeforeMove(pokemon) {
+			if (pokemon.hasAbility('quickfeet')) return;
+			if (this.randomChance(1, 4)) {
+				this.add('cant', pokemon, 'par');
+				return false;
+			}
+		},
+	},
 	hail: {
 		effectType: 'Weather',
 		duration: 5,
