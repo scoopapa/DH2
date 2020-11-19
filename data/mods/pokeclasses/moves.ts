6dc8560253d233e7c30e0819d2320b5935c73ef9
Petuuuhhh <pjswan8691@ung.edu>
2020-11-18 20:08:49 -0500

    warrior/mage/thief

--- data/mods/pokeclasses/moves.ts
+++ data/mods/pokeclasses/moves.ts
@@ -24,6 +24,7 @@ export const BattleMovedex: {[k: string]: ModdedMoveData} = {
 		effect: {
 			duration: 5,
 			durationCallback(target, source, effect) {
+				if (source.pokeClass === 'mage') this.effectData.sourceClass = 'mage';
 				if (source?.hasItem('lightclay')) {
 					return 8;
 				}
@@ -34,7 +35,8 @@ export const BattleMovedex: {[k: string]: ModdedMoveData} = {
 					if (!target.getMoveHitData(move).crit && !move.infiltrates) {
 						this.debug('Reflect weaken');
 						if (target.side.active.length > 1) return this.chainModify([0xAAC, 0x1000]);
-						return this.chainModify(0.5);
+						if (this.effectData.sourceClass === 'mage') return this.chainModify(0.45);
+						else return this.chainModify(0.5);
 					}
 				}
 			},
@@ -52,6 +54,52 @@ export const BattleMovedex: {[k: string]: ModdedMoveData} = {
 		type: "Psychic",
 		zMove: {boost: {def: 1}},
 		contestType: "Clever",
+	},
+	lightscreen: {
+		num: 113,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		desc: "For 5 turns, the user and its party members take 0.5x damage from special attacks, or 0.66x damage if in a Double Battle. Damage is not reduced further with Aurora Veil. Critical hits ignore this effect. It is removed from the user's side if the user or an ally is successfully hit by Brick Break, Psychic Fangs, or Defog. Lasts for 8 turns if the user is holding Light Clay. Fails if the effect is already active on the user's side.",
+		shortDesc: "For 5 turns, special damage to allies is halved.",
+		name: "Light Screen",
+		pp: 30,
+		priority: 0,
+		flags: {snatch: 1},
+		sideCondition: 'lightscreen',
+		effect: {
+			duration: 5,
+			durationCallback(target, source, effect) {
+				if (source.pokeClass === 'mage') this.effectData.sourceClass = 'mage';
+				if (source?.hasItem('lightclay')) {
+					return 8;
+				}
+				return 5;
+			},
+			onAnyModifyDamage(damage, source, target, move) {
+				if (target !== source && target.side === this.effectData.target && this.getCategory(move) === 'Special') {
+					if (!target.getMoveHitData(move).crit && !move.infiltrates) {
+						this.debug('Light Screen weaken');
+						if (target.side.active.length > 1) return this.chainModify([0xAAC, 0x1000]);
+						if (this.effectData.sourceClass === 'mage') return this.chainModify(0.45);
+						else return this.chainModify(0.5);
+					}
+				}
+			},
+			onStart(side) {
+				this.add('-sidestart', side, 'move: Light Screen');
+			},
+			onResidualOrder: 21,
+			onResidualSubOrder: 1,
+			onEnd(side) {
+				this.add('-sideend', side, 'move: Light Screen');
+			},
+		},
+		secondary: null,
+		target: "allySide",
+		type: "Psychic",
+		zMove: {boost: {spd: 1}},
+		contestType: "Beautiful",
 	},
 	
 	
