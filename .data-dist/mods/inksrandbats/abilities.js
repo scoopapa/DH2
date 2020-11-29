3d782aca7d3ff3d59565a816420cf8767f2d9e4c
inkbug1 <72170979+inkbug1@users.noreply.github.com>
2020-11-29 13:48:12 -0800

    a
    a

---
+++ .data-dist/mods/inksrandbats/abilities.js
@@ -0,0 +1,77 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); const Abilities = {
+	iceface: {
+		onStart(pokemon) {
+			if (this.field.isWeather('hail') && pokemon.species.id === 'eiscuenoice' && !pokemon.transformed) {
+				this.add('-activate', pokemon, 'ability: Ice Face');
+				this.effectData.busted = false;
+				pokemon.formeChange('Eiscue', this.effect, true);
+			}
+			else if (this.field.isWeather('hail') && pokemon.species.id === 'castform' && !pokemon.transformed) {
+				this.add('-activate', pokemon, 'ability: Ice Face');
+				this.effectData.busted = false;
+				pokemon.formeChange('Castform-Snowy', this.effect, true);
+			}
+		},
+		onDamagePriority: 1,
+		onDamage(damage, target, source, effect) {
+			if (
+				effect && effect.effectType === 'Move' && effect.category === 'Physical' &&
+				 target.species.id === 'eiscue' || target.species.id === 'castformsnowy'  && !target.transformed
+			) {
+				this.add('-activate', target, 'ability: Ice Face');
+				this.effectData.busted = true;
+				return 0;
+			}
+		},
+		onCriticalHit(target, type, move) {
+			if (!target) return;
+			if (move.category !== 'Physical' ||  target.species.id === 'eiscuenoice' || target.species.id === 'castform'  || target.transformed) return;
+			if (target.volatiles['substitute'] && !(move.flags['authentic'] || move.infiltrates)) return;
+			if (!target.runImmunity(move.type)) return;
+			return false;
+		},
+		onEffectiveness(typeMod, target, type, move) {
+			if (!target) return;
+			if (move.category !== 'Physical' || target.species.id === 'eiscuenoice' || target.species.id === 'castform' || target.transformed) return;
+			if (target.volatiles['substitute'] && !(move.flags['authentic'] || move.infiltrates)) return;
+			if (!target.runImmunity(move.type)) return;
+			return 0;
+		},
+		onUpdate(pokemon) {
+			if (pokemon.species.id === 'eiscue' && this.effectData.busted) {
+				pokemon.formeChange('Eiscue-Noice', this.effect, true);
+			}
+			else if (pokemon.species.id === 'castformsnowy' && this.effectData.busted) {
+				pokemon.formeChange('Castform', this.effect, true);
+			}
+		},
+		onAnyWeatherStart() {
+			const pokemon = this.effectData.target;
+			if (this.field.isWeather('hail') && pokemon.species.id === 'eiscuenoice' && !pokemon.transformed) {
+				this.add('-activate', pokemon, 'ability: Ice Face');
+				this.effectData.busted = false;
+				pokemon.formeChange('Eiscue', this.effect, true);
+			}
+			if (this.field.isWeather('hail') && pokemon.species.id === 'castform' && !pokemon.transformed) {
+				this.add('-activate', pokemon, 'ability: Ice Face');
+				this.effectData.busted = false;
+				pokemon.formeChange('Castform-Snowy', this.effect, true);
+			}
+		},
+		isPermanent: true,
+		name: "Ice Face",
+		rating: 3,
+		num: 248,
+	},
+	
+	snowwarning: {
+		onStart(source) {
+			this.field.weatherData.layers ++; 
+			this.add('-activate', source, 'ability: Snow Warning');
+			this.hint("The storm has worsened!");
+		},
+		name: "Snow Warning",
+		rating: 4,
+		num: 117,
+	},
+}; exports.Abilities = Abilities;
---
+++ .data-dist/mods/inksrandbats/conditions.js
@@ -0,0 +1,167 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); const Conditions = {
+	hail: {
+		name: 'Hail',
+		effectType: 'Weather',
+		duration: 0, 
+		onStart(battle) {
+			this.add('-weather', 'Hail');
+			if (!battle.activeTurns) {
+				this.effectData.layers = 1; 
+			}
+			this.effectData.stage = 0;
+		},
+		onResidualOrder: 1,
+		onResidual() {
+			this.add('-weather', 'Hail', '[upkeep]');
+			if (this.field.isWeather('hail')) this.eachEvent('Weather');
+			this.effectData.stage++;
+			
+			if (this.effectData.stage > 2) {
+				if (this.randomChance(1,4)) {
+					this.effectData.layers = 0; 
+					this.hint("The snow falls gently.");
+				} else if (this.randomChance(1,4)) {
+					this.effectData.layers = 1; 
+					this.hint("Hail is falling on the battlefield.");
+				} else if (this.randomChance(1,4)) {
+					this.effectData.layers = 2; 
+					this.hint("The hail is getting treacherous.");
+				} else {
+					this.effectData.layers = 3; 
+					this.hint("The hailstorm is blowing wildly!");
+				}
+				this.effectData.stage = 0;
+			}
+		},
+		onWeather(target) {
+
+			this.damage(target.baseMaxhp * (this.effectData.layers / 16));
+		},
+			
+	},
+	
+	sandstorm: {
+		name: 'Sandstorm',
+		effectType: 'Weather',
+		duration: 0, 
+		// This should be applied directly to the stat before any of the other modifiers are chained
+		// So we give it increased priority.
+		onModifySpDPriority: 10,
+		onModifySpD(spd, pokemon) {
+			if (pokemon.hasType('Rock') && this.field.isWeather('sandstorm')) {
+				return this.modify(spd, 1.5);
+			}
+		},
+		onStart(battle) {
+			this.add('-weather', 'Sandstorm');
+		},
+		onResidualOrder: 1,
+		onResidual() {
+			this.add('-weather', 'Sandstorm', '[upkeep]');
+			if (this.field.isWeather('sandstorm')) this.eachEvent('Weather');
+		},
+		onWeather(target) {
+			this.damage(target.baseMaxhp / 16);
+		},
+		onEnd() {
+			this.add('-weather', 'none');
+		},
+	},
+	
+	sunnyday: {
+		name: 'SunnyDay',
+		effectType: 'Weather',
+		duration: 0,
+		onWeatherModifyDamage(damage, attacker, defender, move) {
+			if (defender.hasItem('utilityumbrella')) return;
+			if (move.type === 'Fire') {
+				this.debug('Sunny Day fire boost');
+				return this.chainModify(1.5);
+			}
+			if (move.type === 'Water') {
+				this.debug('Sunny Day water suppress');
+				return this.chainModify(0.5);
+			}
+		},
+		onStart(battle) {
+			this.add('-weather', 'SunnyDay');
+		},
+		onImmunity(type, pokemon) {
+			if (pokemon.hasItem('utilityumbrella')) return;
+			if (type === 'frz') return false;
+		},
+		onResidualOrder: 1,
+		onResidual() {
+			this.add('-weather', 'SunnyDay', '[upkeep]');
+			this.eachEvent('Weather');
+		},
+		onEnd() {
+			this.add('-weather', 'none');
+		},
+	},
+	
+	raindance: {
+		name: 'RainDance',
+		effectType: 'Weather',
+		duration: 0,
+		onWeatherModifyDamage(damage, attacker, defender, move) {
+			if (defender.hasItem('utilityumbrella')) return;
+			if (move.type === 'Water') {
+				this.debug('rain water boost');
+				return this.chainModify(1.5);
+			}
+			if (move.type === 'Fire') {
+				this.debug('rain fire suppress');
+				return this.chainModify(0.5);
+			}
+		},
+		onStart(battle) {
+			this.add('-weather', 'RainDance');
+		},
+		onResidualOrder: 1,
+		onResidual() {
+			this.add('-weather', 'RainDance', '[upkeep]');
+			this.eachEvent('Weather');
+		},
+		onEnd() {
+			this.add('-weather', 'none');
+		},
+	},
+	
+	
+	frz: {
+		name: 'frz',
+		effectType: 'Status',
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'frz', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+			} else {
+				this.add('-status', target, 'frz');
+			}
+			if (target.species.name === 'Shaymin-Sky' && target.baseSpecies.baseSpecies === 'Shaymin') {
+				target.formeChange('Shaymin', this.effect, true);
+			}
+		},
+		onBeforeMovePriority: 10,
+		onBeforeMove(pokemon, target, move) {
+			if (move.flags['defrost']) return;
+			if (this.randomChance(1, 5)) {
+				pokemon.cureStatus();
+				return;
+			}
+			this.add('cant', pokemon, 'frz');
+			return false;
+		},
+		onModifyMove(move, pokemon) {
+			if (move.flags['defrost']) {
+				this.add('-curestatus', pokemon, 'frz', '[from] move: ' + move);
+				pokemon.setStatus('');
+			}
+		},
+		onHit(target, source, move) {
+			if (move.thawsTarget || move.type === 'Fire' && move.category !== 'Status') {
+				target.cureStatus();
+			}
+		},
+	},
+}; exports.Conditions = Conditions;
