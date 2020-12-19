1b90d81821b1fffcbd477f0d09225255b10549bc
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-12-18 22:16:40 -0700

    Create formats-data.ts
    Consistency with CS2 because they appear to have their Unown working

---
+++ data/mods/smashmodsmelee/formats-data.ts
@@ -0,0 +1,86 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	vanilluxe: {
+		tier: "Melee",
+	},
+	vanilluxemega: {
+		tier: "Melee",
+	},
+	raichu: {
+		tier: "Melee",
+	},
+	raichumega: {
+		tier: "Melee",
+	},
+	garbodor: {
+		tier: "Melee",
+	},
+	garbodormega: {
+		tier: "Melee",
+	},
+	trubbishmarshadow: {
+		tier: "Melee",
+	},
+	beheeyem: {
+		tier: "Melee",
+	},
+	pyukchin: {
+		tier: "Melee",
+	},
+	thundahi: {
+		tier: "Melee",
+	},
+	miltank: {
+		tier: "Melee",
+	},
+	camomander: {
+		tier: "Melee",
+	},
+	moltres-galar: {
+		tier: "Melee",
+	},
+	copperajah-forge: {
+		tier: "Melee",
+	},
+	claydol: {
+		tier: "Melee",
+	},
+	unown: {
+		tier: "Melee",
+	},
+	// unownm: {
+		// tier: "Melee",
+	// },
+	watervellumental: {
+		tier: "Melee",
+	},
+	escavalier: {
+		tier: "Melee",
+	},
+	lemotic: {
+		tier: "Melee",
+	},
+	joltry: {
+		tier: "Melee",
+	},
+	magmacroak: {
+		tier: "Melee",
+	},
+	floraflare: {
+		tier: "Melee",
+	},
+	torgeist: {
+		tier: "Melee",
+	},
+	lycanrocdusk: {
+		tier: "Melee",
+	},
+	roseradescarfed: {
+		tier: "Melee",
+	},
+	gladiaster: {
+		tier: "Melee",
+	},
+	hypnihil: {
+		tier: "Melee",
+	},
+};
