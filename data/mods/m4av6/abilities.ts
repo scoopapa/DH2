37de174a844cf9a795fd37345305e059c5173979
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-12-18 21:59:11 -0700

    Update abilities.ts
    Loophole

--- data/mods/m4av6/abilities.ts
+++ data/mods/m4av6/abilities.ts
@@ -1324,9 +1324,9 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 		num: -1043,
 	},
 	lasttoxin: {
-		onSourceDamagingHit(target, source, damage, effect) {
-			if (!target || !target.hp) return;
-			if (target !== source && target.hp <= target.maxhp / 2 && target.hp + damage > target.maxhp / 2) {
+		onAnyDamagingHit(target, source, damage, effect) {
+			if (!target || !target.hp || target === source || source !== this.effectData.target) return;
+			if (target.hp <= target.maxhp / 2 && target.hp + damage > target.maxhp / 2) {
 				target.trySetStatus('tox', source);
 			}
 		},
