43cfa28065e569124220d0afdedeca9d8ccf998d
scoopapa <pccompositor@aim.com>
2020-12-20 23:24:54 -0500

    Update formats-data.ts

--- data/mods/megarevolution/formats-data.ts
+++ data/mods/megarevolution/formats-data.ts
@@ -2,4 +2,13 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 	arbosaur: {
 		tier:'OU';
 	},
+	charbarian: {
+		tier:'OU';
+	},
+	chargaryen: {
+		tier:'OU';
+	},
+	galapagon: {
+		tier:'OU';
+	},
 };
