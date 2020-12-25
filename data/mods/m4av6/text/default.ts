fb321023dd63f648beb2d54f11656128a552e301
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-12-24 18:40:07 -0700

    Create default.ts
    Will this work actually

---
+++ data/mods/m4av6/text/default.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+export const DefaultText: {[k: string]: DefaultText} = {
+	desertgales: {
+		weatherName: "Desert Gales",
+		start: "  Desert gales kicked up!",
+		end: "  The desert gales subsided.",
+		upkeep: "  (The desert gales are raging.)",
+	},
+};
