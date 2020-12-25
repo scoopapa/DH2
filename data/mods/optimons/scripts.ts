fe243036590704be9bbeaaaa3873fb0e7e83bd95
scoopapa <pccompositor@aim.com>
2020-12-24 22:38:39 -0500

    fix formats

--- data/mods/optimons/scripts.ts
+++ data/mods/optimons/scripts.ts
@@ -3,7 +3,7 @@ export const Scripts: ModdedBattleScriptsData = {
 		const addNewMoves = (pokemonid: string, moveids: string[]) => {
 			for (const moveid of moveids) {
 				console.log( moveid );
-				this.modData('Learnsets', toID(pokemonid)).learnset[moveid] = [moveid === 'dracometeor' || moveid === 'steelbeam' ? '8T' : '8M'];
+				this.modData('Learnsets', pokemonid).learnset[moveid] = [moveid === 'dracometeor' || moveid === 'steelbeam' ? '8T' : '8M'];
 			}
 		};
 		addNewMoves('inteleon', ['taunt', 'encore', 'hypervoice', 'psychic', 'sludgewave']);
