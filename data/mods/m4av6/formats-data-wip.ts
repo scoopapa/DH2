87e3031a4b71c72cecba7b8550db39d9461b6b8e
park <72170979+inkbug1@users.noreply.github.com>
2021-01-01 18:03:32 -0800

    Create formats-data-wip.ts

---
+++ data/mods/m4av6/formats-data-wip.ts
@@ -0,0 +1,779 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+  abomasnow: {
+    tier: "PU", 
+  },
+  abra: {
+    tier: "LC", 
+  },
+  absol: {
+    tier: "PU", 
+  },
+  absolmega: { //NDEX
+    tier: "", 
+  },
+  accelgor: {
+    tier: "PU", 
+  },
+  aegislash: {
+    tier: "UUBL", 
+  },
+  aegislashblade: {
+    tier: "UUBL", 
+  },
+  aerodactyl: {
+    tier: "NU", 
+  },
+  aerodactylmega: { //NDEX
+    tier: "", 
+  },
+  aggron: {
+    tier: "PU", 
+  },
+  aggronmega: { //NDEX
+    tier: "", 
+  },
+  aipom: { //NDEX
+    tier: "", 
+  },
+  alakazam: {
+    tier: "", 
+  },
+  alakazammega: { //NDEX
+    tier: "", 
+  },
+  alcremie: {
+    tier: "", 
+  },
+  alcremiegmax: {
+    tier: "AG", 
+  },
+  alomomola: { //NDEX
+    tier: "", 
+  },
+  altaria: {
+    tier: "", 
+  },
+  altariamega: { //NDEX
+    tier: "", 
+  },
+  amaura: {
+    tier: "LC", 
+  },
+  ambipom: { //NDEX
+    tier: "", 
+  },
+  amoongus: {
+    tier: "", 
+  },
+  ampharos: { //NDEX
+    tier: "", 
+  },
+  ampharosmega: { //NDEX
+    tier: "", 
+  },
+  anorith: {
+    tier: "LC", 
+  },
+  appletun: {
+    tier: "", 
+  },
+  appletungmax: {
+    tier: "AG", 
+  },
+  applin: {
+    tier: "LC", 
+  },
+  araquanid: {
+    tier: "", 
+  },
+  araquanidtotem: { //NDEX
+    tier: "", 
+  },
+  arbok: { //ndex
+    tier: "", 
+  },
+  arcanine: {
+    tier: "", 
+  },
+  arceus: { //NDEX
+    tier: "", 
+  },
+  arceusbug: { //NDEX
+    tier: "", 
+  },
+  arceusdark: { //NDEX
+    tier: "", 
+  },
+  arceusdragon: { //NDEX
+    tier: "", 
+  },
+  arceuselectric: { //NDEX
+    tier: "", 
+  },
+  arceusfairy: { //NDEX
+    tier: "", 
+  },
+  arceusfighting: { //NDEX
+    tier: "", 
+  },
+  arceusfire: { //NDEX
+    tier: "", 
+  },
+  arceusflying: { //NDEX
+    tier: "", 
+  },
+  arceusghost: { //NDEX
+    tier: "", 
+  },
+  arceusgrass: { //NDEX
+    tier: "", 
+  },
+  arceusground: { //NDEX
+    tier: "", 
+  },
+  arceusice: { //NDEX
+    tier: "", 
+  },
+  arceuspoison: { //NDEX
+    tier: "", 
+  },
+  arceuspsychic: { //NDEX
+    tier: "", 
+  },
+  arceusrock: { //NDEX
+    tier: "", 
+  },
+  arceussteel: { //NDEX
+    tier: "", 
+  },
+  arceuswater: { //NDEX
+    tier: "", 
+  },
+  archen: { 
+    tier: "LC", 
+  },
+  archeops: { 
+    tier: "", 
+  },
+  arctovish: { 
+    tier: "", 
+  },
+  arctozolt: { 
+    tier: "", 
+  },
+  ariados: { //NDEX
+    tier: "", 
+  },
+  armaldo: { 
+    tier: "", 
+  },
+  aromatisse: { 
+    tier: "", 
+  },
+  aron: { 
+    tier: "LC", 
+  },
+  arrokuda: { 
+    tier: "LC", 
+  },
+  articuno: { 
+    tier: "", 
+  },
+  articunogalar: { 
+    tier: "", 
+  },
+  audino: { 
+    tier: "", 
+  },
+  audinomega: { //NDEX
+    tier: "", 
+  },
+  aurorus: { 
+    tier: "", 
+  },
+  avalugg: { 
+    tier: "", 
+  },
+  axew: { 
+    tier: "LC", 
+  },
+  azelf: { 
+    tier: "", 
+  },
+  azumarill: { 
+    tier: "", 
+  },
+  azurill: { 
+    tier: "LC", 
+  },
+  
+  
+  // B ----------------------------------------------------------------------------------------------------
+  
+  bagon: { 
+    tier: "LC", 
+  },
+  baltoy: { 
+    tier: "LC", 
+  },
+  banette: { //NDEX
+    tier: "", 
+  },
+  banettemega: { //NDEX
+    tier: "", 
+  },
+  barbaracle: { 
+    tier: "", 
+  },
+  barboach: { 
+    tier: "LC", 
+  },
+  barraskewda: { 
+    tier: "", 
+  },
+  basculin: { 
+    tier: "", 
+  },
+  basculinbluestriped: { 
+    tier: "", 
+  },
+  bastiodon: { //NDEX
+    tier: "", 
+  },
+  bayleef: { //NDEX
+    tier: "", 
+  },
+  beartic: { 
+    tier: "", 
+  },
+  beautifly: { //NDEX
+    tier: "", 
+  },
+  beedrill: { //NDEX
+    tier: "", 
+  },
+  beedrillmega: { //ndex
+    tier: "", 
+  },
+  beheeyem: { 
+    tier: "", 
+  },
+  beldum: { 
+    tier: "LC", 
+  },
+  bellossom: { 
+    tier: "", 
+  },
+  bellsprout: { //ndex
+    tier: "LC", 
+  },
+  bergmite: { 
+    tier: "LC", 
+  },
+  bewear: { 
+    tier: "", 
+  },
+  bibarel: { //ndex
+    tier: "", 
+  },
+  bidoof: { //ndex
+    tier: "LC", 
+  },
+  binacle: { 
+    tier: "LC", 
+  },
+  bisharp: { 
+    tier: "", 
+  },
+  blacephalon: { 
+    tier: "", 
+  },
+  blastoise: { 
+    tier: "", 
+  },
+  blastoisegmax: { 
+    tier: "AG", 
+  },
+  blastoisemega: { //NDEX
+    tier: "", 
+  },
+  blaziken: { 
+    tier: "", 
+  },
+  blazikenmega: { //ndex
+    tier: "", 
+  },
+  blipbug: { 
+    tier: "LC", 
+  },
+  blissey: { 
+    tier: "OU", 
+  },
+  blitzle: { //ndex
+    tier: "", 
+  },
+  boldore: { 
+    tier: "NFE", 
+  },
+  boltund: { 
+    tier: "", 
+  },
+  bonsly: { 
+    tier: "LC", 
+  },
+  bouffalant: { 
+    tier: "", 
+  },
+  bounsweet: { 
+    tier: "LC", 
+  },
+  braixen: { //ndex
+    tier: "", 
+  },
+  braviary: { 
+    tier: "", 
+  },
+  breloom: { //ndex
+    tier: "", 
+  },
+  brionne: { 
+    tier: "NFE", 
+  },
+  bronzong: { 
+    tier: "", 
+  },
+  bronzor: { 
+    tier: "LC", 
+  },
+  bruxish: { //ndex
+    tier: "", 
+  },
+  budew: { 
+    tier: "LC", 
+  },
+  buizel: { //ndex
+    tier: "", 
+  },
+  bulbasaur: { 
+    tier: "LC", 
+  },
+  buneary: { 
+    tier: "LC", 
+  },
+  bunnelby: { 
+    tier: "LC", 
+  },
+  burmy: { //ndex
+    tier: "", 
+  },
+  butterfree: { 
+    tier: "", 
+  },
+  butterfreegmax: { 
+    tier: "AG", 
+  },
+  buzzwole: { 
+    tier: "", 
+  },
+  
+  
+  // C -------------------------------------------------------------------------
+  
+  cacnea: { //ndex
+    tier: "", 
+  },
+  cacturne: { //ndex
+    tier: "", 
+  },
+  calyrex: { 
+    tier: "(PU)", 
+  },
+  calyrexice: { 
+    tier: "Uber", 
+  },
+  calyrexshadow: { 
+    tier: "Uber", 
+  },
+  camerupt: { //ndex
+    tier: "", 
+  },
+  cameruptmega: { //ndex
+    tier: "", 
+  },
+  carbink: { 
+    tier: "", 
+  },
+  carkol: { 
+    tier: "NFE", 
+  },
+  carnivine: { //ndex
+    tier: "", 
+  },
+  carracosta: { 
+    tier: "", 
+  },
+  carvanha: { 
+    tier: "LC", 
+  },
+  cascoon: { //ndex
+    tier: "", 
+  },
+  castform: { //ndex
+    tier: "", 
+  },
+  castformrainy: { //ndex
+    tier: "", 
+  },
+  castformsnowy: { //ndex
+    tier: "", 
+  },
+  castformsunny: { //ndex
+    tier: "", 
+  },
+  caterpie: { 
+    tier: "LC", 
+  },
+  celebi: { 
+    tier: "", 
+  },
+  celesteela: { 
+    tier: "", 
+  },
+  centiskorch: { 
+    tier: "", 
+  },
+  centiskorchgmax: { 
+    tier: "AG", 
+  },
+  chandelure: { 
+    tier: "", 
+  },
+  chansey: { 
+    tier: "", 
+  },
+  charizard: { 
+    tier: "", 
+  },
+  charizardgmax: { 
+    tier: "AG", 
+  },
+  charizardmegax: { //ndex
+    tier: "", 
+  },
+  charizardmegay: { //ndex
+    tier: "", 
+  },
+  charjabug: { 
+    tier: "NFE", 
+  },
+  charmander: { 
+    tier: "LC", 
+  },
+  charmeleon: { 
+    tier: "NFE", 
+  },
+  chatot: { //ndex
+    tier: "", 
+  },
+  cherrim: { 
+    tier: "", 
+  },
+  cherrimsunshine: { 
+    tier: "", 
+  },
+  cherubi: { 
+    tier: "LC Uber", 
+  },
+  chesnaught: { //ndex
+    tier: "", 
+  },
+  chespin: { //ndex
+    tier: "LC", 
+  },
+  chewtle: { 
+    tier: "LC", 
+  },
+  chikorita: { //ndex
+    tier: "LC", 
+  },
+  chimchar: { //ndex
+    tier: "LC", 
+  },
+  chimecho: { //ndex
+    tier: "", 
+  },
+  chinchou: { 
+    tier: "LC", 
+  },
+  chingling: { //ndex
+    tier: "LC", 
+  },
+  cinccino: { 
+    tier: "", 
+  },
+  cinderace: { 
+    tier: "Uber", 
+  },
+  cinderacegmax: { 
+    tier: "AG", 
+  },
+  clamperl: { //ndex
+    tier: "", 
+  },
+  clauncher: { 
+    tier: "LC", 
+  },
+  clawitzer: { 
+    tier: "", 
+  },
+  claydol: { 
+    tier: "", 
+  },
+  clefable: { 
+    tier: "", 
+  },
+  clefairy: { 
+    tier: "", 
+  },
+  cleffa: { 
+    tier: "LC", 
+  },
+  clobbopus: { 
+    tier: "LC", 
+  },
+  cloyster: { 
+    tier: "", 
+  },
+  coalossal: { 
+    tier: "", 
+  },
+  coalossalgmax: { 
+    tier: "AG", 
+  },
+  cobalion: { 
+    tier: "", 
+  },
+  cofagrigus: { 
+    tier: "", 
+  },
+  combee: { 
+    tier: "LC", 
+  },
+  combusken: { 
+    tier: "NFE", 
+  },
+  comfey: { 
+    tier: "", 
+  },
+  conkeldurr: { 
+    tier: "", 
+  },
+  copperajah: { 
+    tier: "", 
+  },
+  copperajahgmax: { 
+    tier: "AG", 
+  },
+  corphish: { 
+    tier: "LC", 
+  },
+  corsola: { 
+    tier: "", 
+  },
+  corsolagalar: { 
+    tier: "", 
+  },
+  corviknight: { 
+    tier: "", 
+  },
+  corviknightgmax: { 
+    tier: "AG", 
+  },
+  corvisquire: { 
+    tier: "NFE", 
+  },
+  cosmoem: { 
+    tier: "NFE", 
+  },
+  cosmog: { 
+    tier: "LC", 
+  },
+  cottonee: { 
+    tier: "LC", 
+  },
+  crabominable: { //ndex
+    tier: "", 
+  },
+  crabrawler: { //ndex
+    tier: "", 
+  },
+  cradily: { 
+    tier: "", 
+  },
+  cramorant: { 
+    tier: "", 
+  },
+  cranidos: { //ndex
+    tier: "", 
+  },
+  crawdaunt: { 
+    tier: "", 
+  },
+  cresselia: { 
+    tier: "", 
+  },
+  croagunk: { 
+    tier: "LC", 
+  },
+  crobat: { 
+    tier: "", 
+  },
+  croconaw: { //ndex
+    tier: "NFE", 
+  },
+  crustle: { 
+    tier: "", 
+  },
+  cryogonal: { 
+    tier: "", 
+  },
+  cubchoo: { 
+    tier: "LC", 
+  },
+  cubone: { 
+    tier: "LC", 
+  },
+  cursola: { 
+    tier: "", 
+  },
+  cutiefly: { 
+    tier: "LC Uber", 
+  },
+  cyndaquil: { //ndex
+    tier: "LC", 
+  },
+  
+  
+  // D --------------------------------------------------------------------------------
+  
+  darkrai: { //ndex
+    tier: "Uber", 
+  },
+  darmanitan: { 
+    tier: "", 
+  },
+  darmanitangalar: { 
+    tier: "Uber", 
+  },
+  darmanitangalarzen: { 
+    tier: "Uber", 
+  },
+  darmanitanzen: { 
+    tier: "", 
+  },
+  dartrix: { 
+    tier: "NFE", 
+  },
+  darumaka: { 
+    tier: "LC", 
+  },
+  darumakagalar: { 
+    tier: "LC", 
+  },
+  decidueye: { 
+    tier: "", 
+  },
+  dedenne: { 
+    tier: "", 
+  },
+  deerling: { //ndex
+    tier: "", 
+  },
+  deino: { 
+    tier: "LC", 
+  },
+  delcatty: { //ndex
+    tier: "", 
+  },
+  delibird: { 
+    tier: "", 
+  },
+  delphox: { //ndex
+    tier: "", 
+  },
+  deoxys: { //ndex
+    tier: "", 
+  },
+  deoxysattack: { //ndex
+    tier: "", 
+  },
+  deoxysdefense: { //ndex
+    tier: "", 
+  },
+  deoxysspeed: { //ndex
+    tier: "", 
+  },
+  dewgong: { //ndex
+    tier: "", 
+  },
+  dewott: { //ndex
+    tier: "NFE", 
+  },
+  dewpider: { 
+    tier: "LC", 
+  },
+  dhelmise: { 
+    tier: "", 
+  },
+  dialga: { 
+    tier: "Uber", 
+  },
+  diancie: { 
+    tier: "", 
+  },
+  dianciemega: { //ndex
+    tier: "", 
+  },
+  diggersby: { 
+    tier: "", 
+  },
+  diglett: { 
+    tier: "LC", 
+  },
+  diglettalola: { 
+    tier: "LC", 
+  },
+  ditto: { 
+    tier: "", 
+  },
+  dodrio: { //ndex
+    tier: "", 
+  },
+  doduo: { //ndex
+    tier: "LC", 
+  },
+  donphan: { //ndex
+    tier: "", 
+  },
+  dottler: { 
+    tier: "NFE", 
+  },
+  doublade: { 
+    tier: "", 
+  },
+  dracovish: { 
+    tier: "Uber", 
+  },
+  dracozolt: { 
+    tier: "", 
+  },
+  dragalge: { 
+    tier: "", 
+  },
+  dragapult: { 
+    tier: "Uber", 
+  },
+  dragonair: { 
+    tier: "NFE", 
+  },
+};
