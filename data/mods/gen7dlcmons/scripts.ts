16d539c87053a6b37c734c0192faa69f118a3b2b
Petuuuhhh <pjswan8691@ung.edu>
2021-01-03 20:18:12 -0500

    fix prism leftovers

--- data/mods/gen7dlcmons/scripts.ts
+++ data/mods/gen7dlcmons/scripts.ts
@@ -1,8 +1,8 @@
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 7,
 	init: function () {
-		for (const id in this.dataCache.Formats['gen5prism'].banlist) {
-            this.dataCache.FormatsData[this.toID(this.dataCache.Formats['gen5prism'].banlist[id])].tier = "Illegal";
+		for (const id in this.dataCache.Formats['gen7dlcmons'].banlist) {
+            this.dataCache.FormatsData[this.toID(this.dataCache.Formats['gen7dlcmons'].banlist[id])].tier = "Illegal";
 		}
 		this.modData('Learnsets', 'kommoo').learnset.clangoroussoul = ['7T'];
 	},
