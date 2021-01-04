b923880607d5ebd75b0949d2637bdd9d5df729a4
Petuuuhhh <pjswan8691@ung.edu>
2021-01-03 21:03:51 -0500

    dlcmons script

--- data/mods/gen7dlcmons/scripts.ts
+++ data/mods/gen7dlcmons/scripts.ts
@@ -1,8 +1,12 @@
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 7,
 	init: function () {
-		for (const id in this.dataCache.Formats['gen7dlcmons'].banlist) {
-            this.dataCache.FormatsData[this.toID(this.dataCache.Formats['gen7dlcmons'].banlist[id])].tier = "Illegal";
+		for (const id in this.dataCache.Pokedex) {
+			do {
+				console.log(id);
+				if (this.dataCache.FormatsData[id] !== undefined) this.dataCache.FormatsData[id].tier = "Illegal";
+			}
+			while (!this.dataCache.Pokedex[id].name in this.dataCache.Formats['gen7dlcmons'].unbanlist);
 		}
 		this.modData('Learnsets', 'kommoo').learnset.clangoroussoul = ['7T'];
 	},
