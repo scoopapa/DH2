1558f1dc25576591b9c4440bfeb1583e85e120e9
Petuuuhhh <pjswan8691@ung.edu>
2021-01-03 21:46:43 -0500

    fix dlcmons script

--- data/mods/gen7dlcmons/scripts.ts
+++ data/mods/gen7dlcmons/scripts.ts
@@ -2,11 +2,11 @@ export const Scripts: ModdedBattleScriptsData = {
 	gen: 7,
 	init: function () {
 		for (const id in this.dataCache.Pokedex) {
-			do {
-				console.log(id);
+			let unbanlist = this.dataCache.Formats['gen7dlcmons'].unbanlist;
+			let speciesName = this.dataCache.Pokedex[id].name;
+			if (!unbanlist.includes(speciesName)) {
 				if (this.dataCache.FormatsData[id] !== undefined) this.dataCache.FormatsData[id].tier = "Illegal";
 			}
-			while (!this.dataCache.Pokedex[id].name in this.dataCache.Formats['gen7dlcmons'].unbanlist);
 		}
 		this.modData('Learnsets', 'kommoo').learnset.clangoroussoul = ['7T'];
 	},
