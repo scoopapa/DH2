5fdf41363e1c1670210b601c4763daec7e8a2f98
Petuuuhhh <pjswan8691@ung.edu>
2021-01-03 20:17:12 -0500

    dlcmons script

--- data/mods/gen7dlcmons/scripts.ts
+++ data/mods/gen7dlcmons/scripts.ts
@@ -1,6 +1,9 @@
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 7,
 	init: function () {
+		for (const id in this.dataCache.Formats['gen5prism'].banlist) {
+            this.dataCache.FormatsData[this.toID(this.dataCache.Formats['gen5prism'].banlist[id])].tier = "Illegal";
+		}
 		this.modData('Learnsets', 'kommoo').learnset.clangoroussoul = ['7T'];
 	},
 };
