3ff7501bb3cacd04ecb669b0c58b45bdcbbb78c3
Petuuuhhh <pjswan8691@ung.edu>
2021-01-03 20:06:22 -0500

    fix scripts for prism

--- data/mods/prism/scripts.ts
+++ data/mods/prism/scripts.ts
@@ -3,7 +3,7 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 	gen: 5,
 	init: function () {
 		for (const id in this.dataCache.Formats['gen5prism'].banlist) {
-            this.dataCache.FormatsData[this.toID(id)] = { tier: "Illegal"};
+            this.dataCache.FormatsData[this.toID(this.dataCache.Formats['gen5prism'].banlist[id])].tier = "Illegal";
 		}
 		for (let i in this.data.Pokedex) {
 			delete this.data.Pokedex[i].abilities['H'];
