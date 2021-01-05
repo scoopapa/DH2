08965638099a3f15f419e306f1800f6e1bd6fdd0
scoopapa <pccompositor@aim.com>
2021-01-05 12:15:11 -0500

    test

--- data/mods/gen7dlcmons/scripts.ts
+++ data/mods/gen7dlcmons/scripts.ts
@@ -5,7 +5,7 @@ export const Scripts: ModdedBattleScriptsData = {
 			let unbanlist = this.dataCache.Formats['gen7dlcmons'].unbanlist;
 			let speciesName = this.dataCache.Pokedex[id].name;
 			if (!unbanlist.includes(speciesName)) {
-				if (this.dataCache.FormatsData[id] !== undefined) this.dataCache.FormatsData[id].tier = "Illegal";
+				// if (this.dataCache.FormatsData[id] !== undefined) this.dataCache.FormatsData[id].tier = "Illegal";
 			}
 		}
 		this.modData('Learnsets', 'kommoo').learnset.clangoroussoul = ['7T'];
