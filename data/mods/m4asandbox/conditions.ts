1ea0f7448783f57b6ca08d4415bcdc92267d812f
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-10 02:13:51 -0700

    Update conditions.ts

--- data/mods/m4asandbox/conditions.ts
+++ data/mods/m4asandbox/conditions.ts
@@ -1,14 +1,77 @@
 export const Conditions: {[k: string]: ConditionData} = {
+	reverberation1: {
+		duration: 3,
+		onResidualOrder: 1,
+		onResidual(pokemon) {
+			console.log(this.effectData.duration);
+			console.log(this.effectData.source.name);
+			console.log(this.effectData.moveid);
+			if (this.effectData.duration === 2) {
+				if (this.effectData.source.isActive && this.effectData.source.hasAbility('reverberation')) {
+					this.effectData.source.addVolatile('reverberationTurn1');
+					this.useMove(this.effectData.moveid, this.effectData.source);
+				}
+			},
+			if (this.effectData.duration === 1) {
+				if (this.effectData.source.isActive && this.effectData.source.hasAbility('reverberation')) {
+					this.effectData.source.addVolatile('reverberationTurn2');
+					this.useMove(this.effectData.moveid, this.effectData.source);
+				}
+			},
+		},
+	},
+	reverberation2: {
+		duration: 3,
+		onResidualOrder: 1,
+		onResidual(pokemon) {
+			console.log(this.effectData.duration);
+			console.log(this.effectData.source.name);
+			console.log(this.effectData.moveid);
+			if (this.effectData.duration === 2) {
+				if (this.effectData.source.isActive && this.effectData.source.hasAbility('reverberation')) {
+					this.effectData.source.addVolatile('reverberationTurn1');
+					this.useMove(this.effectData.moveid, this.effectData.source);
+				}
+			},
+			if (this.effectData.duration === 1) {
+				if (this.effectData.source.isActive && this.effectData.source.hasAbility('reverberation')) {
+					this.effectData.source.addVolatile('reverberationTurn2');
+					this.useMove(this.effectData.moveid, this.effectData.source);
+				}
+			},
+		},
+	},
+	reverberation3: {
+		duration: 3,
+		onResidualOrder: 1,
+		onResidual(pokemon) {
+			console.log(this.effectData.duration);
+			console.log(this.effectData.source.name);
+			console.log(this.effectData.moveid);
+			if (this.effectData.duration === 2) {
+				if (this.effectData.source.isActive && this.effectData.source.hasAbility('reverberation')) {
+					this.effectData.source.addVolatile('reverberationTurn1');
+					this.useMove(this.effectData.moveid, this.effectData.source);
+				}
+			},
+			if (this.effectData.duration === 1) {
+				if (this.effectData.source.isActive && this.effectData.source.hasAbility('reverberation')) {
+					this.effectData.source.addVolatile('reverberationTurn2');
+					this.useMove(this.effectData.moveid, this.effectData.source);
+				}
+			},
+		},
+	},
 	acidicterrain: {
 		inherit: true,
 		durationCallback(source, effect) {
 			if (source?.hasItem('terrainextender')) {
-				return 8;
+				 return 8;
 			}
 			if (source?.hasAbility('acidrock')) {
-				return 0;
+				 return 0;
 			}
 			return 5;
 		},
- 	},
+	},
 };
