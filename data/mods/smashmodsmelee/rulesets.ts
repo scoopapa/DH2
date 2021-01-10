446df3f6bda1a95631b9aab2c8a8424f11947129
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-10 00:53:27 -0700

    Update rulesets.ts
    Haha I'm an idiot I forgot the problem is that they *were* legal on *non*-Escavalier users

--- data/mods/smashmodsmelee/rulesets.ts
+++ data/mods/smashmodsmelee/rulesets.ts
@@ -11,8 +11,10 @@ export const Formats: {[k: string]: FormatData} = {
 			if (set.moves) {
 				for (const moveId of set.moves) {
 					const move = this.dex.getMove(moveId);
-          if (set.species === 'Escavalier') continue;
 					if (move.ohko) problems.push(move.name + ' is banned by OHKO Clause.');
+					if (set.species === 'Escavalier') continue;
+					if (move.name === 'Guillotine') problems.push(move.name + ' is banned by OHKO Clause.');
+					if (move.ohko === 'Horn Drill') problems.push(move.name + ' is banned by OHKO Clause.');
 				}
 			}
 			return problems;
