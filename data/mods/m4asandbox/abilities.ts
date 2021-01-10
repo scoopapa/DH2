a375b92edfb236bd0ee07fe4a4250854138b3835
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-10 03:05:00 -0700

    Update abilities.ts
    Double-oops

--- data/mods/m4asandbox/abilities.ts
+++ data/mods/m4asandbox/abilities.ts
@@ -21,15 +21,15 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 				if (!source.volatiles['reverberation1']) {
 					console.log("Reverberation 1: " + move.id);
 					source.addVolatile('reverberation1')
-					target.side.slotConditions[target.position]['reverberation1'].moveid = move.id;
+					source.volatiles['reverberation1'].moveid = move.id;
 				} else if (!source.volatiles['reverberation2']) {
 					console.log("Reverberation 2: " + move.id);
 					source.addVolatile('reverberation2')
-					target.side.slotConditions[target.position]['reverberation2'].moveid = move.id;
+					source.volatiles['reverberation2'].moveid = move.id;
 				} else if (!source.volatiles['reverberation3']) {
 					console.log("Reverberation 3: " + move.id);
 					source.addVolatile('reverberation3')
-					target.side.slotConditions[target.position]['reverberation3'].moveid = move.id;
+					source.volatiles['reverberation3'].moveid = move.id;
 				}
 			}
 		},
