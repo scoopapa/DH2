faea119764ca84f23db2ca6c94917ddaff5212ac
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-10 04:03:16 -0700

    Update conditions.ts
    mmmm?

--- data/mods/m4asandbox/conditions.ts
+++ data/mods/m4asandbox/conditions.ts
@@ -9,15 +9,13 @@ export const Conditions: {[k: string]: ConditionData} = {
 			if (this.effectData.duration === 2) {
 				if (pokemon.hasAbility('reverberation')) {
 					pokemon.addVolatile('reverberationTurn1');
-					this.add('-ability', pokemon, 'Reverberation');
-					this.useMove(this.effectData.moveid, pokemon);
+					this.useMove(this.effectData.moveid, pokemon, this.dex.getAbility('reverberation'));
 				}
 			}
 			if (this.effectData.duration === 1) {
 				if (pokemon.hasAbility('reverberation')) {
 					pokemon.addVolatile('reverberationTurn2');
-					this.add('-ability', pokemon, 'Reverberation');
-					this.useMove(this.effectData.moveid, pokemon);
+					this.useMove(this.effectData.moveid, pokemon, this.dex.getAbility('reverberation'));
 					pokemon.removeVolatile('reverberation1');
 				}
 			}
@@ -33,15 +31,13 @@ export const Conditions: {[k: string]: ConditionData} = {
 			if (this.effectData.duration === 2) {
 				if (pokemon.hasAbility('reverberation')) {
 					pokemon.addVolatile('reverberationTurn1');
-					this.add('-ability', pokemon, 'Reverberation');
-					this.useMove(this.effectData.moveid, pokemon);
+					this.useMove(this.effectData.moveid, pokemon, this.dex.getAbility('reverberation'));
 				}
 			}
 			if (this.effectData.duration === 1) {
 				if (pokemon.hasAbility('reverberation')) {
 					pokemon.addVolatile('reverberationTurn2');
-					this.add('-ability', pokemon, 'Reverberation');
-					this.useMove(this.effectData.moveid, pokemon);
+					this.useMove(this.effectData.moveid, pokemon, this.dex.getAbility('reverberation'));
 					pokemon.removeVolatile('reverberation2');
 				}
 			}
@@ -57,18 +53,40 @@ export const Conditions: {[k: string]: ConditionData} = {
 			if (this.effectData.duration === 2) {
 				if (pokemon.hasAbility('reverberation')) {
 					pokemon.addVolatile('reverberationTurn1');
-					this.add('-ability', pokemon, 'Reverberation');
-					this.useMove(this.effectData.moveid, pokemon);
+					this.useMove(this.effectData.moveid, pokemon, this.dex.getAbility('reverberation'));
 				}
 			}
 			if (this.effectData.duration === 1) {
 				if (pokemon.hasAbility('reverberation')) {
 					pokemon.addVolatile('reverberationTurn2');
-					this.add('-ability', pokemon, 'Reverberation');
-					this.useMove(this.effectData.moveid, pokemon);
+					this.useMove(this.effectData.moveid, pokemon, this.dex.getAbility('reverberation'));
 					pokemon.removeVolatile('reverberation3');
 				}
 			}
+		},
+	},
+	reverberationTurn1: {
+		duration: 0,
+		onModifyMove(move, source, target) {
+			console.log("Modifying move");
+			console.log("Reverberation turn 1");
+			if (move.basePower) {
+				move.basePower = move.basePower.chainModify([0x0200, 0x1000]);
+				console.log(move.basePower);
+			}
+			delete source.volatiles['reverberationTurn1'];
+		},
+	},
+	reverberationTurn2: {
+		duration: 0,
+		onModifyMove(move, source, target) {
+			console.log("Modifying move");
+			console.log("Reverberation turn 2");
+			if (move.basePower) {
+				move.basePower = move.basePower.chainModify([0x0100, 0x1000]);
+				console.log(move.basePower);
+			}
+			delete source.volatiles['reverberationTurn2'];
 		},
 	},
 	acidicterrain: {
