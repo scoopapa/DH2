95b8d4b6c7b0f6a5f9a0fb58ea8f63e17809f790
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-10 02:50:11 -0700

    Update scripts.ts

--- data/mods/m4asandbox/scripts.ts
+++ data/mods/m4asandbox/scripts.ts
@@ -82,4 +82,20 @@ export const Scripts: ModdedBattleScriptsData = {
 		}
 		return item.megaStone;
 	},
+	pokemon: {
+		isGrounded(negateImmunity = false) {
+			if ('gravity' in this.battle.field.pseudoWeather) return true;
+			if ('ingrain' in this.volatiles && this.battle.gen >= 4) return true;
+			if ('smackdown' in this.volatiles) return true;
+			const item = (this.ignoringItem() ? '' : this.item);
+			if (item === 'ironball') return true;
+			// If a Fire/Flying type uses Burn Up and Roost, it becomes ???/Flying-type, but it's still grounded.
+			if (!negateImmunity && this.hasType('Flying') && !('roost' in this.volatiles)) return false;
+			if (this.hasAbility('levitate') && !this.battle.suppressingAttackEvents()) return null;
+			if ('magnetrise' in this.volatiles) return false;
+			if ('telekinesis' in this.volatiles) return false;
+			if ('magnetrock' in this.side.sideConditions) return false;
+			return item !== 'airballoon';
+		}
+	}
 };
