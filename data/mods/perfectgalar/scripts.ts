2e740ab5d0e65e268e7b719e413a39c5824d23e6
scoopapa <pccompositor@aim.com>
2021-01-13 13:51:31 -0500

    Update scripts.ts

--- data/mods/perfectgalar/scripts.ts
+++ data/mods/perfectgalar/scripts.ts
@@ -6,7 +6,7 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 				if (pkmnid.learnset && pkmnid.learnset[preHomeDist[i]]) {
 					let gLearn = false
 					for (const j in pkmnid.learnset[preHomeDist[i]]) {
-						const learnType = pkmnid.learnset[preHomeDist[i][j];
+						const learnType = pkmnid.learnset[preHomeDist[i][j]];
 						if (learnType.includes('8') && !learnType.includes('V') gLearn = true;
 					}
 					if (!gLearn) delete this.modData('Learnsets', pkmnid).learnset[preHomeDist[i]];
