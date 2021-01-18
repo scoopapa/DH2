c665f687ef2216c1ba7b141c9ab6a76157ebca53
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-18 02:11:18 -0700

    Create formats-data.ts

---
+++ data/mods/conniecorddraft/formats-data.ts
@@ -0,0 +1,62 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	boohawk: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	buzzstacian: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	chandeluregalaponea: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	nihilegogalar: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	drampamega: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	sceptilemegasword: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	vaporeonmegax: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	neuroboxin: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	poultergeist: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	poultergeistheadless: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	regiyehehe: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	scout: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	stamizer: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	tatsunami: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+	venonator: {
+		tier: "CCD1",
+		doublesTier: "CCD1",
+	},
+};
