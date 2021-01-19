bb482779fac7bebd13436c33b4a7fca65144a639
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-19 03:44:48 -0700

    Update items.ts

--- data/mods/m4asandbox/items.ts
+++ data/mods/m4asandbox/items.ts
@@ -2,7 +2,7 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 	mossysandwich: {
 		name: "Mossy Sandwich",
 		spritenum: 578,
-		megaStone: "Toxtricity-Mega-Mossy",
+		megaStone: "Toxtricity-Mega",
 		megaEvolves: "Toxtricity",
 		itemUser: ["Toxtricity"],
 		onTakeItem(item, source) {
@@ -16,7 +16,7 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 	ausma: {
 		name: "Ausma",
 		spritenum: 578,
-		megaStone: "Exploud-Mega-Ausma",
+		megaStone: "Exploud-Mega",
 		megaEvolves: "Exploud",
 		itemUser: ["Exploud"],
 		onTakeItem(item, source) {
@@ -30,7 +30,7 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 	bitbitio: {
 		name: "BitBitio",
 		spritenum: 578,
-		megaStone: "Toxtricity-Mega-BitBitio",
+		megaStone: "Toxtricity-Mega",
 		megaEvolves: "Toxtricity",
 		itemUser: ["Toxtricity"],
 		onTakeItem(item, source) {
@@ -40,5 +40,33 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 		num: -5000,
 		gen: 8,
 		desc: "BitBitio's stone for the submission sandbox.",
+	},
+	magmajudis: {
+		name: "Magmajudis",
+		spritenum: 578,
+		megaStone: "Hawlucha-Mega-Magmajudis",
+		megaEvolves: "Hawlucha",
+		itemUser: ["Hawlucha"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -5000,
+		gen: 8,
+		desc: "Magmajudis's stone for the submission sandbox.",
+	},
+	blueray: {
+		name: "BlueRay",
+		spritenum: 578,
+		megaStone: "Hawlucha-Mega-BlueRay",
+		megaEvolves: "Hawlucha",
+		itemUser: ["Hawlucha"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -5000,
+		gen: 8,
+		desc: "BlueRay's stone for the submission sandbox.",
 	},
 };
