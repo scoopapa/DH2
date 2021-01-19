1d24be1f87f9543dae58d681788b6cc2cc8d1f63
inkbug1 <72170979+inkbug1@users.noreply.github.com>
2021-01-19 12:09:17 -0800

    Mega Revolution sprites + TTG setup

---
+++ data/mods/tothegigantamax/pokedex.ts
@@ -0,0 +1,302 @@
+export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
+	scyther: {
+		
+	},
+	scythergmax: {
+		
+	},
+	heracross: {
+		
+	},
+	heracrossgmax: {
+		
+	},
+	lycanrocdusk: {
+		
+	},
+	lycanrocduskgmax: {
+		
+	},
+	magnezone: {
+		
+	},
+	magnezonegmax: {
+		
+	},
+	arctozolt: {
+		
+	},
+	arctozoltgmax: {
+		
+	},
+	scolipede: {
+		
+	},
+	scolipedegmax: {
+		
+	},
+	cursola: {
+		
+	},
+	cursolagmax: {
+		
+	},
+	excadrill: {
+		
+	},
+	excadrillgmax: {
+		
+	},
+	tyranitar: {
+		
+	},
+	tyranitargmax: {
+		
+	},
+	primarina: {
+		
+	},
+	primarinagmax: {
+		
+	},
+	jirachi: {
+		
+	},
+	jirachigmax: {
+		
+	},
+	flygon: {
+		
+	},
+	flygongmax: {
+		
+	},
+	polteageist: {
+		
+	},
+	polteageistgmax: {
+		
+	},
+	rapidashgalar: {
+		
+	},
+	rapidashgalargmax: {
+		
+	},
+	incineroar: {
+		
+	},
+	incineroargmax: {
+		
+	},
+	darmanitangalar: {
+		
+	},
+	darmanitangalargmax: {
+		
+	},
+	decidueye: {
+		
+	},
+	decidueyegmax: {
+		
+	},
+	kyuremblack: {
+		
+	},
+	kyuremblackgmax: {
+		
+	},
+	scizor: {
+		
+	},
+	scizorgmax: {
+		
+	},
+	sableye: {
+		
+	},
+	sableyegmax: {
+		
+	},
+	poliwrath: {
+		
+	},
+	poliwrathgmax: {
+		
+	},
+	talonflame: {
+		
+	},
+	talonflamegmax: {
+		
+	},
+	lurantis: {
+		
+	},
+	lurantisgmax: {
+		
+	},
+	porygonz: {
+		
+	},
+	porygonzgmax: {
+		
+	},
+	klefki: {
+		
+	},
+	klefkigmax: {
+		
+	},
+	slowbrogalar: {
+		
+	},
+	slowbrogalargmax: {
+		
+	},
+	jigglypuff: {
+		
+	},
+	jigglypuffgmax: {
+		
+	},
+	palossand: {
+		
+	},
+	palossandgmax: {
+		
+	},
+	aggron: {
+		
+	},
+	aggrongmax: {
+		
+	},
+	articunogalar: {
+		
+	},
+	articunogalargmax: {
+		
+	},
+	zapdosgalar: {
+		
+	},
+	zapdosgalargmax: {
+		
+	},
+	moltres: {
+		
+	},
+	moltresgalargmax: {
+		
+	},
+	braviary: {
+		
+	},
+	braviarygmax: {
+		
+	},
+	wobbuffet: {
+		
+	},
+	wobbuffetgmax: {
+		
+	},
+	groudon: {
+		
+	},
+	groudongmax: {
+		
+	},
+	kyogre: {
+		
+	},
+	kyogregmax: {
+		
+	},
+	aurorus: {
+		
+	},
+	aurorusgmax: {
+		
+	},
+	quagsire: {
+		
+	},
+	quagsiregmax: {
+		
+	},
+	reshiram: {
+		
+	},
+	reshiramgmax: {
+		
+	},
+	obstagoon: {
+		
+	},
+	obstagoongmax: {
+		
+	},
+	starmie: {
+		
+	},
+	starmiegmax: {
+		
+	},
+	gastrodon: {
+		
+	},
+	gastrodongmax: {
+		
+	},
+	regirock: {
+		
+	},
+	regirockgmax: {
+		
+	},
+	regice: {
+		
+	},
+	regicegmax: {
+		
+	},
+	registeel: {
+		
+	},
+	registeelgmax: {
+		
+	},
+	xurkitree: {
+		
+	},
+	xurkitreegmax: {
+		
+	},
+	necrozma: {
+		
+	},
+	necrozmagmax: {
+		
+	},
+	necrozmaduskmanegmax: {
+		
+	},
+	necrozmadawnwingsgmax: {
+		
+	},
+	latias: {
+		
+	},
+	latiasgmax: {
+		
+	},
+	rayquaza: {
+		
+	},
+	rayquazagmax: {
+		
+	},
+};
