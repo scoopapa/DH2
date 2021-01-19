9866355785fd49c4ab95ab0f971e0b4efba3cde4
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-19 03:43:39 -0700

    Update formats-data.ts

--- data/mods/m4asandbox/formats-data.ts
+++ data/mods/m4asandbox/formats-data.ts
@@ -1,23 +1,8 @@
 export const FormatsData: {[k: string]: SpeciesFormatsData} = {
-	exploudmegaausma: {
+	hawluchamegamagmajudis: {
 		tier: "Sandbox",
 	},
-	noivernmegaausma: {
-		tier: "Sandbox",
-	},
-	noivernmegabitbitio: {
-		tier: "Sandbox",
-	},
-	toxtricitymegamossy: {
-		tier: "Sandbox",
-	},
-	toxtricitylowkeymegamossy: {
-		tier: "Sandbox",
-	},
-	toxtricitymegabitbitio: {
-		tier: "Sandbox",
-	},
-	toxtricitylowkeymegabitbitio: {
+	hawluchamegablueray: {
 		tier: "Sandbox",
 	},
 };
