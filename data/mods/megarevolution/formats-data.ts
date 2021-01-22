079d89cf95f020f85c0edd984cead6d16fe2f784
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-22 11:18:48 -0700

    Update formats-data.ts

--- data/mods/megarevolution/formats-data.ts
+++ data/mods/megarevolution/formats-data.ts
@@ -1,65 +1,86 @@
 export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 	arbosaur: {
 		tier:'OU',
+		isModded: true,
 	},
 	charbarian: {
 		tier:'OU',
+		isModded: true,
 	},
 	chargaryen: {
 		tier:'OU',
+		isModded: true,
 	},
 	galapagon: {
 		tier:'OU',
+		isModded: true,
 	},
 	coleoptyran: {
 		tier:'OU',
+		isModded: true,
 	},
 	herascarab: {
 		tier:'OU',
+		isModded: true,
 	},
 	scibrawl: {
 		tier:'OU',
+		isModded: true,
 	},
 	ramjestic: {
 		tier:'OU',
+		isModded: true,
 	},
 	houndread: {
 		tier:'OU',
+		isModded: true,
 	},
 	zipette: {
 		tier:'OU',
+		isModded: true,
 	},
 	mediscend: {
 		tier:'OU',
+		isModded: true,
 	},
 	stalagmitar: {
 		tier:'OU',
+		isModded: true,
 	},
 	starietta: {
 		tier:'OU',
+		isModded: true,
 	},
 	metamalgam: {
 		tier:'OU',
+		isModded: true,
 	},
 	scawsert: {
 		tier:'OU',
+		isModded: true,
 	},
 	slowclone: {
 		tier:'OU',
+		isModded: true,
 	},
 	lordiathan: {
 		tier:'OU',
+		isModded: true,
 	},
 	gnashark: {
 		tier:'OU',
+		isModded: true,
 	},
 	dimster: {
 		tier:'OU',
+		isModded: true,
 	},
 	kangatwo: {
 		tier:'OU',
+		isModded: true,
 	},
 	kylario: {
 		tier:'OU',
+		isModded: true,
 	},
 };
