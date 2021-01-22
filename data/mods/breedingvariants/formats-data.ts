2059a8f8c74d8bf1b94432c6b5bcdf51c6edfd98
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-22 11:17:57 -0700

    Update formats-data.ts

--- data/mods/breedingvariants/formats-data.ts
+++ data/mods/breedingvariants/formats-data.ts
@@ -2,145 +2,181 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	goodraflame: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	// electivirekungfu: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	flygonbuzz: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	// banetteblademaster: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	// banettemegablademaster: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	// kingdracamo: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	vikavoltmigale: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	// typhlosioncinder: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	flareonocean: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	laprasangry: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	// exploudmeow: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	butterfreearmor: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	espeondusk: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	avaluggshield: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	dusclopsgastric: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	// delphoxlazy: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	// lurantisnut: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	sylveonprotector: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	// skarmoryprimordial: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	// infernapebugout: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	// breloomluau: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	leafeoncutlass: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	froslassgunwoman: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	// dodriomystical: {
 		// tier: "OU",
 		// doublesTier: "DOU",
+		// isModded: true,
 	// },
 	mantinespiny: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	rapidashgalarmeow: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	mrrimespoon: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	thievulbananas: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	stonjournercastle: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	copperajahforge: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	grapploctray: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	appletunburned: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	vespiquenterra: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	cursolashock: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	machamplucha: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 	roseradescarfed: {
 		tier: "OU",
 		doublesTier: "DOU",
+		isModded: true,
 	},
 };
