a8cbb1f46ae8d2880f81ae9bf3881905421dd7a4
scoopapa <pccompositor@aim.com>
2021-02-09 00:58:55 -0500

    Update pokedex.ts

--- data/mods/restrictions/pokedex.ts
+++ data/mods/restrictions/pokedex.ts
@@ -1,183 +1,273 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
-	vulcaraptor: {
+	vulcaraptor: { 
+		num: 1001,
 		name: "Vulcaraptor",
 		types: ["Rock", "Fire"],
-		baseStats: {hp: 68, atk: 122, def: 115, spa: 76, spd: 63, spe: 111},
-		abilities: {0: "Technician", H: "Quick Feet"},
+		baseStats: { 
+		num: 1001,hp: 68, atk: 122, def: 115, spa: 76, spd: 63, spe: 111},
+		abilities: { 
+		num: 1001,0: "Technician", H: "Quick Feet"},
 	},
-	parakinesis: {
+	parakinesis: { 
+		num: 1002,
 		name: "Parakinesis",
 		types: ["Psychic", "Flying"],
-		baseStats: {hp: 124, atk: 130, def: 66, spa: 79, spd: 72, spe: 109},
-		abilities: {0: "Prankster", 1: "Big Pecks", H: "Iron Fist"},
+		baseStats: { 
+		num: 1001,hp: 124, atk: 130, def: 66, spa: 79, spd: 72, spe: 109},
+		abilities: { 
+		num: 1001,0: "Prankster", 1: "Big Pecks", H: "Iron Fist"},
 	},
-	serpaint: {
+	serpaint: { 
+		num: 1003,
 		name: "Serpaint",
 		types: ["Dragon", "Ghost"],
-		baseStats: {hp: 86, atk: 94, def: 60, spa: 123, spd: 90, spe: 72},
-		abilities: {0: "Infiltrator", H: "Shadow Shield"},
+		baseStats: { 
+		num: 1001,hp: 86, atk: 94, def: 60, spa: 123, spd: 90, spe: 72},
+		abilities: { 
+		num: 1001,0: "Infiltrator", H: "Shadow Shield"},
 	},
-	torgeist: {
+	torgeist: { 
+		num: 1004,
 		name: "Torgeist",
 		types: ["Ghost", "Flying"],
-		baseStats: {hp: 55, atk: 75, def: 90, spa: 115, spd: 100, spe: 105},
-		abilities: {0: "Clear Body", 1: "Cursed Body", H: "Merciless"},
+		baseStats: { 
+		num: 1001,hp: 55, atk: 75, def: 90, spa: 115, spd: 100, spe: 105},
+		abilities: { 
+		num: 1001,0: "Clear Body", 1: "Cursed Body", H: "Merciless"},
 	},
-	regolite: {
+	regolite: { 
+		num: 1005,
 		name: "Regolite",
 		types: ["Ground", "Psychic"],
-		baseStats: {hp: 80, atk: 75, def: 70, spa: 120, spd: 100, spe: 80},
-		abilities: {0: "Shield Dust", 1: "Sand Spit", H: "Unburden"},
+		baseStats: { 
+		num: 1001,hp: 80, atk: 75, def: 70, spa: 120, spd: 100, spe: 80},
+		abilities: { 
+		num: 1001,0: "Shield Dust", 1: "Sand Spit", H: "Unburden"},
 	},
-	polbearab: {
+	polbearab: { 
+		num: 1006,
 		name: "Polbearab",
 		types: ["Ice", "Fighting"],
-		baseStats: {hp: 95, atk: 98, def: 130, spa: 55, spd: 101, spe: 101},
-		abilities: {0: "Poison Heal"},
+		baseStats: { 
+		num: 1001,hp: 95, atk: 98, def: 130, spa: 55, spd: 101, spe: 101},
+		abilities: { 
+		num: 1001,0: "Poison Heal"},
 	},
-	centhorn: {
+	centhorn: { 
+		num: 1007,
 		name: "Centhorn",
 		types: ["Grass", "Fighting"],
-		baseStats: {hp: 80, atk: 97, def: 93, spa: 65, spd: 75, spe: 85},
-		abilities: {0: "Triage", H: "Grassy Surge"},
+		baseStats: { 
+		num: 1001,hp: 80, atk: 97, def: 93, spa: 65, spd: 75, spe: 85},
+		abilities: { 
+		num: 1001,0: "Triage", H: "Grassy Surge"},
 	},
-	donzyxote: {
+	donzyxote: { 
+		num: 1008,
 		name: "Donzyxote",
 		types: ["Dragon", "Psychic"],
-		baseStats: {hp: 93, atk: 90, def: 72, spa: 90, spd: 121, spe: 59},
-		abilities: {0: "Marvel Scale", H: "Own Tempo"},
+		baseStats: { 
+		num: 1001,hp: 93, atk: 90, def: 72, spa: 90, spd: 121, spe: 59},
+		abilities: { 
+		num: 1001,0: "Marvel Scale", H: "Own Tempo"},
 	},
-	scimicobra: {
+	scimicobra: { 
+		num: 1009,
 		name: "Scimicobra",
 		types: ["Poison", "Fighting"],
-		baseStats: {hp: 79, atk: 135, def: 101, spa: 64, spd: 81, spe: 75},
-		abilities: {0: "Shed Skin", 1: "Sniper", H: "Infiltrator"},
+		baseStats: { 
+		num: 1001,hp: 79, atk: 135, def: 101, spa: 64, spd: 81, spe: 75},
+		abilities: { 
+		num: 1001,0: "Shed Skin", 1: "Sniper", H: "Infiltrator"},
 	},
-	folliclawe: {
+	folliclawe: { 
+		num: 1010,
 		name: "Folliclawe",
 		types: ["Dark", "Steel"],
-		baseStats: {hp: 50, atk: 72, def: 86, spa: 60, spd: 75, spe: 119},
-		abilities: {0: "Hustle", H: "Hyper Cutter"},
+		baseStats: { 
+		num: 1001,hp: 50, atk: 72, def: 86, spa: 60, spd: 75, spe: 119},
+		abilities: { 
+		num: 1001,0: "Hustle", H: "Hyper Cutter"},
 	},
-	afloof: {
+	afloof: { 
+		num: 1011,
 		name: "Afloof",
 		types: ["Normal", "Dark"],
-		baseStats: {hp: 90, atk: 90, def: 56, spa: 129, spd: 70, spe: 80},
-		abilities: {0: "Hyper Cutter", 1: "Fluffy", H: "Adaptability"},
+		baseStats: { 
+		num: 1001,hp: 90, atk: 90, def: 56, spa: 129, spd: 70, spe: 80},
+		abilities: { 
+		num: 1001,0: "Hyper Cutter", 1: "Fluffy", H: "Adaptability"},
 	},
-	nimbusteed: {
+	nimbusteed: { 
+		num: 1012,
 		name: "Nimbusteed",
 		types: ["Flying", "Electric"],
-		baseStats: {hp: 80, atk: 155, def: 90, spa: 75, spd: 70, spe: 50},
-		abilities: {0: "Aerilate", 1: "Lightning Rod", H: "Cloud Nine"},
+		baseStats: { 
+		num: 1001,hp: 80, atk: 155, def: 90, spa: 75, spd: 70, spe: 50},
+		abilities: { 
+		num: 1001,0: "Aerilate", 1: "Lightning Rod", H: "Cloud Nine"},
 	},
-	aerock: {
+	aerock: { 
+		num: 1013,
 		name: "Aerock",
 		types: ["Rock", "Flying"],
-		baseStats: {hp: 80, atk: 105, def: 100, spa: 103, spd: 90, spe: 95},
-		abilities: {0: "Poison Heal", H: "Volt Absorb"},
+		baseStats: { 
+		num: 1001,hp: 80, atk: 105, def: 100, spa: 103, spd: 90, spe: 95},
+		abilities: { 
+		num: 1001,0: "Poison Heal", H: "Volt Absorb"},
 	},
-	borassa: {
+	borassa: { 
+		num: 1014,
 		name: "Borassa",
 		types: ["Dark", "Grass"],
-		baseStats: {hp: 136, atk: 76, def: 118, spa: 88, spd: 95, spe: 87},
-		abilities: {0: "Thick Fat", 1: "Chlorophyll", H: "Anger Point"},
+		baseStats: { 
+		num: 1001,hp: 136, atk: 76, def: 118, spa: 88, spd: 95, spe: 87},
+		abilities: { 
+		num: 1001,0: "Thick Fat", 1: "Chlorophyll", H: "Anger Point"},
 	},
-	sharm: {
+	sharm: { 
+		num: 1015,
 		name: "Sharm",
 		types: ["Water", "Electric"],
-		baseStats: {hp: 80, atk: 70, def: 95, spa: 85, spd: 130, spe: 45},
-		abilities: {0: "Unaware", H: "No Guard"},
+		baseStats: { 
+		num: 1001,hp: 80, atk: 70, def: 95, spa: 85, spd: 130, spe: 45},
+		abilities: { 
+		num: 1001,0: "Unaware", H: "No Guard"},
 	},
-	nunopod: {
+	nunopod: { 
+		num: 1016,
 		name: "Nunopod",
 		types: ["Normal", "Bug"],
-		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100},
-		abilities: {0: "Slow Start", 1: "Emergency Exit", H: "Stall"},
+		baseStats: { 
+		num: 1001,hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100},
+		abilities: { 
+		num: 1001,0: "Slow Start", 1: "Emergency Exit", H: "Stall"},
 	},
-	crypterid: {
+	crypterid: { 
+		num: 1017,
 		name: "Crypterid",
 		types: ["Ground", "Poison"],
-		baseStats: {hp: 81, atk: 100, def: 103, spa: 75, spd: 121, spe: 60},
-		abilities: {0: "Poison Point", 1: "Swift Swim", H: "Unnerve"},
+		baseStats: { 
+		num: 1001,hp: 81, atk: 100, def: 103, spa: 75, spd: 121, spe: 60},
+		abilities: { 
+		num: 1001,0: "Poison Point", 1: "Swift Swim", H: "Unnerve"},
 	},
-	abysseil: {
+	abysseil: { 
+		num: 1018,
 		name: "Abysseil",
 		types: ["Water", "Psychic"],
-		baseStats: {hp: 80, atk: 44, def: 95, spa: 83, spd: 95, spe: 138},
-		abilities: {0: "Water Veil", 1: "Sap Sipper", H: "Rain Dish"},
+		baseStats: { 
+		num: 1001,hp: 80, atk: 44, def: 95, spa: 83, spd: 95, spe: 138},
+		abilities: { 
+		num: 1001,0: "Water Veil", 1: "Sap Sipper", H: "Rain Dish"},
 	},
-	faerenheit: {
+	faerenheit: { 
+		num: 1019,
 		name: "Faerenheit",
 		types: ["Fire", "Fairy"],
-		baseStats: {hp: 73, atk: 79, def: 137, spa: 107, spd: 101, spe: 73},
-		abilities: {0: "Beast Boost"},
+		baseStats: { 
+		num: 1001,hp: 73, atk: 79, def: 137, spa: 107, spd: 101, spe: 73},
+		abilities: { 
+		num: 1001,0: "Beast Boost"},
 	},
-	spiriteal: {
+	spiriteal: { 
+		num: 1020,
 		name: "Spiriteal",
 		types: ["Ghost"],
-		baseStats: {hp: 60, atk: 105, def: 60, spa: 120, spd: 60, spe: 105},
-		abilities: {0: "Illusion"},
+		baseStats: { 
+		num: 1001,hp: 60, atk: 105, def: 60, spa: 120, spd: 60, spe: 105},
+		abilities: { 
+		num: 1001,0: "Illusion"},
 	},
-	zoltanka: {
+	zoltanka: { 
+		num: 1021,
 		name: "Zoltanka",
 		types: ["Steel", "Ghost"],
-		baseStats: {hp: 112, atk: 77, def: 53, spa: 109, spd: 129, spe: 60},
-		abilities: {0: "Filter", 1: "Minus", H: "Screen Cleaner"},
+		baseStats: { 
+		num: 1001,hp: 112, atk: 77, def: 53, spa: 109, spd: 129, spe: 60},
+		abilities: { 
+		num: 1001,0: "Filter", 1: "Minus", H: "Screen Cleaner"},
 	},
-	flydra: {
+	flydra: { 
+		num: 1022,
 		name: "Flydra",
 		types: ["Bug", "Dragon"],
-		baseStats: {hp: 60, atk: 95, def: 50, spa: 120, spd: 80, spe: 125},
-		abilities: {0: "Compound Eyes", 1: "Levitate", H: "Swarm"},
+		baseStats: { 
+		num: 1001,hp: 60, atk: 95, def: 50, spa: 120, spd: 80, spe: 125},
+		abilities: { 
+		num: 1001,0: "Compound Eyes", 1: "Levitate", H: "Swarm"},
 	},
-	coracrab: {
+	coracrab: { 
+		num: 1023,
 		name: "Coracrab",
 		types: ["Water", "Rock"],
-		baseStats: {hp: 105, atk: 130, def: 120, spa: 45, spd: 45, spe: 40},
-		abilities: {0: "Lightning Rod", 1: "Rock Head", H: "Reckless"},
+		baseStats: { 
+		num: 1001,hp: 105, atk: 130, def: 120, spa: 45, spd: 45, spe: 40},
+		abilities: { 
+		num: 1001,0: "Lightning Rod", 1: "Rock Head", H: "Reckless"},
 	},
-	quadringo: {
+	quadringo: { 
+		num: 1024,
 		name: "Quadringo",
 		types: ["Fairy", "Dragon"],
-		baseStats: {hp: 85, atk: 110, def: 103, spa: 82, spd: 78, spe: 82},
-		abilities: {0: "Keen Eye", 1: "Big Pecks", H: "Mold Breaker"},
+		baseStats: { 
+		num: 1001,hp: 85, atk: 110, def: 103, spa: 82, spd: 78, spe: 82},
+		abilities: { 
+		num: 1001,0: "Keen Eye", 1: "Big Pecks", H: "Mold Breaker"},
 	},
-	teslaple: {
+	teslaple: { 
+		num: 1025,
 		name: "Teslaple",
 		types: ["Electric", "Grass"],
-		baseStats: {hp: 80, atk: 40, def: 120, spa: 120, spd: 40, spe: 120},
-		abilities: {0: "Grass Pelt", 1: "Flower Veil", H: "Competitive"},
+		baseStats: { 
+		num: 1001,hp: 80, atk: 40, def: 120, spa: 120, spd: 40, spe: 120},
+		abilities: { 
+		num: 1001,0: "Grass Pelt", 1: "Flower Veil", H: "Competitive"},
 	},
-	kapagon: {
+	kapagon: { 
+		num: 1026,
 		name: "Kapagon",
 		types: ["Water", "Dragon"],
-		baseStats: {hp: 90, atk: 90, def: 100, spa: 70, spd: 80, spe: 75},
-		abilities: {0: "Regenerator", 1: "Rain Dish", H: "Drizzle"},
+		baseStats: { 
+		num: 1001,hp: 90, atk: 90, def: 100, spa: 70, spd: 80, spe: 75},
+		abilities: { 
+		num: 1001,0: "Regenerator", 1: "Rain Dish", H: "Drizzle"},
 	},
-	cyknight: {
+	cyknight: { 
+		num: 1027,
 		name: "Cyknight",
 		types: ["Steel"],
-		baseStats: {hp: 80, atk: 80, def: 100, spa: 50, spd: 100, spe: 90},
-		abilities: {0: "Intrepid Sword"},
+		baseStats: { 
+		num: 1001,hp: 80, atk: 80, def: 100, spa: 50, spd: 100, spe: 90},
+		abilities: { 
+		num: 1001,0: "Intrepid Sword"},
 	},
-	dirtoad: {
+	dirtoad: { 
+		num: 1028,
 		name: "Dirtoad",
 		types: ["Water", "Ground"],
-		baseStats: {hp: 95, atk: 110, def: 60, spa: 60, spd: 110, spe: 95},
-		abilities: {0: "Dry Skin", 1: "Hydration", H: "Damp"},
+		baseStats: { 
+		num: 1001,hp: 95, atk: 110, def: 60, spa: 60, spd: 110, spe: 95},
+		abilities: { 
+		num: 1001,0: "Dry Skin", 1: "Hydration", H: "Damp"},
 	},
-	crystelf: {
+	crystelf: { 
+		num: 1029,
 		name: "Crystelf",
 		types: ["Rock", "Fairy"],
-		baseStats: {hp: 50, atk: 95, def: 45, spa: 113, spd: 93, spe: 124},
-		abilities: {0: "Trace", 1: "Sand Force", H: "Sand Rush"},
+		baseStats: { 
+		num: 1001,hp: 50, atk: 95, def: 45, spa: 113, spd: 93, spe: 124},
+		abilities: { 
+		num: 1001,0: "Trace", 1: "Sand Force", H: "Sand Rush"},
 	},
-	rancourgar: {
+	rancourgar: { 
+		num: 1030,
 		name: "Rancourgar",
 		types: ["Normal", "Fire"],
-		baseStats: {hp: 85, atk: 106, def: 64, spa: 98, spd: 118, spe: 100},
-		abilities: {0: "Reckless", 1: "Soundproof", H: "Anger Point"},
+		baseStats: { 
+		num: 1001,hp: 85, atk: 106, def: 64, spa: 98, spd: 118, spe: 100},
+		abilities: { 
+		num: 1001,0: "Reckless", 1: "Soundproof", H: "Anger Point"},
 	},
 
 };
