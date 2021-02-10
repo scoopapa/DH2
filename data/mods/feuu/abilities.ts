b371ab923fa1a502b13230fd80ab115285553376
inkbug1 <72170979+inkbug1@users.noreply.github.com>
2021-02-10 12:57:06 -0800

    Update abilities.ts

--- data/mods/feuu/abilities.ts
+++ data/mods/feuu/abilities.ts
@@ -442,35 +442,7 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		id: "pillage",
 		name: "Pillage",
 		shortDesc: "On switch-in, swaps ability with the opponent.",
-		onStart(pokemon) {
-			if (pokemon.side.foe.active.some(
-				foeActive => foeActive && this.isAdjacent(pokemon, foeActive) && foeActive.ability === 'noability'
-			)) {
-				this.effectData.gaveUp = true;
-			}
-		},
-		onUpdate(pokemon) {
-			if (!pokemon.isStarted || this.effectData.gaveUp) return;
-			const possibleTargets = pokemon.side.foe.active.filter(foeActive => foeActive && this.isAdjacent(pokemon, foeActive));
-			while (possibleTargets.length) {
-				let rand = 0;
-				if (possibleTargets.length > 1) rand = this.random(possibleTargets.length);
-				const target = possibleTargets[rand];
-				const additionalBannedAbilities = [
-					// Zen Mode included here for compatability with Gen 5-6
-					'noability', 'flowergift', 'forecast', 'hungerswitch', 'illusion', 'imposter', 'neutralizinggas', 'powerofalchemy', 'receiver', 'trace', 'zenmode',
-				];
-				if (target.getAbility().isPermanent || additionalBannedAbilities.includes(target.ability)) {
-					possibleTargets.splice(rand, 1);
-					continue;
-				}
-				const sourceAbility = pokemon.setAbility('wanderingspirit', target);
-				if (!sourceAbility) return;
-				this.add('-activate', target, 'ability: Pillage', this.dex.getAbility(sourceAbility).name, 'Pillage', '[of] ' + pokemon);
-				target.setAbility(sourceAbility);
-				return;
-			}
-		},
+		
 	},
 	magneticwaves: {
 		id: "magneticwaves",
