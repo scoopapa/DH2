a0d87dfed93d358a3cab9c842f91afee3a67f4c9
inkbug1 <72170979+inkbug1@users.noreply.github.com>
2021-02-11 11:14:20 -0800

    Update pokedex.ts
    testing learnset fix...?

--- data/mods/tothegigantamax/pokedex.ts
+++ data/mods/tothegigantamax/pokedex.ts
@@ -577,8 +577,8 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 	articunogalargmax: {
 		num: 144,
 		name: "Articuno-Galar-Gmax",
-		baseSpecies: "Articuno",
-		forme: "Galar-Gmax",
+		baseSpecies: "Articuno-Galar",
+		forme: "Gmax",
 		types: ["Psychic", "Flying"],
 		baseStats: {hp: 90, atk: 85, def: 85, spa: 125, spd: 100, spe: 95},
 		abilities: {0: "Competitive"},
