9937bd35d515dc6ae84b030b990a332d7fa24cfe
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-02-21 21:05:14 -0700

    Update scripts.ts

--- data/mods/m4av6/scripts.ts
+++ data/mods/m4av6/scripts.ts
@@ -143,6 +143,34 @@ export const Scripts: ModdedBattleScriptsData = {
 		}
 		return item.megaStone;
 	},
+	runMegaEvo(pokemon) {
+		const speciesid = pokemon.canMegaEvo || pokemon.canUltraBurst;
+		if (!speciesid) return false;
+		const side = pokemon.side;
+
+		// Pokémon affected by Sky Drop cannot mega evolve. Enforce it here for now.
+		for (const foeActive of side.foe.active) {
+			if (foeActive.volatiles['skydrop'] && foeActive.volatiles['skydrop'].source === pokemon) {
+				return false;
+			}
+		}
+
+		pokemon.illusion = null;
+		pokemon.formeChange(speciesid, pokemon.getItem(), true);
+
+		// Limit one mega evolution
+		const wasMega = pokemon.canMegaEvo;
+		for (const ally of side.pokemon) {
+			if (wasMega) {
+				ally.canMegaEvo = null;
+			} else {
+				ally.canUltraBurst = null;
+			}
+		}
+
+		this.runEvent('AfterMega', pokemon);
+		return true;
+	},
 	pokemon: {
 		lostItemForDelibird: null,
 		setItem(item: string | Item, source?: Pokemon, effect?: Effect) {
