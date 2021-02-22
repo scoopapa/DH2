71b66226588403a84b8af571d20db80c5ca08a1b
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-02-21 21:55:57 -0700

    Update formats-data.ts

--- data/mods/m4asandbox/formats-data.ts
+++ data/mods/m4asandbox/formats-data.ts
@@ -2,6 +2,9 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 	dragonitemega: {
 		tier: "Sandbox",
 	},
+	toxtricitymeganerfed: {
+		tier: "Sandbox",
+	},
 	toxtricitylowkeymeganerfed: {
 		tier: "Sandbox",
 	},
