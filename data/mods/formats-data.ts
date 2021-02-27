e8a28e0d88a36aec8faec520dbabf574588b7f43
ViZarAT <76686131+ViZarAT@users.noreply.github.com>
2021-02-27 19:05:33 +0100

    Add files via upload

---
+++ data/mods/formats-data.ts
@@ -0,0 +1,158 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	cinderace: {
+		tier:'Uber',
+		isModded: true,
+	},
+	magearna: {
+		tier:'Uber',
+		isModded: true,
+	},
+	magearnaoriginal: {
+		tier:'Uber',
+		isModded: true,
+	},
+	spectrier: {
+		tier:'Uber',
+		isModded: true,
+	},
+	arbosaur: {
+		tier:'OU',
+		isModded: true,
+	},
+	charbarian: {
+		tier:'OU',
+		isModded: true,
+	},
+	chargaryen: {
+		tier:'OU',
+		isModded: true,
+	},
+	galapagon: {
+		tier:'OU',
+		isModded: true,
+	},
+	coleoptyran: {
+		tier:'OU',
+		isModded: true,
+	},
+	herascarab: {
+		tier:'OU',
+		isModded: true,
+	},
+	scibrawl: {
+		tier:'OU',
+		isModded: true,
+	},
+	ramjestic: {
+		tier:'OU',
+		isModded: true,
+	},
+	houndread: {
+		tier:'OU',
+		isModded: true,
+	},
+	zipette: {
+		tier:'OU',
+		isModded: true,
+	},
+	mediscend: {
+		tier:'OU',
+		isModded: true,
+	},
+	stalagmitar: {
+		tier:'OU',
+		isModded: true,
+	},
+	starietta: {
+		tier:'OU',
+		isModded: true,
+	},
+	metamalgam: {
+		tier:'OU',
+		isModded: true,
+	},
+	scawsert: {
+		tier:'OU',
+		isModded: true,
+	},
+	slowclone: {
+		tier:'OU',
+		isModded: true,
+	},
+	lordiathan: {
+		tier:'OU',
+		isModded: true,
+	},
+	gnashark: {
+		tier:'OU',
+		isModded: true,
+	},
+	dimster: {
+		tier:'OU',
+		isModded: true,
+	},
+	kangatwo: {
+		tier:'OU',
+		isModded: true,
+	},
+	kylario: {
+		tier:'OU',
+		isModded: true,
+	},
+	horrfrost: {
+		tier:'OU',
+		isModded: true,
+	},
+	blizzarex: {
+		tier:'OU',
+		isModded: true,
+	},
+	nurseheary: {
+		tier:'OU',
+		isModded: true,
+	},
+	pulgasaron: {
+		tier:'OU',
+		isModded: true,
+	},
+	orichalix: {
+		tier:'OU',
+		isModded: true,
+	},
+	humpeii: {
+		tier:'OU',
+		isModded: true,
+	},
+	sabsidian: {
+		tier:'OU',
+		isModded: true,
+	},
+	gracefuld: {
+		tier:'OU',
+		isModded: true,
+	},
+	baritaria: {
+		tier:'OU',
+		isModded: true,
+	},
+	mawtale: {
+		tier:'OU',
+		isModded: true,
+	},
+	abhornet: {
+		tier:'OU',
+		isModded: true,
+	},
+	pideastroph: {
+		tier:'OU',
+		isModded: true,
+	},
+	hocusfocus: {
+		tier:'OU',
+		isModded: true,
+	},
+	lophodactylus: {
+		tier:'OU',
+		isModded: true,
+	},
+};
