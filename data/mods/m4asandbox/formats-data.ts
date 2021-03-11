ccd3c63388bfcfe2fc5b8f0b346bdda6acca2537
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-03-11 01:24:02 -0700

    Sandbox cleanup: remove redundant Wishiwashi

--- data/mods/m4asandbox/formats-data.ts
+++ data/mods/m4asandbox/formats-data.ts
@@ -8,22 +8,4 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 	toxtricitylowkeymeganerfed: {
 		tier: "Sandbox",
 	},
-	wishiwashimega: {
-		tier: "Sandbox",
-	},
-	wishiwashimega1: {
-		tier: "Sandbox",
-	},
-	wishiwashimega2: {
-		tier: "Sandbox",
-	},
-	wishiwashimega3: {
-		tier: "Sandbox",
-	},
-	wishiwashimega4: {
-		tier: "Sandbox",
-	},
-	wishiwashimegaschool: {
-		tier: "Sandbox",
-	},
 };
