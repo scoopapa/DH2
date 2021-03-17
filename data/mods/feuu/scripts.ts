0acceff791a60c35139bb3f25e2be42858abbd48
inkbug1 <72170979+inkbug1@users.noreply.github.com>
2021-03-16 19:29:43 -0700

    Update scripts.ts
    Attempt 1 to fix the nonstandard move exclusion code

--- data/mods/feuu/scripts.ts
+++ data/mods/feuu/scripts.ts
@@ -18,7 +18,7 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 				for (let name of learnsetFusionList) {					
 					const learnset = this.dataCache.Learnsets[this.toID(name)].learnset;//get the learnset of each pokemon in the list
 					for (const moveid in learnset) {
-						if (this.dex.getMove(moveid).isNonstandard === 'Past') continue; //exclude dexited moves (I hope!) 
+						if (this.dataCache.Moves[moveid].isNonstandard === 'Past') continue; //exclude dexited moves (I hope!) 
 						this.modData('Learnsets', id).learnset[moveid] = ['8L1'];//all moves are compatible with the fusion's only ability, so just set it to 8L1
 					}
 				}
