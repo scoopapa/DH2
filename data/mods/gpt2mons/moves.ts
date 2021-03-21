806f7367dd2f05555c3153c22dcc0486636d2a7f
RTL-27 <80811098+RTL-27@users.noreply.github.com>
2021-03-20 20:23:57 -0700

    Update moves.ts

--- data/mods/gpt2mons/moves.ts
+++ data/mods/gpt2mons/moves.ts
@@ -453,6 +453,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		onHit(pokemon) {
 				if (pokemon.level >= 1) {
 				pokemon.level = Math.max(1, pokemon.level + 3);
+				},
 		},
 		secondary: null,
 		target: "self",
