8046668675edbfbf94e75f89f81ac2840d07de59
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-04-07 22:04:03 -0400

    Create formats-data.ts

---
+++ data/mods/genx/formats-data.ts
@@ -0,0 +1,38 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	peepin: {
+		tier: "LC",
+		doublesTier: "(DUU)",
+	},
+	scareech: {
+		tier: "NFE",
+		doublesTier: "(DUU)",
+	},
+	macawphony: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	folovo: {
+		tier: "LC",
+		doublesTier: "(DUU)",
+	},
+	sworchid: {
+		tier: "NFE",
+		doublesTier: "(DUU)",
+	},
+	esplada: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pescafin: {
+		tier: "LC",
+		doublesTier: "(DUU)",
+	},
+	pedrowana: {
+		tier: "NFE",
+		doublesTier: "(DUU)",
+	},
+	arapaitan: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+};
