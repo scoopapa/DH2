07f82377efe79528bb0faad1ecaaec8b4a7b5876
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-04-08 07:45:27 -0400

    Create abilities.ts

---
+++ data/mods/genx/abilities.ts
@@ -0,0 +1,46 @@
+export const Abilities: {[k: string]: ModdedAbilityData} = {
+/*
+	windblaster: {
+		id: "windblaster",
+		name: "Wind Blaster",
+		shortDesc: "This Pokemon blocks non-contact Flying-type moves and Whirlwind and bounces them back to the user.",
+		onTryHitPriority: 1,
+		onTryHit(target, source, move) {
+			if (target === source || move.hasBounced || move.type !== 'Flying') {
+				return;
+			}
+			const newMove = this.dex.getActiveMove(move.id);
+			newMove.hasBounced = true;
+			newMove.pranksterBoosted = false;
+			this.useMove(newMove, target, source);
+			return null;
+		},
+		onAllyTryHitSide(target, source, move) {
+			if (target.side === source.side || move.hasBounced || move.type !== 'Flying') {
+				return;
+			}
+			const newMove = this.dex.getActiveMove(move.id);
+			newMove.hasBounced = true;
+			newMove.pranksterBoosted = false;
+			this.useMove(newMove, this.effectData.target, source);
+			return null;
+		},
+		condition: {
+			duration: 1,
+		},
+	},
+*/
+	piercingvision: {
+		id: "piercingvision",
+		onModifyMovePriority: -5,
+		onModifyMove(move) {
+			if (!move.ignoreImmunity) move.ignoreImmunity = {};
+			if (move.ignoreImmunity !== true) {
+				move.ignoreImmunity['Psychic'] = true;
+			}
+		},
+		name: "Piercing Vision",
+		shortDesc: "This Pokemon's Psychic-type moves can hit Dark-types.",
+		rating: 3,
+	},	
+};
