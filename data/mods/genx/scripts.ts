a7d6189661bab58e338c9a87b079832c0049230d
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-04-07 21:30:34 -0400

    Create scripts.ts

---
+++ data/mods/genx/scripts.ts
@@ -0,0 +1,22 @@
+export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
+	init: function () {
+/*
+		for (const id in this.dataCache.Pokedex) {
+			const poke = this.dataCache.Pokedex[id];
+			if (poke.restrictedLearnset) {
+				console.log(this.toID(poke.name));
+				const thisPoke = this.toID(poke.name);
+				const learnset = this.dataCache.Learnsets[this.toID(poke.name)].learnset;
+				for (const move in learnset) {
+					console.log(thisPoke + " has " + move);
+					const moveid = this.dataCache.Moves[move];
+					if (moveid.isNonstandard) {
+						console.log(moveid.isNonstandard);
+						delete this.modData('Learnsets', thisPoke).learnset.moveid;
+					}
+				}
+			}
+		}
+*/
+	},
+};
