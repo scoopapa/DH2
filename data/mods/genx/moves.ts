76d7184bc9c74075b1cee44b3c9de58ce7975694
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-04-07 23:24:18 -0400

    Create moves.ts

---
+++ data/mods/genx/moves.ts
@@ -0,0 +1,53 @@
+export const Moves: {[moveid: string]: ModdedMoveData} = {
+	citrusysting: {
+		accuracy: 90,
+		basePower: 0,
+		category: "Status",
+    shortDesc: "Paralyzes the target. Grass-types are immune.",
+		isViable: true,
+		name: "Citrusy Sting",
+		pp: 20,
+		priority: 0,
+		flags: {protect: 1, reflectable: 1, mirror: 1, powder: 1},
+		status: 'par',
+		ignoreImmunity: false,
+		secondary: null,
+		target: "normal",
+		type: "Grass",
+		zMove: {boost: {spd: 1}},
+		contestType: "Cool",
+	},
+	berryblast: {
+		accuracy: 100,
+		basePower: 0,
+		category: "Special",
+    shortDesc: "Power and type depends on the user's berry.",
+		isViable: true,
+		name: "Berry Blast",
+		pp: 15,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		onModifyType(move, pokemon) {
+			if (pokemon.ignoringItem()) return;
+			const item = pokemon.getItem();
+			if (!item.naturalGift) return;
+			move.type = item.naturalGift.type;
+		},
+		onPrepareHit(target, pokemon, move) {
+			if (pokemon.ignoringItem()) return false;
+			const item = pokemon.getItem();
+			if (!item.naturalGift) return false;
+			move.basePower = item.naturalGift.basePower;
+			pokemon.setItem('');
+			pokemon.lastItem = item.id;
+			pokemon.usedItemThisTurn = true;
+			this.runEvent('AfterUseItem', pokemon, null, null, item);
+		},
+		secondary: null,
+		target: "normal",
+		type: "Normal",
+		zMove: {basePower: 160},
+		maxMove: {basePower: 130},
+		contestType: "Clever",
+	},
+};
