0d95eb30c45991dc618d20a84ff3a88dd6de8e61
Home User <vizarsmogon@gmail.com>
2021-04-17 23:45:30 +0200

    Update abilities.ts

--- data/mods/gen3hoenngaiden/abilities.ts
+++ data/mods/gen3hoenngaiden/abilities.ts
@@ -229,6 +229,7 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 	},
 	rkssystem: {
 		// RKS System's type-changing itself is implemented in statuses.js
+		inherit: true,
 		isPermanent: true,
 	},
 };
