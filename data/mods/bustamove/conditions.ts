12866096a39b47e27f55868e7618ec9823394262
Home User <vizarsmogon@gmail.com>
2021-04-30 16:25:05 +0200

    Update conditions.ts

--- data/mods/bustamove/conditions.ts
+++ data/mods/bustamove/conditions.ts
@@ -1,6 +1,9 @@
 export const Conditions: {[k: string]: ConditionData} = {
 	jawlock: {
 		name: 'jawlock',
+		onStart(pokemon, source) {
+			this.add('-activate', pokemon, 'move: ' + this.effectData.sourceEffect, '[of] ' + source);
+		},
 		onHit(target, source, sourceEffect) {
 			if (sourceEffect && sourceEffect.effectType === 'Ability') {
 				this.add('-status', target, 'jawlock');
@@ -8,13 +11,10 @@ export const Conditions: {[k: string]: ConditionData} = {
 		},
 		onResidualOrder: 9,
 		onResidual(pokemon) {
-			const gmaxEffect = ['gmaxcentiferno', 'gmaxsandblast'].includes(this.effectData.sourceEffect.id);
-			if (!source.isActive || source.hp <= 0 || !source.activeTurns) {
-				delete pokemon.volatiles['partiallytrapped'];
-				this.add('-end', pokemon, this.effectData.sourceEffect, '[partiallytrapped]', '[silent]');
-				return;
-			}
 			this.damage(pokemon.baseMaxhp / 8);
+		},
+		onEnd(pokemon) {
+			this.add('-end', pokemon, this.effectData.sourceEffect, '[jawlock]');
 		},
 	},
 };
