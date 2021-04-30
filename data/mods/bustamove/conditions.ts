09d8f1bee412b33354b1bff34fa40fbac7ef0dc3
Home User <vizarsmogon@gmail.com>
2021-04-30 16:15:27 +0200

    Update conditions.ts

--- data/mods/bustamove/conditions.ts
+++ data/mods/bustamove/conditions.ts
@@ -8,12 +8,13 @@ export const Conditions: {[k: string]: ConditionData} = {
 		},
 		onResidualOrder: 9,
 		onResidual(pokemon) {
-			this.damage(pokemon.baseMaxhp / 8);
-		},
-		onEnd(target) {
-			if (pokemon.newlySwitched) {
-				delete pokemon.volatiles['jawlock'];
+			const gmaxEffect = ['gmaxcentiferno', 'gmaxsandblast'].includes(this.effectData.sourceEffect.id);
+			if (source && (!source.isActive || source.hp <= 0 || !source.activeTurns) && !gmaxEffect) {
+				delete pokemon.volatiles['partiallytrapped'];
+				this.add('-end', pokemon, this.effectData.sourceEffect, '[partiallytrapped]', '[silent]');
+				return;
 			}
+			this.damage(pokemon.baseMaxhp / 8);
 		},
 	},
 };
