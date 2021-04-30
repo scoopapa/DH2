15aec25a75c7d3bab467dea4346417e3f113f83c
Home User <vizarsmogon@gmail.com>
2021-04-30 17:42:40 +0200

    Update moves.ts

--- data/mods/bustamove/moves.ts
+++ data/mods/bustamove/moves.ts
@@ -134,7 +134,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1},
 		onModifyMove(basePower, source) {
-			if (this.field.isSideConditions('stealthrock')) {
+			if (this.side.isSideConditions('stealthrock')) {
 				this.chainModify(1.5);
 			}
 		},
