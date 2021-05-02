c5ec9e2c7602e3e924a0741f28ea8ed2f0f47fff
inkbug1 <72170979+inkbug1@users.noreply.github.com>
2021-05-02 13:59:02 -0700

    Update pokedex.ts

--- data/mods/feuu/pokedex.ts
+++ data/mods/feuu/pokedex.ts
@@ -626,7 +626,7 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		num: 50,
 		name: "Weezlord-Galar",
 		types: ["Fairy", "Dark"],
-		baseStats: {hp: 151, atk: 103, def: 97, spa: 103 spd: 61, spe: 53},
+		baseStats: {hp: 151, atk: 103, def: 97, spa: 103, spd: 61, spe: 53},
 		abilities: {0: "Lemegeton"},
 		weightkg: 452,
 	},
