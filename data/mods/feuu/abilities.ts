c84a7e3e787a515fe430b194e3597da45851333d
scoopapa <pccompositor@aim.com>
2021-05-02 02:32:01 -0500

    delete log message

--- data/mods/feuu/abilities.ts
+++ data/mods/feuu/abilities.ts
@@ -913,7 +913,6 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 			if (target.isSemiInvulnerable()) return;
 			if (target.ignoringItem()) return false;
 			const item = target.getItem();
-			console.log(item);
 			if (!this.singleEvent('TakeItem', item, target.itemData, target, target, move, item)) return false;
 			if (item.id && !item.megaStone) {
 				this.damage(source.baseMaxhp / 4, source, target);
