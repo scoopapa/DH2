63ec89bb12c066edc94349c280b99f90b4b51316
Home User <vizarsmogon@gmail.com>
2021-05-09 18:28:13 +0200

    Update conditions.ts

--- data/mods/jounrey2kilirthy/conditions.ts
+++ data/mods/jounrey2kilirthy/conditions.ts
@@ -720,14 +720,19 @@ export const Conditions: {[k: string]: ConditionData} = {
 			this.add('-weather', 'Acid Rain', '[upkeep]');
 			if (this.field.isWeather('acidrain')) this.eachEvent('Weather');
 		},
-		/*onWeather(target) {
-			this.damage(target.baseMaxhp / 16);
-			if (type === 'Fairy', 'Grass', 'Water') this.damage(target.baseMaxhp / 8);
-			else if (type === 'Ghost', 'Ground', 'Rock') this.damage(target.baseMaxhp / 32);
-			else (type === 'Poison', 'Steel') this.damage(target.baseMaxhp / 0);
-			},
-		},
 		onWeather(target) {
+			this.damage(target.baseMaxhp / 16);
+			if (type === “Water” || type === “Grass” || type === “Fairy”) {  
+			this.damage(target.baseMaxhp / 8);
+			}
+			else if (type === “Rock” || type === “Ground” || type === “Ghost”) {  
+			this.damage(target.baseMaxhp / 32);
+			}
+			else (type === “Poison” || type === “Steel””) {  
+			this.damage(target.baseMaxhp / 0);
+			}
+		},
+		/*onWeather(target) {
 			if (type === 'Bug', 'Dark', 'Dragon', 'Electric', 'Fighting', 'Fire', 'Flying', 'Ice', 'Normal', 'Psychic');
 			this.damage(target.baseMaxhp / 16); {
 			} else (type === 'Ghost', 'Ground', 'Rock');
