66fd9e4c20c2eb6a9669387d2c0a845b4cdef681
Home User <vizarsmogon@gmail.com>
2021-05-12 15:31:29 +0200

    Update conditions.ts

--- data/mods/jounrey2kilirthy/conditions.ts
+++ data/mods/jounrey2kilirthy/conditions.ts
@@ -730,7 +730,7 @@ export const Conditions: {[k: string]: ConditionData} = {
 		},
 		onWeather(target) {
 			this.damage(target.baseMaxhp / 16);
-			if (type === 'Poison' || type === 'Steel' ) return false;
+			if (targe.type === 'Poison' || target.type === 'Steel' ) return false;
 		},
 		onStart: function (battle, source, effect) {
 			if (effect && effect.effectType === 'Ability') {
