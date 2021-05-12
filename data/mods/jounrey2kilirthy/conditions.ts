56b472494afe80e258624d55969592da28f6e6e1
Home User <vizarsmogon@gmail.com>
2021-05-12 14:10:59 +0200

    Update conditions.ts

--- data/mods/jounrey2kilirthy/conditions.ts
+++ data/mods/jounrey2kilirthy/conditions.ts
@@ -715,7 +715,7 @@ export const Conditions: {[k: string]: ConditionData} = {
 			if (this.field.isWeather('acidrain')) this.eachEvent('Weather');
 		},
 		onWeather(pokemon) {
-			this.damage(pokemon.maxhp * Math.pow(2, typeMod) / 8);
+			this.damage(pokemon.baseMaxhp * Math.pow(2, typeMod) / 8);
 			const typeMod = this(pokemon.runEffectiveness(Poison));
 			/*this.damage(target.baseMaxhp / 16);
 			if (target.type === "Water" || target.type === "Grass" || target.type === "Fairy") {
