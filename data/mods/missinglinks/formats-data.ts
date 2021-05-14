7fb5d7ec15cfbbf7d1de1854074b947e30308780
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-05-14 01:12:54 -0400

    Create formats-data.ts

---
+++ data/mods/missinglinks/formats-data.ts
@@ -0,0 +1,10 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	butterfreemega: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	canonbell: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+};
