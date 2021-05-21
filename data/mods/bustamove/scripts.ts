cb37506fe2e206008ed53ba42bfbb4d16c74d772
Home User <vizarsmogon@gmail.com>
2021-05-21 18:07:42 +0200

    Update scripts.ts

--- data/mods/bustamove/scripts.ts
+++ data/mods/bustamove/scripts.ts
@@ -2,7 +2,7 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 	init() {
 		const newMoves = (mon: string, moves: string[]) => {
 			for (const move of moves) {
-				this.modData('Learnsets', this.toID(mon)).learnset[this.toID(move)] = ["8M"];
+				this.modData('Learnsets', this.toID(mon)).learnset[this.toID(move)] = ["8L1"];
 			}
 		};
     //newMoves("pkmn", ["move"]);
