2f1292576f84c544786307359585ae9aa3f36f8f
Home User <vizarsmogon@gmail.com>
2021-05-21 20:09:48 +0200

    Update scripts.ts

--- data/mods/bustamove/scripts.ts
+++ data/mods/bustamove/scripts.ts
@@ -1,22 +1,20 @@
+//this.modData('Learnsets', 'pkmn').learnset.move = ['8L1'];
+
 export const Scripts: BattleScriptsData = {
   init: function() {
-	//this.modData('Learnsets', 'pkmn').learnset.move = ['8L1'];
-	//Burning Jealousy
+	
 	this.modData('Learnsets', 'houndoom').learnset.burningjealousy = ['8L1'];
 	this.modData('Learnsets', 'infernape').learnset.burningjealousy = ['8L1'];
 	this.modData('Learnsets', 'pyroar').learnset.burningjealousy = ['8L1'];
 	
-	//Decorate
 	this.modData('Learnsets', 'hatterene').learnset.decorate = ['8L1'];
 	this.modData('Learnsets', 'jirachi').learnset.decorate = ['8L1'];
 	this.modData('Learnsets', 'victini').learnset.decorate = ['8L1'];
 	
-	//Eerie Spell
 	this.modData('Learnsets', 'delphox').learnset.eeriespell = ['8L1'];
 	this.modData('Learnsets', 'hatterene').learnset.eeriespell = ['8L1'];
 	this.modData('Learnsets', 'mismagius').learnset.eeriespell = ['8L1'];
 	
-	//Fairy Wind
 	this.modData('Learnsets', 'celffa').learnset.fairywind = ['8L1'];
 	this.modData('Learnsets', 'clefairy').learnset.fairywind = ['8L1'];
 	this.modData('Learnsets', 'clefable').learnset.fairywind = ['8L1'];
@@ -26,7 +24,6 @@ export const Scripts: BattleScriptsData = {
 	this.modData('Learnsets', 'primarina').learnset.fairywind = ['8L1'];
 	this.modData('Learnsets', 'mew').learnset.fairywind = ['8L1'];
 	
-	//Jaw Lock
 	this.modData('Learnsets', 'rockruff').learnset.jawlock = ['8L1'];
 	this.modData('Learnsets', 'lycanroc').learnset.jawlock = ['8L1'];
 	this.modData('Learnsets', 'lycanrocdusk').learnset.jawlock = ['8L1'];
@@ -34,7 +31,6 @@ export const Scripts: BattleScriptsData = {
 	this.modData('Learnsets', 'tyrunt').learnset.jawlock = ['8L1'];
 	this.modData('Learnsets', 'tyrantrum').learnset.jawlock = ['8L1'];
 	
-	//Razor Wind
 	this.modData('Learnsets', 'tornadus').learnset.razorwind = ['8L1'];
 	this.modData('Learnsets', 'thundurus').learnset.razorwind = ['8L1'];
 	this.modData('Learnsets', 'landorus').learnset.razorwind = ['8L1'];
@@ -44,7 +40,6 @@ export const Scripts: BattleScriptsData = {
 	this.modData('Learnsets', 'celesteela').learnset.razorwind = ['8L1'];
 	this.modData('Learnsets', 'xatu').learnset.razorwind = ['8L1'];
 	
-	//Strength
 	this.modData('Learnsets', 'beedrill').learnset.strength = ['8L1'];
 	},
 };
