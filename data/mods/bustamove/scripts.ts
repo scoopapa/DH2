1969a1fd9a76257dd0fdf45871f1cd8c3f346a84
Home User <vizarsmogon@gmail.com>
2021-05-21 19:40:30 +0200

    Update scripts.ts

--- data/mods/bustamove/scripts.ts
+++ data/mods/bustamove/scripts.ts
@@ -1,55 +1,51 @@
 export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
-	/*init() {
-		const newMoves = (mon: string, moves: string[]) => {
-			for (const move of moves) {
-				this.modData('Learnsets', this.toID(mon)).learnset[this.toID(move)] = ["8L1"];
-			}
-		};
-    //newMoves("pkmn", ["move"]);
-	//Burning Jealousy
-	newMoves("houndoom", ["burningjealousy"]);
-	newMoves("infernape", ["burningjealousy"]);
-	newMoves("pyroar", ["burningjealousy"]);
+	init: function {
+		
+		//newMoves("pkmn", ["move"]);
+		//Burning Jealousy
+		newMoves("houndoom", ["burningjealousy"]);
+		newMoves("infernape", ["burningjealousy"]);
+		newMoves("pyroar", ["burningjealousy"]);
 	
-	//Decorate
-	newMoves("hatterene", ["decorate"]);
-	newMoves("jirachi", ["decorate"]);
-	newMoves("victini", ["decorate"]);
+		//Decorate
+		newMoves("hatterene", ["decorate"]);
+		newMoves("jirachi", ["decorate"]);
+		newMoves("victini", ["decorate"]);
 	
-	//Eerie Spell
-	newMoves("delphox", ["eeriespell"]);
-	newMoves("hatterene", ["eeriespell"]);
-	newMoves("mismagius", ["eeriespell"]);
+		//Eerie Spell
+		newMoves("delphox", ["eeriespell"]);
+		newMoves("hatterene", ["eeriespell"]);
+		newMoves("mismagius", ["eeriespell"]);
 	
-	//Fairy Wind
-	newMoves("celffa", ["fairywind"]);
-	newMoves("clefairy", ["fairywind"]);
-	newMoves("clefable", ["fairywind"]);
-	newMoves("hatenna", ["fairywind"]);
-	newMoves("hattrem", ["fairywind"]);
-	newMoves("hatterene", ["fairywind"]);
-	newMoves("primarina", ["fairywind"]);
-	newMoves("mew", ["fairywind"]);
+		//Fairy Wind
+		newMoves("celffa", ["fairywind"]);
+		newMoves("clefairy", ["fairywind"]);
+		newMoves("clefable", ["fairywind"]);
+		newMoves("hatenna", ["fairywind"]);
+		newMoves("hattrem", ["fairywind"]);
+		newMoves("hatterene", ["fairywind"]);
+		newMoves("primarina", ["fairywind"]);
+		newMoves("mew", ["fairywind"]);
 	
-	//Jaw Lock
-	newMoves("rockruff", ["jawlock"]);
-	newMoves("lycanroc", ["jawlock"]);
-	newMoves("lycanrocdusk", ["jawlock"]);
-	newMoves("lycanrocmidnight", ["jawlock"]);
-	newMoves("tyrunt", ["jawlock"]);
-	newMoves("tyrantrum", ["jawlock"]);
+		//Jaw Lock
+		newMoves("rockruff", ["jawlock"]);
+		newMoves("lycanroc", ["jawlock"]);
+		newMoves("lycanrocdusk", ["jawlock"]);
+		newMoves("lycanrocmidnight", ["jawlock"]);
+		newMoves("tyrunt", ["jawlock"]);
+		newMoves("tyrantrum", ["jawlock"]);
 	
-	//Razor Wind
-	newMoves("tornadus", ["razorwind"]);
-	newMoves("thundurus", ["razorwind"]);
-	newMoves("landorus", ["razorwind"]);
-	newMoves("articunogalar", ["razorwind"]);
-	newMoves("zapdosgalar", ["razorwind"]);
-	newMoves("moltresgalar", ["razorwind"]);
-	newMoves("celesteela", ["razorwind"]);
-	newMoves("xatu", ["razorwind"]);
+		//Razor Wind
+		newMoves("tornadus", ["razorwind"]);
+		newMoves("thundurus", ["razorwind"]);
+		newMoves("landorus", ["razorwind"]);
+		newMoves("articunogalar", ["razorwind"]);
+		newMoves("zapdosgalar", ["razorwind"]);
+		newMoves("moltresgalar", ["razorwind"]);
+		newMoves("celesteela", ["razorwind"]);
+		newMoves("xatu", ["razorwind"]);
 	
-	//Strength
-	newMoves("beedrill", ["strength"]);
-  },*/
+		//Strength
+		newMoves("beedrill", ["strength"]);
+  },
 };
