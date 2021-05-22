7d38087cec870a0e2db3ec7ef4dc808c493b1229
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-05-22 16:37:57 -0600

    I'm an idiot sometiiimes
    so I actually just didn't realize it was case-sensitive
    it works now but I want to see if my shortened version worked in the first place
    also making it not case-sensitive and seeing if I can make the nicknames appear more normal .w.

--- data/mods/m4asandbox/rulesets.ts
+++ data/mods/m4asandbox/rulesets.ts
@@ -5,125 +5,87 @@ export const Formats: {[k: string]: FormatData} = {
 		desc: "Allows customization of a Pokémon's types and stats based on its nickname.",
 		onModifySpecies(species, target, source) {
 			if (source || !target?.side) return;
-			if (target.set.name.substr(0, 1) == "*") {
+			if (target.set.name.substr(0, 1) === "*") {
 				let newSpecies = this.dex.deepClone(species);
-				switch (target.set.name.substr(1, 1)) {
-					case "a":
-						newSpecies.types[0] = "Dragon";
-						break;
-					case "b":
-						newSpecies.types[0] = "Bug";
-						break;
-					case "c":
-						newSpecies.types[0] = "Psychic";
-						break;
-					case "d":
-						newSpecies.types[0] = "Dark";
-						break;
-					case "e":
-						newSpecies.types[0] = "Electric";
-						break;
-					case "f":
-						newSpecies.types[0] = "Fairy";
-						break;
-					case "g":
-						newSpecies.types[0] = "Grass";
-						break;
-					case "h":
-						newSpecies.types[0] = "Fighting";
-						break;
-					case "i":
-						newSpecies.types[0] = "Ice";
-						break;
-					case "k":
-						newSpecies.types[0] = "Rock";
-						break;
-					case "n":
-						newSpecies.types[0] = "Normal";
-						break;
-					case "o":
-						newSpecies.types[0] = "Ghost";
-						break;
-					case "p":
-	   				newSpecies.types[0] = "Poison";
-						break;
-					case "r":
-						newSpecies.types[0] = "Fire";
-						break;
-					case "s":
-						newSpecies.types[0] = "Steel";
-						break;
-					case "u":
-						newSpecies.types[0] = "Ground";
-						break;
-					case "w":
-						newSpecies.types[0] = "Water";
-						break;
-					case "y":
-						newSpecies.types[0] = "Flying";
-						break;
-					case "z":
-						newSpecies.types[0] = "";
-						break;
-				}
-				switch (target.set.name.substr(2, 1)) {
-					case "a":
-						newSpecies.types[1] = "Dragon";
-						break;
-					case "b":
-						newSpecies.types[1] = "Bug";
-						break;
-					case "c":
-						newSpecies.types[1] = "Psychic";
-						break;
-					case "d":
-						newSpecies.types[1] = "Dark";
-						break;
-					case "e":
-						newSpecies.types[1] = "Electric";
-						break;
-					case "f":
-						newSpecies.types[1] = "Fairy";
-						break;
-					case "g":
-						newSpecies.types[1] = "Grass";
-						break;
-					case "h":
-						newSpecies.types[1] = "Fighting";
-						break;
-					case "i":
-						newSpecies.types[1] = "Ice";
-						break;
-					case "k":
-						newSpecies.types[1] = "Rock";
-						break;
-					case "n":
-						newSpecies.types[1] = "Normal";
-						break;
-					case "o":
-						newSpecies.types[1] = "Ghost";
-						break;
-					case "p":
-	   				newSpecies.types[1] = "Poison";
-						break;
-					case "r":
-						newSpecies.types[1] = "Fire";
-						break;
-					case "s":
-						newSpecies.types[1] = "Steel";
-						break;
-					case "u":
-						newSpecies.types[1] = "Ground";
-						break;
-					case "w":
-						newSpecies.types[1] = "Water";
-						break;
-					case "y":
-						newSpecies.types[1] = "Flying";
-						break;
-					case "z":
-						newSpecies.types[1] = "";
-						break;
+				for (const type in [0, 1]) {
+					switch (target.set.name.substr((type + 1), 1)) {
+						case "a":
+						case "A":
+							newSpecies.types[type] = "Dragon";
+							break;
+						case "b":
+						case "B":
+							newSpecies.types[type] = "Bug";
+							break;
+						case "c":
+						case "C":
+							newSpecies.types[type] = "Psychic";
+							break;
+						case "d":
+						case "D":
+							newSpecies.types[type] = "Dark";
+							break;
+						case "e":
+						case "E":
+							newSpecies.types[type] = "Electric";
+							break;
+						case "f":
+						case "F":
+							newSpecies.types[type] = "Fairy";
+							break;
+						case "g":
+						case "G":
+							newSpecies.types[type] = "Grass";
+							break;
+						case "h":
+						case "H":
+							newSpecies.types[type] = "Fighting";
+							break;
+						case "i":
+						case "I":
+							newSpecies.types[type] = "Ice";
+							break;
+						case "k":
+						case "K":
+							newSpecies.types[type] = "Rock";
+							break;
+						case "n":
+						case "N":
+							newSpecies.types[type] = "Normal";
+							break;
+						case "o":
+						case "O":
+							newSpecies.types[type] = "Ghost";
+							break;
+						case "p":
+						case "P":
+		   				newSpecies.types[type] = "Poison";
+							break;
+						case "r":
+						case "R":
+							newSpecies.types[type] = "Fire";
+							break;
+						case "s":
+						case "S":
+							newSpecies.types[type] = "Steel";
+							break;
+						case "u":
+						case "U":
+							newSpecies.types[type] = "Ground";
+							break;
+						case "w":
+						case "W":
+							newSpecies.types[type] = "Water";
+							break;
+						case "y":
+						case "Y":
+							newSpecies.types[type] = "Flying";
+							break;
+						case "z":
+						case "Z":
+							newSpecies.types[type] = "";
+							break;
+					}
 				}
 				newSpecies.baseStats.atk = target.set.name.substr(3, 3);
 				newSpecies.baseStats.def = target.set.name.substr(6, 3);
@@ -131,6 +93,7 @@ export const Formats: {[k: string]: FormatData} = {
 				newSpecies.baseStats.spd = target.set.name.substr(12, 3);
 				newSpecies.baseStats.spe = target.set.name.substr(15, 3);
 				target.isModded = true;
+				target.set.name = target.species.name;
 				return newSpecies;
 			}
 		},
