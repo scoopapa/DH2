c48c9d3425253240321f5c91875d8f5d0c78790c
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-05-23 13:38:29 -0600

    Okay
    This is the last one that was recorded as working so I will leave it at this for now

--- data/mods/m4asandbox/rulesets.ts
+++ data/mods/m4asandbox/rulesets.ts
@@ -3,179 +3,174 @@ export const Formats: {[k: string]: FormatData} = {
 		effectType: 'Rule',
 		name: 'Sandbox Mod',
 		desc: "Allows customization of a Pokémon's types and stats based on its nickname.",
-		onBegin() {
-			for (const pokemon of this.getAllPokemon()) {
-				if (!pokemon.set.name) return;
-				if (pokemon.set.name.substr(0, 1) === "*") {
-					let newSpecies = this.dex.deepClone(pokemon.species);
-					switch (pokemon.set.name.substr(1, 1)) {
-						case "a":
-						case "A":
-							newSpecies.types[0] = "Dragon";
-							break;
-						case "b":
-						case "B":
-							newSpecies.types[0] = "Bug";
-							break;
-						case "c":
-						case "C":
-							newSpecies.types[0] = "Psychic";
-							break;
-						case "d":
-						case "D":
-							newSpecies.types[0] = "Dark";
-							break;
-						case "e":
-						case "E":
-							newSpecies.types[0] = "Electric";
-							break;
-						case "f":
-						case "F":
-							newSpecies.types[0] = "Fairy";
-							break;
-						case "g":
-						case "G":
-							newSpecies.types[0] = "Grass";
-							break;
-						case "h":
-						case "H":
-							newSpecies.types[0] = "Fighting";
-							break;
-						case "i":
-						case "I":
-							newSpecies.types[0] = "Ice";
-							break;
-						case "k":
-						case "K":
-							newSpecies.types[0] = "Rock";
-							break;
-						case "n":
-						case "N":
-							newSpecies.types[0] = "Normal";
-							break;
-						case "o":
-						case "O":
-							newSpecies.types[0] = "Ghost";
-							break;
-						case "p":
-						case "P":
-	   					newSpecies.types[0] = "Poison";
-							break;
-						case "r":
-						case "R":
-							newSpecies.types[0] = "Fire";
-							break;
-						case "s":
-						case "S":
-							newSpecies.types[0] = "Steel";
-							break;
-						case "u":
-						case "U":
-							newSpecies.types[0] = "Ground";
-							break;
-						case "w":
-						case "W":
-							newSpecies.types[0] = "Water";
-							break;
-						case "y":
-						case "Y":
-							newSpecies.types[0] = "Flying";
-							break;
-						case "z":
-						case "Z":
-							newSpecies.types[0] = "";
-							break;
-					}
-					switch (pokemon.set.name.substr(2, 1)) {
-						case "a":
-						case "A":
-							newSpecies.types[1] = "Dragon";
-							break;
-						case "b":
-						case "B":
-							newSpecies.types[1] = "Bug";
-							break;
-						case "c":
-						case "C":
-							newSpecies.types[1] = "Psychic";
-							break;
-						case "d":
-						case "D":
-							newSpecies.types[1] = "Dark";
-							break;
-						case "e":
-						case "E":
-							newSpecies.types[1] = "Electric";
-							break;
-						case "f":
-						case "F":
-							newSpecies.types[1] = "Fairy";
-							break;
-						case "g":
-						case "G":
-							newSpecies.types[1] = "Grass";
-							break;
-						case "h":
-						case "H":
-							newSpecies.types[1] = "Fighting";
-							break;
-						case "i":
-						case "I":
-							newSpecies.types[1] = "Ice";
-							break;
-						case "k":
-						case "K":
-							newSpecies.types[1] = "Rock";
-							break;
-						case "n":
-						case "N":
-							newSpecies.types[1] = "Normal";
-							break;
-						case "o":
-						case "O":
-							newSpecies.types[1] = "Ghost";
-							break;
-						case "p":
-						case "P":
-	   					newSpecies.types[1] = "Poison";
-							break;
-						case "r":
-						case "R":
-							newSpecies.types[1] = "Fire";
-							break;
-						case "s":
-						case "S":
-							newSpecies.types[1] = "Steel";
-							break;
-						case "u":
-						case "U":
-							newSpecies.types[1] = "Ground";
-							break;
-						case "w":
-						case "W":
-							newSpecies.types[1] = "Water";
-							break;
-						case "y":
-						case "Y":
-							newSpecies.types[1] = "Flying";
-							break;
-						case "z":
-						case "Z":
-							newSpecies.types[1] = "";
-							break;
-					}
-					newSpecies.baseStats.atk = pokemon.set.name.substr(3, 3);
-					newSpecies.baseStats.def = pokemon.set.name.substr(6, 3);
-					newSpecies.baseStats.spa = pokemon.set.name.substr(9, 3);
-					newSpecies.baseStats.spd = pokemon.set.name.substr(12, 3);
-					newSpecies.baseStats.spe = pokemon.set.name.substr(15, 3);
-					pokemon.isModded = newSpecies;
-				}
-			}
-		},
 		onModifySpecies(species, target, source) {
 			if (source || !target?.side) return;
-			if (target.isModded) return target.isModded;
+			if (target.set.name.substr(0, 1) === "*") {
+				let newSpecies = this.dex.deepClone(species);
+				switch (target.set.name.substr(1, 1)) {
+					case "a":
+					case "A":
+						newSpecies.types[0] = "Dragon";
+						break;
+					case "b":
+					case "B":
+						newSpecies.types[0] = "Bug";
+						break;
+					case "c":
+					case "C":
+						newSpecies.types[0] = "Psychic";
+						break;
+					case "d":
+					case "D":
+						newSpecies.types[0] = "Dark";
+						break;
+					case "e":
+					case "E":
+						newSpecies.types[0] = "Electric";
+						break;
+					case "f":
+					case "F":
+						newSpecies.types[0] = "Fairy";
+						break;
+					case "g":
+					case "G":
+						newSpecies.types[0] = "Grass";
+						break;
+					case "h":
+					case "H":
+						newSpecies.types[0] = "Fighting";
+						break;
+					case "i":
+					case "I":
+						newSpecies.types[0] = "Ice";
+						break;
+					case "k":
+					case "K":
+						newSpecies.types[0] = "Rock";
+						break;
+					case "n":
+					case "N":
+						newSpecies.types[0] = "Normal";
+						break;
+					case "o":
+					case "O":
+						newSpecies.types[0] = "Ghost";
+						break;
+					case "p":
+					case "P":
+	   				newSpecies.types[0] = "Poison";
+						break;
+					case "r":
+					case "R":
+						newSpecies.types[0] = "Fire";
+						break;
+					case "s":
+					case "S":
+						newSpecies.types[0] = "Steel";
+						break;
+					case "u":
+					case "U":
+						newSpecies.types[0] = "Ground";
+						break;
+					case "w":
+					case "W":
+						newSpecies.types[0] = "Water";
+						break;
+					case "y":
+					case "Y":
+						newSpecies.types[0] = "Flying";
+						break;
+					case "z":
+					case "Z":
+						newSpecies.types[0] = "";
+						break;
+				}
+				switch (target.set.name.substr(2, 1)) {
+					case "a":
+					case "A":
+						newSpecies.types[1] = "Dragon";
+						break;
+					case "b":
+					case "B":
+						newSpecies.types[1] = "Bug";
+						break;
+					case "c":
+					case "C":
+						newSpecies.types[1] = "Psychic";
+						break;
+					case "d":
+					case "D":
+						newSpecies.types[1] = "Dark";
+						break;
+					case "e":
+					case "E":
+						newSpecies.types[1] = "Electric";
+						break;
+					case "f":
+					case "F":
+						newSpecies.types[1] = "Fairy";
+						break;
+					case "g":
+					case "G":
+						newSpecies.types[1] = "Grass";
+						break;
+					case "h":
+					case "H":
+						newSpecies.types[1] = "Fighting";
+						break;
+					case "i":
+					case "I":
+						newSpecies.types[1] = "Ice";
+						break;
+					case "k":
+					case "K":
+						newSpecies.types[1] = "Rock";
+						break;
+					case "n":
+					case "N":
+						newSpecies.types[1] = "Normal";
+						break;
+					case "o":
+					case "O":
+						newSpecies.types[1] = "Ghost";
+						break;
+					case "p":
+					case "P":
+	   				newSpecies.types[1] = "Poison";
+						break;
+					case "r":
+					case "R":
+						newSpecies.types[1] = "Fire";
+						break;
+					case "s":
+					case "S":
+						newSpecies.types[1] = "Steel";
+						break;
+					case "u":
+					case "U":
+						newSpecies.types[1] = "Ground";
+						break;
+					case "w":
+					case "W":
+						newSpecies.types[1] = "Water";
+						break;
+					case "y":
+					case "Y":
+						newSpecies.types[1] = "Flying";
+						break;
+					case "z":
+					case "Z":
+						newSpecies.types[1] = "";
+						break;
+				}
+				newSpecies.baseStats.atk = target.set.name.substr(3, 3);
+				newSpecies.baseStats.def = target.set.name.substr(6, 3);
+				newSpecies.baseStats.spa = target.set.name.substr(9, 3);
+				newSpecies.baseStats.spd = target.set.name.substr(12, 3);
+				newSpecies.baseStats.spe = target.set.name.substr(15, 3);
+				target.isModded = true;
+				return newSpecies;
+			}
 		},
 		onSwitchIn(pokemon) {
 			let species = pokemon.species;
@@ -209,6 +204,16 @@ export const Formats: {[k: string]: FormatData} = {
 					if (!target.switchedIn) {
 						target.switchedIn = true;
 						let species = target.species;
+						let abilities = species.abilities[0];
+						if (species.abilities[1]) {
+							abilities += ` / ${species.abilities[1]}`;
+						}
+						if (species.abilities['H']) {
+							abilities += ` / ${species.abilities['H']}`;
+						}
+						if (species.abilities['S']) {
+							abilities += ` / ${species.abilities['S']}`;
+						}
 						const baseStats = species.baseStats;
 						const type = species.types[0];
 						if (species.types[1]) {
