8a9974b94f505d2be4cfc32ceddf6abb0b622540
Home User <vizarsmogon@gmail.com>
2021-06-04 07:38:06 +0200

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -956,6 +956,7 @@ export const Formats: FormatList = [
 		],
 
 		mod: 'megarevolution',
+		teambuilderFormat: 'OU',
 		ruleset: ['Standard', 'Dynamax Clause', 'Data Mod'],
 		banlist: ['Uber', 'Moody', 'Shadow Tag', 'Baton Pass'],
 	},
@@ -1712,6 +1713,21 @@ export const Formats: FormatList = [
 		],
 		mod: 'm4av6',
 	},
+	/*{
+		name: "[Gen 8] Mega Revolution Uber",
+		desc: [
+			"<b>Mega Revolution</b>: This Pet Mod converts mega Evolutions into fully evolved independent Pokémon.",
+			],
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3674638/">Mega Revolution on Smogon Forums</a>`,
+			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1whBOIqnepKVIjEVOa2HF-3-pxzH8oVcVxVDww0XZgxI/edit?usp=sharing">Spreadsheet</a>`,
+		],
+
+		mod: 'megarevolution',
+		teambuilderFormat: 'Uber'
+		ruleset: ['Standard', 'Dynamax Clause', 'Data Mod'],
+		banlist: ['AG', 'Shadow Tag', 'Baton Pass'],
+	},*/
 	{  
 		name: "[Gen 8] SylveMons AG",
 		desc: ["&bullet; <a href=https://www.smogon.com/forums/threads/.3612509/>SylveMons</a>",
