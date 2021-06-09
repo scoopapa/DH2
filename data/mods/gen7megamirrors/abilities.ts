f05986eb5ff1ae08242611145e5333c2f40124f0
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-06-09 17:12:56 -0400

    Create abilities.ts

---
+++ data/mods/gen7megamirrors/abilities.ts
@@ -0,0 +1,28 @@
+export const Abilities: {[k: string]: ModdedAbilityData} = {
+	savagery: {
+		shortDesc: "Boosts the user's SpA by 1 after a KO.",
+		onSourceAfterFaint(length, target, source, effect) {
+			if (effect && effect.effectType === 'Move') {
+				this.boost({spa: length}, source);
+			}
+		},
+		name: "Savagery",
+		rating: 3,
+	},
+	pointedplummage: {
+		shortDesc: "The opponent loses 1/8 of their max HP if they hit this Pokemon with a contact move.",
+		onDamagingHitOrder: 1,
+		onDamagingHit(damage, target, source, move) {
+			if (move.flags['contact']) {
+				this.damage(source.baseMaxhp / 8, source, target);
+			}
+		},
+		name: "Pointed Plummage",
+		rating: 2.5,
+	},
+	rhoinflunence: {
+		shortDesc: "Placeholder.",
+		name: "Rho Influence",
+		rating: 5,
+	},
+};
