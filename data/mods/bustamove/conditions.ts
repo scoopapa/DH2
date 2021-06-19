bc7290c838750af5606d3bf74c0da9a5addab426
Home User <vizarsmogon@gmail.com>
2021-06-19 09:37:29 +0200

    dive

--- data/mods/bustamove/conditions.ts
+++ data/mods/bustamove/conditions.ts
@@ -24,43 +24,38 @@ export const Conditions: {[k: string]: ConditionData} = {
 			this.add('-end', pokemon, this.effectData.sourceEffect, '[jawlock]');
 		},
 	},
-	//not finished
-	/*dive: {
-		name: 'dive',
-				onTryMove(attacker, defender, move) {
-			if (attacker.removeVolatile(move.id)) {
+	twoturnmove: { // modified for Dive
+		// Skull Bash, SolarBeam, Sky Drop...
+		name: 'twoturnmove',
+		duration: 2,
+		onStart(target, source, effect) {
+			this.effectData.move = effect.id;
+			target.addVolatile(effect.id, source);
+			this.attrLastMove('[still]');
+		},
+		onEnd(target) {
+			target.removeVolatile(this.effectData.move);
+		},
+		onLockMove(pokemon) {
+			if (pokemon.volatile('dive')) return; // onLockMove traps the user
+			return this.effectData.move;
+		},
+		onDisableMove(pokemon) {
+			if (!pokemon.volatile('dive')) return; // equivalent to onLockMove if the user should not be trapped
+			if (!this.effectData.move || !pokemon.hasMove(this.effectData.move)) {
 				return;
 			}
-			if (attacker.hasAbility('gulpmissile') && attacker.species.name === 'Cramorant' && !attacker.transformed) {
-				const forme = attacker.hp <= attacker.maxhp / 2 ? 'cramorantgorging' : 'cramorantgulping';
-				attacker.formeChange(forme, move);
-			}
-			this.add('-prepare', attacker, move.name);
-			if (!this.runEvent('ChargeMove', attacker, defender, move)) {
-				return;
-			}
-			attacker.addVolatile('twoturnmove', defender );
-			return null;
-		},
-		condition: {
-			duration: 2,
-			onImmunity(type, pokemon) {
-				if (type === 'sandstorm' || type === 'hail') return false;
-			},
-			onInvulnerability(target, source, move) {
-				if (['surf', 'whirlpool'].includes(move.id)) {
-					return;
+			for (const moveSlot of pokemon.moveSlots) {
+				if (moveSlot.id !== this.effectData.move) {
+					pokemon.disableMove(moveSlot.id);
 				}
-				return false;
-			},
-			onSourceModifyDamage(damage, source, target, move) {
-				if (move.id === 'surf' || move.id === 'whirlpool') {
-					return this.chainModify(2);
-				}
-			},
+			}
 		},
-	},*/
-	corrosed: {
+		onMoveAborted(pokemon) {
+			pokemon.removeVolatile('twoturnmove');
+		},
+	},
+	/*corrosed: {
 		name: 'corrosed',
 		effectType: 'Status',
 		onStart(pokemon, source, sourceEffect) {
@@ -74,5 +69,5 @@ export const Conditions: {[k: string]: ConditionData} = {
 		onModifyType(type) {
 			if (type === 'Steel') onNegateImmunity: false,
 		},
-	},
+	},*/
 };
--- data/mods/bustamove/moves.ts
+++ data/mods/bustamove/moves.ts
@@ -87,15 +87,18 @@ export const Moves: {[moveid: string]: MoveData} = {
 		contestType: "Cute",
 	},
 	//not finished
-	/*dive: {
+	dive: {
 		num: 291,
 		accuracy: 100,
-		basePower: 90,
+		basePower: 80,
 		category: "Physical",
 		name: "Dive",
 		pp: 10,
 		priority: 0,
 		flags: {contact: 1, charge: 1, protect: 1, mirror: 1, nonsky: 1},
+		onPrepareHit(pokemon) {
+			source.volatileStatus: 'dive',
+		},
 		onTryMove(attacker, defender, move) {
 			if (attacker.removeVolatile(move.id)) {
 				return;
@@ -132,7 +135,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		target: "normal",
 		type: "Water",
 		contestType: "Beautiful",
-	},*/
+	},
 	eeriespell: {
 		num: 826,
 		accuracy: 100,
