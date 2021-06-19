343d97f79ad72b2281b5fa46d68c20e077669578
Home User <vizarsmogon@gmail.com>
2021-06-19 09:47:12 +0200

    Update conditions.ts

--- data/mods/bustamove/conditions.ts
+++ data/mods/bustamove/conditions.ts
@@ -41,7 +41,7 @@ export const Conditions: {[k: string]: ConditionData} = {
 			return this.effectData.move;
 		},
 		onDisableMove(pokemon) {
-			if (!pokemon.volatile('dive')) return; // equivalent to onLockMove if the user should not be trapped
+			if (!pokemon.hasVolatiles['dive']) return; // equivalent to onLockMove if the user should not be trapped
 			if (!this.effectData.move || !pokemon.hasMove(this.effectData.move)) {
 				return;
 			}
