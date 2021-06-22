0a03788d723bdb01a2d0dab7f4d9501568ac7bec
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-06-22 00:51:09 -0400

    Update abilities.ts

--- data/mods/missinglinks/abilities.ts
+++ data/mods/missinglinks/abilities.ts
@@ -7,4 +7,32 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
        name: "Oceanic Veil",
        rating: 3,
     },
+	bask: {
+      shortDesc: "This Pokemon is healed 1/4 by Fire, 1/8 by Sun; is hurt 1.25x by Water, 1/8 by Rain.",
+		onTryHit(target, source, move) {
+			if (target !== source && move.type === 'Fire') {
+				if (!this.heal(target.baseMaxhp / 4)) {
+					this.add('-immune', target, '[from] ability: Bask');
+				}
+				return null;
+			}
+		},
+		onFoeBasePowerPriority: 17,
+		onFoeBasePower(basePower, attacker, defender, move) {
+			if (this.effectData.target !== defender) return;
+			if (move.type === 'Water') {
+				return this.chainModify(1.25);
+			}
+		},
+		onWeather(target, source, effect) {
+			if (target.hasItem('utilityumbrella')) return;
+			if (effect.id === 'sunnyday' || effect.id === 'desolateland') {
+				this.heal(target.baseMaxhp / 8);
+			} else if (effect.id === 'raindance' || effect.id === 'primordialsea') {
+				this.damage(target.baseMaxhp / 8, target, target);
+			}
+		},
+		name: "Bask",
+		rating: 3,
+	},
 };
