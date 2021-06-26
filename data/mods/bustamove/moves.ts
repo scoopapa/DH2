0dcfbd3d1f0b2e6001f903637fbc3fbac4a37871
Home User <vizarsmogon@gmail.com>
2021-06-26 07:34:35 +0200

    Update moves.ts

--- data/mods/bustamove/moves.ts
+++ data/mods/bustamove/moves.ts
@@ -536,7 +536,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		priority: 0,
 		flags: {protect: 1, mirror: 1, sound: 1},
 		onBasePower(basePower, target, move, source) {
-			if (!target || target.fainted || !target.hasType(source.types));
+			if (!target || target.fainted || !target.hasType(source.type));
 			this.debug('Synchronoise damage boost');
 			return move.basePower * 2;
 		},
