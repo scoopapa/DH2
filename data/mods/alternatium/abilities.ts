5d12c95761d7aba80508a9d3be91280b55e3164c
Home User <vizarsmogon@gmail.com>
2021-07-03 11:07:07 +0200

    Update abilities.ts

--- data/mods/alternatium/abilities.ts
+++ data/mods/alternatium/abilities.ts
@@ -47,8 +47,12 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 	},*/
 	quickdraw: {
 		onModifyPriority(priority, source, move) {
-			if (pokemon.activeMoveActions < 2) return priority + 1;
-			else if (pokemon.activeMoveActions > 1) return priority + 0;
+			if (pokemon.activeMoveActions < 2) {
+				return priority + 1;
+			}
+			else if (pokemon.activeMoveActions > 1) {
+				return priority + 0;
+			}
 		},
 		onBasePower(basePower, source, move) {
 			if (source.activeMoveActions < 2) {
