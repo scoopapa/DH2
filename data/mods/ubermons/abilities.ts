3a1f620bce20cf08f6cb834756e593122616434b
Home User <vizarsmogon@gmail.com>
2021-07-05 16:09:49 +0200

    Update abilities.ts

--- data/mods/ubermons/abilities.ts
+++ data/mods/ubermons/abilities.ts
@@ -107,7 +107,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 	shadowtag: {
 		onSwitchOut(target) {
 			for (const target of pokemon.side.foe.active) {
-				target.heal(pokemon.baseMaxhp / 8);
+				target.damage(pokemon.baseMaxhp / 8);
 			}
 		},
 		name: "Shadow Tag",
