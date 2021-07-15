55e71a6c2bb649d9764e9464d50fec599ad0a775
Home User <vizarsmogon@gmail.com>
2021-07-15 23:10:48 +0200

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1848,6 +1848,21 @@ export const Formats: FormatList = [
 		banlist: ['AG', 'Moody', 'Shadow Tag', 'Baton Pass'],
 	},
 	{
+		name: "[Gen 8] Mega Revolution AG",
+		desc: [
+			"<b>Mega Revolution</b>: This Pet Mod converts mega Evolutions into fully evolved independent Pokémon.",
+			],
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3674638/">Mega Revolution on Smogon Forums</a>`,
+			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1whBOIqnepKVIjEVOa2HF-3-pxzH8oVcVxVDww0XZgxI/edit?usp=sharing">Spreadsheet</a>`,
+		],
+
+		mod: 'megarevolution',
+		teambuilderFormat: 'AG',
+		ruleset: ['Obtainable', 'Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause', 'Data Mod'],
+		banlist: [],
+	},
+	{
 		name: "[Gen 8] Stereotypes LC",
 		mod: "stereotypes",
 		searchShow: false,
