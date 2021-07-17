120089bb59549b29a170089769512c52f94ae8e8
scoopapa <pccompositor@aim.com>
2021-07-17 07:07:25 -0500

    Update scripts.ts

--- data/mods/j2k/scripts.ts
+++ data/mods/j2k/scripts.ts
@@ -5,6 +5,7 @@ const CHOOSABLE_TARGETS = new Set(['normal', 'any', 'adjacentAlly', 'adjacentAll
 export const Scripts: BattleScriptsData = {
 	init: function () {
 		for (var id in this.data.Pokedex) {
+			if (!this.modData('Learnsets', id)) continue;
 			if (this.data.Pokedex[id].num > 809) {
 				this.modData('Learnsets', id).learnset.toxic = ['8L1']
 				this.modData('Learnsets', id).learnset.hiddenpower = ['8L1']
