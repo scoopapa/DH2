c1744fff3587933c573e530bf3a026a457c3e3ab
Home User <vizarsmogon@gmail.com>
2021-07-22 12:00:40 +0200

    teambuilder fixes

--- data/mods/gen7dlcmons/formats-data.ts
+++ data/mods/gen7dlcmons/formats-data.ts
@@ -79,4 +79,188 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 		tier: "OU",
 		isModded: true,
 	},
+	gastrodon: {
+		tier: "OU",
+		isModded: true,
+	},
+	krookodile: {
+		tier: "OU",
+		isModded: true,
+	},
+	lucario: {
+		tier: "OU",
+		isModded: true,
+	},
+	volcanion: {
+		tier: "OU",
+		isModded: true,
+	},
+	zeraora: {
+		tier: "OU",
+		isModded: true,
+	},
+	porygonz: {
+		tier: "OU",
+		isModded: true,
+	},
+	bisharp: {
+		tier: "OU",
+		isModded: true,
+	},
+	necrozma: {
+		tier: "OU",
+		isModded: true,
+	},
+	shaymin: {
+		tier: "RU",
+		isModded: true,
+	},
+	absolmega: {
+		tier: "RUBL",
+		isModded: true,
+	},
+	aerodactylmega: {
+		tier: "OU",
+		isModded: true,
+	},
+	alakazammega: {
+		tier: "OU",
+		isModded: true,
+	},
+	garchompmega: {
+		tier: "(OU)",
+		isModded: true,
+	},
+	gyaradosmega: {
+		tier: "OU",
+		isModded: true,
+	},
+	heracrossmega: {
+		tier: "UUBL",
+		isModded: true,
+	},
+	houndoommega: {
+		tier: "RUBL",
+		isModded: true,
+	},
+	lopunnymega: {
+		tier: "OU",
+		isModded: true,
+	},
+	manectricmega: {
+		tier: "OU",
+		isModded: true,
+	},
+	mawilemega: {
+		tier: "OU",
+		isModded: true,
+	},
+	pinsirmega: {
+		tier: "OU",
+		isModded: true,
+	},
+	sableyemega: {
+		tier: "OU",
+		isModded: true,
+	},
+	scizormega: {
+		tier: "OU",
+		isModded: true,
+	},
+	sharpedomega: {
+		tier: "OU",
+		isModded: true,
+	},
+	slowbromega: {
+		tier: "OU",
+		isModded: true,
+	},
+	tyranitarmega: {
+		tier: "OU",
+		isModded: true,
+	},
+	ampharosmega: {
+		tier: "RU",
+		isModded: true,
+	},
+	glaliemega: {
+		tier: "NU",
+		isModded: true,
+	},
+	banettemega: {
+		tier: "RU",
+		isModded: true,
+	},
+	cameruptmega: {
+		tier: "NUBL",
+		isModded: true,
+	},
+	diglettalola: {
+		tier: "LC",
+		isModded: true,
+	},
+	dugtrioalola: {
+		tier: "PU",
+		isModded: true,
+	},
+	exeggutoralola: {
+		tier: "NU",
+		isModded: true,
+	},
+	geodudealola: {
+		tier: "LC",
+		isModded: true,
+	},
+	graveleralola: {
+		tier: "NFE",
+		isModded: true,
+	},
+	golemalola: {
+		tier: "(PU)",
+		isModded: true,
+	},
+	grimeralola: {
+		tier: "LC",
+		isModded: true,
+	},
+	marowakalola: {
+		tier: "RU",
+		isModded: true,
+	},
+	meowthalola: {
+		tier: "LC",
+		isModded: true,
+	},
+	ninetalesalola: {
+		tier: "UUBL",
+		isModded: true,
+	},
+	persianalola: {
+		tier: "PU",
+		isModded: true,
+	},
+	raichualola: {
+		tier: "PU",
+		isModded: true,
+	},
+	raticatealola: {
+		tier: "(PU)",
+		isModded: true,
+	},
+	rattataalola: {
+		tier: "LC",
+		isModded: true,
+	},
+	sandshrewalola: {
+		tier: "LC",
+		isModded: true,
+	},
+	sandslashalola: {
+		tier: "PU",
+		isModded: true,
+	},
+	vulpixalola: {
+		tier: "LC",
+		isModded: true,
+	},
 };
