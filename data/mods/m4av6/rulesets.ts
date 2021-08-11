1d2a5d21113d51bfd490fa53a6ff6818e749e9ae
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-08-11 17:29:32 -0600

    Update rulesets.ts

--- data/mods/m4av6/rulesets.ts
+++ data/mods/m4av6/rulesets.ts
@@ -23,7 +23,7 @@ export const Formats: {[k: string]: FormatData} = {
 			}
 		},
 		onChangeSet(set) {
-			if (set.species.baseSpecies === 'Silvally' && set.item === 'rksmegamemory') {
+			if (set.species.baseSpecies === 'Silvally' && this.toID(set.item) === 'rksmegamemory') {
 				set.species = 'Silvally-' + (set.hpType || 'Dark');
 			}
 		},
