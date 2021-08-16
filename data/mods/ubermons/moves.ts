a56815ddba421a06b89c4caa8e36fc1668a98a9e
Home User <vizarsmogon@gmail.com>
2021-08-16 09:09:18 +0200

    Update moves.ts

--- data/mods/ubermons/moves.ts
+++ data/mods/ubermons/moves.ts
@@ -237,8 +237,8 @@ export const Moves: {[moveid: string]: MoveData} = {
 		priority: 0,
 		flags: {protect: 1, mirror: 1},
 		onModifyMove(move, source, target) {
-			if (this.field.isTerrain('psychicterrain') && source.isGrounded() && type === 'Dark') {
-				ignoreImmunity: true,
+			if (this.field.isTerrain('psychicterrain') && source.isGrounded()) {
+				move.ignoreImmunity['Psychic'] = true;
 			}
 		},
 		secondary: null,
