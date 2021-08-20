8800a6336fe96fcad0006602e69e5dd896aa43de
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-08-20 13:41:20 -0400

    Update pokedex.ts

--- data/mods/pkmnybv2/pokedex.ts
+++ data/mods/pkmnybv2/pokedex.ts
@@ -1,7 +1,7 @@
   
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	blastoisemega: {
-    inherit: true,
+    	inherit: true,
 		baseStats: {hp: 79, atk: 103, def: 120, spa: 135, spd: 115, spe: 78},
 	},
 };
