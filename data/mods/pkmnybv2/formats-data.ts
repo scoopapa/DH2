3892d0beeeda0bb58d05d20eb5443b7c5953c832
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-08-22 04:56:14 -0400

    Update formats-data.ts

--- data/mods/pkmnybv2/formats-data.ts
+++ data/mods/pkmnybv2/formats-data.ts
@@ -59,4 +59,161 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		tier: "YB",
 		doublesTier: "DYB",
 	},
+	arbborry: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	mustellar: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	wolverflare: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	photyrant: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	ghoulgoyle: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	cortefauna: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	gammaroo: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	serpentorch: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	kaclash: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	solinira: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	zapish: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	sanatee: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	glaciarch: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	creaviary: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	plummuse: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	sparsqueak: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	darsqueak: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	giraflame: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	lycacia: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	remodile: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	petrasapien: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	petradvena: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	fyrecho: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	osprime: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	buffalocean: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	ingarde: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	venometta: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	nimbless: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	frixen: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	necrice: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	goblizz: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	ptarabola: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	dominidon: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	poufos: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	balatadi: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	ultratom: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	frostonna: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	geareon: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	mytheon: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+
 };
