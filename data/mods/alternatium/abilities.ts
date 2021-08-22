0e744eeb60a9ee4b6317a3862ebaf9bed8d10091
Home User <vizarsmogon@gmail.com>
2021-08-22 23:11:25 +0200

    Update abilities.ts

--- data/mods/alternatium/abilities.ts
+++ data/mods/alternatium/abilities.ts
@@ -387,7 +387,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		},
 		isUnbreakable: true,
 		name: "Shadow World",
-		shortDesc: "When this Pokemon is active, Ghost and Dark moves do 1.2x damage. Psychic and Fairy moves do 0.8x damage.",
+		shortDesc: "When this Ability is active, Ghost & Dark moves have 1.2x power. Psychic & Fairy have do 0.8x power.",
 		rating: 3,
 		num: 1010,
 	},
