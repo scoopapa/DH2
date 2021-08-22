e3b1d53d08817d860d4b9b8f4058bb822a0ad3e1
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-08-22 04:06:15 -0400

    Update formats-data.ts

--- data/mods/pkmnybv2/formats-data.ts
+++ data/mods/pkmnybv2/formats-data.ts
@@ -3,5 +3,60 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		tier: "YB",
 		doublesTier: "DYB",
 	},
-
+	chemicander: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	primadillo: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	bersawk: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	electzal: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	pyrogrine: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	chompean: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	mothicoal: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	neurowatt: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	menursa: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	bloomivolt: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	muaytiger: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	razorine: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	hareloom: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
+	frospherous: {
+		tier: "YB",
+		doublesTier: "DYB",
+	},
 };
