c54b6a5993591544796d0b794a678bf8e84399f9
ViZarSmogon <vizarsmogon@gmail.com>
2021-08-26 18:36:41 +0200

    Buffs for Resonake

--- data/mods/stereotypes/pokedex.ts
+++ data/mods/stereotypes/pokedex.ts
@@ -193,7 +193,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		num: 1017,
 		name: "Resonake",
 		types: ["Normal"],
-		baseStats: {hp: 84, atk: 70, def: 119, spa: 76, spd: 113, spe: 67},
+		baseStats: {hp: 84, atk: 70, def: 119, spa: 91, spd: 113, spe: 67},
 		abilities: {0: "Soundproof", 1: "Gluttony", H: "Sheer Force"},
 		weightkg: 7,
 	},
