8d66ff14a57aa0528f9bc33ca14ac4933877610d
ViZarSmogon <vizarsmogon@gmail.com>
2021-09-18 20:56:33 +0200

    Update scripts.ts

--- data/mods/smashstereotypes/scripts.ts
+++ data/mods/smashstereotypes/scripts.ts
@@ -1,23 +1,30 @@
 export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
-	init: function () {
-/*
-		for (const id in this.dataCache.Pokedex) {
-			const poke = this.dataCache.Pokedex[id];
-			if (poke.restrictedLearnset) {
-				console.log(this.toID(poke.name));
-				const thisPoke = this.toID(poke.name);
-				const learnset = this.dataCache.Learnsets[this.toID(poke.name)].learnset;
-				for (const move in learnset) {
-					console.log(thisPoke + " has " + move);
-					const moveid = this.dataCache.Moves[move];
-					if (moveid.isNonstandard) {
-						console.log(moveid.isNonstandard);
-						delete this.modData('Learnsets', thisPoke).learnset.moveid;
+	init: function () {	
+
+		for (const id in this.dataCache.Pokedex) {//check the dex for fusions
+			const fusionEntry = this.dataCache.Pokedex[id];
+			if (fusionEntry.fusion) {//if the pokedex entry has a fusion field, it's a fusion
+				const learnsetFusionList = [];//list of pokemon whose learnsets need to be fused
+				for (let name of fusionEntry.fusion) {
+					let prevo = true;
+					while (prevo) {//make sure prevos of both fused pokemon are added to the list
+						learnsetFusionList.push(name);
+						const dexEntry = this.dataCache.Pokedex[this.toID(name)];
+						if (dexEntry.prevo) name = dexEntry.prevo;
+						else prevo = false;
+					}
+				}
+				if (!this.dataCache.Learnsets[id]) this.dataCache.Learnsets[id] = { learnset: {}};//create a blank learnset entry so we don't need a learnsets file
+				for (let name of learnsetFusionList) {					
+					const learnset = this.dataCache.Learnsets[this.toID(name)].learnset;//get the learnset of each pokemon in the list
+					for (const moveid in learnset) {
+						if (this.dataCache.Moves[moveid].isNonstandard === 'Past') continue; //exclude dexited moves (I hope!) 
+						this.modData('Learnsets', id).learnset[moveid] = ['8L1'];//all moves are compatible with the fusion's only ability, so just set it to 8L1
 					}
 				}
 			}
 		}
-*/
+
 		this.modData('Learnsets', 'shaymin').learnset.allterrainblast = ['8L1'];
 		this.modData('Learnsets', 'shaymin').learnset.leafage = ['8L1'];
 		this.modData('Learnsets', 'shaymin').learnset.shedleaves = ['8L1'];
