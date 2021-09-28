0e3ca6391a28230cd2232fbf4d3ff89cf63a78e6
scoopapa <pccompositor@aim.com>
2021-09-27 20:43:31 -0500

    Update items.ts

--- data/mods/smashstereotypes/items.ts
+++ data/mods/smashstereotypes/items.ts
@@ -12,4 +12,17 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 		num: 657,
 		desc: "If held by a Kokovoir, this item allows it to Mega Evolve in battle.",
 	},
+	machampite: {
+		name: "Machampite",
+		spritenum: 9999,
+		megaStone: "Machamp-Mega",
+		megaEvolves: "Machamp",
+		itemUser: ["Machamp"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: 657,
+		desc: "If held by a Machamp, this item allows it to Mega Evolve in battle.",
+	},
 };
