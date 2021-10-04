0a429467b4bbbb5969465c0de7fc399f1e5c6a2d
ViZarSmogon <vizarsmogon@gmail.com>
2021-10-04 14:44:56 +0200

    Update abilities.ts

--- data/mods/alternatium/abilities.ts
+++ data/mods/alternatium/abilities.ts
@@ -479,14 +479,15 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		rating: 2,
 		num: 59,
 	},
-	/*liquidscales: {
+	liquidscales: {
 		name: "Liquid Scales",
 		shortDesc: "If targeted by a foe's move, this Pokemon restores 1/10 max HP.",
-		onResidual(target, source) {
-			if (target !== source) return;
-			this.heal(target.baseMaxhp / 10);
+		onDamagingHit(damage, target, source, move) {
+			if (move.category !== 'Status']) {
+				this.heal(target.baseMaxhp / 10);
+			}
 		},
 		rating: 3,
 		num: 1013,
-	},*/
+	},
 };
