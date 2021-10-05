44ed4b072c55aceda9234a4b437aba57a4dadf31
ViZarSmogon <vizarsmogon@gmail.com>
2021-10-05 15:48:11 +0200

    Update scripts.ts

--- data/mods/smashstereotypes/scripts.ts
+++ data/mods/smashstereotypes/scripts.ts
@@ -57,4 +57,101 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
         // only to specify the order of custom tiers
         customTiers: ['SSS', 'SSS Uber'],
 	},
+	
+	pokemon {
+		setSpecies(rawSpecies: Species, source: Effect | null = this.battle.effect, isTransform = false) {
+			const species = this.battle.runEvent('ModifySpecies', this, null, source, rawSpecies);
+			if (!species) return null;
+			this.species = species;
+
+			this.setType(species.types, true);
+			this.apparentType = rawSpecies.types.join('/');
+			this.addedType = species.addedType || '';
+			this.knownType = true;
+			this.weighthg = species.weighthg;
+
+			const stats = this.battle.spreadModify(this.species.baseStats, this.set);
+			if (this.species.maxHP) stats.hp = this.species.maxHP;
+
+			if (!this.maxhp) {
+				this.baseMaxhp = stats.hp;
+				this.maxhp = stats.hp;
+				this.hp = stats.hp;
+			}
+
+			if (!isTransform) this.baseStoredStats = stats;
+			let statName: StatNameExceptHP;
+			for (statName in this.storedStats) {
+				this.storedStats[statName] = stats[statName];
+				if (this.modifiedStats) this.modifiedStats[statName] = stats[statName]; // Gen 1: Reset modified stats.
+			}
+			if (this.battle.gen <= 1) {
+				// Gen 1: Re-Apply burn and para drops.
+				if (this.status === 'par') this.modifyStat!('spe', 0.25);
+				if (this.status === 'brn') this.modifyStat!('atk', 0.5);
+			}
+			this.speed = this.storedStats.spe;
+			return species;
+		}
+
+		/**
+		 * Changes this Pokemon's forme to match the given speciesId (or species).
+		 * This function handles all changes to stats, ability, type, species, etc.
+		 * as well as sending all relevant messages sent to the client.
+		 */
+		formeChange(
+			speciesId: string | Species, source: Effect = this.battle.effect,
+			isPermanent?: boolean, message?: string
+		) {
+			const rawSpecies = this.battle.dex.getSpecies(speciesId);
+
+			const species = this.setSpecies(rawSpecies, source);
+			if (!species) return false;
+
+			if (this.battle.gen <= 2) return true;
+
+			// The species the opponent sees
+			const apparentSpecies =
+				this.illusion ? this.illusion.species.name : species.baseSpecies;
+			if (isPermanent) {
+				this.baseSpecies = rawSpecies;
+				this.details = species.name + (this.level === 100 ? '' : ', L' + this.level) +
+					(this.gender === '' ? '' : ', ' + this.gender) + (this.set.shiny ? ', shiny' : '');
+				this.battle.add('detailschange', this, (this.illusion || this).details);
+				if (source.effectType === 'Item') {
+					if (source.zMove) {
+						this.battle.add('-burst', this, apparentSpecies, species.requiredItem);
+						this.moveThisTurnResult = true; // Ultra Burst counts as an action for Truant
+					} else if (source.onPrimal) {
+						if (this.illusion) {
+							this.ability = '';
+							this.battle.add('-primal', this.illusion);
+						} else {
+							this.battle.add('-primal', this);
+						}
+					} else {
+						this.battle.add('-mega', this, apparentSpecies, species.requiredItem);
+						this.moveThisTurnResult = true; // Mega Evolution counts as an action for Truant
+					}
+				} else if (source.effectType === 'Status') {
+					// Shaymin-Sky -> Shaymin
+					this.battle.add('-formechange', this, species.name, message);
+				}
+			} else {
+				if (source.effectType === 'Ability') {
+					this.battle.add('-formechange', this, species.name, message, `[from] ability: ${source.name}`);
+				} else {
+					this.battle.add('-formechange', this, this.illusion ? this.illusion.species.name : species.name, message);
+				}
+			}
+			if (isPermanent && !['disguise', 'iceface'].includes(source.id)) {
+				if (this.illusion) {
+					this.ability = ''; // Don't allow Illusion to wear off
+				}
+				this.setAbility(species.abilities['0'], null, true);
+				this.baseAbility = this.ability;
+			}
+			return true;
+		}
+	},
 };
