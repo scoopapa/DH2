0edd1212f33c6118e4f8912910c3134058c2a659
JosJet <89114731+JosJet@users.noreply.github.com>
2021-10-06 04:08:02 -0400

    Update pokedex.ts

--- data/mods/micrometamafia/pokedex.ts
+++ data/mods/micrometamafia/pokedex.ts
@@ -1,1 +1,18 @@
-
+export const Pokedex: {[speciesid: string]: SpeciesData} = {
+	gastrodon: {
+		num: 423,
+		name: "Gastrodon",
+		types: ["Water", "Ground"],
+		baseStats: {hp: 95, atk: 75, def: 75, spa: 75, spd: 95, spe: 74},
+		abilities: {0: "Damp", 1: "Poison Touch", H: "Water Absorb"},
+		weightkg: 29.9,
+	},
+	klefki: {
+		num: 707,
+		name: "Klefki",
+		types: ["Steel", "Fairy"],
+		baseStats: {hp: 85, atk: 90, def: 90, spa: 90, spd: 90, spe: 85},
+		abilities: {0: "Damp", 1: "Poison Touch", H: "Water Absorb"},
+		weightkg: 29.9,
+	},
+};
