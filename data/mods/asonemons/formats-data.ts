858ed83b5d607fe80f37b44499ab56f294f7e132
JosJet <89114731+JosJet@users.noreply.github.com>
2021-10-11 02:12:35 -0400

    Create formats-data.ts

---
+++ data/mods/asonemons/formats-data.ts
@@ -0,0 +1,122 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	yanmegashell: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+	pelipperink: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+	excadrillboulder: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+	vanilluxefur: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+	butterfreeangler: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+	dedenneluchador: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+	shucklebrick: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+	munchlaxforest: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    bunnelbyworker: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    sirfetchdfantasy: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    frogadierbeetle: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    chandelureballoon: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    infernapeclimber: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    ribombeecharmer: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    beheeyemufo: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    sableyedoom: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    mareaniemagma: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    thwackeydune: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    sphealsiren: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    pincurchingoo: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    nihilegopsychosis: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    stunfiskjagged: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    pawniardaviation: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    watchogscout: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    mothimgourd: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    clefairyselene: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    hoothootperch: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    swoobataroma: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    blacephalonborealis: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+    malamarparallel: {
+		tier: "As OUne",
+		doublesTier: "As OUne",
+	},	
+};
