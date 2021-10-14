7f2466bc7a9230d434792e85966f726c73130396
JosJet <89114731+JosJet@users.noreply.github.com>
2021-10-14 14:21:22 -0400

    Create pokedex.ts

---
+++ data/mods/asonemons/pokedex.ts
@@ -0,0 +1,10 @@
+export const Pokedex: {[speciesid: string]: SpeciesData} = {
+	yanmegashell: {
+		num: -1001,
+		name: "Yanmega-Shell",
+		types: ["Bug", "Fire"],
+		baseStats: {hp: 70, atk: 85, def: 145, spa: 105, spd: 90, spe: 60},
+		abilities: {0: "Drought"},
+		weightkg: 131.9,
+	},
+};
