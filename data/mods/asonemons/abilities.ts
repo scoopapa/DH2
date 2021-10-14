06899094a02c0013bc49f9662b25c5451f31affb
JosJet <89114731+JosJet@users.noreply.github.com>
2021-10-14 19:14:18 -0400

    Update abilities.ts

--- data/mods/asonemons/abilities.ts
+++ data/mods/asonemons/abilities.ts
@@ -1,2 +1,20 @@
 export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
+	asonetorkoal: {
+		onResidualOrder: 26,
+		onResidualSubOrder: 1,
+		onResidual(pokemon) {
+			if (pokemon.activeTurns) {
+				this.boost({spe: 1});
+			}
+		},
+		onStart(source) {
+			for (const action of this.queue) {
+				if (action.choice === 'runPrimal' && action.pokemon === source && source.species.id === 'groudon') return;
+				if (action.choice !== 'runSwitch' && action.choice !== 'runPrimal') break;
+			}
+			this.field.setWeather('sunnyday');
+		},
+		name: "As One (Torkoal)",
+		shortDesc: "The combination of Drought and Speed Boost.",
+	},
 };
