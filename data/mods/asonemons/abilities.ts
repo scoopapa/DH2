d56cd4b0c9aff8b28e1330048f02da601b34c171
JosJet <89114731+JosJet@users.noreply.github.com>
2021-10-15 01:16:15 -0400

    Update abilities.ts

--- data/mods/asonemons/abilities.ts
+++ data/mods/asonemons/abilities.ts
@@ -7,10 +7,12 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 		onResidualSubOrder: 1,
 		onResidual(pokemon) {
 			if (pokemon.activeTurns) {
+				this.add('-ability', pokemon, 'Speed Boost');
 				this.boost({spe: 1});
 			}
 		},
 		onStart(source) {
+			this.add('-ability', pokemon, 'Drought');
 			for (const action of this.queue) {
 				if (action.choice === 'runPrimal' && action.pokemon === source && source.species.id === 'groudon') return;
 				if (action.choice !== 'runSwitch' && action.choice !== 'runPrimal') break;
