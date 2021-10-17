4be4985d93ee4aacc2847f250f5ae9ddc9f35620
GraviTMonkey <69470101+GraviTMonkey@users.noreply.github.com>
2021-10-17 09:05:39 -0400

    Add files via upload

---
+++ data/mods/pokemonns/abilities.ts
@@ -0,0 +1,97 @@
+export const Abilities: {[k: string]: ModdedAbilityData} = {
+	brawler: {
+		name: "Brawler",
+		rating: 0,
+		num: 123001,
+	},
+	everlasting: {
+		name: "Everlasting",
+		rating: 0,
+		num: 123002,
+	},
+	floataway: {
+		name: "Float Away",
+		rating: 0,
+		num: 123003,
+	},
+	flytrap: {
+		name: "Flytrap",
+		rating: 0,
+		num: 123004,
+	},
+	highflight: {
+		name: "High Flight",
+		rating: 0,
+		num: 123005,
+	},
+	immovable: {
+		name: "Immovable",
+		rating: 0,
+		num: 123006,
+	},
+	inksplatter: {
+		name: "Ink Splatter",
+		rating: 0,
+		num: 123007,
+	},
+	juggernaut: {
+		name: "Juggernaut",
+		rating: 0,
+		num: 123008,
+	},
+	lowflight: {
+		name: "Low Flight",
+		rating: 0,
+		num: 123009,
+	},
+	partyanimal: {
+		name: "Party Animal",
+		rating: 0,
+		num: 123010,
+	},
+	quickfix: {
+		name: "Quick Fix",
+		rating: 0,
+		num: 123011,
+	},
+	sonarsensor: {
+		name: "Sonar Sensor",
+		rating: 0,
+		num: 123012,
+	},
+	spinaround: {
+		name: "Spin Around",
+		rating: 0,
+		num: 123013,
+	},
+	stampede: {
+		name: "Stampede",
+		rating: 0,
+		num: 123014,
+	},
+	stubborn: {
+		name: "Stubborn",
+		rating: 0,
+		num: 123015,
+	},
+	superglued: {
+		name: "Super Glued",
+		rating: 0,
+		num: 123016,
+	},
+	vibrato: {
+		name: "Vibrato",
+		rating: 0,
+		num: 123017,
+	},
+	weakspotter: {
+		name: "Weak Spotter",
+		rating: 0,
+		num: 123018,
+	},
+	winterpower: {
+		name: "Winter Power",
+		rating: 0,
+		num: 123019,
+	},
+};
