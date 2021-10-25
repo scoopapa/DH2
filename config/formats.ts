7f447fc32563897dd7970d45c1337679b209fc04
scoopapa <pccompositor@aim.com>
2021-10-25 10:59:14 -0500

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1053,7 +1053,7 @@ export const Formats: FormatList = [
 		      ],
 		ruleset: ['Standard NatDex', 'Standard M4A', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Freeze Clause Mod', 'Mega Data Mod'],
 		mod: 'm4av6',
-		teambuilderFormat: 'OU',
+		// teambuilderFormat: 'OU',
 	},
 	{
 		name: "[Gen 8] Megamax",
