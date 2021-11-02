e3423385f4e4e79d995afa5ea1934686a7e282c8
ViZarSmogon <vizarsmogon@gmail.com>
2021-11-02 21:48:47 +0100

    Update scripts.ts

--- data/mods/j2k/scripts.ts
+++ data/mods/j2k/scripts.ts
@@ -6,6 +6,7 @@ export const Scripts: BattleScriptsData = {
 	init: function () {
 		for (var id in this.data.Pokedex) {
 			if (!this.modData('Learnsets', id)) continue;
+			const learnset = modDex.data.Learnsets[id].learnset;
 			if (this.data.Pokedex[id].num > 809) {
 				this.modData('Learnsets', id).learnset.toxic = ['8L1']
 				this.modData('Learnsets', id).learnset.hiddenpower = ['8L1']
