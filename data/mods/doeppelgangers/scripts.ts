bb88c2133694c96e3c102179599dae1cd49c2cbd
ViZarSmogon <vizarsmogon@gmail.com>
2021-11-02 21:53:03 +0100

    Delete scripts.ts

--- data/mods/doeppelgangers/scripts.ts
+++
@@ -1,5 +0,0 @@
-export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
-	teambuilderConfig: {
-		customTiers: ["AU", "OU", "UUBL", "UU", "RUBL", "RU", "NUBL", "NU", "PUBL", "PU", "(PU)", "NFE", "LC"],
-	},
-};
