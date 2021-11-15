cb6ba91a3001dac43648149025af4989238436d9
JosJet <89114731+JosJet@users.noreply.github.com>
2021-11-15 00:29:04 -0500

    Update moves.ts

--- data/mods/asonemons/moves.ts
+++ data/mods/asonemons/moves.ts
@@ -24,12 +24,12 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		basePower: 60,
 		category: "Special",
 		name: "Ink Gulp",
-		shortDesc: "User recovers 50% of the damage dealt. Raises user's Defense by 3 if this KOes the target.",
+		shortDesc: "User recovers 50% of the damage dealt. Raises user's Special Attack by 1 if this KOes the target.",
 		pp: 10,
 		priority: 0,
 		flags: {protect: 1, mirror: 1, heal: 1},
 		onAfterMoveSecondarySelf(pokemon, target, move) {
-			if (!target || target.fainted || target.hp <= 0) this.boost({def: 3}, pokemon, pokemon, move);
+			if (!target || target.fainted || target.hp <= 0) this.boost({spa: 1}, pokemon, pokemon, move);
 		},
       drain: [3, 4],
 		target: "normal",
