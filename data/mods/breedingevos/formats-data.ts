12e5a2c68638dc6cd3080a21c34044f6fea7e41e
charizard8888 <omkumr082001@outlook.com>
2021-11-16 14:38:34 +0530

    Create formats-data.ts

---
+++ data/mods/breedingevos/formats-data.ts
@@ -0,0 +1,63 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	barboachkanto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	whisfloor: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	sunflorakanto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	floraflare: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	applinkanto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	badpple: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	fujisei: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	snomkanto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	frosmothkanto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	behemoth: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	dreepykanto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	drakloakkanto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	drakdart: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	qwilfishkanto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	bakufugu: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+
+};
