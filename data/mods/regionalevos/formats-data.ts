bf2178085721af35e11908976e3fdd191425f048
charizard8888 <omkumr082001@outlook.com>
2021-11-17 12:07:00 +0530

    Update formats-data.ts

--- data/mods/regionalevos/formats-data.ts
+++ data/mods/regionalevos/formats-data.ts
@@ -59,5 +59,85 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 		tier: "OU",
 		doublesTier: "DOU",
 	},
+	impidimpjohto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	morgremjohto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	grimmdigo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	trapinchjohto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	vibravajohto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	flaygon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	bagonjohto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	shelgonjohto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	shelldra: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	combeejohto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	dronarchmale: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	dronarchfelame: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	makuhitajohto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	fojiyama: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	seviperjohto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	shinobite: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	zangoosejohto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	shogoose: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	sinisteajohto: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	longcha: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
 
 };
