a627988c4d8bd7cafa140f479d50ee4856608115
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-12-08 09:34:24 -0500

    Create abilities.ts

---
+++ data/mods/joltemons/abilities.ts
@@ -0,0 +1,55 @@
+export const Abilities: {[k: string]: ModdedAbilityData} = {
+	lightpower: {
+		onModifySpAPriority: 5,
+		onModifySpA(spa) {
+			return this.chainModify(2);
+		},
+		name: "Light Power",
+    shortDesc: "This Pokemon's Special Attack is doubled.",
+	},
+	raindish: {
+		onResidualOrder: 5,
+		onResidualSubOrder: 2,
+		onResidual(pokemon) {
+			if (this.field.isTerrain('grassyterrain')) return;
+			this.heal(pokemon.maxhp / 16);
+		},
+		onTerrain(pokemon) {
+			if (!this.field.isTerrain('grassyterrain')) return;
+			this.heal(pokemon.maxhp / 16);
+		},
+		onWeather(target, source, effect) {
+			if (target.hasItem('utilityumbrella')) return;
+			if (effect.id === 'raindance' || effect.id === 'primordialsea') {
+				this.heal(target.baseMaxhp / 8);
+			}
+		},
+		name: "Rain Dish",
+    shortDesc: "Heals 6.25% of user's max HP at the end of each turn. Heals 12.5% in Rain",
+		num: 44,
+	},
+	icebody: {
+		onResidualOrder: 5,
+		onResidualSubOrder: 2,
+		onResidual(pokemon) {
+			if (this.field.isTerrain('grassyterrain')) return;
+			this.heal(pokemon.maxhp / 16);
+		},
+		onTerrain(pokemon) {
+			if (!this.field.isTerrain('grassyterrain')) return;
+			this.heal(pokemon.maxhp / 16);
+		},
+		onWeather(target, source, effect) {
+			if (effect.id === 'hail') {
+				this.heal(target.baseMaxhp / 8);
+			}
+		},
+		onTerrain(pokemon) {
+			if (!this.field.isTerrain('grassyterrain')) return;
+			this.heal(pokemon.maxhp / 16);
+		},
+		name: "Ice Body",
+    shortDesc: "Heals 6.25% of user's max HP at the end of each turn. Heals 12.5% in Rain",
+		num: 115,
+	},
+};
