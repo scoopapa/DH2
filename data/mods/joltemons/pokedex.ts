0c9e4d3a82bf2755fddbca9bc46ebbef64170161
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-12-08 08:58:48 -0500

    Create pokedex.ts

---
+++ data/mods/joltemons/pokedex.ts
@@ -0,0 +1,18 @@
+export const Pokedex: {[speciesid: string]: SpeciesData} = {
+	moltresgalar: {
+		inherit: true,
+		abilities: {0: "Berserk", H: "Regenerator"},
+	},
+	zapdosgalar: {
+		inherit: true,
+		abilities: {0: "Defiant", H: "Intimidate"},
+	},
+	articunogalar: {
+		inherit: true,
+		abilities: {0: "Competitive", H: "Magic Bounce"},
+	},
+	magmortar: {
+		inherit: true,
+		abilities: {0: "Flame Body", H: "Neutralizing Gas"},
+	},
+};
