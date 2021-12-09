8697deceea733b3c3260236543f4129fa6a76965
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-12-08 20:47:45 -0500

    Update abilities.ts

--- data/mods/joltemons/abilities.ts
+++ data/mods/joltemons/abilities.ts
@@ -12,15 +12,15 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		onResidualSubOrder: 2,
 		onResidual(pokemon) {
 			if (this.field.isWeather(['raindance', 'primordialsea'])) return;
-			this.heal(pokemon.maxhp / 8);
+			this.heal(pokemon.maxhp / 16);
 		},
 		onTerrain(pokemon) {
 			if (!this.field.isTerrain('grassyterrain')) return;
 			this.heal(pokemon.maxhp / 16);
 		},
 		onWeather(target, source, effect) {
-			if (!target.hasItem('utilityumbrella')) return;
-			if (!effect.id === 'raindance' || !effect.id === 'primordialsea') {
+			if (target.hasItem('utilityumbrella')) return;
+			if (effect.id === 'raindance' || effect.id === 'primordialsea') {
 				this.heal(target.baseMaxhp / 16);
 			}
 		},
@@ -33,14 +33,14 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		onResidualSubOrder: 2,
 		onResidual(pokemon) {
 			if (this.field.isWeather('hail')) return;
-			this.heal(pokemon.maxhp / 8);
+			this.heal(pokemon.maxhp / 16);
 		},
 		onTerrain(pokemon) {
 			if (!this.field.isTerrain('grassyterrain')) return;
 			this.heal(pokemon.maxhp / 16);
 		},
 		onWeather(target, source, effect) {
-			if (!effect.id === 'hail') {
+			if (effect.id === 'hail') {
 				this.heal(target.baseMaxhp / 16);
 			}
 		},
