29f0585b11ec916b9a379240e517135f74e8785c
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-12-08 23:03:42 -0500

    Update pokedex.ts

--- data/mods/joltemons/pokedex.ts
+++ data/mods/joltemons/pokedex.ts
@@ -15,4 +15,88 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		inherit: true,
 		abilities: {0: "Flame Body", H: "Neutralizing Gas"},
 	},
+	sandslashalola: {
+		inherit: true,
+		abilities: {0: "Snow Cloak", 1: "Ice Body", H: "Slush Rush"},
+	},
+	ninetalesalola: {
+		inherit: true,
+		abilities: {0: "Snow Cloak", 1: "Ice Body", H: "Snow Warning"},
+	},
+	volbeat: {
+		inherit: true,
+		abilities: {0: "Light Power", 1: "Swarm", H: "Prankster"},
+	},
+	solrock: {
+		inherit: true,
+		abilities: {0: "Levitate", H: "Light Power"},
+	},
+	pachirisu: {
+		inherit: true,
+		abilities: {0: "Light Power", 1: "Pickup", H: "Volt Absorb"},
+	},
+	lumineon: {
+		inherit: true,
+		abilities: {0: "Swift Swim", 1: "Storm Drain", H: "Light Power"},
+	},
+	froslass: {
+		inherit: true,
+		abilities: {0: "Snow Cloak", 1: "Ice Body", H: "Cursed Body"},
+	},
+	watchog: {
+		inherit: true,
+		abilities: {0: "Light Power", 1: "Keen Eye", H: "Analytic"},
+	},
+	simipour: {
+		inherit: true,
+		abilities: {0: "Gluttony", 1: "Rain Dish", H: "Torrent"},
+	},
+	gigalith: {
+		inherit: true,
+		abilities: {0: "Sturdy", 1: "Sand Stream", H: "Light Power"},
+	},
+	jellicent: {
+		inherit: true,
+		abilities: {0: "Water Absorb", 1: "Cursed Body", H: "Rain Dish"},
+	},
+	cryogonal: {
+		inherit: true,
+		abilities: {0: "Levitate", H: "Ice Body"},
+	},
+	gourgeist: {
+		inherit: true,
+		abilities: {0: "Pickup", 1: "Frisk", H: "Light Power"},
+	},
+	gourgeistsmall: {
+		inherit: true,
+		abilities: {0: "Pickup", 1: "Frisk", H: "Light Power"},
+	},
+	gourgeistlarge: {
+		inherit: true,
+		abilities: {0: "Pickup", 1: "Frisk", H: "Light Power"},
+	},
+	gourgeistsuper: {
+		inherit: true,
+		abilities: {0: "Pickup", 1: "Frisk", H: "Light Power"},
+	},
+	crabominable: {
+		inherit: true,
+		abilities: {0: "Hyper Cutter", 1: "Iron Fist", H: "Ice Body"},
+	},
+	grapploct: {
+		inherit: true,
+		abilities: {0: "Limber", 1: "Rain Dish", H: "Technician"},
+	},
+	polteageist: {
+		inherit: true,
+		abilities: {0: "Weak Armor", 1: "Rain Dish", H: "Cursed Body"},
+	},
+	polteageistantique: {
+		inherit: true,
+		abilities: {0: "Weak Armor", 1: "Rain Dish", H: "Cursed Body"},
+	},
+	glastrier: {
+		inherit: true,
+		abilities: {0: "Chilling Neigh", H: "Ice Body"},
+	},
 };
