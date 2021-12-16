ae6eefee5e2c178866090ac62efb9fb661a31d8b
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-12-15 21:17:30 -0500

    Update abilities.ts

--- data/mods/joltemons/abilities.ts
+++ data/mods/joltemons/abilities.ts
@@ -48,4 +48,140 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
     shortDesc: "Heals 6.25% of user's max HP at the end of each turn. Heals 12.5% in Hail.",
 		num: 115,
 	},
+	honeygather: {
+      shortDesc: "This Pokemon heals 1/8 of its max HP if it's holding Honey.",
+		onResidualOrder: 26,
+		onResidualSubOrder: 1,
+		onResidual(pokemon) {
+			if (pokemon.hasItem('honey')) {
+					this.heal(pokemon.baseMaxhp / 8);
+			}
+		},
+		name: "Honey Gather",
+		rating: 2,
+		num: 118,
+	},
+	sweetveil: {
+		name: "Sweet Veil",
+      shortDesc: "This Pokemon and its allies can't fall asleep. This Pokemon heals 1/8 of its max HP if it's holding Honey.",
+		onAllySetStatus(status, target, source, effect) {
+			if (status.id === 'slp') {
+				this.debug('Sweet Veil interrupts sleep');
+				const effectHolder = this.effectData.target;
+				this.add('-block', target, 'ability: Sweet Veil', '[of] ' + effectHolder);
+				return null;
+			}
+		},
+		onAllyTryAddVolatile(status, target) {
+			if (status.id === 'yawn') {
+				this.debug('Sweet Veil blocking yawn');
+				const effectHolder = this.effectData.target;
+				this.add('-block', target, 'ability: Sweet Veil', '[of] ' + effectHolder);
+				return null;
+			}
+		},
+		onResidualOrder: 26,
+		onResidualSubOrder: 1,
+		onResidual(pokemon) {
+			if (pokemon.hasItem('honey')) {
+					this.heal(pokemon.baseMaxhp / 8);
+			}
+		},
+		rating: 2,
+		num: 175,
+	},
+	libero: {
+      shortDesc: "Non-STAB moves have 1.2x power.",
+		onBasePowerPriority: 23,
+		onBasePower(basePower, attacker, defender, move) {
+			if (!move.hasType(attacker.getTypes())) {
+				this.debug('Libero boost');
+				return this.chainModify([0x1333, 0x1000]);
+			}
+		},
+		name: "Libero",
+		rating: 4.5,
+		num: 236,
+	},
+	libero: {
+      shortDesc: "Non-STAB moves have 1.2x power.",
+		onBasePowerPriority: 23,
+		onBasePower(basePower, attacker, defender, move) {
+			if (!move.hasType(attacker.getTypes())) {
+				this.debug('Libero boost');
+				return this.chainModify([0x1333, 0x1000]);
+			}
+		},
+		name: "Libero",
+		rating: 4.5,
+		num: 236,
+	},
+	moody: {
+      shortDesc: "This Pokemon's lowest stat goes up by 1 every turn.",
+		onResidualOrder: 26,
+		onResidualSubOrder: 1,
+		onResidual(length, target, source, effect) {
+			if (effect && effect.effectType === 'Move') {
+				let statName = 'atk';
+				let bestStat = 0;
+				let s: StatNameExceptHP;
+				for (s in source.storedStats) {
+					if (source.storedStats[s] < bestStat) {
+						statName = s;
+						bestStat = source.storedStats[s];
+					}
+				}
+				this.boost({[statName]: length}, source);
+			}
+		},
+		name: "Moody",
+		rating: 3,
+		num: 141,
+	},
+	stickyhold: {
+		onTakeItem(item, pokemon, source) {
+			if (this.suppressingAttackEvents(pokemon) || !pokemon.hp || pokemon.item === 'stickybarb') return;
+			if (!this.activeMove) throw new Error("Battle.activeMove is null");
+			if ((source && source !== pokemon) || this.activeMove.id === 'knockoff') {
+				this.add('-activate', pokemon, 'ability: Sticky Hold');
+				return false;
+			}
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.name === 'Knock Off') {
+				this.debug('Sticky Hold weaken');
+				return this.chainModify(0.67);
+			}
+		},
+		onTryHit(pokemon, target, move) {
+			if (move.name === 'Poltergeist') {
+				this.add('-immune', pokemon, '[from] ability: Sticky Hold');
+				return null;
+			}
+		},
+		name: "Sticky Hold",
+		rating: 2,
+		num: 60,
+	},
+	watercompaction: {
+		shortDesc: "This Pokemon's Defense goes up 2 stages when hit by a Water-type move; Water immunity",
+		onTryHitPriority: 1,
+		onTryHit(target, source, move) {
+			if (target !== source && move.type === 'Water') {
+				if (!this.boost({def: 2})) {
+					this.add('-immune', target, '[from] ability: Water Compaction');
+				}
+				return null;
+			}
+		},
+		onAllyTryHitSide(target, source, move) {
+			if (target === this.effectData.target || target.side !== source.side) return;
+			if (move.type === 'Water') {
+				this.boost({def: 2}, this.effectData.target);
+			}
+		},
+		name: "Water Compaction",
+		rating: 3,
+		num: 195,
+	},
 };
