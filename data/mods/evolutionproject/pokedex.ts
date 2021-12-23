a1ced0f27d641f3342d9077152acd2a2534b1acb
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-12-22 18:01:14 -0700

    Update pokedex.ts

--- data/mods/evolutionproject/pokedex.ts
+++ data/mods/evolutionproject/pokedex.ts
@@ -1,2 +1,24 @@
 export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
+// sample entry... mostly to remind me of the functionality I need P:
+	raticate: {
+		inherit: true,
+		evos: ["Plaguicate"], // Eviolite-compatible
+	},
+	plaguicate: {
+		name: "Plaguicate",
+		copyData: "Raticate", // inherits information from Raticate wherever it's missing data
+
+		types: ["Normal", "Poison"],
+		baseStats: {hp: 75, atk: 91, def: 80, spa: 65, spd: 90, spe: 97},
+		abilities: {0: "Poison Touch", 1: "Guts", H: "Hustle"},
+
+		// copyMoves: "Raticate", // not always the same as prevo, but it should copy that by default so I don't need to enumerate this every time
+		movepoolAdditions: ["plaquefang", "poisonfang", "venoshock"],
+		// movepoolDeletions: ["struggle"], // an optional separate line for split evolutions
+		// (example: Tenoris, which inherits moves from Altaria mostly but removes some that Swablu doesn't get anyway)
+
+		prevo: "Raticate",
+		evoType: "other",
+		evoCondition: "A newly-discovered evolution", // generic flavor where not specified
+	},
 };
