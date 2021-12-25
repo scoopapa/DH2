bf32f6f317a16b6f73aff4df7753c8916ea62889
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-12-24 18:47:54 -0700

    Update scripts.ts

--- data/mods/evolutionproject/scripts.ts
+++ data/mods/evolutionproject/scripts.ts
@@ -6,23 +6,22 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 	},
 	init() {
 		for (const id in this.dataCache.Pokedex) {
-			const pokemon = this.dataCache.Pokedex[id];
-			if (!pokemon.copyData) continue;
-			let newData = this.dex.getSpecies(pokemon);
-			let copyData = this.dex.getSpecies(pokemon.copyData);
+			const newMon = this.dataCache.Pokedex[id];
+			if (!newMon || !newMon.copyData) continue; // weeding out Pokémon that aren't new
+			let copyData = this.dataCache.Pokedex[this.toID(newMon.copyData)];
 
-			if (!newData.types && copyData.types) newData.types = copyData.types;
-			if (!newData.baseStats && copyData.baseStats) newData.baseStats = copyData.baseStats;
-			if (!newData.abilities && copyData.abilities) newData.abilities = copyData.abilities;
-			if (!newData.num && copyData.num) newData.num = copyData.num *= -1; // inverting the original's dex number
-			if (!newData.genderRatio && copyData.genderRatio) newData.genderRatio = copyData.genderRatio;
-			if (!newData.heightm && copyData.heightm) newData.heightm = copyData.heightm;
-			if (!newData.weightkg && copyData.weightkg) newData.weightkg = copyData.weightkg;
-			if (!newData.color && copyData.color) newData.color = copyData.color;
-			if (!newData.eggGroups && copyData.eggGroups) newData.eggGroups = copyData.eggGroups;
+			if (!newMon.types && copyData.types) newMon.types = copyData.types;
+			if (!newMon.baseStats && copyData.baseStats) newMon.baseStats = copyData.baseStats;
+			if (!newMon.abilities && copyData.abilities) newMon.abilities = copyData.abilities;
+			if (!newMon.num && copyData.num) newMon.num = copyData.num *= -1; // inverting the original's dex number
+			if (!newMon.genderRatio && copyData.genderRatio) newMon.genderRatio = copyData.genderRatio;
+			if (!newMon.heightm && copyData.heightm) newMon.heightm = copyData.heightm;
+			if (!newMon.weightkg && copyData.weightkg) newMon.weightkg = copyData.weightkg;
+			if (!newMon.color && copyData.color) newMon.color = copyData.color;
+			if (!newMon.eggGroups && copyData.eggGroups) newMon.eggGroups = copyData.eggGroups;
 
-			let copyMoves = pokemon.copyData;
-			if (pokemon.copyMoves) copyMoves = pokemon.copyMoves;
+			let copyMoves = newMon.copyData;
+			if (newMon.copyMoves) copyMoves = newMon.copyMoves;
 			if (!this.dataCache.Learnsets[id]) this.dataCache.Learnsets[id] = { learnset: {}}; // create a blank learnset entry so we don't need a learnsets file (thank you ink)
 			for (let name of copyMoves) {
 				const learnset = this.dataCache.Learnsets[this.toID(name)].learnset; // get the learnset of each pokemon in the list
@@ -30,13 +29,13 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 					this.modData('Learnsets', id).learnset[moveid] = ['8M'];
 				}
 			}
-			if (pokemon.movepoolAdditions) {
-				for (const move of pokemon.movepoolAdditions) {
+			if (newMon.movepoolAdditions) {
+				for (const move of newMon.movepoolAdditions) {
 					this.modData('Learnsets', this.toID(id)).learnset[this.toID(move)] = ["8M"];
 				}
 			}
-			if (pokemon.movepoolDeletions) {
-				for (const move of pokemon.movepoolDeletions) {
+			if (newMon.movepoolDeletions) {
+				for (const move of newMon.movepoolDeletions) {
 					delete this.modData('Learnsets', this.toID(id)).learnset[this.toID(move)];
 				}
 			}
