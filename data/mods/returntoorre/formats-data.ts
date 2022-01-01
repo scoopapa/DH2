ab33fb9e9adca54eafce9a06ebaac3242a75660a
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2022-01-01 01:35:59 -0500

    Create formats-data.ts

---
+++ data/mods/returntoorre/formats-data.ts
@@ -0,0 +1,728 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	//Custom tiers are "RTO" and "RTO NFE"
+		absol: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	absolmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	aggron: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	aggronmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	altaria: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	altariamega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	ambipom: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	ampharos: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	ampharosmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	arbok: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	arcanine: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	ariados: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	articuno: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	articunogalar: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	banette: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	banettemega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	beedrill: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	beedrillmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	blissey: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	breloom: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	butterfree: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	camerupt: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	cameruptmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	castform: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	claydol: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	cloyster: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	delcatty: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	delibird: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	dewgong: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	dodrio: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	donphan: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	dragonite: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	dugtrio: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	dunsparce: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	dusknoir: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	electivire: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	electrode: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	entei: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	espeon: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	exeggutor: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	exeggutoralola: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	farfetchd: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	fearow: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	feraligatr: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	flareon: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	flygon: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	forretress: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	froslass: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	furret: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	gallade: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	gardevoir: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	gardevoirmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	glaceon: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	glalie: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	glaliemega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	gliscor: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	golbat: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	golduck: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	grumpig: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	hariyama: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	heracross: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	hitmonchan: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	hitmonlee: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	hitmontop: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	hooh: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	honchkrow: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	houndoom: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	houndoommega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	hypno: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	jolteon: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	jumpluff: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	kangaskhan: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	kangaskhanmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	kecleon: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	lapras: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	leafeon: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	ledian: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	lickilicky: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	lugia: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	lunatone: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	magcargo: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	magmortar: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	magnezone: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	mamoswine: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	manectric: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	manectricmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	mantine: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	marowak: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	marowakalola: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	masquerain: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	mawile: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	mawilemega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	medicham: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	medichammega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	meganium: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	metagross: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	metagrossmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	mightyena: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	miltank: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	minun: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	mismagius: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	moltres: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	moltresgalar: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	mrmime: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	muk: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	mukalola: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	ninetales: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	ninetalesalola: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	noctowl: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	octillery: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	parasect: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	perrserker: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	persian: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	persianalola: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	pidgeot: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	pidgeotmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	pinsir: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	pinsirmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	plusle: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	politoed: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	poliwrath: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	primeape: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	probopass: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	quagsire: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	qwilfish: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	raikou: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	rapidash: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	rapidashgalar: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	raticate: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	raticatealola: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	rhyperior: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	roserade: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	sableye: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	sableyemega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	salamence: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	salamencemega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	sandslash: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	sandslashalola: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	scizor: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	scizormega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	scyther: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	sharpedo: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	sharpedomega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	shiftry: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	shuckle: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	sirfetchd: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	skarmory: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	smeargle: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	snorlax: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	solrock: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	stantler: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	starmie: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	sudowoodo: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	suicune: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	sunflora: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	swalot: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	swellow: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	sylveon: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	tangrowth: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	tauros: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	togekiss: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	tropius: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	typhlosion: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	tyranitar: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	tyranitarmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	umbreon: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	ursaring: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	vaporeon: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	venomoth: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	victreebel: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	wailord: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	walrein: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	weavile: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	whiscash: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	wobbuffet: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	xatu: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	yanmega: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	zangoose: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	zapdos: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+	zapdosgalar: {
+		tier: "RTO",
+		doublesTier: "DOU",
+	},
+
+
+
+
+	
+};
