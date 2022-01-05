ee6cfbb145e28f5c481091e9e4c96ef3f4a4d084
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-01-05 08:12:40 -0700

    Evolution Project first half
    my computer shuts down without warning sometimes so I am just paranoid and want to save my progress at the halfway point

--- data/mods/evolutionproject/pokedex.ts
+++ data/mods/evolutionproject/pokedex.ts
@@ -1,4 +1,7 @@
 export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
+
+// SLATE 1
+
 // sample entry... mostly to remind me of the functionality I need P:
 	raticate: {
 		inherit: true,
@@ -18,14 +21,14 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		// (example: Tenoris, which inherits moves from Altaria mostly but removes some that Swablu doesn't get anyway)
 
 		prevo: "Raticate",
-		evoType: "other",
-		evoCondition: "A newly-discovered evolution", // generic flavor where not specified
+		evoLevel: 36,
+		evoCondition: "Level up while poisoned",
 		creator: "ausma",
 	},
 
 	noctowl: {
 		inherit: true,
-		evos: ["hoobarn"],
+		evos: ["Hoobarn"],
 	},
 	hoobarn: {
 		name: "Hoobarn",
@@ -36,7 +39,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 
 		prevo: "Noctowl",
 		evoType: "other",
-		evoCondition: "A newly-discovered evolution",
+		evoCondition: "A newly-discovered evolution", // use generic flavor where not specified
 		creator: "Bolivia",
 	},
 
@@ -117,12 +120,17 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 
 	mienfoo: {
 		inherit: true,
-		evos: ["Mienshao", "Mienshao-Variant"],
+		evos: ["Mienshao", "Mienshao-Yaoguai"],
 	},
-	mienshaovariant: {
-		name: "Mienshao-Variant",
+	mienshao: {
+		inherit: true,
+		otherFormes: ["Mienshao-Yaoguai"],
+		formeOrder: ["Mienshao", "Mienshao-Yaoguai"],
+	},
+	mienshaoyaoguai: {
+		name: "Mienshao-Yaoguai",
 		baseSpecies: "Mienshao",
-		forme: "Variant",
+		forme: "Yaoguai",
 		copyData: "Mienshao",
 
 		types: ["Fighting", "Ghost"],
@@ -166,8 +174,8 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		baseStats: {hp: 55, atk: 95, def: 45, spa: 120, spd: 70, spe: 115},
 		abilities: {0: "Tough Claws", 1: "Inner Focus", H: "Super Luck"},
 
-		copyMoves: "Kadabra", // although its data is based on Alakazam, it doesn't have Alakazam's moves
-		movepoolAdditions: ["metalclaw", "shadowclaw", "spite", "willowisp"],
+		copyMoves: "Kadabra", // although its data is based on Alakazam, it doesn't have all of Alakazam's moves
+		movepoolAdditions: ["gigaimpact", "hyperbeam", "metalclaw", "shadowclaw", "spite", "willowisp"],
 
 		prevo: "Kadabra",
 		evoType: "other",
@@ -268,13 +276,13 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 
 	inkay: {
 		inherit: true,
-		otherFormes: ["Inkay-Variant"],
-		formeOrder: ["Inkay", "Inkay-Variant"],
+		otherFormes: ["Inkay-Alola"],
+		formeOrder: ["Inkay", "Inkay-Alola"],
 	},
-	inkayvariant: {
-		name: "Inkay-Variant",
+	inkayalola: {
+		name: "Inkay-Alola",
 		baseSpecies: "Inkay",
-		forme: "Variant",
+		forme: "Alola",
 		copyData: "Inkay",
 
 		types: ["Dark", "Water"],
@@ -283,18 +291,18 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		movepoolAdditions: ["brine", "dive", "firstimpression", "terrainpulse", "scald", "surf", "watergun", "waterpulse"],
 		movepoolDeletions: ["brutalswing", "calmmind", "futuresight", "guardswap", "powersplit", "psybeam", "psyshock", "storedpower"],
 
-		evos: ["Malamar-Variant"],
-		creator: "Violettes",
+		evos: ["Malamar-Alola"],
+		creator: "Violet",
 	},
 	malamar: {
 		inherit: true,
-		otherFormes: ["Malamar-Variant"],
-		formeOrder: ["Malamar", "Malamar-Variant"],
+		otherFormes: ["Malamar-Alola"],
+		formeOrder: ["Malamar", "Malamar-Alola"],
 	},
-	malamarvariant: {
-		name: "Malamar-Variant",
+	malamaralola: {
+		name: "Malamar-Alola",
 		baseSpecies: "Malamar",
-		forme: "Variant",
+		forme: "Alola",
 		copyData: "Malamar",
 
 		types: ["Dark", "Water"],
@@ -303,10 +311,10 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		movepoolAdditions: ["brine", "dive", "firstimpression", "terrainpulse", "scald", "surf", "watergun", "waterpulse"],
 		movepoolDeletions: ["brutalswing", "calmmind", "futuresight", "guardswap", "powersplit", "psybeam", "psyshock", "storedpower"],
 
-		prevo: ["Inkay-Variant"],
+		prevo: ["Inkay-Alola"],
 		evoLevel: 30,
 		evoCondition: "with the console turned upside-down",
-		creator: "Violettes",
+		creator: "Violet",
 	},
 
 	scrafty: {
@@ -326,4 +334,206 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		evoCondition: "A newly-discovered evolution",
 		creator: "Magmajudis",
 	},
+
+// SLATE 2
+
+	greedent: {
+		inherit: true,
+		evos: ["Farmunk"],
+	},
+	farmunk: {
+		name: "Farmunk",
+		copyData: "Greedent",
+
+		types: ["Normal", "Grass"],
+		baseStats: {hp: 130, atk: 85, def: 115, spa: 95, spd: 75, spe: 20},
+		abilities: {0: "Cheek Pouch", 1: "Harvest", H: "Gluttony"},
+		movepoolAdditions: ["hiddenpower", "grassknot", "naturalgift", "solarbeam", "toxic", "weatherball"],
+
+		prevo: "Greedent",
+		evoType: "other",
+		evoCondition: "A newly-discovered evolution",
+		creator: "quagsi",
+	},
+
+	persian: {
+		inherit: true,
+		evos: ["Carnelion"],
+	},
+	carnelion: {
+		name: "Carnelion",
+		copyData: "Persian",
+
+		types: ["Normal", "Rock"],
+		baseStats: {hp: 85, atk: 105, def: 70, spa: 95, spd: 70, spe: 120},
+		movepoolAdditions: ["rockslide", "rocktomb", "sandstorm", "stealthrock", "stoneedge"],
+
+		prevo: "Persian",
+		evoType: "other",
+		evoCondition: "A newly-discovered evolution",
+		creator: "abismal",
+	},
+
+	whiscash: {
+		inherit: true,
+		evos: ["Whiscazu"],
+	},
+	whiscazu: {
+		name: "Whiscazu",
+		copyData: "Whiscash",
+
+		baseStats: {hp: 110, atk: 78, def: 78, spa: 116, spd: 81, spe: 70},
+		abilities: {0: "Oblivious", 1: "Cheek Pouch", H: "Hydration"},
+		movepoolAdditions: ["calmmind", "recycle"],
+
+		prevo: "Whiscash",
+		evoType: "other",
+		evoCondition: "A newly-discovered evolution",
+		creator: "ausma",
+	},
+
+	tropius: {
+		inherit: true,
+		evos: ["Musasoar"],
+	},
+	musasoar: {
+		name: "Musasoar",
+		copyData: "Tropius",
+
+		baseStats: {hp: 109, atk: 108, def: 83, spa: 72, spd: 87, spe: 76},
+		abilities: {0: "Chlorophyll", 1: "Leaf Guard", H: "Harvest"},
+		movepoolAdditions: ["acrobatics", "dualwingbeat", "woodhammer"],
+		heightm: 2.4,
+		weightkg: 180,
+
+		prevo: "Tropius",
+		evoType: "other",
+		evoCondition: "A newly-discovered evolution",
+		creator: "Violet",
+	},
+
+	snover: {
+		inherit: true,
+		evos: ["Abomasnow", "Skiversnow"],
+	},
+	skiversnow: {
+		name: "Skiversnow",
+		copyData: "Abomasnow",
+
+		baseStats: {hp: 90, atk: 92, def: 55, spa: 92, spd: 65, spe: 100},
+		abilities: {0: "Snow Warning", H: "Pressure"},
+
+		copyMoves: "Snover", // although its data is based on Abomasnow, it doesn't have all of Abomasnow's moves
+		movepoolAdditions: ["brickbreak", "earthpower", "gigaimpact", "hyperbeam", "outrage", "rapidspin", "rockslide", "uturn"],
+
+		prevo: "Snover",
+		evoType: "other",
+		evoCondition: "A newly-discovered evolution",
+		creator: "ausma",
+	},
+
+	falinks: {
+		inherit: true,
+		otherFormes: ["Falinks-Terracotta"],
+		formeOrder: ["Falinks", "Falinks-Terracotta"],
+	},
+	falinksterracotta: {
+		name: "Falinks-Terracotta",
+		baseSpecies: "Falinks",
+		forme: "Terracotta",
+		copyData: "Falinks",
+
+		types: ["Fighting", "Rock"],
+		baseStats: {hp: 65, atk: 100, def: 100, spa: 60, spd: 60, spe: 85},
+		abilities: {0: "Solid Rock", 1: "Skill Link", H: "Defiant"},
+		movepoolAdditions: ["drainpunch", "rockblast", "stoneedge", "smackdown", "stealthrock"],
+		movepoolDeletions: ["closecombat", "swordsdance"],
+
+		creator: "Hematite",
+	},
+
+	swirlix: {
+		inherit: true,
+		otherFormes: ["Swirlix-Variant"],
+		formeOrder: ["Swirlix", "Swirlix-Variant"],
+	},
+	swirlixvariant: {
+		name: "Swirlix-Variant",
+		baseSpecies: "Swirlix",
+		forme: "Variant",
+		copyData: "Swirlix",
+
+		types: ["Poison"],
+		abilities: {0: "Sticky Hold", H: "Unburden"},
+		movepoolAdditions: ["belch", "sludgebomb", "stuffcheeks"],
+		movepoolDeletions: ["aromatherapy", "bellydrum", "cottonguard", "dazzlinggleam", "drainingkiss", "fairywind", "mistyexplosion", "playrough"],
+
+		evos: ["Slurpuff-Variant"],
+		creator: "abismal",
+	},
+	slurpuff: {
+		inherit: true,
+		otherFormes: ["Slurpuff-Variant"],
+		formeOrder: ["Slurpuff", "Slurpuff-Variant"],
+	},
+	slurpuffvariant: {
+		name: "Slurpuff-Variant",
+		baseSpecies: "Slurpuff",
+		forme: "Variant",
+		copyData: "Slurpuff",
+
+		types: ["Poison"],
+		abilities: {0: "Sticky Hold", H: "Unburden"},
+		movepoolAdditions: ["belch", "sludgebomb", "stuffcheeks"],
+		movepoolDeletions: ["aromatherapy", "bellydrum", "cottonguard", "dazzlinggleam", "drainingkiss", "fairywind", "mistyexplosion", "playrough"],
+
+		prevo: ["Swirlix-Variant"],
+		evoType: "trade",
+		evoItem: "Black Sludge",
+		creator: "abismal",
+	},
+
+	morelull: {
+		inherit: true,
+		evos: ["Shiinotic", "Shiinotic-Kalos"],
+	},
+	shiinotic: {
+		inherit: true,
+		otherFormes: ["Shiinotic-Kalos"],
+		formeOrder: ["Shiinotic", "Shiinotic-Kalos"],
+	},
+	shiinotickalos: {
+		name: "Shiinotic-Kalos",
+		baseSpecies: "Shiinotic",
+		forme: "Kalos",
+		copyData: "Shiinotic",
+
+		types: ["Poison", "Fairy"],
+		baseStats: {hp: 60, atk: 45, def: 60, spa: 90, spd: 80, spe: 70},
+		abilities: {0: "Illuminate", 1: "Effect Spore", H: "Technician"},
+		movepoolAdditions: ["acidspray", "nastyplot", "venoshock"],
+
+		prevo: "Morelull",
+		evoLevel: 24,
+		creator: "Hematite",
+	},
+
+	unown: {
+		inherit: true,
+		evos: ["Nown"],
+	},
+	nown: {
+		name: "Nown",
+		copyData: "Unown",
+
+		baseStats: {hp: 108, atk: 91, def: 28, spa: 132, spd: 23, spe: 98},
+		abilities: {0: "Protean"},
+		movepoolAdditions: ["guardsplit", "naturepower", "weatherball"],
+
+		prevo: "Unown",
+		evoType: "other",
+		evoCondition: "A newly-discovered evolution",
+		creator: "abismal",
+	},
+
 };
