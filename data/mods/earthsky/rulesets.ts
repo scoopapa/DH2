5356f65074dd82ef170da0928833a22680de5344
En Passant <enpassant4264@gmail.com>
2022-01-05 22:12:53 -0700

    Earth & Sky first real update
    this is like, the anti-comma problem

--- data/mods/earthsky/rulesets.ts
+++ data/mods/earthsky/rulesets.ts
@@ -17,7 +17,7 @@ export const Formats: {[k: string]: ModdedFormatData} = {
 					hpTypeX += i * (ivs[s] % 2);
 					i *= 2;
 				}
-				set.hpType = hpTypes[min(15,tr(hpTypeX * 16 / 63))],
+				set.hpType = hpTypes[min(15,tr(hpTypeX * 16 / 63))];
 			}
 		},
 	},
