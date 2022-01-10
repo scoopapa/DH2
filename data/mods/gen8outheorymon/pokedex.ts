b349abb66db2b1362e23c33c44e387ac07bd34a0
TailGlowVM <92479691+TailGlowVM@users.noreply.github.com>
2022-01-10 16:50:02 +0000

    Update pokedex.ts

--- data/mods/gen8outheorymon/pokedex.ts
+++ data/mods/gen8outheorymon/pokedex.ts
@@ -3,4 +3,8 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
       inherit: true,
       abilities: {0: "Leaf Guard", 1: "Tough Claws"},
    },
+	starmie: {
+		inherit: true,
+		baseStats: {hp: 60, atk: 75, def: 85, spa: 115, spd: 85, spe: 115},
+	},
 };
