ec87212f659229898d13ccbbd0c2158a794a673a
TailGlowVM <92479691+TailGlowVM@users.noreply.github.com>
2022-01-10 16:34:15 +0000

    Update pokedex.ts

--- data/mods/gen8outheorymon/pokedex.ts
+++ data/mods/gen8outheorymon/pokedex.ts
@@ -1,6 +1,6 @@
 export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
    zarude: {
       inherit: true,
-      abilities: {0: "Leaf Guard", 1: "Tough Claws"}
+      abilities: {0: "Leaf Guard", 1: "Tough Claws"},
    },
 };
