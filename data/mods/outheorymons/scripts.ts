42351dc504c17b16c295f2a28048df88ec0fa13f
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2022-01-12 00:06:35 -0500

    Update scripts.ts

--- data/mods/outheorymons/scripts.ts
+++ data/mods/outheorymons/scripts.ts
@@ -2,6 +2,6 @@
 
 export const Scripts: BattleScriptsData = {
    init: function() {
-      this.modData('Learnsets', 'starmie').learnset.mysticalfire = [8L1];
+      this.modData('Learnsets', 'starmie').learnset.mysticalfire = ['8L1'];
    },
 };
