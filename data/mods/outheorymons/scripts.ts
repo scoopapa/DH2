fd3c6448b912e3d4ef367926445b248e7f072c34
TailGlowVM <92479691+TailGlowVM@users.noreply.github.com>
2022-01-15 16:05:20 +0000

    Update scripts.ts

--- data/mods/outheorymons/scripts.ts
+++ data/mods/outheorymons/scripts.ts
@@ -1,7 +1,7 @@
-//this.modData('Learnsets', 'starmie').learnset.mysticalfire = [8L1];
-
 export const Scripts: BattleScriptsData = {
    init: function() {
       this.modData('Learnsets', 'starmie').learnset.mysticalfire = [8L1];
+		this.modData('Learnsets', 'thundurus').learnset.spikes = [8L1];
+		this.modData('Learnsets', 'thundurustherian').learnset.spikes = [8L1];
    },
 };
