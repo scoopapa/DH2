9d11ce863cac1919eb9fed3e729953a0449ad09e
ViZarSmogon <vizarsmogon@gmail.com>
2022-01-18 16:27:51 +0100

    Multiverse Slate 2

--- data/mods/multiverse/pokedex.ts
+++ data/mods/multiverse/pokedex.ts
@@ -41,4 +41,47 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		types: ["Psychic", "Water"],
 		baseStats: {hp: 100, atk: 100, def: 80, spa: 100, spd: 100, spe: 80},
 	},
+	noctowl: {
+		inherit: true,
+		types: ["Steel", "Flying"],
+		baseStats: {hp: 100, atk: 50, def: 50, spa: 96, spd: 106, spe: 70},
+		abilities: {0: "Insomnia", H: "Keen Eye"},
+	},
+	crobat: {
+		inherit: true,
+		types: ["Poison", "Flying"],
+		baseStats: {hp: 95, atk: 95, def: 80, spa: 70, spd: 80, spe: 130},
+		abilities: {0: "Overcoat", H: "Infiltrator"},
+	},
+	jumpluff: {
+		inherit: true,
+		types: ["Flying"],
+		baseStats: {hp: 85, atk: 95, def: 80, spa: 55, spd: 105, spe: 120},
+		abilities: {0: "Frisk", 1: "Limber", H: "Infiltrator"},
+	},
+	murkrow: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 110, def: 62, spa: 110, spd: 62, spe: 111},
+		abilities: {0: "Insomnia", 1: "Super Luck", H: "Smart Prankster"},
+	},
+	forretress: {
+		inherit: true,
+		baseStats: {hp: 85, atk: 90, def: 150, spa: 60, spd: 80, spe: 30},
+	},
+	houndoom: {
+		inherit: true,
+		baseStats: {hp: 85, atk: 90, def: 65, spa: 115, spd: 90, spe: 95},
+		abilities: {0: "Early Bird", H: "Drought"},
+	},
+	magby: {
+		inherit: true,
+		types: ["Fire", "Fighting"],
+		baseStats: {hp: 75, atk: 115, def: 67, spa: 110, spd: 85, spe: 83},
+		abilities: {0: "Flame Body", 1: "Multiscale", H: "Vital Spirit"},
+		evos: null,
+	},
+	entei: {
+		inherit: true,
+		baseStats: {hp: 100, atk: 115, def: 100, spa: 100, spd: 80, spe: 85},
+	},
 };
