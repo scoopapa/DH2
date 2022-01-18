b87e2ec0d4f5f781ca164aafc5cfb2fb5b18b52c
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-01-18 09:16:59 +0000

    Evolution Project: Slate 3 customs
    For prompt-related reasons, both are Storm Drain clones (not coincidental)

--- data/mods/evolutionproject/abilities.ts
+++ data/mods/evolutionproject/abilities.ts
@@ -13,4 +13,54 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 		rating: 3,
 		num: -1,
 	},
+	wildfire: {
+		shortDesc: "The Pokémon draws Fire moves to itself to raise Speed by 1; Fire immunity.",
+		onTryHit(target, source, move) {
+			if (target !== source && move.type === 'Fire') {
+				if (!this.boost({spe: 1})) {
+					this.add('-immune', target, '[from] ability: Wildfire');
+				}
+				return null;
+			}
+		},
+		onAnyRedirectTarget(target, source, source2, move) {
+			if (move.type !== 'Fire' || ['firepledge', 'grasspledge', 'waterpledge'].includes(move.id)) return;
+			const redirectTarget = ['randomNormal', 'adjacentFoe'].includes(move.target) ? 'normal' : move.target;
+			if (this.validTarget(this.effectData.target, source, redirectTarget)) {
+				if (move.smartTarget) move.smartTarget = false;
+				if (this.effectData.target !== target) {
+					this.add('-activate', this.effectData.target, 'ability: Wildfire');
+				}
+				return this.effectData.target;
+			}
+		},
+		name: "Wildfire",
+		rating: 3,
+		num: -2,
+	},
+	centrifuge: {
+		shortDesc: "The Pokémon draws Ground moves to itself to raise Attack by 1; Ground immunity.",
+		onTryHit(target, source, move) {
+			if (target !== source && move.type === 'Ground') {
+				if (!this.boost({atk: 1})) {
+					this.add('-immune', target, '[from] ability: Centrifuge');
+				}
+				return null;
+			}
+		},
+		onAnyRedirectTarget(target, source, source2, move) {
+			if (move.type !== 'Ground' || ['firepledge', 'grasspledge', 'waterpledge'].includes(move.id)) return;
+			const redirectTarget = ['randomNormal', 'adjacentFoe'].includes(move.target) ? 'normal' : move.target;
+			if (this.validTarget(this.effectData.target, source, redirectTarget)) {
+				if (move.smartTarget) move.smartTarget = false;
+				if (this.effectData.target !== target) {
+					this.add('-activate', this.effectData.target, 'ability: Centrifuge');
+				}
+				return this.effectData.target;
+			}
+		},
+		name: "Centrifuge",
+		rating: 3,
+		num: -3,
+	},
 };
