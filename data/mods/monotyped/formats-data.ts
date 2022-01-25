aaae8b5e53ad635f120472e87b298a5e43595c75
ViZarSmogon <vizarsmogon@gmail.com>
2022-01-25 22:10:05 +0100

    MonoTyped

---
+++ data/mods/monotyped/formats-data.ts
@@ -0,0 +1,1545 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	charmander: {
+		tier: "MT LC",
+	},
+	charmeleon: {
+		tier: "MT NFE",
+	},
+	squirtle: {
+		tier: "MT LC",
+	},
+	wartortle: {
+		tier: "MT NFE",
+	},
+	blastoise: {
+		tier: "MT OU",
+	},
+	blastoisemega: {
+		tier: "MT Uber",
+	},
+	caterpie: {
+		tier: "MT LC",
+	},
+	metapod: {
+		tier: "MT NFE",
+	},
+	rattata: {
+		tier: "MT LC",
+	},
+	raticate: {
+		tier: "MT UU",
+	},
+	ekans: {
+		tier: "MT LC",
+	},
+	arbok: {
+		tier: "MT UU",
+	},
+	pichu: {
+		tier: "MT LC",
+	},
+	pikachu: {
+		tier: "MT NFE",
+	},
+	raichu: {
+		tier: "MT OU",
+	},
+	sandshrew: {
+		tier: "MT LC",
+	},
+	sandslash: {
+		tier: "MT UU",
+	},
+	nidoranf: {
+		tier: "MT LC",
+	},
+	nidorina: {
+		tier: "MT NFE",
+	},
+	nidoranm: {
+		tier: "MT LC",
+	},
+	nidorino: {
+		tier: "MT NFE",
+	},
+	cleffa: {
+		tier: "MT LC",
+	},
+	clefairy: {
+		tier: "MT NFE",
+	},
+	clefable: {
+		tier: "MT OU",
+	},
+	vulpix: {
+		tier: "MT LC",
+	},
+	vulpixalola: {
+		tier: "MT NFE",
+	},
+	ninetales: {
+		tier: "MT OU",
+	},
+	diglett: {
+		tier: "MT LC",
+	},
+	dugtrio: {
+		tier: "MT OU",
+	},
+	meowth: {
+		tier: "MT LC",
+	},
+	meowthalola: {
+		tier: "MT LC",
+	},
+	meowthgalar: {
+		tier: "MT LC",
+	},
+	persian: {
+		tier: "MT OU",
+	},
+	persianalola: {
+		tier: "MT OU",
+	},
+	perrserker: {
+		tier: "MT OU",
+	},
+	psyduck: {
+		tier: "MT LC",
+	},
+	golduck: {
+		tier: "MT OU",
+	},
+	mankey: {
+		tier: "MT LC",
+	},
+	primeape: {
+		tier: "MT UU",
+	},
+	growlithe: {
+		tier: "MT LC",
+	},
+	arcanine: {
+		tier: "MT OU",
+	},
+	poliwag: {
+		tier: "MT LC",
+	},
+	poliwhirl: {
+		tier: "MT NFE",
+	},
+	politoed: {
+		tier: "MT OU",
+	},
+	abra: {
+		tier: "MT LC",
+	},
+	kadabra: {
+		tier: "MT NFE",
+	},
+	alakazam: {
+		tier: "MT OU",
+	},
+	alakazammega: {
+		tier: "MT OU",
+	},
+	machop: {
+		tier: "MT LC",
+	},
+	machoke: {
+		tier: "MT NFE",
+	},
+	machamp: {
+		tier: "MT OU",
+	},
+	ponyta: {
+		tier: "MT LC",
+	},
+	ponytagalar: {
+		tier: "MT LC",
+	},
+	rapidash: {
+		tier: "MT OU",
+	},
+	farfetchdgalar: {
+		tier: "MT LC",
+	},
+	sirfetchd: {
+		tier: "MT OU",
+	},
+	seel: {
+		tier: "MT LC",
+	},
+	grimer: {
+		tier: "MT LC",
+	},
+	muk: {
+		tier: "MT UU",
+	},
+	shellder: {
+		tier: "MT LC",
+	},
+	drowzee: {
+		tier: "MT LC",
+	},
+	hypno: {
+		tier: "MT UU",
+	},
+	krabby: {
+		tier: "MT LC",
+	},
+	kingler: {
+		tier: "MT OU",
+	},
+	voltorb: {
+		tier: "MT LC",
+	},
+	electrode: {
+		tier: "MT UU",
+	},
+	cubone: {
+		tier: "MT LC",
+	},
+	marowak: {
+		tier: "MT OU",
+	},
+	tyrogue: {
+		tier: "MT LC",
+	},
+	hitmonlee: {
+		tier: "MT OU",
+	},
+	hitmonchan: {
+		tier: "MT OU",
+	},
+	hitmontop: {
+		tier: "MT OU",
+	},
+	lickitung: {
+		tier: "MT LC",
+	},
+	lickilicky: {
+		tier: "MT OU",
+	},
+	koffing: {
+		tier: "MT LC",
+	},
+	weezing: {
+		tier: "MT OU",
+	},
+	happiny: {
+		tier: "MT LC",
+	},
+	chansey: {
+		tier: "MT OU",
+	},
+	blissey: {
+		tier: "MT OU",
+	},
+	tangela: {
+		tier: "MT NFE",
+	},
+	tangrowth: {
+		tier: "MT OU",
+	},
+	kangaskhan: {
+		tier: "MT OU",
+	},
+	kangaskhanmega: {
+		tier: "MT Uber",
+	},
+	horsea: {
+		tier: "MT LC",
+	},
+	seadra: {
+		tier: "MT NFE",
+	},
+	goldeen: {
+		tier: "MT LC",
+	},
+	seaking: {
+		tier: "MT OU",
+	},
+	staryu: {
+		tier: "MT LC",
+	},
+	starmie: {
+		tier: "MT OU",
+	},
+	elekid: {
+		tier: "MT LC",
+	},
+	electabuzz: {
+		tier: "MT NFE",
+	},
+	electivire: {
+		tier: "MT OU",
+	},
+	magby: {
+		tier: "MT LC",
+	},
+	magmar: {
+		tier: "MT NFE",
+	},
+	magmortar: {
+		tier: "MT OU",
+	},
+	pinsir: {
+		tier: "MT OU",
+	},
+	tauros: {
+		tier: "MT OU",
+	},
+	magikarp: {
+		tier: "MT LC",
+	},
+	ditto: {
+		tier: "MT OU",
+	},
+	eevee: {
+		tier: "MT LC",
+	},
+	vaporeon: {
+		tier: "MT OU",
+	},
+	jolteon: {
+		tier: "MT OU",
+	},
+	flareon: {
+		tier: "MT OU",
+	},
+	espeon: {
+		tier: "MT OU",
+	},
+	umbreon: {
+		tier: "MT OU",
+	},
+	leafeon: {
+		tier: "MT OU",
+	},
+	glaceon: {
+		tier: "MT OU",
+	},
+	sylveon: {
+		tier: "MT OU",
+	},
+	porygon: {
+		tier: "MT LC",
+	},
+	porygon2: {
+		tier: "MT OU",
+	},
+	porygonz: {
+		tier: "MT OU",
+	},
+	munchlax: {
+		tier: "MT LC",
+	},
+	snorlax: {
+		tier: "MT OU",
+	},
+	snorlaxgmax: {
+		tier: "AG",
+	},
+	dratini: {
+		tier: "MT LC",
+	},
+	dragonair: {
+		tier: "MT NFE",
+	},
+	mewtwo: {
+		tier: "MT Uber",
+	},
+	mewtwomegay: {
+		tier: "MT Uber",
+	},
+	mew: {
+		tier: "MT OU",
+	},
+	chikorita: {
+		tier: "MT LC",
+	},
+	bayleef: {
+		tier: "MT NFE",
+	},
+	meganium: {
+		tier: "MT UU",
+	},
+	cyndaquil: {
+		tier: "MT LC",
+	},
+	quilava: {
+		tier: "MT NFE",
+	},
+	typhlosion: {
+		tier: "MT UU",
+	},
+	totodile: {
+		tier: "MT LC",
+	},
+	croconaw: {
+		tier: "MT NFE",
+	},
+	feraligatr: {
+		tier: "MT UU",
+	},
+	sentret: {
+		tier: "MT LC",
+	},
+	furret: {
+		tier: "MT UU",
+	},
+	togepi: {
+		tier: "MT LC",
+	},
+	mareep: {
+		tier: "MT LC",
+	},
+	flaaffy: {
+		tier: "MT NFE",
+	},
+	ampharos: {
+		tier: "MT UU",
+	},
+	bonsly: {
+		tier: "MT LC",
+	},
+	sudowoodo: {
+		tier: "MT OU",
+	},
+	aipom: {
+		tier: "MT LC",
+	},
+	ambipom: {
+		tier: "MT UU",
+	},
+	sunkern: {
+		tier: "MT LC",
+	},
+	sunflora: {
+		tier: "MT UU",
+	},
+	misdreavus: {
+		tier: "MT LC",
+	},
+	mismagius: {
+		tier: "MT UU",
+	},
+	unown: {
+		tier: "MT UU",
+	},
+	wynaut: {
+		tier: "MT LC",
+	},
+	wobbuffet: {
+		tier: "MT OU",
+	},
+	pineco: {
+		tier: "MT LC",
+	},
+	dunsparce: {
+		tier: "MT OU",
+	},
+	snubbull: {
+		tier: "MT LC",
+	},
+	granbull: {
+		tier: "MT UU",
+	},
+	teddiursa: {
+		tier: "MT LC",
+	},
+	ursaring: {
+		tier: "MT UU",
+	},
+	slugma: {
+		tier: "MT LC",
+	},
+	corsolagalar: {
+		tier: "MT NFE",
+	},
+	cursola: {
+		tier: "MT OU",
+	},
+	remoraid: {
+		tier: "MT LC",
+	},
+	octillery: {
+		tier: "MT OU",
+	},
+	phanpy: {
+		tier: "MT LC",
+	},
+	donphan: {
+		tier: "MT OU",
+	},
+	stantler: {
+		tier: "MT UU",
+	},
+	smeargle: {
+		tier: "MT UU",
+	},
+	miltank: {
+		tier: "MT OU",
+	},
+	raikou: {
+		tier: "MT OU",
+	},
+	entei: {
+		tier: "MT OU",
+	},
+	suicune: {
+		tier: "MT OU",
+	},
+	treecko: {
+		tier: "MT LC",
+	},
+	grovyle: {
+		tier: "MT NFE",
+	},
+	sceptile: {
+		tier: "MT OU",
+	},
+	torchic: {
+		tier: "MT LC",
+	},
+	mudkip: {
+		tier: "MT LC",
+	},
+	poochyena: {
+		tier: "MT LC",
+	},
+	mightyena: {
+		tier: "MT UU",
+	},
+	zigzagoon: {
+		tier: "MT LC",
+	},
+	linoone: {
+		tier: "MT OU",
+	},
+	wurmple: {
+		tier: "MT LC",
+	},
+	silcoon: {
+		tier: "MT NFE",
+	},
+	cascoon: {
+		tier: "MT NFE",
+	},
+	shroomish: {
+		tier: "MT LC",
+	},
+	slakoth: {
+		tier: "MT LC",
+	},
+	vigoroth: {
+		tier: "MT NFE",
+	},
+	slaking: {
+		tier: "MT UU",
+	},
+	whismur: {
+		tier: "MT LC",
+	},
+	loudred: {
+		tier: "MT NFE",
+	},
+	exploud: {
+		tier: "MT OU",
+	},
+	makuhita: {
+		tier: "MT LC",
+	},
+	hariyama: {
+		tier: "MT UU",
+	},
+	nosepass: {
+		tier: "MT LC",
+	},
+	skitty: {
+		tier: "MT LC",
+	},
+	delcatty: {
+		tier: "MT UU",
+	},
+	electrike: {
+		tier: "MT LC",
+	},
+	manectric: {
+		tier: "MT OU",
+	},
+	manectricmega: {
+		tier: "MT OU",
+	},
+	plusle: {
+		tier: "MT UU",
+	},
+	minun: {
+		tier: "MT UU",
+	},
+	volbeat: {
+		tier: "MT UU",
+	},
+	illumise: {
+		tier: "MT UU",
+	},
+	gulpin: {
+		tier: "MT LC",
+	},
+	swalot: {
+		tier: "MT UU",
+	},
+	wailmer: {
+		tier: "MT LC",
+	},
+	wailord: {
+		tier: "MT OU",
+	},
+	torkoal: {
+		tier: "MT OU",
+	},
+	spoink: {
+		tier: "MT LC",
+	},
+	grumpig: {
+		tier: "MT UU",
+	},
+	spinda: {
+		tier: "MT UU",
+	},
+	trapinch: {
+		tier: "MT LC",
+	},
+	cacnea: {
+		tier: "MT LC",
+	},
+	zangoose: {
+		tier: "MT UU",
+	},
+	seviper: {
+		tier: "MT UU",
+	},
+	feebas: {
+		tier: "MT LC",
+	},
+	milotic: {
+		tier: "MT OU",
+	},
+	castform: {
+		tier: "MT UU",
+	},
+	castformsunny: {
+	},
+	castformrainy: {
+	},
+	castformsnowy: {
+	},
+	kecleon: {
+		tier: "MT UU",
+	},
+	shuppet: {
+		tier: "MT LC",
+	},
+	banette: {
+		tier: "MT UU",
+	},
+	banettemega: {
+		tier: "MT OU",
+	},
+	duskull: {
+		tier: "MT LC",
+	},
+	dusclops: {
+		tier: "MT NFE",
+	},
+	dusknoir: {
+		tier: "MT OU",
+	},
+	chingling: {
+		tier: "MT LC",
+	},
+	chimecho: {
+		tier: "MT UU",
+	},
+	absol: {
+		tier: "MT OU",
+	},
+	absolmega: {
+		tier: "MT OU",
+	},
+	snorunt: {
+		tier: "MT LC",
+	},
+	glalie: {
+		tier: "MT OU",
+	},
+	glaliemega: {
+		tier: "MT OU",
+	},
+	clamperl: {
+		tier: "MT LC",
+	},
+	huntail: {
+		tier: "MT UU",
+	},
+	gorebyss: {
+		tier: "MT UU",
+	},
+	luvdisc: {
+		tier: "MT UU",
+	},
+	bagon: {
+		tier: "MT LC",
+	},
+	shelgon: {
+		tier: "MT NFE",
+	},
+	regirock: {
+		tier: "MT OU",
+	},
+	regice: {
+		tier: "MT OU",
+	},
+	registeel: {
+		tier: "MT OU",
+	},
+	kyogre: {
+		tier: "MT Uber",
+	},
+	kyogreprimal: {
+		tier: "MT Uber",
+	},
+	groudon: {
+		tier: "MT Uber",
+	},
+	deoxys: {
+		tier: "MT Uber",
+	},
+	deoxysattack: {
+		tier: "MT Uber",
+	},
+	deoxysdefense: {
+		tier: "MT Uber",
+	},
+	deoxysspeed: {
+		tier: "MT Uber",
+	},
+	turtwig: {
+		tier: "MT LC",
+	},
+	grotle: {
+		tier: "MT NFE",
+	},
+	chimchar: {
+		tier: "MT LC",
+	},
+	piplup: {
+		tier: "MT LC",
+	},
+	prinplup: {
+		tier: "MT NFE",
+	},
+	bidoof: {
+		tier: "MT LC",
+	},
+	kricketot: {
+		tier: "MT LC",
+	},
+	kricketune: {
+		tier: "MT UU",
+	},
+	shinx: {
+		tier: "MT LC",
+	},
+	luxio: {
+		tier: "MT NFE",
+	},
+	luxray: {
+		tier: "MT OU",
+	},
+	cranidos: {
+		tier: "MT LC",
+	},
+	rampardos: {
+		tier: "MT UU",
+	},
+	burmy: {
+		tier: "MT LC",
+	},
+	pachirisu: {
+		tier: "MT UU",
+	},
+	buizel: {
+		tier: "MT LC",
+	},
+	floatzel: {
+		tier: "MT UU",
+	},
+	cherubi: {
+		tier: "MT NFE",
+	},
+	cherrim: {
+		tier: "MT OU",
+	},
+	cherrimsunshine: {
+	},
+	shellos: {
+		tier: "MT LC",
+	},
+	buneary: {
+		tier: "MT LC",
+	},
+	lopunny: {
+		tier: "MT OU",
+	},
+	glameow: {
+		tier: "MT LC",
+	},
+	purugly: {
+		tier: "MT UU",
+	},
+	riolu: {
+		tier: "MT LC",
+	},
+	hippopotas: {
+		tier: "MT LC",
+	},
+	hippowdon: {
+		tier: "MT OU",
+	},
+	carnivine: {
+		tier: "MT UU",
+	},
+	finneon: {
+		tier: "MT LC",
+	},
+	lumineon: {
+		tier: "MT UU",
+	},
+	uxie: {
+		tier: "MT OU",
+	},
+	mesprit: {
+		tier: "MT OU",
+	},
+	azelf: {
+		tier: "MT OU",
+	},
+	regigigas: {
+		tier: "MT OU",
+	},
+	cresselia: {
+		tier: "MT OU",
+	},
+	phione: {
+		tier: "MT UU",
+	},
+	manaphy: {
+		tier: "MT OU",
+	},
+	darkrai: {
+		tier: "MT Uber",
+	},
+	shaymin: {
+		tier: "MT OU",
+	},
+	arceus: {
+		tier: "MT Uber",
+	},
+	arceusbug: {
+		tier: "MT Uber",
+	},
+	arceusdark: {
+		tier: "MT Uber",
+	},
+	arceusdragon: {
+		tier: "MT Uber",
+	},
+	arceuselectric: {
+		tier: "MT Uber",
+	},
+	arceusfairy: {
+		tier: "MT Uber",
+	},
+	arceusfighting: {
+		tier: "MT Uber",
+	},
+	arceusfire: {
+		tier: "MT Uber",
+	},
+	arceusflying: {
+		tier: "MT Uber",
+	},
+	arceusghost: {
+		tier: "MT Uber",
+	},
+	arceusgrass: {
+		tier: "MT Uber",
+	},
+	arceusground: {
+		tier: "MT Uber",
+	},
+	arceusice: {
+		tier: "MT Uber",
+	},
+	arceuspoison: {
+		tier: "MT Uber",
+	},
+	arceuspsychic: {
+		tier: "MT Uber",
+	},
+	arceusrock: {
+		tier: "MT Uber",
+	},
+	arceussteel: {
+		tier: "MT Uber",
+	},
+	arceuswater: {
+		tier: "MT Uber",
+	},
+	snivy: {
+		tier: "MT LC",
+	},
+	servine: {
+		tier: "MT NFE",
+	},
+	serperior: {
+		tier: "MT OU",
+	},
+	tepig: {
+		tier: "MT LC",
+	},
+	oshawott: {
+		tier: "MT LC",
+	},
+	dewott: {
+		tier: "MT NFE",
+	},
+	samurott: {
+		tier: "MT UU",
+	},
+	patrat: {
+		tier: "MT LC",
+	},
+	watchog: {
+		tier: "MT UU",
+	},
+	lillipup: {
+		tier: "MT LC",
+	},
+	herdier: {
+		tier: "MT NFE",
+	},
+	stoutland: {
+		tier: "MT OU",
+	},
+	purrloin: {
+		tier: "MT LC",
+	},
+	liepard: {
+		tier: "MT OU",
+	},
+	pansage: {
+		tier: "MT LC",
+	},
+	simisage: {
+		tier: "MT UU",
+	},
+	pansear: {
+		tier: "MT LC",
+	},
+	simisear: {
+		tier: "MT UU",
+	},
+	panpour: {
+		tier: "MT LC",
+	},
+	simipour: {
+		tier: "MT UU",
+	},
+	munna: {
+		tier: "MT LC",
+	},
+	musharna: {
+		tier: "MT OU",
+	},
+	blitzle: {
+		tier: "MT LC",
+	},
+	zebstrika: {
+		tier: "MT UU",
+	},
+	roggenrola: {
+		tier: "MT LC",
+	},
+	boldore: {
+		tier: "MT NFE",
+	},
+	gigalith: {
+		tier: "MT OU",
+	},
+	drilbur: {
+		tier: "MT LC",
+	},
+	audino: {
+		tier: "MT OU",
+	},
+	timburr: {
+		tier: "MT LC",
+	},
+	gurdurr: {
+		tier: "MT NFE",
+	},
+	conkeldurr: {
+		tier: "MT OU",
+	},
+	tympole: {
+		tier: "MT LC",
+	},
+	throh: {
+		tier: "MT OU",
+	},
+	sawk: {
+		tier: "MT OU",
+	},
+	petilil: {
+		tier: "MT LC",
+	},
+	lilligant: {
+		tier: "MT OU",
+	},
+	basculin: {
+		tier: "MT OU",
+	},
+	basculinbluestriped: {
+		tier: "MT OU",
+	},
+	darumaka: {
+		tier: "MT LC",
+	},
+	darumakagalar: {
+		tier: "MT LC",
+	},
+	darmanitan: {
+		tier: "MT OU",
+	},
+	darmanitangalar: {
+		tier: "MT Uber",
+	},
+	maractus: {
+		tier: "MT OU",
+	},
+	yamask: {
+		tier: "MT LC",
+	},
+	cofagrigus: {
+		tier: "MT OU",
+	},
+	trubbish: {
+		tier: "MT LC",
+	},
+	garbodor: {
+		tier: "MT OU",
+	},
+	zorua: {
+		tier: "MT LC",
+	},
+	zoroark: {
+		tier: "MT OU",
+	},
+	minccino: {
+		tier: "MT LC",
+	},
+	cinccino: {
+		tier: "MT OU",
+	},
+	gothita: {
+		tier: "MT NFE",
+	},
+	gothorita: {
+		tier: "MT NFE",
+	},
+	gothitelle: {
+		tier: "MT OU",
+	},
+	solosis: {
+		tier: "MT LC",
+	},
+	duosion: {
+		tier: "MT NFE",
+	},
+	reuniclus: {
+		tier: "MT OU",
+	},
+	vanillite: {
+		tier: "MT LC",
+	},
+	vanillish: {
+		tier: "MT NFE",
+	},
+	vanilluxe: {
+		tier: "MT OU",
+	},
+	karrablast: {
+		tier: "MT LC",
+	},
+	alomomola: {
+		tier: "MT UU",
+	},
+	klink: {
+		tier: "MT LC",
+	},
+	klang: {
+		tier: "MT NFE",
+	},
+	klinklang: {
+		tier: "MT OU",
+	},
+	tynamo: {
+		tier: "MT LC",
+	},
+	eelektrik: {
+		tier: "MT NFE",
+	},
+	eelektross: {
+		tier: "MT UU",
+	},
+	elgyem: {
+		tier: "MT LC",
+	},
+	beheeyem: {
+		tier: "MT OU",
+	},
+	axew: {
+		tier: "MT LC",
+	},
+	fraxure: {
+		tier: "MT NFE",
+	},
+	haxorus: {
+		tier: "MT OU",
+	},
+	cubchoo: {
+		tier: "MT LC",
+	},
+	beartic: {
+		tier: "MT OU",
+	},
+	cryogonal: {
+		tier: "MT OU",
+	},
+	shelmet: {
+		tier: "MT LC",
+	},
+	accelgor: {
+		tier: "MT OU",
+	},
+	mienfoo: {
+		tier: "MT LC",
+	},
+	mienshao: {
+		tier: "MT OU",
+	},
+	druddigon: {
+		tier: "MT OU",
+	},
+	bouffalant: {
+		tier: "MT OU",
+	},
+	heatmor: {
+		tier: "MT OU",
+	},
+	tornadus: {
+		tier: "MT OU",
+	},
+	tornadustherian: {
+		tier: "MT OU",
+	},
+	chespin: {
+		tier: "MT LC",
+	},
+	quilladin: {
+		tier: "MT NFE",
+	},
+	fennekin: {
+		tier: "MT LC",
+	},
+	braixen: {
+		tier: "MT NFE",
+	},
+	froakie: {
+		tier: "MT LC",
+	},
+	frogadier: {
+		tier: "MT NFE",
+	},
+	bunnelby: {
+		tier: "MT LC",
+	},
+	scatterbug: {
+		tier: "MT LC",
+	},
+	spewpa: {
+		tier: "MT NFE",
+	},
+	flabebe: {
+		tier: "MT LC",
+	},
+	floette: {
+		tier: "MT NFE",
+	},
+	florges: {
+		tier: "MT UU",
+	},
+	skiddo: {
+		tier: "MT LC",
+	},
+	gogoat: {
+		tier: "MT UU",
+	},
+	pancham: {
+		tier: "MT LC",
+	},
+	furfrou: {
+		tier: "MT UU",
+	},
+	espurr: {
+		tier: "MT LC",
+	},
+	meowstic: {
+		tier: "MT OU",
+	},
+	meowsticf: {
+		tier: "MT OU",
+	},
+	spritzee: {
+		tier: "MT LC",
+	},
+	aromatisse: {
+		tier: "MT OU",
+	},
+	swirlix: {
+		tier: "MT NFE",
+	},
+	slurpuff: {
+		tier: "MT OU",
+	},
+	clauncher: {
+		tier: "MT LC",
+	},
+	clawitzer: {
+		tier: "MT OU",
+	},
+	goomy: {
+		tier: "MT LC",
+	},
+	sliggoo: {
+		tier: "MT NFE",
+	},
+	goodra: {
+		tier: "MT OU",
+	},
+	bergmite: {
+		tier: "MT LC",
+	},
+	avalugg: {
+		tier: "MT OU",
+	},
+	xerneas: {
+		tier: "MT Uber",
+	},
+	litten: {
+		tier: "MT LC",
+	},
+	torracat: {
+		tier: "MT NFE",
+	},
+	popplio: {
+		tier: "MT LC",
+	},
+	brionne: {
+		tier: "MT NFE",
+	},
+	yungoos: {
+		tier: "MT LC",
+	},
+	gumshoos: {
+		tier: "MT UU",
+	},
+	grubbin: {
+		tier: "MT LC",
+	},
+	crabrawler: {
+		tier: "MT LC",
+	},
+	rockruff: {
+		tier: "MT LC",
+	},
+	rockruffdusk: {
+		tier: "MT LC",
+	},
+	lycanroc: {
+		tier: "MT OU",
+	},
+	lycanrocmidnight: {
+		tier: "MT OU",
+	},
+	lycanrocdusk: {
+		tier: "MT OU",
+	},
+	wishiwashi: {
+		tier: "MT OU",
+	},
+	wishiwashischool: {
+	},
+	mudbray: {
+		tier: "MT LC",
+	},
+	mudsdale: {
+		tier: "MT OU",
+	},
+	fomantis: {
+		tier: "MT LC",
+	},
+	lurantis: {
+		tier: "MT OU",
+	},
+	morelull: {
+		tier: "MT LC",
+	},
+	bounsweet: {
+		tier: "MT LC",
+	},
+	steenee: {
+		tier: "MT NFE",
+	},
+	tsareena: {
+		tier: "MT OU",
+	},
+	comfey: {
+		tier: "MT OU",
+	},
+	passimian: {
+		tier: "MT OU",
+	},
+	pyukumuku: {
+		tier: "MT OU",
+	},
+	typenull: {
+		tier: "MT NFE",
+	},
+	silvally: {
+		tier: "MT OU",
+	},
+	silvallybug: {
+		tier: "MT OU",
+	},
+	silvallydark: {
+		tier: "MT OU",
+	},
+	silvallydragon: {
+		tier: "MT OU",
+	},
+	silvallyelectric: {
+		tier: "MT OU",
+	},
+	silvallyfairy: {
+		tier: "MT OU",
+	},
+	silvallyfighting: {
+		tier: "MT OU",
+	},
+	silvallyfire: {
+		tier: "MT OU",
+	},
+	silvallyflying: {
+		tier: "MT OU",
+	},
+	silvallyghost: {
+		tier: "MT OU",
+	},
+	silvallygrass: {
+		tier: "MT OU",
+	},
+	silvallyground: {
+		tier: "MT OU",
+	},
+	silvallyice: {
+		tier: "MT OU",
+	},
+	silvallypoison: {
+		tier: "MT OU",
+	},
+	silvallypsychic: {
+		tier: "MT OU",
+	},
+	silvallyrock: {
+		tier: "MT OU",
+	},
+	silvallysteel: {
+		tier: "MT OU",
+	},
+	silvallywater: {
+		tier: "MT OU",
+	},
+	komala: {
+		tier: "MT UU",
+	},
+	cosmog: {
+		tier: "MT LC",
+	},
+	cosmoem: {
+		tier: "MT NFE",
+	},
+	xurkitree: {
+		tier: "MT OU",
+	},
+	necrozma: {
+		tier: "MT OU",
+	},
+	meltan: {
+		tier: "MT OU",
+	},
+	melmetal: {
+		tier: "MT OU",
+	},
+	grookey: {
+		tier: "MT LC",
+	},
+	thwackey: {
+		tier: "MT NFE",
+	},
+	rillaboom: {
+		tier: "MT OU",
+	},
+	scorbunny: {
+		tier: "MT LC",
+	},
+	raboot: {
+		tier: "MT NFE",
+	},
+	cinderace: {
+		tier: "MT Uber",
+	},
+	sobble: {
+		tier: "MT LC",
+	},
+	drizzile: {
+		tier: "MT NFE",
+	},
+	inteleon: {
+		tier: "MT OU",
+	},
+	skwovet: {
+		tier: "MT LC",
+	},
+	greedent: {
+		tier: "MT OU",
+	},
+	rookidee: {
+		tier: "MT LC",
+	},
+	corvisquire: {
+		tier: "MT NFE",
+	},
+	blipbug: {
+		tier: "MT LC",
+	},
+	nickit: {
+		tier: "MT LC",
+	},
+	thievul: {
+		tier: "MT OU",
+	},
+	gossifleur: {
+		tier: "MT LC",
+	},
+	eldegoss: {
+		tier: "MT OU",
+	},
+	wooloo: {
+		tier: "MT LC",
+	},
+	dubwool: {
+		tier: "MT OU",
+	},
+	yamper: {
+		tier: "MT LC",
+	},
+	boltund: {
+		tier: "MT OU",
+	},
+	rolycoly: {
+		tier: "MT LC",
+	},
+	silicobra: {
+		tier: "MT LC",
+	},
+	sandaconda: {
+		tier: "MT OU",
+	},
+	arrokuda: {
+		tier: "MT LC",
+	},
+	barraskewda: {
+		tier: "MT OU",
+	},
+	clobbopus: {
+		tier: "MT LC",
+	},
+	grapploct: {
+		tier: "MT OU",
+	},
+	sinistea: {
+		tier: "MT LC",
+	},
+	polteageist: {
+		tier: "MT OU",
+	},
+	milcery: {
+		tier: "MT LC",
+	},
+	alcremie: {
+		tier: "MT OU",
+	},
+	falinks: {
+		tier: "MT OU",
+	},
+	pincurchin: {
+		tier: "MT OU",
+	},
+	stonjourner: {
+		tier: "MT OU",
+	},
+	eiscue: {
+		tier: "MT OU",
+	},
+	cufant: {
+		tier: "MT LC",
+	},
+	copperajah: {
+		tier: "MT OU",
+	},
+	zacian: {
+		tier: "MT Uber",
+	},
+	zamazenta: {
+		tier: "MT Uber",
+	},
+	kubfu: {
+		tier: "MT NFE",
+	},
+	regieleki: {
+		tier: "MT OU",
+	},
+	regidrago: {
+		tier: "MT OU",
+	},
+	glastrier: {
+		tier: "MT OU",
+	},
+	spectrier: {
+		tier: "MT Uber",
+	},
+};
