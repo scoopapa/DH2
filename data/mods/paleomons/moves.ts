c68f4594ba2d6b38aa33c701a7ab566b77d83624
Albatross <83423061+alba-tr0ss@users.noreply.github.com>
2022-01-26 22:37:06 +0200

    Scorched Pebbles #2
    - Minor fix to Scorched Pebbles
    - Added descriptions + short descriptions to all moves (not include secret power, nature power, and camouflage, because fuck those moves)

--- data/mods/paleomons/moves.ts
+++ data/mods/paleomons/moves.ts
@@ -5,6 +5,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		basePower: 75,
 		category: "Physical",
 		name: "Terror Soar",
+		desc: "Has a 30% chance to make the target flinch.",
 		shortDesc: "30% chance to make the target flinch.",
 		pp: 20,
 		priority: 0,
@@ -24,6 +25,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		basePower: 80,
 		category: "Physical",
 		name: "Venom Drain",
+		desc: "The user recovers 1/2 the HP lost by the target, rounded half up. If Big Root is held by the user, the HP recovered is 1.3x normal, rounded half down.",
 		shortDesc: "User recovers 50% of damage dealt.",
 		pp: 10,
 		priority: 0,
@@ -41,6 +43,8 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		basePower: 0,
 		category: "Status",
 		name: "Tarpit",
+		desc: "For 5 turns, the terrain becomes a Tar Pit. During the effect, the power of Poison-type attacks used by grounded Pokemon is multiplied by 1.3 and all Pokemon are under the effects of Powder. Camouflage transforms the user into a Poison type, Nature Power becomes Sludge Bomb, and Secret Power has a 30% chance to cause poison. Fails if the current terrain is Tar Pit.",
+		shortDesc: "5 turns. Grounded: +Poison power, +Powder.",
 		pp: 10,
 		priority: 0,
 		flags: {nonsky: 1},
@@ -99,6 +103,8 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		basePower: 0,
 		category: "Status",
 		name: "Scorched Pebbles",
+		desc: "Sets up a hazard on the opposing side of the field, applying Tar Shot to opposing Pokemon that switch in, unless it is a Flying- or Fire-type Pokemon or has the Levitate Ability. Fails if the effect is already active on the opposing side. Can be removed from the opposing side if any opposing Pokemon uses Rapid Spin or Defog successfully, or is hit by Defog.",
+		shortDesc: "Applies Tar Shot upon switchin",
 		pp: 20,
 		priority: 0,
 		flags: {reflectable: 1},
@@ -111,15 +117,15 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 				if (pokemon.hasType('Flying') || pokemon.hasType('Fire') ) return;
 				if (!pokemon.isGrounded()) return;
 				if (pokemon.hasItem('heavydutyboots')) return;
-				this.add('-message' + pokemon + " was burned by the scorched pebbles!");
+				this.add('-message' + pokemon.name + " was burned by the scorched pebbles!");
 				pokemon.addVolatile('tarshot');
 				},
 		},
 		secondary: null,
 		target: "foeSide",
 		type: "Fire",
-		zMove: {boost: {spe: 1}},
-		contestType: "Tough",
+		zMove: {boost: {atk: 1}},
+		contestType: "Cool",
 	},
 
 	//
@@ -134,6 +140,8 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 
 	acidarmor: {
 		inherit: true,
+		desc: "Raises the user's Defense by 2 stages. If the terrain is Tar Pit, the user's Defense is instead increased by 3 stages.",
+		shortDesc: "Raises the user's Defense by 2.",
 		onHit(target) {
 			if(this.field.isTerrain('tarpit')) {
 				if (target.isGrounded() && !target.isSemiInvulnerable() &&!target.hasItem('heavydutyboots')) {
