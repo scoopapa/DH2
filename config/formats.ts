ab2d9971bb936dd3363c240b110b08050276f4b3
ViZarSmogon <vizarsmogon@gmail.com>
2022-02-05 16:01:50 +0100

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1186,7 +1186,11 @@ export const Formats: FormatList = [
 			let speciesTable = {};
 			for (const set of team) {
 				let template = this.dex.getSpecies(set.species);
-				if ( template.tier !== 'MV') {
+				/*if (speciesTable[template.name]) {
+					return ["You are limited to one of each Pokémon by Species Clause (except for different Rotom formes). ", "You have more than one " + template.baseSpecies + "."];
+				}*/
+				speciesTable[template.name] = true;
+				if (template.tier !== 'MV') {
 					return [set.species + ' is not usable in Multiverse.'];
 				}
 			}
