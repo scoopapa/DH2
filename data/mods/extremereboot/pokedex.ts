20663aa65237d40344b47c311f44133d6d3e569f
Anders Hoffman <pccompositor@aim.com>
2022-02-20 21:13:06 -0600

    extreme ribbit
    part too

---
+++ data/mods/extremereboot/pokedex.ts
@@ -0,0 +1,14 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	extremeribbit: {
+		num: 1001,
+		name: "Extreme Ribbit",
+		types: ["Spring"],
+		gender: "N",
+		baseStats: {hp: 50, atk: 50, def: 50, spa: 50, spd: 50, spe: 50},
+		abilities: {0: "No Ability"},
+		heightm: 0.3,
+		weightkg: 4.5,
+		color: "Gray",
+		eggGroups: ["Undiscovered"],
+	},
+};
