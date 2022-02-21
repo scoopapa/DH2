aea7b51ee81c2b36e0ff2004bf7736f3c3dadf7a
Anders Hoffman <pccompositor@aim.com>
2022-02-20 20:08:38 -0600

    extreme reboot descriptions and more

--- data/mods/extremereboot/abilities.ts
+++ data/mods/extremereboot/abilities.ts
@@ -1,387 +1,464 @@
 export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
-	// Not Implemented
+	// Not Fully Implemented
 	absorptive: {
 		num: 1001,
 		name: "Absorptive",
+		desc: "Draining moves restore 1.5x more HP than normal",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	acrid: {
 		num: 1002,
 		name: "Acrid",
+		desc: "Lowers an opponent's speed in switch-in (along the lines of Intimidate)",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	alchemy: {
 		num: 1003,
 		name: "Alchemy",
+		desc: "When this pokemon attacks a Poisoned pokemon, it does a random effect from the list. (Replaces Poison with Fear, Curse, or Sleep; Inflicts the target with the Taunt, Torment, or Encore effect; Choosing 2 stats and lowering or raising each one by 1.)",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	allseeingeye: {
 		num: 1004,
 		name: "All-Seeing Eye",
+		desc: "Bypasses accuracy checks, but takes 15% more damage from attacks.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	almsgiver: {
 		num: 1005,
 		name: "Almsgiver",
+		desc: "Upon switching out, if the incoming ally is not holding an item, they will recieve a copy of this Pokémon's item.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	amplify: {
 		num: 1006,
 		name: "Amplify",
+		desc: "Increases power of pulse and sound moves by 1.3x.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	arbiter: {
 		num: 1007,
 		name: "Arbiter",
+		desc: "Extends the duration of Rules Rewrite by 2 turns when used by this pokemon.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	ataraxia: {
 		num: 1008,
 		name: "Ataraxia",
+		desc: "Immune to moves' secondary effects",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	berserker: {
 		num: 1009,
 		name: "Berserker",
+		desc: "This pokemon's attack is raised by 1 if it falls below 50% HP.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	bigbellied: {
 		num: 1010,
 		name: "Big Bellied",
+		desc: "If rice feild is active when this pokemon switches in, it instatnly get's the healing without waiting for a countdown. Immune to rice feild damage.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	bingchilling: {
 		num: 1011,
 		name: "Bing Chilling",
+		desc: "This Pokémon's Typless moves become Winter-type and gain a 1.2x. power boost. Existing Winter-type moves also get the power boost.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	bipolar: {
 		num: 1012,
 		name: "Bipolar",
+		desc: "Turns Storm-type moves into Serenity-type moves and Serenity-type moves into Storm-type moves, and boosts them by x1.2.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	blessed: {
 		num: 1013,
 		name: "Blessed",
+		desc: "This Pokemon cannot be cursed, and is considered to have Serenity type as its secondary type.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	blightboost: {
 		num: 1014,
 		name: "Blight Boost",
+		desc: "While this Pokemon is cursed, the power of its special attacks is multiplied by 1.5 and takes no HP loss from it.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	blindrage: {
 		num: 1015,
 		name: "Blind Rage",
+		desc: "Hustle",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	blossom: {
 		num: 1016,
 		name: "Blossom",
+		desc: "The first time this pokemon uses a damaging Spring move, its attacking stats will be multiplied by 1.5x when using his next Spring moves.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	burningrage: {
 		num: 1017,
 		name: "Burning Rage",
+		desc: "Whenever this Pokemon has Sunburn, its highest attacking stat gains a 1.2x boost.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	burnout: {
 		num: 1018,
 		name: "Burnout",
+		desc: "This Pokemon's moves that lower its stats have 1.3x power, but the amount the stat is lowered is doubled.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	celestial: {
 		num: 1019,
 		name: "Celestial",
+		desc: "Status conditions are cured after 2 active turns. 1 for sleep.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	checkmate: {
 		num: 1020,
 		name: "Checkmate",
+		desc: "If the enemy has 33% health or less, it is trapped and cannot escape.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	chill: {
 		num: 1021,
 		name: "Chill",
+		desc: "Pokemon making contact with this pokemon have a 30% chance to be inflicted with chill.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	climaticchange: {
 		num: 1022,
 		name: "Climatic Change",
+		desc: "Every Season type attack became the next one.(Spring become summer,summer become autumn,autumn become winter and winter become spring)",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	contagious: {
 		num: 1023,
 		name: "Contagious",
+		desc: "Contact moves used against this pokemon have a 25% chance to poison the one who made contact.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	contrary: {
 		num: 1024,
 		name: "Contrary",
+		desc: "No Changes",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	conversion: {
 		num: 1025,
 		name: "Conversion",
+		desc: "This pokemon's typeless moves change to match its primary type and deal 1.2x damage.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	counterswirl: {
 		num: 1026,
 		name: "Counterswirl",
+		desc: "This Pokemon is immune to Storm attacks, and if it were to be hit by one, the attacker loses 1/8 of their max health.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	courageous: {
 		num: 1027,
 		name: "Courageous",
+		desc: "This Pokemon is immume to fear, and its Atk and SpA cannot be lowered by other Pokemon. Gaining this Ability while under fear cures it.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	dataupgrade: {
 		num: 1028,
 		name: "Data Upgrade",
+		desc: "When hit by a Special move, raises SpD by 1, but lowers Def by 1. When hit by a Physical move, raises Def by 1, but lowers SpD by 1.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	decay: {
 		num: 1029,
 		name: "Decay",
+		desc: "The first time this pokemon uses a damaging Autumn move, its attacking stats will be multiplied by 1.5x when using his next Autumn moves.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	equivalentexchange: {
 		num: 1030,
 		name: "Equivalent Exchange",
+		desc: "When this Pokémon's Attack is modified, its Special Attack is modified in the opposite way, and vice versa. The same is true for its Defense and Special Defense.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	farreach: {
 		num: 1031,
 		name: "Far Reach",
+		desc: "This pokemon's non-contact moves to 1.2x damage.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	fluffyfloat: {
 		num: 1032,
 		name: "Fluffy Float",
+		desc: "If Cotton Field is active, this Pokemon's Defense and Speed are boosted by x1.5. This Pokemon takes no damage from Cotton Field.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	glide: {
 		num: 1033,
 		name: "Glide",
+		desc: "This pokemon has it's speed raised by 1 when a Pokemon on the feild uses a sky move.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	hotfeet: {
 		num: 1034,
 		name: "Hot Feet",
+		desc: "If this Pokemon is sunburned, its Speed is multiplied by 2",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	ignorance: {
 		num: 1035,
 		name: "Ignorance",
+		desc: "All Pokemon on the field ignore each others' stat changes.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	infinitescaling: {
 		num: 1036,
 		name: "Infinite Scaling",
+		desc: "The damage of this Pokémon increases infinitely. (Turn 1: 0.8x, Turn 2: 0.9x, Turn 3: 1x, Turn 4: 1.1x, etc.) Resets upon switching out.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	internetrage: {
 		num: 1037,
 		name: "Internet Rage",
+		desc: "This Pokemon's Manmade-type moves become Storm-type, and Storm-type moves become Manmade-type.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	jacko: {
 		num: 1038,
 		name: "Jack-o'",
+		desc: "This Pokemon is immune to the effects of Pumpkin Field. If Pumpkin Field is set on the field, the power of this Pokemon's physical and special attacks is multiplied by 1.3.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	lesspell: {
 		num: 1039,
 		name: "Lesspell",
+		desc: "Lowers opposing Pokemon Special Attack by 1 stage when switching in.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	linger: {
 		num: 1040,
 		name: "Linger",
+		desc: "The first time this pokemon uses a damaging Winter move, its attacking stats will be multiplied by 1.5x when using his next Winter moves.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	megatonburst: {
 		num: 1041,
 		name: "Megaton Burst",
+		desc: "This Pokemon uses a 60 BP physical Earth-type move (uses the user's Attack stat) after using a Sound-based move.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	metalcoat: {
 		num: 1042,
 		name: "Metal Coat",
+		desc: "This pokemon is immune to moves that are 60 bp or lower.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	modernadaptation: {
 		num: 1043,
 		name: "Modern Adaptation",
+		desc: "Transform any Folklore type move used by the pokemon into Manmade type.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	nanobarrier: {
 		num: 1044,
 		name: "Nanobarrier",
+		desc: "This pokemon receives 3/4 damage from neutrally effective attacks.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	necromancer: {
 		num: 1045,
 		name: "Necromancer",
+		desc: "This Pokemon's attacking stat is multiplied by 1.5 while using a Folklore-type attack.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	nocturnal: {
 		num: 1046,
 		name: "Nocturnal",
+		desc: "Immune to Night-type moves, raises Spe by  1 if hit by one",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	nonbeliever: {
 		num: 1047,
 		name: "Non-Believer",
+		desc: "This Pokemon is immune to Folklore-type moves (yes i'm planning to give this another effect like flash fire and -absorb clones)",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	petalbody: {
 		num: 1048,
 		name: "Petal Body",
+		desc: "If Rose Field is active, this Pokemon restores 1/8 of its maximum HP, rounded down, at the end of each turn.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	prudentplow: {
 		num: 1049,
 		name: "Prudent Plow",
+		desc: "This Pokemon's Autumn-type moves become two-hit moves, including Status moves. The second hit has its damage/HP recovery quartered. Does not affect moves that are already multi-hit.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	ragingsea: {
 		num: 1050,
 		name: "Raging Sea",
+		desc: "Increases the power of Sea-type moves by up to 40% the lower its HP gets.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	rainbringer: {
 		num: 1051,
 		name: "Rainbringer",
+		desc: "Sets weather to Rain for the next 5 turns. Spring and Sea deals 1.3x damage.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	reality: {
 		num: 1052,
 		name: "Reality",
+		desc: "Immune to Folklore-type, boosts its Attack by 1 stage if hit by Folklore-type moves.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	reaper: {
 		num: 1053,
 		name: "Reaper",
+		desc: "This Pokemon's Autumn-type moves do 1.2x damage and restore the user 50% of the damage dealt.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	regenerator: {
 		num: 1054,
 		name: "Regenerator",
+		desc: "This pokemon has 1/4 of its max hp, rounded down, restored when it switches out",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	rigormortis: {
 		num: 1055,
 		name: "Rigor Mortis",
+		desc: "If the user takes a physical hit, it gains +1 defense stage.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	sacrificer: {
 		num: 1056,
 		name: "Sacrificer",
+		desc: "This Pokemon loses 1/3 of its max HP when it switches out. Next Pokemon gets those HP.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	scavenge: {
 		num: 1057,
 		name: "Scavenge",
+		desc: "This Pokemon restores 1/3 of its max health if another Pokemon on the field faints.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	shatter: {
 		num: 1058,
 		name: "Shatter",
+		desc: "This pokemon's attacks are guaranteed to be critical hits if the opponent is statused.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	shine: {
 		num: 1059,
 		name: "Shine",
+		desc: "The first time this pokemon uses a damaging Summer move, its attacking stats will be multiplied by 1.5x when using his next Summer moves.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	smite: {
 		num: 1060,
 		name: "Smite",
+		desc: "Moves' power is boosted by 1.3x if the target is below half health",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	snowbringer: {
 		num: 1061,
 		name: "Snowbringer",
+		desc: "Sets weather to Snow for the next 5 turns. Winter deals 1.5x damage, Summer deals 0.5x",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	spectralshifter: {
 		num: 1062,
 		name: "Spectral Shifter",
+		desc: "While this Pokemon is active, opposing Pokemons' stat raises will be lowers instead, and vice versa.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	steadfast: {
 		num: 1063,
 		name: "Steadfast",
+		desc: "Inverts Sunburn and Chill effects on the user.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	stoneskin: {
 		num: 1064,
 		name: "Stone Skin",
+		desc: "User takes 25% less damage from contact moves.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	stormwatch: {
 		num: 1065,
 		name: "Storm Watch",
+		desc: "Immunity to Storm, being targeted by a Storm move will boost both defenses by 1 stage.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	strategicretreat: {
 		num: 1066,
 		name: "Strategic Retreat",
+		desc: "emergency exit but activates in 25% of max HP and heals 25% of max HP on switch",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	subrosa: {
 		num: 1067,
 		name: "Sub Rosa",
+		desc: "This Pokemon is unharmed by Rose Field. This Pokemon restores 1/8 of its maximum HP, rounded down, if any Pokemon (including itself) switches into Rose Field.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	sunbringer: {
 		num: 1068,
 		name: "Sunbringer",
+		desc: "Sets weather to Sun for the next 5 turns. Summer deals 1.5x damage, Winter deals 0.5x",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	supersapience: {
 		num: 1069,
 		name: "Super Sapience",
+		desc: "Boosts moves that start with S by 1.5x. Moves that start with any other letter receive a 0.5x  decrease in power.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	thickheaded: {
 		num: 1070,
 		name: "Thick Headed",
+		desc: "When this pokemon makes contact with the foe, nullifies their type-based immunities.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	thickskin: {
 		num: 1071,
 		name: "Thick Skin",
+		desc: "This Pokemon is immune to the negative effects of Sunburn, and its defenses are 1.5x if it is Sunburned.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	toughclaws: {
 		num: 1072,
 		name: "Tough Claws",
+		desc: "No Changes",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	transcription: {
 		num: 1073,
 		name: "Transcription",
+		desc: "Turns the Pokémon's first type to Folklore and the moves of the type into Folklore. They also receive a 1.1 boost in power.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	tropicalspirit: {
 		num: 1074,
 		name: "Tropical Spirit",
+		desc: "This Pokemon's attacks are critical hits if the target is sunburnt. Winter-type moves against this Pokemon deal half damage.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	unstable: {
 		num: 1075,
 		name: "Unstable",
+		desc: "The pokemon is immune to serenity move.But the fear status is doubled if applied on the pokemon with the ability.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	wavecrasher: {
 		num: 1076,
 		name: "Wave Crasher",
+		desc: "After another Pokemon uses a Sea-type move, the user uses the same move. Sea-type moves against this Pokemon deal half damage. Other Pokemon cannot force the user to switch out.",
 	},
-	// Not Implemented
+	// Not Fully Implemented
 	wintercoat: {
 		num: 1077,
 		name: "Winter Coat",
+		desc: "This Pokemon is immune to Winter-type moves and is immune to chill. Gaining this Ability while under chill cures it.",
 	},
 };
--- data/mods/extremereboot/items.ts
+++ data/mods/extremereboot/items.ts
@@ -2,6 +2,7 @@ export const Items: {[itemid: string]: ItemData} = {
 	bloodvial: {
 		name: "Blood Vial",
 		num: 1001,
+		desc: "If this Pokemon is at full HP, it survives one hit with at least 1 HP and Poisons the attacker.",
 		onDamage(damage, target, source, effect) {
 			if (target.hp === target.maxhp && damage >= target.hp && effect && effect.effectType === 'Move') {
 				if (target.useItem()) {
@@ -17,6 +18,7 @@ export const Items: {[itemid: string]: ItemData} = {
 	calmingsalt: {
 		name: "Calming Salt",
 		num: 1002,
+		desc: "Clear any status. Single use.",
 		onAfterSetStatus(status, pokemon) {
 			pokemon.eatItem();
 		},
@@ -34,16 +36,19 @@ export const Items: {[itemid: string]: ItemData} = {
 	cursedorb: {
 		name: "Cursed Orb",
 		num: 1003,
+		desc: "Does nothing. If this item is disturbed or passed on to a different holder, said holder will lose 1/8th of their max HP per turn.",
 		gen: 8,
 	},
 	emblematicscarf: {
 		name: "Emblematic Scarf",
 		num: 1004,
+		desc: "Holder's Speed is 1.5x, but it can only select moves that match the holder's primary type (including status moves).",
 		gen: 8,
 	},
 	enigmaticshield: {
 		name: "Enigmatic Shield",
 		num: 1005,
+		desc: "Reduces damage taken from Supereffective attakcs by 25%.",
 		onSourceModifyDamage(damage, source, target, move) {
 			if (target.getMoveHitData(move).typeMod > 0) {
 				return this.chainModify(0.75);
@@ -54,6 +59,7 @@ export const Items: {[itemid: string]: ItemData} = {
 	fellscythe: {
 		name: "Fell Scythe",
 		num: 1006,
+		desc: "The holder's moves have 1.3x power, but it loses 1/10 of its max HP after successfully damaging an opponent with a move. Instead restores 1/16 max HP if the holder is an Autumn type.",
 		onModifyDamage(damage, source, target, move) {
 			if (!(pokemon.hasType("Autumn"))) return this.chainModify([0x14CC, 0x1000]);
 		},
@@ -82,11 +88,13 @@ export const Items: {[itemid: string]: ItemData} = {
 			}
 		},
 		num: 1007,
+		desc: "This Pokemon's sound moves become Serenity type.",
 		gen: 8,
 	},
 	lifegem: {
 		name: "Life Gem",
 		num: 1008,
+		desc: "Recover 10% of your max HP when using an attacking move.",
 		onAfterMoveSecondarySelf(source, target, move) {
 			if (source && source !== target && move && move.category !== 'Status') {
 				this.heal(source.baseMaxhp / 10);
@@ -97,6 +105,7 @@ export const Items: {[itemid: string]: ItemData} = {
 	maiddress: {
 		name: "Maid Dress",
 		num: 1009,
+		desc: "Increases the power of all moves used by active pokemon by 1.3x",
 		onModifyDamage(damage, source, target, move) {
 			return this.chainModify([0x14CC, 0x1000]);
 		},
@@ -108,6 +117,7 @@ export const Items: {[itemid: string]: ItemData} = {
 	metalmorph: {
 		name: "Metalmorph",
 		num: 1010,
+		desc: "Grants the holder the Shield Dust effect. If the holder is Manmade, they also gain 1.2x Attack.",
 		onModifySecondaries(secondaries) {
 			return secondaries.filter(effect => !!(effect.self || effect.dustproof));
 		},
@@ -120,6 +130,7 @@ export const Items: {[itemid: string]: ItemData} = {
 	pokemonstandard: {
 		name: "Pokemon Standard",
 		num: 1011,
+		desc: "This Pokémon's STAB moves are boosted by 25%.",
 		onModifyDamage(damage, source, target, move) {
 			if (source.hasType(move.type)) return this.chainModify(1.25);
 		},
@@ -127,12 +138,14 @@ export const Items: {[itemid: string]: ItemData} = {
 	},
 	powerstone: {
 		name: "Power Stone",
+		desc: "Boosts the power of the first move in the user's moveset by 1.3x. All of its other moves deal 0.9x damage.",
 		num: 1012,
 		gen: 8,
 	},
 	seasonsgem: {
 		name: "Seasons Gem",
 		num: 1013,
+		desc: "Your first Spring, Summer, Autumn or Winter move will have its power multiplied by x1.5. Consumed when used.",
 		isGem: true,
 		onSourceTryPrimaryHit(target, source, move) {
 			const seasons = ["Spring", "Summer", "Autumn", "Winter"];
@@ -146,6 +159,7 @@ export const Items: {[itemid: string]: ItemData} = {
 	trickyhourglass: {//mostly coded in the field conditions themselves
 		name: "Tricky Hourglass",
 		num: 1014,
+		desc: "Holder's moves and effects that last 4 or 5 turns last 8 turns instead, except Rules Rewrite, which lasts 2 turns.",
 		gen: 8,
 	},
 };
