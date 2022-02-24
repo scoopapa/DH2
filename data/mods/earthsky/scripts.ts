c5b95f96802a6300834eb3b62a7919990cd95b47
En Passant <enpassant4264@gmail.com>
2022-02-23 21:38:02 -0700

    Earth & Sky script battle editing
    oh shit it passed the previous crash point. Too bad there's another one

--- data/mods/earthsky/scripts.ts
+++ data/mods/earthsky/scripts.ts
@@ -5,6 +5,7 @@
 	export type Other = "Unreleased" | "Illegal" | "ES Uber" | "ES OU" | "ES NFE" | "ES LC" | "CAP" | "CAP NFE" | "CAP LC";
 };*/
 import {Pokemon} from '../../../sim/pokemon';
+import {Battle} from '../../../sim/battle';
 
 export const Scripts: ModdedBattleScriptsData = {
 	teambuilderConfig: {
