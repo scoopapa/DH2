66e4b603681144021a57e0d7fdd6224e6cc69dec
ViZarSmogon <vizarsmogon@gmail.com>
2022-03-06 09:10:07 +0100

    h

--- data/mods/multiverse/items.ts
+++ data/mods/multiverse/items.ts
@@ -25,15 +25,6 @@ export const Items: {[itemid: string]: ItemData} = {
 			if (source.baseSpecies.baseSpecies === 'Greninja') return false;
 			return true;
 		},
-		/*onChargeMove (pokemon, target, move) {
-			if (move.id !== 'Dig') return;
-			if (pokemon.useItem()) {
-				this.debug('smoke bomb - remove charge turn for ' + move.id);
-				this.attrLastMove('[still]');
-				this.addMove('-anim', pokemon, move.name, target);
-				return false; // skip charge turn
-			}
-		},*/
 		num: -2,
 		gen: 8,
 		shortDesc: "When held by Greninja, Dig: +20 BP & 1 turn. Single Use.",
