ca821b373456bb04d2a57df6213d605ad0102dab
ViZarSmogon <vizarsmogon@gmail.com>
2022-03-12 09:21:07 +0100

    Update pokedex.ts

--- data/mods/alternatium/pokedex.ts
+++ data/mods/alternatium/pokedex.ts
@@ -1297,6 +1297,8 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		heightm: 0.8,
 		weightkg: 18,
 		eggGroups: ["Water 1", "Flying"],
+		otherFormes: null,
+		formeOrder: null,
 	},
 	cramorantswimmer: {
 		num: 845,
@@ -1317,6 +1319,10 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		heightm: 0.8,
 		weightkg: 18,
 		eggGroups: ["Water 1", "Flying"],
+		baseSpecies: null,
+		forme: null,
+		requiredAbility: null,
+		battleOnly: null,
 	},
 	eiscue: {
 		num: 875,
