803d75a13a42ad69772164d9eb60874960f3292e
ViZarSmogon <vizarsmogon@gmail.com>
2022-03-15 19:03:19 +0100

    Update abilities.ts

--- data/mods/feuu/abilities.ts
+++ data/mods/feuu/abilities.ts
@@ -5197,10 +5197,11 @@ lifedrain: {
 					stats.push(statPlus);
 				}
 			}
-			//let randomStat: BoostName | undefined = stats.length ? this.sample(stats) : undefined;
+			let randomStat: BoostName | undefined = stats.length ? this.sample(stats) : undefined;
 			if (statsLowered) {
 				this.add('-ability', target, 'Subvergent');
-				this.boost({atk: 2, [statPlus]: 2}, target, target, null, true);
+				this.boost({atk: 2}, target, target, null, true);
+				boost[randomStat] = 2;
 			}
 		},
 		name: "Subvergent",
