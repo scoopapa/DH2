8b2012c22f79693b5d9eeebf8885f9a1a590d9d3
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2022-03-20 18:21:41 -0400

    Update formats-data.ts

--- data/mods/joltemons/formats-data.ts
+++ data/mods/joltemons/formats-data.ts
@@ -172,7 +172,7 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		doublesTier: "DOU",
 	},
 	lucariomega: {
-		tier: "Uber",
+		tier: "OU",
 		doublesTier: "DOU",
 	},
 	lugia: {
