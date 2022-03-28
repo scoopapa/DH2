ae643d393cd0ab33b7127d38a35851a78c07151f
meezypeezy <72401075+meezypeezy@users.noreply.github.com>
2022-03-28 18:48:59 -0400

    Create formats-data.ts

---
+++ data/mods/roulettemonsthesolomod/formats-data.ts
@@ -0,0 +1,58 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	spinmadillo: {
+		tier: "RTS",
+	},
+	coyoctric: {
+		tier: "RTS",
+	},
+	fierhog: {
+		tier: "RTS",
+	},
+	elatuff: {
+		tier: "RTS",
+	},
+	glasyte: {
+		tier: "RTS",
+	},
+	bisong: {
+		tier: "RTS",
+	},
+	megalo: {
+		tier: "RTS",
+	},
+	oysteat: {
+		tier: "RTS",
+	},
+	ponymph: {
+		tier: "RTS",
+	},
+	hypepion: {
+		tier: "RTS",
+	},
+	chickola: {
+		tier: "RTS",
+	},
+	skelehawk: {
+		tier: "RTS",
+	},
+	catetar: {
+		tier: "RTS",
+	},
+	blastquito: {
+		tier: "RTS",
+	},
+	hawkward: {
+		tier: "RTS",
+	},
+	pandaid: {
+		tier: "RTS",
+	},
+	autoad: {
+		tier: "RTS",
+	},
+	skelephin: {
+		tier: "RTS",
+	},
+	doomossum: {
+		tier: "RTS",
+	},
