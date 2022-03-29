acfda4f9589e644a7413355f0bf408f36680fdd5
JosJet <89114731+JosJet@users.noreply.github.com>
2022-03-29 18:54:29 -0400

    Update moves.ts

--- data/mods/gen2crystalseviiislands/moves.ts
+++ data/mods/gen2crystalseviiislands/moves.ts
@@ -69,4 +69,92 @@ export const Moves: {[moveid: string]: MoveData} = {
 		target: "normal",
 		type: "Dark",
 	},
+	parry: {
+        num: -100,
+        accuracy: true,
+        basePower: 0,
+        category: "Status",
+        pp: 5,
+        priority: -1,
+        flags: {contact: 1, protect: 1},
+        volatileStatus: 'parry',
+        beforeTurnCallback(pokemon) {
+            pokemon.addVolatile('parry');
+        },
+        beforeMoveCallback(pokemon) {
+            if (pokemon.volatiles['parry'] && pokemon.volatiles['parry'].untouched) {
+                return false;
+            }
+            else if (pokemon.volatiles['parry'] && !pokemon.volatiles['parry'].untouched) {
+                this.add('-message', `${pokemon.name} was unable to parry...`);
+                return true;
+            }
+        },
+        condition: {
+            duration: 1,
+            onStart(pokemon) {
+                this.add('-message', `${pokemon.name} is attempting to parry!`);
+            },
+            onHit(pokemon, source, move) {
+                if (move.category !== 'Status') {
+                    pokemon.volatiles['parry'].untouched = true;
+                }
+            },
+        },
+		  onHit(pokemon) {
+            if (pokemon.volatiles['parry'] && pokemon.volatiles['parry'].untouched) {
+                const NoParry = ['assist', 'beakblast', 'belch', 'bide', 'celebrate', 'chatter', 'copycat', 'dynamaxcannon', 'focuspunch', 'mefirst', 'metronome', 'mimic', 'mirrormove', 'naturepower', 'shelltrap', 'sketch', 'uproar', 'sketch', 'protect', 'detect'];
+                const moves = [];
+                for (const moveSlot of pokemon.moveSlots) {
+                    const move = moveSlot.id;
+                    if (move && !NoParry.includes(move) && !this.dex.getMove(move).flags['charge']) {
+                        moves.push(move);
+                    }
+                }
+                let randomMove = '';
+                if (moves.length) randomMove = this.sample(moves);
+                if (!randomMove) return false;
+                this.useMove(randomMove, pokemon);
+                if (randomMove == 'sleeptalk') {
+                    const ppDeducted = pokemon.deductPP(randomMove, 4);
+                    if (!ppDeducted) return false;
+                }
+                else {
+                    const ppDeducted = pokemon.deductPP(randomMove, 4);
+                    if (!ppDeducted) return false;
+                }
+            }
+        },
+        name: "Parry",
+        desc: "Parry",
+        secondary: null,
+        target: "self",
+        type: "Fighting",
+        contestType: "Cool",
+    },
+	
+	///////
+	
+	spikes: {
+		inherit: true,
+		desc: "Sets up a hazard on the opposing side of the field, causing each opposing Pokemon that switches in to lose 1/8 of their maximum HP, rounded down, unless it is a Flying-type Pokemon. Fails if the effect is already active on the opposing side. Can be removed from the opposing side if any opposing Pokemon uses Rapid Spin successfully.",
+		shortDesc: "Hurts grounded foes on switch-in. Max 1 layer.",
+		condition: {
+			// this is a side condition
+			onStart(side) {
+				if (!this.effectData.layers || this.effectData.layers === 0) {
+					this.add('-sidestart', side, 'Spikes');
+					this.effectData.layers = 1;
+				} else {
+					return false;
+				}
+			},
+			onSwitchIn(pokemon) {
+				if (!pokemon.runImmunity('Ground')) return;
+				const damageAmounts = [0, 3];
+				this.damage(damageAmounts[this.effectData.layers] * pokemon.maxhp / 24);
+			},
+		},
+	},
+	
 };
