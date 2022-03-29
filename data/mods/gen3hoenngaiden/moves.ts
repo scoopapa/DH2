3c9092afcbc569fd26979d635b21529bad289ea3
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2022-03-29 17:50:00 -0400

    Update moves.ts

--- data/mods/gen3hoenngaiden/moves.ts
+++ data/mods/gen3hoenngaiden/moves.ts
@@ -1113,6 +1113,22 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		inherit: true,
 		basePower: 100,
 	},
+	wish: {
+		inherit: true,
+		flags: {heal: 1},
+		slotCondition: 'Wish',
+		condition: {
+			duration: 2,
+			onResidualOrder: 7,
+			onEnd(target) {
+				if (!target.fainted) {
+					const source = this.effectState.source;
+					const damage = this.heal(target.baseMaxhp / 2, target, target);
+					if (damage) this.add('-heal', target, target.getHealth, '[from] move: Wish', '[wisher] ' + source.name);
+				}
+			},
+		},
+	},
 	//Temporary Multi-Attack Fix
 	multiattack: {
 		accuracy: 100,
