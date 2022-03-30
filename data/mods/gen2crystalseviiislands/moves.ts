a4c118a3a2dab14d670efa423e49c0f3061b544b
JosJet <89114731+JosJet@users.noreply.github.com>
2022-03-30 00:08:43 -0400

    Update moves.ts

--- data/mods/gen2crystalseviiislands/moves.ts
+++ data/mods/gen2crystalseviiislands/moves.ts
@@ -90,7 +90,7 @@ export const Moves: {[moveid: string]: MoveData} = {
             }
             else if (pokemon.volatiles['parry'] && !pokemon.volatiles['parry'].untouched) {
 					 this.add('-message', `${pokemon.name} was unable to parry...`);
-					 pokemon.deductPP(move, 1);
+					 ppDeducted = pokemon.deductPP(move, 1);
                 return true;
             }
         },
