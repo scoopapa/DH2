6c8148f20c253d65c388a6832f9b3e567e3a650d
JosJet <89114731+JosJet@users.noreply.github.com>
2022-03-31 14:02:39 -0400

    Update moves.ts

--- data/mods/gen2crystalseviiislands/moves.ts
+++ data/mods/gen2crystalseviiislands/moves.ts
@@ -397,10 +397,10 @@ export const Moves: {[moveid: string]: MoveData} = {
 				if (move.id === 'twineedle') {
 					move.secondaries = move.secondaries!.filter(p => !p.kingsrock);
 				}
-				if (move.id === 'swagger') {
+				/*if (move.id === 'swagger') {
 					this.debug('sub bypass: swagger');
 					return;
-				}
+				}*/
 				if (move.drain) {
 					this.add('-miss', source);
 					this.hint("In Gen 2, draining moves always miss against Substitute.");
