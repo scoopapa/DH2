2672ede9efae2657cec77a1c3268e45dcca04bd4
JosJet <89114731+JosJet@users.noreply.github.com>
2022-03-31 03:08:22 -0400

    Update scripts.ts

--- data/mods/gen2crystalseviiislands/scripts.ts
+++ data/mods/gen2crystalseviiislands/scripts.ts
@@ -54,10 +54,9 @@ export const Scripts: ModdedBattleScriptsData = {
 	pokemon: {
 		getDamage(pokemon, target, move, suppressMessages) {
 		//Parry damage reduction
-		  let damage = level * 2;
         if (target.volatiles['parry']) {
-            damage = Math.floor(pokemon.maxhp / 2);
-         }
+            damage = Math.floor(damage / 2);
+        }
 		}
 	},
 };
