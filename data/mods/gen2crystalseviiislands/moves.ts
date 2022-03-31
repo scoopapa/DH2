9fb6b2695b74ce3efcdfd264710e7815ad7dd241
JosJet <89114731+JosJet@users.noreply.github.com>
2022-03-31 02:28:51 -0400

    Update moves.ts

--- data/mods/gen2crystalseviiislands/moves.ts
+++ data/mods/gen2crystalseviiislands/moves.ts
@@ -262,10 +262,10 @@ export const Moves: {[moveid: string]: MoveData} = {
 			onAnySetStatus(status, pokemon) {
 				if (status.id === 'slp') {
 					if (pokemon === this.effectData.target) {
-						this.add('-fail', pokemon, 'slp', '[from] Hype Up', '[msg]');
+						this.add('-fail', pokemon, 'slp');
 						this.add('-message', `${pokemon.name} is too hyped to sleep.`);
 					} else {
-						this.add('-fail', pokemon, 'slp', '[from] Hype Up');
+						this.add('-fail', pokemon, 'slp');
 						this.add('-message', `${pokemon.name} is too hyped to sleep.`);
 					}
 					return null;
