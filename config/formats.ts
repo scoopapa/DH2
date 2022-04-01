a13f094338c226a3809594926269fd56e7ca200c
JosJet <89114731+JosJet@users.noreply.github.com>
2022-03-31 20:39:19 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -271,9 +271,10 @@ export const Formats: FormatList = [
 		desc: ["<b>Crystal: Sevii Islands</b>- A Gen 2 pet mod that aims to create new Pokemon, items, and moves for the GSC OU Metagame."],
 		threads: [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/crystal-sevii-islands.3695569/">Crystal: Sevii Islands on Smogon Forums</a>`,
+			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1QL_789vTzxG8An43itUxPonK9ee7ezxH6GCR9dD_JyQ/edit?usp=sharing">Crystal: Sevii Islands spreadsheet</a>`,
 		      ],
 		mod: 'gen2crystalseviiislands',
-		ruleset: ['Standard', 'Data Mod'],
+		ruleset: ['Standard', 'Data Mod', 'Crystal: Sevii Islands Mod'],
 		banlist: ['Uber'],
 	},
 	{
