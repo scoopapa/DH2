24c9946f4f2c3db664749aac963e96392b483722
JosJet <89114731+JosJet@users.noreply.github.com>
2022-04-02 05:17:13 -0400

    Update pokedex.ts

--- data/mods/gen2crystalseviiislands/pokedex.ts
+++ data/mods/gen2crystalseviiislands/pokedex.ts
@@ -27,7 +27,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		weightkg: 131,
 		prevo: "Quagsire",
 	},
-	animon: {
+	animon_: {
 		num: 255,
 		name: "Animon",
 		types: ["Steel"],
