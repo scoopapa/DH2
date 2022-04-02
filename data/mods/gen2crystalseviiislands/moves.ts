99087e0299f851c0f775fe49c621b3f963bba099
JosJet <89114731+JosJet@users.noreply.github.com>
2022-04-01 22:09:12 -0400

    Update moves.ts

--- data/mods/gen2crystalseviiislands/moves.ts
+++ data/mods/gen2crystalseviiislands/moves.ts
@@ -171,9 +171,8 @@ export const Moves: {[moveid: string]: MoveData} = {
 				   }
 			   },
 			  onTryMove(pokemon, source, move) {
- 			  if (!source.hasType('Fire') || !source.runImmunity('Ground') || move.id !== 'rapidspin') {
- 			  		pokemon.trySetStatus('brn', source);
-			  }
+ 			  if (!pokemon.hasType('Fire') || !pokemon.runImmunity('Ground') || move.id !== 'rapidspin') return;
+ 			  		this pokemon.trySetStatus('brn', source);
  			},
 			/*onTryMove(pokemon, source, move) {
 			  if (pokemon.hasType('Fire') || pokemon.runImmunity('Ground') || move.id === 'rapid spin') {
