2e3315e3fdc46203381698b0940f36df3bec0613
ViZarSmogon <81812196+ViZarSmogon@users.noreply.github.com>
2022-04-06 17:02:56 +0200

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -535,8 +535,8 @@ export const Formats: FormatList = [
 		banlist: ['Uber'],
 		unbanlist: [
 			//Abilities
-			'Sand Veil', 'Snow Warning', 'Ice Body', 'Magic Guard', 'Galvanize', 'Overcoat', 'Desolate Land', 'Mold Breaker', 'Gulp Missile', 'Analytic', 'Protean', 
-			'Neutralizing Gas', 'Stakeout', 'Fur Coat', 'Gluttony',
+			/*'Sand Veil', 'Snow Warning', 'RKS System', 'Ice Body', 'Magic Guard', 'Galvanize', 'Overcoat', 'Desolate Land', 'Mold Breaker', 'Gulp Missile', 'Analytic', 'Protean', 
+			'Neutralizing Gas', 'Stakeout', 'Fur Coat', 'Gluttony',*/
 			
 			//Pokémon
 			'Snover', 'Abomasnow', 'Gliscor', 'Frillish', 'Jellicent', 'Tynamo', 'Eelektrik', 'Eelektross', 'Cryogonal', 'Type: Null', 'Silvally', 'Silvally-Bug', 'Silvally-Dark',
