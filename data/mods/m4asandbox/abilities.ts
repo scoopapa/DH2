037f4b22badc38ac11c80b828117965a227307b2
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-04-07 15:59:31 -0600

    Overflow
    This was in conditions.ts before but I feel like it shouldn't be

--- data/mods/m4asandbox/abilities.ts
+++ data/mods/m4asandbox/abilities.ts
@@ -1035,6 +1035,18 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 				source.addVolatile('overflow');
 			}
 		},
+		condition: {
+			duration: 3,
+			onResidualOrder: 1,
+			onStart(pokemon) {
+				pokemon.setType(pokemon.getTypes(true).map(type => type === "Fire" ? "???" : type));
+				this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[from] ability: Overflow');
+			},
+			onEnd(pokemon) {
+				pokemon.setType(pokemon.getTypes(true).map(type => type === "???" ? "Fire" : type));
+				this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[from] ability: Overflow');
+			},
+		},
 		name: "Overflow",
 		rating: 4,
 		num: -6034,
