033cdf5ec6ec2f1a25316550cabc6658749db9b6
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2022-04-12 11:16:51 +0700

    Update scripts.ts

--- data/mods/optimons/scripts.ts
+++ data/mods/optimons/scripts.ts
@@ -1,4 +1,4 @@
-export const Scripts: ModdedBattleScriptsData = {
+export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 	init() {
 		const addNewMoves = (pokemonid: string, moveids: string[]) => {
 			for (const moveid of moveids) {
