015a48e647631d305ef09feb251c2391c5050e3c
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2022-04-13 08:37:00 +0700

    Update pokedex.ts

--- data/mods/optimons/pokedex.ts
+++ data/mods/optimons/pokedex.ts
@@ -1,4 +1,4 @@
-export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+export const Pokedex: {[k: string]: ModdedSpeciesFormatsData} = {
 	inteleon: {
 		inherit: true,
 		types: ["Water", "Normal"],
