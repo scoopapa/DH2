8d064ad3ba756d8a3c1d66f4218e8fe1ebba9282
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2022-04-15 08:13:19 +0700

    Create moves.ts

---
+++ data/mods/plutonium/moves.ts
@@ -0,0 +1,10 @@
+export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
+   init: function() {
+      this.modData('Learnsets', 'poliwrath').learnset.Surging Strikes = ['7L1'];
+      this.modData('Learnsets', 'drapion').learnset.Wicked Blow = ['7L1'];
+      this.modData('Learnsets', 'electivire').learnset.Plasma Fists = ['7L1'];
+      this.modData('Learnsets', 'basculin').learnset.Fishous Rend = ['7L1'];
+      this.modData('Learnsets', 'pangoro').learnset. Wicked Blow = ['7L1'];
+      this.modData('Learnsets', 'grapploct').learnset.Surging Strikes = ['7L1'];
+   },
+};
