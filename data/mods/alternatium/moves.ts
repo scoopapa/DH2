816ed363cad5a1a193d7437f50913a62f55d8a27
ViZarSmogon <81812196+ViZarSmogon@users.noreply.github.com>
2022-04-22 16:49:42 +0200

    Update moves.ts

--- data/mods/alternatium/moves.ts
+++ data/mods/alternatium/moves.ts
@@ -521,7 +521,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 			onHit(source) {
 				if (this.randomChance(1, 1)) {
 					for (const pokemon of source.side.active) {
-						if (!pokemon.item && pokemon.lastItem && this.dex.items.get(pokemon.lastItem).isBerry) {
+						if (!pokemon.item && pokemon.lastItem && this.dex.getItem(pokemon.lastItem).isBerry) {
 							const item = pokemon.lastItem;
 							pokemon.lastItem = '';
 							this.add('-item', pokemon, this.dex.items.get(item), '[from] move: Egg Bomb');
