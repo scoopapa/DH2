c6126a78c08bfbf0c06a074f6320cf7a0dbec09c
ViZarSmogon <81812196+ViZarSmogon@users.noreply.github.com>
2022-04-30 09:18:45 +0200

    Update abilities.ts

--- data/mods/alternatium/abilities.ts
+++ data/mods/alternatium/abilities.ts
@@ -40,7 +40,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		rating: 2.5,
 		num: 259,
 	},
-	rkssystem: {
+	rkssystem: { //Unused code but I'll keep it cause it can be useful in the future
 		onStart(pokemon) {
 			switch (pokemon.species.id) {
 			case 'silvally':
@@ -163,8 +163,8 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 			}
 		},
 		name: "Static Cling",
-		shortDesc: "This Pokemon cannot lose its held item. Contact: Steals opponent's item on contact, if the user has no item.",
-		rating: 0,
+		shortDesc: "User cannot lose its item. Steals opponent's item on contact.",
+		rating: 4,
 		num: 1001,
 	},
 	rarecold: {
