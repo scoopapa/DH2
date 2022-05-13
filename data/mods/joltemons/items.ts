777173401ad3085e96b3f39dfc10805435a2042b
ViZarSmogon <81812196+ViZarSmogon@users.noreply.github.com>
2022-05-13 15:00:04 +0200

    pillowtest

--- data/mods/joltemons/items.ts
+++ data/mods/joltemons/items.ts
@@ -545,26 +545,12 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 		onResidualSubOrder: 5,
 		onResidual(pokemon) {
 			if ((pokemon.status === 'slp' || pokemon.hasAbility('comatose'))) {
-				this.heal(pokemon.baseMaxhp / 8);
-				const noSleepTalk = [
-					'assist', 'beakblast', 'belch', 'bide', 'celebrate', 'chatter', 'copycat', 'dynamaxcannon', 'focuspunch', 'mefirst', 'metronome', 'mimic', 'mirrormove', 'naturepower', 'shelltrap', 'sketch', 'sleeptalk', 'uproar',
-				];
-				const moves = [];
-				for (const moveSlot of pokemon.moveSlots) {
-					const moveid = moveSlot.id;
-					if (!moveid) continue;
-					const move = this.dex.getMove(moveid);
-					if (noSleepTalk.includes(moveid) || move.flags['charge'] || (move.isZ && move.basePower !== 1)) {
-						continue;
-					}
-					moves.push(moveid);
-				}
-				let randomMove = '';
-				if (moves.length) randomMove = this.sample(moves);
-				if (!randomMove) {
-                    return false;
-				}
-				this.useMove(randomMove, pokemon);
+			this.heal(pokemon.baseMaxhp / 8);
+			}
+		},
+		onStart(pokemon) {
+			if ((pokemon.status === 'slp' || pokemon.hasAbility('comatose'))) {
+				pokemon.addVolatile('pillow');
 			}
 		},
 		fling: {
