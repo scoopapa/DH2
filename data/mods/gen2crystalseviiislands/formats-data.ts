e24823862c08c985b25788fac2d9f92ed12de7a7
JosJet <89114731+JosJet@users.noreply.github.com>
2022-05-30 13:32:09 -0400

    Update formats-data.ts

--- data/mods/gen2crystalseviiislands/formats-data.ts
+++ data/mods/gen2crystalseviiislands/formats-data.ts
@@ -20,4 +20,16 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	terricks: {
 	gen: 2,
 	},
+	skunking: {
+	gen: 2,
+	},
+	kipuro: {
+	gen: 2,
+	},
+	kamebi: {
+	gen: 2,
+	},
+	pyronoir: {
+	gen: 2,
+	},
 }
