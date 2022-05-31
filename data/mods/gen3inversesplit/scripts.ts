b3dd287165ba2dc4c7901103be091c9559181786
JosJet <89114731+JosJet@users.noreply.github.com>
2022-05-31 13:42:04 -0400

    Create scripts.ts

---
+++ data/mods/gen3inversesplit/scripts.ts
@@ -0,0 +1,19 @@
+export const Scripts: ModdedBattleScriptsData = {
+	inherit: 'gen3',
+	gen: 3,
+  init() {
+		for (const i in this.data.Pokedex) {
+			delete this.data.Pokedex[i].abilities['H'];
+		}
+		const specialTypes = ['Bug', 'Fighting', 'Flying', 'Ground', 'Ghost', 'Normal', 'Poison', 'Rock', 'Steel'];
+		let newCategory = '';
+		for (const i in this.data.Movedex) {
+			if (!this.data.Movedex[i]) console.log(i);
+			if (this.data.Movedex[i].category === 'Status') continue;
+			newCategory = specialTypes.includes(this.data.Movedex[i].type) ? 'Special' : 'Physical';
+			if (newCategory !== this.data.Movedex[i].category) {
+				this.modData('Movedex', i).category = newCategory;
+			}
+		}
+  },
+};
