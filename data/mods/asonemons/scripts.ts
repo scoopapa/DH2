3971cf6504bf73f609c3b5df61b0098986426cc2
JosJet <89114731+JosJet@users.noreply.github.com>
2022-06-01 00:51:32 -0400

    Update scripts.ts

--- data/mods/asonemons/scripts.ts
+++ data/mods/asonemons/scripts.ts
@@ -101,7 +101,7 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 				if (isGrounded === null) {
 					if (message) {
 						if (this.hasAbility('asonelunatone')) {
-							this.battle.add('-immune', this, '[from] ability: Levitate');
+							this.battle.add('-immune', this, '[from] ability: asonelunatone');
 						}
 					}
 					return false;
