8022225b5ed748b8ff5fb6b4df124b4a17559578
JosJet <89114731+JosJet@users.noreply.github.com>
2022-06-01 18:13:06 -0400

    Update pokedex.ts

--- data/mods/asonemons/pokedex.ts
+++ data/mods/asonemons/pokedex.ts
@@ -5,7 +5,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		name: "Yanmega-Shell",
 		types: ["Bug", "Fire"],
 		baseStats: {hp: 70, atk: 105, def: 145, spa: 85, spd: 90, spe: 60},
-		abilities: {0: "As One (Torkoal)", 1: "As One (Lunatone)"},
+		abilities: {0: "As One (Torkoal)"},
 		weightkg: 131.9,
 	},
 	pelipperink: {
