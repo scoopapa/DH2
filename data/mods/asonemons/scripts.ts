ed7846f5bebdd957094e4d89848074b15fbb7f31
JosJet <89114731+JosJet@users.noreply.github.com>
2022-06-02 12:30:58 -0400

    Update scripts.ts

--- data/mods/asonemons/scripts.ts
+++ data/mods/asonemons/scripts.ts
@@ -26,7 +26,7 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 			}
 		}
 
-		this.modData('Learnsets', 'yanmegashell').learnset.coalsting = ['7L1'];
+		this.modData('Learnsets', 'yanmegashell').learnset.coalsting = ['8L1'];
 		delete this.modData('Learnsets', 'yanmegashell').learnset.shellsmash;
 		
 		this.modData('Learnsets', 'pelipperink').learnset.inkgulp = ['8L1'];
