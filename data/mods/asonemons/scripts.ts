8b7c9622b22f81de260d7fffb2929803b85c4197
JosJet <89114731+JosJet@users.noreply.github.com>
2022-06-01 23:02:12 -0400

    Update scripts.ts

--- data/mods/asonemons/scripts.ts
+++ data/mods/asonemons/scripts.ts
@@ -1,4 +1,68 @@
 export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
+	pokemon: {
+		runImmunity(type: string, message?: string | boolean) {
+			if (!type || type === '???') return true;
+			if (!(type in this.battle.dex.data.TypeChart)) {
+				if (type === 'Fairy' || type === 'Dark' || type === 'Steel') return true;
+				throw new Error("Use runStatusImmunity for " + type);
+			}
+			if (this.fainted) return false;
+
+			const negateResult = this.battle.runEvent('NegateImmunity', this, type);
+			let isGrounded;
+			if (type === 'Ground') {
+				isGrounded = this.isGrounded(!negateResult);
+				if (isGrounded === null) {
+					if (message) {
+						if (this.hasAbility('asonelunatone')) {
+							this.battle.add('-immune', this, '[from] ability: Levitate');
+						}
+					}
+					return false;
+				}
+			}
+			if (!negateResult) return true;
+			if ((isGrounded === undefined && !this.battle.dex.getImmunity(type, this)) || isGrounded === false) {
+				if (message) {
+					this.battle.add('-immune', this);
+				}
+				return false;
+			}
+			return true;
+		},
+		isGrounded(negateImmunity = false) {
+		if ('gravity' in this.battle.field.pseudoWeather) return true;
+		if ('ingrain' in this.volatiles && this.battle.gen >= 4) return true;
+		if ('smackdown' in this.volatiles) return true;
+		const item = (this.ignoringItem() ? '' : this.item);
+		if (item === 'ironball') return true;
+		// If a Fire/Flying type uses Burn Up and Roost, it becomes ???/Flying-type, but it's still grounded.
+		if (!negateImmunity && this.hasType('Flying') && !('roost' in this.volatiles)) return false;
+		if (this.hasAbility('levitate') || this.hasAbility('asonelunatone') && !this.battle.suppressingAttackEvents()) return null;
+		if ('magnetrise' in this.volatiles) return false;
+		if ('telekinesis' in this.volatiles) return false;
+		if ('float' in this.volatiles) return false;
+		return item !== 'airballoon';
+		},
+		setStatus(
+		status: string | Condition,
+		source: Pokemon | null = null,
+		sourceEffect: Effect | null = null,
+		ignoreImmunities = false
+	) {
+    if (!ignoreImmunities && status.id &&
+				!(source?.hasAbility('asonesalazzle') && ['tox', 'psn'].includes(status.id))) {
+			// the game currently never ignores immunities
+			if (!this.runStatusImmunity(status.id === 'tox' ? 'psn' : status.id)) {
+				this.battle.debug('immune to status');
+				if ((sourceEffect as Move)?.status) {
+					this.battle.add('-immune', this);
+				}
+				return false;
+				}
+			}
+   	}
+	},	
 	init(){ 
 		//To construct learnsets for these evolutions, I'm gonna cheat and copy the code Scoopapa made for construction FE fusion learnsets
 		//If you delete moves that a prevo learns from a learnset that's constructed via inheriting said moves from a prevo the way Showdown does normally,
@@ -89,69 +153,4 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 		this.modData('Learnsets', 'malamarparallel').learnset.innerdeviation = ['8L1'];
 		
 	},
-
-    pokemon: {
-		runImmunity(type: string, message?: string | boolean) {
-			if (!type || type === '???') return true;
-			if (!(type in this.battle.dex.data.TypeChart)) {
-				if (type === 'Fairy' || type === 'Dark' || type === 'Steel') return true;
-				throw new Error("Use runStatusImmunity for " + type);
-			}
-			if (this.fainted) return false;
-
-			const negateResult = this.battle.runEvent('NegateImmunity', this, type);
-			let isGrounded;
-			if (type === 'Ground') {
-				isGrounded = this.isGrounded(!negateResult);
-				if (isGrounded === null) {
-					if (message) {
-						if (this.hasAbility('asonelunatone')) {
-							this.battle.add('-immune', this, '[from] ability: Levitate');
-						}
-					}
-					return false;
-				}
-			}
-			if (!negateResult) return true;
-			if ((isGrounded === undefined && !this.battle.dex.getImmunity(type, this)) || isGrounded === false) {
-				if (message) {
-					this.battle.add('-immune', this);
-				}
-				return false;
-			}
-			return true;
-		},
-		isGrounded(negateImmunity = false) {
-		if ('gravity' in this.battle.field.pseudoWeather) return true;
-		if ('ingrain' in this.volatiles && this.battle.gen >= 4) return true;
-		if ('smackdown' in this.volatiles) return true;
-		const item = (this.ignoringItem() ? '' : this.item);
-		if (item === 'ironball') return true;
-		// If a Fire/Flying type uses Burn Up and Roost, it becomes ???/Flying-type, but it's still grounded.
-		if (!negateImmunity && this.hasType('Flying') && !('roost' in this.volatiles)) return false;
-		if (this.hasAbility('levitate') || this.hasAbility('asonelunatone') && !this.battle.suppressingAttackEvents()) return null;
-		if ('magnetrise' in this.volatiles) return false;
-		if ('telekinesis' in this.volatiles) return false;
-		if ('float' in this.volatiles) return false;
-		return item !== 'airballoon';
-		},
-		setStatus(
-		status: string | Condition,
-		source: Pokemon | null = null,
-		sourceEffect: Effect | null = null,
-		ignoreImmunities = false
-	) {
-    if (!ignoreImmunities && status.id &&
-				!(source?.hasAbility('asonesalazzle') && ['tox', 'psn'].includes(status.id))) {
-			// the game currently never ignores immunities
-			if (!this.runStatusImmunity(status.id === 'tox' ? 'psn' : status.id)) {
-				this.battle.debug('immune to status');
-				if ((sourceEffect as Move)?.status) {
-					this.battle.add('-immune', this);
-				}
-				return false;
-				}
-			}
-   	}
-	},	
 };
