504ce983344c5382a39323b40595aa9136b0be3b
JosJet <89114731+JosJet@users.noreply.github.com>
2022-06-02 20:50:03 -0400

    Update scripts.ts

--- data/mods/asonemons/scripts.ts
+++ data/mods/asonemons/scripts.ts
@@ -1,6 +1,8 @@
 
 export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
-	init(){
+	init(){ 
+		// *(Referenced from FEUU)* 
+		// Automatically construct fusion learnsets! (Thank u scoopapa)
 		for (const id in this.dataCache.Pokedex) {//check the dex for fusions
 			const fusionEntry = this.dataCache.Pokedex[id];
 			if (fusionEntry.fusion) {//if the pokedex entry has a fusion field, it's a fusion
@@ -18,7 +20,8 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 				for (let name of learnsetFusionList) {					
 					const learnset = this.dataCache.Learnsets[this.toID(name)].learnset;//get the learnset of each pokemon in the list
 					for (const moveid in learnset) {
-						this.modData('Learnsets', id).learnset[moveid] = ['8L1', '7L1', '6L1', '5L1', '4L1'];//all moves are compatible with the fusion's only ability, so just set it to 8L1
+						if (this.dataCache.Moves[moveid].isNonstandard === 'Past') continue; //exclude dexited moves (I hope!) 
+						this.modData('Learnsets', id).learnset[moveid] = ['8L1'];//all moves are compatible with the fusion's only ability, so just set it to 8L1
 					}
 				}
 			}
