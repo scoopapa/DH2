88adc060ad60a15f1474776ad51ff456c9052079
JosJet <89114731+JosJet@users.noreply.github.com>
2022-06-05 08:50:31 -0400

    Update scripts.ts

--- data/mods/asonemons/scripts.ts
+++ data/mods/asonemons/scripts.ts
@@ -1,33 +1,7 @@
 
 export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
-	/*
 	init(){ 
-		// *(Referenced from FEUU)* 
-		// Automatically construct fusion learnsets! (Thank u scoopapa)
-		for (const id in this.dataCache.Pokedex) {//check the dex for fusions
-			const fusionEntry = this.dataCache.Pokedex[id];
-			if (fusionEntry.fusion) {//if the pokedex entry has a fusion field, it's a fusion
-				const learnsetFusionList = [];//list of pokemon whose learnsets need to be fused
-				for (let name of fusionEntry.fusion) {
-					let prevo = true;
-					while (prevo) {//make sure prevos of both fused pokemon are added to the list
-						learnsetFusionList.push(name);
-						const dexEntry = this.dataCache.Pokedex[this.toID(name)];
-						if (dexEntry.prevo) name = dexEntry.prevo;
-						else prevo = false;
-					}
-				}
-				if (!this.dataCache.Learnsets[id]) this.dataCache.Learnsets[id] = { learnset: {}};//create a blank learnset entry so we don't need a learnsets file
-				for (let name of learnsetFusionList) {					
-					const learnset = this.dataCache.Learnsets[this.toID(name)].learnset;//get the learnset of each pokemon in the list
-					for (const moveid in learnset) {
-						if (this.dataCache.Moves[moveid].isNonstandard === 'Past') continue; //exclude dexited moves (I hope!) 
-						this.modData('Learnsets', id).learnset[moveid] = ['8L1'];//all moves are compatible with the fusion's only ability, so just set it to 8L1
-					}
-				}
-			}
-		}
-
+		
 		this.modData('Learnsets', 'yanmegashell').learnset.coalsting = ['8L1'];
 		delete this.modData('Learnsets', 'yanmegashell').learnset.shellsmash;
 		
@@ -88,7 +62,31 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 		this.modData('Learnsets', 'blacephalonborealis').learnset.vengefulspirit = ['8L1'];
 		
 		this.modData('Learnsets', 'malamarparallel').learnset.innerdeviation = ['8L1'];
-		
+		// *(Referenced from FEUU)* 
+		// Automatically construct fusion learnsets! (Thank u scoopapa)
+		for (const id in this.dataCache.Pokedex) {//check the dex for fusions
+			const fusionEntry = this.dataCache.Pokedex[id];
+			if (fusionEntry.fusion) {//if the pokedex entry has a fusion field, it's a fusion
+				const learnsetFusionList = [];//list of pokemon whose learnsets need to be fused
+				for (let name of fusionEntry.fusion) {
+					let prevo = true;
+					while (prevo) {//make sure prevos of both fused pokemon are added to the list
+						learnsetFusionList.push(name);
+						const dexEntry = this.dataCache.Pokedex[this.toID(name)];
+						if (dexEntry.prevo) name = dexEntry.prevo;
+						else prevo = false;
+					}
+				}
+				if (!this.dataCache.Learnsets[id]) this.dataCache.Learnsets[id] = { learnset: {}};//create a blank learnset entry so we don't need a learnsets file
+				for (let name of learnsetFusionList) {					
+					const learnset = this.dataCache.Learnsets[this.toID(name)].learnset;//get the learnset of each pokemon in the list
+					for (const moveid in learnset) {
+						if (this.dataCache.Moves[moveid].isNonstandard === 'Past') continue; //exclude dexited moves (I hope!) 
+						this.modData('Learnsets', id).learnset[moveid] = ['8L1'];//all moves are compatible with the fusion's only ability, so just set it to 8L1
+					}
+				}
+			}
+		}
 	},
 pokemon: {
 	runImmunity(type: string, message?: string | boolean) {
@@ -154,6 +152,5 @@ pokemon: {
 			}
    	}
 	},	
-	*/
 };
 
