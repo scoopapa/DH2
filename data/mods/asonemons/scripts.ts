6659859b4c4bf25ce3030cf2b71c5092ce05be5b
JosJet <89114731+JosJet@users.noreply.github.com>
2022-06-06 03:55:33 -0400

    Update scripts.ts

--- data/mods/asonemons/scripts.ts
+++ data/mods/asonemons/scripts.ts
@@ -1,7 +1,6 @@
 export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 	init(){ 
 		this.modData('Learnsets', 'yanmega').learnset.coalsting = ['8L1'];
-		delete this.modData('Learnsets', 'torkoal').learnset.shellsmash;
 		
 		this.modData('Learnsets', 'pelipper').learnset.inkgulp = ['8L1'];
 		
@@ -60,30 +59,6 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 		this.modData('Learnsets', 'blacephalon').learnset.vengefulspirit = ['8L1'];
 		
 		this.modData('Learnsets', 'malamar').learnset.innerdeviation = ['8L1'];
-		// *(Referenced from FEUU)* 
-		// Automatically construct fusion learnsets! (Thank u scoopapa)
-		for (const id in this.dataCache.Pokedex) {//check the dex for fusions
-			const fusionEntry = this.dataCache.Pokedex[id];
-			if (fusionEntry.fusion) {//if the pokedex entry has a fusion field, it's a fusion
-				const learnsetFusionList = [];//list of pokemon whose learnsets need to be fused
-				for (let name of fusionEntry.fusion) {
-					let prevo = true;
-					while (prevo) {//make sure prevos of both fused pokemon are added to the list
-						learnsetFusionList.push(name);
-						const dexEntry = this.dataCache.Pokedex[this.toID(name)];
-						if (dexEntry.prevo) name = dexEntry.prevo;
-						else prevo = false;
-					}
-				}
-				if (!this.dataCache.Learnsets[id]) this.dataCache.Learnsets[id] = { learnset: {}};//create a blank learnset entry so we don't need a learnsets file
-				for (let name of learnsetFusionList) {					
-					const learnset = this.dataCache.Learnsets[this.toID(name)].learnset;//get the learnset of each pokemon in the list
-					for (const moveid in learnset) {
-						this.modData('Learnsets', id).learnset[moveid] = ['8L1', '7L1', '6L1', '5L1', '4L1'];//all moves are compatible with the fusion's only ability, so just set it to 8L1
-					}
-				}
-			}
-		}
 	},
 pokemon: {
 	runImmunity(type: string, message?: string | boolean) {
@@ -116,7 +91,7 @@ pokemon: {
 			}
 			return true;
 		},
-		isGrounded(negateImmunity = false) {
+	isGrounded(negateImmunity = false) {
 		if ('gravity' in this.battle.field.pseudoWeather) return true;
 		if ('ingrain' in this.volatiles && this.battle.gen >= 4) return true;
 		if ('smackdown' in this.volatiles) return true;
