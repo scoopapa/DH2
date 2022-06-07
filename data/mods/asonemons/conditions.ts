0a3f63a89217f06736aaa6d77c291980af4f34be
JosJet <89114731+JosJet@users.noreply.github.com>
2022-06-07 00:11:47 -0400

    Update conditions.ts

--- data/mods/asonemons/conditions.ts
+++ data/mods/asonemons/conditions.ts
@@ -17,11 +17,9 @@ export const Conditions: {[k: string]: ConditionData} = {
 		},
 		onBeforeMovePriority: 10,
 		onBeforeMove(pokemon, target, move) {
-			if (pokemon.hasAbility('earlybird')) {
+			if (pokemon.hasAbility('earlybird')) || (pokemon.hasAbility('asonegirafarig')) {
 				pokemon.statusData.time--;
-			} else if (pokemon.hasAbility('asonegirafarig')) {
-				pokemon.statusData.time--;
-			}
+			} 
 			pokemon.statusData.time--;
 			if (pokemon.statusData.time <= 0) {
 				pokemon.cureStatus();
