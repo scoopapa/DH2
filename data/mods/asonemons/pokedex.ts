ac8280f5b7d5e37cb670dec97dc9291ac0ec081f
JosJet <89114731+JosJet@users.noreply.github.com>
2022-06-09 17:06:20 -0400

    Update pokedex.ts

--- data/mods/asonemons/pokedex.ts
+++ data/mods/asonemons/pokedex.ts
@@ -14,7 +14,7 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		baseStats: {hp: 80, atk: 78, def: 85, spa: 100, spd: 135, spe: 105},
 		abilities: {0: "As One (Tentacruel)"},
 		weightkg: 83,
-		prevo: "Tentacool",
+		prevo: ["Wingull", "Tentacool"],
 	},
 	excadrillboulder: {
 		fusion: ['Excadrill', 'Gigalith'],
