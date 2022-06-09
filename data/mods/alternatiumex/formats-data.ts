8abfea0b8b2682cec53df5c07b4b4ec02d322907
ViZarSmogon <vizarsmogon@gmail.com>
2022-06-09 15:23:29 +0200

    Alternatium Slate 2-3 + Ubermons Fix

--- data/mods/alternatiumex/formats-data.ts
+++ data/mods/alternatiumex/formats-data.ts
@@ -23,4 +23,48 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		tier: "Alternatium EX",
 		doublesTier: "Alternatium EX",
 	},
+	oricorio: {
+		tier: "Alternatium EX",
+		doublesTier: "Alternatium EX",
+	},
+	oricoriocheerleader: {
+		tier: "Alternatium EX",
+		doublesTier: "Alternatium EX",
+	},
+	oricoriopau: {
+		tier: "Alternatium EX",
+		doublesTier: "Alternatium EX",
+	},
+	horrorcorio: {
+		tier: "Alternatium EX",
+		doublesTier: "Alternatium EX",
+	},
+	pikachukanto: {
+		tier: "Alternatium EX",
+		doublesTier: "Alternatium EX",
+	},
+	pikachuhoenn: {
+		tier: "Alternatium EX",
+		doublesTier: "Alternatium EX",
+	},
+	pikachusinnoh: {
+		tier: "Alternatium EX",
+		doublesTier: "Alternatium EX",
+	},
+	pikachuunova: {
+		tier: "Alternatium EX",
+		doublesTier: "Alternatium EX",
+	},
+	pikachutactic: {
+		tier: "Alternatium EX",
+		doublesTier: "Alternatium EX",
+	},
+	pikachualola: {
+		tier: "Alternatium EX",
+		doublesTier: "Alternatium EX",
+	},
+	pikachuworld: {
+		tier: "Alternatium EX",
+		doublesTier: "Alternatium EX",
+	},
 };
