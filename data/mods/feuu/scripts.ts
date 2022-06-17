4a2edf31392a7a1cfcb577b62b21daa9ae7dfbe6
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2022-06-17 18:03:42 -0400

    Update scripts.ts

--- data/mods/feuu/scripts.ts
+++ data/mods/feuu/scripts.ts
@@ -358,15 +358,15 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 				this.hasAbility('magneticwaves') ||
 				this.hasAbility('leviflame') ||
 				this.hasAbility('levitability') || 
-				this.hasAbility('stickyfloat')) || 
-				this.hasAbility('etativel')) || 
-				this.hasAbility('lighthearted')) || 
-				this.hasAbility('clearlyfloating')) || 
-				this.hasAbility('floatguise')) || 
-				this.hasAbility('aerialbreak')) || 
-				this.hasAbility('levimetal')) || 
-				this.hasAbility('hoverboard')) || 
-				this.hasAbility('levistatic')) || 
+				this.hasAbility('stickyfloat') || 
+				this.hasAbility('etativel') || 
+				this.hasAbility('lighthearted') || 
+				this.hasAbility('clearlyfloating') || 
+				this.hasAbility('floatguise') || 
+				this.hasAbility('aerialbreak') || 
+				this.hasAbility('levimetal') || 
+				this.hasAbility('hoverboard') || 
+				this.hasAbility('levistatic') || 
 				this.hasAbility('lovelessfloat')) &&
 				
 				!this.battle.suppressingAttackEvents()
