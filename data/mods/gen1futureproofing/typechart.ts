5eeb47ade7df1867ddbd3535be5afa2a5312446a
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2022-06-24 14:50:50 -0400

    Update typechart.ts

--- data/mods/gen1futureproofing/typechart.ts
+++ data/mods/gen1futureproofing/typechart.ts
@@ -1,9 +1,8 @@
 /**
- * Types were different on Gen 1.
- * We had no steel nor dark types and there were a couple of important differences:
- * Bug and Poison were weak to eachother
- * Ice was neutral to fire
- * Psychic was immune to ghost
+ * New types use data from their earliest known data.
+ * So, Steel- and Dark-type use the SW97 incarnations. 
+ * Fairy works how it does in modern (SWSH at time of writing, unchanged from XY)
+ * All types are put in here in case something crazy happens, also so Fairy and everything work properly.
  */
 
 export const TypeChart: {[k: string]: ModdedTypeData} = {
@@ -26,6 +25,63 @@ export const TypeChart: {[k: string]: ModdedTypeData} = {
 			Water: 0,
 		},
 	},
+	dragon: {
+		damageTaken: {
+			Bug: 0,
+			Dragon: 1,
+			Electric: 2,
+			Fighting: 0,
+			Fire: 2,
+			Flying: 0,
+			Ghost: 0,
+			Grass: 2,
+			Ground: 0,
+			Ice: 1,
+			Normal: 0,
+			Poison: 0,
+			Psychic: 0,
+			Rock: 0,
+			Water: 2,
+		},
+	},
+	electric: {
+		damageTaken: {
+			Bug: 0,
+			Dragon: 0,
+			Electric: 2,
+			Fighting: 0,
+			Fire: 0,
+			Flying: 2,
+			Ghost: 0,
+			Grass: 0,
+			Ground: 1,
+			Ice: 0,
+			Normal: 0,
+			Poison: 0,
+			Psychic: 0,
+			Rock: 0,
+			Water: 0,
+		},
+	},
+	fighting: {
+		damageTaken: {
+			Bug: 2,
+			Dragon: 0,
+			Electric: 0,
+			Fighting: 0,
+			Fire: 0,
+			Flying: 1,
+			Ghost: 0,
+			Grass: 0,
+			Ground: 0,
+			Ice: 0,
+			Normal: 0,
+			Poison: 0,
+			Psychic: 1,
+			Rock: 2,
+			Water: 0,
+		},
+	},
 	fire: {
 		damageTaken: {
 			Bug: 2,
@@ -43,6 +99,25 @@ export const TypeChart: {[k: string]: ModdedTypeData} = {
 			Psychic: 0,
 			Rock: 1,
 			Water: 1,
+		},
+	},
+	flying: {
+		damageTaken: {
+			Bug: 2,
+			Dragon: 0,
+			Electric: 1,
+			Fighting: 2,
+			Fire: 0,
+			Flying: 0,
+			Ghost: 0,
+			Grass: 2,
+			Ground: 3,
+			Ice: 1,
+			Normal: 0,
+			Poison: 0,
+			Psychic: 0,
+			Rock: 1,
+			Water: 0,
 		},
 	},
 	ghost: {
@@ -64,6 +139,44 @@ export const TypeChart: {[k: string]: ModdedTypeData} = {
 			Water: 0,
 		},
 	},
+	grass: {
+		damageTaken: {
+			Bug: 1,
+			Dragon: 0,
+			Electric: 2,
+			Fighting: 0,
+			Fire: 1,
+			Flying: 1,
+			Ghost: 0,
+			Grass: 2,
+			Ground: 2,
+			Ice: 1,
+			Normal: 0,
+			Poison: 1,
+			Psychic: 0,
+			Rock: 0,
+			Water: 2,
+		},
+	},
+	ground: {
+		damageTaken: {
+			Bug: 0,
+			Dragon: 0,
+			Electric: 3,
+			Fighting: 0,
+			Fire: 0,
+			Flying: 0,
+			Ghost: 0,
+			Grass: 1,
+			Ground: 0,
+			Ice: 1,
+			Normal: 0,
+			Poison: 2,
+			Psychic: 0,
+			Rock: 2,
+			Water: 1,
+		},
+	},
 	ice: {
 		damageTaken: {
 			Bug: 0,
@@ -83,6 +196,25 @@ export const TypeChart: {[k: string]: ModdedTypeData} = {
 			Psychic: 0,
 			Rock: 1,
 			Steel: 1,
+			Water: 0,
+		},
+	},
+	normal: {
+		damageTaken: {
+			Bug: 0,
+			Dragon: 0,
+			Electric: 0,
+			Fighting: 1,
+			Fire: 0,
+			Flying: 0,
+			Ghost: 3,
+			Grass: 0,
+			Ground: 0,
+			Ice: 0,
+			Normal: 0,
+			Poison: 0,
+			Psychic: 0,
+			Rock: 0,
 			Water: 0,
 		},
 	},
@@ -125,6 +257,48 @@ export const TypeChart: {[k: string]: ModdedTypeData} = {
 			Rock: 0,
 			Water: 0,
 		},
+	},
+	rock: {
+		damageTaken: {
+			Bug: 0,
+			Dragon: 0,
+			Electric: 0,
+			Fighting: 1,
+			Fire: 2,
+			Flying: 2,
+			Ghost: 0,
+			Grass: 1,
+			Ground: 1,
+			Ice: 0,
+			Normal: 2,
+			Poison: 2,
+			Psychic: 0,
+			Rock: 0,
+			Water: 1,
+		},
+	},
+	water: {
+		damageTaken: {
+			Bug: 0,
+			Dragon: 0,
+			Electric: 1,
+			Fighting: 0,
+			Fire: 2,
+			Flying: 0,
+			Ghost: 0,
+			Grass: 1,
+			Ground: 0,
+			Ice: 2,
+			Normal: 0,
+			Poison: 0,
+			Psychic: 0,
+			Rock: 0,
+			Water: 2,
+		},
+	},
+	fairy: {
+		inherit: true,
+		isNonstandard: 'Future',
 	},
 	dark: {
 		inherit: true,
