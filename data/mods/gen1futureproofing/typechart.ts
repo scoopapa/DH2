de298d14004159eb92af413e640ba127211b2057
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2022-06-24 14:44:04 -0400

    Update typechart.ts

--- data/mods/gen1futureproofing/typechart.ts
+++ data/mods/gen1futureproofing/typechart.ts
@@ -6,7 +6,7 @@
  * Psychic was immune to ghost
  */
 
-export const TypeChart: {[k: string]: ModdedTypeData | null} = {
+export const TypeChart: {[k: string]: ModdedTypeData} = {
 	bug: {
 		damageTaken: {
 			Bug: 0,
