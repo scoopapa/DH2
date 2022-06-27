338d8f24f1c4ad8111dbc1fd43360664d1dc8ba4
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2022-06-27 16:24:28 -0400

    Update items.ts

--- data/mods/genxloria/items.ts
+++ data/mods/genxloria/items.ts
@@ -26,4 +26,45 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 		desc: "If held by a Galapagunk, this item changes it to its Completed form.",
 		gen: 8,
 	},
+	
+// Mega Stones
+	jaibastionite: {
+		name: "Jaibastionite",
+		spritenum: 578,
+		megaStone: "Jaibastion-Mega",
+		megaEvolves: "Jaibastion",
+		itemUser: ["Jaibastion"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		gen: 8,
+		desc: "If held by a Jaibastion, this item allows it to Mega Evolve in battle.",
+	},
+	sulfuramite: {
+		name: "Sulfuramite",
+		spritenum: 578,
+		megaStone: "Sulfuram-Mega",
+		megaEvolves: "Sulfuram",
+		itemUser: ["Sulfuram"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		gen: 8,
+		desc: "If held by a Sulfuram, this item allows it to Mega Evolve in battle.",
+	},
+	minkfinitite: {
+		name: "Minkfinitite",
+		spritenum: 578,
+		megaStone: "Minkfinit-Mega",
+		megaEvolves: "Minkfinit",
+		itemUser: ["Minkfinit"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		gen: 8,
+		desc: "If held by a Minkfinit, this item allows it to Mega Evolve in battle.",
+	},
 };
