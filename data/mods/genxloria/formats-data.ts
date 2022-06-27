546e741f3a0d93790827f1f38d0de89c57eb77c3
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2022-06-27 14:32:30 -0400

    Create formats-data.ts

---
+++ data/mods/genxloria/formats-data.ts
@@ -0,0 +1,495 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	cocolipso: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	tropiclaw: {
+		tier: "Loria NFE",
+		doublesTier: "DOU",
+	},
+	jaibastion: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	embaa: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	cinemere: {
+		tier: "Loria NFE",
+		doublesTier: "DOU",
+	},
+	sulfuram: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	flowtter: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	ottorrent: {
+		tier: "Loria NFE",
+		doublesTier: "DOU",
+	},
+	minkfinit: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	perocotton: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	largotton: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	zippyr: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	zoomraj: {
+		tier: "Loria NFE",
+		doublesTier: "DOU",
+	},
+	zetztream: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	antyke: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	pupant: {
+		tier: "Loria NFE",
+		doublesTier: "DOU",
+	},
+	dronaught: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	combattant: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	antunnel: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	formiquen: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	kithra: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	zorboreal: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	srinbow: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	whipintow: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	glucatty: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	tropitano: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	tropiconti: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	hoglet: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	hogtide: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	hogcules: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	hogyron: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	hoggnid: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	hogoblin: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	hogust: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	hogven: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	rascoon: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	maskoon: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	nandare: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	marbowl: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	selustre: {
+		tier: "Loria NFE",
+		doublesTier: "DOU",
+	},
+	monolux: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	canvis: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	carnivoross: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	grizzline: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	myoce: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	viverodon: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	usawald: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	thermonewt: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	frozuna: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	firack: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	fiarsho: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	nymphire: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	nymphork: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	ribbolt: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	dendrobolt: {
+		tier: "Loria NFE",
+		doublesTier: "DOU",
+	},
+	thundart: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	croakorrode: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	candlen: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	bombastor: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	coatick: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	coatorch: {
+		tier: "Loria NFE",
+		doublesTier: "DOU",
+	},
+	coatrin: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	escargnome: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	fairysnale: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	jungelion: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	feer: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	phantelope: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	corruptomb: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	sinistect: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	grimoth: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	knightmare: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	yanmazure: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	searie: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	steemevent: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	flambéngo: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	flamilion: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	belubble: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	drakumuy: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	krokoasis: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	tsunamey: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	erythrope: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	jahvolen: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	kluhbur: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	restrictorch: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	conductorch: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	hearow: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	keisberg: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	stontravank: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	manatike: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	managma: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	kidmodo: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	komotide: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	kittuff: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	catamacho: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	punkey: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	funkey: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	clunkey: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	junkey: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	simisand: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	simismash: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	simisoar: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	foifish: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	sailre: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	visiguana: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	conquiguana: {
+		tier: "Loria NFE",
+		doublesTier: "DOU",
+	},
+	imperiguana: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	whiscraft: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	crambicoff: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	tyraloth: {
+		tier: "Loria NFE",
+		doublesTier: "DOU",
+	},
+	pyraustragon: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	narabel: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	torklear: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	torkool: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	aretle: {
+		tier: "Loria LC",
+		doublesTier: "DOU",
+	},
+	beetleton: {
+		tier: "Loria NFE",
+		doublesTier: "DOU",
+	},
+	ironmor: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	beesquitto: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	masaque: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	hauntoll: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	amadire: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	pratrame: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	vishneute: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+	negashive: {
+		tier: "Loria FE",
+		doublesTier: "DOU",
+	},
+
+};
