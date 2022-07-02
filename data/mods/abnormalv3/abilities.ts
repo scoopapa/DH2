522f0fc8c5b744bd7f7ff289b0c86886a7e82962
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2022-07-02 16:41:51 -0400

    Update abilities.ts

--- data/mods/abnormalv3/abilities.ts
+++ data/mods/abnormalv3/abilities.ts
@@ -1,6 +1,6 @@
 export const Abilities: {[k: string]: ModdedAbilityData} = {
  
-"normalize": {
+normalize: {
     desc: "This Pokemon's moves are changed to match its primery type and have their power multiplied by 1.2. This effect comes before other effects that change a move's type.",
     shortDesc: "This Pokemon's moves are changed to match its primary type and have 1.2x power.",
     onModifyType(move, pokemon) {
@@ -9,8 +9,15 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
         move.normalizeBoosted = true;
       }
     },
+		onBasePowerPriority: 23,
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.normalizeBoosted) return this.chainModify([0x1333, 0x1000]);
+		},
+	name: "Normalize",
+	rating: 0,
+	num: 96,
   },
-  "aerilate": {
+  aerilate: {
     desc: "This Pokemon's contact and sound moves become Flying-type moves and have their power multiplied by 1.2. This effect comes after other effects that change a move's type, but before Ion Deluge and Electrify's effects.",
     shortDesc: "This Pokemon's contact/sound moves become Flying type and have 1.2x power.",
     onModifyType(move, pokemon) {
@@ -19,8 +26,15 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
         move.aerilateBoosted = true;
       }
     },
+	onBasePowerPriority: 23,
+	onBasePower(basePower, pokemon, target, move) {
+		if (move.aerilateBoosted) return this.chainModify([0x1333, 0x1000]);
+	},
+	name: "Aerilate",
+	rating: 4,
+	num: 185,
   },
-  "refrigerate": {
+  refrigerate: {
     desc: "This Pokemon's contact and sound moves become Ice-type moves and have their power multiplied by 1.2. This effect comes after other effects that change a move's type, but before Ion Deluge and Electrify's effects.",
     shortDesc: "This Pokemon's contact/sound moves become Ice type and have 1.2x power.",
     onModifyType(move, pokemon) {
@@ -29,8 +43,15 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
         move.refrigerateBoosted = true;
       }
     },
+	onBasePowerPriority: 23,
+	onBasePower(basePower, pokemon, target, move) {
+		if (move.refrigerateBoosted) return this.chainModify([0x1333, 0x1000]);
+	},
+	name: "Refrigerate",
+	rating: 4,
+	num: 174,
   },
-  "pixilate": {
+  pixilate: {
     desc: "This Pokemon's contact and sound moves become Fairy-type moves and have their power multiplied by 1.2. This effect comes after other effects that change a move's type, but before Ion Deluge and Electrify's effects.",
     shortDesc: "This Pokemon's contact/sound moves become Fairy type and have 1.2x power.",
     onModifyType(move, pokemon) {
@@ -39,8 +60,15 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
         move.pixilateBoosted = true;
       }
     },
+	onBasePowerPriority: 23,
+	onBasePower(basePower, pokemon, target, move) {
+		if (move.pixilateBoosted) return this.chainModify([0x1333, 0x1000]);
+	},
+	name: "Pixilate",
+	rating: 4,
+	num: 182,
   },
-  "galvanize": {
+  galvanize: {
     desc: "This Pokemon's contact and sound moves become Electric-type moves and have their power multiplied by 1.2. This effect comes after other effects that change a move's type, but before Ion Deluge and Electrify's effects.",
     shortDesc: "This Pokemon's contact/sound moves become Electric type and have 1.2x power.",
     onModifyType(move, pokemon) {
@@ -49,8 +77,15 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
         move.galvanizeBoosted = true;
       }
     },
+	onBasePowerPriority: 23,
+	onBasePower(basePower, pokemon, target, move) {
+		if (move.galvanizeBoosted) return this.chainModify([0x1333, 0x1000]);
+	},
+	name: "Galvanize",
+	rating: 4,
+	num: 206,
   },
-  "scrappy": {
+  scrappy: {
     desc: "This Pokemon can hit Ghost types with Fighting-type moves. Immune to Intimidate.",
     shortDesc: "Fighting moves hit Ghost. Immune to Intimidate.",
     onModifyMove(move) {
@@ -59,5 +94,14 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
         move.ignoreImmunity['Fighting'] = true;
       }
     },
+	onBoost(boost, target, source, effect) {
+		if (effect.id === 'intimidate') {
+			delete boost.atk;
+			this.add('-immune', target, '[from] ability: Scrappy');
+		}
+	},
+	name: "Scrappy",
+	rating: 3,
+	num: 113,
   },
 };
