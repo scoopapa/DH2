d14ea59a2ff5935ee68b82b1721289e5b74292d6
AnalysisGeek <101986458+AnalysisGeek@users.noreply.github.com>
2022-07-08 21:52:23 -0400

    MetaMons Pet Mod Code
    Code for MetaMons

---
+++ data/mods/metamons/scripts.ts
@@ -0,0 +1,65 @@
+export const Scripts: ModdedBattleScriptsData = {
+		init: function () {
+			this.modData("Learnsets", "whimsicott").learnset.bodypress = ["8L1"];
+			this.modData("Learnsets", "whimsicott").learnset.synthesis = ["8L1"];
+			this.modData("Learnsets", "whimsicott").learnset.reflect = ["8L1"];
+			this.modData("Learnsets", "sandslash").learnset.shoreup = ["8L1"];
+			this.modData("Learnsets", "sandslash").learnset.spikyshield = ["8L1"];
+			this.modData("Learnsets", "bastiodon").learnset.slackoff = ["8L1"];
+			this.modData("Learnsets", "bastiodon").learnset.steelbeam = ["8L1"];
+			this.modData("Learnsets", "bastiodon").learnset.bodypress = ["8L1"];
+			this.modData("Learnsets", "bastiodon").learnset.spikes = ["8L1"];
+			this.modData("Learnsets", "bastiodon").learnset.explosion = ["8L1"];
+			this.modData("Learnsets", "bastiodon").learnset.meteorbeam = ["8L1"];
+			this.modData("Learnsets", "kingler").learnset.waterfall = ["8L1"];
+			this.modData("Learnsets", "kingler").learnset.flipturn = ["8L1"];
+			this.modData("Learnsets", "kingler").learnset.icepunch = ["8L1"];
+			this.modData("Learnsets", "kingler").learnset.freezedry = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.bulkup = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.calmmind = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.dracometeor = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.dragonclaw = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.dragonpulse = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.dragonrush = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.dragontail = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.memento = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.partingshot = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.outrage = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.scaleshot = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.spikes = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.skittersmack = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.taunt = ["8L1"];
+			this.modData("Learnsets", "ariados").learnset.uturn = ["8L1"];
+			this.modData("Learnsets", "salazzle").learnset.acidrain = ["8L1"];
+			this.modData("Learnsets", "salazzle").learnset.aerialace = ["8L1"];
+			this.modData("Learnsets", "salazzle").learnset.bounce = ["8L1"];
+			this.modData("Learnsets", "salazzle").learnset.defog = ["8L1"];
+			this.modData("Learnsets", "salazzle").learnset.hurricane = ["8L1"];
+			this.modData("Learnsets", "salazzle").learnset.uturn = ["8L1"];
+			delete this.modData('Learnsets', 'kingler').learnset.rockslide;
+			delete this.modData('Learnsets', 'kingler').learnset.rocktomb;
+			delete this.modData('Learnsets', 'kingler').learnset.stompingtantrum;
+			delete this.modData('Learnsets', 'kingler').learnset.superpower;
+			delete this.modData('Learnsets', 'kingler').learnset.rockslide;
+			delete this.modData('Learnsets', 'salazzle').learnset.ember;
+			delete this.modData('Learnsets', 'salazzle').learnset.fireblast;
+			delete this.modData('Learnsets', 'salazzle').learnset.flameburst;
+			delete this.modData('Learnsets', 'salazzle').learnset.flamecharge;
+			delete this.modData('Learnsets', 'salazzle').learnset.flamethrower;
+			delete this.modData('Learnsets', 'salazzle').learnset.flareblitz;
+			delete this.modData('Learnsets', 'salazzle').learnset.heatwave;
+			delete this.modData('Learnsets', 'salazzle').learnset.incinerate;
+			delete this.modData('Learnsets', 'salazzle').learnset.overheat;
+			delete this.modData('Learnsets', 'salazzle').learnset.willowisp;
+			delete this.modData('Learnsets', 'ariados').learnset.poisonjab;
+			delete this.modData('Learnsets', 'ariados').learnset.poisonsting;
+			delete this.modData('Learnsets', 'ariados').learnset.sludgebomb;
+			delete this.modData('Learnsets', 'ariados').learnset.crosspoison;
+			delete this.modData('Learnsets', 'ariados').learnset.venomdrench;
+			delete this.modData('Learnsets', 'ariados').learnset.venoshock;
+			delete this.modData('Learnsets', 'ariados').learnset.toxicthread;
+			},
+			
+			
+			
+};
