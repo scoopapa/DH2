2e794d333af3e46519e2dcfd5daa005ef0e522b2
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-07-10 02:01:15 -0600

    Bugfixes take 1

--- data/mods/evolutionproject/abilities.ts
+++ data/mods/evolutionproject/abilities.ts
@@ -124,7 +124,9 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 			if (!move.flags['sound'] || target.getAbility().isPermanent || target.ability === 'soundproof' || target.ability === 'truant') return;
 			const oldAbility = target.setAbility('soundproof');
 			if (oldAbility) {
-				this.add('-ability', target, 'Soundproof', '[from] ability: Blowout', '[of] ' + source);
+				this.add('-ability', source, 'Blowout');
+				this.add('-ability', target, 'Soundproof');
+				this.add('-message', `${target.illusion ? target.illusion.name : target.name} lost its hearing!`);
 			}
 		},
 		name: "Blowout",
@@ -144,6 +146,19 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 					pokemon.formeChange('Starly-Crown');
 				}
 			}
+			if (!this.effectData.busted && pokemon.species.id === 'starlycrowncloud') { // this is just to make a dt that only shows up once per Starly
+				const species = this.dex.getSpecies(pokemon.species.name);
+				const abilities = species.abilities;
+				const baseStats = species.baseStats;
+				const type = species.types[0];
+				if (species.types[1]) {
+					const type2 = species.types[1];
+					this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"><img src="https://${Config.routes.client}/sprites/types/${type2}.png" alt="${type2}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities[0] + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
+				} else {
+					this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities[0] + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
+				}
+				this.effectData.busted = true;
+			}
 		},
 		onResidualOrder: 27,
 		onResidual(pokemon) {
@@ -157,6 +172,19 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 					pokemon.formeChange('Starly-Crown');
 				}
 			}
+			if (!this.effectData.busted && pokemon.species.id === 'starlycrowncloud') { // this is just to make a dt that only shows up once per Starly
+				const species = this.dex.getSpecies(pokemon.species.name);
+				const abilities = species.abilities;
+				const baseStats = species.baseStats;
+				const type = species.types[0];
+				if (species.types[1]) {
+					const type2 = species.types[1];
+					this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"><img src="https://${Config.routes.client}/sprites/types/${type2}.png" alt="${type2}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities[0] + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
+				} else {
+					this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities[0] + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
+				}
+				this.effectData.busted = true;
+			}
 		},
 		isPermanent: true,
 		name: "Murmuration",
@@ -167,17 +195,18 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 		shortDesc: "In a double battle, the Pokémon copies its partner's first type.",
 		onUpdate(pokemon) {
 			if (!pokemon.isStarted) return; // should activate *after* Data Mod
-			let type = 'Normal'; // default to Normal just in case
-			if (
-				this.dex.getSpecies(pokemon.species.name) && this.dex.getSpecies(pokemon.species.name).types[0]
-			) type = this.dex.getSpecies(pokemon.species.name).types[0]; // default to the correct primary type for the species if there is no ally
+			let types = pokemon.baseSpecies.types;
 			for (const ally of pokemon.side.active) {
-				if (ally !== pokemon && !ally.hasAbility('scaleshift')) {
-					type = ally.types[0];
+				if (ally !== pokemon && !ally.hasAbility('scaleshift') && !pokemon.hasType(ally.types[0])) {
+					types.types[0] = ally.types[0];
 				}
 			}
-			if (pokemon.hasType(type)) return;
-			pokemon.types[0] = type;
+			if (!pokemon.setType(types)) return;
+			this.add('-ability', pokemon, 'Scale Shift');
+			this.add('-start', pokemon, 'typechange', pokemon.getTypes(true).join('/'));
+		},
+		onEnd(pokemon) {
+			if (!pokemon.setType(pokemon.baseSpecies.types)) return;
 			this.add('-ability', pokemon, 'Scale Shift');
 			this.add('-start', pokemon, 'typechange', pokemon.getTypes(true).join('/'));
 		},
@@ -246,7 +275,7 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 			if (pokemon.transformed) return;
 			let targetForme = null;
 			if (pokemon.species.baseSpecies === 'Morpeko') targetForme = pokemon.species.name === 'Morpeko' ? 'Morpeko-Hangry' : 'Morpeko';
-			if (pokemon.species.name.startsWith('Klefki-Galar')) targetForme = pokemon.species.name === 'Klefki-Galar' ? 'Klefki-Galar-Revealed' : 'Klefki-Galar';
+			if (pokemon.species.name === 'Klefki-Galar' || pokemon.species.name === 'Klefki-Galar-Revealed') targetForme = pokemon.species.name === 'Klefki-Galar' ? 'Klefki-Galar-Revealed' : 'Klefki-Galar';
 			if (targetForme) {
 				pokemon.formeChange(targetForme);
 				if (!this.effectData.busted && pokemon.species.baseSpecies !== 'Morpeko') { // this is just to make a dt that only shows up once per Klefki
@@ -266,6 +295,7 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 				return;
 			}
 		},
+		isPermanent: true,
 		name: "Hunger Switch",
 		rating: 1,
 		num: 258,
