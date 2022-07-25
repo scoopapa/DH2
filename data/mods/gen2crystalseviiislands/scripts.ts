c8e6b1da5434d9de68402ec977392388999721fe
Rezzo64 <34076225+Rezzo64@users.noreply.github.com>
2022-07-25 15:44:03 +0100

    gen2csi: remove duplicate phys spec split function

--- data/mods/gen2crystalseviiislands/scripts.ts
+++ data/mods/gen2crystalseviiislands/scripts.ts
@@ -6,18 +6,7 @@ export const Scripts: ModdedBattleScriptsData = {
 
 	
 	init: function () {
-		
-		const specialTypes = ['Fire', 'Water', 'Grass', 'Ice', 'Electric', 'Dark', 'Psychic', 'Dragon'];
-		let newCategory = '';
-		for (const i in this.data.Moves) {
-			if (!this.data.Moves[i]) console.log(i);
-			if (this.data.Moves[i].category === 'Status') continue;
-			newCategory = specialTypes.includes(this.data.Moves[i].type) ? 'Special' : 'Physical';
-			if (newCategory !== this.data.Moves[i].category) {
-				this.modData('Moves', i).category = newCategory;
-			}
-		}
-		
+				
 		for (const id in this.dataCache.Pokedex) {
 			const pokemon = this.dataCache.Pokedex[id];
 			if (this.modData('FormatsData', id)) {
