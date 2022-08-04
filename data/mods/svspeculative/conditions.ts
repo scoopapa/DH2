1a29ccc42500511af573b5908ba3dc37f24628b0
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-08-03 21:37:45 -0600

    Update conditions.ts

--- data/mods/svspeculative/conditions.ts
+++ data/mods/svspeculative/conditions.ts
@@ -1,11 +1,4 @@
 export const Conditions: {[k: string]: ConditionData} = {
-	terastal: {
-		name: 'terastal',
-		duration: 0,
-		onModifyMove(move) {
-			if (this.effectData.target.m.teraboost) move.stab = 2;
-		},
-	},
 	frz: {
 		name: 'frz',
 		effectType: 'Status',
