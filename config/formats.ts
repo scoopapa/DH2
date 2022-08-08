414d58ccd25726e2b0d3dcc00a7b5b611213d632
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2022-08-08 02:50:23 +0100

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -3531,7 +3531,7 @@ export const Formats: FormatList = [
 		banlist: ['Uber', 'Arena Trap', 'Baton Pass', 'Sand Veil', 'Snow Cloak'],
 	},
 	{
-		name: "[Gen 1] Kanto Expansion Pak",
+		name: "[Gen 1] Kanto Expansion Pak OU",
 		threads: [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/solomods-megathread.3660004/post-9233581">Post in Solomods Megathread</a>`,
       	`&bullet; <a href="https://docs.google.com/spreadsheets/d/1TpTH9ksUWOOJJQK69RIcvnot_mH_JvfGfM4zra6V3Ec/edit?usp=sharing">Spreadsheet</a>`,
@@ -3539,6 +3539,15 @@ export const Formats: FormatList = [
 		mod: 'gen1expansionpack',
 		ruleset: ['Standard', 'Data Mod', 'Welcome Message'],
 		banlist: ['Uber'],
+	},
+		{
+		name: "[Gen 1] Kanto Expansion Pak Ubers",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/solomods-megathread.3660004/post-9233581">Post in Solomods Megathread</a>`,
+      	`&bullet; <a href="https://docs.google.com/spreadsheets/d/1TpTH9ksUWOOJJQK69RIcvnot_mH_JvfGfM4zra6V3Ec/edit?usp=sharing">Spreadsheet</a>`,
+		],
+		mod: 'gen1expansionpack',
+		ruleset: ['Standard', 'Data Mod', 'Welcome Message'],
 	},
 	{
       name: "[Gen 8] Mememons",
