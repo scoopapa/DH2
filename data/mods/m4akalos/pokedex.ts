d9ec3f5db8162394cbfb41e4093afe76484613fa
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-08-21 22:39:04 -0600

    Making this easier on myself

--- data/mods/m4akalos/pokedex.ts
+++ data/mods/m4akalos/pokedex.ts
@@ -4,7 +4,8 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 
 	gogoat: {
 		inherit: true,
-		mega: "Gogoat-Mega",
+		mega: "gogoatmega",
+		megaName: "Gogoat-Mega",
 		megaType: ["Grass"],
 		megaAbility: {0: "Speed Boost"},
 		megaStats: {hp: 123, atk: 135, def: 77, spa: 142, spd: 106, spe: 48},
@@ -13,7 +14,8 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 
 	pyroar: {
 		inherit: true,
-		mega: "Pyroar-Mega",
+		mega: "pyroarmega",
+		megaName: "Pyroar-Mega",
 		megaType: ["Fire", "Normal"],
 		megaAbility: {0: "Pounce"},
 		megaStats: {hp: 86, atk: 68, def: 119, spa: 139, spd: 66, spe: 119},
@@ -24,7 +26,8 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 
 	clawitzer: {
 		inherit: true,
-		mega: "Clawitzer-Mega",
+		mega: "clawitzermega",
+		megaName: "Clawitzer-Mega",
 		megaType: ["Water", "Poison"],
 		megaAbility: {0: "Download"},
 		megaStats: {hp: 71, atk: 92, def: 110, spa: 127, spd: 111, spe: 89},
