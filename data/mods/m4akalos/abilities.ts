9e3b69da9080dd0243f9d215b15752aeed3ddad2
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-08-21 22:53:46 -0600

    Pounce

--- data/mods/m4akalos/abilities.ts
+++ data/mods/m4akalos/abilities.ts
@@ -1,1 +1,30 @@
-// test
+export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
+	pounce: {
+		desc: "This Pokémon is immune to all entry hazards. If it lands on any type of entry hazard, it lowers the Sp. Def of adjacent opponents.",
+		shortDesc: "Hazard immunity. Lowers adjacent opponents' Sp. Def by 1 stage if switched in on them.",
+		onStart(pokemon) {
+			let activated = false;
+			for (const sideCondition of ['gmaxsteelsurge', 'spikes', 'stealthrock', 'stickyweb', 'toxicspikes']) {
+				if (pokemon.side.getSideCondition(sideCondition)) {
+					for (const target of pokemon.side.foe.active) {
+						if (!target || !this.isAdjacent(target, pokemon)) continue;
+						if (!activated) {
+							this.add('-ability', pokemon, 'Pounce', 'boost');
+							activated = true;
+						}
+						if (target.volatiles['substitute']) {
+							this.add('-immune', target);
+						} else {
+							this.boost({spd: -1}, target, pokemon, null, true);
+						}
+					}
+					return;
+				}
+			}
+		},
+		hazardImmune: true,
+		name: "Pounce",
+		rating: 4,
+		num: -1001,
+	},
+};
