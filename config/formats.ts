7e9b1f709bc04265155bb2c468d4112d596eace2
Duo <63012562+DuoM2@users.noreply.github.com>
2022-08-26 21:50:46 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -23,20 +23,6 @@ export const Formats: FormatList = [
 	},
 	
 	{
-        name: "[Gen 8] leif",
-        desc: `<b>bobolieffakemon</b>: This is your mod!`,
-        mod: 'bobolieffakemon',
-        ruleset: ['Standard NatDex', 'Dynamax Clause', 'Sleep Clause Mod', 'Species Clause', 'Moody Clause', 'Evasion Moves Clause', 'Swagger Clause', 'Baton Pass Clause', 'OHKO Clause', 'Subscribe For More Content'],
-        unbanlist: [
-             'breroot', 'furnostrich', 'sholossus', 'espopod', 'croissansect', 'piedraderm', 'kakavo', 'pelli', 'dripig', 'harmoth', 'formagia', 'sodia', 'dripig', 'woosher', 'gurso', 'phelpinch', 'shiah', 'gillomen', 'uriotl', 'cuzima', 'themon', 'shocbrute', 'lightnimbus', 'cephalethal', 'malizor', 'lunheron', 'erverena', 'siltmanya', 'tudek', 'glypdorsa', 'voraciousect', 'pichiri', 'wooliba', 'olfacrid', 'rodiche', 'mowisp', 'cephalopath', 'vambyss', 'mamicky', 'sklea', 'corazone', 'folivora', 'galena', 'grumplet', 'marlion', 'androsmos', 'dustoph', 'aeromo', 'peekoceros', 'aquimanna', 'apparicious', 'quetzadrakon', 'trybas', 'raiga', 'frogaloft',
-			],
-            
-		onSwitchIn(pokemon) {
-            this.add('-start', pokemon, 'typechange', pokemon.species.types.join('/'), '[silent]');
-        },    
-    },
-	
-	{
 		name: "[Gen 8] OU",
 		threads: [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/3666169/">OU Metagame Discussion</a>`,
