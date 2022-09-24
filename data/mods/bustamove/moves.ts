63498b8ea1dd71cf7573adc9d5a6d1013ba05180
Rasdanation <Rasdanation@gmail.com>
2022-09-23 23:38:01 -0400

    Dive Fix 4

--- data/mods/bustamove/moves.ts
+++ data/mods/bustamove/moves.ts
@@ -456,6 +456,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 			return null;
 		},
 		condition: {
+			duration: 1,
 			onSwap(target) {
 				target.side.removeSideCondition('dive');
 				this.runMove('dive2', target, this.getTargetLoc(target.side.foe.active[0], target), null, false, true);
