5acee93ddb21aefcb0012486e61e124c0db61d91
ViZarSmogon <vizarsmogon@gmail.com>
2022-10-10 16:57:19 +0200

    Update moves.ts

--- data/mods/ubermons/moves.ts
+++ data/mods/ubermons/moves.ts
@@ -563,7 +563,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		self: {
 			onHit(source) {
 				source.clearBoosts();
-				this.add('-clearallboost', source);
+				this.add('-clearboost', source);
 				this.hint("Baton Pass can't pass Stat Changes.");
 			}
 		}
