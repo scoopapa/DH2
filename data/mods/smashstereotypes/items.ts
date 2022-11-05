e71f98507b6ceec16eb57ee98d989e2b2fdfafbd
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2022-11-04 23:33:40 -0400

    Update items.ts

--- data/mods/smashstereotypes/items.ts
+++ data/mods/smashstereotypes/items.ts
@@ -449,4 +449,43 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 		},
 		desc: "Protects the holder from the secondary effects of opponent's moves.",
 	},
+	playerpin: {
+		name: "Player Pin",
+		spritenum: 297,
+		fling: {
+			basePower: 10,
+		},
+		onStart(pokemon) {
+			for (const target of pokemon.side.foe.active) {
+				if (!target || target.fainted) continue;
+				if (target.item) {
+					this.add('-item', target, target.getItem().name, '[from] item: Player Pin', '[of] ' + pokemon, '[identify]');
+				}
+			}
+			let warnMoves: (Move | Pokemon)[][] = [];
+			let warnBp = 1;
+			for (const target of pokemon.side.foe.active) {
+				if (target.fainted) continue;
+				for (const moveSlot of target.moveSlots) {
+					const move = this.dex.getMove(moveSlot.move);
+					let bp = move.basePower;
+					if (move.ohko) bp = 150;
+					if (move.id === 'counter' || move.id === 'metalburst' || move.id === 'mirrorcoat') bp = 120;
+					if (bp === 1) bp = 80;
+					if (!bp && move.category !== 'Status') bp = 80;
+					if (bp > warnBp) {
+						warnMoves = [[move, target]];
+						warnBp = bp;
+					} else if (bp === warnBp) {
+						warnMoves.push([move, target]);
+					}
+				}
+			}
+			if (!warnMoves.length) return;
+			const [warnMoveName, warnTarget] = this.sample(warnMoves);
+			this.add('-activate', pokemon, 'item: Player Pin', warnMoveName, '[of] ' + warnTarget);
+		},
+		gen: 8,
+		desc: "Reveals the foe(s)'s item and strongest move on switch-in, unless they are also holding a Player Pin.",
+	},
 };
