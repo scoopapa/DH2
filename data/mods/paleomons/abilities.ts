0999e4c08ea2f11a0c12ffb6ebbc7f759b973366
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2022-11-06 11:07:13 +0700

    Update abilities.ts

--- data/mods/paleomons/abilities.ts
+++ data/mods/paleomons/abilities.ts
@@ -269,8 +269,6 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 		onBeforeMove(target, source, move) {
 			if (!source || source === target || move.category === 'Status' || move.name === "Counter") return;
 			const moveType = move.id === 'hiddenpower' ? target.hpType : move.type;
-			if (move.flags['charge'] && !target.volatiles['twoturnmove']) {
-				this.boost({atk: 1});
 			} /*else if (!this.dex.getImmunity(moveType, source)) {
 				this.boost({atk: 1});
 			}
