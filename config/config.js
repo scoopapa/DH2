15fbafae2b7b55b5bf9f115870bc7fc76c3cf696
Rezzo64 <rezzo64@gmail.com>
2023-08-23 23:29:41 +0100

    ease build load on memory via async and disabling repl

--- config/config.js
+++ config/config.js
@@ -460,7 +460,7 @@ exports.appealurl = '';
  * replsocketprefix - the prefix for the repl sockets to be listening on
  * replsocketmode - the file mode bits to use for the repl sockets
  */
-exports.repl = true;
+exports.repl = false;
 exports.replsocketprefix = './logs/repl/';
 exports.replsocketmode = 0o600;
 
