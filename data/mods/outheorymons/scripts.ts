6bb4b4bff97afb0a4cf002c3c20a981c9eb7a925
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-08-29 16:00:45 +0700

    Update scripts.ts

--- data/mods/outheorymons/scripts.ts
+++ data/mods/outheorymons/scripts.ts
@@ -5,11 +5,11 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 		customTiers: ['Theorymons', 'OU', 'UU', 'RU', 'NU', 'PU', '(PU)'],
 	},
    init() {
-      this.modData('Learnsets', 'tyranitar').learnset.knockoff = ['8L1'];
-      this.modData('Learnsets', 'salamence').learnset.defog = ['8L1'];
-      this.modData('Learnsets', 'delphox').learnset.moonblast = ['8L1'];
-      this.modData('Learnsets', 'slitherwing').learnset.spikes = ['8L1'];
-      this.modData('Learnsets', 'taurospaldeacombat').learnset.playrough = ['8L1'];
-      this.modData('Learnsets', 'cloyster').learnset.bonerush = ['8L1'];
+      this.modData('Learnsets', 'tyranitar').learnset.knockoff = ['9L1'];
+      this.modData('Learnsets', 'salamence').learnset.defog = ['9L1'];
+      this.modData('Learnsets', 'delphox').learnset.moonblast = ['9L1'];
+      this.modData('Learnsets', 'slitherwing').learnset.spikes = ['9L1'];
+      this.modData('Learnsets', 'taurospaldeacombat').learnset.playrough = ['9L1'];
+      this.modData('Learnsets', 'cloyster').learnset.bonerush = ['9L1'];
 	},
 };
