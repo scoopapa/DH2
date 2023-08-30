462771f3abd6bc5ff3ed1a55e32278b3679da050
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-08-30 15:10:05 +0700

    Update scripts.ts

--- data/mods/returntoorretercera/scripts.ts
+++ data/mods/returntoorretercera/scripts.ts
@@ -3,5 +3,87 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 	teambuilderConfig: {
 		excludeStandardTiers: true,
 		customTiers: ["TERCERA", "TERCERA NFE"],
+   },
+	// sim edits
+	pokemon: {
+		setStatus(
+			status: string | Condition,
+			source: Pokemon | null = null,
+			sourceEffect: Effect | null = null,
+			ignoreImmunities = false
+		) {
+			if (!this.hp) return false;
+			status = this.battle.dex.conditions.get(status);
+			if (this.battle.event) {
+				if (!source) source = this.battle.event.source;
+				if (!sourceEffect) sourceEffect = this.battle.effect;
+			}
+			if (!source) source = this;
+
+			if (this.status === status.id) {
+				if ((sourceEffect as Move)?.status === this.status) {
+					this.battle.add('-fail', this, this.status);
+				} else if ((sourceEffect as Move)?.status) {
+					this.battle.add('-fail', source);
+					this.battle.attrLastMove('[still]');
+				}
+				return false;
+			}
+
+			if (!ignoreImmunities && status.id &&
+				!(source?.hasAbility([!((source?.hasAbility('corrosion') && source !== this) && ['tox', 'psn'].includes(status.id)))
+               || ([!((source?.hasAbility('combustion') && source !== this) && ['brn'].includes(status.id)))) {) {
+				// the game currently never ignores immunities
+				if (!this.runStatusImmunity(status.id === 'tox' ? 'psn' || 'brn' : status.id)) {
+					this.battle.debug('immune to status');
+					if ((sourceEffect as Move)?.status) {
+						this.battle.add('-immune', this);
+					}
+					return false;
+				}
+			}
+
+			if (!ignoreImmunities && status.id &&
+				!(source?.hasAbility([!((source?.hasAbility('combustion') && source !== this) && ['brn'].includes(status.id))) {
+				// the game currently never ignores immunities
+				if (!this.runStatusImmunity(status.id === 'brn' : status.id)) {
+					this.battle.debug('immune to status');
+					if ((sourceEffect as Move)?.status) {
+						this.battle.add('-immune', this);
+					}
+					return false;
+				}
+			}
+
+			const prevStatus = this.status;
+			const prevStatusState = this.statusState;
+			if (status.id) {
+				const result: boolean = this.battle.runEvent('SetStatus', this, source, sourceEffect, status);
+				if (!result) {
+					this.battle.debug('set status [' + status.id + '] interrupted');
+					return result;
+				}
+			}
+			
+			this.status = status.id;
+			this.statusState = {id: status.id, target: this};
+			if (source) this.statusState.source = source;
+			if (status.duration) this.statusState.duration = status.duration;
+			if (status.durationCallback) {
+				this.statusState.duration = status.durationCallback.call(this.battle, this, source, sourceEffect);
+			}
+
+			if (status.id && !this.battle.singleEvent('Start', status, this.statusState, this, source, sourceEffect)) {
+				this.battle.debug('status start [' + status.id + '] interrupted');
+				// cancel the setstatus
+				this.status = prevStatus;
+				this.statusState = prevStatusState;
+				return false;
+			}
+			if (status.id && !this.battle.runEvent('AfterSetStatus', this, source, sourceEffect, status)) {
+				return false;
+			}
+			return true;
+		},
 	},
 };
