be8f75c16a70b12a9c8b1bca97d8e798ffb8f5ef
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-08-30 16:42:51 +0700

    Update scripts.ts

--- data/mods/returntoorretercera/scripts.ts
+++ data/mods/returntoorretercera/scripts.ts
@@ -3,5 +3,20 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 	teambuilderConfig: {
 		excludeStandardTiers: true,
 		customTiers: ["TERCERA", "TERCERA NFE"],
-   }
+   },
+	pokemon: {
+	   setStatus(
+	      if (!ignoreImmunities && status.id &&
+				!(source?.hasAbility('combustion') && ['brn'].includes(status.id))) {
+			// the game currently never ignores immunities
+		      if (!this.runStatusImmunity(status.id === 'brn' : status.id)) {
+				   this.battle.debug('immune to status');
+				   if ((sourceEffect as Move)?.status) {
+					   this.battle.add('-immune', this);
+			      }
+				   return false;
+				}
+			}
+		},
+	},
 };
