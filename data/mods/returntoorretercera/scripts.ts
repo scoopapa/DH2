05f9a7eb2c42090e571d753eacd5e4cb1b1c9a00
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-08-30 14:10:47 +0700

    Update scripts.ts

--- data/mods/returntoorretercera/scripts.ts
+++ data/mods/returntoorretercera/scripts.ts
@@ -4,4 +4,18 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 		excludeStandardTiers: true,
 		customTiers: ["TERCERA", "TERCERA NFE"],
 	},
+	init() {
+this.modData('Learnsets', 'dugtrio').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'poliwrath').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'rapidash').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'magneton').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'farfetch'd').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'tauros').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'lapras').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'articuno').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'zapdos').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'swellow').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'manectric').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'lunatone').learnset.shadowsky = ['8L1'];
+this.modData('Learnsets', 'solrock').learnset.shadowsky = ['8L1'];
 };
