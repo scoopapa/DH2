b4e268412b2407f76969dcfb7f83821304f8e038
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-08-30 15:10:42 +0700

    Update scripts.ts

--- data/mods/returntoorretercera/scripts.ts
+++ data/mods/returntoorretercera/scripts.ts
@@ -43,18 +43,6 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 				}
 			}
 
-			if (!ignoreImmunities && status.id &&
-				!(source?.hasAbility([!((source?.hasAbility('combustion') && source !== this) && ['brn'].includes(status.id))) {
-				// the game currently never ignores immunities
-				if (!this.runStatusImmunity(status.id === 'brn' : status.id)) {
-					this.battle.debug('immune to status');
-					if ((sourceEffect as Move)?.status) {
-						this.battle.add('-immune', this);
-					}
-					return false;
-				}
-			}
-
 			const prevStatus = this.status;
 			const prevStatusState = this.statusState;
 			if (status.id) {
