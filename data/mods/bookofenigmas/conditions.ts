c14e27c06f09fed105a53883e46af49a641d4a5b
Lysio4 <lukas.moncorge@gmail.com>
2023-09-13 19:22:30 +0200

    Removing no longer needed code

--- data/mods/bookofenigmas/conditions.ts
+++ data/mods/bookofenigmas/conditions.ts
@@ -1,35 +1,4 @@
 export const Conditions: {[k: string]: ConditionData} = {
-	// hail: {
-	// 	name: 'Hail',
-	// 	effectType: 'Weather',
-	// 	duration: 5,
-	// 	durationCallback(source, effect) {
-	// 		if (source?.hasItem('icyrock')) {
-	// 			return 8;
-	// 		}
-	// 		return 5;
-	// 	},
-	// 	onStart(battle, source, effect) {
-	// 		if (effect?.effectType === 'Ability') {
-	// 			if (this.gen <= 5) this.effectState.duration = 0;
-	// 			this.add('-weather', 'Hail', '[from] ability: ' + effect, '[of] ' + source);
-	// 		} else {
-	// 			this.add('-weather', 'Hail');
-	// 		}
-	// 	},
-	// 	onModifyDefPriority: 10,
-	// 	onModifyDef(def, pokemon) {
-	// 		if (pokemon.hasType('Ice') && this.field.isWeather('hail')) {
-	// 			return this.modify(def, 1.5);
-	// 		}
-	// 	},
-	// 	onWeather(target) {
-	// 		this.damage(target.baseMaxhp / 16);
-	// 	},
-	// 	onEnd() {
-	// 		this.add('-weather', 'none');
-	// 	},
-	// },
 	storm: {
 		name: 'storm',
 		onStart(pokemon) {
