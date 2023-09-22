c738aa9a09dc9a0ee9afd30586561b24c3aaef3d
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-09-22 21:29:48 +0700

    Create formats-data.ts

---
+++ data/mods/bearticphone/formats-data.ts
@@ -0,0 +1,122 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	basalisk: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	beryl: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	bexipine: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	bidenblaster: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	blunderbass: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	combaudit: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	dolsin: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	extremeribbit: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	fanctacee: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	gasket: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	glassupreme: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	handeyebert: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	hourcuuda: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	icybeeyem: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	jubaturbo: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	maraiste: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	medusear: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	octazure: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	ohmyrod: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	osbait: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	ostretch: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	parkyon: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	quackologist: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	retrucable: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	shadelisk: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	slugchuck: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	soupirate: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	strawmmoth: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	unpatroll: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+	wolversnare: {
+		tier: "bear",
+		doublesTier: "bear",
+	},
+};
