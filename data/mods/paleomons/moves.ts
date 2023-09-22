8d20a76683d80355984bcc8911c6281fffa8a141
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-09-22 09:39:20 +0700

    Create moves.ts

---
+++ data/mods/paleomons/moves.ts
@@ -0,0 +1,36 @@
+export const Moves: {[moveid: string]: MoveData} = {
+	bloodstream: {
+		num: 202,
+		accuracy: 100,
+		basePower: 90,
+		category: "Special",
+		name: "Blood Stream",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, heal: 1},
+		drain: [1, 2],
+		secondary: null,
+		target: "normal",
+		type: "Poison",
+		contestType: "Clever",
+	},
+	stormsurge: {
+		num: 874,
+		accuracy: 100,
+		basePower: 120,
+		category: "Special",
+		name: "Storm Surge",
+		pp: 5,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		self: {
+			boosts: {
+				spa: -1,
+			},
+		},
+		secondary: null,
+		target: "allAdjacentFoes",
+		type: "Water",
+		contestType: "Beautiful",
+	},
+};
