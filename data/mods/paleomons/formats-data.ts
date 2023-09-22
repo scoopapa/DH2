61fc8068e7f36cb1847dda4d666791ab3081f5f4
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-09-22 09:58:47 +0700

    Create formats-data.ts

---
+++ data/mods/paleomons/formats-data.ts
@@ -0,0 +1,17 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+  kabutops: {
+    tier: "Paleomons",
+  },
+  omastar: {
+    tier: "Paleomons",
+  },
+  aerodactyl: {
+    tier: "Paleomons",
+  },
+  armaldo: {
+    tier: "Paleomons",
+  },
+  cradily: {
+    tier: "Paleomons",
+  },
+};
