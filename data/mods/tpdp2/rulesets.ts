e6b3591c3c0eb74659972eb5e9fa5de6ad2f5449
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2023-10-07 10:47:58 +0900

    Create rulesets.ts

---
+++ data/mods/tpdp2/rulesets.ts
@@ -0,0 +1,16 @@
+export const Rulesets: {[k: string]: FormatData} = {
+	stylemonsmovelegality: {
+		effectType: 'ValidatorRule',
+		name: 'Stylemons Move Legality',
+		desc: "Allows Puppets to use any move that they or another style learns",
+		checkCanLearn(move, species, setSources, set) {
+			const matchingSpecies = this.dex.species.all()
+				.filter(s => (
+					s.spriteid === species.spriteid && !this.ruleTable.isBannedSpecies(s)
+				));
+			const someCanLearn = matchingSpecies.some(s => this.checkCanLearn(move, s, setSources, set) === null);
+			if (someCanLearn) return null;
+			return this.checkCanLearn(move, species, setSources, set);
+		},
+	},
+}
