ec6422d6ad2b38d26477421fd5d18e32b8b89c19
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-10-29 21:28:28 -0400

    Create conditions.ts

---
+++ data/mods/teramax/conditions.ts
@@ -0,0 +1,70 @@
+export const Conditions: {[k: string]: ConditionData} = {
+	dynamax: {
+		name: 'Dynamax',
+		noCopy: true,
+		durationCallback(source, effect) {
+			if (source.gigantamax) return 0;
+		},
+		onStart(pokemon) {
+			this.effectState.turns = 0;
+			pokemon.removeVolatile('minimize');
+			pokemon.removeVolatile('substitute');
+			if (pokemon.volatiles['torment']) {
+				delete pokemon.volatiles['torment'];
+				this.add('-end', pokemon, 'Torment', '[silent]');
+			}
+			if (['cramorantgulping', 'cramorantgorging'].includes(pokemon.species.id) && !pokemon.transformed) {
+				pokemon.formeChange('cramorant');
+			}
+			this.add('-start', pokemon, 'Dynamax', pokemon.gigantamax ? 'Gmax' : '');
+			if (pokemon.baseSpecies.name === 'Shedinja') return;
+
+			// Changes based on dynamax level, 2 is max (at LVL 10)
+			const ratio = 1.5 + (pokemon.dynamaxLevel * 0.05);
+
+			pokemon.maxhp = Math.floor(pokemon.maxhp * ratio);
+			pokemon.hp = Math.floor(pokemon.hp * ratio);
+			this.add('-heal', pokemon, pokemon.getHealth, '[silent]');
+		},
+		onBasePowerPriority: 30,
+		onBasePower(basePower, attacker, defender, move) {
+			if (move.isMax && move.category !== 'Status' && (move.type === 'Fighting' || move.type === 'Poison')) {
+				return move.basepower - 10;
+			}
+			if (move.isMax && move.category !== 'Status' && (move.type === 'Flying')) {
+				return move.basepower - 40;
+			}
+			else if (move.isMax && move.category !== 'Status' && (move.type !== 'Flying' || move.type !== 'Fighting' || move.type !== 'Poison')) {
+				return move.basepower - 20;
+			}
+		},
+		onTryAddVolatile(status, pokemon) {
+			if (status.id === 'flinch') return null;
+		},
+		onBeforeSwitchOutPriority: -1,
+		onBeforeSwitchOut(pokemon) {
+			pokemon.removeVolatile('dynamax');
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.id === 'behemothbash' || move.id === 'behemothblade' || move.id === 'dynamaxcannon') {
+				return this.chainModify(2);
+			}
+		},
+		onDragOutPriority: 2,
+		onDragOut(pokemon) {
+			this.add('-block', pokemon, 'Dynamax');
+			return null;
+		},
+		onResidualPriority: -100,
+		onResidual() {
+			this.effectState.turns++;
+		},
+		onEnd(pokemon) {
+			this.add('-end', pokemon, 'Dynamax');
+			if (pokemon.baseSpecies.name === 'Shedinja') return;
+			pokemon.hp = pokemon.getUndynamaxedHP();
+			pokemon.maxhp = pokemon.baseMaxhp;
+			this.add('-heal', pokemon, pokemon.getHealth, '[silent]');
+		},
+	},
+};
