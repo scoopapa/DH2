84e92c60dd537c4e88d1f0467f1967ff2f222e97
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-10-31 21:15:32 -0400

    Create moves.ts

---
+++ data/mods/teramax/moves.ts
@@ -0,0 +1,25 @@
+export const Moves: {[k: string]: ModdedMoveData} = {
+	ragefist: {
+		num: 889,
+		shortDesc: "+25 power for every time the user is hit. Recoil damage equal to BP.",
+		accuracy: 100,
+		basePower: 50,
+		basePowerCallback(pokemon) {
+			return Math.min(350, 50 + 25 * pokemon.timesAttacked);
+		},
+		category: "Physical",
+		name: "Rage Fist",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1, punch: 1},
+		self: {
+			onHit(pokemon) {
+			  let bp = move.basePower;
+					this.damage(bp, pokemon, pokemon);
+			},
+		},
+		secondary: null,
+		target: "normal",
+		type: "Ghost",
+	},
+};
