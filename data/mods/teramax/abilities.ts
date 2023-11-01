732031f98ed8bb1e100db65f9d040a5272c06c4b
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-11-01 00:04:33 -0400

    Update abilities.ts

--- data/mods/teramax/abilities.ts
+++ data/mods/teramax/abilities.ts
@@ -36,4 +36,47 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		shortDesc: "Changes this Pokemon's form to Zen Mode before using a Special move.",
 		num: 161,
 	},
+	gorillatactics: {
+		onStart(pokemon) {
+			pokemon.abilityState.choiceLock = "";
+			pokemon.addVolatile('embargo');
+		},
+		onBeforeMove(pokemon, target, move) {
+			if (move.isZOrMaxPowered || move.id === 'struggle') return;
+			if (pokemon.abilityState.choiceLock && pokemon.abilityState.choiceLock !== move.id) {
+				// Fails unless ability is being ignored (these events will not run), no PP lost.
+				this.addMove('move', pokemon, move.name);
+				this.attrLastMove('[still]');
+				this.debug("Disabled by Gorilla Tactics");
+				this.add('-fail', pokemon);
+				return false;
+			}
+		},
+		onModifyMove(move, pokemon) {
+			if (pokemon.abilityState.choiceLock || move.isZOrMaxPowered || move.id === 'struggle') return;
+			pokemon.abilityState.choiceLock = move.id;
+		},
+		onModifyAtkPriority: 1,
+		onModifyAtk(atk, pokemon) {
+			if (pokemon.volatiles['dynamax']) return;
+			// PLACEHOLDER
+			this.debug('Gorilla Tactics Atk Boost');
+			return this.chainModify(1.5);
+		},
+		onDisableMove(pokemon) {
+			if (!pokemon.abilityState.choiceLock) return;
+			if (pokemon.volatiles['dynamax']) return;
+			for (const moveSlot of pokemon.moveSlots) {
+				if (moveSlot.id !== pokemon.abilityState.choiceLock) {
+					pokemon.disableMove(moveSlot.id, false, this.effectState.sourceEffect);
+				}
+			}
+		},
+		onEnd(pokemon) {
+			pokemon.abilityState.choiceLock = "";
+		},
+		name: "Gorilla Tactics",
+		rating: 4.5,
+		num: 255,
+	},
 };
