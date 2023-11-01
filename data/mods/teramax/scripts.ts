aec58d2ac1abf8535f069dbd30e6b75b007214e1
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-10-31 23:44:30 -0400

    Update scripts.ts

--- data/mods/teramax/scripts.ts
+++ data/mods/teramax/scripts.ts
@@ -6,30 +6,7 @@ export const Scripts: ModdedBattleScriptsData = {
 	},
 	actions: {
 	inherit: true,
-		getActiveMaxMove(move: Move, pokemon: Pokemon) {
-			if (typeof move === 'string') move = this.dex.getActiveMove(move);
-			if (move.name === 'Struggle') return this.dex.getActiveMove(move);
-			let maxMove = this.dex.getActiveMove(this.MAX_MOVES[move.category === 'Status' ? move.category : move.type]);
-			if (move.category !== 'Status') {
-				if (pokemon.gigantamax && pokemon.canGigantamax) {
-					const gMaxMove = this.dex.getActiveMove(pokemon.canGigantamax);
-					if (gMaxMove.exists && gMaxMove.type === move.type) maxMove = gMaxMove;
-				}
-				if (!move.maxMove?.basePower) throw new Error(`${move.name} doesn't have a maxMove basePower`);
-				if (!['gmaxdrumsolo', 'gmaxfireball', 'gmaxhydrosnipe'].includes(maxMove.id)) {
-					maxMove.basePower = move.maxMove.basePower;
-				}
-				maxMove.category = move.category;
-			}
-			let maxNewPower = this.newMaxPower(move); // new max power
-			maxMove.basePower = maxNewPower; // bypass old max power
-			maxMove.baseMove = move.id;
-			// copy the priority for Psychic Terrain, Quick Guard
-			maxMove.priority = move.priority;
-			maxMove.isZOrMaxPowered = true;
-			return maxMove;
-		}
-		newMaxPower(move){
+		newMaxPower(move) {
 			let oldMaxPowers = [100, 110, 120, 130, 140, 150];
 			let oldweakMaxPowers = [70, 80, 85, 90, 95, 100];
 			let weakMaxPowers = [60, 70, 75, 80, 85, 90];
