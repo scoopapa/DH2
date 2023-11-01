7c59441dc8a030b933c144029e847e4c1e817e56
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-11-01 17:47:34 -0400

    Update scripts.ts

--- data/mods/teramax/scripts.ts
+++ data/mods/teramax/scripts.ts
@@ -511,4 +511,37 @@ export const Scripts: ModdedBattleScriptsData = {
 			return true;
 		},
 	},
+	pokemon: {
+	inherit: true,
+		getDynamaxRequest(skipChecks?: boolean) {
+			// {gigantamax?: string, maxMoves: {[k: string]: string} | null}[]
+			if (!skipChecks) {
+				if (!this.side.canDynamaxNow()) return;
+				if (
+					this.species.isMega || this.species.isPrimal || this.species.forme === "Ultra" || this.canMegaEvo
+				) {
+					return;
+				}
+				// Some pokemon species are unable to dynamax
+				if (this.species.cannotDynamax || this.illusion?.species.cannotDynamax) return;
+			}
+			const result: DynamaxOptions = {maxMoves: []};
+			let atLeastOne = false;
+			for (const moveSlot of this.moveSlots) {
+				const move = this.battle.dex.moves.get(moveSlot.id);
+				const maxMove = this.battle.actions.getMaxMove(move, this);
+				if (maxMove) {
+					if (this.maxMoveDisabled(move)) {
+						result.maxMoves.push({move: maxMove.id, target: maxMove.target, disabled: true});
+					} else {
+						result.maxMoves.push({move: maxMove.id, target: maxMove.target});
+						atLeastOne = true;
+					}
+				}
+			}
+			if (!atLeastOne) return;
+			if (this.canGigantamax) result.gigantamax = this.canGigantamax;
+			return result;
+		},		
+	},
 };
