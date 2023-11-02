6f778e5ebe49deb1bd4e5c8071266fc9b968401c
Lysio4 <lukas.moncorge@gmail.com>
2023-11-02 19:44:51 +0100

    Actually fixing Divination

--- data/mods/agoldenexperience/abilities.ts
+++ data/mods/agoldenexperience/abilities.ts
@@ -230,7 +230,7 @@ export const Abilities: { [abilityid: string]: ModdedAbilityData; } = {
 				for (const moveSlot of target.moveSlots) {
 					if (revealed.includes(moveSlot)) continue;
 					if (r === 0) {
-						this.add('-message', `${(target.illusion ? target.illusion.name : target.name)} knows the move ${this.dex.getMove(moveSlot.move).name}!`);
+						this.add('-message', `${(target.illusion ? target.illusion.name : target.name)} knows the move ${this.dex.moves.get(moveSlot.move).name}!`);
 					}
 					r--;
 					revealed.push(moveSlot);
