4c7dcb12000723de46374cfb846885d4d8faede6
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-11-03 15:33:20 -0400

    Update pokedex.ts

--- data/mods/teramax/pokedex.ts
+++ data/mods/teramax/pokedex.ts
@@ -27,4 +27,142 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		baseStats: {hp: 130, atk: 115, def: 70, spa: 75, spd: 50, spe: 95},
 		abilities: {0: "Vital Spirit", 1: "Cursed Body", H: "Iron Fist"},
 	},
+
+// GMaxes
+	venusaurgmax: {
+		inherit: true,
+		abilities: {0: "Flower Veil"},
+	},
+	charizardgmax: {
+		inherit: true,
+		abilities: {0: "Mold Breaker"},
+	},
+	blastoisegmax: {
+		inherit: true,
+		abilities: {0: "Bulletproof"},
+	},
+	butterfreegmax: {
+		inherit: true,
+		abilities: {0: "Magic Bounce"},
+	},
+	meowthgmax: {
+		inherit: true,
+		abilities: {0: "Limber"},
+	},
+	pikachugmax: {
+		inherit: true,
+		abilities: {0: "Huge Power"},
+	},
+	machampgmax: {
+		inherit: true,
+		abilities: {0: "Scrappy"},
+	},
+	gengargmax: {
+		inherit: true,
+		abilities: {0: "Mummy"},
+	},
+	kinglergmax: {
+		inherit: true,
+		abilities: {0: "Anger Shell"},
+	},
+	laprasgmax: {
+		inherit: true,
+		abilities: {0: "Ice Scales"},
+	},
+	eeveegmax: {
+		inherit: true,
+		abilities: {0: "Friend Guard"},
+	},
+	snorlaxgmax: {
+		inherit: true,
+		abilities: {0: "Sap Sipper"},
+	},
+	garbodorgmax: {
+		inherit: true,
+		abilities: {0: "Toxic Debris"},
+	},
+	melmetalgmax: {
+		inherit: true,
+		abilities: {0: "Liquid Ooze"},
+	},
+	rillaboomgmax: {
+		inherit: true,
+		abilities: {0: "Grass Pelt"},
+	},
+	cinderacegmax: {
+		inherit: true,
+		abilities: {0: "Quick Feet"},
+	},
+	inteleongmax: {
+		inherit: true,
+		abilities: {0: "Storm Drain"},
+	},
+	corviknightgmax: {
+		inherit: true,
+		abilities: {0: "Iron Barbs"},
+	},
+	orbeetlegmax: {
+		inherit: true,
+		abilities: {0: "Magic Guard"},
+	},
+	drednawgmax: {
+		inherit: true,
+		abilities: {0: "Solid Rock"},
+	},
+	coalossalgmax: {
+		inherit: true,
+		abilities: {0: "Earth Eater"},
+	},
+	flapplegmax: {
+		inherit: true,
+		abilities: {0: "Supersweet Syrup"},
+	},
+	appletungmax: {
+		inherit: true,
+		abilities: {0: "Supersweet Syrup"},
+	},
+	sandacondagmax: {
+		inherit: true,
+		abilities: {0: "Sand Rush"},
+	},
+	toxtricitygmax: {
+		inherit: true,
+		abilities: {0: "Electromorphosis"},
+	},
+	toxtricitylowkeygmax: {
+		inherit: true,
+		abilities: {0: "Electromorphosis"},
+	},
+	centiskorchgmax: {
+		inherit: true,
+		abilities: {0: "Unnerve"},
+	},
+	hatterenegmax: {
+		inherit: true,
+		abilities: {0: "Natural Cure"},
+	},
+	grimmsnarlgmax: {
+		inherit: true,
+		abilities: {0: "Tangling Hair"},
+	},
+	alcremiegmax: {
+		inherit: true,
+		abilities: {0: "Well-Baked Body"},
+	},
+	copperajahgmax: {
+		inherit: true,
+		abilities: {0: "Stamina"},
+	},
+	duraludongmax: {
+		inherit: true,
+		abilities: {0: "Pressure"},
+	},
+	urshifugmax: {
+		inherit: true,
+		abilities: {0: "Inner Focus"},
+	},
+	urshifurapidstrikegmax: {
+		inherit: true,
+		abilities: {0: "Inner Focus"},
+	},
 };
