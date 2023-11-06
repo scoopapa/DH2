e5afd334c77b8865939b788430ee1190c973d2cd
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2023-11-06 23:41:05 +0900

    Revert "REALLY BIG CHANGE"
    This reverts commit f956efe4da6dc8d0476489343deeaa9ada026da0.

--- data/mods/sm/scripts.ts
+++ data/mods/sm/scripts.ts
@@ -1,6 +1,6 @@
 import {Utils} from '../../../lib';
 export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
-	gen: 9,
+	inherit: 'gen9',
 	teambuilderConfig: {
 		// for micrometas to only show custom tiers
 		excludeStandardTiers: true,
