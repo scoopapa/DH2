908e9f1c5a481fbbf50f3c529092362e62f95321
KirbyRider1337 <39121695+KirbyRider1337@users.noreply.github.com>
2023-11-13 21:16:52 -0500

    Also slipped this in for Choreography

--- data/mods/feou/scripts.ts
+++ data/mods/feou/scripts.ts
@@ -54,111 +54,130 @@ export const Scripts: ModdedBattleScriptsData = {
 				return "Necrotrik-Ultra";
 			}
 			return null;
-		}, /*
-		runMove(moveOrMoveName, pokemon, targetLoc, sourceEffect, zMove, externalMove, maxMove, originalTarget) {
-			pokemon.activeMoveActions++;
-			let target = this.battle.getTarget(pokemon, maxMove || zMove || moveOrMoveName, targetLoc, originalTarget);
-			let baseMove = this.dex.getActiveMove(moveOrMoveName);
-			const pranksterBoosted = baseMove.pranksterBoosted;
-			if (baseMove.id !== 'struggle' && !zMove && !maxMove && !externalMove) {
-				const changedMove = this.battle.runEvent('OverrideAction', pokemon, target, baseMove);
-				if (changedMove && changedMove !== true) {
-					baseMove = this.dex.getActiveMove(changedMove);
-					if (pranksterBoosted) baseMove.pranksterBoosted = pranksterBoosted;
-					target = this.battle.getRandomTarget(pokemon, baseMove);
+		},
+
+		/*
+  			runMove(moveOrMoveName, pokemon, targetLoc, sourceEffect, zMove, externalMove, maxMove, originalTarget) {
+				pokemon.activeMoveActions++;
+				let target = this.battle.getTarget(pokemon, maxMove || zMove || moveOrMoveName, targetLoc, originalTarget);
+				let baseMove = this.dex.getActiveMove(moveOrMoveName);
+				const pranksterBoosted = baseMove.pranksterBoosted;
+				if (baseMove.id !== 'struggle' && !zMove && !maxMove && !externalMove) {
+					const changedMove = this.battle.runEvent('OverrideAction', pokemon, target, baseMove);
+					if (changedMove && changedMove !== true) {
+						baseMove = this.dex.getActiveMove(changedMove);
+						if (pranksterBoosted) baseMove.pranksterBoosted = pranksterBoosted;
+						target = this.battle.getRandomTarget(pokemon, baseMove);
+					}
 				}
-			}
-			let move = baseMove;
-			if (zMove) {
-				move = this.battle.getActiveZMove(baseMove, pokemon);
-			} else if (maxMove) {
-				move = this.battle.getActiveMaxMove(baseMove, pokemon);
-			}
-	
-			move.isExternal = externalMove;
-	
-			this.battle.setActiveMove(move, pokemon, target);
-			const willTryMove = this.battle.runEvent('BeforeMove', pokemon, target, move);
-			if (!willTryMove) {
-				this.battle.runEvent('MoveAborted', pokemon, target, move);
-				this.battle.clearActiveMove(true);
-				// The event 'BeforeMove' could have returned false or null
-				// false indicates that this counts as a move failing for the purpose of calculating Stomping Tantrum's base power
-				// null indicates the opposite, as the Pokemon didn't have an option to choose anything
-				pokemon.moveThisTurnResult = willTryMove;
-				return;
-			}
-			if (move.beforeMoveCallback) {
-				if (move.beforeMoveCallback.call(this, pokemon, target, move)) {
+				let move = baseMove;
+				if (zMove) {
+					move = this.getActiveZMove(baseMove, pokemon);
+				} else if (maxMove) {
+					move = this.getActiveMaxMove(baseMove, pokemon);
+				}
+		
+				move.isExternal = externalMove;
+		
+				this.battle.setActiveMove(move, pokemon, target);
+		
+				const willTryMove = this.battle.runEvent('BeforeMove', pokemon, target, move);
+				if (!willTryMove) {
+					this.battle.runEvent('MoveAborted', pokemon, target, move);
 					this.battle.clearActiveMove(true);
-					pokemon.moveThisTurnResult = false;
+					// The event 'BeforeMove' could have returned false or null
+					// false indicates that this counts as a move failing for the purpose of calculating Stomping Tantrum's base power
+					// null indicates the opposite, as the Pokemon didn't have an option to choose anything
+					pokemon.moveThisTurnResult = willTryMove;
 					return;
 				}
-			}
-			pokemon.lastDamage = 0;
-			let lockedMove;
-			if (!externalMove) {
-				lockedMove = this.battle.runEvent('LockMove', pokemon);
-				if (lockedMove === true) lockedMove = false;
-				if (!lockedMove) {
-					if (!pokemon.deductPP(baseMove, null, target) && (move.id !== 'struggle')) {
-						this.battle.add('cant', pokemon, 'nopp', move);
-						const gameConsole = [
-							null, 'Game Boy', 'Game Boy Color', 'Game Boy Advance', 'DS', 'DS', '3DS', '3DS',
-						][this.battle.gen] || 'Switch';
-						this.battle.hint(`This is not a bug, this is really how it works on the ${gameConsole}; try it yourself if you don't believe us.`);
+		
+				// Used exclusively for a hint later
+				if (move.flags['cantusetwice'] && pokemon.lastMove?.id === move.id) {
+					pokemon.addVolatile(move.id);
+				}
+		
+				if (move.beforeMoveCallback) {
+					if (move.beforeMoveCallback.call(this.battle, pokemon, target, move)) {
 						this.battle.clearActiveMove(true);
 						pokemon.moveThisTurnResult = false;
 						return;
 					}
-				} else {
-					sourceEffect = this.dex.getEffect('lockedmove');
 				}
-				pokemon.moveUsed(move, targetLoc);
-			}
-	
-			// Dancer Petal Dance hack
-			// TODO: implement properly
-			const noLock = externalMove && !pokemon.volatiles['lockedmove'];
-	
-			if (zMove) {
-				if (pokemon.illusion) {
-					this.battle.singleEvent('End', this.dex.abilities.get('Illusion'), pokemon.abilityData, pokemon);
+				pokemon.lastDamage = 0;
+				let lockedMove;
+				if (!externalMove) {
+					lockedMove = this.battle.runEvent('LockMove', pokemon);
+					if (lockedMove === true) lockedMove = false;
+					if (!lockedMove) {
+						if (!pokemon.deductPP(baseMove, null, target) && (move.id !== 'struggle')) {
+							this.battle.add('cant', pokemon, 'nopp', move);
+							this.battle.clearActiveMove(true);
+							pokemon.moveThisTurnResult = false;
+							return;
+						}
+					} else {
+						sourceEffect = this.dex.conditions.get('lockedmove');
+					}
+					pokemon.moveUsed(move, targetLoc);
 				}
-				this.battle.add('-zpower', pokemon);
-				pokemon.side.zMoveUsed = true;
-			}
-			const moveDidSomething = this.useMove(baseMove, pokemon, target, sourceEffect, zMove, maxMove);
-			this.battle.lastSuccessfulMoveThisTurn = moveDidSomething ? this.battle.activeMove && this.battle.activeMove.id : null;
-			if (this.battle.activeMove) move = this.battle.activeMove;
-			this.battle.singleEvent('AfterMove', move, null, pokemon, target, move);
-			this.battle.runEvent('AfterMove', pokemon, target, move);
-	
-			// Dancer's activation order is completely different from any other event, so it's handled separately
-			if (move.flags['dance'] && moveDidSomething && !move.isExternal) {
-				const dancers = [];
-				for (const currentPoke of this.battle.getAllActive()) {
-					if (pokemon === currentPoke) continue;
-					if ((currentPoke.hasAbility(['dancer', 'choreography'])) && !currentPoke.isSemiInvulnerable()) {
-						dancers.push(currentPoke);
+		
+				// Dancer Petal Dance hack
+				// TODO: implement properly
+				const noLock = externalMove && !pokemon.volatiles['lockedmove'];
+		
+				if (zMove) {
+					if (pokemon.illusion) {
+						this.battle.singleEvent('End', this.dex.abilities.get('Illusion'), pokemon.abilityState, pokemon);
+					}
+					this.battle.add('-zpower', pokemon);
+					pokemon.side.zMoveUsed = true;
+				}
+		
+				const oldActiveMove = move;
+		
+				const moveDidSomething = this.useMove(baseMove, pokemon, target, sourceEffect, zMove, maxMove);
+				this.battle.lastSuccessfulMoveThisTurn = moveDidSomething ? this.battle.activeMove && this.battle.activeMove.id : null;
+				if (this.battle.activeMove) move = this.battle.activeMove;
+				this.battle.singleEvent('AfterMove', move, null, pokemon, target, move);
+				this.battle.runEvent('AfterMove', pokemon, target, move);
+				if (move.flags['cantusetwice'] && pokemon.removeVolatile(move.id)) {
+					this.battle.add('-hint', `Some effects can force a Pokemon to use ${move.name} again in a row.`);
+				}
+		
+				// Dancer's activation order is completely different from any other event, so it's handled separately
+				if (move.flags['dance'] && moveDidSomething && !move.isExternal) {
+					const dancers = [];
+					for (const currentPoke of this.battle.getAllActive()) {
+						if (pokemon === currentPoke) continue;
+						if (currentPoke.hasAbility('choreography') && !currentPoke.abilityState.choreography && !currentPoke.isSemiInvulnerable()) {
+							dancers.push(currentPoke);
+						}
+					}
+					// Dancer activates in order of lowest speed stat to highest
+					// Note that the speed stat used is after any volatile replacements like Speed Swap,
+					// but before any multipliers like Agility or Choice Scarf
+					// Ties go to whichever Pokemon has had the ability for the least amount of time
+					dancers.sort(
+						(a, b) => -(b.storedStats['spe'] - a.storedStats['spe']) || b.abilityOrder - a.abilityOrder
+					);
+					const targetOf1stDance = this.battle.activeTarget!;
+					for (const dancer of dancers) {
+						if (this.battle.faintMessages()) break;
+						if (dancer.fainted) continue;
+						this.battle.add('-activate', dancer, 'ability: Choreography');
+						const dancersTarget = !targetOf1stDance.isAlly(dancer) && pokemon.isAlly(dancer) ?
+							targetOf1stDance :
+							pokemon;
+						const dancersTargetLoc = dancer.getLocOf(dancersTarget);
+						this.runMove(move.id, dancer, dancersTargetLoc, this.dex.abilities.get('choreography'), undefined, true);
+						dancer.abilityState.choreography = true;
 					}
 				}
-				// Dancer activates in order of lowest speed stat to highest
-				// Note that the speed stat used is after any volatile replacements like Speed Swap,
-				// but before any multipliers like Agility or Choice Scarf
-				// Ties go to whichever Pokemon has had the ability for the least amount of time
-				dancers.sort(
-					(a, b) => -(b.storedStats['spe'] - a.storedStats['spe']) || b.abilityOrder - a.abilityOrder
-				);
-				for (const dancer of dancers) {
-					if (this.battle.faintMessages()) break;
-					if (dancer.fainted) continue;
-					const dancersTarget = target!.side !== dancer.side && pokemon.side === dancer.side ? target! : pokemon;
-					this.battle.runMove(move.id, dancer, this.getTargetLoc(dancersTarget, dancer), this.dex.abilities.get(dancer.ability), undefined, true);
-				}
-			}
-			if (noLock && pokemon.volatiles['lockedmove']) delete pokemon.volatiles['lockedmove'];
-		}, */
+				if (noLock && pokemon.volatiles['lockedmove']) delete pokemon.volatiles['lockedmove'];
+				this.battle.faintMessages();
+				this.battle.checkWin();
+			},*/
 	},
 	pokemon: { 
 		/*
