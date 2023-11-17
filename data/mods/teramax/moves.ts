edaf1a58fe4d81213491122766510a25d2296103
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-11-17 14:42:08 -0500

    Update moves.ts

--- data/mods/teramax/moves.ts
+++ data/mods/teramax/moves.ts
@@ -16,11 +16,88 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			onHit(pokemon) {
 				let bp = Math.min(350, 50 + 25 * pokemon.timesAttacked);
 				this.damage(bp, pokemon, pokemon);
+				this.add('-message', `Rage Fist currently has a BP of ${bp}!`);
 			},
 		},
 		secondary: null,
 		target: "normal",
 		type: "Ghost",
+	},
+	decorate: {
+		num: 777,
+		shortDesc: "Swaps all stat changes with target.",
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Decorate",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, bypasssub: 1, allyanim: 1},
+		onHit(target, source) {
+			const targetBoosts: SparseBoostsTable = {};
+			const sourceBoosts: SparseBoostsTable = {};
+			let i: BoostID;
+			for (i in target.boosts) {
+				targetBoosts[i] = target.boosts[i];
+				sourceBoosts[i] = source.boosts[i];
+			}
+			target.setBoost(sourceBoosts);
+			source.setBoost(targetBoosts);
+			this.add('-swapboost', source, target, '[from] move: Decorate');
+		},
+		secondary: null,
+		target: "normal",
+		type: "Fairy",
+		zMove: {effect: 'crit2'},
+		contestType: "Clever",
+	},
+	appleacid: {
+		num: 787,
+		shortDesc: "20% poison chance. Recovers 50% dmg dealt.",
+		accuracy: 90,
+		basePower: 80,
+		category: "Special",
+		name: "Matcha Gotcha",
+		pp: 15,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		drain: [1, 2],
+		thawsTarget: true,
+		secondary: {
+			chance: 20,
+			status: 'psn',
+		},
+		target: "allAdjacentFoes",
+		type: "Grass",
+	},
+	gravapple: {
+		num: 788,
+		shortDesc: "100% -1 Def. 1.5x power in Gravity. Sets Gravity if resisted.",
+		accuracy: 100,
+		basePower: 80,
+		category: "Physical",
+		name: "Grav Apple",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		onBasePower(basePower) {
+			if (this.field.getPseudoWeather('gravity')) {
+				return this.chainModify(1.5);
+			}
+		},
+		onHit(pokemon, source) {
+			if (pokemon.getMoveHitData(move).typeMod < 0) {
+				this.field.addPseudoWeather('gravity', source, source.move);
+			}
+		},
+		secondary: {
+			chance: 100,
+			boosts: {
+				def: -1,
+			},
+		},
+		target: "normal",
+		type: "Grass",
 	},
 
 // Max and GMax Moves
