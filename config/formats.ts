d14b1f2bc655c17f575a7ce1f2fe5362218b399d
SDM0 <60699143+SDM0@users.noreply.github.com>
2023-11-17 22:12:38 +0100

    Initial commit of the Eramons mod

--- config/formats.ts
+++ config/formats.ts
@@ -383,6 +383,36 @@ export const Formats: FormatList = [
 		},
 	},
 	{
+		name: "[Gen 9] Eramons",
+		desc: [
+			`<b>Eramons</b>: A Gen 9 Pet Mod based on broad strokes of real-life historical time periods.`,
+		],
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3727769/">Gen 9 Eramons</a>`,
+		],
+		
+		mod: 'eramons',
+		ruleset: ['Standard'],
+		banlist: ['Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Razor Fang', 'Baton Pass', 'Last Respects', 'Shed Tail'],
+		onValidateTeam(team, format, teamHas) {
+			/**@type {{[k: string]: true}} */
+			let speciesTable = {};
+			let era : string[] = [];
+			let allowedTiers = ['ECiv', 'Med', 'PrDay', 'FFut'];
+			for (const set of team) {
+				let template = this.dex.species.get(set.species);
+				let tier = template.tier;
+				if (!allowedTiers.includes(template.tier)) {
+					return [set.species + ' is not legal in Eramons.'];
+				}
+				if (!(era.includes(tier))) {
+					era.push(tier)
+				}
+			}
+			if (era.length > 1) return ['Each Pokemon needs to be from the same era.'];
+		},
+	},
+	{
 		name: "[Gen 9] Fakemon Frontier OU",
 		desc: `<b>[Gen 9] Fakemon Frontier OU</b>: A meta where the only legal Pokemon are community-made Fakemon that follow two of four predetermined "rules."`,
 		threads: [
