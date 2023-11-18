ef2431e83d813303c511d6523c91f3dc3c403d87
Thiediev <chanjection@gmail.com>
2023-11-18 17:22:55 -0600

    add full violet roster

--- data/mods/gen1violetversion/formats-data.ts
+++ data/mods/gen1violetversion/formats-data.ts
@@ -1,1 +1,138 @@
-
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	charizard: {
+		inherit: true,
+		tier: "OU",
+	},
+	blastoise: {
+		inherit: true,
+		tier: "OU",
+	},
+	butterfree: {
+		inherit: true,
+		tier: "OU",
+	},
+	beedrill: {
+		inherit: true,
+		tier: "OU",
+	},
+	pidgeot: {
+		inherit: true,
+		tier: "OU",
+	},
+	arbok: {
+		inherit: true,
+		tier: "OU",
+	},
+	nidoqueen: {
+		inherit: true,
+		tier: "OU",
+	},
+	ninetales: {
+		inherit: true,
+		tier: "OU",
+	},
+	golbat: {
+		inherit: true,
+		tier: "OU",
+	},
+	vileplume: {
+		inherit: true,
+		tier: "OU",
+	},
+	parasect: {
+		inherit: true,
+		tier: "OU",
+	},
+	dugtrio: {
+		inherit: true,
+		tier: "OU",
+	},
+	golduck: {
+		inherit: true,
+		tier: "OU",
+	},
+	poliwrath: {
+		inherit: true,
+		tier: "OU",
+	},
+	alakazam: {
+		inherit: true,
+		tier: "OU",
+	},
+	machamp: {
+		inherit: true,
+		tier: "OU",
+	},
+	magneton: {
+		inherit: true,
+		tier: "OU",
+	},
+	muk: {
+		inherit: true,
+		tier: "OU",
+	},
+	hypno: {
+		inherit: true,
+		tier: "OU",
+	},
+	electrode: {
+		inherit: true,
+		tier: "OU",
+	},
+	tangela: {
+		inherit: true,
+		tier: "OU",
+	},
+	electabuzz: {
+		inherit: true,
+		tier: "OU",
+	},
+	magmar: {
+		inherit: true,
+		tier: "OU",
+	},
+	pinsir: {
+		inherit: true,
+		tier: "OU",
+	},
+	gyarados: {
+		inherit: true,
+		tier: "OU",
+	},
+	flareon: {
+		inherit: true,
+		tier: "OU",
+	},
+	porygon: {
+		inherit: true,
+		tier: "OU",
+	},
+	kabutops: {
+		inherit: true,
+		tier: "OU",
+	},
+	aerodactyl: {
+		inherit: true,
+		tier: "OU",
+	},
+	articuno: {
+		inherit: true,
+		tier: "OU",
+	},
+	moltres: {
+		inherit: true,
+		tier: "OU",
+	},
+	starmie: {
+		inherit: true,
+		tier: "OU",
+	},
+	dragonite: {
+		inherit: true,
+		tier: "OU",
+	},
+	mew: {
+		inherit: true,
+		tier: "OU",
+	},
+};
