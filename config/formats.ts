112b472a9bd5b0bccb0f48abc0a11ae8abbd7b1e
Lysio4 <lukas.moncorge@gmail.com>
2023-11-18 20:51:05 +0100

    Adding a tier

--- config/formats.ts
+++ config/formats.ts
@@ -2791,6 +2791,31 @@ export const Formats: FormatList = [
 		// name: "nonpetmodformats",
 	},
 	{
+		name: "[Gen 9] Black & White 3",
+
+		mod: 'bw3',
+		ruleset: ['Standard NatDex', 'Data Mod'],
+		banlist: ['Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass',
+		'Sceptilite', 'Blazikenite', 'Swampertite', 'Gardevoirite', 'Galladite', 'Alakazite', 'Gyaradosite',
+		'Sablenite', 'Mawilite', 'Aggronite', 'Medichamite', 'Manectite', 'Sharpedonite', 'Cameruptite', 
+		'Altarianite', 'Absolite', 'Glalitite', 'Salamencite', 'Metagrossite', 'Latiasite', 'Latiosite', 
+		'Garchompite', 'Steelixite', 'Beedrillite', 'Pidgeotite', 
+		'Blue Orb', 'Red Orb', //this is just copied from ANL's lol
+		'Beedrill-Mega', 'Pidgeot-Mega', //?????
+	],
+		onValidateTeam(team, format) {
+			/**@type {{[k: string]: true}} */
+			let speciesTable = {};
+			let allowedTiers = ['BW OU', "BW NFE", "BW LC"];
+			for (const set of team) {
+				let template = this.dex.species.get(set.species);
+				if (!allowedTiers.includes(template.tier)) {
+					return [set.species + ' is not legal in Black & White 3 OU.'];
+				}
+			}
+		},
+	},
+	{
 		name: "[Gen 9] Littlest Cup",
 		desc: [
 			"<b>Littlest Cup</b>: A National Dex metagame where only Baby Pokemon are allowed."
