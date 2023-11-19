565a29123d5969762e2415823c3972618ce73dca
Thiediev <chanjection@gmail.com>
2023-11-18 19:09:55 -0600

    implement stat and typing changes

--- data/mods/gen1violetversion/pokedex.ts
+++ data/mods/gen1violetversion/pokedex.ts
@@ -1,1 +1,115 @@
-
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	aerodactyl: {
+		inherit: true,
+		baseStats: {hp:80, atk:95, def:65, spa:60, spd:60, spe:130},
+	},
+	arbok: {
+		inherit: true,
+		baseStats: {hp:60, atk:105, def:69, spa:65, spd:65, spe:100},
+	},
+	beedrill: {
+		inherit: true,
+		baseStats: {hp:65, atk:80, def:40, spa:45, spd:45, spe:130},
+	},
+	blastoise: {
+		inherit: true,
+		types: ["Water", "Ground"],
+	},			
+	butterfree: {
+		inherit: true,
+		types: ["Bug", "Psychic"],
+		baseStats: {hp:70, atk:55, def:60, spa:95, spd:95, spe:80},
+	},
+	charizard: {
+		inherit: true,
+		types: ["Fire", "Dragon"],
+	},
+	dugtrio: {
+		inherit: true,
+		baseStats: {hp:35, atk:110, def:50, spa:70, spd:70, spe:120},
+	},
+	electabuzz: {
+		inherit: true,
+		types: ["Electric", "Fighting"],
+	},
+	electrode: {
+		inherit: true,
+		baseStats: {hp:65, atk:65, def:70, spa:110, spd:110, spe:140},
+	},
+	flareon: {
+		inherit: true,
+		baseStats: {hp:115, atk:130, def:90, spa:110, spd:110, spe:65},
+	},
+	golbat: {
+		inherit: true,
+		baseStats: {hp:85, atk:90, def:80, spa:80, spd:80, spe:90},
+	},
+	golduck: {
+		inherit: true,
+		types: ["Water", "Ghost"],
+		baseStats: {hp:90, atk:82, def:78, spa:80, spd:80, spe:95},
+	},
+	hypno: {
+		inherit: true,
+		baseStats: {hp:85, atk:113, def:70, spa:115, spd:115, spe:67},
+	},
+	kabutops: {
+		inherit: true,
+		baseStats: {hp:70, atk:115, def:135, spa:70, spd:70, spe:80},
+	},
+	machamp: {
+		inherit: true,
+		baseStats: {hp:900, atk:130, def:80, spa:65, spd:65, spe:55},
+	},
+	magmar: {
+		inherit: true,
+		types: ["Fire", "Fighting"],
+		baseStats: {hp:65, atk:95, def:57, spa:85, spd:85, spe:118},
+	},
+	magneton: {
+		inherit: true,
+		types: ["Electric", "Rock"],
+		baseStats: {hp:120, atk:60, def:95, spa:120, spd:120, spe:70},
+	},
+	mew: {
+		inherit: true,
+		baseStats: {hp:70, atk:70, def:70, spa:70, spd:70, spe:70},
+	},			
+	muk: {
+		inherit: true,
+		baseStats: {hp:135, atk:105, def:75, spa:65, spd:65, spe:50},
+	},
+	nidoqueen: {
+		inherit: true,
+		baseStats: {hp:90, atk:92, def:87, spa:75, spd:75, spe:85},
+	},
+	ninetales: {
+		inherit: true,
+		types: ["Fire", "Psychic"],
+	},
+	parasect: {
+		inherit: true,
+		baseStats: {hp:90, atk:115, def:130, spa:80, spd:80, spe:30},
+	},
+	pidgeot: {
+		inherit: true,
+		baseStats: {hp:83, atk:110, def:75, spa:105, spd:105, spe:100},
+	},
+	poliwrath: {
+		inherit: true,
+		baseStats: {hp:90, atk:115, def:95, spa:70, spd:70, spe:70},
+	},
+	porygon: {
+		inherit: true,
+		types: ["Ghost"],
+		baseStats: {hp:75, atk:80, def:80, spa:105, spd:105, spe:60},
+	},			
+	tangela: {
+		inherit: true,
+		baseStats: {hp:85, atk:55, def:115, spa:100, spd:100, spe:60},
+	},
+	vileplume: {
+		inherit: true,
+		baseStats: {hp:80, atk:105, def:85, spa:100, spd:100, spe:80},
+	},
+};
