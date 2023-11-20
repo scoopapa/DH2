917f01e16da7e0c827fb192f8a447bb929c19122
Mintly <nickguin123@gmail.com>
2023-11-20 13:28:37 -0500

    blunderbuzz

--- data/mods/kitchen/pokedex.ts
+++ data/mods/kitchen/pokedex.ts
@@ -8,9 +8,21 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
         abilities: {0: "Sprinter", 1: "Limber", H: "Quick Feet"},
         heightm: 1.6,
         weightkg: 45,
-        color: "Yellow",
+        color: "Brown",
         eggGroups: ["Field", "Human-Like"],
-        gen: 8,
+        gen: 9,
+    },
+    blunderbuzz: {
+        num: 3003,
+        name: "Blunderbuzz",
+        types: ["Bug", "Dragon"],
+        baseStats: {hp: 80, atk: 70, def: 120, spa: 135, spd: 70, spe: 50},
+        abilities: {0: "Battle Armor", 1: "Technician", H: "Mega Launcher"},
+        heightm: 1.9,
+        weightkg: 130,
+        color: "Green",
+        eggGroups: ["Bug", "Water 3"],
+        gen: 9,
     },
 
 };
