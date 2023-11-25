eb2f9f91f3e1dca46e91d48018c22154763c919a
Lysio4 <lukas.moncorge@gmail.com>
2023-11-25 10:53:05 +0100

    Changing BW3

--- config/formats.ts
+++ config/formats.ts
@@ -1916,6 +1916,33 @@ export const Formats: FormatList = [
 		},
 	},
 	{
+		name: "[Gen 9] Black & White 3",
+
+		mod: 'blackandwhite3',
+		teambuilderFormat: 'National Dex',
+		ruleset: ['Standard NatDex', 'Data Mod'],
+		banlist: ['Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass',
+		'Last Respects', 'Shed Tail', 
+		'Sceptilite', 'Blazikenite', 'Swampertite', 'Gardevoirite', 'Galladite', 'Alakazite', 'Gyaradosite',
+		'Sablenite', 'Mawilite', 'Aggronite', 'Medichamite', 'Manectite', 'Sharpedonite', 'Cameruptite', 
+		'Altarianite', 'Absolite', 'Glalitite', 'Salamencite', 'Metagrossite', 'Latiasite', 'Latiosite', 
+		'Garchompite', 'Steelixite', 'Beedrillite', 'Pidgeotite', 
+		'Blue Orb', 'Red Orb', //this is just copied from ANL's lol
+		'Beedrill-Mega', 'Pidgeot-Mega', //?????
+	],
+		onValidateTeam(team, format) {
+			/**@type {{[k: string]: true}} */
+			let speciesTable = {};
+			let allowedTiers = ['BW OU', "BW NFE", "BW LC"];
+			for (const set of team) {
+				let template = this.dex.species.get(set.species);
+				if (!allowedTiers.includes(template.tier)) {
+					return [set.species + ' is not legal in Black & White 3 OU.'];
+				}
+			}
+		},
+	},
+	{
 		name: "[Gen 2] Crystal Legacy",
 		mod: 'gen2crystallegacy',
 		desc: `A Solomod centered around Crystal Legacy.`,
@@ -2863,32 +2890,6 @@ export const Formats: FormatList = [
 		section: "Non-Pet Mod Formats",
 		column: 3,
 		// name: "nonpetmodformats",
-	},
-	{
-		name: "[Gen 9] Black & White 3",
-
-		mod: 'blackandwhite3',
-		ruleset: ['Standard NatDex', 'Data Mod'],
-		banlist: ['Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass',
-		'Last Respects', 'Shed Tail', 
-		'Sceptilite', 'Blazikenite', 'Swampertite', 'Gardevoirite', 'Galladite', 'Alakazite', 'Gyaradosite',
-		'Sablenite', 'Mawilite', 'Aggronite', 'Medichamite', 'Manectite', 'Sharpedonite', 'Cameruptite', 
-		'Altarianite', 'Absolite', 'Glalitite', 'Salamencite', 'Metagrossite', 'Latiasite', 'Latiosite', 
-		'Garchompite', 'Steelixite', 'Beedrillite', 'Pidgeotite', 
-		'Blue Orb', 'Red Orb', //this is just copied from ANL's lol
-		'Beedrill-Mega', 'Pidgeot-Mega', //?????
-	],
-		onValidateTeam(team, format) {
-			/**@type {{[k: string]: true}} */
-			let speciesTable = {};
-			let allowedTiers = ['BW OU', "BW NFE", "BW LC"];
-			for (const set of team) {
-				let template = this.dex.species.get(set.species);
-				if (!allowedTiers.includes(template.tier)) {
-					return [set.species + ' is not legal in Black & White 3 OU.'];
-				}
-			}
-		},
 	},
 	{
 		name: "[Gen 9] Littlest Cup",
