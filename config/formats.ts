f5d09ec4e043ab6f04320900634982e6d4f00012
Lysio4 <lukas.moncorge@gmail.com>
2023-11-26 14:25:57 +0100

    Almost done here

--- config/formats.ts
+++ config/formats.ts
@@ -1908,8 +1908,8 @@ export const Formats: FormatList = [
 			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1YJXE8wUNJijWSfNKIUqgObN5uEVgTliewTluGe0w4Y4/edit?usp=sharing">Spreadsheet for the mod</a>`,
 		],
 		mod: 'agoldenexperience',
-		// gameType: 'doubles',
-		// teambuilderFormat: 'National Dex Doubles',
+		gameType: 'doubles',
+		teambuilderFormat: 'National Dex Doubles',
 		ruleset: ['Flat Rules', '!! Adjust Level = 50', 'VGC Timer', 'Dynamax Clause', 'Data Mod', 'Mega Data Mod'],
 		banlist: [
 			'Mewtwo', 'Mew',
