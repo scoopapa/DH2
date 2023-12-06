78f8de1c93fe3340e5e093dadbe1c942b4283501
Yoshiblaze <rosefias@gmail.com>
2023-12-06 17:16:22 -0500

    .

--- data/mods/moderngen1/rulesets.ts
+++ data/mods/moderngen1/rulesets.ts
@@ -2,8 +2,8 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 	partialtrappingclause: {
 		effectType: 'ValidatorRule',
 		name: 'Partial Trapping Clause',
-		desc: "Bans moves that partially trap the opponent (later gen additions)",
-		banlist: ['Infestation', 'Magma Storm', 'Sand Tomb', 'Snap Trap', 'Thunder Cage', 'Whirlpool'],
+		desc: "Bans moves that partially trap the opponent",
+		banlist: ['Infestation', 'Magma Storm', 'Sand Tomb', 'Snap Trap', 'Thunder Cage', 'Whirlpool', 'Wrap', 'Bind', 'Fire Spin', 'Clamp'],
 		onBegin() {
 			this.add('rule', 'Partial Trapping Clause: Partial Trapping moves are banned');
 		},
@@ -11,18 +11,17 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 	protectclause: {
 		effectType: 'ValidatorRule',
 		name: 'Protect Clause',
-		desc: "Bans moves that protect the user",
-		banlist: ['Protect', 'Detect'/*, 'Baneful Bunker', 'King\u2019s Shield', 'Obstruct', 'Silk Trap', 'Spiky Shield'*/],
+		desc: "Bans Protect and Detect",
+		banlist: ['Protect', 'Detect'],
 		onBegin() {
-			this.add('rule', 'Protect Clause: Protecting moves are banned');
+			this.add('rule', 'Protect Clause: Widely-distributed protecting moves are banned');
 		},
 	},
 	fieldeffectclause: {
 		effectType: 'ValidatorRule',
 		name: 'Field Effect Clause',
 		desc: "Bans moves that set a field effect",
-		banlist: ['Spikes', 'Toxic Spikes', 'Stealth Rock', 'Sticky Web', 'Stone Axe', 'Ceaseless Edge', 'Wonder Room', 
-				'Trick Room', 'Magic Room', 'Lucky Chant', 'Tailwind', 'Safeguard', 'Gravity'],
+		banlist: ['Spikes', 'Toxic Spikes', 'Stealth Rock', 'Sticky Web', 'Stone Axe', 'Ceaseless Edge', 'Wonder Room', 'Trick Room', 'Magic Room', 'Lucky Chant', 'Tailwind', 'Safeguard', 'Gravity'],
 		onBegin() {
 			this.add('rule', 'Field Effect Clause: Field Effects are banned');
 		},
