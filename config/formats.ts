282344b618b2fd794f3ab4105842b183d828fec7
AnalysisGeek <101986458+AnalysisGeek@users.noreply.github.com>
2023-12-06 20:04:31 -0500

    Conquest Dex Solomod
    My Solomod Conquest Dex

--- config/formats.ts
+++ config/formats.ts
@@ -1916,6 +1916,27 @@ export const Formats: FormatList = [
 		},
 	},
 	{
+		name: "[Gen 9] Conquest Dex",
+		mod: 'conquestdex',
+		desc: `A metagame based on the pokemon side game pokemon conquest`,
+		ruleset: ['Standard NatDex', 'Data Mod', 'Terastal Clause', 'Z-Move Clause', 'Mega Stone Clause'],
+		banlist: ['Arceus', 'Dialga', 'Rayquaza', 'Zekrom', 'Reshiram', 'Mewtwo'],
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/solomods-megathread.3711007/post-9883990">Conquest Dex on Smogon Forums</a>`,
+		],
+		onValidateTeam(team, format) {
+			/**@type {{[k: string]: true}}*/
+			let speciesTable = {};
+			let allowedTiers = ['Conq'];
+			for (const set of team) {
+				let template = this.dex.species.get(set.species);
+				if (!allowedTiers.includes(template.tier)) {
+					return [set.species + ' is not legal in Conquest Dex.'];
+				}
+			}
+		},
+	},
+	{
 		name: "[Gen 2] Crystal Legacy",
 		mod: 'gen2crystallegacy',
 		desc: `A Solomod centered around Crystal Legacy.`,
