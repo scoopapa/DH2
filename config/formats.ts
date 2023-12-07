342874e167a682f5d2332a61489e8b2717b998d0
AnalysisGeek <101986458+AnalysisGeek@users.noreply.github.com>
2023-12-06 21:09:43 -0500

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1878,28 +1878,7 @@ export const Formats: FormatList = [
 			}
 		},
 	},
-  {
-		name: "[Gen 9] Conquest Dex",
-		mod: 'conquestdex',
-		desc: `A metagame based on the pokemon side game pokemon conquest`,
-		ruleset: ['Standard NatDex', 'Data Mod', 'Terastal Clause', 'Z-Move Clause', 'Mega Stone Clause'],
-		banlist: ['Arceus', 'Dialga', 'Rayquaza', 'Zekrom', 'Reshiram', 'Mewtwo', 'Rayquaza'],
-		threads: [
-			`&bullet; <a href="https://www.smogon.com/forums/threads/solomods-megathread.3711007/post-9883990">Conquest Dex on Smogon Forums</a>`,
-		],
-		onValidateTeam(team, format) {
-			/**@type {{[k: string]: true}}*/
-			let speciesTable = {};
-			let allowedTiers = ['Conq'];
-			for (const set of team) {
-				let template = this.dex.species.get(set.species);
-				if (!allowedTiers.includes(template.tier)) {
-					return [set.species + ' is not legal in Conquest Dex.'];
-        }
-      }
-    },
-},
-  {
+	{
 		name: "[Gen 9] Black & White 3",
 
 		mod: 'blackandwhite3',
@@ -1921,10 +1900,31 @@ export const Formats: FormatList = [
 			for (const set of team) {
 				let template = this.dex.species.get(set.species);
 				if (!allowedTiers.includes(template.tier)) {
-					return [set.species + ' is not legal in Black & White 3 OU. '],
+					return [set.species + ' is not legal in Black & White 3 OU.'];
 				}
 			}
 		},
+	},
+	{
+		name: "[Gen 9] Conquest Dex",
+		mod: 'conquestdex',
+		desc: `A metagame based on the pokemon side game pokemon conquest`,
+		ruleset: ['Standard NatDex', 'Data Mod', 'Terastal Clause', 'Z-Move Clause', 'Mega Stone Clause'],
+		banlist: ['Arceus', 'Dialga', 'Rayquaza', 'Zekrom', 'Reshiram', 'Mewtwo', 'Rayquaza'],
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/solomods-megathread.3711007/post-9883990">Conquest Dex on Smogon Forums</a>`,
+		],
+		onValidateTeam(team, format) {
+			/**@type {{[k: string]: true}}*/
+			let speciesTable = {};
+			let allowedTiers = ['Conq'];
+			for (const set of team) {
+				let template = this.dex.species.get(set.species);
+				if (!allowedTiers.includes(template.tier)) {
+					return [set.species + ' is not legal in Conquest Dex.'];
+        }
+      }
+    },
 	},
 	{
 		name: "[Gen 2] Crystal Legacy",
