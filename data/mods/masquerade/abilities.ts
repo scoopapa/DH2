3eae369721c9ccb23182a2e89889758e677cb533
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-12-06 22:59:48 -0500

    Create abilities.ts

---
+++ data/mods/masquerade/abilities.ts
@@ -0,0 +1,7 @@
+export const Abilities: {[k: string]: ModdedAbilityData} = {
+	residuecleaning: {
+    // effect hardcoded into defog (just defog for now)
+		shortDesc: "Clearing hazards heals 25% max HP per hazard.",
+		name: "Residue Cleaning",
+	},
+};
