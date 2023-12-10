49e431a8affc08cacb3c7d610b2cf2a49e9b50a3
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-12-10 12:58:19 -0500

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1918,7 +1918,7 @@ export const Formats: FormatList = [
 		mod: 'conquestdex',
 		desc: `A metagame based on the pokemon side game pokemon conquest`,
 		ruleset: ['Standard NatDex', 'Data Mod', 'Terastal Clause', 'Z-Move Clause', 'Mega Stone Clause'],
-		banlist: ['Arceus', 'Dialga', 'Rayquaza', 'Zekrom', 'Reshiram', 'Mewtwo', 'Rayquaza', 'Groudon'],
+		banlist: ['Arceus', 'Dialga', 'Rayquaza', 'Zekrom', 'Reshiram', 'Mewtwo', 'Groudon'],
 		threads: [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/solomods-megathread.3711007/post-9883990">Conquest Dex on Smogon Forums</a>`,
 		],
