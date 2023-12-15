775483d55ec1304fa8548a35c82a6b771880bc2e
Rezzo64 <rezzo64@gmail.com>
2023-12-15 20:26:38 +0000

    JEP: express alpha stage of project in formats list

--- config/formats.ts
+++ config/formats.ts
@@ -2177,7 +2177,7 @@ export const Formats: FormatList = [
 		],
     },
 	{
-		name: "[Gen 2] Johto Expansion Pak OU",
+		name: "[Gen 2] Johto Expansion Pak (Alpha) OU",
 		threads: [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/solomods-megathread.3711007/page-6#post-9880873">Post in Solomods Megathread</a>`,
       	`&bullet; <a href="https://docs.google.com/spreadsheets/d/1t54hCQrMGj102ck9L7mW47GJxrHMmJy5Ygum6ldWVX0/edit?usp=sharing">Spreadsheet</a>`,
