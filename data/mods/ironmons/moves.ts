196029ad0b26548ca51661a2d420b97043d9757f
Lysio4 <lukas.moncorge@gmail.com>
2023-12-23 14:41:34 +0100

    Slate 2 Ironmons

--- data/mods/ironmons/moves.ts
+++ data/mods/ironmons/moves.ts
@@ -14,6 +14,10 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 				return false;
 			}
 		},
+		onPrepareHit: function(target, source) {	
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Aerial Ace", target);
+		},
 		shortDesc: "Hits first. First turn out only.",
 		desc: "Hits first. First turn out only.",
 		secondary: null,
@@ -44,6 +48,10 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			if (target.getAbility().isPermanent) return;
 			target.addVolatile('gastroacid');
 		},
+		onPrepareHit: function(target, source) {	
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Signal Beam", target);
+		},
 		secondary: {
 			chance: 100,
 			volatileStatus: 'flinch',
@@ -52,5 +60,69 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		type: "Electric",
 		zMove: {basePower: 140},
 		contestType: "Tough",
+	},
+	chlorohorn: {
+		num: -3,
+		accuracy: 100,
+		basePower: 70,
+		category: "Physical",
+		name: "Chloro Horn",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		onModifyPriority(priority, source, target, move) {
+			if (['sunnyday', 'desolateland'].includes(source.effectiveWeather())) {
+				return priority + 1;
+			}
+		},
+		onPrepareHit: function(target, source) {	
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Grassy Glide", target);
+		},
+		shortDesc: "Priority +1 in Sun.",
+		desc: "Priority +1 in Sun.",
+		secondary: null,
+		target: "normal",
+		type: "Grass",
+		contestType: "Cool",
+	},
+	titansjudgment: {
+		num: -4,
+		accuracy: 100,
+		basePower: 100,
+		category: "Special",
+		name: "Titan's Judgment",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		onHit(target) {
+			let success = false;
+			let i: BoostID;
+			for (i in target.boosts) {
+				if (target.boosts[i] <= 0) continue;
+				target.boosts[i] = 0;
+				success = true;
+			}
+			if (!success) return false;
+			this.add('-clearboost', target, '[from] move: Titan\'s Judgment');
+			if (target.volatiles['protosynthesis']) {
+				delete target.volatiles['protosynthesis'];
+				this.add('-end', target, 'move: Titan\'s Judgment', '[silent]');
+			}
+			if (target.volatiles['quarkdrive']) {
+				delete target.volatiles['quarkdrive'];
+				this.add('-end', target, 'move: Titan\'s Judgment', '[silent]');
+			}
+		},
+		onPrepareHit: function(target, source) {	
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Core Enforcer", target);
+		},
+		shortDesc: "Resets target's positive stat stages.",
+		desc: "Resets target's positive stat stages.",
+		secondary: null,
+		target: "normal",
+		type: "Steel",
+		contestType: "Beautiful",
 	},
 }
