d2c303f06f4bb5ac0652390eef671cc0da692270
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-12-24 19:15:10 -0500

    Create formats-data.ts

---
+++ data/mods/naturalselection/formats-data.ts
@@ -0,0 +1,122 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	uxie: {
+		tier: "MS",
+	},
+	barraskewda: {
+		tier: "MS",
+	},
+	minior: {
+		tier: "MS",
+	},
+	kingambit: {
+		tier: "MS",
+	},
+	slowbro: {
+		tier: "MS",
+	},
+	orthworm: {
+		tier: "MS",
+	},
+	emolga: {
+		tier: "MS",
+	},
+	crabominable: {
+		tier: "MS",
+	},
+	eelektross: {
+		tier: "MS",
+	},
+	ironhands: {
+		tier: "MS",
+	},
+	ariados: {
+		tier: "MS",
+	},
+	klawf: {
+		tier: "MS",
+	},
+	latios: {
+		tier: "MS",
+	},
+	hawlucha: {
+		tier: "MS",
+	},
+	jumpluff: {
+		tier: "MS",
+	},
+	volcanion: {
+		tier: "MS",
+	},
+	muk: {
+		tier: "MS",
+	},
+	skuntank: {
+		tier: "MS",
+	},
+	munkidori: {
+		tier: "MS",
+	},
+	crustle: {
+		tier: "MS",
+	},
+	bouffalant: {
+		tier: "MS",
+	},
+	jellicent: {
+		tier: "MS",
+	},
+	delibird: {
+		tier: "MS",
+	},
+	mightyena: {
+		tier: "MS",
+	},
+	orbeetle: {
+		tier: "MS",
+	},
+	mandibuzz: {
+		tier: "MS",
+	},
+	dugtrio: {
+		tier: "MS",
+	},
+	aggron: {
+		tier: "MS",
+	},
+	barbaracle: {
+		tier: "MS",
+	},
+	pangoro: {
+		tier: "MS",
+	},
+	victreebel: {
+		tier: "MS",
+	},
+	eiscue: {
+		tier: "MS",
+	},
+	haxorus: {
+		tier: "MS",
+	},
+	centiskorch: {
+		tier: "MS",
+	},
+	typhlosion: {
+		tier: "MS",
+	},
+	slurpuff: {
+		tier: "MS",
+	},
+	pyroar: {
+		tier: "MS",
+	},
+	eldegoss: {
+		tier: "MS",
+	},
+	ceruledge: {
+		tier: "MS",
+	},
+	dragapult: {
+		tier: "MS",
+	},
+};
