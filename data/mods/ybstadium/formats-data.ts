9e2f62e2d4cd5b95e4a426737749f13e423784b5
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-12-24 23:54:39 -0500

    Update formats-data.ts

--- data/mods/ybstadium/formats-data.ts
+++ data/mods/ybstadium/formats-data.ts
@@ -1,147 +1,600 @@
 export const FormatsData: {[k: string]: SpeciesFormatsData} = {
-	mew: {
+	aegislash: {
 		tier: "YBS",
-		randomBattleMoves: ["psychicfangs", "dragondance", "poltergeist", "closecombat"],
-		randomBattleLevel: 50,
-		requiredItem: "Life Orb",
-		forceTeraType: "Fairy",
-  },
-	shaymin: {
-		tier: "YBS",
-		randomBattleMoves: ["seedflare", "earthpower", "synthesis", "airslash"],
-		randomBattleLevel: 50,
-		requiredItem: "Heavy-Duty Boots",
-  },
-	jirachi: {
-		tier: "YBS",
-		randomBattleMoves: ["meteormash", "uturn", "drainpunch", "zenheadbutt"],
-		randomBattleLevel: 50,
-		requiredItem: "Choice Band",
+		requiredItem: "Aguav Berry",
+		randomBattleMoves: ["heavyslam", "spite", "kingsshield", "shadowclaw"],
 	},
-	manaphy: {
+	aggron: {
 		tier: "YBS",
-		randomBattleMoves: ["tailglow", "surf", "energyball", "psychic"],
-		randomBattleLevel: 50,
-		requiredItem: "Leftovers",
-	},
-	phione: {
-		tier: "YBS",
-		randomBattleMoves: ["terablast", "hydropump", "icebeam", "calmmind"],
-		randomBattleLevel: 50,
-		requiredItem: "Magnet",
-	},
-	spinda: {
-		tier: "YBS",
-		randomBattleMoves: ["superpower", "doubleedge", "suckerpunch", "fakeout"],
-		randomBattleLevel: 50,
 		requiredItem: "Assault Vest",
+		randomBattleMoves: ["doubleedge", "hydropump", "heavyslam", "rockslide"],
 	},
-	glalie: {
+	altaria: {
 		tier: "YBS",
-		randomBattleMoves: ["icespinner", "earthquake", "crunch", "spikes"],
-		randomBattleLevel: 50,
 		requiredItem: "Heavy-Duty Boots",
+		randomBattleMoves: ["defog", "roost", "bravebird", "heatwave"],
 	},
-	silvally: {
+	anorith: {
 		tier: "YBS",
-		randomBattleMoves: ["multiattack", "partingshot", "thunderwave", "defog"],
-		randomBattleLevel: 50,
-		requiredItem: "Steel Memory",
+		requiredItem: "Life Orb",
+		randomBattleMoves: ["stoneedge", "xscissor", "wavecrash", "swordsdance"],
 	},
-	sunkern: {
+	araquanid: {
 		tier: "YBS",
-		randomBattleMoves: ["gigadrain", "earthpower", "synthesis", "leechseed"],
-		randomBattleLevel: 50,
-		requiredItem: "Eviolite",
+		requiredItem: "Lum Berry",
+		randomBattleMoves: ["hydropump", "frostbreath", "pounce", "stickyweb"],
 	},
-	snorunt: {
+	arboliva: {
 		tier: "YBS",
-		randomBattleMoves: ["protect", "freezedry", "substitute", "terablast"],
-		randomBattleLevel: 50,
-		requiredItem: "Eviolite",
+		requiredItem: "Big Root",
+		randomBattleMoves: ["grassknot", "terablast", "earthpower", "strengthsap"],
 	},
-	arceus: {
+	arcanine: {
 		tier: "YBS",
-		randomBattleMoves: ["extremespeed", "swordsdance", "recover", "shadowclaw"],
-		randomBattleLevel: 50,
-		requiredItem: "Spooky Plate",
+		requiredItem: "Leftovers",
+		randomBattleMoves: ["temperflare", "terablast", "willowisp", "teleport"],
 	},
-	castform: {
+	arctozolt: {
 		tier: "YBS",
-		randomBattleMoves: ["weatherball", "solarbeam", "icebeam", "sunnyday"],
-		randomBattleLevel: 50,
-		requiredItem: "Heat Rock",
+		requiredItem: "Rocky Helmet",
+		randomBattleMoves: ["boltbeak", "freezedry", "taunt", "thunderwave"],
 	},
-	typenull: {
+	aurorus: {
 		tier: "YBS",
-		randomBattleMoves: ["shadowball", "swordsdance", "crushclaw", "rest"],
-		randomBattleLevel: 50,
-		requiredItem: "Eviolite",
+		requiredItem: "Chople Berry",
+		randomBattleMoves: ["icebeam", "ancientpower", "discharge", "stealthrock"],
+	},
+	avalugghisui: {
+		tier: "YBS",
+		requiredItem: "Loaded Dice",
+		randomBattleMoves: ["iciclespear", "rockblast", "rapidspin", "recover"],
+	},
+	basculinwhitestriped: {
+		tier: "YBS",
+		requiredItem: "Lum Berry",
+		randomBattleMoves: ["flipturn", "lastrespects", "endeavor", "hydropump"],
+	},
+	bisharp: {
+		tier: "YBS",
+		requiredItem: "Black Glasses",
+		randomBattleMoves: ["pursuit", "nightslash", "metalclaw", "psychocut"],
+	},
+	breloom: {
+		tier: "YBS",
+		requiredItem: "Toxic Orb",
+		randomBattleMoves: ["spore", "drainpunch", "bulletseed", "laserfocus"],
+	},
+	centiskorch: {
+		tier: "YBS",
+		requiredItem: "Heavy-Duty Boots",
+		randomBattleMoves: ["burnup", "skittersmack", "thunderfang", "scald"],
+	},
+	cetitan: {
+		tier: "YBS",
+		requiredItem: "Liechi Berry",
+		randomBattleMoves: ["icepunch", "stompingtantrum", "substitute", "facade"],
+	},
+	charizard: {
+		tier: "YBS",
+		requiredItem: "Heavy-Duty Boots",
+		randomBattleMoves: ["firepledge", "airslash", "solarbeam", "willowisp"],
+	},
+	cinccino: {
+		tier: "YBS",
+		requiredItem: "Heavy-Duty Boots",
+		randomBattleMoves: ["tailslap", "knockoff", "tidyup", "thunderwave"],
+	},
+	claydol: {
+		tier: "YBS",
+		requiredItem: "Life Orb",
+		randomBattleMoves: ["gravity", "earthquake", "expandingforce", "rapidspin"],
+	},
+	clodsire: {
+		tier: "YBS",
+		requiredItem: "Zoom Lens",
+		randomBattleMoves: ["gunkshot", "megahorn", "highhorsepower", "curse"],
 	},
 	combusken: {
 		tier: "YBS",
-		randomBattleMoves: ["detect", "closecombat", "flareblitz", "swordsdance"],
-		randomBattleLevel: 50,
 		requiredItem: "Life Orb",
+		randomBattleMoves: ["detect", "blazekick", "swordsdance", "closecombat"],
 	},
-	zeraora: {
+	crustle: {
 		tier: "YBS",
-		randomBattleMoves: ["thunderbolt", "terablast", "focusblast", "voltswitch"],
-		randomBattleLevel: 50,
+		requiredItem: "Custap Berry",
+		randomBattleMoves: ["rockwrecker", "shadowclaw", "poisonjab", "spikes"],
+	},
+	cyclizar: {
+		tier: "YBS",
 		requiredItem: "Heavy-Duty Boots",
+		randomBattleMoves: ["raindance", "aquatail", "dracometeor", "icespinner"],
 	},
-	zarude: {
+	ditto: {
 		tier: "YBS",
-		randomBattleMoves: ["junglehealing", "swordsdance", "knockoff", "powerwhip"],
-		randomBattleLevel: 50,
-		requiredItem: "Leftovers",
+		requiredItem: "Choice Scarf",
+		randomBattleMoves: ["transform"],
 	},
-	mudsdale: {
+	dracozolt: {
 		tier: "YBS",
-		randomBattleMoves: ["earthquake", "closecombat", "heavyslam", "stoneedge"],
-		randomBattleLevel: 50,
-		requiredItem: "Berserk Gene",
+		requiredItem: "Power Herb",
+		randomBattleMoves: ["meteorbeam", "discharge", "dragonpulse", "firespin"],
 	},
-	latios: {
+	dragalge: {
 		tier: "YBS",
-		randomBattleMoves: ["dracometeor", "lusterpurge", "aurasphere", "trick"],
-		randomBattleLevel: 50,
+		requiredItem: "Air Balloon",
+		randomBattleMoves: ["gunkshot", "dragontail", "shadowball", "toxicspikes"],
+	},
+	dragonair: {
+		tier: "YBS",
+		requiredItem: "Eviolite",
+		randomBattleMoves: ["extremespeed", "dragondance", "outrage", "rest"],
+	},
+	drampa: {
+		tier: "YBS",
+		requiredItem: "Heavy-Duty Boots",
+		randomBattleMoves: ["terablast", "dragonpulse", "roost", "defog"],
+	},
+	dugtrio: {
+		tier: "YBS",
+		requiredItem: "Life Orb",
+		randomBattleMoves: ["bulldoze", "suckerpunch", "stealthrock", "aerialace"],
+	},
+	eelektross: {
+		tier: "YBS",
+		requiredItem: "Punching Glove",
+		randomBattleMoves: ["coil", "thunderpunch", "drainpunch", "firepunch"],
+	},
+	elgyem: {
+		tier: "YBS",
+		requiredItem: "TR51",
+		randomBattleMoves: ["psystrike", "thunderbolt", "darkpulse", "trickroom"],
+	},
+	emolga: {
+		tier: "YBS",
 		requiredItem: "Choice Specs",
+		randomBattleMoves: ["thunderbolt", "airslash", "energyball", "voltswitch"],
 	},
-	pecharunt: {
+	espeon: {
 		tier: "YBS",
-		randomBattleMoves: ["hex", "malignantchain", "recover", "toxic"],
-		randomBattleLevel: 50,
-		requiredItem: "Black Sludge",
+		requiredItem: "Wise Glasses",
+		randomBattleMoves: ["psyshock", "signalbeam", "futuresight", "batonpass"],
 	},
 	feraligatr: {
 		tier: "YBS",
-		randomBattleMoves: ["liquidation", "crunch", "icepunch", "swordsdance"],
-		randomBattleLevel: 50,
 		requiredItem: "Life Orb",
+		randomBattleMoves: ["scald", "focusblast", "snarl", "scaryface"],
 	},
-	calyrexshadow: {
+	flareon: {
 		tier: "YBS",
-		randomBattleMoves: ["confusion", "hiddenpowerghost", "swift", "grassyterrain"],
-		randomBattleLevel: 50,
-		requiredItem: "Sitrus Berry",
+		requiredItem: "Heavy-Duty Boots",
+		randomBattleMoves: ["flareblitz", "superpower", "trailblaze", "willowisp"],
 	},
-	malamar: {
+	flygon: {
 		tier: "YBS",
-		randomBattleMoves: ["superpower", "knockoff", "zenheadbutt", "liquidation"],
-		randomBattleLevel: 50,
+		requiredItem: "Scope Lens",
+		randomBattleMoves: ["dracometeor", "scorchingsands", "alluringvoice", "focusenergy"],
+	},
+	gallade: {
+		tier: "YBS",
 		requiredItem: "Assault Vest",
+		randomBattleMoves: ["psychocut", "brickbreak", "knockoff", "aquacutter"],
 	},
-	dragonite: {
+	galvantula: {
 		tier: "YBS",
-		randomBattleMoves: ["extremespeed", "earthquake", "roost", "dragondance"],
-		randomBattleLevel: 50,
+		requiredItem: "Loaded Dice",
+		randomBattleMoves: ["pinmissile", "risingvoltage", "suckerpunch", "stickyweb"],
+	},
+	garbodor: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+		randomBattleMoves: ["gunkshot", "stompingtantrum", "toxicspikes", "thunderbolt"],
+	},
+	gigalith: {
+		tier: "YBS",
+		requiredItem: "Kee Berry",
+		randomBattleMoves: ["rockslide", "bodypress", "stealthrock", "gravity"],
+	},
+	glaceon: {
+		tier: "YBS",
+		requiredItem: "Heavy-Duty Boots",
+		randomBattleMoves: ["icebeam", "shadowball", "aquatail", "batonpass"],
+	},
+	gligar: {
+		tier: "YBS",
+		requiredItem: "Eviolite",
+		randomBattleMoves: ["highhorsepower", "spikes", "uturn", "roost"],
+	},
+	glimmora: {
+		tier: "YBS",
+		requiredItem: "Focus Sash",
+		randomBattleMoves: ["mortalspin", "ancientpower", "sludgebomb", "flashcannon"],
+	},
+	golisopod: {
+		tier: "YBS",
+		requiredItem: "Sitrus Berry",
+		randomBattleMoves: ["firstimpression", "razorshell", "spikes", "suckerpunch"],
+	},
+	golurk: {
+		tier: "YBS",
+		requiredItem: "Lagging Tail",
+		randomBattleMoves: ["poweruppunch", "knockoff", "highhorsepower", "trick"],
+	},
+	grafaiai: {
+		tier: "YBS",
+		requiredItem: "Focus Sash",
+		randomBattleMoves: ["swordsdance", "crosspoison", "slash", "uturn"],
+	},
+	guzzlord: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+		randomBattleMoves: ["dragonclaw", "darkpulse", "stockpile", "swallow"],
+	},
+	hatterene: {
+		tier: "YBS",
+		requiredItem: "Sitrus Berry",
+		randomBattleMoves: ["magicpowder", "shadowball", "playrough", "psychicnoise"],
+	},
+	haunter: {
+		tier: "YBS",
+		requiredItem: "Eviolite",
+		randomBattleMoves: ["nastyplot", "shadowball", "sludgebomb", "reflecttype"],
+	},
+	hippowdon: {
+		tier: "YBS",
+		requiredItem: "Soft Sand",
+		randomBattleMoves: ["stompingtantrum", "crunch", "stealthrock", "slackoff"],
+	},
+	hitmontop: {
+		tier: "YBS",
+		requiredItem: "Heavy-Duty Boots",
+		randomBattleMoves: ["machpunch", "triplekick", "tripleaxel", "rapidspin"],
+	},
+	hoopa: {
+		tier: "YBS",
+		requiredItem: "Power Herb",
+		randomBattleMoves: ["phantomforce", "hyperspacehole", "firepunch", "foulplay"],
+	},
+	illumise: {
+		tier: "YBS",
+		requiredItem: "Throat Spray",
+		randomBattleMoves: ["bugbuzz", "thunderbolt", "moonlight", "uturn"],
+	},
+	indeedeef: {
+		tier: "YBS",
+		requiredItem: "Room Service",
+		randomBattleMoves: ["extrasensory", "alluringvoice", "trickroom", "healingwish"],
+	},
+	ironbundle: {
+		tier: "YBS",
+		requiredItem: "Mystic Water",
+		randomBattleMoves: ["glaciate", "bubblebeam", "present", "uturn"],
+	},
+	ironthorns: {
+		tier: "YBS",
+		requiredItem: "Expert Belt",
+		randomBattleMoves: ["thunderpunch", "icebeam", "powergem", "spikes"],
+	},
+	irontreads: {
+		tier: "YBS",
+		requiredItem: "Metal Coat",
+		randomBattleMoves: ["stompingtantrum", "steelroller", "thunder", "electricterrain"],
+	},
+	jolteon: {
+		tier: "YBS",
+		requiredItem: "Heavy-Duty Boots",
+		randomBattleMoves: ["discharge", "terablast", "substitute", "batonpass"],
+	},
+	kangaskhan: {
+		tier: "YBS",
+		requiredItem: "Pink Bow",
+		randomBattleMoves: ["fakeout", "lastresort"],
+	},
+	klawf: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+		randomBattleMoves: ["stoneedge", "temperflare", "knockoff", "stealthrock"],
+	},
+	klinklang: {
+		tier: "YBS",
+		requiredItem: "Shuca Berry",
+		randomBattleMoves: ["geargrind", "magneticflux", "rest", "sleeptalk"],
+	},
+	kyuremblack: {
+		tier: "YBS",
+		requiredItem: "Rawst Berry",
+		randomBattleMoves: ["freezeshock", "breakingswipe", "dualwingbeat", "nobleroar"],
+	},
+	leafeon: {
+		tier: "YBS",
+		requiredItem: "Loaded Dice",
+		randomBattleMoves: ["bulletseed", "knockoff", "sunnyday", "swordsdance"],
+	},
+	ledian: {
+		tier: "YBS",
+		requiredItem: "Light Clay",
+		randomBattleMoves: ["reflect", "lightscreen", "silverwind", "focusblast"],
+	},
+	liepard: {
+		tier: "YBS",
+		requiredItem: "Expert Belt",
+		randomBattleMoves: ["nastyplot", "shadowball", "burningjealousy", "copycat"],
+	},
+	lilliganthisui: {
+		tier: "YBS",
+		requiredItem: "Heavy-Duty Boots",
+		randomBattleMoves: ["petalblizzard", "lowsweep", "tripleaxel", "healbell"],
+	},
+	linoone: {
+		tier: "YBS",
+		requiredItem: "Starf Berry",
+		randomBattleMoves: ["extremespeed", "throatchop", "substitute", "hydropump"],
+	},
+	lokix: {
+		tier: "YBS",
+		requiredItem: "Dark Gem",
+		randomBattleMoves: ["skittersmack", "thief", "axekick", "swordsdance"],
+	},
+	lucario: {
+		tier: "YBS",
+		requiredItem: "Air Balloon",
+		randomBattleMoves: ["vacuumwave", "flashcannon", "dragonpulse", "metalsound"],
+	},
+	lurantis: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+		randomBattleMoves: ["petalblizzard", "terablast", "superpower", "defog"],
+	},
+	mabosstiff: {
+		tier: "YBS",
+		requiredItem: "Red Card",
+		randomBattleMoves: ["lashout", "wildcharge", "comeuppance", "painsplit"],
+	},
+	magcargo: {
+		tier: "YBS",
+		requiredItem: "White Herb",
+		randomBattleMoves: ["lavaplume", "powergem", "scorchingsands", "shellsmash"],
+	},
+	mamoswine: {
+		tier: "YBS",
+		requiredItem: "Lum Berry",
+		randomBattleMoves: ["icefang", "stompingtantrum", "ironhead", "stealthrock"],
+	},
+	mawile: {
+		tier: "YBS",
+		requiredItem: "Life Orb",
+		randomBattleMoves: ["playrough", "suckerpunch", "ironhead", "stealthrock"],
+	},
+	meowscarada: {
+		tier: "YBS",
+		requiredItem: "Eject Pack",
+		randomBattleMoves: ["leafstorm", "powergem", "aurasphere", "spikes"],
+	},
+	mienshao: {
+		tier: "YBS",
+		requiredItem: "Assault Vest",
+		randomBattleMoves: ["aurasphere", "grassknot", "vacuumwave", "uturn"],
+	},
+	mimikyu: {
+		tier: "YBS",
+		requiredItem: "Red Card",
+		randomBattleMoves: ["shadowsneak", "playrough", "willowisp", "painsplit"],
+	},
+	mismagius: {
+		tier: "YBS",
+		requiredItem: "Magnet",
+		randomBattleMoves: ["hex", "thunderbolt", "thunderwave", "painsplit"],
+	},
+	munkidori: {
+		tier: "YBS",
+		requiredItem: "Black Sludge",
+		randomBattleMoves: ["calmmind", "venoshock", "psychicnoise", "trick"],
+	},
+	murkrow: {
+		tier: "YBS",
+		requiredItem: "Eviolite",
+	},
+	ninjask: {
+		tier: "YBS",
+		requiredItem: "Heavy-Duty Boots",
+	},
+	oricoriosensu: {
+		tier: "YBS",
+		requiredItem: "Flying Gem",
+	},
+	pelipper: {
+		tier: "YBS",
 		requiredItem: "Heavy-Duty Boots",
 	},
 	pikachu: {
 		tier: "YBS",
-		randomBattleMoves: ["thunderbolt", "surf", "nastyplot", "grassknot"],
-		randomBattleLevel: 50,
 		requiredItem: "Light Ball",
+	},
+	pincurchin: {
+		tier: "YBS",
+		requiredItem: "Life Orb",
+	},
+	politoed: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+	},
+	primeape: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+	},
+	qwilfishhisui: {
+		tier: "YBS",
+		requiredItem: "Eviolite",
+	},
+	raticate: {
+		tier: "YBS",
+		requiredItem: "Flame Orb",
+	},
+	revavroom: {
+		tier: "YBS",
+		requiredItem: "Lum Berry",
+	},
+	runerigus: {
+		tier: "YBS",
+		requiredItem: "Kasib Berry",
+	},
+	sableye: {
+		tier: "YBS",
+		requiredItem: "Kee Berry",
+	},
+	sandslashalola: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+	},
+	sawk: {
+		tier: "YBS",
+		requiredItem: "Salac Berry",
+	},
+	screamtail: {
+		tier: "YBS",
+		requiredItem: "Booster Energy",
+	},
+	shiftry: {
+		tier: "YBS",
+		requiredItem: "Chesto Berry",
+	},
+	sigilyph: {
+		tier: "YBS",
+		requiredItem: "Wise Glasses",
+	},
+	silvally: {
+		tier: "YBS",
+		requiredItem: "Fairy Memory",
+	},
+	simisear: {
+		tier: "YBS",
+		requiredItem: "Petaya Berry",
+	},
+	sirfetchd: {
+		tier: "YBS",
+		requiredItem: "Leek",
+	},
+	skarmory: {
+		tier: "YBS",
+		requiredItem: "Rocky Helmet",
+	},
+	skuntank: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+	},
+	slowbro: {
+		tier: "YBS",
+		requiredItem: "Berserk Gene",
+	},
+	sneaselhisui: {
+		tier: "YBS",
+		requiredItem: "Big Nugget",
+	},
+	spectrier: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+	},
+	stoutland: {
+		tier: "YBS",
+		requiredItem: "Silk Scarf",
+	},
+	swellow: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+	},
+	sylveon: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+	},
+	tapubulu: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+	},
+	tapufini: {
+		tier: "YBS",
+		requiredItem: "Sitrus Berry",
+	},
+	tapukoko: {
+		tier: "YBS",
+		requiredItem: "Electric Seed",
+	},
+	tapulele: {
+		tier: "YBS",
+		requiredItem: "Life Orb",
+	},
+	thwackey: {
+		tier: "YBS",
+		requiredItem: "Eviolite",
+	},
+	torkoal: {
+		tier: "YBS",
+		requiredItem: "Eject Pack",
+	},
+	toucannon: {
+		tier: "YBS",
+		requiredItem: "Heavy-Duty Boots",
+	},
+	trevenant: {
+		tier: "YBS",
+		requiredItem: "Custap Berry",
+	},
+	tyrantrum: {
+		tier: "YBS",
+		requiredItem: "Life Orb",
+	},
+	umbreon: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+	},
+	urshifu: {
+		tier: "YBS",
+		requiredItem: "Roseli Berry",
+	},
+	vanilluxe: {
+		tier: "YBS",
+		requiredItem: "Silk Scarf",
+	},
+	vaporeon: {
+		tier: "YBS",
+		requiredItem: "Leftovers",
+	},
+	veluza: {
+		tier: "YBS",
+		requiredItem: "Scope Lens",
+	},
+	vigoroth: {
+		tier: "YBS",
+		requiredItem: "Eviolite",
+	},
+	vikavolt: {
+		tier: "YBS",
+		requiredItem: "Sitrus Berry",
+	},
+	volcarona: {
+		tier: "YBS",
+		requiredItem: "Heavy-Duty Boots",
+	},
+	vulpix: {
+		tier: "YBS",
+		requiredItem: "Life Orb",
+	},
+	wartortle: {
+		tier: "YBS",
+		requiredItem: "Eviolite",
+	},
+	weavile: {
+		tier: "YBS",
+		requiredItem: "Focus Sash",
+	},
+	weezing: {
+		tier: "YBS",
+		requiredItem: "Figy Berry",
+	},
+	weezinggalar: {
+		tier: "YBS",
+		requiredItem: "Fairy Gem",
+	},
+	whimsicott: {
+		tier: "YBS",
+		requiredItem: "Choice Specs",
 	},
 };
