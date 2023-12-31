29611cfb2b57bca4b237267acdfae2a496e7bddd
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-12-31 13:15:09 -0500

    Update moves.ts

--- data/mods/teramax/moves.ts
+++ data/mods/teramax/moves.ts
@@ -209,6 +209,43 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		zMove: {effect: 'redirect'},
 		contestType: "Clever",
 	},
+	terablast: {
+		num: 851,
+		accuracy: 100,
+		basePower: 80,
+		basePowerCallback(pokemon, target, move) {
+			if (pokemon.terastallized === 'Stellar') {
+				return 100;
+			}
+			return 80;
+		},
+		category: "Special",
+		name: "Tera Blast",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, mustpressure: 1},
+		onPrepareHit(target, source, move) {
+			if (source.terastallized) {
+				this.attrLastMove('[anim] Tera Blast ' + source.teraType);
+			}
+		},
+		onModifyType(move, pokemon, target) {
+			if (pokemon.terastallized) {
+				move.type = pokemon.teraType;
+			}
+		},
+		onModifyMove(move, pokemon, target) {
+			if (pokemon.terastallized && pokemon.getStat('atk', false, true) > pokemon.getStat('spa', false, true)) {
+				move.category = 'Physical';
+			}
+			if (pokemon.terastallized === 'Stellar' && !target.volatiles['dynamax']) {
+				move.self = {boosts: {atk: -1, spa: -1}};
+			}
+		},
+		secondary: null,
+		target: "normal",
+		type: "Normal",
+	},
 
 // Max and GMax Moves
 	gmaxbefuddle: {
