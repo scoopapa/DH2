cad56059876f9ce7002c52c1b346b8ddc9c1358e
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-01-03 22:35:32 -0500

    Update items.ts

--- data/mods/aroundwithyb/items.ts
+++ data/mods/aroundwithyb/items.ts
@@ -662,4 +662,237 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 		num: 1880,
 		gen: 9,
 	},
+
+// Mega Stones
+	flygonite: {
+		name: "Flygonite",
+		spritenum: 589,
+		megaStone: "Flygon-Mega",
+		megaEvolves: "Flygon",
+		itemUser: ["Flygon"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1010,
+		gen: 9,
+		desc: "If held by a Flygon, this item allows it to Mega Evolve in battle.",
+		isNonstandard: null,
+	},
+	roseradite: {
+		name: "Roseradite",
+		spritenum: 608,
+		megaStone: "Roserade-Mega",
+		megaEvolves: "Roserade",
+		itemUser: ["Roserade"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1011,
+		gen: 9,
+		desc: "If held by a Roserade, this item allows it to Mega Evolve in battle.",
+	},
+	cryogonite: {
+		name: "Cryogonite",
+		spritenum: 623,
+		megaStone: "Cryogonal-Mega",
+		megaEvolves: "Cryogonal",
+		itemUser: ["Cryogonal"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1012,
+		gen: 9,
+		desc: "If held by a Cryogonal, this item allows it to Mega Evolve in battle.",
+	},
+	abomasite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	absolite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	aerodactylite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	aggronite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	alakazite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	altarianite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	ampharosite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	audinite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	banettite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	beedrillite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	blastoisinite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	blazikenite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	cameruptite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	charizarditex: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	charizarditey: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	diancite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	galladite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	garchompite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	gardevoirite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	gengarite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	glalitite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	gyaradosite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	heracronite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	houndoominite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	kangaskhanite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	latiasite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	latiosite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lopunnite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lucarionite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	manectite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mawilite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	medichamite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	metagrossite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mewtwonitex: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mewtwonitey: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	pidgeotite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	pinsirite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	sablenite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	salamencite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	sceptilite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	scizorite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	sharpedonite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	slowbronite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	steelixite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	swampertite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	tyranitarite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	venusaurite: {
+		inherit: true,
+		isNonstandard: null,
+	},
 };
