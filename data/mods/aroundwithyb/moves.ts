801c887f80897d4c7096ac8984762290e9914cc8
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-01-05 03:07:39 -0500

    Update moves.ts

--- data/mods/aroundwithyb/moves.ts
+++ data/mods/aroundwithyb/moves.ts
@@ -737,11 +737,11 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 	},
 	workout: {
 		accuracy: 100,
-		basePower: 70,
+		basePower: 70, /*
 		basePowerCallback(pokemon) {
 			if (!pokemon.volatiles['warmup']?.layers) return 70;
 			return (pokemon.volatiles['warmup'].layers + 1) * 70;
-		},
+		}, */
 		category: "Physical",
 		shortDesc: "Hits 1-3 times based on Warm Up levels.",
 		viable: true,
@@ -754,6 +754,12 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			this.add('-anim', source, "Burn Up", target);
 		},
 		onModifyMove(move, pokemon) {
+			if (!pokemon.volatiles['warmup']?.layers) {
+				move.levels = pokemon.volatiles['warmup'].layers;
+			} else {
+				move.levels = 0;
+			}
+			move.multihit = move.levels.length + 1;
 			if (!pokemon.volatiles['warmup']?.layers) {
 				move.recoil = [1, 4];
 			}
