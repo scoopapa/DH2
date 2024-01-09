92d21f8f27d390ca5960b2c0e1d77615ba85d5e2
KirbyRider1337 <39121695+KirbyRider1337@users.noreply.github.com>
2024-01-09 15:32:42 -0500

    Add files via upload

---
+++ data/mods/feou/aliases.ts
@@ -0,0 +1,27 @@
+export const Aliases: {[alias: string]: string} = {
+	weezer: "Weezaluna-Bloodmoon",
+	
+	//Charbebe and evos have varying colors
+	charbebered: "Charbebe",
+	charbebeblue: "Charbebe",
+	charbebeyellow: "Charbebe",
+	charbebeorange: "Charbebe",
+	charbebewhite: "Charbebe",
+	charb: "Florgerouge",
+	charbred: "Florgerouge",
+	charbblue: "Florgerouge",
+	charbyellow: "Florgerouge",
+	charborange: "Florgerouge",
+	charbwhite: "Florgerouge",
+	florgerougered: "Florgerouge",
+	florgerougeblue: "Florgerouge",
+	florgerougeyellow: "Florgerouge",
+	florgerougeorange: "Florgerouge",
+	florgerougewhite: "Florgerouge",
+	florge: "Florgerouge",
+	florgered: "Florgerouge",
+	florgeblue: "Florgerouge",
+	florgeyellow: "Florgerouge",
+	florgeorange: "Florgerouge",
+	florgewhite: "Florgerouge",
+};
