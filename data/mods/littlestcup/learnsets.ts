b5467b0dcbb63193d307c72c063f63b1d70bab9f
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-01-13 18:14:10 -0500

    Create learnsets.ts

---
+++ data/mods/littlestcup/learnsets.ts
@@ -0,0 +1,26 @@
+export const Learnsets: {[k: string]: LearnsetData} = {
+  budew: {
+		learnset: {
+			inherit: true,
+			terablast: ["9L1"],
+		},
+	},
+	mantyke: {
+		learnset: {
+			inherit: true,
+			terablast: ["9L1"],
+		},
+	},
+	mimejr: {
+		learnset: {
+			inherit: true,
+			terablast: ["9L1"],
+		},
+	},
+	smoochum: {
+		learnset: {
+			inherit: true,
+			terablast: ["9L1"],
+		},
+	},
+};
