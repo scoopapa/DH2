35272cd8e87ce66f29a16530d613beecee0cf1ad
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-01-16 18:40:27 -0500

    Create formats-data.ts

---
+++ data/mods/fevgc/formats-data.ts
@@ -0,0 +1,262 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	meowsdrahisui: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	meowsdraf: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	honchkario: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	indeechufalola: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	empozinggalar: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	samureedeemale: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	exeggolivaalola: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	noctoed: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	torklod: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	tyranaught: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	glacetalesalola: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	sandslashcrossbreed: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	porynoirz: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	victreevile: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	megaflor: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	armarizor: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	ceruleavor: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	luxraptor: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	jumpathra: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	haxocross: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	quaquadueyehisui: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	gholdentales: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	miensphlosionhisui: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	araquaton: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	garganacoal: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	salazzern: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	hattelure: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	oinksler: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	wyrdguru: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	scrafdent: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	toxtricoriopompomamped: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	toxtricoriobaileamped: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	toxtricoriopauamped: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	toxtricoriosensuamped: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	toxtricoriopompomkeylow: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	toxtricoriobailekeylow: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	toxtricoriopaukeylow: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	toxtricoriosensukeylow: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	sabliados: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	fablapple: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	slowzonegalar: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	wigglylurk: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	bravitry: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	cryomagius: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	lycanrosaquapaldeamidday: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	lycanrosblazepaldeamidnight: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	lycanroscombatpaldeadusk: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	revarantis: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	laprossand: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	spiritzer: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	serpeblim: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	venuvolt: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	frosmivire: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	rhyperluna: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	yanroar: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	yanroarf: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	krookorotthisui: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	pincunguss: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	mausaiai: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	mausaiaifour: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	incineboar: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	genglizar: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	tropisdale: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	galvillon: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+	kingturne: {
+		tier: "FEOU",
+		doublesTier: "FEOU",
+	},
+};
