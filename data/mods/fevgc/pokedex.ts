950eb5aaa352acefaecc671920bd73cab4b10bce
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-01-16 19:08:41 -0500

    Update pokedex.ts

--- data/mods/fevgc/pokedex.ts
+++ data/mods/fevgc/pokedex.ts
@@ -6,6 +6,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Psychic", "Steel"],
 		baseStats: {hp: 80, atk: 74, def: 88, spa: 96, spd: 115, spe: 82},
 		abilities: {0: "Sly Slime", 1: "Sappy Jest", H: "Knights Eye"},
+		weightkg: 171.3,
 	},
 	meowsdraf: {
 		num: 2001,
@@ -14,6 +15,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Psychic", "Dragon"],
 		baseStats: {hp: 82, atk: 74, def: 73, spa: 99, spd: 115, spe: 92},
 		abilities: {0: "Root Snap", 1: "Hydrovision", H: "Goo-Getter"},
+		weightkg: 79.5,
 	},
 	honchkario: {
 		num: 2002,
@@ -21,6 +23,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Steel", "Dark"],
 		baseStats: {hp: 85, atk: 123, def: 61, spa: 110, spd: 61, spe: 90},
 		abilities: {0: "Restless Speed", 1: "Hyperfocus", H: "Hero Ego"},
+		weightkg: 40.7,
 	},
 	indeechufalola: {
 		num: 2003,
@@ -28,6 +31,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Electric", "Psychic"],
 		baseStats: {hp: 70, atk: 70, def: 60, spa: 95, spd: 100, spe: 100},
 		abilities: {0: "Electric Surge"},
+		weightkg: 24.5,
 	},
 	empozinggalar: {
 		num: 2004,
@@ -35,6 +39,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Water", "Fairy"],
 		baseStats: {hp: 84, atk: 88, def: 104, spa: 104, spd: 85, spe: 60},
 		abilities: {0: "Magic Surge", H: "Neutral Match"},
+		weightkg: 50.3,
 	},
 	samureedee: {
 		num: 2005,
@@ -43,6 +48,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Psychic", "Water"],
 		baseStats: {hp: 78, atk: 82, def: 70, spa: 106, spd: 82, spe: 82},
 		abilities: {0: "Focus Falls", H: "Armor Surge"},
+		weightkg: 61.3,
 	},
 	exeggolivaalola: {
 		num: 2006,
@@ -50,6 +56,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Grass", "Dragon"],
 		baseStats: {hp: 86, atk: 107, def: 87, spa: 125, spd: 92, spe: 42},
 		abilities: {0: "Grassy Surge", H: "Harvest"},
+		weightkg: 231.9,
 	},
 	noctoed: {
 		num: 2007,
@@ -57,6 +64,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Water", "Flying"],
 		baseStats: {hp: 95, atk: 62, def: 62, spa: 88, spd: 98, spe: 90},
 		abilities: {0: "Dive Goggles", 1: "High Energy", H: "Stormy Sight"},
+		weightkg: 37.4,
 	},
 	torklod: {
 		num: 2008,
@@ -64,6 +72,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Fire", "Poison"],
 		baseStats: {hp: 100, atk: 80, def: 100, spa: 85, spd: 85, spe: 20},
 		abilities: {0: "Smoke Absorb", 1: "Solar Radiation", H: "Daft Shield"},
+		weightkg: 151.7,
 	},
 	tyranaught: {
 		num: 2009,
@@ -71,6 +80,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Rock", "Fighting"],
 		baseStats: {hp: 94, atk: 130, def: 123, spa: 84, spd: 87, spe: 62},
 		abilities: {0: "Forbidden Garden", H: "Sandproof"},
+		weightkg: 146,
 	},
 	glacetalesalola: {
 		num: 2010,
@@ -78,6 +88,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Ice", "Fairy"],
 		baseStats: {hp: 69, atk: 63, def: 92, spa: 105, spd: 97, spe: 107},
 		abilities: {0: "Snow Cloak", H: "Cryowarning"},
+		weightkg: 22.9,
 	},
 	sandslash: {
 		inherit: true,
@@ -92,6 +103,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Ground", "Ice"],
 		baseStats: {hp: 75, atk: 110, def: 115, spa: 35, spd: 60, spe: 75},
 		abilities: {0: "Tundra Veil", H: "Tundra Rush"},
+		weightkg: 34.8,
 	},
 	porynoirz: {
 		num: 2012,
@@ -99,6 +111,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Normal", "Ghost"],
 		baseStats: {hp: 65, atk: 90, def: 102, spa: 116, spd: 105, spe: 67},
 		abilities: {0: "Night Vision", H: "Malware"},
+		weightkg: 70.3,
 	},
 	victreevile: {
 		num: 2013,
@@ -106,6 +119,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Grass", "Poison"],
 		baseStats: {hp: 77, atk: 92, def: 75, spa: 105, spd: 80, spe: 80},
 		abilities: {0: "Quick Delivery", H: "Fast Venom"},
+		weightkg: 17.1,
 	},
 	megaflor: {
 		num: 2014,
@@ -117,6 +131,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		abilities: {0: "Overbloom", H: "Teamwork"},
 		cosmeticFormes: ["Megaflor-Blue", "Megaflor-Orange", "Megaflor-White", "Megaflor-Yellow"],
 		formeOrder: ["Megaflor", "Megaflor-Blue", "Megaflor-Orange", "Megaflor-White", "Megaflor-Yellow"],
+		weightkg: 55.3,
 	},
 	armarizor: {
 		num: 2015,
@@ -124,6 +139,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Fire", "Steel"],
 		baseStats: {hp: 77, atk: 95, def: 100, spa: 109, spd: 80, spe: 70},
 		abilities: {0: "Pyrotechnic", H: "Light Armor"},
+		weightkg: 101.5,
 	},
 	ceruleavor: {
 		num: 2016,
@@ -131,6 +147,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Fire", "Rock"],
 		baseStats: {hp: 81, atk: 130, def: 87, spa: 63, spd: 85, spe: 85},
 		abilities: {0: "Heatblade", H: "Strong Armor"},
+		weightkg: 75.5,
 	},
 	luxraptor: {
 		num: 2017,
@@ -138,6 +155,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Electric", "Flying"],
 		baseStats: {hp: 92, atk: 120, def: 82, spa: 72, spd: 69, spe: 85},
 		abilities: {0: "Underestimate", H: "Migrate"},
+		weightkg: 33.5,
 	},
 	jumpathra: {
 		num: 2018,
@@ -145,6 +163,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Psychic", "Flying"],
 		baseStats: {hp: 85, atk: 57, def: 65, spa: 98, spd: 77, spe: 107},
 		abilities: {0: "Seize the Moment", 1: "Safe Entry", H: "Speed Demon"},
+		weightkg: 46.5,
 	},
 	haxocross: {
 		num: 2019,
@@ -152,6 +171,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Dragon", "Bug"],
 		baseStats: {hp: 78, atk: 136, def: 82, spa: 51, spd: 82, spe: 91},
 		abilities: {0: "Pestilence", 1: "Breaking Character", H: "Unsettling"},
+		weightkg: 79.8,
 	},
 	quaquadueyehisui: {
 		num: 2020,
@@ -159,6 +179,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Water", "Fighting"],
 		baseStats: {hp: 86, atk: 116, def: 80, spa: 90, spd: 85, spe: 83},
 		abilities: {0: "Kelp Power", H: "Prideful"},
+		weightkg: 49.5,
 	},
 	gholdentales: {
 		num: 2021,
@@ -166,6 +187,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Steel", "Fire"],
 		baseStats: {hp: 81, atk: 75, def: 85, spa: 111, spd: 95, spe: 93},
 		abilities: {0: "Smelting"},
+		weightkg: 25,
 	},
 	miensphlosionhisui: {
 		num: 2022,
@@ -173,6 +195,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Fire", "Fighting"],
 		baseStats: {hp: 79, atk: 106, def: 72, spa: 107, spd: 72, spe: 104},
 		abilities: {0: "Down In Flames", H: "From Ashes"},
+		weightkg: 52.7,
 	},
 	araquaton: {
 		num: 2023,
@@ -181,6 +204,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Water", "Steel"],
 		baseStats: {hp: 76, atk: 72, def: 90, spa: 60, spd: 132, spe: 68},
 		abilities: {0: "Bubble Burster", H: "Own Tides"},
+		weightkg: 97.4,
 	},
 	garganacoal: {
 		num: 2024,
@@ -188,6 +212,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Rock"],
 		baseStats: {hp: 105, atk: 100, def: 125, spa: 63, spd: 90, spe: 32},
 		abilities: {0: "Saltwater Sauna", 1: "Obsidian Body", H: "Sturdy Fire"},
+		weightkg: 275.3,
 	},
 	salazzern: {
 		num: 2025,
@@ -196,6 +221,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Flying", "Poison"],
 		baseStats: {hp: 86, atk: 67, def: 75, spa: 104, spd: 75, spe: 120},
 		abilities: {0: "Deep Toxin", H: "Clueless"},
+		weightkg: 53.6,
 	},
 	hattelure: {
 		num: 2026,
@@ -204,6 +230,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Ghost", "Fairy"],
 		baseStats: {hp: 71, atk: 72, def: 92, spa: 145, spd: 96, spe: 54},
 		abilities: {0: "Eerie Flames", 1: "Guards Up", H: "Healing Burns"},
+		weightkg: 19.7,
 	},
 	oinksler: {
 		num: 2027,
@@ -212,6 +239,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Normal", "Fighting"],
 		baseStats: {hp: 95, atk: 115, def: 77, spa: 49, spd: 80, spe: 102},
 		abilities: {0: "Smelly Touch", 1: "Berry Feast", H: "Thick Pressure"},
+		weightkg: 81.5,
 	},
 	wyrdguru: {
 		num: 2028,
@@ -219,6 +247,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Normal", "Psychic"],
 		baseStats: {hp: 105, atk: 82, def: 80, spa: 101, spd: 95, spe: 62},
 		abilities: {0: "Incorporate", 1: "Item Meddler", H: "Mind Align"},
+		weightkg: 85.6,
 	},
 	scrafdent: {
 		num: 2029,
@@ -226,6 +255,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Normal", "Fighting"],
 		baseStats: {hp: 112, atk: 92, def: 105, spa: 50, spd: 95, spe: 39},
 		abilities: {0: "Scavenge", H: "Hunger Fate"},
+		weightkg: 18,
 	},
 	toxtricoriopompomamped: {
 		num: 2030,
@@ -235,6 +265,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Flying", "Electric"],
 		baseStats: {hp: 75, atk: 84, def: 70, spa: 106, spd: 70, spe: 104},
 		abilities: {0: "Parroting"},
+		weightkg: 21.7,
 	},
 	toxtricoriobaileamped: {
 		num: 2030,
@@ -244,6 +275,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Fire", "Electric"],
 		baseStats: {hp: 75, atk: 84, def: 70, spa: 106, spd: 70, spe: 104},
 		abilities: {0: "Parroting"},
+		weightkg: 21.7,
 	},
 	toxtricoriopauamped: {
 		num: 2030,
@@ -253,6 +285,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Psychic", "Electric"],
 		baseStats: {hp: 75, atk: 84, def: 70, spa: 106, spd: 70, spe: 104},
 		abilities: {0: "Parroting"},
+		weightkg: 21.7,
 	},
 	toxtricoriosensuamped: {
 		num: 2030,
@@ -262,6 +295,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Ghost", "Electric"],
 		baseStats: {hp: 75, atk: 84, def: 70, spa: 106, spd: 70, spe: 104},
 		abilities: {0: "Parroting"},
+		weightkg: 21.7,
 	},
 	toxtricoriopompomkeylow: {
 		num: 2030,
@@ -271,6 +305,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Electric", "Poison"],
 		baseStats: {hp: 75, atk: 84, def: 70, spa: 106, spd: 70, spe: 104},
 		abilities: {0: "Parroting"},
+		weightkg: 21.7,
 	},
 	toxtricoriobailekeylow: {
 		num: 2030,
@@ -280,6 +315,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Fire", "Poison"],
 		baseStats: {hp: 75, atk: 84, def: 70, spa: 106, spd: 70, spe: 104},
 		abilities: {0: "Parroting"},
+		weightkg: 21.7,
 	},
 	toxtricoriopaukeylow: {
 		num: 2030,
@@ -289,6 +325,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Psychic", "Poison"],
 		baseStats: {hp: 75, atk: 84, def: 70, spa: 106, spd: 70, spe: 104},
 		abilities: {0: "Parroting"},
+		weightkg: 21.7,
 	},
 	toxtricoriosensukeylow: {
 		num: 2030,
@@ -298,6 +335,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Ghost", "Poison"],
 		baseStats: {hp: 75, atk: 84, def: 70, spa: 106, spd: 70, spe: 104},
 		abilities: {0: "Parroting"},
+		weightkg: 21.7,
 	},
 	sabliados: {
 		num: 2038,
@@ -305,6 +343,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Ghost", "Poison"],
 		baseStats: {hp: 60, atk: 85, def: 75, spa: 65, spd: 70, spe: 45},
 		abilities: {0: "Telescopic Sight", 1: "Slow Bugs", H: "Nightly Jokes"},
+		weightkg: 22.3,
 	},
 	fablapple: {
 		num: 2039,
@@ -312,6 +351,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Fairy", "Dragon"],
 		baseStats: {hp: 100, atk: 75, def: 91, spa: 107, spd: 85, spe: 52},
 		abilities: {0: "Supercharming Syrup", 1: "Magic Sticks", H: "Recollect"},
+		weightkg: 66.5,
 	},
 	slowzonegalar: {
 		num: 2040,
@@ -319,6 +359,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Poison", "Electric"],
 		baseStats: {hp: 90, atk: 67, def: 97, spa: 121, spd: 110, spe: 45},
 		abilities: {0: "Surgeon Eye", 1: "Neutral Polarity", H: "Tough Brains"},
+		weightkg: 129.8,
 	},
 	wigglylurk: {
 		num: 2041,
@@ -326,6 +367,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Fairy", "Ghost"],
 		baseStats: {hp: 114, atk: 114, def: 62, spa: 70, spd: 65, spe: 50},
 		abilities: {0: "All-Seeing", 1: "Tuff Claws", H: "Malfunction"},
+		weightkg: 171,
 	},
 	bravitry: {
 		num: 2042,
@@ -334,6 +376,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Normal", "Dark"],
 		baseStats: {hp: 95, atk: 118, def: 67, spa: 73, spd: 67, spe: 90},
 		abilities: {0: "Sirocco", 1: "Sun Bathe", H: "Combative"},
+		weightkg: 50.3,
 	},
 	cryomagius: {
 		num: 2043,
@@ -341,33 +384,37 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Ice", "Ghost"],
 		baseStats: {hp: 70, atk: 55, def: 55, spa: 120, spd: 120, spe: 105},
 		abilities: {0: "Levitate"},
+		weightkg: 76.2,
 	},
-	lycanrosaquapaldeamidday: {
+	lycanrosmiddaypaldeaaqua: {
 		num: 2044,
-		name: "Lycanros-Aqua-Paldea-Midday",
-		baseForme: "Aqua-Paldea-Midday",
-		forme: "Aqua-Paldea-Midday",
+		name: "Lycanros-Midday-Paldea-Aqua",
+		baseSpecies: "Lycanros",
+		forme: "Midday-Paldea-Aqua",
 		types: ["Rock", "Water"],
 		baseStats: {hp: 85, atk: 117, def: 85, spa: 42, spd: 67, spe: 109},
 		abilities: {0: "Eliminate", 1: "Oasis Lunch", H: "Fighting Fury"},
+		weightkg: 56.7,
 	},
-	lycanrosblazepaldeamidnight: {
+	lycanrosmidnightpaldeablaze: {
 		num: 2044,
-		name: "Lycanros-Blaze-Paldea-Midnight",
+		name: "Lycanros-Midnight-Paldea-Blaze",
 		baseSpecies: "Lycanros",
-		forme: "Blaze-Paldea-Midnight",
+		forme: "Midnight-Paldea-Blaze",
 		types: ["Rock", "Fire"],
 		baseStats: {hp: 95, atk: 115, def: 90, spa: 42, spd: 72, spe: 91},
 		abilities: {0: "Dominate", 1: "Night Snack", H: "Rustle Rage"},
+		weightkg: 56.7,
 	},
-	lycanroscombatpaldeadusk: {
+	lycanrosduskpaldeacombat: {
 		num: 2044,
-		name: "Lycanros-Combat-Paldea-Dusk",
+		name: "Lycanros-Dusk-Paldea-Combat",
 		baseSpecies: "Lycanros",
-		forme: "Combat-Paldea-Dusk",
+		forme: "Dusk-Paldea-Combat",
 		types: ["Rock", "Fighting"],
 		baseStats: {hp: 90, atk: 113, def: 85, spa: 42, spd: 70, spe: 105},
 		abilities: {0: "Obliterate"},
+		weightkg: 56.7,
 	},
 	revarantis: {
 		num: 2047,
@@ -375,6 +422,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Steel", "Grass"],
 		baseStats: {hp: 75, atk: 112, def: 90, spa: 87, spd: 78, spe: 67},
 		abilities: {0: "Leaf Coat", H: "Unfiltered"},
+		weightkg: 69.3,
 	},
 	laprossand: {
 		num: 2048,
@@ -382,6 +430,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Water", "Ground"],
 		baseStats: {hp: 110, atk: 80, def: 95, spa: 105, spd: 85, spe: 50},
 		abilities: {0: "Clumping Up", H: "Dissolution"},
+		weightkg: 235,
 	},
 	spiritzer: {
 		num: 2049,
@@ -389,6 +438,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Dark", "Water"],
 		baseStats: {hp: 70, atk: 82, def: 98, spa: 115, spd: 98, spe: 47},
 		abilities: {0: "Hydrostatic Pressure"},
+		weightkg: 71.7,
 	},
 	serpeblim: {
 		num: 2050,
@@ -396,6 +446,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Grass", "Ghost"],
 		baseStats: {hp: 115, atk: 80, def: 70, spa: 85, spd: 75, spe: 105},
 		abilities: {0: "Germinate", H: "Sacrifice"},
+		weightkg: 39,
 	},
 	venuvolt: {
 		num: 2051,
@@ -403,6 +454,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Poison", "Bug"],
 		baseStats: {hp: 78, atk: 76, def: 86, spa: 142, spd: 87, spe: 61},
 		abilities: {0: "Sunlit Flight"},
+		weightkg: 72.5,
 	},
 	frosmivire: {
 		num: 2052,
@@ -410,6 +462,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Bug", "Electric"],
 		baseStats: {hp: 80, atk: 95, def: 70, spa: 110, spd: 90, spe: 80},
 		abilities: {0: "Electric Dust", H: "Vital Scales"},
+		weightkg: 90.3,
 	},
 	rhyperluna: {
 		num: 2053,
@@ -417,6 +470,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Ground", "Normal"],
 		baseStats: {hp: 122, atk: 140, def: 117, spa: 50, spd: 71, spe: 50},
 		abilities: {0: "Careless", 1: "Lithoproof", H: "Shock Horror"},
+		weightkg: 286.4,
 	},
 	yanroar: {
 		num: 2054,
@@ -427,6 +481,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		abilities: {0: "Move Mastery", 1: "Item Lockdown", H: "Speedy"},
 		otherFormes: ["Yanroar-F"],
 		formeOrder: ["Yanroar", "Yanroar-F"],
+		weightkg: 66.5,
 	},
 	yanroarf: {
 		num: 2054,
@@ -435,6 +490,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Bug", "Fire"],
 		baseStats: {hp: 86, atk: 72, def: 79, spa: 112, spd: 61, spe: 100},
 		abilities: {0: "Move Mastery", 1: "Item Lockdown", H: "Speedy"},
+		weightkg: 66.5,
 	},
 	krookorotthisui: {
 		num: 2055,
@@ -442,6 +498,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Dark", "Water"],
 		baseStats: {hp: 100, atk: 115, def: 80, spa: 85, spd: 70, spe: 90},
 		abilities: {0: "Sea Monster", H: "Blade Edge"},
+		weightkg: 77.3,
 	},
 	pincunguss: {
 		num: 2056,
@@ -449,6 +506,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Electric", "Grass"],
 		baseStats: {hp: 81, atk: 93, def: 82, spa: 90, spd: 82, spe: 22},
 		abilities: {0: "Jolt Spores", H: "Respark"},
+		weightkg: 5.8,
 	},
 	mausaiai: {
 		num: 2057,
@@ -458,6 +516,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		abilities: {0: "Friendly Prank", 1: "Berry Diet", H: "Toxicologist"},
 		otherFormes: ["Mausaiai-Four"],
 		formeOrder: ["Mausaiai", "Mausaiai-Four"],
+		weightkg: 14.8,
 	},
 	mausaiaifour: {
 		num: 2057,
@@ -467,6 +526,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Normal", "Poison"],
 		baseStats: {hp: 68, atk: 85, def: 67, spa: 72, spd: 73, spe: 110},
 		abilities: {0: "Friendly Prank", 1: "Berry Diet", H: "Toxicologist"},
+		weightkg: 15,
 	},
 	incineboar: {
 		num: 2058,
@@ -474,6 +534,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Fire", "Dark"],
 		baseStats: {hp: 102, atk: 119, def: 85, spa: 92, spd: 85, spe: 62},
 		abilities: {0: "Inflame", H: "Blazing Passion"},
+		weightkg: 116.5,
 	},
 	genglizar: {
 		num: 2059,
@@ -481,6 +542,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Ghost", "Dragon"],
 		baseStats: {hp: 70, atk: 80, def: 62, spa: 107, spd: 70, spe: 126},
 		abilities: {0: "Revitalize"},
+		weightkg: 51.8,
 	},
 	tropisdale: {
 		num: 2060,
@@ -488,6 +550,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Flying", "Ground"],
 		baseStats: {hp: 100, atk: 100, def: 95, spa: 63, spd: 97, spe: 43},
 		abilities: {0: "Growing Grass", 1: "Healthy Lunch", H: "Inner Power"},
+		weightkg: 510,
 	},
 	galvillon: {
 		num: 2061,
@@ -495,6 +558,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Electric", "Flying"],
 		baseStats: {hp: 75, atk: 65, def: 55, spa: 100, spd: 55, spe: 100},
 		abilities: {0: "Friendly Looks", 1: "Freaky Eyes", H: "Dusty Bugs"},
+		weightkg: 15.7,
 	},
 	kingturne: {
 		num: 2062,
@@ -502,6 +566,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Dark", "Dragon"],
 		baseStats: {hp: 77, atk: 105, def: 77, spa: 105, spd: 77, spe: 85},
 		abilities: {0: "Hydrophilic", H: "Desert Shot"},
+		weightkg: 114.7,
 	},
 
 // NFEs
