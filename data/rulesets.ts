601337673a406ca95a94dbdcaccff8609fc78cae
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-01-18 12:18:59 -0500

    Update rulesets.ts

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -2896,7 +2896,7 @@ export const Rulesets: {[k: string]: FormatData} = {
 		onPrepareHit(target, source, move) {
 			const isTeraStellar = source.terastallized === 'Stellar';
 	  		const type = move.type;
-			if (isTeraStellar &&  && !pokemon.stellarBoostedTypes.includes(type)) {
+			if (isTeraStellar && !pokemon.stellarBoostedTypes.includes(type)) {
 				this.add('-anim', source, "Conversion", source);
 				this.hint("This move will be boosted by Stellar Tera if it hits.");
 			}
