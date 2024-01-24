f12fd9fb47a193477881e33f73319846974c3b9c
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-01-24 13:27:17 -0500

    Update scripts.ts

--- data/mods/donotuse/scripts.ts
+++ data/mods/donotuse/scripts.ts
@@ -2,6 +2,6 @@ export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
 	teambuilderConfig: {
 		excludeStandardTiers: true,
-		customTiers: ["DoNU"],
+		customTiers: ["DoNU", "DoNU Uber"],
 	},
 };
