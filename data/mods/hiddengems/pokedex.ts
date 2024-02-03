a50a69781d3fbf3d34f6a8b8cf1273ec64ea0cb8
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-02-03 14:29:09 -0500

    Create pokedex.ts

---
+++ data/mods/hiddengems/pokedex.ts
@@ -0,0 +1,7 @@
+export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
+// Special Ability Test
+	sandslash: {
+		inherit: true,
+		abilities: {0: "Sand Veil", H: "Sand Rush", S: "Iron Barbs"},
+	},
+};
