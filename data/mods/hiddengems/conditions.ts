f758887b56e99ac0db8efaa9d87a39585b5f01c9
Yoshiblaze <rosefias@gmail.com>
2024-02-28 22:24:50 -0500

    .

---
+++ data/mods/hiddengems/conditions.ts
@@ -0,0 +1,19 @@
+export const Conditions: {[k: string]: ConditionData} = {
+// Hidden Gem activation template
+/*
+	psychicgem: {
+		name: 'psychicgem',
+		duration: 1,
+		affectsFainted: true,
+		onAfterMoveSecondarySelf(source, target, move) {
+			if (!move || !target) return;
+      this.add('-message', `${source.name}'s Hidden Gem activated!`);
+      const targetType = source.types[1];
+      this.add('-start', source, 'typeadd', 'Psychic', '[from] item: Psychic Gem');
+      source.setAbility('goodasgold', source, true);
+      this.add('-activate', source, 'ability: Good as Gold');
+      this.boost({spa: 1, spd: 1, spe: 1}, source);
+		},
+	},
+*/
+};
