68730f8ebe9287031eb3fc663f070c5027ef59b0
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-03-01 22:58:30 -0500

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -474,6 +474,9 @@ export const Formats: FormatList = [
 	},
 	{
 		name: "[Gen 9] Hidden Gems",
+		threads: [
+			`<a href="https://www.smogon.com/forums/threads/3737861/">Hidden Gems</a>`,
+		],
 		mod: 'hiddengems',
 		ruleset: ['Standard', 'Terastal Clause', 'Sleep Moves Clause', '!Sleep Clause Mod'],
 		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Shadow Tag', 'King\'s Rock', 'Baton Pass', 'Last Respects', 'Shed Tail'],
