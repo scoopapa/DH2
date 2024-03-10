23bff7a02afae7aedfeb761e81732e5cd481354f
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-03-10 14:10:47 -0400

    Update scripts.ts

--- data/mods/colosseumyb/scripts.ts
+++ data/mods/colosseumyb/scripts.ts
@@ -4004,10 +4004,13 @@ export const Scripts: ModdedBattleScriptsData = {
 		this.modData("Learnsets", "mightyena").learnset.healbell = ["9L1"];
 		// Lotad
 		this.modData("Learnsets", "lotad").learnset.appleacid = ["9L1"];
+		this.modData("Learnsets", "lotad").learnset.doubleteam = ["9L1"];
 		// Lombre
 		this.modData("Learnsets", "lombre").learnset.appleacid = ["9L1"];
+		this.modData("Learnsets", "lombre").learnset.doubleteam = ["9L1"];
 		// Ludicolo
 		this.modData("Learnsets", "ludicolo").learnset.appleacid = ["9L1"];
+		this.modData("Learnsets", "ludicolo").learnset.doubleteam = ["9L1"];
 		// Seedot
 		this.modData("Learnsets", "seedot").learnset.refresh = ["9L1"];
 		// Nuzleaf
@@ -15703,4 +15706,36 @@ export const Scripts: ModdedBattleScriptsData = {
     this.modData('Learnsets', 'minior').learnset.explosion = ["9L1"];
     this.modData('Learnsets', 'turtonator').learnset.explosion = ["9L1"];
   },
+	actions: {
+		inherit: true,
+		terastallize(pokemon: Pokemon) {
+			if (pokemon.illusion && ['Ogerpon', 'Terapagos'].includes(pokemon.illusion.species.baseSpecies)) {
+				this.battle.singleEvent('End', this.dex.abilities.get('Illusion'), pokemon.abilityState, pokemon);
+			}
+			const type = 'Shadow';
+			this.battle.add('-terastallize', pokemon, type);
+			pokemon.terastallized = type;
+			for (const ally of pokemon.side.pokemon) {
+				ally.canTerastallize = null;
+			}
+			pokemon.addedType = '';
+			pokemon.knownType = true;
+			pokemon.apparentType = type;
+			if (pokemon.species.baseSpecies === 'Ogerpon') {
+				const tera = pokemon.species.id === 'ogerpon' ? 'tealtera' : 'tera';
+				pokemon.formeChange(pokemon.species.id + tera, null, true);
+			}
+			if (pokemon.species.name === 'Terapagos-Terastal' && type === 'Stellar') {
+				pokemon.formeChange('Terapagos-Stellar', null, true);
+				pokemon.baseMaxhp = Math.floor(Math.floor(
+					2 * pokemon.species.baseStats['hp'] + pokemon.set.ivs['hp'] + Math.floor(pokemon.set.evs['hp'] / 4) + 100
+				) * pokemon.level / 100 + 10);
+				const newMaxHP = pokemon.baseMaxhp;
+				pokemon.hp = newMaxHP - (pokemon.maxhp - pokemon.hp);
+				pokemon.maxhp = newMaxHP;
+				this.battle.add('-heal', pokemon, pokemon.getHealth, '[silent]');
+			}
+			this.battle.runEvent('AfterTerastallization', pokemon);
+		},
+	};
 };
