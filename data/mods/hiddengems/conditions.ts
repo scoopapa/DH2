dec137ac968aaa9991de2d7f48075475e7162700
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-03-14 21:49:51 -0400

    Update conditions.ts

--- data/mods/hiddengems/conditions.ts
+++ data/mods/hiddengems/conditions.ts
@@ -67,7 +67,6 @@ export const Conditions: {[k: string]: ConditionData} = {
 	      source.setAbility('unaware', source, true);
 	      this.add('-activate', source, 'ability: Unaware');
 	      this.boost({def: 1, spa: 1}, source);
-			this.actions.useMove("Hidden Gem Water", source, source);
 		},
 	},
 	grassgem: {
