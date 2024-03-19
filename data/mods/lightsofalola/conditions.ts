6ff59fcf7827627ee6b0c094c684ea72bb8bc0aa
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-03-19 12:26:53 -0400

    Create conditions.ts

---
+++ data/mods/lightsofalola/conditions.ts
@@ -0,0 +1,44 @@
+export const Conditions: {[k: string]: ConditionData} = {
+	frz: {
+		name: 'frz',
+		effectType: 'Status',
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'frz', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+			} else {
+				this.add('-status', target, 'frz');
+			}
+			if (target.species.name === 'Shaymin-Sky' && target.baseSpecies.baseSpecies === 'Shaymin') {
+				target.formeChange('Shaymin', this.effect, true);
+			}
+		},
+		onResidualOrder: 9,
+		onResidual(pokemon) {
+			if (this.randomChance(1, 10)) {
+				pokemon.cureStatus();
+				return;
+			}
+			this.damage(pokemon.baseMaxhp / 8);
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			this.debug('Freeze extra damage');
+			return this.chainModify([5325, 4096]);
+		},
+		onModifyMove(move, pokemon) {
+			if (move.flags['defrost']) {
+				this.add('-curestatus', pokemon, 'frz', '[from] move: ' + move);
+				pokemon.clearStatus();
+			}
+		},
+		onAfterMoveSecondary(target, source, move) {
+			if (move.thawsTarget) {
+				target.cureStatus();
+			}
+		},
+		onDamagingHit(damage, target, source, move) {
+			if (move.type === 'Fire' && move.category !== 'Status') {
+				target.cureStatus();
+			}
+		},
+	},
+};
