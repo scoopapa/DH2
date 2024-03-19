e55264bb8c73affbf2569b3c46cc82e953ca4852
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-03-19 12:18:59 -0400

    Create abilities.ts

---
+++ data/mods/lightsofalola/abilities.ts
@@ -0,0 +1,145 @@
+export const Abilities: {[k: string]: ModdedAbilityData} = {
+  // New Abilities
+	graviseeds: {
+		onDamagingHit(damage, target, source, move) {
+			this.add('-activate', target, 'ability: Graviseeds');
+			this.field.addPseudoWeather('gravity', target, target.ability);
+			if (!source.hasType('Grass')) {
+				source.addVolatile('leechseed', this.effectState.target);
+			}
+		},
+		flags: {},
+		name: "Graviseeds",
+		shortDesc: "When this Pokemon is hit by an attack, the effect of Leech Seed begins.",
+		rating: 3,
+	},
+  tippedscales: {
+		onPrepareHit(source, target, move) {
+			if (move.hasBounced || move.flags['futuremove'] || move.sourceEffect === 'snatch') return;
+			const type = move.type;
+			if (type && type !== '???' && source.getTypes().join() !== type) {
+				if (!source.setType(type)) return;
+				this.add('-start', source, 'typechange', type, '[from] ability: Tipped Scales');
+			}
+		},
+		flags: {},
+		name: "Tipped Scales",
+		rating: 5,
+		shortDesc: "This Pokemon's type changes to match the type of the move it is about to use. Works multiple times per switch-in.",
+	},
+	totemtrial: {
+		onStart(pokemon) {
+  		if ((pokemon.baseSpecies.baseSpecies === 'Gumshoos' && ['Totem'].includes(pokemon.species.forme)) ||
+          (pokemon.baseSpecies.baseSpecies === 'Salazzle' && ['Totem'].includes(pokemon.species.forme)) ||
+          (pokemon.baseSpecies.baseSpecies === 'Mimikyu' && ['Totem', 'Busted-Totem'].includes(pokemon.species.forme))) {
+			  this.boost({def: 1}, pokemon);
+			}
+    	if ((pokemon.baseSpecies.baseSpecies === 'Raticate' && ['Alola-Totem'].includes(pokemon.species.forme)) ||
+          (pokemon.baseSpecies.baseSpecies === 'Togedemaru' && ['Totem'].includes(pokemon.species.forme)) ||
+          (pokemon.baseSpecies.baseSpecies === 'Vikavolt' && ['Totem'].includes(pokemon.species.forme))) {
+			  this.boost({spd: 1}, pokemon);
+			}
+    	if ((pokemon.baseSpecies.baseSpecies === 'Lurantis' && ['Totem'].includes(pokemon.species.forme)) ||
+          (pokemon.baseSpecies.baseSpecies === 'Kommo-o' && ['Totem'].includes(pokemon.species.forme))) {
+			  this.boost({spe: 1}, pokemon);
+			}
+    	if (pokemon.baseSpecies.baseSpecies === 'Araquanid' && ['Totem'].includes(pokemon.species.forme)) {
+			  this.boost({atk: 1}, pokemon);
+			}
+    	if (pokemon.baseSpecies.baseSpecies === 'Marowak' && ['Alola-Totem'].includes(pokemon.species.forme)) {
+			  this.boost({spa: 1}, pokemon);
+			}
+      if (pokemon.baseSpecies.baseSpecies === 'Ribombee' && ['Totem'].includes(pokemon.species.forme)) {
+				pokemon.addVolatile('gmaxchistrike');
+			}
+			pokemon.addVolatile('totemtrial');
+			this.add('-message', `${pokemon.name} aura flared to life!`);
+		},
+		condition: {
+			duration: 2,
+			onEnd(pokemon) {
+			  if (pokemon.hasItem('strangesouvenir')) return;
+				this.add('-ability', pokemon, 'Totem Trial');
+				this.add('-message', `${pokemon.name} aura died down!`);
+    		if ((pokemon.baseSpecies.baseSpecies === 'Gumshoos' && ['Totem'].includes(pokemon.species.forme)) ||
+            (pokemon.baseSpecies.baseSpecies === 'Salazzle' && ['Totem'].includes(pokemon.species.forme)) ||
+            (pokemon.baseSpecies.baseSpecies === 'Mimikyu' && ['Totem', 'Busted-Totem'].includes(pokemon.species.forme))) {
+  			  this.boost({def: -1}, pokemon);
+  			}
+      	if ((pokemon.baseSpecies.baseSpecies === 'Raticate' && ['Alola-Totem'].includes(pokemon.species.forme)) ||
+            (pokemon.baseSpecies.baseSpecies === 'Togedemaru' && ['Totem'].includes(pokemon.species.forme)) ||
+            (pokemon.baseSpecies.baseSpecies === 'Vikavolt' && ['Totem'].includes(pokemon.species.forme))) {
+  			  this.boost({spd: -1}, pokemon);
+  			}
+      	if ((pokemon.baseSpecies.baseSpecies === 'Lurantis' && ['Totem'].includes(pokemon.species.forme)) ||
+            (pokemon.baseSpecies.baseSpecies === 'Kommo-o' && ['Totem'].includes(pokemon.species.forme))) {
+  			  this.boost({spe: -1}, pokemon);
+  			}
+      	if (pokemon.baseSpecies.baseSpecies === 'Araquanid' && ['Totem'].includes(pokemon.species.forme)) {
+  			  this.boost({atk: -1}, pokemon);
+  			}
+      	if (pokemon.baseSpecies.baseSpecies === 'Marowak' && ['Alola-Totem'].includes(pokemon.species.forme)) {
+  			  this.boost({spa: -1}, pokemon);
+  			}
+        if (pokemon.baseSpecies.baseSpecies === 'Ribombee' && ['Totem'].includes(pokemon.species.forme)) {
+  				pokemon.removeVolatile('gmaxchistrike');
+  			}
+      },
+		},
+		name: "Totem Trial",
+		rating: 3.5,
+    shortDesc: "Boosts a certain stat on switch-in. Boost goes away at the end of the next turn.",
+	},
+
+  // Old Abilities
+	regenerator: {
+		onSwitchOut(pokemon) {
+			if (!pokemon.volatiles['healblock']) {
+				pokemon.heal(pokemon.baseMaxhp / 3);
+			}
+		},
+		flags: {},
+		name: "Regenerator",
+		rating: 4.5,
+		num: 144,
+	},
+	normalize: {
+		onModifyTypePriority: 1,
+		onModifyType(move, pokemon) {
+			const noModifyType = [
+				'hiddenpower', 'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'struggle', 'technoblast', 'terrainpulse', 'weatherball',
+			];
+			if (!(move.isZ && move.category !== 'Status') && !noModifyType.includes(move.id) &&
+				// TODO: Figure out actual interaction
+				!(move.name === 'Tera Blast' && pokemon.terastallized)) {
+				move.type = 'Normal';
+				move.typeChangerBoosted = this.effect;
+			}
+		},
+		onBasePowerPriority: 23,
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.typeChangerBoosted === this.effect) return this.chainModify(2);
+		},
+		flags: {},
+		name: "Normalize",
+		rating: 0,
+		num: 96,
+		shortDesc: "This Pokemon's moves are changed to be Normal type and have 2x power.",
+	},
+	rivalry: {
+		onBasePowerPriority: 24,
+		onBasePower(basePower, attacker, defender, move) {
+			if (attacker.gender && defender.gender) {
+				if (attacker.gender === defender.gender) {
+					this.debug('Rivalry boost');
+					return this.chainModify(1.25);
+				}
+			}
+		},
+		flags: {},
+		name: "Rivalry",
+		rating: 3,
+		num: 79,
+		shortDesc: "This Pokemon's attacks do 1.25x on same gender targets.",
+	},
+};
