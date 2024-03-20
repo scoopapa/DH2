8ddd770d00c3e902c0337b51b283ab0f3b6599da
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-03-20 15:09:25 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -2439,7 +2439,7 @@ export const Formats: FormatList = [
 		onValidateTeam(team, format) {
 			/**@type {{[k: string]: true}}*/
 			let speciesTable = {};
-			let allowedTiers = ['OU LoA', 'UU LoA', 'RU LoA', 'NU LoA', 'NFE LoA', 'LC LoA', 'Uber LoA'];
+			let allowedTiers = ['OU LoA', 'UU LoA', 'RU LoA', 'NU LoA', 'NFE LoA', 'LC LoA'];
 			for (const set of team) {
 				let template = this.dex.species.get(set.species);
 				if (!allowedTiers.includes(template.tier)) {
