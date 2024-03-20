4dd2b9de3b129034737b24903d51f1a3d6dc1c4e
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-03-19 23:20:19 -0400

    Update items.ts

--- data/mods/lightsofalola/items.ts
+++ data/mods/lightsofalola/items.ts
@@ -318,5 +318,553 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 		desc: "If held by a Clamperl, its Atk & SpA are doubled.",
 	},
 
+// Memories
+	bugmemory: {
+		name: "Bug Memory",
+		spritenum: 673,
+		onMemory: 'Bug',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Bug') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Bug attacks. Multi-Attack is Bug-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Bug",
+		itemUser: ["Silvally-Bug"],
+		num: 909,
+		gen: 7,
+	},
+	darkmemory: {
+		name: "Dark Memory",
+		spritenum: 683,
+		onMemory: 'Dark',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Dark') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Dark attacks. Multi-Attack is Dark-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Dark",
+		itemUser: ["Silvally-Dark"],
+		num: 919,
+		gen: 7,
+	},
+	dragonmemory: {
+		name: "Dragon Memory",
+		spritenum: 682,
+		onMemory: 'Dragon',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Dragon') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Dragon attacks. Multi-Attack is Dragon-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Dragon",
+		itemUser: ["Silvally-Dragon"],
+		num: 918,
+		gen: 7,
+	},
+	electricmemory: {
+		name: "Electric Memory",
+		spritenum: 679,
+		onMemory: 'Electric',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Electric') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Electric attacks. Multi-Attack is Electric-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Electric",
+		itemUser: ["Silvally-Electric"],
+		num: 915,
+		gen: 7,
+	},
+	fairymemory: {
+		name: "Fairy Memory",
+		spritenum: 684,
+		onMemory: 'Fairy',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Fairy') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Fairy attacks. Multi-Attack is Fairy-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Fairy",
+		itemUser: ["Silvally-Fairy"],
+		num: 920,
+		gen: 7,
+	},
+	fightingmemory: {
+		name: "Fighting Memory",
+		spritenum: 668,
+		onMemory: 'Fighting',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Fighting') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Fighting attacks. Multi-Attack is Fighting-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Fighting",
+		itemUser: ["Silvally-Fighting"],
+		num: 904,
+		gen: 7,
+	},
+	firememory: {
+		name: "Fire Memory",
+		spritenum: 676,
+		onMemory: 'Fire',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Fire') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Fire attacks. Multi-Attack is Fire-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Fire",
+		itemUser: ["Silvally-Fire"],
+		num: 912,
+		gen: 7,
+	},
+	flyingmemory: {
+		name: "Flying Memory",
+		spritenum: 669,
+		onMemory: 'Flying',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Flying') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Flying attacks. Multi-Attack is Flying-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Flying",
+		itemUser: ["Silvally-Flying"],
+		num: 905,
+		gen: 7,
+	},
+	ghostmemory: {
+		name: "Ghost Memory",
+		spritenum: 674,
+		onMemory: 'Ghost',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Ghost') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Ghost attacks. Multi-Attack is Ghost-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Ghost",
+		itemUser: ["Silvally-Ghost"],
+		num: 910,
+		gen: 7,
+	},
+	grassmemory: {
+		name: "Grass Memory",
+		spritenum: 678,
+		onMemory: 'Grass',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Grass') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Grass attacks. Multi-Attack is Grass-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Grass",
+		itemUser: ["Silvally-Grass"],
+		num: 914,
+		gen: 7,
+	},
+	groundmemory: {
+		name: "Ground Memory",
+		spritenum: 671,
+		onMemory: 'Ground',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Ground') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Ground attacks. Multi-Attack is Ground-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Ground",
+		itemUser: ["Silvally-Ground"],
+		num: 907,
+		gen: 7,
+	},
+	icememory: {
+		name: "Ice Memory",
+		spritenum: 681,
+		onMemory: 'Ice',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Ice') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Ice attacks. Multi-Attack is Ice-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Ice",
+		itemUser: ["Silvally-Ice"],
+		num: 917,
+		gen: 7,
+	},
+	poisonmemory: {
+		name: "Poison Memory",
+		spritenum: 670,
+		onMemory: 'Poison',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Poison') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Poison attacks. Multi-Attack is Poison-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Poison",
+		itemUser: ["Silvally-Poison"],
+		num: 906,
+		gen: 7,
+	},
+	psychicmemory: {
+		name: "Psychic Memory",
+		spritenum: 680,
+		onMemory: 'Psychic',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Psychic') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Psychic attacks. Multi-Attack is Psychic-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Psychic",
+		itemUser: ["Silvally-Psychic"],
+		num: 916,
+		gen: 7,
+	},
+	rockmemory: {
+		name: "Rock Memory",
+		spritenum: 672,
+		onMemory: 'Rock',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Rock') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Rock attacks. Multi-Attack is Rock-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Rock",
+		itemUser: ["Silvally-Rock"],
+		num: 908,
+		gen: 7,
+	},
+	steelmemory: {
+		name: "Steel Memory",
+		spritenum: 675,
+		onMemory: 'Steel',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Steel') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Steel attacks. Multi-Attack is Steel-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Steel",
+		itemUser: ["Silvally-Steel"],
+		num: 911,
+		gen: 7,
+	},
+	watermemory: {
+		name: "Water Memory",
+		spritenum: 677,
+		onMemory: 'Water',
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.num === 773) || pokemon.baseSpecies.num === 773) {
+				return false;
+			}
+			return true;
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Water') {
+				return this.chainModify(0.75);
+			}
+		},
+		desc: "Holder's takes 0.75x damage from Water attacks. Multi-Attack is Water-type.",
+		isNonstandard: null,
+		forcedForme: "Silvally-Water",
+		itemUser: ["Silvally-Water"],
+		num: 913,
+		gen: 7,
+	},
+
 // Z-Crystals
+	terapagiumz: {
+		name: "Terapagium Z",
+		spritenum: 687,
+		onTakeItem: false,
+		itemUser: ["Terapagos", "Terapagos-Terastal", "Terapagos-Stellar"],
+		gen: 9,
+		desc: "Allows Terapagos to Tera Burst into Terapagos-Stellar.",
+	},
+	ultranecroziumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	wateriumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	aloraichiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	buginiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	darkiniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	decidiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	dragoniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	eeviumz: {
+		name: "Eevium Z",
+		spritenum: 657,
+		onTakeItem: false,
+		zMove: "Extreme Evoboost",
+		zMoveFrom: "Evoboost",
+		itemUser: ["Eevee"],
+		num: 805,
+		gen: 7,
+		isNonstandard: null,
+		desc: "If held by a Eevee with Evoboost, it can use Extreme Evoboost.",
+	},
+	electriumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	fairiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	fightiniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	firiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	flyiniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	ghostiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	grassiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	groundiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	iciumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	inciniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	kommoniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lunaliumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lycaniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	marshadiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mewniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mimikiumz: {
+		name: "Mimikium Z",
+		spritenum: 688,
+		onTakeItem: false,
+		zMove: "Let's Snuggle Forever",
+		zMoveFrom: "Shadow Claw",
+		itemUser: ["Mimikyu", "Mimikyu-Busted", "Mimikyu-Totem", "Mimikyu-Busted-Totem"],
+		num: 924,
+		isNonstandard: null,
+		gen: 7,
+		desc: "If held by a Mimikyu with Shadow Claw, it can use Let's Snuggle Forever.",
+	},
+	normaliumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	pikaniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	pikashuniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	poisoniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	primariumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	psychiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	rockiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	snorliumz: {
+		name: "Snorlium Z",
+		spritenum: 656,
+		onTakeItem: false,
+		zMove: "Pulverizing Pancake",
+		zMoveFrom: "Body Slam",
+		itemUser: ["Snorlax"],
+		num: 804,
+		gen: 7,
+		isNonstandard: null,
+		desc: "If held by a Snorlax with Body Slam, it can use Pulverizing Pancake.",
+	},
+	solganiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},	
+	steeliumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	tapuniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
 };
