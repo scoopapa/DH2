bea3b4cfd0f1576b2bbc8fb6fdd51f91a192bef5
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-04-02 23:06:24 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -3143,6 +3143,18 @@ export const Formats: FormatList = [
 		unbanlist: ['Pichu', 'Cleffa', 'Igglybuff', 'Togepi', 'Tyrogue', 'Smoochum', 'Elekid', 'Magby', 'Azurill', 'Wynaut', 'Budew', 'Chingling', 'Bonsly', 'Mime Jr.', 'Happiny', 'Munchlax', 'Riolu', 'Mantyke', 'Toxel'],
   },
 	{
+		name: "[Gen 9] Littlest Cup Random Battle",
+		desc: [
+			"<b>Littlest Cup</b>: A National Dex metagame where only Baby Pokemon are allowed."
+		],
+		threads: [
+			`&bullet; <a href="https://pastebin.com/PtqmRUhG">Littlest Cup VR and Sample Sets</a>`,
+		],
+		mod: 'littlestcup',
+		team: 'random',
+		ruleset: ['Data Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Endless Battle Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Z-Move Clause', 'Max Level = 1'],
+	},
+	{
 		name: "[Gen 6] TPDP Open",
 		mod: 'tpdp',
 		debug: true,
