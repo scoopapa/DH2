4353f6069af71889e4ee8b5e6df36bda202855f5
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-04-28 23:35:55 -0400

    Update conditions.ts

--- data/mods/kantorewind/conditions.ts
+++ data/mods/kantorewind/conditions.ts
@@ -3,9 +3,9 @@ export const Conditions: {[k: string]: ConditionData} = {
 		name: 'Retro Rewind',
 		noCopy: true,
 		onStart(pokemon) {
-      this.add('-start', pokemon, 'Retro Rewind');
-      const newspa = pokemon.storedStats.spd;
-      const newspd = pokemon.storedStats.spa;
+      	this.add('-start', pokemon, 'Retro Rewind');
+      	const newspa = pokemon.storedStats.spd;
+      	const newspd = pokemon.storedStats.spa;
 			if (pokemon.getStat('spd', false, true) > pokemon.getStat('spa', false, true)) {
         		pokemon.storedStats.spa = newspa;
       	} else {
@@ -19,9 +19,12 @@ export const Conditions: {[k: string]: ConditionData} = {
       	}
 		},
 		onModifyMove(move) {
-  		if ((move.type === 'Fire' || move.type === 'Water' || move.type === 'Grass' || move.type === 'Electric' || move.type === 'Dark' || move.type === 'Psychic' || move.type === 'Dragon' || move.type === 'Fairy')  && move.category === 'Physical') move.category = 'Special';
+  			if ((move.type === 'Fire' || move.type === 'Water' || move.type === 'Grass' || move.type === 'Electric' || move.type === 'Dark' || move.type === 'Psychic' || move.type === 'Dragon' || move.type === 'Fairy')  && move.category === 'Physical') move.category = 'Special';
 			if ((move.type === 'Normal' || move.type === 'Fighting' || move.type === 'Flying' || move.type === 'Ground' || move.type === 'Rock' || move.type === 'Bug' || move.type === 'Ghost' || move.type === 'Poison' || move.type === 'Steel')  && move.category === 'Special') move.category = 'Physical';
-			if (move.critRatio => 2) {
+			if (['nightslash','slash','psychocut','stoneedge','blazekick','ivycudgel',
+				  'shadowclaw','snipeshot','crabhammer','aeroblast','aircutter','aquacutter','attackorder','crosschop',
+				  'crosspoison','direclaw','drillrun','esperwing','karatechop','leafblade','poisontail',
+				  'razorleaf','razorwind','skyattack','spacialrend','triplearrows'].includes(move.id)) {
 			  move.willCrit = true;
       	}
 		},
