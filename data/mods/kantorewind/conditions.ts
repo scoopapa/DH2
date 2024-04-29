383359aedf58ba7a50b32240468440e1ff296409
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-04-28 23:24:24 -0400

    Update conditions.ts

--- data/mods/kantorewind/conditions.ts
+++ data/mods/kantorewind/conditions.ts
@@ -7,23 +7,23 @@ export const Conditions: {[k: string]: ConditionData} = {
       const newspa = pokemon.storedStats.spd;
       const newspd = pokemon.storedStats.spa;
 			if (pokemon.getStat('spd', false, true) > pokemon.getStat('spa', false, true)) {
-        pokemon.storedStats.spa = newspa;
-      } else {
-        pokemon.storedStats.spd = newspd;
-      }
+        		pokemon.storedStats.spa = newspa;
+      	} else {
+        		pokemon.storedStats.spd = newspd;
+      	}
 		},
 		onModifySecondaries(secondaries, move, target) {
 			if (target.hasType(move.type))) {
 			  this.debug('Same type prevent secondary');
 			  return secondaries.filter(effect => !!(effect.self || effect.dustproof));
-      }
+      	}
 		},
 		onModifyMove(move) {
   		if ((move.type === 'Fire' || move.type === 'Water' || move.type === 'Grass' || move.type === 'Electric' || move.type === 'Dark' || move.type === 'Psychic' || move.type === 'Dragon' || move.type === 'Fairy')  && move.category === 'Physical') move.category = 'Special';
 			if ((move.type === 'Normal' || move.type === 'Fighting' || move.type === 'Flying' || move.type === 'Ground' || move.type === 'Rock' || move.type === 'Bug' || move.type === 'Ghost' || move.type === 'Poison' || move.type === 'Steel')  && move.category === 'Special') move.category = 'Physical';
 			if (move.critRatio => 2) {
 			  move.willCrit = true;
-      }
+      	}
 		},
 		onAfterMoveSecondary(target, source, move) {
 			if (!target.hp && source.volatiles['mustrecharge']) {
