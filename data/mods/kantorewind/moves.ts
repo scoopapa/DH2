b8c9f4e7aa1c0146822c4bfe800f08d0c0fe319d
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-04-28 22:59:16 -0400

    Create moves.ts

---
+++ data/mods/kantorewind/moves.ts
@@ -0,0 +1,30 @@
+export const Moves: {[k: string]: ModdedMoveData} = {
+	spikecannon: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	hiddenpower: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	boneclub: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lovelykiss: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	bonemerang: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	shadowbone: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	meteorassault: {
+		inherit: true,
+		isNonstandard: null,
+	},
+};
