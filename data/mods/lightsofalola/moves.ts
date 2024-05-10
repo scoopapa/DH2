6eccdb0baf0166d866d531076af56f2bac464635
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-05-10 15:11:40 -0400

    Update moves.ts

--- data/mods/lightsofalola/moves.ts
+++ data/mods/lightsofalola/moves.ts
@@ -1776,7 +1776,44 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		type: "Normal",
 		contestType: "Cool",
 	},
-
+	terablast: {
+		num: 851,
+		accuracy: 100,
+		basePower: 70,
+		basePowerCallback(pokemon, target, move) {
+			if (pokemon.terastallized === 'Stellar') {
+				return 100;
+			}
+			return move.basePower;
+		},
+		category: "Special",
+		shortDesc: "Physical if user's Atk > Sp. Atk.",
+		name: "Tera Blast",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, metronome: 1, mustpressure: 1},
+		onPrepareHit(target, source, move) {
+			if (source.terastallized) {
+				this.attrLastMove('[anim] Tera Blast ' + source.teraType);
+			}
+		},
+		onModifyType(move, pokemon, target) {
+			if (pokemon.terastallized) {
+				move.type = pokemon.teraType;
+			}
+		},
+		onModifyMove(move, pokemon) {
+			if (pokemon.getStat('atk', false, true) > pokemon.getStat('spa', false, true)) {
+				move.category = 'Physical';
+			}
+			if (pokemon.terastallized === 'Stellar') {
+				move.self = {boosts: {atk: -1, spa: -1}};
+			}
+		},
+		secondary: null,
+		target: "normal",
+		type: "Stellar",
+	},
 
 	// Undexited Moves	
 	snatch: {
