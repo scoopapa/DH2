0554e613350f1ac995b3e1bd387a6dbc013fe4f9
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2024-05-23 10:37:43 +0700

    Update moves.ts

--- data/mods/metamons/moves.ts
+++ data/mods/metamons/moves.ts
@@ -467,8 +467,9 @@ export const Moves: {[moveid: string]: MoveData} = {
 			if (pokemon.species.name === 'Tauros-Paldea-Blaze') {
 				move.volatileStatus = 'partiallytrapped';
 			}
-			if (!move.secondaries) move.secondaries = [];
-         if (pokemon.species.name === 'Tauros-Paldea-Aqua') {
+			if (pokemon.species.name !== 'Tauros-Paldea-Aqua') return;
+			move.secondaries = [];
+			if (pokemon.species.name === 'Tauros-Paldea-Aqua') {
 			   move.secondaries.push({
 				   chance: 40,
 			   	sideCondition: 'swamp',
