59a556835eabe8fc93a3f72ed95cae7c6a96fcc5
Thiediev <chanjection@gmail.com>
2024-05-23 21:30:13 -0500

    Update moves.ts

--- data/mods/gen1violetversion/moves.ts
+++ data/mods/gen1violetversion/moves.ts
@@ -122,20 +122,20 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 				undefined;
 		},
 		condition: {
-			onStart(pokemon) {
+			onStart(target) {
 				// disable can only select moves that have pp > 0, hence the onTryHit modification
-				const moveSlot = this.sample(pokemon.moveSlots.filter(ms => ms.pp > 0));
-				this.add('-start', pokemon, 'Disable', moveSlot.move);
+				const moveSlot = this.sample(target.moveSlots.filter(ms => ms.pp > 0));
+				this.add('-start', target, 'Disable', moveSlot.move);
 				this.effectState.move = moveSlot.id;
 				// 1-8 turns (which will in effect translate to 0-7 missed turns for the target)
 			},
 			onEnd(pokemon) {
 				this.add('-end', pokemon, 'Disable');
 			},
-			onDisableMove(pokemon) {
-				for (const moveSlot of pokemon.moveSlots) {
+			onDisableMove(target) {
+				for (const moveSlot of target.moveSlots) {
 					if (moveSlot.id === this.effectState.move) {
-						pokemon.disableMove(moveSlot.id);
+						target.disableMove(moveSlot.id);
 					}
 				}
 			},
