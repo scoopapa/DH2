797d6cb10bff94584a5ad94cb971a36555f8da4c
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-05-28 14:02:09 -0400

    Update scripts.ts

--- data/mods/gen6megasrevisited/scripts.ts
+++ data/mods/gen6megasrevisited/scripts.ts
@@ -13,8 +13,8 @@ export const Scripts: ModdedBattleScriptsData = {
 		},
 	},
 	init: function () {
-		this.modData("Learnsets", "lucario").learnset.meteormash = ["6L1"];
 		this.modData("Learnsets", "lucario").learnset.machpunch = ["6L1"];
+		this.modData("Learnsets", "lucario").learnset.meteormash = ["6L1"];
 		this.modData("Learnsets", "houndoom").learnset.toxicspikes = ["6L1"];
 		this.modData("Learnsets", "houndoom").learnset.venoshock = ["6L1"];
 		this.modData("Learnsets", "houndoom").learnset.hex = ["6L1"];
