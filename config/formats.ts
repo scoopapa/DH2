fdbbd131143efacb1b9b4f6df3fed81d773e3e53
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-05-28 16:17:49 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -2048,6 +2048,29 @@ export const Formats: FormatList = [
 		},
 	},
 	{
+        name: "[Gen 9] Climate Change",
+        desc: [
+            "weather war",
+        ],
+        threads: [
+            `&bullet; <a href="https://www.youtube.com/shorts/bbZCltuyZlM">Climate Change on Smogon Forums</a>`,
+              ],
+        ruleset: ['Standard NatDex', 'Terastal Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Z-Move Clause'],
+        banlist: ['Sunny Day', 'Rain Dance', 'Sandstorm', 'Hail', 'Snowscape', 'Chilly Reception'],
+		
+        onValidateTeam(team, format) {
+            /**@type {{[k: string]: true}} */
+            let speciesTable = {};
+            for (const set of team) {
+                let template = this.dex.species.get(set.species);
+                if (template.tier !== 'CC') {
+                    return [set.species + ' is not usable in Climate Change.'];
+                }
+            }
+        },
+        mod: 'weatherwar',
+    },
+	{
 		name: "[Gen 9] Clubmons: Requiem",
 		mod: 'clubmonsrequiem',
 		desc: `A micrometagame focused on accessibility and teambuilder diversity.`,
