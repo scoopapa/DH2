f500aaa2e0e40e7cf08945533b8272c3ad3d4104
Thiediev <chanjection@gmail.com>
2024-05-28 20:22:52 -0500

    Update moves.ts

--- data/mods/gen1violetversion/moves.ts
+++ data/mods/gen1violetversion/moves.ts
@@ -124,7 +124,9 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			noCopy: true, // doesn't get copied by Baton Pass
 			onSideStart (targetSide, target) {
 				let moves = target.moves;
-				let moveId = moves[this.random(moves.length)];
+				const moveSlot = this.sample(pokemon.moveSlots.filter(ms => ms.pp > 0));
+				this.add('-start', pokemon, 'Disable', moveSlot.move);
+				this.effectState.move = moveSlot.id;
 				if (!moveId) return false;
 				let move = this.dex.moves.get(moveId);
 				this.add('-start', target, 'Disable', move.name);
