63fe29cf1c0bdb6afaca49f4c1162fae8bef2ef0
Thiediev <chanjection@gmail.com>
2024-05-30 21:43:04 -0500

    Update moves.ts

--- data/mods/gen1violetversion/moves.ts
+++ data/mods/gen1violetversion/moves.ts
@@ -132,12 +132,21 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 				let move = this.dex.moves.get(moveId);
 				this.add('-start', target, 'Disable', move.name);
 				this.effectState.move = move.id;**/
-				for (const moveSlot of target.moveSlots) {
+				
+				for (const pokemon of target.side.pokemon) {
+					if (pokemon.isActive) {
+						const moveSlot = this.sample(pokemon.moveSlots.filter(ms => ms.pp > 0));
+						this.add('-start', pokemon, 'Disable', moveSlot.move)
+						this.effectState.move = moveSlot.id; 
+					}
+				}
+						
+		/**		for (const moveSlot of target.moveSlots) {
 					const moveid = moveSlot.id;
 					const move = this.dex.moves.get(moveid);
 					this.add('-start', target, 'Disable', moveSlot.move);
-					this.effectState.move = moveSlot.id;
-				}
+					this.effectState.move = moveSlot.id; 
+				}**/
 		/**	for (const pokemon of target.side.pokemon) {
 				if (pokemon === target) {
 					for (const moveSlot of pokemon.moveSlots) {
