a208e1d4775a28c4c0c5d4649ab019c79d83db9a
ausmacon <107449244+ausmacon@users.noreply.github.com>
2024-06-04 17:44:21 -0500

    Create conditions.ts (for pale winds)

---
+++ data/mods/clubmonsrequiem/conditions.ts
@@ -0,0 +1,34 @@
+export const Conditions: {[k: string]: ConditionData} = {
+  hail: {
+  		name: 'Hail',
+  		effectType: 'Weather',
+  		duration: 5,
+  		durationCallback(source, effect) {
+  			if (source?.hasItem('icyrock')) {
+  				return 8;
+  			}
+  			return 5;
+  		},
+  		onFieldStart(field, source, effect) {
+  			if (effect?.effectType === 'Ability') {
+  				if (this.gen <= 5) this.effectState.duration = 0;
+  				this.add('-weather', 'Hail', '[from] ability: ' + effect.name, '[of] ' + source);
+  			} else {
+  				this.add('-weather', 'Hail');
+  			}
+  		},
+  		onFieldResidualOrder: 1,
+  		onFieldResidual() {
+  			this.add('-weather', 'Hail', '[upkeep]');
+  			if (this.field.isWeather('hail')) this.eachEvent('Weather');
+  		},
+  		onWeather(target) {
+        if (this.effectState.palewinds = true;) {
+  			  this.damage(target.baseMaxhp / 8);
+  		  } else this.damage(target.baseMaxhp / 16);
+      },
+  		onFieldEnd() {
+  			this.add('-weather', 'none');
+  		},
+  },
+};
