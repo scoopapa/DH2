e6ef2f3a0f8dd1bafa54bfb457e39b4ac3fd68f4
Thiediev <chanjection@gmail.com>
2024-06-05 17:56:33 -0500

    Update moves.ts

--- data/mods/gen1violetversion/moves.ts
+++ data/mods/gen1violetversion/moves.ts
@@ -135,8 +135,8 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 					return;
 			}, **/
 			onHit (target, move) {
+				const moveSlot = this.sample(target.moveSlots.filter(ms => ms.pp > 0));
 				if (move.id === 'disable')
-					const moveSlot = this.sample(target.moveSlots.filter(ms => ms.pp > 0));
 					this.add('-start', target, 'Disable', moveSlot.move)
 					this.effectState.move = moveSlot.id; 
 					return;
