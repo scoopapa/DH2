712d75fde17608676007aeb7500b58eb2f8e7d45
Thiediev <chanjection@gmail.com>
2024-06-09 15:43:30 -0500

    Update moves.ts

--- data/mods/gen1violetversion/moves.ts
+++ data/mods/gen1violetversion/moves.ts
@@ -101,7 +101,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		category: "Status",
 		sideCondition: 'convertflareon',
 		condition: {
-			onStart(target, source) {
+			onSideStart(target, source) {
 				//if (target.species.baseSpecies === 'Porygon') {
 				this.add('-start', source, 'typechange', 'Fire');
 				//source.setType(source.getTypes(true).map(type => type === "Fire" ? "???" : type));
