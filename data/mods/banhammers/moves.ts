36c336e61445e028cecaa417abdae1a922390092
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-06-10 16:24:49 -0400

    Update moves.ts

--- data/mods/banhammers/moves.ts
+++ data/mods/banhammers/moves.ts
@@ -1,6 +1,4 @@
-// List of flags and their descriptions can be found in sim/dex-moves.ts
-
-export const Moves: {[moveid: string]: MoveData} = {
+export const Moves: {[k: string]: ModdedMoveData} = {
 	earthpower: {
 		inherit: true,
 		isNonstandard: "Past",
