3e8ea20fe95c7c41a96b71d5395556718479c4d7
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-06-19 02:23:26 -0400

    Update items.ts

--- data/mods/lightsofalola/items.ts
+++ data/mods/lightsofalola/items.ts
@@ -713,11 +713,6 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 				return this.chainModify([4915, 4096]);
 			}
 		},
-		onModifySTAB(stab, source, target, move) {
-			if (source.baseSpecies.name.startsWith('Terapagos-Stellar')) {
-				return 1;
-			}
-		},
 		itemUser: ["Terapagos", "Terapagos-Terastal", "Terapagos-Stellar"],
 		gen: 9,
 		desc: "Allows Terapagos to Tera Burst into Terapagos-Stellar.",
