babcb85b46a13e4f6bba9df801ddbd600daf6322
KnivesTotality <olivesparkly@gmail.com>
2024-06-21 03:56:17 -0400

    created formats-data.ts

---
+++ data/mods/monsterhunter/formats-data.ts
@@ -0,0 +1,182 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	aknosom: {
+		tier: "OU",
+	},
+	arzuros: {
+		tier: "OU",
+	},
+	barroth: {
+		tier: "OU",
+	},
+	basarios: {
+		tier: "OU",
+	},
+	greatbaggi: {
+		tier: "OU",
+	},
+	greatizuchi: {
+		tier: "OU",
+	},
+	greatwroggi: {
+		tier: "OU",
+	},
+	kuluyaku: {
+		tier: "OU",
+	},
+	khezu: {
+		tier: "OU",
+	},
+	lagombi: {
+		tier: "OU",
+	},
+	royalludroth: {
+		tier: "OU",
+	},
+	tetranadon: {
+		tier: "OU",
+	},
+	anjanath: {
+		tier: "OU",
+	},
+	barioth: {
+		tier: "OU",
+	},
+	bishaten: {
+		tier: "OU",
+	},
+	daimyo: {
+		tier: "OU",
+	},
+	jyuratodus: {
+		tier: "OU",
+	},
+	pukeipukei: {
+		tier: "OU",
+	},
+	somnacanth: {
+		tier: "OU",
+	},
+	tobikadachi: {
+		tier: "OU",
+	},
+	volvidon: {
+		tier: "OU",
+	},
+	gossharag: {
+		tier: "OU",
+	},
+	auroracanth: {
+		tier: "OU",
+	},
+	almudron: {
+		tier: "OU",
+	},
+	orangaten: {
+		tier: "OU",
+	},
+	diablos: {
+		tier: "OU",
+	},
+	garangolm: {
+		tier: "OU",
+	},
+	mizutsune: {
+		tier: "OU",
+	},
+	nargacuga: {
+		tier: "OU",
+	},
+	rathalos: {
+		tier: "OU",
+	},
+	rathian: {
+		tier: "OU",
+	},
+	shogun: {
+		tier: "OU",
+	},
+	zinogre: {
+		tier: "OU",
+	},
+	astalos: {
+		tier: "OU",
+	},
+	espinas: {
+		tier: "OU",
+	},
+	gore: {
+		tier: "OU",
+	},
+	lunagaron: {
+		tier: "OU",
+	},
+	magmadron: {
+		tier: "OU",
+	},
+	magnamalo: {
+		tier: "OU",
+	},
+	rakna: {
+		tier: "OU",
+	},
+	pyre: {
+		tier: "OU",
+	},
+	seregios: {
+		tier: "OU",
+	},
+	tigrex: {
+		tier: "OU",
+	},
+	bazel: {
+		tier: "OU",
+	},
+	chameleos: {
+		tier: "OU",
+	},
+	kushala: {
+		tier: "OU",
+	},
+	teostra: {
+		tier: "OU",
+	},
+	flamingespinas: {
+		tier: "OU",
+	},
+	malzeno: {
+		tier: "OU",
+	},
+	rajang: {
+		tier: "OU",
+	},
+	shagaru: {
+		tier: "OU",
+	},
+	chaotic: {
+		tier: "OU",
+	},
+	velkhana: {
+		tier: "OU",
+	},
+	violetmizutsune: {
+		tier: "OU",
+	},
+	lucent: {
+		tier: "OU",
+	},
+	ibushi: {
+		tier: "OU",
+	},
+	narwa: {
+		tier: "OU",
+	},
+	amatsu: {
+		tier: "OU",
+	},
+	valstrax: {
+		tier: "OU",
+	},
+	gaismagorm: {
+		tier: "OU",
+	},
+};
