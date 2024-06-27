63f1f4fccf50c7facd157e8a610dc25e733249c4
ViZarSmogon <vizarsmogon@gmail.com>
2024-06-27 14:01:44 +0200

    Update abilities.ts

--- data/mods/showcase/abilities.ts
+++ data/mods/showcase/abilities.ts
@@ -5,5 +5,5 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 			if (move?.type === 'Flying' && pokemon.hp === pokemon.maxhp) return priority + 2;
 		},
 		shortDesc: "If this Pokemon is at full HP, its Flying-type moves have their priority increased by 2.",
-	}
+	
 };
