2f0bfcbd68fe2244ad5b1b66e27cf82413b81b3e
KirbyRider1337 <kirbyrider1337@yahoo.com>
2024-07-09 04:55:34 -0400

    New Gen X slate (also sprites for the Grass and Fire LCs)

--- data/mods/genxbrunica/conditions.ts
+++ data/mods/genxbrunica/conditions.ts
@@ -22,6 +22,36 @@ export const Conditions: {[k: string]: ConditionData} = {
 			}
 		},
 	},
+	twoturnmove: {
+		// Cloning Genes
+		inherit: true,
+		onStart(attacker, defender, effect) {
+			// ("attacker" is the Pokemon using the two turn move and the Pokemon this condition is being applied to)
+			this.effectState.move = effect.id;
+			attacker.addVolatile(effect.id);
+			// lastMoveTargetLoc is the location of the originally targeted slot before any redirection
+			// note that this is not updated for moves called by other moves
+			// i.e. if Dig is called by Metronome, lastMoveTargetLoc will still be the user's location
+			let moveTargetLoc: number = attacker.lastMoveTargetLoc!;
+			if (effect.sourceEffect && this.dex.moves.get(effect.id).target !== 'self') {
+				// this move was called by another move such as Metronome
+				// and needs a random target to be determined this turn
+				// it will already have one by now if there is any valid target
+				// but if there isn't one we need to choose a random slot now
+				if (defender.fainted) {
+					defender = this.sample(attacker.foes(true));
+				}
+				moveTargetLoc = attacker.getLocOf(defender);
+			}
+			attacker.volatiles[effect.id].targetLoc = moveTargetLoc;
+			this.attrLastMove('[still]');
+			if (defender.hasAbility('cloninggenes')) {
+				defender.addVolatile('cloninggenes', defender);
+			}
+			// Run side-effects normally associated with hitting (e.g., Protean, Libero)
+			this.runEvent('PrepareHit', attacker, defender, effect);
+		},
+	},
 	typebalm: {
 		//This is here to track whether a mon has used a Type Balm this switch-in
 		duration: 0,
@@ -31,7 +61,9 @@ export const Conditions: {[k: string]: ConditionData} = {
 			const balmMoveList = {
 				'Photalohm': 'Magnetic Updraft',
 				'Smeltusk': 'Leaping Onrush',
-				'Panthoard': 'Cupric Deluge'
+				'Panthoard': 'Cupric Deluge',
+				'Mustelone': 'Clone Express',
+				'Froskua': 'Dive Bomb'
 			};
 			const species = target.baseSpecies.baseSpecies;
 			//...All this to adjust the PP. 
