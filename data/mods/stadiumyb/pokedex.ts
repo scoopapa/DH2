863da956805c87f164e02903ec5cad09f4f7a51b
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-07-10 23:10:03 -0400

    Create pokedex.ts

---
+++ data/mods/stadiumyb/pokedex.ts
@@ -0,0 +1,540 @@
+export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
+	snivy: {
+		inherit: true,
+		abilities: {0: "Contrary"},
+	},
+	servine: {
+		inherit: true,
+		abilities: {0: "Contrary"},
+	},
+	serperior: {
+		inherit: true,
+		abilities: {0: "Overgrow"},
+	},
+	torchic: {
+		inherit: true,
+		abilities: {0: "Speed Boost"},
+	},
+	combusken: {
+		inherit: true,
+		abilities: {0: "Speed Boost"},
+	},
+	blaziken: {
+		inherit: true,
+		abilities: {0: "Blaze"},
+	},
+	totodile: {
+		inherit: true,
+		abilities: {0: "Sheer Force"},
+	},
+	croconaw: {
+		inherit: true,
+		abilities: {0: "Sheer Force"},
+	},
+	feraligatr: {
+		inherit: true,
+		abilities: {0: "Torrent"},
+	},
+	minccino: {
+		inherit: true,
+		abilities: {0: "Technician"},
+	},
+	cinccino: {
+		inherit: true,
+		abilities: {0: "Skill Link"},
+	},
+	okidogi: {
+		inherit: true,
+		abilities: {0: "Toxic Chain"},
+	},
+	munkidori: {
+		inherit: true,
+		abilities: {0: "Toxic Chain"},
+	},
+	fezandipiti: {
+		inherit: true,
+		abilities: {0: "Toxic Chain"},
+	},
+	pecharunt: {
+		num: 1015,
+		abilities: {0: "Poison Puppeteer"},
+	},
+	smeargle: {
+		inherit: true,
+		abilities: {0: "Technician"},
+	},
+	pansear: {
+		inherit: true,
+		abilities: {0: "Gluttony"},
+	},
+	simisear: {
+		inherit: true,
+		abilities: {0: "Blaze"},
+	},
+	nymble: {
+		inherit: true,
+		abilities: {0: "Tinted Lens"},
+	},
+	lokix: {
+		inherit: true,
+		abilities: {0: "Tinted Lens"},
+	},
+	doduo: {
+		inherit: true,
+		abilities: {0: "Tangled Feet"},
+	},
+	dodrio: {
+		inherit: true,
+		abilities: {0: "Early Bird"},
+	},
+	riolu: {
+		inherit: true,
+		abilities: {0: "Prankster"},
+	},
+	lucario: {
+		inherit: true,
+		abilities: {0: "Steadfast"},
+	},
+	eevee: {
+		inherit: true,
+		abilities: {0: "Adaptability"},
+	},
+	vaporeon: {
+		inherit: true,
+		abilities: {0: "Water Absorb"},
+	},
+	jolteon: {
+		inherit: true,
+		abilities: {0: "Quick Feet"},
+	},
+	flareon: {
+		inherit: true,
+		abilities: {0: "Flash Fire"},
+	},
+	espeon: {
+		inherit: true,
+		abilities: {0: "Magic Bounce"},
+	},
+	umbreon: {
+		inherit: true,
+		abilities: {0: "Inner Focus"},
+	},
+	leafeon: {
+		inherit: true,
+		abilities: {0: "Chlorophyll"},
+	},
+	glaceon: {
+		inherit: true,
+		abilities: {0: "Ice Body"},
+	},
+	sylveon: {
+		inherit: true,
+		abilities: {0: "Pixilate"},
+	},
+	ralts: {
+		inherit: true,
+		abilities: {0: "Trace"},
+	},
+	kirlia: {
+		inherit: true,
+		abilities: {0: "Trace"},
+	},
+	gardevoir: {
+		inherit: true,
+		abilities: {0: "Synchronize"},
+	},
+	gallade: {
+		inherit: true,
+		abilities: {0: "Justified"},
+	},
+	buizel: {
+		inherit: true,
+		abilities: {0: "Swift Swim"},
+	},
+	floatzel: {
+		inherit: true,
+		abilities: {0: "Water Veil"},
+	},
+	marshadow: {
+		inherit: true,
+		abilities: {0: "Technician"},
+	},
+	absol: {
+		inherit: true,
+		abilities: {0: "Super Luck"},
+	},
+	cranidos: {
+		inherit: true,
+		abilities: {0: "Mold Breaker"},
+	},
+	rampardos: {
+		inherit: true,
+		abilities: {0: "Sheer Force"},
+	},
+	shieldon: {
+		inherit: true,
+		abilities: {0: "Sturdy"},
+	},
+	bastiodon: {
+		inherit: true,
+		abilities: {0: "Soundproof"},
+	},
+	dracovish: {
+		inherit: true,
+		abilities: {0: "Water Absorb"},
+	},
+	dracozolt: {
+		inherit: true,
+		abilities: {0: "Volt Absorb"},
+	},
+	arctovish: {
+		inherit: true,
+		abilities: {0: "Slush Rush"},
+	},
+	arctozolt: {
+		inherit: true,
+    abilities: {0: "Static"},
+	},
+	aerodactyl: {
+		inherit: true,
+		abilities: {0: "Unnerve"},
+	},
+	pansage: {
+		inherit: true,
+		abilities: {0: "Overgrow"},
+	},
+	simisage: {
+		inherit: true,
+		abilities: {0: "Gluttony"},
+	},
+	panpour: {
+		inherit: true,
+		abilities: {0: "Torrent"},
+	},
+	simipour: {
+		inherit: true,
+		abilities: {0: "Gluttony"},
+	},
+	zangoose: {
+		inherit: true,
+		abilities: {0: "Toxic Boost"},
+	},
+	seviper: {
+		inherit: true,
+		abilities: {0: "Shed Skin"},
+	},
+	genesect: {
+		inherit: true,
+		abilities: {0: "Download"},
+	},
+	cobalion: {
+		inherit: true,
+		abilities: {0: "Justified"},
+	},
+	virizion: {
+		inherit: true,
+		abilities: {0: "Justified"},
+	},
+	terrakion: {
+		inherit: true,
+		abilities: {0: "Justified"},
+	},
+	keldeo: {
+		inherit: true,
+		abilities: {0: "Justified"},
+	},
+	poochyena: {
+		inherit: true,
+		abilities: {0: "Quick Feet"},
+	},
+	mightyena: {
+		inherit: true,
+		abilities: {0: "Intimidate"},
+	},
+	dratini: {
+		inherit: true,
+		abilities: {0: "Shed Skin"},
+	},
+	dragonair: {
+		inherit: true,
+		abilities: {0: "Shed Skin"},
+	},
+	dragonite: {
+		inherit: true,
+		abilities: {0: "Multiscale"},
+	},
+	tynamo: {
+		inherit: true,
+		abilities: {0: "Levitate"},
+	},
+	eelektrik: {
+		inherit: true,
+		abilities: {0: "Levitate"},
+	},
+	eelektross: {
+		inherit: true,
+		abilities: {0: "Levitate"},
+	},
+	munna: {
+		inherit: true,
+		abilities: {0: "Synchronize"},
+	},
+	musharna: {
+		inherit: true,
+		abilities: {0: "Synchronize"},
+	},
+	delibird: {
+		inherit: true,
+		abilities: {0: "Hustle"},
+	},
+	pawmi: {
+		inherit: true,
+		abilities: {0: "Static"},
+	},
+	pawmo: {
+		inherit: true,
+		abilities: {0: "Iron Fist"},
+	},
+	pawmot: {
+		inherit: true,
+		abilities: {0: "Natural Cure"},
+	},
+	skarmory: {
+		inherit: true,
+		abilities: {0: "Weak Armor"},
+	},
+	ditto: {
+		inherit: true,
+		abilities: {0: "Imposter"},
+	},
+	meowth: {
+		inherit: true,
+		abilities: {0: "Technician"},
+	},
+	persian: {
+		inherit: true,
+		abilities: {0: "Technician"},
+	},
+	meowthalola: {
+		inherit: true,
+		abilities: {0: "Pickup"},
+	},
+	persianalola: {
+		inherit: true,
+		abilities: {0: "Fur Coat"},
+	},
+	meowthgalar: {
+		inherit: true,
+		abilities: {0: "Tough Claws"},
+	},
+	perrserker: {
+		inherit: true,
+		abilities: {0: "Steely Spirit"},
+	},
+	terapagos: {
+		num: 1084,
+		abilities: {0: "Tera Shift"},
+	},
+	larvitar: {
+		inherit: true,
+    abilities: {0: "Guts"},
+	},
+	pupitar: {
+		inherit: true,
+		abilities: {0: "Shed Skin"},
+	},
+	tyranitar: {
+		inherit: true,
+		abilities: {0: "Sand Stream"},
+	},
+	orthworm: {
+		inherit: true,
+		abilities: {0: "Earth Eater"},
+	},
+	shelmet: {
+		inherit: true,
+		abilities: {0: "Shell Armor"},
+	},
+	accelgor: {
+		inherit: true,
+		abilities: {0: "Unburden"},
+	},
+	karrablast: {
+		inherit: true,
+		abilities: {0: "Shed Skin"},
+	},
+	escavalier: {
+		inherit: true,
+		abilities: {0: "Overcoat"},
+	},
+	snom: {
+		inherit: true,
+		abilities: {0: "Ice Scales"},
+	},
+	frosmoth: {
+		inherit: true,
+		abilities: {0: "Ice Scales"},
+	},
+	mimejr: {
+		inherit: true,
+		abilities: {0: "Filter"},
+	},
+	mrmime: {
+		inherit: true,
+		abilities: {0: "Technician"},
+	},
+	mrmimegalar: {
+		inherit: true,
+		abilities: {0: "Vital Spirit"},
+	},
+	mrrime: {
+		inherit: true,
+		abilities: {0: "Ice Body"},
+	},
+	bonsly: {
+		inherit: true,
+		abilities: {0: "Sturdy"},
+	},
+	sudowoodo: {
+		inherit: true,
+		abilities: {0: "Rock Head"},
+	},
+	capsakid: {
+		inherit: true,
+		abilities: {0: "Chlorophyll"},
+	},
+	scovillain: {
+		inherit: true,
+		abilities: {0: "Chlorophyll"},
+	},
+	alomomola: {
+		inherit: true,
+		abilities: {0: "Hydration"},
+	},
+	luvdisc: {
+		inherit: true,
+		abilities: {0: "Hydration"},
+	},
+	emolga: {
+		inherit: true,
+		abilities: {0: "Motor Drive"},
+	},
+	cramorant: {
+		inherit: true,
+		abilities: {0: "Gulp Missile"},
+	},
+	sandshrew: {
+		inherit: true,
+		abilities: {0: "Sand Rush"},
+	},
+	sandslash: {
+		inherit: true,
+		abilities: {0: "Sand Rush"},
+	},
+	sandshrewalola: {
+		inherit: true,
+		abilities: {0: "Slush Rush"},
+	},
+	sandslashalola: {
+		inherit: true,
+		abilities: {0: "Slush Rush"},
+	},
+	carnivine: {
+		inherit: true,
+		abilities: {0: "Levitate"},
+	},
+	yamask: {
+		inherit: true,
+		abilities: {0: "Mummy"},
+	},
+	cofagrigus: {
+		inherit: true,
+		abilities: {0: "Mummy"},
+	},
+	yamaskgalar: {
+		inherit: true,
+		abilities: {0: "Wandering Spirit"},
+	},
+	runerigus: {
+		inherit: true,
+		abilities: {0: "Wandering Spirit"},
+	},
+	paras: {
+		inherit: true,
+		abilities: {0: "Dry Skin"},
+	},
+	parasect: {
+		inherit: true,
+		abilities: {0: "Effect Spore"},
+	},
+	morelull: {
+		inherit: true,
+		abilities: {0: "Effect Spore"},
+	},
+	shiinotic: {
+		inherit: true,
+		abilities: {0: "Rain Dish"},
+	},
+	croagunk: {
+		inherit: true,
+		abilities: {0: "Dry Skin"},
+	},
+	toxicroak: {
+		inherit: true,
+		abilities: {0: "Poison Touch"},
+	},
+	chingling: {
+		inherit: true,
+		abilities: {0: "Levitate"},
+	},
+	chimecho: {
+		inherit: true,
+		abilities: {0: "Levitate"},
+	},
+	sneasel: {
+		inherit: true,
+		abilities: {0: "Inner Focus"},
+	},
+	weavile: {
+		inherit: true,
+		abilities: {0: "Pickpocket"},
+	},
+	sneaselhisui: {
+		inherit: true,
+		abilities: {0: "Keen Eye"},
+	},
+	sneasler: {
+		inherit: true,
+		abilities: {0: "Unburden"},
+	},
+
+// Wave 2
+	corphish: {
+		inherit: true,
+		abilities: {0: "Adaptability"},
+	},
+	crawdaunt: {
+		inherit: true,
+		abilities: {0: "Adaptability"},
+	},
+	cutiefly: {
+		inherit: true,
+		abilities: {0: "Shield Dust"},
+	},
+	ribombee: {
+		inherit: true,
+		abilities: {0: "Sweet Veil"},
+	},
+	trapinch: {
+		inherit: true,
+		abilities: {0: "Arena Trap"},
+	},
+	vibrava: {
+		inherit: true,
+		abilities: {0: "Levitate"},
+	},
+	flygon: {
+		inherit: true,
+		abilities: {0: "Levitate"},
+	},
+};
