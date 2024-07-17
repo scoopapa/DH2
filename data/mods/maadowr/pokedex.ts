4e1242c34612b533462df94e682b44415fb216bc
BlueRaySmogon <longnguyen93@gmx.de>
2024-07-17 17:56:17 +0200

    Update pokedex.ts

--- data/mods/maadowr/pokedex.ts
+++ data/mods/maadowr/pokedex.ts
@@ -4,8 +4,10 @@ export const Pokedex: { [k: string]: ModdedSpeciesData } = {
 		name: "Chantyrus-0",
 
 		types: ["Grass", "Flying"],
+		genderRatio: {M: 0.875, F: 0.125},
 		baseStats: {hp: 56, atk: 35, def: 42, spa: 71, spd: 42, spe: 64},
 		abilities: {0: "Overgrow", H: "Harvest"},
+		category: "Papyrus Bird",
 		heightm: 0.3,
 		weightkg: 1.8,
 		color: "Green",
@@ -19,8 +21,10 @@ export const Pokedex: { [k: string]: ModdedSpeciesData } = {
 		name: "Chantyrus-1",
 
 		types: ["Grass", "Flying"],
+		genderRatio: {M: 0.875, F: 0.125},
 		baseStats: {hp: 71, atk: 50, def: 57, spa: 96, spd: 57, spe: 89},
 		abilities: {0: "Overgrow", H: "Harvest"},
+		category: "Papyrus Bird",
 		heightm: 1.1,
 		weightkg: 30.0,
 		color: "Green",
@@ -36,8 +40,10 @@ export const Pokedex: { [k: string]: ModdedSpeciesData } = {
 		name: "Chantyrus",
 
 		types: ["Grass", "Flying"],
+		genderRatio: {M: 0.875, F: 0.125},
 		baseStats: {hp: 86, atk: 60, def: 72, spa: 126, spd: 72, spe: 114},
 		abilities: {0: "Overgrow", H: "Harvest"},
+		category: "Papyrus Bird",
 		heightm: 1.5,
 		weightkg: 39.5,
 		color: "Green",
