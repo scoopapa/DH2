cf8ce5f583a8323165cbe4a3881bf4acbe8d020b
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-07-17 10:07:23 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -2461,6 +2461,31 @@ export const Formats: FormatList = [
 		unbanlist: ['Athleetah'],
 	},
 	{
+		name: "[Gen 9] Forgottenmons",
+		desc: `A National Dex metagame featuring only Pokemon not in Gen 9.`,
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/solomods-megathread.3711007/post-10106531">Forgottenmons in the Solomods Megathread</a>`,
+		],
+		mod: 'forgottenmons',
+		teambuilderFormat: 'National Dex',
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod', 'Z-Move Clause'],
+		banlist: [
+			'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
+			'Quick Claw', 'Razor Fang', 'Assist', 'Baton Pass', 'Last Respects', 'Shed Tail',
+		],
+		onValidateTeam(team, format) {
+			/**@type {{[k: string]: true}}*/
+			let speciesTable = {};
+			let allowedTiers = ['Forgottenmons','Forgottenmons NFE','Forgottenmons LC'];
+			for (const set of team) {
+				let template = this.dex.species.get(set.species);
+				if (!allowedTiers.includes(template.tier)) {
+					return [set.species + ' is not legal in Forgottenmons.'];
+				}
+			}
+		},
+	},
+	{
 		name: "[Gen 9] Fusion Evolution Corrupt Council",
 		mod: 'fecc',
 		team: 'random',
