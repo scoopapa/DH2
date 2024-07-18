5a3b935bbf98064bdc7a9e9c74b2bcbad5bf8ad2
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2024-07-18 08:50:51 -0700

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -362,7 +362,7 @@ export const Formats: FormatList = [
 		onValidateTeam(team, format) {
 			/**@type {{[k: string]: true}}*/
 			let speciesTable = {};
-			let allowedTiers = ['CC OU', 'CC UU', 'CC Ubers'];
+			let allowedTiers = ['CC OU', 'CC UU'];
 			for (const set of team) {
 				let template = this.dex.species.get(set.species);
 				if (!allowedTiers.includes(template.tier)) {
