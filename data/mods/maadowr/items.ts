45c354bb00b08ad508a6f76990fa09141c3db78e
BlueRaySmogon <longnguyen93@gmx.de>
2024-07-18 23:20:16 +0200

    Create items.ts

---
+++ data/mods/maadowr/items.ts
@@ -0,0 +1,24 @@
+export const Items: {[k: string]: ModdedItemData} = {
+acidicseed: {
+		name: "Acidic Seed",
+		spritenum: 666,
+		fling: {
+			basePower: 10,
+		},
+		onStart(pokemon) {
+			if (!pokemon.ignoringItem() && this.field.isTerrain('acidicterrain')) {
+				pokemon.useItem();
+			}
+		},
+		onTerrainChange(pokemon) {
+			if (this.field.isTerrain('acidicterrain')) {
+				pokemon.useItem();
+			}
+		},
+		boosts: {
+			spd: 1,
+		},
+		num: -1001,
+		desc: "If the terrain is Acidic Terrain, raises holder's Sp. Def by 1 stage. Single use.",
+		},
+};
