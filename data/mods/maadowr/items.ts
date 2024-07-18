7d5896c8e8a10680f69334fc005b5f937519db58
BlueRaySmogon <longnguyen93@gmx.de>
2024-07-18 23:37:33 +0200

    Update items.ts

--- data/mods/maadowr/items.ts
+++ data/mods/maadowr/items.ts
@@ -20,5 +20,22 @@ acidicseed: {
 		},
 		num: -1001,
 		desc: "If the terrain is Acidic Terrain, raises holder's Sp. Def by 1 stage. Single use.",
+	},
+	// end
+
+	// start
+	ancientarmor: {
+		name: "Ancient Armor",
+		spritenum: 581,
+		fling: {
+			basePower: 80,
 		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (target.getMoveHitData(move).typeMod > 0) {
+				this.debug('Ancient Armor neutralize');
+				return this.chainModify(0.75);
+			},
+		num: -1002,
+		desc: "User receives 25% less damage from a super effective move.",
+	},
 };
