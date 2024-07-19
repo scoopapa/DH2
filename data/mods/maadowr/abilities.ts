f97140086ff31998a20e691ec188ff7c9b73a2cb
BlueRaySmogon <longnguyen93@gmx.de>
2024-07-19 23:57:32 +0200

    Update abilities.ts

--- data/mods/maadowr/abilities.ts
+++ data/mods/maadowr/abilities.ts
@@ -1,2 +1,95 @@
 export const Abilities: { [abilityid: string]: ModdedAbilityData } = {
-  
+  absorption: {
+		onModifyDefPriority: 6,
+		onModifyDef(pokemon) {
+			if (this.field.isTerrain('grassyterrain') || this.field.isTerrain('electricterrain')) return this.chainModify(1.5);
+		},
+	   onModifySpDPriority: 6,
+		onModifySpD(pokemon) {
+			if (this.field.isTerrain('acidicterrain') || this.field.isTerrain('mistyterrain') || this.field.isTerrain('psychicterrain')) return this.chainModify(1.5);
+		},
+		flags: {breakable: 1},
+		name: "Absorption",
+		rating: 2,
+		num: -1,
+	},
+	// end
+
+	// start
+	acidicsurge: {
+		desc: "On switch-in, this Pokémon summons Acidic Terrain for 5 turns. During the effect, the power of Poison-type attacks made by grounded Pokémon is multiplied by 1.3, and grounded Steel-types are not immune to Poison-type damage. Steel-type Pokémon are still immune to being poisoned and badly poisoned, except by Pokémon with Corrosion. Camouflage transforms the user into a Poison-type, Nature Power becomes Sludge Bomb, and Secret Power has a 30% chance to cause poison. Lasts for 8 turns if the user is holding a Terrain Extender (such as through Skill Swap).",
+		shortDesc: "5 turns. Grounded: +Poison power, Steel not immune to Poison type.",
+		onStart(source) {
+			this.field.setTerrain('acidicterrain');
+		},
+		flags: {},
+		name: "Acidic Surge",
+		rating: 4,
+		num: -2,
+	},
+	// end
+
+	// start
+	mimicry: {
+		onStart(pokemon) {
+			this.singleEvent('TerrainChange', this.effect, this.effectState, pokemon);
+		},
+		onTerrainChange(pokemon) {
+			let types;
+			switch (this.field.terrain) {
+			case 'electricterrain':
+				types = ['Electric'];
+				break;
+			case 'grassyterrain':
+				types = ['Grass'];
+				break;
+			case 'mistyterrain':
+				types = ['Fairy'];
+				break;
+			case 'psychicterrain':
+				types = ['Psychic'];
+				break;
+			case 'acidicterrain':
+				types = ['Poison'];
+				break;		
+			default:
+				types = pokemon.baseSpecies.types;
+			}
+			const oldTypes = pokemon.getTypes();
+			if (oldTypes.join() === types.join() || !pokemon.setType(types)) return;
+			if (this.field.terrain || pokemon.transformed) {
+				this.add('-start', pokemon, 'typechange', types.join('/'), '[from] ability: Mimicry');
+				if (!this.field.terrain) this.hint("Transform Mimicry changes you to your original un-transformed types.");
+			} else {
+				this.add('-activate', pokemon, 'ability: Mimicry');
+				this.add('-end', pokemon, 'typechange', '[silent]');
+			}
+		},
+		flags: {},
+		name: "Mimicry",
+		rating: 0,
+		num: 250,
+	},
+	// end
+
+	// start
+	agitation: {
+		desc: "When this Pokémon raises or lowers another Pokémon's stat stages, the effect is increased by one stage for each affected stat.",
+		shortDesc: "Increases stat stage changes the Pokémon inflicts by 1 stage.",
+		onAnyBoost(boost, target, source, effect) {
+			if (effect && effect.id === 'zpower') return;
+			if (!target || !source || target === source || source !== this.effectState.target) return; // doesn't work on itself
+			let i: BoostName;
+			for (i in boost) {
+				if (boost[i]! < 0) boost[i]! -= 1; // exacerbate debuffs
+				if (boost[i]! > 0) boost[i]! += 1; // augment buffs
+			}
+		},
+		name: "Agitation",
+		rating: 4,
+		num: -3,
+	},
+	// end
+
+	// start
+};
