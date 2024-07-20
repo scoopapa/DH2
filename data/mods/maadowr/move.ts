79b77b2d0a54b309bb3a216691cfe92bfdd85592
BlueRaySmogon <longnguyen93@gmx.de>
2024-07-21 01:32:37 +0200

    Update move.ts

--- data/mods/maadowr/move.ts
+++ data/mods/maadowr/move.ts
@@ -4,6 +4,7 @@ export const Moves: { [moveid: string]: ModdedMoveData } = {
     accuracy: 100,
 		basePower: 100,
 		category: "Special",
+	   shortDesc: "All grounded Grass Pokémon on the field have their Atk and SpA raised by 1 stage.",
 		name: "Aloe Vera",
 		pp: 5,
 		priority: 0,
@@ -53,9 +54,9 @@ export const Moves: { [moveid: string]: ModdedMoveData } = {
         this.boost({def: -1}, source, target, this.dex.getActiveMove("Clive Jump"));
 			}
 		},
-		},
-		target: "normal",
-		type: "Rock",
+	 },
+	 target: "normal",
+	 type: "Rock",
     contestType: "Cool",
 	},
   // end
@@ -66,6 +67,7 @@ export const Moves: { [moveid: string]: ModdedMoveData } = {
 		accuracy: 100,
 		basePower: 100,
 		category: "Physical",
+	   shortDesc: "This move hits in two turns and sets Snow.",
 		name: "Cold Rush",
 		pp: 10,
 		priority: 0,
@@ -97,9 +99,137 @@ export const Moves: { [moveid: string]: ModdedMoveData } = {
 		secondary: null,
 		target: "normal",
 		type: "Ice",
-		contestType: "Clever",
+		contestType: "Cool",
 	},
   // end
 
   // start
+  // colourmegone to be coded
+  // end
+
+  // start
+  cuttingedge: {
+		num: -5,
+		accuracy: 100,
+		basePower: 85,
+		category: "Physical",
+	   shortDesc: "This move does 50% more damage in Grassy Terrain.",
+		name: "Cutting Edge",
+		pp: 15,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1, metronome: 1, slicing: 1},
+		secondary: null,
+		onBasePower(basePower, source) {
+			if (this.field.isTerrain('grassyterrain')) {
+				this.debug('cuttingedge grassy terrain boost');
+				return this.chainModify(1.5);
+			}
+		},
+		target: "normal",
+		type: "Ground",
+	   contestType: "Cool",
+	},
+	// end
+
+	// start
+   dispersion: {
+		num: -6,
+		accuracy: 100,
+		basePower: 90,
+		category: "Special",
+		shortDesc: "Type varies based on the user's primary type. Hits foes.",
+		name: "Dispersion",
+		pp: 15,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, dance: 1},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Silver Wind", target);
+		},
+		onModifyType(move, pokemon) {
+			let type = pokemon.types[0];
+			if (type === "Bird") type = "???";
+			move.type = type;
+		},
+		secondary: null,
+		target: "allAdjacentFoes",
+		type: "Normal",
+		contestType: "Beautiful",
+	},
+	// end
+
+	// start
+	enragedassault: {
+		num: -7,
+		accuracy: 100,
+		basePower: 50,
+		basePowerCallback(pokemon) {
+			return Math.min(350, 50 + 50 * pokemon.timesAttacked);
+		},
+		category: "Physical",
+		shortDesc: "Works like Rage Fist but isn't a punch move.",
+		name: "Enraged Assault",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		secondary: null,
+		target: "normal",
+		type: "Bug",
+		contestType: "Cool",
+	},
+	// end
+
+	// start
+	entanglement: {
+		num: -8,
+		accuracy: 100,
+		basePower: 80,
+		category: "Special",
+		shortDesc: "Sets Sticky Web in Electric Terrain.",
+		name: "Entanglement",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, metronome: 1},
+		onAfterHit(target, source, move) {
+			if (!move.hasSheerForce && source.hp && (this.field.isTerrain('electricterrain'))) {
+				for (const side of source.side.foeSidesWithConditions()) {
+					side.addSideCondition('stickyweb');
+				}
+			}
+		},
+		onAfterSubDamage(damage, target, source, move) {
+			if (!move.hasSheerForce && source.hp && (this.field.isTerrain('electricterrain'))) {
+				for (const side of source.side.foeSidesWithConditions()) {
+					side.addSideCondition('stickyweb');
+				}
+			}
+		},
+		secondary: {}, // Sheer Force-boosted
+		target: "normal",
+		type: "Bug",
+		contestType: "Cool",
+	},
+	// end
+
+	// start
+	enzymaticbite: {
+		num: -9,
+		accuracy: 100,
+		basePower: 85,
+		category: "Physical",
+		shortDesc: "Recovers half of the damage done to the target, 2/3 in Psychic Terrain.",
+		name: "Enzymatic Bite",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1, heal: 1, metronome: 1, bite: 1},
+		drain: [1, 2] if (!this.field.isTerrain('psychicterrain')),
+		drain: [2, 3] if (this.field.isTerrain('psychicterrain')),
+		secondary: null,
+		target: "normal",
+		type: "Poison",
+		contestType: "Clever",
+	},
+	// end
+
+	// start
 };
