405f1f1efc8ff0babe40e02336613baa4b07595f
BlueRaySmogon <longnguyen93@gmx.de>
2024-07-21 12:42:08 +0200

    Update move.ts

--- data/mods/maadowr/move.ts
+++ data/mods/maadowr/move.ts
@@ -320,7 +320,7 @@ export const Moves: { [moveid: string]: ModdedMoveData } = {
 		name: "Honey Dew",
 		pp: 5,
 		priority: 0,
-		flags: {snatch: 1, heal: 1, metronome: 1},
+		flags: {snatch: 1, heal: 1, bypasssub: 1, metronome: 1},
 		onHit(pokemon) {
 			const success = !!this.heal(this.modify(pokemon.maxhp, 0.25));
 			return boost({atk: 1, spa: 1}) if (pokemon.hasType('Bug')); || success;
@@ -437,7 +437,7 @@ export const Moves: { [moveid: string]: ModdedMoveData } = {
 	// end
 
 	// start
-	prismaticlaser: {
+	lunarstorm: {
 		num: -18,
 		accuracy: 90,
 		basePower: 120,
@@ -594,4 +594,349 @@ export const Moves: { [moveid: string]: ModdedMoveData } = {
 		contestType: "Clever",
 	},
 	// end
+
+	// start
+	sunbathing: {
+		num: -26,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Sun Bathing",
+		pp: 5,
+		priority: 0,
+		flags: {snatch: 1, heal: 1, bypasssub: 1, metronome: 1},
+		heal: [1, 4],
+		volatileStatus: 'clearnegativeboost',
+		secondary: null,
+		target: "allies",
+		type: "Fire",
+		contestType: "Beautiful",
+	},
+	// end
+
+	// start
+	terraform: {
+    num: -27,
+    accuracy: 100,
+		basePower: 100,
+		category: "Physical",
+	   shortDesc: "All grounded Ground Pokémon on the field have their Def and SpD raised by 1 stage.",
+		name: "Terraform",
+		pp: 5,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, metronome: 1},
+    onHitField(target, source) {
+			const targets: Pokemon[] = [];
+			let anyAirborne = false;
+			for (const pokemon of this.getAllActive()) {
+				if (!pokemon.runImmunity('Ground')) {
+					this.add('-immune', pokemon);
+					anyAirborne = true;
+					continue;
+				}
+				if (pokemon.hasType('Ground')) {
+					// This move affects every grounded Ground-type Pokemon in play.
+					targets.push(pokemon);
+				}
+			}
+			if (!targets.length && !anyAirborne) return false; // Fails when there are no grounded Ground types or airborne Pokemon
+			for (const pokemon of targets) {
+				this.boost({def: 1, spd: 1}, pokemon, source);
+			}
+		},
+		secondary: null,
+		target: "normal",
+		type: "Ground",
+    contestType: "Tough",
+	},
+	// end
+
+	// start
+	thunderousroar: {
+		num: -28,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Thunderous Roar",
+		pp: 10,
+		priority: 0,
+		flags: {sound: 1},
+		// TODO show prepare message before the "POKEMON used MOVE!" message
+		// This happens even before sleep shows its "POKEMON is fast asleep." message
+		terrain: 'electricterrain',
+		selfSwitch: true,
+		secondary: null,
+		target: "all",
+		type: "Electric",
+	},
+	// end
+
+	// start
+	timecompressor: {
+		num: -29,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		shortDesc: "Sets Trick Room two turns later.",
+		name: "Time Compressor",
+		pp: 5,
+		priority: 0,
+		flags: {metronome: 1, futuremove: 1},
+		pseudoWeather: "Trick Room",
+		secondary: null,
+		target: "all",
+		type: "Psychic",
+		contestType: "Clever",
+	},
+	// end
+
+	// start
+	voltomator: {
+		num: -30,
+		accuracy: 100,
+		basePower: 20,
+		basePowerCallback(pokemon, target, move) {
+			const bp = move.basePower + 20 * pokemon.positiveBoosts();
+			this.debug('BP: ' + bp);
+			return bp;
+		},
+		category: "Physical",
+		name: "Voltomator",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, metronome: 1},
+		secondary: null,
+		target: "normal",
+		type: "Electric",
+		contestType: "Clever",
+	},
+	// end
+
+	// start
+	wondergleam: {
+		num: -31,
+		accuracy: 100,
+		basePower: 70,
+		category: "Special",
+		name: "Wonder Gleam",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, metronome: 1},
+		onEffectiveness(typeMod, target, type) {
+			if (type === {'Psychic', 'Steel', 'Dark'}) return 1;
+			if (type === {'Poison', 'Fighting'}) return 2;
+		},
+		secondary: null,
+		target: "normal",
+		type: "Psychic",
+		contestType: "Clever",
+	},
+	// end
+
+   // start
+   acidicterrain: {
+		num: -32,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Acidic Terrain",
+		pp: 10,
+		priority: 0,
+		flags: {nonsky: 1, metronome: 1},
+		terrain: 'acidicterrain',
+		condition: {
+			duration: 5,
+		durationCallback(source, effect) {
+			if (source?.hasItem('terrainextender')) {
+				return 8;
+			}
+			return 5;
+		},
+		onBasePowerPriority: 6,
+		onBasePower(basePower, attacker, defender, move) {
+			if (this.getAllActive().some(x => x.hasAbility('downtoearth'))) return;
+			if (move.type === 'Poison' && attacker.isGrounded() && !attacker.isSemiInvulnerable()) {
+				this.debug('acidic terrain boost');
+				return this.chainModify([5325, 4096]);
+			}
+		},
+		onModifyMovePriority: -5,
+		onModifyMove(move, source, target) {
+			if (this.getAllActive().some(x => x.hasAbility('downtoearth'))) return;
+			if (!move.ignoreImmunity) move.ignoreImmunity = {};
+			if (move.ignoreImmunity !== true) {
+				move.ignoreImmunity['Poison'] = true;
+			}
+		},
+		onTryHit(target, source, move) {
+			if (move.type === 'Poison') {
+				if ((!target.isGrounded() || target.isSemiInvulnerable()) && !this.dex.getImmunity('Poison', target)) {
+					this.add('-immune', target);
+					this.hint(`Only targets that are affected by terrain lose their immunity to Poison.`);
+					return null;
+				}
+			}
+		},
+		onFieldStart(field, source, effect) {
+			if (effect?.effectType === 'Ability') {
+				this.add('-fieldstart', 'move: Acidic Terrain', '[from] ability: ' + effect.name, '[of] ' + source);
+				this.add('-message', "Poison-type moves used by grounded Pokémon will have their power increased.");
+				this.add('-message', "Grounded Steel-type Pokémon will also lose their immunity to Poison-type moves.");
+			} else {
+				this.add('-fieldstart', 'move: Acidic Terrain');
+			}
+		},
+		onFieldResidualOrder: 27,
+		onFieldResidualSubOrder: 7,
+		onFieldEnd() {
+			this.add('-fieldend', 'move: Acidic Terrain');
+		},
+ 	},
+		secondary: null,
+		target: "all",
+		type: "Poison",
+		contestType: "Clever",
+	},
+   // end
+
+	// start
+	oilspill: {
+		num: -33,
+		accuracy: 90,
+		basePower: 100,
+		category: "Special",
+		name: "Oil Spill",
+		pp: 10,
+		flags: {protect: 1, mirror: 1, metronome: 1},
+		onEffectiveness(typeMod, target, type, move) {
+			return typeMod + this.dex.getEffectiveness('Water', type);
+		},
+		priority: 0,
+		secondary: {
+			chance: 100,
+			status: 'psn' if (this.field.isTerrain('acidicterrain')),
+		},
+		target: "allAdjacentFoes",
+		type: "Poison",
+		contestType: "Tough",
+	},
+	// end
+
+	// start
+	camouflage: {
+		inherit: true,
+		onHit(target) {
+			let newType = 'Normal';
+			if (this.field.isTerrain('electricterrain')) {
+				newType = 'Electric';
+			} else if (this.field.isTerrain('grassyterrain')) {
+				newType = 'Grass';
+			} else if (this.field.isTerrain('mistyterrain')) {
+				newType = 'Fairy';
+			} else if (this.field.isTerrain('psychicterrain')) {
+				newType = 'Psychic';
+			} else if (this.field.isTerrain('acidicterrain')) {
+				newType = 'Poison';
+			}
+
+			if (target.getTypes().join() === newType || !target.setType(newType)) return false;
+			this.add('-start', target, 'typechange', newType);
+		},
+	},
+	naturepower: {
+		inherit: true,
+		onTryHit(target, pokemon) {
+			let move = 'triattack';
+			if (this.field.isTerrain('electricterrain')) {
+				move = 'thunderbolt';
+			} else if (this.field.isTerrain('grassyterrain')) {
+				move = 'energyball';
+			} else if (this.field.isTerrain('mistyterrain')) {
+				move = 'moonblast';
+			} else if (this.field.isTerrain('psychicterrain')) {
+				move = 'psychic';
+			} else if (this.field.isTerrain('acidicterrain')) {
+				move = 'sludgebomb';
+			}
+			this.actions.useMove(move, pokemon, target);
+			return null;
+		},
+	},
+	secretpower: {
+		inherit: true,
+		onModifyMove(move, pokemon) {
+			if (this.field.isTerrain('')) return;
+			move.secondaries = [];
+			if (this.field.isTerrain('electricterrain')) {
+				move.secondaries.push({
+					chance: 30,
+					status: 'par',
+				});
+			} else if (this.field.isTerrain('grassyterrain')) {
+				move.secondaries.push({
+					chance: 30,
+					status: 'slp',
+				});
+			} else if (this.field.isTerrain('mistyterrain')) {
+				move.secondaries.push({
+					chance: 30,
+					boosts: {
+						spa: -1,
+					},
+				});
+			} else if (this.field.isTerrain('psychicterrain')) {
+				move.secondaries.push({
+					chance: 30,
+					boosts: {
+						spe: -1,
+					},
+				});
+			} else if (this.field.isTerrain('acidicterrain')) {
+				move.secondaries.push({
+					chance: 30,
+					status: 'psn',
+				});
+			}
+		},
+	},
+	terrainpulse: {
+		num: 805,
+		accuracy: 100,
+		basePower: 50,
+		category: "Special",
+		name: "Terrain Pulse",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, pulse: 1},
+		onModifyType(move, pokemon) {
+			if (!pokemon.isGrounded() || this.field.isTerrain('')) return;
+			switch (this.field.terrain) {
+			case 'electricterrain':
+				move.type = 'Electric';
+				break;
+			case 'grassyterrain':
+				move.type = 'Grass';
+				break;
+			case 'mistyterrain':
+				move.type = 'Fairy';
+				break;
+			case 'psychicterrain':
+				move.type = 'Psychic';
+				break;
+			case 'acidicterrain':
+				move.type = 'Poison';
+				break;
+			}
+		},
+		onModifyMove(move, pokemon) {
+			if (this.field.isTerrain('')) return; // Down-to-Earth
+			if (this.field.terrain && pokemon.isGrounded()) {
+				move.basePower *= 2;
+				this.debug('BP doubled in Terrain');
+			}
+		},
+	},
+	// end
+
+	// start
 };
