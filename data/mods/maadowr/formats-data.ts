689ccb0650f829bd7030fa31e92495254fdc9ed8
BlueRaySmogon <longnguyen93@gmx.de>
2024-07-21 13:45:33 +0200

    Create formats-data.ts

---
+++ data/mods/maadowr/formats-data.ts
@@ -0,0 +1,169 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	exeggcute: {
+		tier: "MD NFE",
+		doublesTier: "MD FE",
+	},
+  exeggutor: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  sunkern: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  sunflora: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  cacnea: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  cacturne: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  snover: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  abomasnow: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  leafeon: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  ferroseed: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  ferrothorn: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  honedgemaadowr: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  doublademaadowr: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  aegislashmaadowr: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  aegislashblademaadowr: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  rolycolymaadowr: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  carkolmaadowr: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  coalossalmaadowr: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  applin: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  flapple: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  appletun: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  dipplin: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  hydrapple: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  bramblin: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  brambleghast: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  chantyrus0: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  chantyrus1: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  chantyrus: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  paradisica0: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  paradisica: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  shelldure: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  cloysture: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  immanitite: {
+		tier: "MD NFE",
+		doublesTier: "MD  NFE",
+	},
+  immanicham: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  immanicus: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  caterpie: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  metapod: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  butterfree: {
+		tier: "MD",
+		doublesTier: "M",
+	},
+  heracross: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  nincada: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+  ninjask: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+  shedinja: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
