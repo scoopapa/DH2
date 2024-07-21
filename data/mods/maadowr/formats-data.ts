ba896a3627622158f3acc8836add4423967e2b04
BlueRaySmogon <longnguyen93@gmx.de>
2024-07-21 17:20:55 +0200

    Update formats-data.ts

--- data/mods/maadowr/formats-data.ts
+++ data/mods/maadowr/formats-data.ts
@@ -167,3 +167,151 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		tier: "MD",
 		doublesTier: "MD",
 	},
+	illumise: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	volbeat: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	yanma: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	yanmega: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	larvesta: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	volcarona: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	karrablast: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	escavalier: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	shelmet: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	accelgor: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	joltik: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	galvantula: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	dwebble: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	crustle: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	sewaddle: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	swadloon: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	leavanny: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	rellor: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	rabsca: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	nymble: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	lokix: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	pinsirmaadowr: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	kabujatsu: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	paras: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	parasect: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	parascent: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	combee: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	vespiquen: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	oonabee: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	durant: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	aurulant: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+   dragantis0: {
+		tier: "MD NFE",
+		doublesTier: "MD NFE",
+	},
+	dragantis: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	goldenorbweaver: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	cropcircleartist: {
+		tier: "MD",
+		doublesTier: "MD",
+	},
+	arastinith: {
+		tier: "MD Ubers",
+		doublesTier: "MD Ubers",
+	},
+	cicadillo: {
+		tier: "MD",
+	},
+};
