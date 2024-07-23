e61d90606a737fea37858c0fd953989ad54c0427
BlueRaySmogon <longnguyen93@gmx.de>
2024-07-23 23:47:03 +0200

    Update items.ts

--- data/mods/maadowr/items.ts
+++ data/mods/maadowr/items.ts
@@ -1,4 +1,225 @@
 export const Items: {[k: string]: ModdedItemData} = {
+	cacturnite: {
+		name: "Cacturnite",
+		//spritenum: 586,
+		megaStone: "Cacturne-Mega",
+		megaEvolves: "Cacturne",
+		itemUser: ["Cacturne"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1008,
+		gen: 9,
+		desc: "If held by a Cacturne, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	dragantistite: {
+		name: "Dragantistite",
+		//spritenum: 586,
+		megaStone: "Dragantis-Mega",
+		megaEvolves: "Dragantis",
+		itemUser: ["Dragantis"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1009,
+		gen: 9,
+		desc: "If held by a Dragantis, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	pyroarite: {
+		name: "Pyroarite",
+		//spritenum: 586,
+		megaStone: "Pyroar-Mega",
+		megaEvolves: "Pyroar",
+		itemUser: ["Pyroar"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1010,
+		gen: 9,
+		desc: "If held by a Pyroar, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	cryogonalite: {
+		name: "Cryogonalite",
+		//spritenum: 586,
+		megaStone: "Cryogonal-Mega",
+		megaEvolves: "Cryogonal",
+		itemUser: ["Cryogonal"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1011,
+		gen: 9,
+		desc: "If held by a Cryogonal, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	luxrite: {
+		name: "Luxrite",
+		//spritenum: 586,
+		megaStone: "Luxray-Mega",
+		megaEvolves: "Luxray",
+		itemUser: ["Luxray"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1012,
+		gen: 9,
+		desc: "If held by a Luxray, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	stormulexite: {
+		name: "Stormulexite",
+		//spritenum: 586,
+		megaStone: "Stormulex-Mega",
+		megaEvolves: "Stormulex",
+		itemUser: ["Stormulex"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1013,
+		gen: 9,
+		desc: "If held by a Stormulex, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	drifblimite: {
+		name: "Drifblimite",
+		//spritenum: 586,
+		megaStone: "Drifblim-Mega",
+		megaEvolves: "Drifblim",
+		itemUser: ["Drifblim"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1014,
+		gen: 9,
+		desc: "If held by a Drifblim, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	cofagrigusite: {
+		name: "Cofagrigusite",
+		//spritenum: 586,
+		megaStone: "Cofagrigus-Mega",
+		megaEvolves: "Cofagrigus",
+		itemUser: ["Cofagrigus"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1015,
+		gen: 9,
+		desc: "If held by a Cofagrigus, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	gigalithite: {
+		name: "Gigalithite",
+		//spritenum: 586,
+		megaStone: "Gigalith-Mega",
+		megaEvolves: "Gigalith",
+		itemUser: ["Gigalith"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1016,
+		gen: 9,
+		desc: "If held by a Gigalith, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	pasturite: {
+		name: "Pasturite",
+		//spritenum: 586,
+		megaStone: "Pastura-Mega",
+		megaEvolves: "Pastura",
+		itemUser: ["Pastura"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1017,
+		gen: 9,
+		desc: "If held by a Pastura, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	crobatite: {
+		name: "Crobatite",
+		//spritenum: 586,
+		megaStone: "Crobat-Mega",
+		megaEvolves: "Crobat",
+		itemUser: ["Crobat"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1019,
+		gen: 9,
+		desc: "If held by a Crobat, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	drapionite: {
+		name: "Drapionite",
+		//spritenum: 586,
+		megaStone: "Drapion-Mega",
+		megaEvolves: "Drapion",
+		itemUser: ["Drapion"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1020,
+		gen: 9,
+		desc: "If held by a Drapion, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
+	dragalgite: {
+		name: "Dragalgite",
+		//spritenum: 586,
+		megaStone: "Dragalge-Mega",
+		megaEvolves: "Dragalge",
+		itemUser: ["Dragalge"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: -1021,
+		gen: 9,
+		desc: "If held by a Dragalge, this item allows it to Mega Evolve in battle.",
+	},
+	// end
+
+	// start
 	acidicseed: {
 		name: "Acidic Seed",
 		spritenum: 666,
