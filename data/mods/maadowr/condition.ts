36c258f4c0bb4e7e0fa6abeca8543b0061be82ea
BlueRaySmogon <longnguyen93@gmx.de>
2024-07-25 00:56:17 +0200

    Update condition.ts

--- data/mods/maadowr/condition.ts
+++ data/mods/maadowr/condition.ts
@@ -42,60 +42,7 @@ export const Conditions: {[k: string]: ConditionData} = {
 		},
 	},
   // end
-
-  // start
-  sunnyday: {
-		name: 'SunnyDay',
-		effectType: 'Weather',
-		duration: 5,
-		durationCallback(source, effect) {
-			if (source?.hasItem('heatrock')) {
-				return 8;
-			}
-			return 5;
-		},
-		onWeatherModifyDamage(damage, attacker, defender, move) {
-			if (move.id === 'hydrosteam' && !attacker.hasItem('utilityumbrella')) {
-				this.debug('Sunny Day Hydro Steam boost');
-				return this.chainModify(1.5);
-			}
-      if (move.id === 'incandescentflame' && !attacker.hasItem('utilityumbrella')) {
-				this.debug('Sunny Day Incandescent Flame boost');
-				return this.chainModify(1.5);
-			}
-			if (defender.hasItem('utilityumbrella')) return;
-			if (move.type === 'Fire') {
-				this.debug('Sunny Day fire boost');
-				return this.chainModify(1.5);
-			}
-			if (move.type === 'Water') {
-				this.debug('Sunny Day water suppress');
-				return this.chainModify(0.5);
-			}
-		},
-		onFieldStart(battle, source, effect) {
-			if (effect?.effectType === 'Ability') {
-				if (this.gen <= 5) this.effectState.duration = 0;
-				this.add('-weather', 'SunnyDay', '[from] ability: ' + effect.name, '[of] ' + source);
-			} else {
-				this.add('-weather', 'SunnyDay');
-			}
-		},
-		onImmunity(type, pokemon) {
-			if (pokemon.hasItem('utilityumbrella')) return;
-			if (type === 'frz') return false;
-		},
-		onFieldResidualOrder: 1,
-		onFieldResidual() {
-			this.add('-weather', 'SunnyDay', '[upkeep]');
-			this.eachEvent('Weather');
-		},
-		onFieldEnd() {
-			this.add('-weather', 'none');
-		},
-	},
-  // end
-
+	
   // start
   psn: {
 		name: 'psn',
