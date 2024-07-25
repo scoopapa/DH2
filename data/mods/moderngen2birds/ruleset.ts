6bc8ad23002cf127a7cc18e15e244a08ec1f6f91
kpscannell <kpscannell@gmail.com>
2024-07-25 17:17:25 -0600

    Update ruleset.ts

--- data/mods/moderngen2birds/ruleset.ts
+++ data/mods/moderngen2birds/ruleset.ts
@@ -15,13 +15,204 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 		inherit: true,
 		tier: "OU",
 	},
-	Squakabilly: {
+	Zapdos: {
 		inherit: true,
 		tier: "OU",
 	},
-	Squakabilly: {
+	Fezandipiti: {
+		inherit: true,
+		tier: "OU",
+	},
+		Altaria: {
+		inherit: true,
+		tier: "OU",
+	},
+		Altariamega: {
+		inherit: true,
+		tier: "OU",
+	},
+		Archeops: {
+		inherit: true,
+		tier: "OU",
+	},
+		Articuno: {
+		inherit: true,
+		tier: "OU",
+	},
+		Charizardmegax: {
+		inherit: true,
+		tier: "OU",
+	},
+		Charizardmegay: {
+		inherit: true,
+		tier: "OU",
+	},
+		Charizard: {
+		inherit: true,
+		tier: "OU",
+	},
+		Empoleon: {
+		inherit: true,
+		tier: "OU",
+	},
+		Pelipper: {
+		inherit: true,
+		tier: "OU",
+	},
+		Tornadustherian: {
+		inherit: true,
+		tier: "OU",
+	},
+		articunogalar: {
+		inherit: true,
+		tier: "OU",
+	},
+		corviknight: {
+		inherit: true,
+		tier: "OU",
+	},
+		decidueye: {
+		inherit: true,
+		tier: "OU",
+	},
+		pidgeotmega: {
+		inherit: true,
+		tier: "OU",
+	},
+		pidgeot: {
+		inherit: true,
+		tier: "OU",
+	},
+		sirfetchd: {
+		inherit: true,
+		tier: "OU",
+	},
+		skarmory: {
+		inherit: true,
+		tier: "OU",
+	},
+		talonflame: {
+		inherit: true,
+		tier: "OU",
+	},
+		togekiss: {
+		inherit: true,
+		tier: "OU",
+	},
+		eiscue: {
+		inherit: true,
+		tier: "OU",
+	},
+		hawlucha: {
+		inherit: true,
+		tier: "OU",
+	},
+		Kilowattrel: {
+		inherit: true,
+		tier: "OU",
+	},
+		Moltres: {
+		inherit: true,
+		tier: "OU",
+	},
+		Moltresgalar: {
+		inherit: true,
+		tier: "OU",
+	},
+		Staraptor: {
+		inherit: true,
+		tier: "OU",
+	},
+		Zapdosgalar: {
+		inherit: true,
+		tier: "OU",
+	},
+		Decidueyehisui: {
+		inherit: true,
+		tier: "OU",
+	},
+		Braviary: {
+		inherit: true,
+		tier: "OU",
+	},
+		Braviaryhisui: {
+		inherit: true,
+		tier: "OU",
+	},
+		Dodrio: {
+		inherit: true,
+		tier: "OU",
+	},
+		Fearow: {
+		inherit: true,
+		tier: "OU",
+	},
+		Flamigo: {
+		inherit: true,
+		tier: "OU",
+	},
+		Honchrow: {
+		inherit: true,
+		tier: "OU",
+	},
+		Mandibuzz: {
+		inherit: true,
+		tier: "OU",
+	},
+		Oricoro: {
+		inherit: true,
+		tier: "OU",
+	},
+		Oricoropompom: {
+		inherit: true,
+		tier: "OU",
+	},
+		Oricorosensu: {
+		inherit: true,
+		tier: "OU",
+	},
+		Oricoropau: {
+		inherit: true,
+		tier: "OU",
+	},
+		Swellow: {
+		inherit: true,
+		tier: "OU",
+	},
+		Toucannon: {
+		inherit: true,
+		tier: "OU",
+	},
+		Xatu: {
+		inherit: true,
+		tier: "OU",
+	},
+		Bombirdier: {
+		inherit: true,
+		tier: "OU",
+	},
+		Chatot: {
+		inherit: true,
+		tier: "OU",
+	},
+		Delibird: {
+		inherit: true,
+		tier: "OU",
+	},
+		Farfetchd: {
+		inherit: true,
+		tier: "OU",
+	},
+		Noctowl: {
+		inherit: true,
+		tier: "OU",
+	},
+		Swanna: {
+		inherit: true,
+		tier: "OU",
+	},
+		Unfezant: {
 		inherit: true,
 		tier: "OU",
 	},
 };
-Squawkabilly", "Squawkabiluck", "Squawkalone", "Squawkabilly-Yellow", “Zapdos”, “Fezandipiti”, “Altaria”, “Altaria-Mega”, “Archeops”, “Articuno”, “Charizard-Mega-X”, “Charizard-Mega-Y”, “Charizard”, “Empoleon”, “Pelipper”, “Tornadus-Therian”, “Articuno-Galar”, “Corviknight”, “Decidueye”, “Pidgeot-Mega”, “Fezandipiti”, “Sirfetch’d”, “Skarmory”, “Talonflame”, “Togekiss”, “Eiscue”, “Hawlucha”, “Kilowattrel”, “Moltres”, “Moltres-Galar”, “Staraptor”, “Zapdos-Galar”, “Decidueye-Hisui”, “Braviary”, “Braviary-Hisui”, “Dodrio”, “Fearow”, “Flamingo”, “Honchrow”, “Mandibuzz”, “Oricoro”, “Oricoro-Pom-Pom”, “Oricoro-Sensu”, “Oricoro-Pa’u”, “Swellow”, “Toucannon”, “Xatu”, “Bombirdier”, “Chatot”, “Delibird”, “Farfetch’d”, “Noctowl”, “Swanna”, “Unfezant”
