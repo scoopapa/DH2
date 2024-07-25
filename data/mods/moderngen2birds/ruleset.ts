40bbc60d70f8aaf1b500631e742903a12f5e7404
kpscannell <kpscannell@gmail.com>
2024-07-25 17:11:12 -0600

    Update ruleset.ts

--- data/mods/moderngen2birds/ruleset.ts
+++ data/mods/moderngen2birds/ruleset.ts
@@ -1,1 +1,27 @@
 
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	Squawkabilly: {
+		inherit: true,
+		tier: "OU",
+	},
+	Squawkabiluck: {
+		inherit: true,
+		tier: "OU",
+	Squawkalone: {
+		inherit: true,
+		tier: "OU",
+	},
+	SquawkabillyYellow: {
+		inherit: true,
+		tier: "OU",
+	},
+	Squakabilly: {
+		inherit: true,
+		tier: "OU",
+	},
+	Squakabilly: {
+		inherit: true,
+		tier: "OU",
+	},
+};
+Squawkabilly", "Squawkabiluck", "Squawkalone", "Squawkabilly-Yellow", “Zapdos”, “Fezandipiti”, “Altaria”, “Altaria-Mega”, “Archeops”, “Articuno”, “Charizard-Mega-X”, “Charizard-Mega-Y”, “Charizard”, “Empoleon”, “Pelipper”, “Tornadus-Therian”, “Articuno-Galar”, “Corviknight”, “Decidueye”, “Pidgeot-Mega”, “Fezandipiti”, “Sirfetch’d”, “Skarmory”, “Talonflame”, “Togekiss”, “Eiscue”, “Hawlucha”, “Kilowattrel”, “Moltres”, “Moltres-Galar”, “Staraptor”, “Zapdos-Galar”, “Decidueye-Hisui”, “Braviary”, “Braviary-Hisui”, “Dodrio”, “Fearow”, “Flamingo”, “Honchrow”, “Mandibuzz”, “Oricoro”, “Oricoro-Pom-Pom”, “Oricoro-Sensu”, “Oricoro-Pa’u”, “Swellow”, “Toucannon”, “Xatu”, “Bombirdier”, “Chatot”, “Delibird”, “Farfetch’d”, “Noctowl”, “Swanna”, “Unfezant”
