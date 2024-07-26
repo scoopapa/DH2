9e068eaa155626d9efd057819e895b6026a51d51
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-07-26 09:57:31 -0400

    Create scripts.ts

---
+++ data/mods/moderngen2birds/scripts.ts
@@ -0,0 +1,10 @@
+export const Scripts: ModdedBattleScriptsData = {
+	inherit: 'moderngen3',
+	gen: 3,
+	teambuilderConfig: {
+		// for micrometas to only show custom tiers
+		excludeStandardTiers: true,
+		// only to specify the order of custom tiers
+		customTiers: ['Birds', 'Birds Uber'],
+	},
+};
