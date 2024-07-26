b745031b6ae7c734fa6e9a8bd1738de1e02ac74d
KirbyRider1337 <39121695+KirbyRider1337@users.noreply.github.com>
2024-07-26 14:21:34 -0600

    Update scripts.ts

--- data/mods/genxbrunica/scripts.ts
+++ data/mods/genxbrunica/scripts.ts
@@ -74,6 +74,11 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 			//Toxic distribution is reduced among non-Poisons (the ones that returned for Desvega but not SV that kept it will have it returned)
 			if (!mon.types.includes('Poison') && learnset.toxic) delete learnset.toxic;
 			//Now we free all the moves
+			//(first one replaces hail with snowscape)
+			if (learnset.hail) {
+				delete learnset.hail;
+				learnset.snowscape = [];
+			}
 			for (const move in learnset) {
 				learnset[move].push("9L1");
 			}
