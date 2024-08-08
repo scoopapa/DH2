ca7795243cb7b43edb685ddfcf387e6bc68b941e
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-08-08 19:30:31 -0400

    Update conditions.ts

--- data/mods/maxmeter/conditions.ts
+++ data/mods/maxmeter/conditions.ts
@@ -8,7 +8,7 @@ export const Conditions: {[k: string]: ConditionData} = {
 			pokemon.side.addSideCondition('dynamaxused', pokemon);
 			this.add('-message', `This Pokemon has now entered Max Mode!`);
 			this.add('-message', `In Max Mode, all of this Pokemon's stats (except HP & Speed) are increased by 1.5x!`);
-			this.add('-message', `Additionally, this Pokemon is now immune to the negative effects of status conditions and can't be forced out (unless it's holding an Eject Button)!`);
+			this.add('-message', `Additionally, this Pokemon is now immune to the negative effects of Burn, Paralysis, Freeze, Poison, and Confusion!`);
 			this.add('-message', `Max Mode will last for the next 3 turns and can't be entered again!`);
 			this.add('-start', pokemon, 'Dynamax', pokemon.gigantamax ? 'Gmax' : '');
 		},
@@ -16,6 +16,12 @@ export const Conditions: {[k: string]: ConditionData} = {
 		onBeforeSwitchOut(pokemon) {
 			pokemon.removeVolatile('dynamax');
 			pokemon.side.removeSideCondition('maxmeter7');
+		},
+		onDamagePriority: 1,
+		onDamage(damage, target, source, effect) {
+			if (effect.id === 'psn' || effect.id === 'tox' || effect.id === 'brn') {
+				return false;
+			}
 		},
 		onSourceModifyDamage(damage, source, target, move) {
 			if (move.id === 'behemothbash' || move.id === 'behemothblade' || move.id === 'dynamaxcannon') {
@@ -77,11 +83,15 @@ export const Conditions: {[k: string]: ConditionData} = {
 				pokemon.cureStatus();
 				return;
 			}
-			this.damage(pokemon.baseMaxhp / 8);
+			if (!pokemon.volatiles['dynamax']) {
+				this.damage(pokemon.baseMaxhp / 8);
+			}
 		},
 		onSourceModifyDamage(damage, source, target, move) {
-			this.debug('Freeze extra damage');
-			return this.chainModify([5325, 4096]);
+			if (!target.volatiles['dynamax']) {
+				this.debug('Freeze extra damage');
+				return this.chainModify([5325, 4096]);
+			}
 		},
 		onModifyMove(move, pokemon) {
 			if (move.flags['defrost']) {
@@ -98,6 +108,68 @@ export const Conditions: {[k: string]: ConditionData} = {
 			if (move.type === 'Fire' && move.category !== 'Status') {
 				target.cureStatus();
 			}
+		},
+	},
+	par: {
+		name: 'par',
+		effectType: 'Status',
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'par', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+			} else {
+				this.add('-status', target, 'par');
+			}
+		},
+		onModifySpe(spe, pokemon) {
+			// Paralysis occurs after all other Speed modifiers, so evaluate all modifiers up to this point first
+			spe = this.finalModify(spe);
+			if (!pokemon.hasAbility('quickfeet') && !pokemon.volatiles['dynamax']) {
+				spe = Math.floor(spe * 50 / 100);
+			}
+			return spe;
+		},
+		onBeforeMovePriority: 1,
+		onBeforeMove(pokemon) {
+			if (this.randomChance(1, 4) && !pokemon.volatiles['dynamax']) {
+				this.add('cant', pokemon, 'par');
+				return false;
+			}
+		},
+	},
+	confusion: {
+		name: 'confusion',
+		// this is a volatile status
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect?.id === 'lockedmove') {
+				this.add('-start', target, 'confusion', '[fatigue]');
+			} else if (sourceEffect?.effectType === 'Ability') {
+				this.add('-start', target, 'confusion', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+			} else {
+				this.add('-start', target, 'confusion');
+			}
+			const min = sourceEffect?.id === 'axekick' ? 3 : 2;
+			this.effectState.time = this.random(min, 6);
+		},
+		onEnd(target) {
+			this.add('-end', target, 'confusion');
+		},
+		onBeforeMovePriority: 3,
+		onBeforeMove(pokemon) {
+			pokemon.volatiles['confusion'].time--;
+			if (!pokemon.volatiles['confusion'].time) {
+				pokemon.removeVolatile('confusion');
+				return;
+			}
+			this.add('-activate', pokemon, 'confusion');
+			if (!this.randomChance(33, 100) || pokemon.volatiles['dynamax']) {
+				return;
+			}
+			this.activeTarget = pokemon;
+			const damage = this.actions.getConfusionDamage(pokemon, 40);
+			if (typeof damage !== 'number') throw new Error("Confusion damage not dealt");
+			const activeMove = {id: this.toID('confused'), effectType: 'Move', type: '???'};
+			this.damage(damage, pokemon, pokemon, activeMove as ActiveMove);
+			return false;
 		},
 	},
 };
