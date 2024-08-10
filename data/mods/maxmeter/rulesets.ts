b246e1d0850462d600ffbfe68157183555593d55
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-08-10 13:28:37 -0400

    Create rulesets.ts

---
+++ data/mods/maxmeter/rulesets.ts
@@ -0,0 +1,35 @@
+export const Rulesets: {[k: string]: ModdedFormatData} = {
+	supermoveadditions: {
+    effectType: 'Rule',
+		name: 'Super Move Additions',
+		desc: 'Adds a Super to every Pokemon.',
+    onBegin() {
+          for (const side0 of this.sides[0].pokemon) {
+      	 	  const newMove  = {
+      	    	move: "Super Move",
+            	id: "supermove",
+            	pp: 10,
+              maxpp: 10,
+            	target: "normal",
+            	disabled: false,
+              used: false,
+         	};
+    			side0.baseMoveSlots[side0.baseMoveSlots.length] = newMove
+    			side0.moveSlots[side0.moveSlots.length] = newMove
+    		  }
+          for (const side1 of this.sides[1].pokemon) {
+            const newMove  = {
+              move: "Super Move",
+            	id: "supermove",
+            	pp: 10,
+              maxpp: 10,
+            	target: "normal",
+            	disabled: false,
+            	used: false,
+         	};
+    			side1.baseMoveSlots[side1.baseMoveSlots.length] = newMove
+    			side1.moveSlots[side1.moveSlots.length] = newMove
+        }
+      },
+    },
+  };
