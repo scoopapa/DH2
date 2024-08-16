e8a8bb02d9bb64b8ef5160e7834485607088bb4d
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-08-16 02:15:54 -0400

    Update formats-data.ts

--- data/mods/littlecolosseum/formats-data.ts
+++ data/mods/littlecolosseum/formats-data.ts
@@ -1,4 +1,4 @@
-export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable = {
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 	bulbasaur: {
 		tier: "LC",
 	},
