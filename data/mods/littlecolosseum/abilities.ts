80f0453ce3f39e9f24a3e49f28a3da0e3a760ee8
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-08-16 02:30:05 -0400

    Create abilities.ts

---
+++ data/mods/littlecolosseum/abilities.ts
@@ -0,0 +1,22 @@
+export const Abilities: {[k: string]: ModdedAbilityData} = {
+	hazardabsorb: {
+    // implemented in moves.ts
+		shortDesc: "This Pokemon doesn't take damage from hazards.",
+		name: "Hazard Absorb",
+		rating: 4,
+	},
+	proteangen7: {
+		onPrepareHit(source, target, move) {
+			if (move.hasBounced || move.flags['futuremove'] || move.sourceEffect === 'snatch') return;
+			const type = move.type;
+			if (type && type !== '???' && source.getTypes().join() !== type) {
+				if (!source.setType(type)) return;
+				this.add('-start', source, 'typechange', type, '[from] ability: Protean (Gen 7)');
+			}
+		},
+		flags: {},
+		name: "Protean (Gen 7)",
+		rating: 4,
+		num: -168,
+	},
+};
