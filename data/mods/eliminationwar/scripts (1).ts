f99da1dae07d89d608c5691e9488c7b3a28eebff
Loginator <139919813+Loginator1652@users.noreply.github.com>
2024-08-22 17:40:19 -0700

    Elimination War
    Mod is real

---
+++ data/mods/eliminationwar/scripts (1).ts
@@ -0,0 +1,9 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
+		gen: 9,
+		teambuilderConfig: {
+			excludeStandardTiers: true,
+			customTiers: ['Legal'],
+			customDoublesTiers: ['Legal'],
+		},
+	};
