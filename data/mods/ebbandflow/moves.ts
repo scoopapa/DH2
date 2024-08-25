617741fa9e8574f965452b886aa75ac898274e7b
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-08-25 00:12:12 -0400

    Update moves.ts

--- data/mods/ebbandflow/moves.ts
+++ data/mods/ebbandflow/moves.ts
@@ -103,7 +103,6 @@ focuspunch: {
 			volatileStatus: 'flinch',
 		},
 		flags: {contact: 1, protect: 1, mirror: 1, metronome: 1},
-		secondary: null,
 		target: "normal",
 		type: "Ground",
 		contestType: "Tough",
