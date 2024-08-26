d7889539b4686708ea7fb511853df1ff5c96a110
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-08-25 21:54:45 -0400

    Update scripts.ts

--- data/mods/littlecolosseum/scripts.ts
+++ data/mods/littlecolosseum/scripts.ts
@@ -14,5 +14,15 @@ export const Scripts: ModdedBattleScriptsData = {
 		delete this.modData('Learnsets', 'gastly').learnset.nastyplot;
 		this.modData("Learnsets", "sprigatito").learnset.flowertrick = ["9L1"];
 		this.modData("Learnsets", "sprigatito").learnset.knockoff = ["9L1"];
+		this.modData("Learnsets", "meowthgalar").learnset.slackoff = ["9L1"];
+		this.modData("Learnsets", "tepig").learnset.slackoff = ["9L1"];
+		this.modData("Learnsets", "tepig").learnset.earthquake = ["9L1"];
+		this.modData("Learnsets", "tepig").learnset.highhorsepower = ["9L1"];
+		this.modData("Learnsets", "tepig").learnset.stealthrock = ["9L1"];
+		this.modData("Learnsets", "dreepy").learnset.willowisp = ["9L1"];
+		this.modData("Learnsets", "dreepy").learnset.dragonclaw = ["9L1"];
+		this.modData("Learnsets", "dreepy").learnset.uturn = ["9L1"];
+		this.modData("Learnsets", "dreepy").learnset.hex = ["9L1"];
+		this.modData("Learnsets", "dreepy").learnset.psychicfangs = ["9L1"];
 	},
 };
