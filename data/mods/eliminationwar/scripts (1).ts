08e925b18872864fd147a84773c522dbbc2ce310
Loginator <139919813+Loginator1652@users.noreply.github.com>
2024-08-26 21:07:21 -0700

    Elimination War Slate 1 Bans

--- data/mods/eliminationwar/scripts (1).ts
+++ data/mods/eliminationwar/scripts (1).ts
@@ -2,7 +2,7 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 	gen: 9,
 	teambuilderConfig: {
 		excludeStandardTiers: true,
-		customTiers: ['FFOU'],
+		customTiers: ['Legal', 'Eliminated'],
 		customDoublesTiers: ['FFOU'],
 	},
 };
