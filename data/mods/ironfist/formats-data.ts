f4655f88c9e34e07cb9cf022d0e2cc1e7eb0a9c6
charizard8888 <omkumr082001@outlook.com>
2024-08-30 13:02:39 +0530

    Create formats-data.ts

---
+++ data/mods/ironfist/formats-data.ts
@@ -0,0 +1,154 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	melmetal: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ninjask: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	toxapex: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	silcoon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	togedemaru: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	bramblin: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	anarlvet: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gholdengo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	kingler: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	feebas: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	microwave: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	glaceon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	lytlegai: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	sylveon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ohmyrod: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	celesteela: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	bigcrammer: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	archeops: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	runerigus: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	crabominable: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	samurottundefined: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	wyrdeer: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	goomba: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	fridgile: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	zaciancrowned: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	melmetal2: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	traike: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pidown: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	silvally: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	vivillonplainshigh: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	moltres: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	kurayami: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	hitmontop: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	zelda: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	cascoon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	sentret: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	drigike: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	phish: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+}
