874edb882cfeabf858c40baa2d8203b693b4924c
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-09-03 21:34:49 -0400

    Update conditions.ts

--- data/mods/generationyb/conditions.ts
+++ data/mods/generationyb/conditions.ts
@@ -21,4 +21,46 @@ export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDa
 			this.actions.useMove("Aurum Aura Used", pokemon, pokemon);
 		},
 	},
+	frz: {
+		name: 'frz',
+		effectType: 'Status',
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'frz', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+			} else {
+				this.add('-status', target, 'frz');
+			}
+			if (target.species.name === 'Shaymin-Sky' && target.baseSpecies.baseSpecies === 'Shaymin') {
+				target.formeChange('Shaymin', this.effect, true);
+			}
+		},
+		onResidualOrder: 9,
+		onResidual(pokemon) {
+			if (this.randomChance(1, 10)) {
+				pokemon.cureStatus();
+				return;
+			}
+			this.damage(pokemon.baseMaxhp / 8);
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			this.debug('Freeze extra damage');
+			return this.chainModify([5325, 4096]);
+		},
+		onModifyMove(move, pokemon) {
+			if (move.flags['defrost']) {
+				this.add('-curestatus', pokemon, 'frz', '[from] move: ' + move);
+				pokemon.clearStatus();
+			}
+		},
+		onAfterMoveSecondary(target, source, move) {
+			if (move.thawsTarget) {
+				target.cureStatus();
+			}
+		},
+		onDamagingHit(damage, target, source, move) {
+			if (move.type === 'Fire' && move.category !== 'Status') {
+				target.cureStatus();
+			}
+		},
+	},
 };
