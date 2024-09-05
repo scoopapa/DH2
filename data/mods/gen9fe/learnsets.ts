daa239cbedd08baf6bbc79f777b1611602f7b60f
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-09-04 22:09:52 -0400

    Update learnsets.ts

--- data/mods/gen9fe/learnsets.ts
+++ data/mods/gen9fe/learnsets.ts
@@ -346,7 +346,7 @@ export const Learnsets: {[k: string]: LearnsetData} = {
 			skittersmack: ["9L1"],
 		},
 	},
-	smoliqwil: {
+	smoliquil: {
 		learnset: {
 			absorb: ["9L1"],
 			bulletseed: ["9L1"],
