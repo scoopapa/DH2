0ecf19eb381f3f0d463a6b76335f9a1aaa4a4ff7
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-09-07 20:19:43 -0400

    Create scripts.ts

---
+++ data/mods/donotusetmptm/scripts.ts
@@ -0,0 +1,7 @@
+export const Scripts: ModdedBattleScriptsData = {
+	gen: 9,
+	teambuilderConfig: {
+		excludeStandardTiers: true,
+		customTiers: ["DoNU", "DoNU UUBL", "DoNU UU", "DoNU RUBL", "DoNU RU", "DoNU Uber"],
+	},
+};
