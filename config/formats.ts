e24749b2f1d8ff00c0a8316539fa3dfb30af0871
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2024-09-13 12:59:21 -0500

    when the

--- config/formats.ts
+++ config/formats.ts
@@ -715,7 +715,7 @@ export const Formats: FormatList = [
 		],
 		mod: 'ironfist',
 		teambuilderFormat: "National Dex",
-		ruleset: ['Standard NatDex', 'Terastal Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Data Mod', 'Z-Move Clause'],
+		ruleset: ['Standard NatDex', 'Terastal Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Sleep Clause Mod', 'Data Mod', 'Z-Move Clause'],
 		banlist: ['Baton Pass', 'King\'s Rock', 'Razor Fang', 'Moody'],
 		onValidateTeam(team, format) {
 			/**@type {{[k: string]: true}}*/
