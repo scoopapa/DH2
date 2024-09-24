b587af22b0dd1850d39a50474eba9b5ef0125232
ViZarSmogon <vizarsmogon@gmail.com>
2024-09-24 09:55:39 +0200

    Ubermons Slate 2

--- data/mods/gen9ubermons/pokedex.ts
+++ data/mods/gen9ubermons/pokedex.ts
@@ -1,4 +1,4 @@
-export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
 	landorus: {
 		inherit: true,
 		abilities: {0: "Sand Force", H: "Defiant"},
@@ -10,5 +10,21 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	ogerponhearthflame: {
 		inherit: true,
 		abilities: {0: "Regenerator"},
+	},
+	darkrai: {
+		inherit: true,
+		baseStats: {hp: 90, atk: 90, def: 90, spa: 120, spd: 90, spe: 120},
+	},
+	kingambit: {
+		inherit: true,
+		baseStats: {hp: 100, atk: 125, def: 130, spa: 60, spd: 85, spe: 50},
+	},
+	chienpao: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 100, def: 80, spa: 90, spd: 85, spe: 135},
+	},
+	chiyu: {
+		inherit: true,
+		baseStats: {hp: 85, atk: 90, def: 90, spa: 95, spd: 130, spe: 80},
 	},
 };
