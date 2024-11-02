07f8633c4896a440a473c174ead4aa4d06cbb872
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2024-11-02 08:25:53 -0500

    Update learnsets.ts

--- data/mods/ironfist/learnsets.ts
+++ data/mods/ironfist/learnsets.ts
@@ -8661,7 +8661,7 @@ export const Learnsets: {[speciesid: string]: ModdedLearnsetData} = {
 			toxic: ["9L1"],
 		},
 	},
-	minunplusled: {
+	minunandplusled: {
 		learnset: {
 			agility: ["9L1"],
 			alluringvoice: ["9L1"],
