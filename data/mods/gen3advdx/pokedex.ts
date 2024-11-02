6e107177b615427257c301832509204ac45dbaf8
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-11-01 20:19:31 -0400

    Create pokedex.ts

---
+++ data/mods/gen3advdx/pokedex.ts
@@ -0,0 +1,333 @@
+export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
+	donphan: {
+    inherit: true,
+		abilities: {0: "Intimidate"},
+	},
+	dragonite: {
+    inherit: true,
+		abilities: {0: "Marvel Scale"},
+	},
+	crobat: {
+    inherit: true,
+		abilities: {0: "Serene Grace", 1: "Corrosion"},
+	},
+	houndoom: {
+    inherit: true,
+		baseStats: {hp: 75, atk: 80, def: 50, spa: 110, spd: 80, spe: 105},
+		abilities: {0: "Run Away", 1: "Flash Fire"},
+	},
+	dodrio: {
+    inherit: true,
+		baseStats: {hp: 60, atk: 110, def: 70, spa: 60, spd: 60, spe: 110},
+	},
+	ludicolo: {
+    inherit: true,
+		abilities: {0: "Swift Swim", 1: "Oblivious"},
+	},
+	regirock: {
+    inherit: true,
+		abilities: {0: "Rough Skin"},
+	},
+	regice: {
+    inherit: true,
+		abilities: {0: "Illuminate"},
+	},
+	sableye: {
+    inherit: true,
+		abilities: {0: "Keen Eye", 1: "Prankster"},
+	},
+	ariados: {
+    inherit: true,
+		baseStats: {hp: 70, atk: 95, def: 85, spa: 50, spd: 60, spe: 80},
+	},
+	sunflora: {
+    inherit: true,
+		types: ["Fire", "Grass"],
+		baseStats: {hp: 105, atk: 65, def: 65, spa: 115, spd: 95, spe: 30},
+  },
+	castformsunny: {
+    inherit: true,
+		baseStats: {hp: 70, atk: 90, def: 80, spa: 110, spd: 60, spe: 110},
+	},
+	castformrainy: {
+    inherit: true,
+		baseStats: {hp: 70, atk: 60, def: 110, spa: 80, spd: 110, spe: 90},
+	},
+	castformsnowy: {
+    inherit: true,
+		baseStats: {hp: 70, atk: 110, def: 80, spa: 110, spd: 90, spe: 60},
+	},
+	charizard: {
+    inherit: true,
+		abilities: {0: "Blaze", 1: "Flame Body"},
+	},
+	blastoise: {
+    inherit: true,
+		abilities: {0: "Torrent", 1: "Shell Armor"},
+	},
+	venusaur: {
+    inherit: true,
+		abilities: {0: "Overgrow", 1: "Effect Spore"},
+	},
+	typhlosion: {
+    inherit: true,
+		baseStats: {hp: 75, atk: 99, def: 70, spa: 103, spd: 82, spe: 105},
+		abilities: {0: "Blaze", 1: "Flash Fire"},
+	},
+	feraligatr: {
+    inherit: true,
+		abilities: {0: "Torrent", 1: "Hustle"},
+	},
+	meganium: {
+    inherit: true,
+		abilities: {0: "Overgrow", 1: "Cloud Nine"},
+	},
+	sceptile: {
+    inherit: true,
+		abilities: {0: "Overgrow", 1: "Limber"},
+	},
+	swampert: {
+    inherit: true,
+		abilities: {0: "Torrent", 1: "Rain Dish"},
+	},
+	blaziken: {
+    inherit: true,
+		abilities: {0: "Blaze", 1: "Speed Boost"},
+	},
+	chespin: {
+    inherit: true,
+		abilities: {0: "Overgrow"},
+		gen: 3,
+	},
+	quilladin: {
+    inherit: true,
+		abilities: {0: "Overgrow"},
+		gen: 3,
+	},
+	chesnaught: {
+    inherit: true,
+		abilities: {0: "Overgrow", 1: "Rough Skin"},
+		gen: 3,
+	},
+	froakie: {
+    inherit: true,
+		abilities: {0: "Torrent"},
+		gen: 3,
+	},
+	frogadier: {
+    inherit: true,
+		abilities: {0: "Torrent"},
+		gen: 3,
+	},
+	greninja: {
+    inherit: true,
+		abilities: {0: "Torrent", 1: "Color Change"},
+		gen: 3,
+	},
+	fennekin: {
+    inherit: true,
+		abilities: {0: "Blaze"},
+		gen: 3,
+	},
+	braixen: {
+    inherit: true,
+		abilities: {0: "Blaze"},
+		gen: 3,
+	},
+	delphox: {
+    inherit: true,
+		abilities: {0: "Blaze", 1: "Insomnia"},
+		gen: 3,
+	},
+	snover: {
+		inherit: true,
+		abilities: {0: "Snow Warning"},
+		gen: 3,
+	},
+	abomasnow: {
+		inherit: true,
+		abilities: {0: "Snow Warning"},
+		gen: 3,
+	},
+	rhyhorn: {
+    inherit: true,
+		abilities: {0: "Rough Skin", 1: "Rock Head"},
+	},
+	rhydon: {
+    inherit: true,
+		abilities: {0: "Rough Skin", 1: "Rock Head"},
+	},
+	rhyperior: {
+    inherit: true,
+		abilities: {0: "Rough Skin", 1: "Rock Head"},
+		gen: 3,
+	},
+	cacnea: {
+    inherit: true,
+		abilities: {0: "Rough Skin", 1: "Sand Veil"},
+	},
+	cacturne: {
+    inherit: true,
+		abilities: {0: "Rough Skin", 1: "Sand Veil"},
+	},
+	gastly: {
+    inherit: true,
+		abilities: {0: "Levitate", 1: "Corrosion"},
+	},
+	haunter: {
+    inherit: true,
+		abilities: {0: "Levitate", 1: "Corrosion"},
+	},
+	gengar: {
+    inherit: true,
+		abilities: {0: "Levitate", 1: "Corrosion"},
+	},
+	koffing: {
+    inherit: true,
+		abilities: {0: "Levitate", 1: "Corrosion"},
+	},
+	weezing: {
+    inherit: true,
+		abilities: {0: "Levitate", 1: "Corrosion"},
+	},
+	muk: {
+    inherit: true,
+		abilities: {0: "Corrosion", 1: "Sticky Hold"},
+	},
+	porygonz: {
+    inherit: true,
+		abilities: {0: "Trace"},
+		gen: 3,
+	},
+	electivire: {
+    inherit: true,
+		abilities: {0: "Volt Absorb", 1: "Static"},
+		gen: 3,
+	},
+	froslass: {
+    inherit: true,
+		abilities: {0: "Pressure"},
+		gen: 3,
+	},
+	gallade: {
+    inherit: true,
+		abilities: {0: "Inner Focus"},
+		gen: 3,
+	},
+	ambipom: {
+    inherit: true,
+		abilities: {0: "Run Away", 1: "Pickup"},
+		gen: 3,
+	},
+	probopass: {
+    inherit: true,
+		baseStats: {hp: 60, atk: 85, def: 145, spa: 75, spd: 120, spe: 40},
+		abilities: {0: "Levitate", 1: "Magnet Pull"},
+		gen: 3,
+	},
+	regigigas: {
+    inherit: true,
+		abilities: {0: "Pressure"},
+		gen: 3,
+	},
+	regieleki: {
+    inherit: true,
+		abilities: {0: "Speed Boost"},
+		gen: 3,
+	},
+	regidrago: {
+    inherit: true,
+		abilities: {0: "Thick Fat"},
+		gen: 3,
+	},
+	golett: {
+    inherit: true,
+		abilities: {0: "Battle Armor"},
+		gen: 3,
+	},
+	golurk: {
+    inherit: true,
+		abilities: {0: "Battle Armor"},
+		gen: 3,
+	},
+	shellos: {
+    inherit: true,
+		abilities: {0: "Sticky Hold", 1: "Water Absorb"},
+		gen: 3,
+	},
+	gastrodon: {
+    inherit: true,
+		abilities: {0: "Sticky Hold", 1: "Water Absorb"},
+		gen: 3,
+	},
+	lickilicky: {
+    inherit: true,
+		abilities: {0: "Own Tempo", 1: "Oblivious"},
+		gen: 3,
+	},
+	magnezone: {
+    inherit: true,
+		abilities: {0: "Magnet Pull", 1: "Sturdy"},
+		gen: 3,
+	},
+	mismagius: {
+    inherit: true,
+		abilities: {0: "Levitate"},
+		gen: 3,
+	},
+	magmortar: {
+    inherit: true,
+		abilities: {0: "Flame Body", 1: "Vital Spirit"},
+		gen: 3,
+	},
+	togekiss: {
+    inherit: true,
+		abilities: {0: "Hustle", 1: "Serene Grace"},
+		gen: 3,
+	},
+	roserade: {
+    inherit: true,
+		abilities: {0: "Natural Cure", 1: "Poison Point"},
+		gen: 3,
+	},
+	dusknoir: {
+    inherit: true,
+		abilities: {0: "Pressure"},
+		gen: 3,
+	},
+	rotom: {
+    inherit: true,
+		abilities: {0: "Levitate"},
+		gen: 3,
+	},
+	cryogonal: {
+    inherit: true,
+		abilities: {0: "Levitate"},
+		gen: 3,
+	},
+	mamoswine: {
+    inherit: true,
+		abilities: {0: "Oblivious", 1: "Thick Fat"},
+		gen: 3,
+	},
+	weavile: {
+    inherit: true,
+		abilities: {0: "Pressure"},
+		gen: 3,
+	},
+	tangrowth: {
+    inherit: true,
+		abilities: {0: "Chlorophyll"},
+		gen: 3,
+	},
+	yanmega: {
+    inherit: true,
+		abilities: {0: "Speed Boost"},
+		gen: 3,
+	},
+	honchkrow: {
+    inherit: true,
+		abilities: {0: "Insomnia"},
+		gen: 3,
+	},
+};
