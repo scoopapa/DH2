0f6939f9e1a4dc07151dd0ff25342e85e05ad4b4
Black Box <30437711+BB-Box@users.noreply.github.com>
2024-11-27 23:48:22 +0100

    Fix server crash

--- data/mods/ggsbattleformat/learnsets.ts
+++ data/mods/ggsbattleformat/learnsets.ts
@@ -1,8 +1,8 @@
-/*export const Learnsets: {[speciesid: string]: ModdedLearnsetData} = {
-	dugtrio: {
+export const Learnsets: {[speciesid: string]: ModdedLearnsetData} = {
+	/*dugtrio: {
 		learnset: {
 			inherit: true,
 			tripledig: ["9L1"],
 		},
-	},
-};*/
+	},*/
+};
