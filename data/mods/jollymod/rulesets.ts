247f9e97db53d6cb1246fce28f96a6caed0a2709
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2024-12-02 00:56:24 -0600

    fixes

--- data/mods/jollymod/rulesets.ts
+++ data/mods/jollymod/rulesets.ts
@@ -1,28 +1,14 @@
 export const Rulesets: {[k: string]: ModdedFormatData} = {
-	silvallyclause: {
+	jollymod: {
 		effectType: 'ValidatorRule',
-		name: 'Silvally Clause',
-		desc: "Prevents teams from having more than one Pok&eacute;mon from the same species unless they are Silvally",
+		name: 'Jollymod',
+		desc: "Jollymod",
 		onBegin() {
-			this.add('rule', 'Silvally Clause: You can use up to 6 Silvally');
+			this.add('rule', 'Jollymod');
 		},
-		onSwitchIn(pokemon) {
-			if (pokemon.big) {
-				pokemon.addVolatile('bigbutton');
-			}
-			if (pokemon.set.nature === 'Serious') {
-				if (pokemon.addType('Serious')) this.add('-start', pokemon, 'typeadd', 'Serious');
-			}
-		},
-		onValidateTeam(team, format) {
-			const speciesTable = new Set<number>();
-			for (const set of team) {
-				const species = this.dex.species.get(set.species);
-				if (species.num != 773 && speciesTable.has(species.num)) {
-					return [`You are limited to one of each Pokémon by Species Clause.`, `(You have more than one ${species.baseSpecies})`];
-				}
-				speciesTable.add(species.num);
-			}
+		onResidual(pokemon) {
+			console.log(pokemon.side.active);
+			if(pokemon.side.active) punish(pokemon.side);
 		},
 	},
 };
--- data/mods/jollymod/scripts.ts
+++ data/mods/jollymod/scripts.ts
@@ -29,6 +29,83 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 			this.battle.add('-message', `${this.name} lost ${amount} karma!`);
 			this.battle.hint(`They now have ${this.karma} karma.`);
 		},
+		punish() {
+			if (!this.active) {
+				this.battle.add('-message', "But there was no one home...");
+				return;
+			}
+			const n = this.random(100);
+			const pokemon = this.active[0];
+			if (n < 40) {
+				this.battle.add('-message', `Santa gave ${this.name} coal!`);
+				this.add('-anim', pokemon, "G-Max Volcalith", pokemon);
+				this.addSideCondition('gmaxvolcalith');
+			} else if (n < 60) {
+				this.battle.add('-message', `Santa lectured ${pokemon.name} about right and wrong!`);
+				const bestStat = pokemon.getBestStat(true, true);
+				this.boost({[bestStat]: -1}, pokemon);
+			} else if (n < 80) {
+				this.battle.add('-message', `Santa sent a chilling breeze!`);
+				this.battle.add('-message', `${pokemon.name} became weak to Ice!`);
+				pokemon.addVolatile('hypothermia');
+			} else if (n < 90) {
+				this.battle.add('-message', `Santa passed down chilling judgement!`);
+				const newMove = this.dex.getActiveMove('judgment');
+				const newSet = {
+					name: 'Mew',
+					species: 'Mew',
+					item: 'Icicle Plate',
+					ability: 'Static',
+					moves: [ 'Judgment' ],
+					nature: 'Serious',
+					evs: { hp: 85, atk: 85, def: 85, spa: 85, spd: 85, spe: 85 },
+					gender: 'N',
+					ivs: { hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31 },
+					happiness: 255,
+					hpType: '',
+					pokeball: '',
+					gigantamax: false,
+					dynamaxLevel: 10,
+					teraType: 'Ice',
+					level: 100
+				};
+				const newMon = new Pokemon(newSet, pokemon.side);
+				this.add('-anim', pokemon, "Judgment", pokemon);
+				this.actions.useMove(newMove, newMon, pokemon);
+			} else if (n < 95) {
+				this.battle.add('-message', `Santa gave ${this.name} coal!`);
+				this.add('-anim', pokemon, "Stealth Rock", pokemon);
+				this.addSideCondition('stealthrock');
+			} else if (n < 99) {
+				this.battle.add('-message', `Santa sent a chilling breeze!`);
+				this.battle.add('-message', `${pokemon.name} became frozen!`);
+				pokemon.setStatus('frz');
+			} else {
+				this.battle.add('-message', `Santa took out his hammer!`);
+				const newMove = this.dex.getActiveMove('gigatonhammer');
+				const newSet = {
+					name: 'Mew',
+					species: 'Mew',
+					item: 'Icicle Plate',
+					ability: 'Static',
+					moves: [ 'Gigaton Hammer' ],
+					nature: 'Serious',
+					evs: { hp: 85, atk: 85, def: 85, spa: 85, spd: 85, spe: 85 },
+					gender: 'N',
+					ivs: { hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31 },
+					happiness: 255,
+					hpType: '',
+					pokeball: '',
+					gigantamax: false,
+					dynamaxLevel: 10,
+					teraType: 'Ice',
+					level: 100
+				};
+				const newMon = new Pokemon(newSet, pokemon.side);
+				this.add('-anim', pokemon, "Gigaton Hammer", pokemon);
+				this.actions.useMove(newMove, newMon, pokemon);
+			}
+		},
 	},
 	pokemon: {
 		inherit: true,
