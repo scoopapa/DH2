70bda226cecee119d060ad22dd56af0e596b0b07
Lysio4 <lukas.moncorge@gmail.com>
2024-12-06 22:59:20 +0900

    print

--- data/mods/glacemons/moves.ts
+++ data/mods/glacemons/moves.ts
@@ -548,9 +548,11 @@ export const Moves: { [moveid: string]: ModdedMoveData; } = {
 			const targetAtk = target.storedStats.atk;
 			const sourceAtk = source.storedStats.atk;
 			if (sourceAtk >= targetAtk) {
+				console.log("I boost!");
 				this.boost({atk: 2, def: 2}, source, source);
 			}
 			else if (sourceAtk < targetAtk){
+				console.log("I don't boost, I'm weak! " + target + "does more damage than" + source);
 				this.boost({atk: 2, def: 2}, source, target);
 			}
 		},
