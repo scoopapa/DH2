af74579ddad232b69870ef3abf51880978110c8d
Lysio4 <lukas.moncorge@gmail.com>
2024-12-20 20:49:21 +0100

    removing comments

--- data/mods/dlcmons/abilities.ts
+++ data/mods/dlcmons/abilities.ts
@@ -1,25 +1,5 @@
 export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 	swiftretreat: {
-		// try to code this?
-		// come on, you can do it
-		// you can do it!
-		// you can do it!
-		// you can do it!
-		// you can do it!
-		// Please do it instead of saying "you can do it!"
-		// I can't do it, I'm sorry
-		// I can't do it, I'm sorry
-		// I can't do it, I'm sorry
-		// you can do it!
-		// you can do it!
-		// you can do it!
-		// AAAAAAAAAAAAAAAAAAAAAAAAAAA
-		// AAAAAAAAAAAAAAAAAAAAAAAAAAA
-		// AAAAAAAAAAAAAAAAAAAAAAAAAAA
-		// What is love
-		// Baby don't hurt me
-		// Don't hurt me
-		// No more
 		name: "Swift Retreat",
 		desc: "This Pokemon can choose to switch out on the second turn of moves with a charge turn of invulnerability instead of attacking. This list does not include Sky Drop.",
 		shortDesc: "This Pokemon can choose to switch out on the second turn of a two-turn move.",
