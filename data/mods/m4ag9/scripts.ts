2a0b006d545921cefe6d29a425bfe599bb29fe9d
Lysio4 <lukas.moncorge@gmail.com>
2024-12-21 20:57:50 +0100

    hey trying something for m4a

--- data/mods/m4ag9/scripts.ts
+++ data/mods/m4ag9/scripts.ts
@@ -49,7 +49,7 @@ export const Scripts: ModdedBattleScriptsData = {
 		customTiers: ['Mega of the Day!', 'Popular', 'Popular Megas', 'Other Megas', 'Heat!', 'NFE'],
 	},
 	init() {
-		if (this.dataCache.Pokedex.raichumega) return; // don't bother generating the dex if it's already stored!
+		if (this.dataCache.Pokedex.quaquavalmega) return; // don't bother generating the dex if it's already stored!
 		let megaList = [];
 		for (const id in this.dataCache.Pokedex) {
 			const pokemon = this.dataCache.Pokedex[id];
