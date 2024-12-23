8af5f99e668e243419f8f3629c1722e8089fb873
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-12-22 22:58:53 -0500

    Update abilities.ts

--- data/mods/teraforming/abilities.ts
+++ data/mods/teraforming/abilities.ts
@@ -22,6 +22,7 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 		condition: {
 			duration: 2,
 			onStart(pokemon) {
+				this.add('-ability', pokemon, 'Uptime');
 				this.add('-message', `{$pokemon.name}'s next move will have +1 priority!`);
 			},
 			onModifyPriority(priority, pokemon, target, move) {
