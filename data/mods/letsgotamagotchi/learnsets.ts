de604184fe5d1d3776e6a46a3da8fa946a9d1370
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-12-23 17:24:48 -0500

    Update learnsets.ts

--- data/mods/letsgotamagotchi/learnsets.ts
+++ data/mods/letsgotamagotchi/learnsets.ts
@@ -6740,7 +6740,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 	},
 	himespetchi: {
 		learnset: {
-			kirakira☆dream: ["9L1"],
+			kirakiradream: ["9L1"],
 			alienlaser: ["9L1"],
 			faketears: ["9L1"],
 			firepunch: ["9L1"],
@@ -6834,7 +6834,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 	},
 	yumemitchi: {
 		learnset: {
-			kirakira☆dream: ["9L1"],
+			kirakiradream: ["9L1"],
 			yumekirabag: ["9L1"],
 			transform: ["9L1"],
 			faketears: ["9L1"],
@@ -6900,7 +6900,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 	},
 	kiraritchi: {
 		learnset: {
-			kirakira☆dream: ["9L1"],
+			kirakiradream: ["9L1"],
 			yumekirabag: ["9L1"],
 			transform: ["9L1"],
 			faketears: ["9L1"],
@@ -7890,7 +7890,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 	},
 	pianitchi: {
 		learnset: {
-			kirakira☆dream: ["9L1"],
+			kirakiradream: ["9L1"],
 			smapi: ["9L1"],
 			faketears: ["9L1"],
 			firepunch: ["9L1"],
@@ -8152,7 +8152,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 	},
 	coffretchi: {
 		learnset: {
-			kirakira☆dream: ["9L1"],
+			kirakiradream: ["9L1"],
 			dreamcoffretpalette: ["9L1"],
 			moripakucoffretmarch: ["9L1"],
 			faketears: ["9L1"],
