771a19ef550c075770ce3298383c7a8cd6ff3a5f
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-12-22 21:45:30 -0500

    Update moves.ts

--- data/mods/teraforming/moves.ts
+++ data/mods/teraforming/moves.ts
@@ -1,5 +1,44 @@
 export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
-  teraused: {
+	// non-new moves
+	gigatonhammer: {
+		num: 893,
+		accuracy: 100,
+		basePower: 160,
+		category: "Physical",
+		name: "Gigaton Hammer",
+		pp: 5,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, metronome: 1, cantusetwice: 1},
+		self: {
+			volatileStatus: 'gigatonhammer',
+		},
+		condition: {
+			duration: 2,
+			onModifyPriority(priority, source, target, move) {
+				return priority + 1;
+			},
+		},
+		secondary: null,
+		target: "normal",
+		type: "Steel",
+	},
+	bloodmoon: {
+		num: 901,
+		accuracy: 100,
+		basePower: 140,
+		category: "Special",
+		name: "Blood Moon",
+		pp: 5,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, metronome: 1, cantusetwice: 1},
+		self: {
+			volatileStatus: 'gigatonhammer',
+		},
+		secondary: null,
+		target: "normal",
+		type: "Normal",
+	},
+	teraused: {
 		shortDesc: "Prevents Terastalization from being used multiple times.",
 		accuracy: true,
 		basePower: 0,
