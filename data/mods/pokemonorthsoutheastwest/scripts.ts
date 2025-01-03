3e61ab74dc025e88100cf4cc5381949d71f7fea4
Nbrimson <78097164+Nbrimson@users.noreply.github.com>
2025-01-03 14:55:56 +0000

    Fixing a script bug

--- data/mods/pokemonorthsoutheastwest/scripts.ts
+++ data/mods/pokemonorthsoutheastwest/scripts.ts
@@ -4,6 +4,6 @@ export const Scripts: ModdedBattleScriptsData = {
 		// for micrometas to only show custom tiers
 		excludeStandardTiers: true,
 		// only to specify the order of custom tiers
-		customTiers: ['NSEW', 'NSEW2' 'Pokémon North, South, East, and West DOU'],
+		customTiers: ['NSEW', 'NSEW2', 'Pokémon North, South, East, and West DOU'],
 	},
 };
