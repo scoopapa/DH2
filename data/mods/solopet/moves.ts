90a8bd9d927596b8041427c8e1c920faba5d784c
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-01-06 14:58:30 -0500

    Create moves.ts

---
+++ data/mods/solopet/moves.ts
@@ -0,0 +1,26 @@
+export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	shelltrap: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	thousandwaves: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	thousandarrows: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	landswrath: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	coreenforcer: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	fishiousrend: {
+		inherit: true,
+		isNonstandard: null,
+	},
+};
