17d67c10aa3a4d334e385077f688a504619f48ca
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-01-10 03:13:00 -0500

    Update abilities.ts

--- data/mods/teraforming/abilities.ts
+++ data/mods/teraforming/abilities.ts
@@ -34,4 +34,45 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 		rating: 3.5,
 		shortDesc: "If any of this Pokemon's moves are disabled, this Pokemon's next move has +1 priority.",
 	},
+	sinkorswim: {
+		onStart(pokemon) {
+			let activated = false;
+			for (const target of pokemon.adjacentFoes()) {
+				if (!activated) {
+					this.add('-ability', pokemon, 'Sink or Swim', 'boost');
+					activated = true;
+				}
+				if (target.volatiles['substitute']) {
+					this.add('-immune', target);
+				} else {
+					this.boost({spe: -1}, target, pokemon, null, true);
+				}
+			}
+		},
+		flags: {},
+		name: "Sink or Swim",
+		rating: 3.5,
+		shortDesc: "On switch-in, this Pokemon lowers the Speed of opponents by 1 stage.",
+	},
+	energizer: {
+		name: "Energizer",
+	   onFaint(pokemon) {
+			pokemon.side.addSlotCondition(pokemon, 'energizer');
+	   },
+	   condition: {
+			onSwap(target) {
+				 if (!target.fainted) {
+					const source = this.effectState.source;
+					const damage = this.heal(target.baseMaxhp / 4, target, target);
+					this.add('-anim', target, "Charge", target);
+					if (damage) this.add('-heal', target, target.getHealth, '[from] ability: Energizer', '[of] ' + this.effectState.source);
+					target.cureStatus();
+					target.side.removeSlotCondition(target, 'energizer');
+				 }
+			},
+	   },
+		flags: {},
+		rating: 3,
+		shortDesc: "On switch out, replacement heals 25% of their max HP and cures its status.",
+	},
 };
