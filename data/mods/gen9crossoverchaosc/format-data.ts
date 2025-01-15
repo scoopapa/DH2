a4fc7469af2fc083337332a1b35fe3849bae1f71
Rasdanation <Rasdanation@gmail.com>
2025-01-15 17:27:21 -0500

    CC Slate 1 Format Data

---
+++ data/mods/gen9crossoverchaosc/format-data.ts
@@ -0,0 +1,14 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+  narwa: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
+	hinakagiyama: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
+	luigi: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
+};
