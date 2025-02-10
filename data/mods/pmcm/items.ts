af10f8d2dc21da3c77e9096a41c6b9e4ec9283d5
vaderaven-a <vaderaven12345@gmail.com>
2025-02-11 01:10:59 +0700

    Update items.ts (PMCM)
    Fixing Big Root Item

--- data/mods/pmcm/items.ts
+++ data/mods/pmcm/items.ts
@@ -1,12 +1,13 @@
 export const Items: {[itemid: string]: ModdedItemData} = {
-  bigroot: {
-    inherit: true,
-    onTryHeal(damage, target, source, effect) {
-			const heals = ['drain', 'leechseed', 'ingrain', 'aquaring', 'strengthsap'];
-			if (heals.includes(effect.id)) {
-				return this.chainModify([1.5]);
-			}
-		},
-    shortDesc: "Holder gains 1.5x HP from draining/Aqua Ring/Ingrain/Leech Seed/Strength Sap.",
-  },
+	bigroot: {
+	  inherit: true,
+	  onTryHeal(damage, target, source, effect) {
+		const heals = ['drain', 'leechseed', 'ingrain', 'aquaring', 'strengthsap'];
+		if (heals.includes(effect.id)) {
+		  return this.chainModify([6144,4096]);
+		}
+	  },
+	  shortDesc: "Holder gains 1.5x HP from draining, Aqua Ring, Ingrain, Leech Seed, Strength Sap.",
+	},
 };
+  
