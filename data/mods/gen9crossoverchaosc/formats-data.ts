eede43e2cb5de86be1275ec6c61407f846f50842
Rasdanation <Rasdanation@gmail.com>
2025-02-19 16:22:10 -0500

    Update formats-data.ts

--- data/mods/gen9crossoverchaosc/formats-data.ts
+++ data/mods/gen9crossoverchaosc/formats-data.ts
@@ -11,4 +11,28 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		tier: "CC OU",
 		doublesTier: "DCC OU",
 	},
+	v1: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
+	thedefect: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
+	suwakomoriya: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
+	donkeykong: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
+	wizardlordphoenix: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
+	eox: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
 };
