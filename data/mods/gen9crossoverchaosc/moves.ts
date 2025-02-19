8d5f30b08a80015045ad2d73be7e615a6399b7c8
Rasdanation <Rasdanation@gmail.com>
2025-02-19 16:39:57 -0500

    Slates 2 & 3

--- data/mods/gen9crossoverchaosc/moves.ts
+++ data/mods/gen9crossoverchaosc/moves.ts
@@ -24,53 +24,53 @@ sound: Has no effect on Pokemon with the Soundproof Ability.
 */
 
 export const Moves: {[k: string]: ModdedMoveData} = {
-  luigilogic: {
+	luigilogic: {
 		num: -1,
 		accuracy: 90,
 		basePower: 0,
 		category: "Status",
 		name: "Luigi Logic",
+		shortDesc: "Disables target's ability and previous move choice.",
 		pp: 5,
 		priority: 0,
 		flags: {protect: 1, reflectable: 1, mirror: 1, allyanim: 1, metronome: 1},
 		volatileStatus: 'luigilogic',
 		onTryHit(target) {
-      // If both ability suppression & and move disabling aren't possible (cannot stack with disable)
+			// If both ability suppression & and move disabling aren't possible (cannot stack with disable)
 			if ((target.getAbility().flags['cantsuppress'] || target.volatiles['gastroacid'] || target.hasItem('Ability Shield'))
-         && (!target.lastMove || target.lastMove.isZ || target.lastMove.isMax || target.lastMove.id === 'struggle' || target.volatiles['disable'] )) {
-        if (target.hasItem('Ability Shield')) {
-          this.add('-block', target, 'item: Ability Shield');
-        }
-        this.hint("Luigi Logic will fail if it is both impossible to suppress ability & disable the last used move.");
+          && (!target.lastMove || target.lastMove.isZ || target.lastMove.isMax || target.lastMove.id === 'struggle' || target.volatiles['disable'] )) {
+				if (target.hasItem('Ability Shield')) {
+				  this.add('-block', target, 'item: Ability Shield');
+				}
+				this.hint("Luigi Logic will fail if it is both impossible to suppress ability & disable the last used move.");
 				return false;
 			}
 		},
-    onHit(target) {
-      // Ability suppression
-      if (!target.getAbility().flags['cantsuppress'] && !target.volatiles['gastroacid'] && !target.hasItem('Ability Shield')) {
-        target.addVolatile('gastroacid');
-      }
-      else if(target.hasItem('Ability Shield')) {
-        this.add('-block', target, 'item: Ability Shield');
-      }
-      // Move disable
-      if (target.lastMove && !target.lastMove.isZ && !target.lastMove.isMax && !target.lastMove.id === 'struggle' && !target.volatiles['disable']) {
-        target.addVolatile('disable');
-      }
-    },
-    onPrepareHit(target, source, move) {
+		onHit(target) {
+			// Ability suppression
+			if (!target.getAbility().flags['cantsuppress'] && !target.volatiles['gastroacid'] && !target.hasItem('Ability Shield')) {
+			target.addVolatile('gastroacid');
+			}
+			else if(target.hasItem('Ability Shield')) {
+				this.add('-block', target, 'item: Ability Shield');
+			}
+			// Move disable
+			if (target.lastMove && !target.lastMove.isZ && !target.lastMove.isMax && !target.lastMove.id === 'struggle' && !target.volatiles['disable']) {
+				target.addVolatile('disable');
+			}
+	  },
+		onPrepareHit(target, source, move) {
 			this.attrLastMove('[still]');
 			this.add('-anim', source, "Nasty Plot", source);
-      this.add('-anim', source, "Flash", target);
+			this.add('-anim', source, "Flash", target);
 		},
 		secondary: null,
 		target: "normal",
 		type: "Normal",
 		zMove: {effect: 'clearnegativeboost'},
 		contestType: "Clever",
-	},
-
-  linkinglighthouselaunch: {
+  },
+	linkinglighthouselaunch: {
 		num: -2,
 		accuracy: true,
 		basePower: 175,
@@ -83,8 +83,8 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		onPrepareHit(target, source, move) {
 			this.attrLastMove('[still]');
 			this.add('-anim', source, "Mountain Gale", target);
-      this.add('-anim', source, "Fusion Flare", target);
-      this.add('-anim', source, "Stoked Sparksurfer", target);
+			this.add('-anim', source, "Fusion Flare", target);
+			this.add('-anim', source, "Stoked Sparksurfer", target);
 		},
 		isZ: "bondiumz",
 		secondary:  {
@@ -103,5 +103,30 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		target: "normal",
 		type: "Normal",
 		contestType: "Cool",
+  },
+	froggybravesthewindandrain: {
+		num: -3,
+		accuracy: true,
+		basePower: 180,
+		category: "Special",
+		name: "Froggy Braves the Wind and Rain",
+		shortDesc: "Sets rain.",
+		pp: 1,
+		priority: 0,
+		flags: {},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Hurricane", target);
+			this.add('-anim', source, "Whirlpool", target);
+		},
+		isZ: "suwakiumz",
+		self: {
+			onHit(source) {
+				this.field.setWeather('raindance');
+			},
+		},
+	  	target: "normal",
+	  	type: "Normal",
+		contestType: "Cute",
 	},
 };
