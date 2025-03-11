dfd5fbd5ef22c50ab18994e5d00abac32d2136e3
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2025-03-10 21:25:49 -0600

    Create formats-data.ts

---
+++ data/mods/gen9evolutionproject/formats-data.ts
@@ -0,0 +1,14 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+
+	eevee: {tier: "Evo (NFE)"},
+	vaporeon: {tier: "Evo!"},
+	jolteon: {tier: "Evo!"},
+	flareon: {tier: "Evo!"},
+	espeon: {tier: "Evo!"},
+	umbreon: {tier: "Evo!"},
+	leafeon: {tier: "Evo!"},
+	glaceon: {tier: "Evo!"},
+	sylveon: {tier: "Evo!"},
+	aleon: {tier: "Evo!"},
+
+};
