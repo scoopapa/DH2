a997fc1503077b8da567ebee80cef7bb07d65d7e
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2025-03-10 21:04:45 -0600

    Create scripts.ts

---
+++ data/mods/gen9evolutionproject/scripts.ts
@@ -0,0 +1,48 @@
+export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
+	teambuilderConfig: {
+		excludeStandardTiers: true,
+    // sorting the teambuilder by slate / prompt
+		customTiers: ['S1P1', 'S1P2', 'S1P3', 'Evo 1', 'Canon', 'Evo (NFE)'],
+		customDoublesTiers: ['S1P1', 'S1P2', 'S1P3', 'Evo 1', 'Canon', 'Evo (NFE)'],
+	},
+	init() {
+		for (const id in this.dataCache.Pokedex) {
+			const newMon = this.dataCache.Pokedex[id];
+			if (!newMon || !newMon.copyData) continue; // weeding out Pokémon that aren't new
+			const copyData = this.dataCache.Pokedex[this.toID(newMon.copyData)];
+
+			if (!newMon.types && copyData.types) newMon.types = copyData.types;
+			if (!newMon.baseStats && copyData.baseStats) newMon.baseStats = copyData.baseStats;
+			if (!newMon.abilities && copyData.abilities) newMon.abilities = copyData.abilities;
+			if (!newMon.num && copyData.num) newMon.num = copyData.num * -1; // inverting the original's dex number
+			if (!newMon.genderRatio && copyData.genderRatio) newMon.genderRatio = copyData.genderRatio;
+			if (!newMon.heightm && copyData.heightm) newMon.heightm = copyData.heightm;
+			if (!newMon.weightkg && copyData.weightkg) newMon.weightkg = copyData.weightkg;
+			if (!newMon.color && copyData.color) newMon.color = copyData.color;
+			if (!newMon.eggGroups && copyData.eggGroups) newMon.eggGroups = copyData.eggGroups;
+
+			let copyMoves = newMon.copyData;
+			if (newMon.copyMoves) copyMoves = newMon.copyMoves;
+			if (copyMoves) {
+				if (!this.dataCache.Learnsets[id]) this.dataCache.Learnsets[id] = {learnset: {}}; // create a blank learnset entry so we don't need a learnsets file
+				const learnset = this.dataCache.Learnsets[this.toID(copyMoves)].learnset;
+        const notm = ['terablast', 'hiddenpower']; // certain moves don't count TMs
+				for (const moveid in learnset) {
+          this.modData('Learnsets', id).learnset[moveid] = learnset[moveid].filter(
+            (method) => !(method.includes('S') && (!notm.contains(move.id) || method.includes('M')))
+          );
+				}
+				if (newMon.movepoolAdditions) {
+					for (const move of newMon.movepoolAdditions) {
+						this.modData('Learnsets', this.toID(id)).learnset[this.toID(move)] = ["8M"];
+					}
+				}
+				if (newMon.movepoolDeletions) {
+					for (const move of newMon.movepoolDeletions) {
+						delete this.modData('Learnsets', this.toID(id)).learnset[this.toID(move)];
+					}
+				}
+			}
+		}
+	},
+};
