39ae033c09eb041f632f0c826ef7f13951b2d0bd
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2025-03-11 19:28:43 -0600

    Evolution Project 2

---
+++ data/mods/gen9evolutionproject/formats-data.ts
@@ -0,0 +1,283 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	// key: 'S1P1', 'S1P2', 'S1P3', 'Evo 1', 'Canon', 'Evo (NFE)'
+	// bans are handled separately so the tiers can be consistent between singles and VGC
+
+	// SLATE 1 PROMPT 1
+
+	crabrawler: {tier: "Evo (NFE)"},
+	crabominable: {tier: "Evo (NFE)"},
+	crablitzerd: {tier: "S1P1"},
+
+	croagunk: {tier: "Evo (NFE)"},
+	croagunkglacial: {tier: "Evo (NFE)"},
+	toxicroak: {tier: "Evo (NFE)"},
+	toxicroakglacial: {tier: "Evo (NFE)"},
+	mycecroak: {tier: "Evo 1"},
+	ycecroak: {tier: "S1P1"},
+
+	amaura: {tier: "Evo (NFE)"},
+	amauravolcanic: {tier: "Evo (NFE)"},
+	aurorus: {tier: "Canon"},
+	aurorusvolcanic: {tier: "S1P1"},
+
+	barboach: {tier: "Evo (NFE)"},
+	whiscash: {tier: "Evo (NFE)"},
+	whiscazu: {tier: "Evo 1"},
+	whiscazuhisui: {tier: "S1P1"},
+
+	munchlax: {tier: "Evo (NFE)"},
+	munchlaxkalos: {tier: "Evo (NFE)"},
+	snorlax: {tier: "Canon"},
+	hibearlax: {tier: "S1P1"},
+	hibearlaxberserk: {tier: "S1P1"},
+
+	meditite: {tier: "Evo (NFE)"},
+	medititevariant: {tier: "Evo (NFE)"},
+	medicham: {tier: "Canon"},
+	medichamvariant: {tier: "Evo (NFE)"},
+	meditendo: {tier: "S1P1"},
+
+	plankteenie: {tier: "Evo (NFE)"},
+	mareanie: {tier: "Evo (NFE)"},
+	mareaniedrifter: {tier: "Evo (NFE)"},
+	toxapex: {tier: "Canon"},
+	toxapexglacial: {tier: "S1P1"},
+	nemesyst: {tier: "S1P3"},
+
+	bronzor: {tier: "Evo (NFE)"},
+	bronzong: {tier: "Canon"},
+	bronzongnewyear: {tier: "S1P1"},
+
+	seel: {tier: "Evo (NFE)"},
+	seelvariant: {tier: "Evo (NFE)"},
+	dewgong: {tier: "Evo (NFE)"},
+	dewgongvariant: {tier: "Evo 1"},
+	narwhirl: {tier: "S1P1"},
+
+	bramblin: {tier: "Evo (NFE)"},
+	brambleghast: {tier: "Canon"},
+	brambleghastvariant: {tier: "S1P1"},
+
+	swinub: {tier: "Evo (NFE)"},
+	piloswine: {tier: "Evo (NFE)"},
+	mamoswine: {tier: "Canon"},
+	hieroswine: {tier: "S1P1"},
+
+	meowth: {tier: "Evo (NFE)"},
+	meowthalola: {tier: "Evo (NFE)"},
+	meowthgalar: {tier: "Evo (NFE)"},
+	persian: {tier: "Evo (NFE)"},
+	persianalola: {tier: "Canon"},
+	perrserker: {tier: "Canon"},
+	carnelion: {tier: "Evo 1"},
+	carnelionvariant: {tier: "S1P1"},
+
+	pyukumuku: {tier: "Canon"},
+	pyukumukuhisui: {tier: "S1P1"},
+
+	// SLATE 1 PROMPT 2
+
+	tynamo: {tier: "Evo (NFE)"},
+	eelektrik: {tier: "Evo (NFE)"},
+	eelektross: {tier: "Canon"},
+	eelektrossvariant: {tier: "S1P2"},
+
+	togepi: {tier: "Evo (NFE)"},
+	togepivariant: {tier: "Evo (NFE)"},
+	togetic: {tier: "Evo (NFE)"},
+	togeticvariant: {tier: "Evo (NFE)"},
+	togekiss: {tier: "Canon"},
+	togepries: {tier: "S1P2"},
+
+	drampa: {tier: "Canon"},
+	drampashifu: {tier: "S1P2"},
+
+	cutiefly: {tier: "Evo (NFE)"},
+	ribombee: {tier: "Canon"},
+	thumbulbee: {tier: "S1P2"},
+
+	porygon: {tier: "Evo (NFE)"},
+	porygon2: {tier: "Evo (NFE)"},
+	porygonz: {tier: "Canon"},
+	datagon2: {tier: "S1P2"}, // this is an NFE but it should still be easy to find
+	datagonz: {tier: "S1P2"},
+
+	noibat: {tier: "Evo (NFE)"},
+	noivern: {tier: "Canon"},
+	noivernvariant: {tier: "Evo 1"}, // banned but counts as canon for VGC? I guess?
+	noivernpaldea: {tier: "S1P2"},
+
+	mareep: {tier: "Evo (NFE)"},
+	flaaffy: {tier: "Evo (NFE)"},
+	ampharos: {tier: "Canon"},
+	ampharosurban: {tier: "S1P2"},
+	voltangent: {tier: "Evo 1"},
+
+	rellor: {tier: "Evo (NFE)"},
+	rabsca: {tier: "Evo (NFE)"},
+	repehk: {tier: "S1P2"},
+
+	spoink: {tier: "Evo (NFE)"},
+	spoinkalpine: {tier: "Evo (NFE)"},
+	grumpig: {tier: "Canon"},
+	grumpigalpine: {tier: "Evo (NFE)"},
+	alpignist: {tier: "S1P2"},
+
+	duskull: {tier: "Evo (NFE)"},
+	duskullvariant: {tier: "Evo (NFE)"},
+	dusclops: {tier: "Evo (NFE)"},
+	dusclopsvariant: {tier: "Evo (NFE)"},
+	dusknoir: {tier: "Canon"},
+	dusglow: {tier: "Evo 1"},
+	weltenschertz: {tier: "S1P2"},
+
+	hawlucha: {tier: "Canon"},
+	hawlucharudo: {tier: "S1P2"},
+
+	// SLATE 1 PROMPT 3
+
+	golett: {tier: "Evo (NFE)"},
+	golurk: {tier: "Canon"},
+	golligerent: {tier: "S1P3"},
+
+	binacle: {tier: "Evo (NFE)"},
+	barbaracle: {tier: "Evo (NFE)"},
+	horroracle: {tier: "S1P3"},
+
+	uxie: {tier: "Canon"},
+	uxieultra: {tier: "S1P3"},
+
+	litwick: {tier: "Evo (NFE)"},
+	clayby: {tier: "Evo (NFE)"},
+	lampent: {tier: "Evo (NFE)"},
+	glament: {tier: "Evo (NFE)"},
+	chandelure: {tier: "Canon"},
+	potthereal: {tier: "S1P3"},
+
+	skiddo: {tier: "Evo (NFE)"},
+	skiddohadean: {tier: "Evo (NFE)"},
+	gogoat: {tier: "Canon"},
+	gogoathadean: {tier: "S1P3"},
+
+	nosepass: {tier: "Evo (NFE)"},
+	probopass: {tier: "Canon"},
+	rootsnoot: {tier: "S1P3"},
+
+	rookidee: {tier: "Evo (NFE)"},
+	corvisquire: {tier: "Evo (NFE)"},
+	corviknight: {tier: "Canon"},
+	corviknightverdant: {tier: "S1P3"},
+
+	regirock: {tier: "Canon"},
+	regirockkanto: {tier: "S1P3"},
+
+	darumaka: {tier: "Evo (NFE)"},
+	darumakagalar: {tier: "Evo (NFE)"},
+	darmanitan: {tier: "Canon"},
+	darmanitansage: {tier: "S1P3"},
+	darmanitangalar: {tier: "Canon"},
+
+	// RETURNING / VANILLA
+
+	eevee: {tier: "Evo (NFE)"},
+	vaporeon: {tier: "Canon"},
+	jolteon: {tier: "Canon"},
+	flareon: {tier: "Canon"},
+	espeon: {tier: "Canon"},
+	umbreon: {tier: "Canon"},
+	leafeon: {tier: "Canon"},
+	glaceon: {tier: "Canon"},
+	sylveon: {tier: "Canon"},
+	aleon: {tier: "Evo 1"},
+
+	stantler: {tier: "Evo (NFE)"},
+	wyrdeer: {tier: "Canon"},
+
+	lillipup: {tier: "Evo (NFE)"},
+	herdier: {tier: "Evo (NFE)"},
+	stoutland: {tier: "Canon"},
+
+	qwilfish: {tier: "Evo (NFE)"},
+	qwilfishhisui: {tier: "Evo (NFE)"},
+	qwilfishkalos: {tier: "Evo (NFE)"},
+	qwilfishkaloszen: {tier: "Evo (NFE)"},
+	overqwil: {tier: "Canon"},
+	overchill: {tier: "Evo 1"},
+	overchillzen: {tier: "Evo 1"},
+
+	oddish: {tier: "Evo (NFE)"},
+	gloom: {tier: "Evo (NFE)"},
+	vileplume: {tier: "Canon"},
+	bellossom: {tier: "Canon"},
+
+	fletchling: {tier: "Evo (NFE)"},
+	fletchinder: {tier: "Evo (NFE)"},
+	talonflame: {tier: "Canon"},
+
+	rotom: {tier: "Canon"},
+	rotomheat: {tier: "Canon"},
+	rotomwash: {tier: "Canon"},
+	rotommow: {tier: "Canon"},
+	rotomfrost: {tier: "Canon"},
+	rotomfan: {tier: "Canon"},
+
+	togedemaru: {tier: "Canon"},
+
+	bruxish: {tier: "Canon"},
+
+	beldum: {tier: "Evo (NFE)"},
+	metang: {tier: "Evo (NFE)"},
+	metagross: {tier: "Canon"},
+
+	phanpy: {tier: "Evo (NFE)"},
+	donphan: {tier: "Canon"},
+
+	mantyke: {tier: "Evo (NFE)"},
+	mantykevariant: {tier: "Evo (NFE)"},
+	mantine: {tier: "Canon"},
+	mantinevariant: {tier: "Evo 1"},
+
+	tinkatink: {tier: "Evo (NFE)"},
+	tinkatuff: {tier: "Evo (NFE)"},
+	tinkaton: {tier: "Canon"},
+
+	paras: {tier: "Evo (NFE)"},
+	parasect: {tier: "Evo (NFE)"},
+	parascelium: {tier: "Evo 1"},
+
+	inkay: {tier: "Evo (NFE)"},
+	inkayalola: {tier: "Evo (NFE)"},
+	malamar: {tier: "Canon"},
+	malamaralola: {tier: "Evo 1"},
+
+	snom: {tier: "Evo (NFE)"},
+	snommaadowr: {tier: "Evo (NFE)"},
+	frosmoth: {tier: "Canon"},
+	frosmothmaadowr: {tier: "Evo 1"},
+
+	murkrow: {tier: "Evo (NFE)"},
+	honchkrow: {tier: "Canon"},
+
+	flabebe: {tier: "Evo (NFE)"},
+	floette: {tier: "Evo (NFE)"},
+	florges: {tier: "Canon"},
+
+	grimer: {tier: "Evo (NFE)"},
+	grimeralola: {tier: "Evo (NFE)"},
+	muk: {tier: "Canon"},
+	mukalola: {tier: "Canon"},
+
+	shroodle: {tier: "Evo (NFE)"},
+	grafaiai: {tier: "Canon"},
+
+	drilbur: {tier: "Evo (NFE)"},
+	drilburhisui: {tier: "Evo (NFE)"},
+	excadrill: {tier: "Canon"},
+	excadrillhisui: {tier: "Evo 1"},
+
+	fomantis: {tier: "Evo (NFE)"},
+	lurantis: {tier: "Canon"},
+
+	dedenne: {tier: "Evo (NFE)"},
+	dedellite: {tier: "Evo 1"},
+};
