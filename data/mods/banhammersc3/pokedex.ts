5289dfce24485822f27de064d64fe570213ef245
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-03-29 02:51:05 -0400

    Update pokedex.ts

--- data/mods/banhammersc3/pokedex.ts
+++ data/mods/banhammersc3/pokedex.ts
@@ -5,7 +5,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 		types: ["Grass", "Poison"],
 		genderRatio: {M: 0.875, F: 0.125},
 		baseStats: {hp: 45, atk: 49, def: 49, spa: 65, spd: 65, spe: 45},
-		abilities: {0: "Overgrow" },
+		abilities: {0: "Overgrow"},
 		heightm: 0.7,
 		weightkg: 6.9,
 		color: "Green",
