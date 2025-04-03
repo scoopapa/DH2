a8c00df573110ca3a8e26d9b4dee1bf9becfad96
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2025-04-03 16:06:54 -0700

    Update moves.ts

--- data/mods/ironfist/moves.ts
+++ data/mods/ironfist/moves.ts
@@ -76,9 +76,9 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 			this.add('-anim', pokemon, "G-Max Steelsurge", target);
 		},
 		onEffectiveness(typeMod, target, type) {
-		    if(target.baseSpecies.types[0] === type) return 1;
-			else return 0;
-		},
+            if (target.getTypes()[0] === type || target.terastallized) return 1;
+            else return 0;
+        },
 		target: "normal",
 		type: "Steel",
 		shortDesc: "Always super-effective.",
