152010c90b94b894bd740c62ca27be1c1ffb4cce
Lysio <lukas.moncorge@gmail.com>
2025-04-13 21:45:46 +0200

    hehe

--- data/mods/glacemons/moves.ts
+++ data/mods/glacemons/moves.ts
@@ -2060,7 +2060,7 @@ export const Moves: { [moveid: string]: ModdedMoveData; } = {
         flags: {protect: 1, mirror: 1, metronome: 1, bullet: 1},
         secondary: null,
         target: "normal",
-        onPrepareHit(target, pokemon, sourceEffect) {
+        onAfterHit(target, pokemon, sourceEffect) {
             if (target !== "normal") return
             this.useMove("Ki Blast", pokemon, "self", sourceEffect);
         },
