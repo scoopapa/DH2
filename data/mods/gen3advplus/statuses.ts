b68876b83871217884abf4e69920938f2122fd00
Soul-8691 <peterswanton191@bellsouth.net>
2025-04-20 13:21:06 -0400

    Update my 2 Gen 3 Solomods to use gen3 source files

--- data/mods/gen3advplus/statuses.ts
+++ data/mods/gen3advplus/statuses.ts
@@ -9,13 +9,13 @@ export const Statuses: {[k: string]: ModdedPureEffectData} = {
 				this.add('-status', target, 'slp');
 			}
 			// 1-4 turns
-			this.effectData.time = this.random(2, 6);
+			this.effectState.time = this.random(2, 6);
 			// Turns spent using Sleep Talk/Snore immediately before switching out while asleep
-			this.effectData.skippedTime = 0;
+			this.effectState.skippedTime = 0;
 		},
 		onSwitchIn(target) {
-			this.effectData.time += this.effectData.skippedTime;
-			this.effectData.skippedTime = 0;
+			this.effectState.time += this.effectState.skippedTime;
+			this.effectState.skippedTime = 0;
 		},
 		onBeforeMovePriority: 10,
 		onBeforeMove(pokemon, target, move) {
@@ -29,10 +29,10 @@ export const Statuses: {[k: string]: ModdedPureEffectData} = {
 			}
 			this.add('cant', pokemon, 'slp');
 			if (move.sleepUsable) {
-				this.effectData.skippedTime++;
+				this.effectState.skippedTime++;
 				return;
 			}
-			this.effectData.skippedTime = 0;
+			this.effectState.skippedTime = 0;
 			return false;
 		},
 	},
@@ -40,7 +40,7 @@ export const Statuses: {[k: string]: ModdedPureEffectData} = {
 		inherit: true,
 		onHit(target, source, move) {
 			// don't count Hidden Power or Weather Ball as Fire-type
-			if (move.thawsTarget || this.dex.getMove(move.id).type === 'Fire' && move.category !== 'Status') {
+			if (move.thawsTarget || this.dex.moves.get(move.id).type === 'Fire' && move.category !== 'Status') {
 				target.cureStatus();
 			}
 		},
