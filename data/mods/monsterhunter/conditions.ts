5a9adf4bb7e622ab604d4b69f7415481fc74962c
KnivesTotality <olivesparkly@gmail.com>
2025-04-25 19:33:02 -0400

    Update conditions.ts

--- data/mods/monsterhunter/conditions.ts
+++ data/mods/monsterhunter/conditions.ts
@@ -1,19 +1,8 @@
 export const Conditions: { [k: string]: ConditionData; } = {
 	frz: {
 		inherit: true,
-		name: 'fsb',
-		start: "  [Pokemon] was chilled!",
-		alreadyStarted: "  [POKEMON] is already chilled!",
-		end: "  [POKEMON] warmed up!",
-		endFromItem: "  [POKEMON]'s [ITEM] warmed it up!",
-		endFromMove: "  [POKEMON]'s [MOVE] warmed it up!",
-		cant: "[POKEMON] is chilled!",
-		effectType: 'Status',
-		onStart(target, source, sourceEffect) {
-			this.add('-start', target, 'Frostbite', '[silent]');
-			this.add('-message', `${target.name} was frostbitten!`);
-		},
-		onResidualOrder: 9,
+		onBeforeMove(pokemon, target, move) {},
+		onResidualOrder: 10,
 		onResidual(pokemon) {
 			this.damage(pokemon.baseMaxhp / 16);
 		},
