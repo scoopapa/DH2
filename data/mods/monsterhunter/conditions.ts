d5ccf903e7b989654f511a8ebaaf104fda8b17cb
KnivesTotality <olivesparkly@gmail.com>
2025-04-25 19:02:05 -0400

    yay

--- data/mods/monsterhunter/conditions.ts
+++ data/mods/monsterhunter/conditions.ts
@@ -1,11 +1,25 @@
 export const Conditions: { [k: string]: ConditionData; } = {
 	frz: {
 		inherit: true,
-		onBeforeMove(pokemon, target, move) {},
-		// Damage reduction is handled directly in the sim/battle.js damage function
-		onResidualOrder: 10,
+		name: 'fsb',
+		start: "  [Pokemon] was chilled!",
+		alreadyStarted: "  [POKEMON] is already chilled!",
+		end: "  [POKEMON] warmed up!",
+		endFromItem: "  [POKEMON]'s [ITEM] warmed it up!",
+		endFromMove: "  [POKEMON]'s [MOVE] warmed it up!",
+		cant: "[POKEMON] is chilled!",
+		effectType: 'Status',
+		onStart(target, source, sourceEffect) {
+			this.add('-start', target, 'Frostbite', '[silent]');
+			this.add('-message', `${target.name} was frostbitten!`);
+		},
+		onResidualOrder: 9,
 		onResidual(pokemon) {
 			this.damage(pokemon.baseMaxhp / 16);
 		},
+		onModifySpA(spa, pokemon) {
+			return this.chainModify(0.5);
+		},
+	},
 	},
 };
