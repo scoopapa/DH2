0c47e341ff64c00c4df6d83f6d4af8e234130081
hidin <114875246+hidinflames@users.noreply.github.com>
2025-04-30 18:29:09 -0400

    Update items.ts

--- data/mods/glacemons/items.ts
+++ data/mods/glacemons/items.ts
@@ -2020,4 +2020,5 @@ export const Items: { [k: string]: ModdedItemData; } = {
 		desc: "On switch-in, the holder uses a 40 BP Physical move with the holder's primary type, Special if SpAtk > Atk. Single use.",
 		shortDesc: "On switch-in: 40 BP move based on primary type and stronger attack. Single use.",
 	},
+	}
 };
