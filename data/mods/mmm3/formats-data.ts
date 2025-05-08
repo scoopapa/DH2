6416831d7d0d816d62365d329050c72ebf188e4e
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2025-05-07 17:29:23 -0700

    micromafia 3

---
+++ data/mods/mmm3/formats-data.ts
@@ -0,0 +1,50 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	cyclizar: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+	toxapex: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+	electivire: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+	vileplume: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+	metagross: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+	stunfiskgalar: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+	moltresgalar: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+	ironmoth: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+	spinarak: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+	araquanid: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+	galvantula: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+	hatterene: {
+		tier: "MMM3",
+		doublesTier: "DMMM3",
+	},
+};
