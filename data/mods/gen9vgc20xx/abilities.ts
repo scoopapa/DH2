dcbcd320b0caf36a924a257b82ed208298d367d6
BlueRaySmogon <longnguyen93@gmx.de>
2025-05-16 01:42:02 +0200

    Update abilities.ts

--- data/mods/gen9vgc20xx/abilities.ts
+++ data/mods/gen9vgc20xx/abilities.ts
@@ -209,7 +209,7 @@ export const Abilities: { [abilityid: string]: ModdedAbilityData } = {
 		onDamage(damage, target, source, effect) {
 			if (effect && effect.effectType !== 'Move') {
 				const ally = target.side.active.find(ally => ally && ally !== target && !ally.fainted);
-				if (ally) {
+				if (ally && !ally.hasAbility('selfish')) {
 					this.add('-ability', target, 'Selfish');
 					this.add('-message', `${target.name}'s Selfish redirects the damage to ${ally.name}!`);
 					this.damage(damage, ally, source, effect);
@@ -591,7 +591,7 @@ export const Abilities: { [abilityid: string]: ModdedAbilityData } = {
 	//
 	cutecharm: {
 		shortDesc: "50% damage reduction if move's type = user's type.",
-		onModifyDamage(damage, source, target, move) {
+		onSourceModifyDamage(damage, source, target, move) {
 			// Check if the move's type matches the defender's type
 			if (target.hasType(move.type)) {
 				this.debug('Cute Charm reducing damage due to type match');
