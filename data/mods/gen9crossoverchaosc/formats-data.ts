6c5e3c64421520e6c3844947d478dbf3452cc776
Rasdanation <Rasdanation@gmail.com>
2025-05-19 19:00:04 -0400

    CC Slate 9

--- data/mods/gen9crossoverchaosc/formats-data.ts
+++ data/mods/gen9crossoverchaosc/formats-data.ts
@@ -99,4 +99,16 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		tier: "CC OU",
 		doublesTier: "DCC OU",
 	},
+	radiance: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
+	sophie: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
+	ragman: {
+		tier: "CC OU",
+		doublesTier: "DCC OU",
+	},
 };
