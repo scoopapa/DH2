ae129e49c673496d5d481f20f1ddbc0384181ab1
Emikatana <itsemi48@gmail.com>
2025-05-27 11:32:51 -0400

    order up fix 9

--- data/mods/pmcm/moves.ts
+++ data/mods/pmcm/moves.ts
@@ -568,7 +568,7 @@ export const Moves: { [moveid: string]: ModdedMoveData } = {
 			},
 			onFaint(pokemon) {
 				this.add('-message', `switchin in?`);
-				this.choice.push({ choice: 'runSwitch', pokemon: 'dondozo' })
+				this.choice({ choice: 'runSwitch', pokemon: 'dondozo' })
 			},
 			// forces Dondozo in when a mon faints while orderup side condition is active (which can only happen when the eaten mon faints
 			//onFaint(pokemon) {
