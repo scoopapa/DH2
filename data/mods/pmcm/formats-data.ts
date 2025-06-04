6563e12239d9fdbe04d4ed8c614d362286a130aa
Emikatana <itsemi48@gmail.com>
2025-06-04 12:08:44 -0400

    salamence + urshifu

--- data/mods/pmcm/formats-data.ts
+++ data/mods/pmcm/formats-data.ts
@@ -63,5 +63,9 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	golurk: {tier: "PMCM"},
 	meowscarada: {tier: "PMCM"},
 	infernape: {tier: "PMCM"},
+	salamence: {tier: "PMCM"},
+	salamencemega: {tier: "PMCM"},
+	urshifu: {tier: "PMCM"},
+	urshifurapidstrike: {tier: "PMCM"},
 	
 };
