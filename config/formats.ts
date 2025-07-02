da2884960bf85902b0353e9673756101a626e806
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2025-07-01 19:28:48 -0700

    tpost

--- config/formats.ts
+++ config/formats.ts
@@ -1665,6 +1665,12 @@ export const Formats: FormatList = [
 		},
 	},
 	{
+		name: "[Gen 9] Trading Post",
+		mod: 'tradingpost',
+		ruleset: ['Standard', 'Sleep Moves Clause', '!Sleep Clause Mod', '!Evasion Items Clause'],
+		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'Baton Pass', 'Last Respects', 'Shed Tail'],
+	},
+	{
 		name: "[Gen 9] Trainer Support",
 		mod: 'trainersupport',
 		ruleset: ['Standard', 'Trainer Support Rule', 'Sleep Moves Clause', '!Sleep Clause Mod'],
--- data/mods/tradingpost/conditions.ts
+++ data/mods/tradingpost/conditions.ts
@@ -1,3 +1,69 @@
 export const Conditions: {[id: string]: ModdedConditionData} = {
-
+	deoxys: {
+		name: 'deoxys',
+		duration: 2,
+		onStart(target, source, sourceEffect) {
+			this.add('-start', target, 'Deoxys');
+		},
+		onModifyAtk(atk, pokemon) {
+			return Math.trunc(Math.trunc(2 * 150 + pokemon.set.ivs['atk'] + Math.trunc(pokemon.set.evs['atk'] / 4)) * pokemon.set.level / 100 + 5);
+		},
+		onModifySpA(spa, pokemon) {
+			return Math.trunc(Math.trunc(2 * 150 + pokemon.set.ivs['spa'] + Math.trunc(pokemon.set.evs['spa'] / 4)) * pokemon.set.level / 100 + 5);
+		},
+		onModifySpe(spe, pokemon) {
+			return Math.trunc(Math.trunc(2 * 150 + pokemon.set.ivs['spe'] + Math.trunc(pokemon.set.evs['spe'] / 4)) * pokemon.set.level / 100 + 5);
+		},
+		onEnd(target) {
+			this.add('-end', pokemon, 'Deoxys');
+		},
+	},
+	deoxysatk: {
+		name: 'deoxysatk',
+		duration: 2,
+		onStart(target, source, sourceEffect) {
+			this.add('-start', target, 'Deoxys-Attack');
+		},
+		onModifyAtk(atk, pokemon) {
+			return Math.trunc(Math.trunc(2 * 180 + pokemon.set.ivs['atk'] + Math.trunc(pokemon.set.evs['atk'] / 4)) * pokemon.set.level / 100 + 5);
+		},
+		onModifySpA(spa, pokemon) {
+			return Math.trunc(Math.trunc(2 * 180 + pokemon.set.ivs['spa'] + Math.trunc(pokemon.set.evs['spa'] / 4)) * pokemon.set.level / 100 + 5);
+		},
+		onEnd(target) {
+			this.add('-end', pokemon, 'Deoxys-Attack');
+		},
+	},
+	deoxysdef: {
+		name: 'deoxysdef',
+		duration: 2,
+		onStart(target, source, sourceEffect) {
+			this.add('-start', target, 'Deoxys-Defense');
+		},
+		onModifyDef(def, pokemon) {
+			return Math.trunc(Math.trunc(2 * 160 + pokemon.set.ivs['def'] + Math.trunc(pokemon.set.evs['def'] / 4)) * pokemon.set.level / 100 + 5);
+		},
+		onModifySpD(spd, pokemon) {
+			return Math.trunc(Math.trunc(2 * 160 + pokemon.set.ivs['spd'] + Math.trunc(pokemon.set.evs['spd'] / 4)) * pokemon.set.level / 100 + 5);
+		},
+		onEnd(target) {
+			this.add('-end', pokemon, 'Deoxys-Defense');
+		},
+	},
+	deoxysspe: {
+		name: 'deoxysspe',
+		duration: 2,
+		onStart(target, source, sourceEffect) {
+			this.add('-start', target, 'Deoxys-Speed');
+		},
+		onModifyDef(def, pokemon) {
+			return Math.trunc(Math.trunc(2 * 160 + pokemon.set.ivs['def'] + Math.trunc(pokemon.set.evs['def'] / 4)) * pokemon.set.level / 100 + 5);
+		},
+		onModifySpe(spe, pokemon) {
+			return Math.trunc(Math.trunc(2 * 180 + pokemon.set.ivs['spe'] + Math.trunc(pokemon.set.evs['spe'] / 4)) * pokemon.set.level / 100 + 5);
+		},
+		onEnd(target) {
+			this.add('-end', pokemon, 'Deoxys-Speed');
+		},
+	},
 };
