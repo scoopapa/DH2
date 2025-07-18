740f167079f50da5a3aa8119de38e9360fbee859
Nbrimson <78097164+Nbrimson@users.noreply.github.com>
2025-07-18 14:08:27 +0100

    Let's Go Tamagotchi! Paradise Update!
    ...Of course, this also brought the star back into KiraKiraDream, so that had to be re-fixed too....

--- data/mods/tamagotchi/learnsets.ts
+++ data/mods/tamagotchi/learnsets.ts
@@ -1582,7 +1582,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 	},
 	memetchi: {
 		learnset: {
-			kirakira☆dream: ["9L1"],
+			kirakiradream: ["9L1"],
 			dazzlinggleam: ["9L1"],
 			playrough: ["9L1"],
 			tidyup: ["9L1"],
@@ -4751,7 +4751,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 	},
 	himespetchi: {
 		learnset: {
-			kirakira☆dream: ["9L1"],
+			kirakiradream: ["9L1"],
 			alienlaser: ["9L1"],
 			dazzlinggleam: ["9L1"],
 			playrough: ["9L1"],
@@ -4778,7 +4778,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 	},
 	yumemitchi: {
 		learnset: {
-			kirakira☆dream: ["9L1"],
+			kirakiradream: ["9L1"],
 			yumekirabag: ["9L1"],
 			dazzlinggleam: ["9L1"],
 			drainingkiss: ["9L1"],
@@ -4806,7 +4806,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 	},
 	kiraritchi: {
 		learnset: {
-			kirakira☆dream: ["9L1"],
+			kirakiradream: ["9L1"],
 			yumekirabag: ["9L1"],
 			dazzlinggleam: ["9L1"],
 			playrough: ["9L1"],
@@ -5565,7 +5565,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 	},
 	pianitchi: {
 		learnset: {
-			kirakira☆dream: ["9L1"],
+			kirakiradream: ["9L1"],
 			smapi: ["9L1"],
 			dazzlinggleam: ["9L1"],
 			psychic: ["9L1"],
@@ -5724,7 +5724,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 	},
 	coffretchi: {
 		learnset: {
-			kirakira☆dream: ["9L1"],
+			kirakiradream: ["9L1"],
 			moripakucoffretmarch: ["9L1"],
 			dreamcoffretpalette: ["9L1"],
 			dazzlinggleam: ["9L1"],
