d70d0d094f5220e055b897b0d518d61275b7bea2
KnivesTotality <olivesparkly@gmail.com>
2025-07-23 17:48:00 -0400

    Update conditions.ts

--- data/mods/monsterhunter/conditions.ts
+++ data/mods/monsterhunter/conditions.ts
@@ -49,9 +49,8 @@ export const Conditions: { [k: string]: ConditionData; } = {
 	},
 	par: {
         inherit: true,
-		onStart(target, source, sourceEffect, pokemon) {
+		onStart(target, source, sourceEffect) {
 			this.add('-message', `${target.name} is Paralyzed! Speed is halved! (Full Paralysis is removed)`);
-			if (pokemon.static === undefined) pokemon.static = 0;
 			if (sourceEffect && sourceEffect.effectType === 'Ability') {
 				this.add('-status', target, 'par', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
 			} else {
@@ -59,6 +58,7 @@ export const Conditions: { [k: string]: ConditionData; } = {
 			}
 		},
 		onResidual(pokemon) {
+			if (pokemon.static === undefined) pokemon.static = 0;
 			pokemon.static ++;
 			if (pokemon.static >= 3) {
 				this.add('-message', `${pokemon.name} has too much static!`);
