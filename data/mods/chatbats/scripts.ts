d00a2a8a48f74b70032f06600ec4445f67ce3a97
Emikatana <itsemi48@gmail.com>
2025-07-24 16:07:31 +0200

    PMCM > ChatBats

--- data/mods/chatbats/scripts.ts
+++ data/mods/chatbats/scripts.ts
@@ -2,7 +2,7 @@ export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
 	teambuilderConfig: {
 		excludeStandardTiers: true,
-		customTiers: ["PMCM"],
+		customTiers: ["ChatBats"],
 	},
 
 	init() {
