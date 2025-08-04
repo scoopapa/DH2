d087f188986849fc0160e8f69b19cc2ca30db6ab
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-08-03 22:35:45 -0400

    Create formats-data.ts

---
+++ data/mods/smashmodsbrawl/formats-data.ts
@@ -0,0 +1,23 @@
+export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {
+	metaknight: {
+		tier: "SSB",
+	},
+	ditto: {
+		tier: "SSB",
+	},
+	zapdos: {
+		tier: "SSB",
+	},
+	scorchingkiln: {
+		tier: "SSB",
+	},
+	delibird: {
+		tier: "SSB",
+	},
+	delibirdmega: {
+		tier: "SSB",
+	},
+	bigcrammer: {
+		tier: "SSB",
+	},
+};
