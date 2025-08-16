1adaa88232d45b47a1d354ccefb591d192e13720
Lysio <lukas.moncorge@gmail.com>
2025-08-16 14:47:49 +0200

    maybe I just missed that

--- data/mods/agoldenexperience/scripts.ts
+++ data/mods/agoldenexperience/scripts.ts
@@ -31,6 +31,32 @@ export const Scripts: ModdedBattleScriptsData = {
   
       return true;
     }
+    setAbility(ability: string | Ability, source?: Pokemon | null, isFromFormeChange = false, isTransform = false) {
+      if (!this.hp) return false;
+      if (typeof ability === 'string') ability = this.battle.dex.abilities.get(ability);
+      const oldAbility = this.ability;
+      if (!isFromFormeChange) {
+        if (ability.flags['cantsuppress'] || this.getAbility().flags['cantsuppress']) return false;
+      }
+      if (!isFromFormeChange && !isTransform) {
+        const setAbilityEvent: boolean | null = this.battle.runEvent('SetAbility', this, source, this.battle.effect, ability);
+        if (!setAbilityEvent) return setAbilityEvent;
+      }
+      this.battle.singleEvent('End', this.battle.dex.abilities.get(oldAbility), this.abilityState, this, source);
+      if (this.battle.effect && this.battle.effect.effectType === 'Move' && !isFromFormeChange) {
+        this.battle.add(
+          '-endability', this, this.battle.dex.abilities.get(oldAbility),
+          `[from] move: ${this.battle.dex.moves.get(this.battle.effect.id)}`
+        );
+      }
+      this.ability = ability.id;
+      this.abilityState = {id: ability.id, target: this};
+      if (ability.id && this.battle.gen > 3 &&
+        (!isTransform || oldAbility !== ability.id || this.battle.gen <= 4)) {
+        this.battle.singleEvent('Start', ability, this.abilityState, this, source);
+      }
+      return oldAbility;
+    },
   },
   actions: {
     canMegaEvo(pokemon) {
