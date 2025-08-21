21a78e4649b16efaa8273173c904837efcb9c2a7
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-08-21 03:53:34 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1670,7 +1670,7 @@ export const Formats: FormatList = [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/3768342/">Super Smash Mods Brawl on Smogon Forums</a>`,
 		],
 		teambuilderFormat: "National Dex",
-		ruleset: ['Standard NatDex', 'Sleep Moves Clause', /*'Z-Move Clause',*/ 'Data Mod', 'Mega Data Mod'],
+		ruleset: ['Standard NatDex', 'Sleep Moves Clause', 'Terastal Clause', /*'Z-Move Clause',*/ 'Data Mod', 'Mega Data Mod'],
 		banlist: [
 			'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Razor Fang', 'Baton Pass', 'Last Respects', 'Shed Tail',
 			'Normalium Z', 'Fairium Z', 'Fightinium Z', 'Firium Z', 'Flyinium Z', 'Darkinium Z', 'Dragonium Z', 'Buginium Z', 'Waterium Z', 'Electrium Z',
