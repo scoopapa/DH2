4ede5f86846d39afe920d3bac568131399c0fce7
JumpheartReal <129568774+JumpheartReal@users.noreply.github.com>
2025-08-24 16:10:14 -0500

    forgot the format whoops

--- config/formats.ts
+++ config/formats.ts
@@ -3142,6 +3142,25 @@ export const Formats: FormatList = [
 			}
 		}
   },
+  {
+		name: "[Gen 9] Double Trouble",
+		mod: 'doubletrouble',
+		desc: `A Fakemons meta made by a few friends`,
+		ruleset: ['Standard NatDex', 'Data Mod', 'Terastal Clause', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod'],
+		teambuilderFormat: 'National Dex',
+		banlist: ['Baton Pass'],
+		onValidateTeam(team, format) {
+			/**@type {{[k: string]: true}}*/
+			let speciesTable = {};
+			let allowedTiers = ['DT OU', 'DT NFE'];
+			for (const set of team) {
+				let template = this.dex.species.get(set.species);
+				if (!allowedTiers.includes(template.tier)) {
+					return [set.species + ' is not legal in Double Trouble.'];
+				}
+			}
+		},
+	},
 	{
 		name: "[Gen 9] Do Not Use",
 		desc: [
