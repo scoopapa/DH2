361b64cac927f5059f4c7495c40e0420e0226749
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-08-28 11:42:21 -0400

    Update scripts.ts

--- data/mods/blankcanvas/scripts.ts
+++ data/mods/blankcanvas/scripts.ts
@@ -2,6 +2,6 @@ export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
 	teambuilderConfig: {
 		excludeStandardTiers: true,
-		customTiers: ['BC OU', 'BC UU'],
+		customTiers: ['BC OU', 'BC UU', 'BC RU'],
 	},
 };
