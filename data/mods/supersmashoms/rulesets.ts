a03a23e2396a6fa1fbbc3717d97dc7a8b69e9fdf
Lysio <lukas.moncorge@gmail.com>
2025-08-28 14:14:08 +0200

    print debug lesgo

--- data/mods/supersmashoms/rulesets.ts
+++ data/mods/supersmashoms/rulesets.ts
@@ -93,6 +93,7 @@ export const Rulesets: {[k: string]: ModdedFormatData} = { // WIP
 			// Convergence
 			const curSpecies = this.dex.species.get(set.species);
 			let ability = set.ability;
+			console.log("Ability is " + ability + "; species has " + curSpecies.abilities);
 			if (!ConvList.includes(curSpecies.name) && !AAAList.includes(curSpecies.name) && !Object.values(curSpecies.abilities).includes(ability.name)) return [`${curSpecies.name} cannot have ${this.dex.abilities.get(set.ability).name}.`];
 			const obtainableAbilityPool = new Set<string>();
 			const matchingSpecies = this.dex.species.all()
