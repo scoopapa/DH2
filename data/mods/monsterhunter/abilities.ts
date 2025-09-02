1bd437413591ce7d2af87a2ed89e15e5197f0745
KnivesTotality <olivesparkly@gmail.com>
2025-09-01 20:24:44 -0400

    MH

--- data/mods/monsterhunter/abilities.ts
+++ data/mods/monsterhunter/abilities.ts
@@ -1363,7 +1363,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		name: "Ambush",
 	},
 	elderseal: {
-		onModifyAtk(target, pokemon, tags) {
+		onModifyAtk(atk, target, pokemon, tags) {
 			if (target.tags.includes("Elder Dragon")) {
 				return this.chainModify(1.5);
 				this.add('-ability', pokemon, 'placeholder');
