31b705d34c1568ebf836711777bedd83eeaed5f6
Rasdanation <Rasdanation@gmail.com>
2025-09-09 12:24:36 +0200

    change 2

--- config/formats.ts
+++ config/formats.ts
@@ -200,7 +200,7 @@ export const Formats: FormatList = [
 		onValidateTeam(team, format) {
 			/**@type {{[k: string]: true}}*/
 			let speciesTable = {};
-			let allowedTiers = ['BC RU'];
+			let allowedTiers = ['BC C'];
 			for (const set of team) {
 				let template = this.dex.species.get(set.species);
 				if (!allowedTiers.includes(template.tier)) {
