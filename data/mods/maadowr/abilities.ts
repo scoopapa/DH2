9ef11d95a50cf25487633a96a325a6ff87cb69a9
BlueRaySmogon <longnguyen93@gmx.de>
2025-09-25 20:04:07 +0200

    Update abilities.ts

--- data/mods/maadowr/abilities.ts
+++ data/mods/maadowr/abilities.ts
@@ -662,7 +662,7 @@ export const Abilities: { [abilityid: string]: ModdedAbilityData } = {
 					let itemRestored = false;
 	
 					if (pokemon.side && Array.isArray(pokemon.side.pokemon)) {
-						for (const ally of pokemon.side.pokemon) {
+						for (const ally of pokemon.side.active) {
 							if (ally && !ally.item) {
 								this.actions.useMove('Recycle', ally);
 								itemRestored = true;
@@ -688,7 +688,7 @@ export const Abilities: { [abilityid: string]: ModdedAbilityData } = {
 			this.add('-ability', pokemon, 'Rewind');
 	
 			if (pokemon.side && Array.isArray(pokemon.side.pokemon)) {
-				for (const ally of pokemon.side.pokemon) {
+				for (const ally of pokemon.side.active) {
 					if (ally && !ally.item) {
 						this.actions.useMove('Recycle', ally);
 						itemRestored = true;
@@ -1657,3 +1657,4 @@ export const Abilities: { [abilityid: string]: ModdedAbilityData } = {
 	},
 	
 };
+
