17464e57ee18e7a9cdb1db8ce67a427c2dd913a4
Lysio4 <lukas.moncorge@proton.me>
2025-09-25 20:31:41 +0200

    Iron Fist last test

--- data/mods/ironfist/scripts.ts
+++ data/mods/ironfist/scripts.ts
@@ -16,12 +16,13 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 				this.modData('Learnsets', this.toID(id)).learnset.mewing = ["9L1"];
 				this.modData('Learnsets', this.toID(id)).learnset.epicbeam = ["9L1"];
 				this.modData('Learnsets', this.toID(id)).learnset.bigbash = ["9L1"];
-				/*if (!id.types.includes('Water') && !id.types.includes('Steel')) {
+				if (!id.getTypes.includes('Water') && !id.getTypes.includes('Steel')) {
 					this.modData('Learnsets', this.toID(id)).learnset.fisheater = ["9L1"];
-				}*/
+				}
 				if (id.diamondhand) {
 					this.modData('Learnsets', this.toID(id)).learnset.diamondhand = ["9L1"];
 				}
+				console.log(id, id.gen);
 				if (id.hoenn || id.gen === 3) {
 					this.modData('Learnsets', this.toID(id)).learnset.hoenn = ["9L1"];
 				}
