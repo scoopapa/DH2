5df42287f856ca8f0669230753da15f05993a619
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-10-17 05:04:50 -0400

    Update moves.ts

--- data/mods/plza/moves.ts
+++ data/mods/plza/moves.ts
@@ -22,4 +22,48 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		zMove: {basePower: 140},
 		contestType: "Tough",
 	},
+	healblock: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	poweruppunch: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	landswrath: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	thousandwaves: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	thousandarrows: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	coreenforcer: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	trickortreat: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	kingsshield: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	geomancy: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	oblivionwing: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	lightofruin: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 };
