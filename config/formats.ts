0bab3a10ca31f54157c4314f38a393e4ea628033
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-10-17 04:09:32 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -6264,6 +6264,47 @@ export const Formats: FormatList = [
 		ruleset: ['Data Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Endless Battle Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Z-Move Clause', 'Max Level = 1'],
 	},
 	{
+		name: "[Gen 9] Speculative Legends Z-A OU",
+		mod: 'plza',
+		ruleset: ['Standard', 'Terastal Clause', 'Sleep Moves Clause', '!Sleep Clause Mod', 'Mega Data Mod'],
+		banlist: [
+			'Arena Trap', 'Moody', 'Shadow Tag', 'King\'s Rock', 'Baton Pass', 'Last Respects', 'Shed Tail',
+			'Alakazite', 'Gengarite', 'Salamencite', 'Metagrossite', 'Lucarionite',
+		],
+		onValidateTeam(team, format) {
+			/**@type {{[k: string]: true}}*/
+			let speciesTable = {};
+			let allowedTiers = ['FE ZA', 'NFE ZA', 'LC ZA'];
+			for (const set of team) {
+				let template = this.dex.species.get(set.species);
+				if (!allowedTiers.includes(template.tier)) {
+					return [set.species + ' is not legal in PLZA.'];
+				}
+			}
+		},
+  	},
+	{
+		name: "[Gen 9] Speculative Legends Z-A VGC",
+		gameType: 'doubles',
+		ruleset: ['Flat Rules', '!! Adjust Level = 50', 'Min Source Gen = 9', 'VGC Timer', 'Open Team Sheets', 'Mega Data Mod', 'Terastal Clause'],
+		banlist: [
+			'Mewtwo', 'Xerneas', 'Yveltal', 'Zygarde', 'Diancie', 'Hoopa', 'Volcanion',
+		],
+		mod: 'plza',
+		bestOfDefault: true,
+		onValidateTeam(team, format) {
+			/**@type {{[k: string]: true}}*/
+			let speciesTable = {};
+			let allowedTiers = ['FE ZA', 'NFE ZA', 'LC ZA', 'Uber ZA'];
+			for (const set of team) {
+				let template = this.dex.species.get(set.species);
+				if (!allowedTiers.includes(template.tier)) {
+					return [set.species + ' is not legal in PLZA.'];
+				}
+			}
+		},
+	},
+	{
 		name: "[Gen 6] TPDP Open",
 		mod: 'tpdp',
 		debug: true,
