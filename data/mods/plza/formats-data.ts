dac43931e3fcb9b07c94683d9119ffab50b3cea3
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-10-17 15:06:22 -0400

    Update formats-data.ts

--- data/mods/plza/formats-data.ts
+++ data/mods/plza/formats-data.ts
@@ -881,7 +881,25 @@ export const FormatsData: import('../../../sim/dex-species').SpeciesFormatsDataT
 	pumpkaboo: {
 		tier: "LC ZA",
 	},
+	pumpkaboosmall: {
+		tier: "LC ZA",
+	},
+	pumpkaboolarge: {
+		tier: "LC ZA",
+	},
+	pumpkaboosuper: {
+		tier: "LC ZA",
+	},
 	gourgeist: {
+		tier: "FE ZA",
+	},
+	gourgeistsmall: {
+		tier: "FE ZA",
+	},
+	gourgeistlarge: {
+		tier: "FE ZA",
+	},
+	gourgeistsuper: {
 		tier: "FE ZA",
 	},
 	bergmite: {
