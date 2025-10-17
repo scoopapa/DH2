873bae16adfe3de178eb8e18399167eec5fcfe8e
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-10-17 05:21:08 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -6305,6 +6305,36 @@ export const Formats: FormatList = [
 		},
 	},
 	{
+		name: "[Gen 9] Speculative Legends Z-A FFA Royale",
+		gameType: 'freeforall',
+		mod: 'plza',
+		ruleset: ['Standard', 'Terastal Clause', 'Sleep Moves Clause', '!Sleep Clause Mod', 'Mega Data Mod'],
+		banlist: [
+			'Arena Trap', 'Moody', 'Shadow Tag', 'King\'s Rock', 'Baton Pass', 'Last Respects', 'Shed Tail',
+			'Alakazite', 'Gengarite', 'Salamencite', 'Metagrossite', 'Lucarionite', 'Power Construct',
+		],
+		onValidateTeam(team, format) {
+			/**@type {{[k: string]: true}}*/
+			let speciesTable = {};
+			let allowedTiers = ['FE ZA', 'NFE ZA', 'LC ZA'];
+			for (const set of team) {
+				let template = this.dex.species.get(set.species);
+				if (!allowedTiers.includes(template.tier)) {
+					return [set.species + ' is not legal in PLZA.'];
+				}
+			}
+		},
+  	},
+	{
+		name: "[Gen 9] PLZA Custom Game",
+		mod: 'plza',
+		searchShow: false,
+		debug: true,
+		battle: {trunc: Math.trunc},
+		// no restrictions, for serious (other than team preview)
+		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100', 'Mega Data Mod'],
+	},
+	{
 		name: "[Gen 6] TPDP Open",
 		mod: 'tpdp',
 		debug: true,
