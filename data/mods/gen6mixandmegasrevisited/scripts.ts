980fa9c0da5f55c6153a9d0c8ac474aae60b6d39
Lysio4 <lukas.moncorge@proton.me>
2025-10-20 14:51:35 +0200

    adding ZA mega - try 2

--- data/mods/gen6mixandmegasrevisited/scripts.ts
+++ data/mods/gen6mixandmegasrevisited/scripts.ts
@@ -14,10 +14,13 @@ export const Scripts: ModdedBattleScriptsData = {
 	},
 	init() {
 		for (const i in this.data.Items) {
-			if (!this.data.Items[i].megaStone) continue;
+			const item = this.data.Items[i];
+			if (!item.megaStone && !item.onDrive && !(item.onPlate && !item.zMove) && !item.onMemory) continue;
 			this.modData('Items', i).onTakeItem = false;
-			const id = this.toID(this.data.Items[i].megaStone);
-			this.modData('FormatsData', id).isNonstandard = null;
+			if (item.isNonstandard === "Past") this.modData('Items', i).isNonstandard = null;
+			if (item.megaStone) {
+				this.modData('FormatsData', this.toID(item.megaStone)).isNonstandard = null;
+			}
 		}
 		this.modData("Learnsets", "lucario").learnset.meteormash = ["6L1"];
 		this.modData("Learnsets", "lucario").learnset.machpunch = ["6L1"];
