7f93bf45f5382ea852492f338f3d14871bf50205
Lysio4 <lukas.moncorge@proton.me>
2025-10-21 12:25:42 +0200

    test?

--- data/mods/earthsky/scripts.ts
+++ data/mods/earthsky/scripts.ts
@@ -2737,8 +2737,10 @@ export const Scripts: ModdedBattleScriptsData = {
 		/* Wide-spread changes */
 		const esrules = this.formats.getRuleTable(this.formats.get('gen9earthskyhorizonsou'));
 		//const dex = Dex.mod('gen9eshorizons');
-		for (let pokemon of this.species.all()) {
-			const pokemonID = this.toID(pokemon.name);
+		//for (let pokemon of this.species.all()) {
+		for (const pokemonID in this.data.Pokedex) {
+			//const pokemonID = this.toID(pokemon.name);
+			const pokemon = this.species.get(id);
 			const learnsetTest = false;//["dedenne"].includes(pokemonID);
 			const formatsTest = false;//["basculin", "basculinbluestriped", "basculinwhitestriped"].includes(pokemonID);
 			if(formatsTest) console.log(pokemonID);
