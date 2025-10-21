52e5e8ef845233640fc63bea74d7d053a30dbbcf
Lysio4 <lukas.moncorge@proton.me>
2025-10-21 12:49:37 +0200

    print here we go again

--- data/mods/earthsky/scripts.ts
+++ data/mods/earthsky/scripts.ts
@@ -2740,6 +2740,7 @@ export const Scripts: ModdedBattleScriptsData = {
 		//for (let pokemon of this.species.all()) {
 		for (const pokemonID in this.data.Pokedex) {
 			//const pokemonID = this.toID(pokemon.name);
+			console.log(pokemonID);
 			const pokemon = this.species.get(pokemonID);
 			const learnsetTest = false;//["dedenne"].includes(pokemonID);
 			const formatsTest = false;//["basculin", "basculinbluestriped", "basculinwhitestriped"].includes(pokemonID);
