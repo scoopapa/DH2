9239231c3481e9213db7e7171403d9a9cb8d5eb0
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-11-17 21:40:28 -0500

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -2068,6 +2068,26 @@ export const Formats: FormatList = [
 		ruleset: ['Standard', 'Terastal Clause', 'Data Mod', 'Sleep Moves Clause', '!Sleep Clause Mod'],
 		banlist: ['AG', 'Uber', 'Arena Trap', 'Moody', 'Shadow Tag', 'King\'s Rock', 'Baton Pass', 'Last Respects', 'Shed Tail', 'Light Clay', 'Fling + Segin Star Shard', /*'Damp Rock'*/],
 	},
+	{
+		name: "[Gen 9] Worldbuilding",
+		threads: [
+			`<a href="https://www.smogon.com/forums/threads/3748841/">Worldbuilding</a>`,
+		],
+		mod: 'worldbuilding',
+		ruleset: ['Standard NatDex', '!Evasion Clause', 'Evasion Moves Clause', 'Evasion Items Clause', 'Mega Rayquaza Clause', 'Terastal Clause', 'Z-Move Clause'],
+		banlist: ['AG', 'Uber', 'Assist', 'Baton Pass'],
+		onValidateTeam(team, format) {
+			/**@type {{[k: string]: true}}*/
+			let speciesTable = {};
+			let allowedTiers = ['WLB'];
+			for (const set of team) {
+				let template = this.dex.species.get(set.species);
+				if (!allowedTiers.includes(template.tier)) {
+					return [set.species + ' is not legal in Worldbuilding.'];
+				}
+			}
+		},
+	},
 	// Start
 	{
 		name: "[Gen 9] VGC 20xx",
