c70845993b3512408ed6623dfaf8bdc01aef2aad
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-11-21 19:06:56 -0500

    Update scripts.ts

--- data/mods/smashmodsbrawl/scripts.ts
+++ data/mods/smashmodsbrawl/scripts.ts
@@ -251,6 +251,8 @@ export const Scripts: ModdedBattleScriptsData = {
 		this.modData('Learnsets', 'glaceon').learnset.breezeburn = ['9L1'];
 		this.modData('Learnsets', 'glaceon').learnset.breezeshock = ['9L1'];
 		this.modData('Learnsets', "yanmega").learnset.earthpower = ["9L1"];
+		this.modData('Learnsets', 'ampharos').learnset.overvoltrail = ['9L1'];
+		this.modData('Learnsets', 'ampharos').learnset.paraboliccharge = ['9L1'];
 	},
 	pokemon: {
 		inherit: true,
@@ -613,6 +615,10 @@ export const Scripts: ModdedBattleScriptsData = {
 			}
 			if (pokemon.species.baseSpecies === 'Clodsire') {
 				const tera = pokemon.species.id === 'clodsire' ? 'tera' : 'tera';
+				pokemon.formeChange(pokemon.species.id + tera, null, true);
+			}
+			if (pokemon.species.baseSpecies === 'Chesnaught') {
+				const tera = pokemon.species.id === 'chesnaught' ? 'basetera' : 'tera';
 				pokemon.formeChange(pokemon.species.id + tera, null, true);
 			}
 			if (pokemon.species.name === 'Terapagos-Terastal' && type === 'Stellar') {
