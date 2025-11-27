474f74989415e04857ed68c1020887e1a0de216c
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-11-26 21:47:51 -0500

    Update formats-data.ts

--- data/mods/blankcanvas/formats-data.ts
+++ data/mods/blankcanvas/formats-data.ts
@@ -355,4 +355,16 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 	zhuguo: {
 		tier: "BC C",
 	},
+	shinobomi: {
+		tier: "BC C",
+	},
+	prionice: {
+		tier: "BC C",
+	},
+	tiaratron: {
+		tier: "BC C",
+	},
+	lianzhao: {
+		tier: "BC C",
+	},
 };
