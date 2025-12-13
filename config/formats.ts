db27f7a9ed3dd1c8c4df40c81118e633fbc9901e
Lysio4 <lukas.moncorge@proton.me>
2025-12-13 17:06:58 +0100

    age removal

--- config/formats.ts
+++ config/formats.ts
@@ -2778,55 +2778,6 @@ export const Formats: FormatList = [
 		teambuilderFormat: 'National Dex',
 	},
 	{
-		name: "[Gen 9] A Golden Experience UU",
-		desc: `A fun metagame where we try to make everything viable, or at least usable. We also have new Fakemons!`,
-		threads: [
-			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1YJXE8wUNJijWSfNKIUqgObN5uEVgTliewTluGe0w4Y4/edit?usp=sharing">Spreadsheet for the mod</a>`,
-		],
-		mod: 'agoldenexperience',
-		ruleset: ['Standard NatDex', 'Terastal Clause', 'Data Mod', 'Mega Data Mod', 'Z-Move Clause'],
-		banlist: [
-			'Uber', 'AG', 'OU', 'UUBL', 'Moody', 'Power Construct', 'King\'s Rock',
-			'Baton Pass', 'Last Respects', 'Quick Claw', 'Razor Fang', 'Shed Tail',
-			'Drizzle', 'Drought', 'Light Clay',
-		],
-		unbanlist: ['Battle Bond', 'Greninja-Bond', 'Light of Ruin'],
-		teambuilderFormat: 'National Dex UU',
-	},
-	{
-		name: "[Gen 9] A Golden Experience RU",
-		desc: `A fun metagame where we try to make everything viable, or at least usable. We also have new Fakemons!`,
-		threads: [
-			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1YJXE8wUNJijWSfNKIUqgObN5uEVgTliewTluGe0w4Y4/edit?usp=sharing">Spreadsheet for the mod</a>`,
-		],
-		mod: 'agoldenexperience',
-		ruleset: ['Standard NatDex', 'Terastal Clause', 'Data Mod', 'Mega Data Mod', 'Z-Move Clause'],
-		banlist: [
-			'Uber', 'AG', 'OU', 'UUBL', 'UU', 'RUBL', 'Moody', 'Power Construct', 'King\'s Rock',
-			'Baton Pass', 'Last Respects', 'Quick Claw', 'Razor Fang', 'Shed Tail',
-			'Drizzle', 'Drought', 'Light Clay',
-		],
-		unbanlist: ['Battle Bond', 'Greninja-Bond', 'Light of Ruin'],
-		teambuilderFormat: 'National Dex RU',
-	},
-	{
-		name: "[Gen 9] A Golden Experience NU",
-		desc: `A fun metagame where we try to make everything viable, or at least usable. We also have new Fakemons!`,
-		threads: [
-			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1YJXE8wUNJijWSfNKIUqgObN5uEVgTliewTluGe0w4Y4/edit?usp=sharing">Spreadsheet for the mod</a>`,
-		],
-		mod: 'agoldenexperience',
-		ruleset: ['Standard NatDex', 'Terastal Clause', 'Data Mod', 'Mega Data Mod', 'Z-Move Clause'],
-		banlist: [
-			'Uber', 'AG', 'OU', 'UUBL', 'UU', 'RUBL', 'RU', 'NUBL', 'Moody', 'Power Construct', 'King\'s Rock',
-			'Baton Pass', 'Last Respects', 'Quick Claw', 'Razor Fang', 'Shed Tail',
-			'Drizzle', 'Drought', 'Light Clay',
-			'Dante\'s Inferno', 'Happy Dance', 'Sticky Web',
-		],
-		unbanlist: ['Battle Bond', 'Greninja-Bond', 'Light of Ruin'],
-		teambuilderFormat: 'National Dex RU',
-	},
-	{
 		name: "[Gen 9] A Golden Experience Doubles",
 		desc: `A fun metagame where we try to make everything viable, or at least usable. We also have new Fakemons!`,
 		threads: [
