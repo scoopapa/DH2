a5c45094bd6f0e815959ca79a903010c78acee8e
Soul-8691 <peterswanton191@bellsouth.net>
2026-01-27 15:17:32 -0500

    [Gen 1] Azure Version: Slates 1-3

--- config/formats.ts
+++ config/formats.ts
@@ -78,6 +78,16 @@ export const Formats: FormatList = [
 		},
 	},
 	{
+		name: "[Gen 1] Azure Version",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/gen-1-azure-version-slate-3-submissions-open.3774689/">[Gen 1] Azure Version</a>`,
+		],
+
+		mod: 'gen1azure',
+		ruleset: ['Standard'],
+		banlist: ['Uber'],
+	},
+	{
 		name: "[Gen 4] Back to Sinnoh",
 		desc: `Recreation of Gen 4 OU, with a lot of new Fakemons, moves, and abilities! No vanilla Gen 4 Pokemon allowed!`,
 		threads: [],
---
+++ data/mods/gen1azure/learnsets.ts
@@ -0,0 +1,602 @@
+export const Learnsets: {[speciesid: string]: LearnsetData} = {
+	sceptile: {
+      learnset: {
+		bide: ["1L1"],
+		rage: ["1L1"],
+		razorleaf: ["1L1"],
+		dragonrage: ["1L1"],
+        absorb: [
+          "1L1"
+        ],
+        agility: [
+          "1M",
+          "1L28",
+          "1L35"
+        ],
+        bodyslam: [
+          "1M",
+          "1T"
+        ],
+        counter: [
+          "1T"
+        ],
+        cut: [
+          "1M"
+        ],
+        dig: [
+          "1M"
+        ],
+        doubleedge: [
+          "1M",
+          "1T"
+        ],
+        doubleteam: [
+          "1L35",
+          "1M"
+        ],
+        earthquake: [
+          "1M"
+        ],
+        flash: [
+          "1M"
+        ],
+        headbutt: [
+          "1T"
+        ],
+        hyperbeam: [
+          "1M"
+        ],
+        leer: [
+          "1L1"
+        ],
+        megadrain: [
+          "1L5",
+          "1L13"
+        ],
+        megakick: [
+          "1M",
+          "1T"
+        ],
+        megapunch: [
+          "1M",
+          "1T"
+        ],
+        mimic: [
+          "1T"
+        ],
+        pound: [
+          "1L1"
+        ],
+        quickattack: [
+          "1L1"
+        ],
+        rest: [
+          "1M"
+        ],
+        roar: [
+          "1M"
+        ],
+        rockslide: [
+          "1M",
+          "1L1"
+        ],
+        screech: [
+          "1L42",
+          "1M",
+          "1L49",
+          "1L69",
+          "1L23"
+        ],
+        seismictoss: [
+          "1T"
+        ],
+        slam: [
+          "1L30",
+          "1L33",
+          "1L43"
+        ],
+        solarbeam: [
+          "1M"
+        ],
+        strength: [
+          "1M"
+        ],
+        substitute: [
+          "1M",
+          "1T"
+        ],
+        swift: [
+          "1M",
+          "1T"
+        ],
+        takedown: [
+          "1M"
+        ],
+        toxic: [
+          "1M"
+        ]
+      }
+    },
+    abomasnow: {
+      learnset: {
+		bide: ["1L1"],
+		rage: ["1L1"],
+		mimic: ["1L1"],
+        blizzard: [
+          "1M",
+          "1L49",
+          "1L47"
+        ],
+        bodyslam: [
+          "1M"
+        ],
+        doubleedge: [
+          "1M"
+        ],
+        doubleteam: [
+          "1M"
+        ],
+        earthquake: [
+          "1M"
+        ],
+        flash: [
+          "1M"
+        ],
+        headbutt: [
+          "1T"
+        ],
+        hyperbeam: [
+          "1M"
+        ],
+        icebeam: [
+          "1M"
+        ],
+        leer: [
+          "1L1"
+        ],
+        lightscreen: [
+          "1M"
+        ],
+        megakick: [
+          "1M"
+        ],
+        megapunch: [
+          "1M"
+        ],
+        mist: [
+          "1L1",
+          "1L21"
+        ],
+        razorleaf: [
+          "1L20",
+          "1L1"
+        ],
+        rest: [
+          "1M"
+        ],
+        rockslide: [
+          "1M"
+        ],
+        solarbeam: [
+          "1M"
+        ],
+        strength: [
+          "1M"
+        ],
+        substitute: [
+          "1M"
+        ],
+        takedown: [
+          "1M"
+        ],
+        toxic: [
+          "1M"
+        ],
+      },
+    },
+    solrock: {
+      learnset: {
+		bide: ["1L1"],
+		rage: ["1L1"],
+        bodyslam: [
+          "1M",
+          "1T"
+        ],
+        confusion: [
+          "1L1",
+          "1D",
+          "1L7"
+        ],
+        defensecurl: [
+          "1T"
+        ],
+        doubleedge: [
+          "1T"
+        ],
+        doubleteam: [
+          "1M"
+        ],
+        dreameater: [
+          "1M",
+          "1T"
+        ],
+        earthquake: [
+          "1M"
+        ],
+        explosion: [
+          "1L50",
+          "1M",
+          "1L45",
+          "1L49",
+          "1L56",
+          "1T"
+        ],
+        fireblast: [
+          "1M"
+        ],
+        flamethrower: [
+          "1M"
+        ],
+        flash: [
+          "1M"
+        ],
+        harden: [
+          "1L1"
+        ],
+        hyperbeam: [
+          "1M"
+        ],
+        lightscreen: [
+          "1M"
+        ],
+        mimic: [
+          "1T"
+        ],
+        psychic: [
+          "1M",
+          "1L30",
+          "1L29",
+          "1L33",
+          "1L1"
+        ],
+        psywave: [
+          "1L13",
+          "1L17",
+          "1L23",
+          "1L25"
+        ],
+        reflect: [
+          "1M"
+        ],
+        rest: [
+          "1M"
+        ],
+        rockslide: [
+          "1M",
+          "1L15",
+          "1L21",
+          "1L25",
+          "1L45",
+          "1T",
+          "1L37"
+        ],
+        rockthrow: [
+          "1L1",
+          "1L5",
+          "1L9",
+          "1L13"
+        ],
+        selfdestruct: [
+          "1M",
+          "1T"
+        ],
+        solarbeam: [
+          "1M",
+          "1L40",
+          "1L41",
+          "1L1",
+          "1L45",
+          "1L53",
+          "1L43"
+        ],
+        substitute: [
+          "1M",
+          "1T"
+        ],
+        swift: [
+          "1M",
+          "1T"
+        ],
+        tackle: [
+          "1L1"
+        ],
+        toxic: [
+          "1M"
+        ],
+      }
+    },
+    turtonator: {
+      learnset: {
+		bide : ["1L1"],
+		rage : ["1L1"],
+		mimic : ["1L1"],
+		dragonrage : ["1L1"],
+		clamp : ["1L1"],
+		withdraw : ["1L1"],
+        bodyslam: [
+          "1M",
+          "1L32",
+          "1L33",
+          "1L1"
+        ],
+        doubleteam: [
+          "1M"
+        ],
+        earthquake: [
+          "1M"
+        ],
+        ember: [
+          "1L4",
+          "1L1"
+        ],
+        explosion: [
+          "1L52",
+          "1M",
+          "1L53"
+        ],
+        fireblast: [
+          "1M"
+        ],
+        firespin: [
+          "1M",
+          "1E"
+        ],
+        flamethrower: [
+          "1M",
+          "1L36",
+          "1L29",
+          "1L1"
+        ],
+        hyperbeam: [
+          "1M"
+        ],
+        megakick: [
+          "1M"
+        ],
+        megapunch: [
+          "1M"
+        ],
+        rest: [
+          "1M"
+        ],
+        roar: [
+          "1M"
+        ],
+        smog: [
+          "1L1",
+          "1L5"
+        ],
+        solarbeam: [
+          "1M"
+        ],
+        substitute: [
+          "1M"
+        ],
+        tackle: [
+          "1L1"
+        ],
+        toxic: [
+          "1M"
+        ],
+      }
+    },
+    glaceon: {
+      learnset: {
+		amnesia: ["1L1"],
+		bubblebeam: ["1L1"],
+        barrier: [
+          "1L29",
+          "1L78"
+        ],
+        bite: [
+          "1L25",
+          "1L17",
+          "1L29"
+        ],
+        blizzard: [
+          "1M",
+          "1L50",
+          "1L45",
+          "1L1",
+          "1L71"
+        ],
+        bodyslam: [
+          "1M"
+        ],
+        doubleedge: [
+          "1M",
+          "1L1"
+        ],
+        doubleteam: [
+          "1M"
+        ],
+        focusenergy: [
+          "1M"
+        ],
+        growl: [
+          "1L1"
+        ],
+        haze: [
+          "1M"
+        ],
+        hyperbeam: [
+          "1M"
+        ],
+        icebeam: [
+          "1M"
+        ],
+        payday: [
+          "1M"
+        ],
+        quickattack: [
+          "1L10",
+          "1L13",
+          "1L22"
+        ],
+        rest: [
+          "1M"
+        ],
+        roar: [
+          "1M"
+        ],
+        sandattack: [
+          "1L5",
+          "1L1",
+          "1L8"
+        ],
+        substitute: [
+          "1M"
+        ],
+        swift: [
+          "1M",
+          "1L1",
+          "1T"
+        ],
+        tackle: [
+          "1L1"
+        ],
+        tailwhip: [
+          "1L1"
+        ],
+        takedown: [
+          "1M",
+          "1L1"
+        ],
+        toxic: [
+          "1M"
+        ],
+      }
+    },
+    medicham: {
+      learnset: {
+		swordsdance: ["1L1"],
+		amnesia: ["1L1"],
+		karatechop: ["1L1"],
+		rage: ["1L1"],
+        bide: [
+          "1L1"
+        ],
+        bodyslam: [
+          "1M",
+          "1T"
+        ],
+        confusion: [
+          "1L1"
+        ],
+        counter: [
+          "1L53",
+          "1T"
+        ],
+        doubleedge: [
+          "1T"
+        ],
+        doubleteam: [
+          "1M"
+        ],
+        dreameater: [
+          "1M",
+          "1T"
+        ],
+        firepunch: [
+          "1M",
+          "1L1",
+          "1T"
+        ],
+        flash: [
+          "1M"
+        ],
+        highjumpkick: [
+          "1L28",
+          "1L32"
+        ],
+        hyperbeam: [
+          "1M"
+        ],
+        icepunch: [
+          "1M",
+          "1L1",
+          "1T"
+        ],
+        lightscreen: [
+          "1M"
+        ],
+        meditate: [
+          "1L1"
+        ],
+        megakick: [
+          "1T"
+        ],
+        megapunch: [
+          "1T"
+        ],
+        metronome: [
+          "1M",
+          "1T"
+        ],
+        mimic: [
+          "1T"
+        ],
+        nightshade: [
+          "1M"
+        ],
+        psybeam: [
+          "1M",
+          "1L20"
+        ],
+        psychic: [
+          "1M"
+        ],
+        recover: [
+          "1L47",
+          "1L62",
+          "1L55",
+          "1L54"
+        ],
+        reflect: [
+          "1M"
+        ],
+        rest: [
+          "1M"
+        ],
+        rockslide: [
+          "1M",
+          "1T"
+        ],
+        seismictoss: [
+          "1T"
+        ],
+        strength: [
+          "1M"
+        ],
+        substitute: [
+          "1M",
+          "1T"
+        ],
+        swift: [
+          "1M",
+          "1T"
+        ],
+        takedown: [
+          "1M"
+        ],
+        thunderpunch: [
+          "1M",
+          "1L1",
+          "1T"
+        ],
+        toxic: [
+          "1M"
+        ],
+      },
+    },
+};
