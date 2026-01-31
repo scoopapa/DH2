7521c41a148a5089c078a8c53d6a7b6d1eb321be
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-01-31 18:59:01 -0500

    vibe code

--- data/mods/gen5valuemons/rulesets.ts
+++ data/mods/gen5valuemons/rulesets.ts
@@ -4,13 +4,14 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
 		name: 'One Legendary Clause',
 		desc: "You can only have one Legendary Pok\u00E9mon on your team.",
 		onValidateTeam(team) {
-			const restrictedSpecies = ['Giratina-Altered', 'Giratina-Origin', 'Darkrai', 'Dialga', 'Groudon', 'Kyogre', 'Mewtwo', 'Palkia', 'Rayquaza', 'Ho-oh', 'Latias', 'Latios', 'Lugia', 'Celebi', 'Heatran', 'Jirachi', 'Manaphy', 'Mew', 'Shaymin', 'Shaymin-Sky', 'Zapdos', 'Azelf', 'Cresselia', 'Raikou', 'Suicune', 'Uxie', 'Entei', 'Mesprit', 'Moltres', 'Regirock', 'Registeel', 'Articuno', 'Regice', 'Regigigas',
-				'Deoxys', 'Deoxys-Attack', 'Deoxys-Defense', 'Deoxys-Speed', 'Arceus', 'Arceus-Bug', 'Arceus-Dark', 'Arceus-Dragon', 'Arceus-Electric', 'Arceus-Fairy', 'Arceus-Fighting', 'Arceus-Fire', 'Arceus-Flying', 'Arceus-Ghost', 'Arceus-Grass', 'Arceus-Ground', 'Arceus-Ice', 'Arceus-Poison', 'Arceus-Psychic', 'Arceus-Rock', 'Arceus-Steel', 'Arceus-Water'];
+			const restrictedSpecies = new Set(['Giratina-Altered', 'Giratina-Origin', 'Darkrai', 'Dialga', 'Groudon', 'Kyogre', 'Mewtwo', 'Palkia', 'Rayquaza', 'Ho-oh', 'Latias', 'Latios', 'Lugia', 'Celebi', 'Heatran', 'Jirachi', 'Manaphy', 'Mew', 'Shaymin', 'Shaymin-Sky', 'Zapdos', 'Azelf', 'Cresselia', 'Raikou', 'Suicune', 'Uxie', 'Entei', 'Mesprit', 'Moltres', 'Regirock', 'Registeel', 'Articuno', 'Regice', 'Regigigas',
+					'Deoxys', 'Deoxys-Attack', 'Deoxys-Defense', 'Deoxys-Speed', 'Arceus', 'Arceus-Bug', 'Arceus-Dark', 'Arceus-Dragon', 'Arceus-Electric', 'Arceus-Fairy', 'Arceus-Fighting', 'Arceus-Fire', 'Arceus-Flying', 'Arceus-Ghost', 'Arceus-Grass', 'Arceus-Ground', 'Arceus-Ice', 'Arceus-Poison', 'Arceus-Psychic', 'Arceus-Rock', 'Arceus-Steel', 'Arceus-Water']);
+			let count = 0;
 			for (const set of team) {
 				const species = this.dex.species.get(set.species);
-				if (this.ruleTable.isRestrictedSpecies(species)) restrictedSpecies.push(species.name);
+				if (restrictedSpecies.has(species.name)) count++;
 			}
-			if (restrictedSpecies.length !== 1) {
+			if (count !== 1) {
 				return [`You must use exactly one Legendary Pok\u00E9mon.`];
 			}
 		},
@@ -21,12 +22,13 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
 		name: 'One Pseudo Clause',
 		desc: "You can only have one Pseudo-Legendary Pok\u00E9mon on your team.",
 		onValidateTeam(team) {
-			const restrictedSpecies = ['Garchomp', 'Salamence', 'Dragonite', 'Tyranitar', 'Metagross'];
+			const restrictedSpecies = new Set(['Garchomp', 'Salamence', 'Dragonite', 'Tyranitar', 'Metagross']);
+			let count = 0;
 			for (const set of team) {
 				const species = this.dex.species.get(set.species);
-				if (this.ruleTable.isRestrictedSpecies(species)) restrictedSpecies.push(species.name);
+				if (restrictedSpecies.has(species.name)) count++;
 			}
-			if (restrictedSpecies.length > 1) {
+			if (count > 1) {
 				return [`You can only use up to one Pseudolegendary Pok\u00E9mon (Pokemon with a BST of 600 at the end of a 3-stage evolution chain.)`];
 			}
 		},
@@ -36,16 +38,17 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
 		name: 'One Starter Clause',
 		desc: "You can only have one Starter Pok\u00E9mon on your team.",
 		onValidateTeam(team) {
-			const restrictedSpecies = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise',
+			const restrictedSpecies = new Set(['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise',
 					 'Chikorita', 'Bayleef', 'Meganium', 'Cyndaquil', 'Quilava', 'Typhlosion', 'Totodile', 'Croconaw', 'Feraligatr',
 					 'Treecko', 'Grovyle', 'Sceptile', 'Torchic', 'Combusken', 'Blaziken', 'Mudkip', 'Marshtomp', 'Swampert',
 					 'Turtwig', 'Grotle', 'Torterra', 'Chimchar', 'Monferno', 'Infernape', 'Piplup', 'Prinplup', 'Empoleon',
-					 'Snivy', 'Servine', 'Serperior', 'Tepig', 'Pignite', 'Emboar', 'Oshawott', 'Dewott', 'Samurott'];
+					 'Snivy', 'Servine', 'Serperior', 'Tepig', 'Pignite', 'Emboar', 'Oshawott', 'Dewott', 'Samurott']);
+			let count = 0;
 			for (const set of team) {
 				const species = this.dex.species.get(set.species);
-				if (this.ruleTable.isRestrictedSpecies(species)) restrictedSpecies.push(species.name);
+				if (restrictedSpecies.has(species.name)) count++;
 			}
-			if (restrictedSpecies.length > 1){
+			if (count > 1){
 				return [`You can only use up to one Starter Pok\u00E9mon.`];
 			}
 		},
