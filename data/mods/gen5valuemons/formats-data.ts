9b0fb0ed6b18fd915d395ccdadc123c113dbffb1
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-01-31 11:56:28 -0500

    its all 33v? always has been

--- data/mods/gen5valuemons/formats-data.ts
+++ data/mods/gen5valuemons/formats-data.ts
@@ -1,2 +1,1556 @@
-export const FormatsData: import('../../../sim/dex-species').SpeciesFormatsDataTable = {
+export const FormatsData: import'../../../sim/dex-species'.SpeciesFormatsDataTable = {
+	bulbasaur: {
+		tier: "33v",
+	},
+	ivysaur: {
+		tier: "33v",
+	},
+	venusaur: {
+		tier: "33v",
+	},
+	charmander: {
+		tier: "33v",
+	},
+	charmeleon: {
+		tier: "33v",
+	},
+	charizard: {
+		tier: "33v",
+	},
+	squirtle: {
+		tier: "33v",
+	},
+	wartortle: {
+		tier: "33v",
+	},
+	blastoise: {
+		tier: "33v",
+	},
+	caterpie: {
+		tier: "33v",
+	},
+	metapod: {
+		tier: "33v",
+	},
+	butterfree: {
+		tier: "33v",
+	},
+	weedle: {
+		tier: "33v",
+	},
+	kakuna: {
+		tier: "33v",
+	},
+	beedrill: {
+		tier: "33v",
+	},
+	pidgey: {
+		tier: "33v",
+	},
+	pidgeotto: {
+		tier: "33v",
+	},
+	pidgeot: {
+		tier: "33v",
+	},
+	rattata: {
+		tier: "33v",
+	},
+	raticate: {
+		tier: "33v",
+	},
+	spearow: {
+		tier: "33v",
+	},
+	fearow: {
+		tier: "33v",
+	},
+	ekans: {
+		tier: "33v",
+	},
+	arbok: {
+		tier: "33v",
+	},
+	pichu: {
+		tier: "33v",
+	},
+	pichuspikyeared: {
+		tier: "33v",
+	},
+	pikachu: {
+		tier: "33v",
+	},
+	raichu: {
+		tier: "33v",
+	},
+	sandshrew: {
+		tier: "33v",
+	},
+	sandslash: {
+		tier: "33v",
+	},
+	nidoranf: {
+		tier: "33v",
+	},
+	nidorina: {
+		tier: "33v",
+	},
+	nidoqueen: {
+		tier: "33v",
+	},
+	nidoranm: {
+		tier: "33v",
+	},
+	nidorino: {
+		tier: "33v",
+	},
+	nidoking: {
+		tier: "33v",
+	},
+	cleffa: {
+		tier: "33v",
+	},
+	clefairy: {
+		tier: "33v",
+	},
+	clefable: {
+		tier: "33v",
+	},
+	vulpix: {
+		tier: "33v",
+	},
+	ninetales: {
+		tier: "33v",
+	},
+	igglybuff: {
+		tier: "33v",
+	},
+	jigglypuff: {
+		tier: "33v",
+	},
+	wigglytuff: {
+		tier: "33v",
+	},
+	zubat: {
+		tier: "33v",
+	},
+	golbat: {
+		tier: "33v",
+	},
+	crobat: {
+		tier: "33v",
+	},
+	oddish: {
+		tier: "33v",
+	},
+	gloom: {
+		tier: "33v",
+	},
+	vileplume: {
+		tier: "33v",
+	},
+	bellossom: {
+		tier: "33v",
+	},
+	paras: {
+		tier: "33v",
+	},
+	parasect: {
+		tier: "33v",
+	},
+	venonat: {
+		tier: "33v",
+	},
+	venomoth: {
+		tier: "33v",
+	},
+	diglett: {
+		tier: "33v",
+	},
+	dugtrio: {
+		tier: "33v",
+	},
+	meowth: {
+		tier: "33v",
+	},
+	persian: {
+		tier: "33v",
+	},
+	psyduck: {
+		tier: "33v",
+	},
+	golduck: {
+		tier: "33v",
+	},
+	mankey: {
+		tier: "33v",
+	},
+	primeape: {
+		tier: "33v",
+	},
+	growlithe: {
+		tier: "33v",
+	},
+	arcanine: {
+		tier: "33v",
+	},
+	poliwag: {
+		tier: "33v",
+	},
+	poliwhirl: {
+		tier: "33v",
+	},
+	poliwrath: {
+		tier: "33v",
+	},
+	politoed: {
+		tier: "33v",
+	},
+	abra: {
+		tier: "33v",
+	},
+	kadabra: {
+		tier: "33v",
+	},
+	alakazam: {
+		tier: "33v",
+	},
+	machop: {
+		tier: "33v",
+	},
+	machoke: {
+		tier: "33v",
+	},
+	machamp: {
+		tier: "33v",
+	},
+	bellsprout: {
+		tier: "33v",
+	},
+	weepinbell: {
+		tier: "33v",
+	},
+	victreebel: {
+		tier: "33v",
+	},
+	tentacool: {
+		tier: "33v",
+	},
+	tentacruel: {
+		tier: "33v",
+	},
+	geodude: {
+		tier: "33v",
+	},
+	graveler: {
+		tier: "33v",
+	},
+	golem: {
+		tier: "33v",
+	},
+	ponyta: {
+		tier: "33v",
+	},
+	rapidash: {
+		tier: "33v",
+	},
+	slowpoke: {
+		tier: "33v",
+	},
+	slowbro: {
+		tier: "33v",
+	},
+	slowking: {
+		tier: "33v",
+	},
+	magnemite: {
+		tier: "33v",
+	},
+	magneton: {
+		tier: "33v",
+	},
+	magnezone: {
+		tier: "33v",
+	},
+	farfetchd: {
+		tier: "33v",
+	},
+	doduo: {
+		tier: "33v",
+	},
+	dodrio: {
+		tier: "33v",
+	},
+	seel: {
+		tier: "33v",
+	},
+	dewgong: {
+		tier: "33v",
+	},
+	grimer: {
+		tier: "33v",
+	},
+	muk: {
+		tier: "33v",
+	},
+	shellder: {
+		tier: "33v",
+	},
+	cloyster: {
+		tier: "33v",
+	},
+	gastly: {
+		tier: "33v",
+	},
+	haunter: {
+		tier: "33v",
+	},
+	gengar: {
+		tier: "33v",
+	},
+	onix: {
+		tier: "33v",
+	},
+	steelix: {
+		tier: "33v",
+	},
+	drowzee: {
+		tier: "33v",
+	},
+	hypno: {
+		tier: "33v",
+	},
+	krabby: {
+		tier: "33v",
+	},
+	kingler: {
+		tier: "33v",
+	},
+	voltorb: {
+		tier: "33v",
+	},
+	electrode: {
+		tier: "33v",
+	},
+	exeggcute: {
+		tier: "33v",
+	},
+	exeggutor: {
+		tier: "33v",
+	},
+	cubone: {
+		tier: "33v",
+	},
+	marowak: {
+		tier: "33v",
+	},
+	tyrogue: {
+		tier: "33v",
+	},
+	hitmonlee: {
+		tier: "33v",
+	},
+	hitmonchan: {
+		tier: "33v",
+	},
+	hitmontop: {
+		tier: "33v",
+	},
+	lickitung: {
+		tier: "33v",
+	},
+	lickilicky: {
+		tier: "33v",
+	},
+	koffing: {
+		tier: "33v",
+	},
+	weezing: {
+		tier: "33v",
+	},
+	rhyhorn: {
+		tier: "33v",
+	},
+	rhydon: {
+		tier: "33v",
+	},
+	rhyperior: {
+		tier: "33v",
+	},
+	happiny: {
+		tier: "33v",
+	},
+	chansey: {
+		tier: "33v",
+	},
+	blissey: {
+		tier: "33v",
+	},
+	tangela: {
+		tier: "33v",
+	},
+	tangrowth: {
+		tier: "33v",
+	},
+	kangaskhan: {
+		tier: "33v",
+	},
+	horsea: {
+		tier: "33v",
+	},
+	seadra: {
+		tier: "33v",
+	},
+	kingdra: {
+		tier: "33v",
+	},
+	goldeen: {
+		tier: "33v",
+	},
+	seaking: {
+		tier: "33v",
+	},
+	staryu: {
+		tier: "33v",
+	},
+	starmie: {
+		tier: "33v",
+	},
+	mimejr: {
+		tier: "33v",
+	},
+	mrmime: {
+		tier: "33v",
+	},
+	scyther: {
+		tier: "33v",
+	},
+	scizor: {
+		tier: "33v",
+	},
+	smoochum: {
+		tier: "33v",
+	},
+	jynx: {
+		tier: "33v",
+	},
+	elekid: {
+		tier: "33v",
+	},
+	electabuzz: {
+		tier: "33v",
+	},
+	electivire: {
+		tier: "33v",
+	},
+	magby: {
+		tier: "33v",
+	},
+	magmar: {
+		tier: "33v",
+	},
+	magmortar: {
+		tier: "33v",
+	},
+	pinsir: {
+		tier: "33v",
+	},
+	tauros: {
+		tier: "33v",
+	},
+	magikarp: {
+		tier: "33v",
+	},
+	gyarados: {
+		tier: "33v",
+	},
+	lapras: {
+		tier: "33v",
+	},
+	ditto: {
+		tier: "33v",
+	},
+	eevee: {
+		tier: "33v",
+	},
+	vaporeon: {
+		tier: "33v",
+	},
+	jolteon: {
+		tier: "33v",
+	},
+	flareon: {
+		tier: "33v",
+	},
+	espeon: {
+		tier: "33v",
+	},
+	umbreon: {
+		tier: "33v",
+	},
+	leafeon: {
+		tier: "33v",
+	},
+	glaceon: {
+		tier: "33v",
+	},
+	porygon: {
+		tier: "33v",
+	},
+	porygon2: {
+		tier: "33v",
+	},
+	porygonz: {
+		tier: "33v",
+	},
+	omanyte: {
+		tier: "33v",
+	},
+	omastar: {
+		tier: "33v",
+	},
+	kabuto: {
+		tier: "33v",
+	},
+	kabutops: {
+		tier: "33v",
+	},
+	aerodactyl: {
+		tier: "33v",
+	},
+	munchlax: {
+		tier: "33v",
+	},
+	snorlax: {
+		tier: "33v",
+	},
+	articuno: {
+		tier: "33v",
+	},
+	zapdos: {
+		tier: "33v",
+	},
+	moltres: {
+		tier: "33v",
+	},
+	dratini: {
+		tier: "33v",
+	},
+	dragonair: {
+		tier: "33v",
+	},
+	dragonite: {
+		tier: "33v",
+	},
+	mewtwo: {
+		tier: "33v",
+	},
+	mew: {
+		tier: "33v",
+	},
+	chikorita: {
+		tier: "33v",
+	},
+	bayleef: {
+		tier: "33v",
+	},
+	meganium: {
+		tier: "33v",
+	},
+	cyndaquil: {
+		tier: "33v",
+	},
+	quilava: {
+		tier: "33v",
+	},
+	typhlosion: {
+		tier: "33v",
+	},
+	totodile: {
+		tier: "33v",
+	},
+	croconaw: {
+		tier: "33v",
+	},
+	feraligatr: {
+		tier: "33v",
+	},
+	sentret: {
+		tier: "33v",
+	},
+	furret: {
+		tier: "33v",
+	},
+	hoothoot: {
+		tier: "33v",
+	},
+	noctowl: {
+		tier: "33v",
+	},
+	ledyba: {
+		tier: "33v",
+	},
+	ledian: {
+		tier: "33v",
+	},
+	spinarak: {
+		tier: "33v",
+	},
+	ariados: {
+		tier: "33v",
+	},
+	chinchou: {
+		tier: "33v",
+	},
+	lanturn: {
+		tier: "33v",
+	},
+	togepi: {
+		tier: "33v",
+	},
+	togetic: {
+		tier: "33v",
+	},
+	togekiss: {
+		tier: "33v",
+	},
+	natu: {
+		tier: "33v",
+	},
+	xatu: {
+		tier: "33v",
+	},
+	mareep: {
+		tier: "33v",
+	},
+	flaaffy: {
+		tier: "33v",
+	},
+	ampharos: {
+		tier: "33v",
+	},
+	azurill: {
+		tier: "33v",
+	},
+	marill: {
+		tier: "33v",
+	},
+	azumarill: {
+		tier: "33v",
+	},
+	bonsly: {
+		tier: "33v",
+	},
+	sudowoodo: {
+		tier: "33v",
+	},
+	hoppip: {
+		tier: "33v",
+	},
+	skiploom: {
+		tier: "33v",
+	},
+	jumpluff: {
+		tier: "33v",
+	},
+	aipom: {
+		tier: "33v",
+	},
+	ambipom: {
+		tier: "33v",
+	},
+	sunkern: {
+		tier: "33v",
+	},
+	sunflora: {
+		tier: "33v",
+	},
+	yanma: {
+		tier: "33v",
+	},
+	yanmega: {
+		tier: "33v",
+	},
+	wooper: {
+		tier: "33v",
+	},
+	quagsire: {
+		tier: "33v",
+	},
+	murkrow: {
+		tier: "33v",
+	},
+	honchkrow: {
+		tier: "33v",
+	},
+	misdreavus: {
+		tier: "33v",
+	},
+	mismagius: {
+		tier: "33v",
+	},
+	unown: {
+		tier: "33v",
+	},
+	wynaut: {
+		tier: "33v",
+	},
+	wobbuffet: {
+		tier: "33v",
+	},
+	girafarig: {
+		tier: "33v",
+	},
+	pineco: {
+		tier: "33v",
+	},
+	forretress: {
+		tier: "33v",
+	},
+	dunsparce: {
+		tier: "33v",
+	},
+	gligar: {
+		tier: "33v",
+	},
+	gliscor: {
+		tier: "33v",
+	},
+	snubbull: {
+		tier: "33v",
+	},
+	granbull: {
+		tier: "33v",
+	},
+	qwilfish: {
+		tier: "33v",
+	},
+	shuckle: {
+		tier: "33v",
+	},
+	heracross: {
+		tier: "33v",
+	},
+	sneasel: {
+		tier: "33v",
+	},
+	weavile: {
+		tier: "33v",
+	},
+	teddiursa: {
+		tier: "33v",
+	},
+	ursaring: {
+		tier: "33v",
+	},
+	slugma: {
+		tier: "33v",
+	},
+	magcargo: {
+		tier: "33v",
+	},
+	swinub: {
+		tier: "33v",
+	},
+	piloswine: {
+		tier: "33v",
+	},
+	mamoswine: {
+		tier: "33v",
+	},
+	corsola: {
+		tier: "33v",
+	},
+	remoraid: {
+		tier: "33v",
+	},
+	octillery: {
+		tier: "33v",
+	},
+	delibird: {
+		tier: "33v",
+	},
+	mantyke: {
+		tier: "33v",
+	},
+	mantine: {
+		tier: "33v",
+	},
+	skarmory: {
+		tier: "33v",
+	},
+	houndour: {
+		tier: "33v",
+	},
+	houndoom: {
+		tier: "33v",
+	},
+	phanpy: {
+		tier: "33v",
+	},
+	donphan: {
+		tier: "33v",
+	},
+	stantler: {
+		tier: "33v",
+	},
+	smeargle: {
+		tier: "33v",
+	},
+	miltank: {
+		tier: "33v",
+	},
+	raikou: {
+		tier: "33v",
+	},
+	entei: {
+		tier: "33v",
+	},
+	suicune: {
+		tier: "33v",
+	},
+	larvitar: {
+		tier: "33v",
+	},
+	pupitar: {
+		tier: "33v",
+	},
+	tyranitar: {
+		tier: "33v",
+	},
+	lugia: {
+		tier: "33v",
+	},
+	hooh: {
+		tier: "33v",
+	},
+	celebi: {
+		tier: "33v",
+	},
+	treecko: {
+		tier: "33v",
+	},
+	grovyle: {
+		tier: "33v",
+	},
+	sceptile: {
+		tier: "33v",
+	},
+	torchic: {
+		tier: "33v",
+	},
+	combusken: {
+		tier: "33v",
+	},
+	blaziken: {
+		tier: "33v",
+	},
+	mudkip: {
+		tier: "33v",
+	},
+	marshtomp: {
+		tier: "33v",
+	},
+	swampert: {
+		tier: "33v",
+	},
+	poochyena: {
+		tier: "33v",
+	},
+	mightyena: {
+		tier: "33v",
+	},
+	zigzagoon: {
+		tier: "33v",
+	},
+	linoone: {
+		tier: "33v",
+	},
+	wurmple: {
+		tier: "33v",
+	},
+	silcoon: {
+		tier: "33v",
+	},
+	beautifly: {
+		tier: "33v",
+	},
+	cascoon: {
+		tier: "33v",
+	},
+	dustox: {
+		tier: "33v",
+	},
+	lotad: {
+		tier: "33v",
+	},
+	lombre: {
+		tier: "33v",
+	},
+	ludicolo: {
+		tier: "33v",
+	},
+	seedot: {
+		tier: "33v",
+	},
+	nuzleaf: {
+		tier: "33v",
+	},
+	shiftry: {
+		tier: "33v",
+	},
+	taillow: {
+		tier: "33v",
+	},
+	swellow: {
+		tier: "33v",
+	},
+	wingull: {
+		tier: "33v",
+	},
+	pelipper: {
+		tier: "33v",
+	},
+	ralts: {
+		tier: "33v",
+	},
+	kirlia: {
+		tier: "33v",
+	},
+	gardevoir: {
+		tier: "33v",
+	},
+	gallade: {
+		tier: "33v",
+	},
+	surskit: {
+		tier: "33v",
+	},
+	masquerain: {
+		tier: "33v",
+	},
+	shroomish: {
+		tier: "33v",
+	},
+	breloom: {
+		tier: "33v",
+	},
+	slakoth: {
+		tier: "33v",
+	},
+	vigoroth: {
+		tier: "33v",
+	},
+	slaking: {
+		tier: "33v",
+	},
+	nincada: {
+		tier: "33v",
+	},
+	ninjask: {
+		tier: "33v",
+	},
+	shedinja: {
+		tier: "33v",
+	},
+	whismur: {
+		tier: "33v",
+	},
+	loudred: {
+		tier: "33v",
+	},
+	exploud: {
+		tier: "33v",
+	},
+	makuhita: {
+		tier: "33v",
+	},
+	hariyama: {
+		tier: "33v",
+	},
+	nosepass: {
+		tier: "33v",
+	},
+	probopass: {
+		tier: "33v",
+	},
+	skitty: {
+		tier: "33v",
+	},
+	delcatty: {
+		tier: "33v",
+	},
+	sableye: {
+		tier: "33v",
+	},
+	mawile: {
+		tier: "33v",
+	},
+	aron: {
+		tier: "33v",
+	},
+	lairon: {
+		tier: "33v",
+	},
+	aggron: {
+		tier: "33v",
+	},
+	meditite: {
+		tier: "33v",
+	},
+	medicham: {
+		tier: "33v",
+	},
+	electrike: {
+		tier: "33v",
+	},
+	manectric: {
+		tier: "33v",
+	},
+	plusle: {
+		tier: "33v",
+	},
+	minun: {
+		tier: "33v",
+	},
+	volbeat: {
+		tier: "33v",
+	},
+	illumise: {
+		tier: "33v",
+	},
+	budew: {
+		tier: "33v",
+	},
+	roselia: {
+		tier: "33v",
+	},
+	roserade: {
+		tier: "33v",
+	},
+	gulpin: {
+		tier: "33v",
+	},
+	swalot: {
+		tier: "33v",
+	},
+	carvanha: {
+		tier: "33v",
+	},
+	sharpedo: {
+		tier: "33v",
+	},
+	wailmer: {
+		tier: "33v",
+	},
+	wailord: {
+		tier: "33v",
+	},
+	numel: {
+		tier: "33v",
+	},
+	camerupt: {
+		tier: "33v",
+	},
+	torkoal: {
+		tier: "33v",
+	},
+	spoink: {
+		tier: "33v",
+	},
+	grumpig: {
+		tier: "33v",
+	},
+	spinda: {
+		tier: "33v",
+	},
+	trapinch: {
+		tier: "33v",
+	},
+	vibrava: {
+		tier: "33v",
+	},
+	flygon: {
+		tier: "33v",
+	},
+	cacnea: {
+		tier: "33v",
+	},
+	cacturne: {
+		tier: "33v",
+	},
+	swablu: {
+		tier: "33v",
+	},
+	altaria: {
+		tier: "33v",
+	},
+	zangoose: {
+		tier: "33v",
+	},
+	seviper: {
+		tier: "33v",
+	},
+	lunatone: {
+		tier: "33v",
+	},
+	solrock: {
+		tier: "33v",
+	},
+	barboach: {
+		tier: "33v",
+	},
+	whiscash: {
+		tier: "33v",
+	},
+	corphish: {
+		tier: "33v",
+	},
+	crawdaunt: {
+		tier: "33v",
+	},
+	baltoy: {
+		tier: "33v",
+	},
+	claydol: {
+		tier: "33v",
+	},
+	lileep: {
+		tier: "33v",
+	},
+	cradily: {
+		tier: "33v",
+	},
+	anorith: {
+		tier: "33v",
+	},
+	armaldo: {
+		tier: "33v",
+	},
+	feebas: {
+		tier: "33v",
+	},
+	milotic: {
+		tier: "33v",
+	},
+	castform: {
+		tier: "33v",
+	},
+	castformsunny: {},
+	castformrainy: {},
+	castformsnowy: {},
+	kecleon: {
+		tier: "33v",
+	},
+	shuppet: {
+		tier: "33v",
+	},
+	banette: {
+		tier: "33v",
+	},
+	duskull: {
+		tier: "33v",
+	},
+	dusclops: {
+		tier: "33v",
+	},
+	dusknoir: {
+		tier: "33v",
+	},
+	tropius: {
+		tier: "33v",
+	},
+	chingling: {
+		tier: "33v",
+	},
+	chimecho: {
+		tier: "33v",
+	},
+	absol: {
+		tier: "33v",
+	},
+	snorunt: {
+		tier: "33v",
+	},
+	glalie: {
+		tier: "33v",
+	},
+	froslass: {
+		tier: "33v",
+	},
+	spheal: {
+		tier: "33v",
+	},
+	sealeo: {
+		tier: "33v",
+	},
+	walrein: {
+		tier: "33v",
+	},
+	clamperl: {
+		tier: "33v",
+	},
+	huntail: {
+		tier: "33v",
+	},
+	gorebyss: {
+		tier: "33v",
+	},
+	relicanth: {
+		tier: "33v",
+	},
+	luvdisc: {
+		tier: "33v",
+	},
+	bagon: {
+		tier: "33v",
+	},
+	shelgon: {
+		tier: "33v",
+	},
+	salamence: {
+		tier: "33v",
+	},
+	beldum: {
+		tier: "33v",
+	},
+	metang: {
+		tier: "33v",
+	},
+	metagross: {
+		tier: "33v",
+	},
+	regirock: {
+		tier: "33v",
+	},
+	regice: {
+		tier: "33v",
+	},
+	registeel: {
+		tier: "33v",
+	},
+	latias: {
+		tier: "33v",
+	},
+	latios: {
+		tier: "33v",
+	},
+	kyogre: {
+		tier: "33v",
+	},
+	groudon: {
+		tier: "33v",
+	},
+	rayquaza: {
+		tier: "33v",
+	},
+	jirachi: {
+		tier: "33v",
+	},
+	deoxys: {
+		tier: "33v",
+	},
+	deoxysattack: {
+		tier: "33v",
+	},
+	deoxysdefense: {
+		tier: "33v",
+	},
+	deoxysspeed: {
+		tier: "33v",
+	},
+	turtwig: {
+		tier: "33v",
+	},
+	grotle: {
+		tier: "33v",
+	},
+	torterra: {
+		tier: "33v",
+	},
+	chimchar: {
+		tier: "33v",
+	},
+	monferno: {
+		tier: "33v",
+	},
+	infernape: {
+		tier: "33v",
+	},
+	piplup: {
+		tier: "33v",
+	},
+	prinplup: {
+		tier: "33v",
+	},
+	empoleon: {
+		tier: "33v",
+	},
+	starly: {
+		tier: "33v",
+	},
+	staravia: {
+		tier: "33v",
+	},
+	staraptor: {
+		tier: "33v",
+	},
+	bidoof: {
+		tier: "33v",
+	},
+	bibarel: {
+		tier: "33v",
+	},
+	kricketot: {
+		tier: "33v",
+	},
+	kricketune: {
+		tier: "33v",
+	},
+	shinx: {
+		tier: "33v",
+	},
+	luxio: {
+		tier: "33v",
+	},
+	luxray: {
+		tier: "33v",
+	},
+	cranidos: {
+		tier: "33v",
+	},
+	rampardos: {
+		tier: "33v",
+	},
+	shieldon: {
+		tier: "33v",
+	},
+	bastiodon: {
+		tier: "33v",
+	},
+	burmy: {
+		tier: "33v",
+	},
+	wormadam: {
+		tier: "33v",
+	},
+	wormadamsandy: {
+		tier: "33v",
+	},
+	wormadamtrash: {
+		tier: "33v",
+	},
+	mothim: {
+		tier: "33v",
+	},
+	combee: {
+		tier: "33v",
+	},
+	vespiquen: {
+		tier: "33v",
+	},
+	pachirisu: {
+		tier: "33v",
+	},
+	buizel: {
+		tier: "33v",
+	},
+	floatzel: {
+		tier: "33v",
+	},
+	cherubi: {
+		tier: "33v",
+	},
+	cherrim: {
+		tier: "33v",
+	},
+	cherrimsunshine: {
+	},
+	shellos: {
+		tier: "33v",
+	},
+	gastrodon: {
+		tier: "33v",
+	},
+	drifloon: {
+		tier: "33v",
+	},
+	drifblim: {
+		tier: "33v",
+	},
+	buneary: {
+		tier: "33v",
+	},
+	lopunny: {
+		tier: "33v",
+	},
+	glameow: {
+		tier: "33v",
+	},
+	purugly: {
+		tier: "33v",
+	},
+	stunky: {
+		tier: "33v",
+	},
+	skuntank: {
+		tier: "33v",
+	},
+	bronzor: {
+		tier: "33v",
+	},
+	bronzong: {
+		tier: "33v",
+	},
+	chatot: {
+		tier: "33v",
+	},
+	spiritomb: {
+		tier: "33v",
+	},
+	gible: {
+		tier: "33v",
+	},
+	gabite: {
+		tier: "33v",
+	},
+	garchomp: {
+		tier: "33v",
+	},
+	riolu: {
+		tier: "33v",
+	},
+	lucario: {
+		tier: "33v",
+	},
+	hippopotas: {
+		tier: "33v",
+	},
+	hippowdon: {
+		tier: "33v",
+	},
+	skorupi: {
+		tier: "33v",
+	},
+	drapion: {
+		tier: "33v",
+	},
+	croagunk: {
+		tier: "33v",
+	},
+	toxicroak: {
+		tier: "33v",
+	},
+	carnivine: {
+		tier: "33v",
+	},
+	finneon: {
+		tier: "33v",
+	},
+	lumineon: {
+		tier: "33v",
+	},
+	snover: {
+		tier: "33v",
+	},
+	abomasnow: {
+		tier: "33v",
+	},
+	rotom: {
+		tier: "33v",
+	},
+	rotomheat: {
+		tier: "33v",
+	},
+	rotomwash: {
+		tier: "33v",
+	},
+	rotomfrost: {
+		tier: "33v",
+	},
+	rotomfan: {
+		tier: "33v",
+	},
+	rotommow: {
+		tier: "33v",
+	},
+	uxie: {
+		tier: "33v",
+	},
+	mesprit: {
+		tier: "33v",
+	},
+	azelf: {
+		tier: "33v",
+	},
+	dialga: {
+		tier: "33v",
+	},
+	palkia: {
+		tier: "33v",
+	},
+	heatran: {
+		tier: "33v",
+	},
+	regigigas: {
+		tier: "33v",
+	},
+	giratina: {
+		tier: "33v",
+	},
+	giratinaorigin: {
+	},
+	cresselia: {
+		tier: "33v",
+	},
+	phione: {
+		tier: "33v",
+	},
+	manaphy: {
+		tier: "33v",
+	},
+	darkrai: {
+		tier: "33v",
+	},
+	shaymin: {
+		tier: "33v",
+	},
+	shayminsky: {
+		tier: "33v",
+	},
+	arceus: {
+		tier: "AG",
+	},
+	arceusbug: {
+	},
+	arceusdark: {
+	},
+	arceusdragon: {
+	},
+	arceuselectric: {
+	},
+	arceusfighting: {
+	},
+	arceusfire: {
+	},
+	arceusflying: {
+	},
+	arceusghost: {
+	},
+	arceusgrass: {
+	},
+	arceusground: {
+	},
+	arceusice: {
+	},
+	arceuspoison: {
+	},
+	arceuspsychic: {
+	},
+	arceusrock: {
+	},
+	arceussteel: {
+	},
+	arceuswater: {
+	},
 }
