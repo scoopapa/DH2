18532f6b190cf1a8996129aaa56e6ab36a5430a5
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2026-02-05 16:36:24 -0500

    Update rulesets.ts

--- data/mods/gen3shadowcolosseum/rulesets.ts
+++ data/mods/gen3shadowcolosseum/rulesets.ts
@@ -7,7 +7,7 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
 			this.add('rule', 'Shadow Mechanic: Turns any Pokemon with a Shadow move into a Shadow Pokemon');
 		},
 		onStart(pokemon) {
-			console.log(pokemon);
+			console.log(pokemon.baseSpecies); // sanity check
 			const shadowMoves = [
 				'shadowrush', 'shadowblitz', 'shadowwave', 'shadowbreak', 'shadowrave', 'shadowsky', 'shadowend', 'shadowstorm',
 				'shadowpanic', 'shadowmist', 'shadowdown', 'shadowhold', 'shadowshed', 'shadowhalf', 'shadowsights', 'shadowbolt',
@@ -17,7 +17,7 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
 			for (const moveSlot of pokemon.moveSlots) {
 				const moveid = moveSlot.id;
 				const move = this.dex.moves.get(moveid);
-				console.log(move);
+				console.log(move.basePower);
 				if (move.type === 'Shadow' || pokemon.hasMove(shadowMoves)) {
 					shadowCount++;
 					pokemon.addVolatile('shadow');
