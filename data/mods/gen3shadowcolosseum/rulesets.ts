4259a15b31718d9d6d1febecf5a077b8562e0b8d
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2026-02-05 01:15:14 -0500

    Update rulesets.ts

--- data/mods/gen3shadowcolosseum/rulesets.ts
+++ data/mods/gen3shadowcolosseum/rulesets.ts
@@ -7,11 +7,13 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
 			this.add('rule', 'Shadow Mechanic: Turns any Pokemon with a Shadow move into a Shadow Pokemon');
 		},
 		onStart(pokemon) {
-			const moves = [];
-			for (const moveSlot of pokemon.moveSlots) {
-				const moveid = moveSlot.id;
-				const move = this.dex.moves.get(moveid);
+			let shadowCount = 0;
+			for (const move of pokemon.moveSlots) {
 				if (move.type === 'Shadow') {
+					shadowCount++;
+					pokemon.addVolatile('shadow');
+				}
+				if (shadowCount > 0) {
 					pokemon.addVolatile('shadow');
 				}
 			}
