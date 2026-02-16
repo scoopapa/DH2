04bec416a5a809dbec625bf28877611bec0cd8c7
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-02-16 11:38:42 -0500

    easy abilities that are probably broken

--- data/mods/nukebuttons/abilities.ts
+++ data/mods/nukebuttons/abilities.ts
@@ -7,4 +7,64 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		shortDesc: "",
 	},
 	*/
+	icescales: {
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.category === 'Special') {
+				return this.chainModify(0.75);
+			}
+			if (move.category === 'Physical') {
+				return this.chainModify(0.9);
+			}
+		},
+		flags: {breakable: 1},
+		name: "Ice Scales",
+		rating: 4,
+		num: 1,
+	},
+	stellarize: {
+		onModifyTypePriority: 1,
+		onModifyType(move, pokemon) {
+			const noModifyType = [
+				'hiddenpower', 'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'struggle', 'technoblast', 'terrainpulse', 'weatherball',
+			];
+			if (!(move.isZ && move.category !== 'Status') && !noModifyType.includes(move.id) &&
+				// TODO: Figure out actual interaction
+				!(move.name === 'Tera Blast' && pokemon.terastallized)) {
+				move.type = 'Stellar';
+				move.typeChangerBoosted = this.effect;
+			}
+		},
+		onBasePowerPriority: 23,
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.typeChangerBoosted === this.effect) return this.chainModify([4915, 4096]);
+		},
+		flags: {},
+		name: "Stellarize",
+		rating: 0,
+		num: 2,
+	},
+	adaptability: {
+		onModifySTAB(stab, source, target, move) {
+			if (move.forceSTAB || source.hasType(move.type)) {
+				return 1.75;
+			}
+		},
+		flags: {},
+		name: "Adaptability",
+		rating: 4,
+		num: 3,
+	},
+	poisonpuppeteer: {
+		onAnyAfterSetStatus(status, target, source, effect) {
+			if (source !== this.effectState.target || target === source || effect.effectType !== 'Move') return;
+			if (status.id === 'psn' || status.id === 'tox') {
+				target.addVolatile('confusion');
+				target.addVolatile('infatuation');
+			}
+		},
+		flags: {failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, failskillswap: 1},
+		name: "Poison Puppeteer",
+		rating: 3,
+		num: 4,
+	},
 };
