8479cab5ffc3a9043260a3a90c3b62decd1f73fe
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-02-21 17:32:53 -0500

    fix duped moves in learnsets

--- data/mods/deadcells/formats.ts
+++ data/mods/deadcells/formats.ts
@@ -7,14 +7,14 @@ export const Formats: FormatData[] = [
 			"bweeeeh"
 		],
 		threads: [
-		/*	`&bullet; <a href="">Do Not Use</a>`,*/
-			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1dZeB11mp_zb99EmeZ6gn6s3PVx-sxKQ6YbhkmagMks8/">Datasheet</a>,`
+		/*	`&bullet; <a href="">Dead Cells on Smogon Forums</a>`,*/
+			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1dZeB11mp_zb99EmeZ6gn6s3PVx-sxKQ6YbhkmagMks8/">Datasheet</a>`,
+			`&bullet; <a href="https://deadcells.wiki.gg/">Dead Cells Wiki</a>`,
 		],
 		mod: 'donotuse',
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Evasion Items Clause', 'Species Clause', 'Sleep Clause Mod', 'Mega Rayquaza Clause', 'Terastal Clause', 'Z-Move Clause'],
+		ruleset: ['Standard NatDex', 'Data Mod', 'Terastal Clause', 'Z-Move Clause'],
 		teambuilderFormat: 'National Dex',
-		banlist: ['Huge Power', 'Pure Power', 'Shadow Tag', 'Arena Trap', 'Baton Pass', 'Moody', 'Cute Charm', 'Hustle', 'Sand Veil', 'Snow Cloak'],
-		unbanlist: ['Assist'],
+	/*	banlist: [''], */
 		onValidateTeam(team, format) {
 			let speciesTable = {};
 			let allowedTiers = ['DeCe', 'DeCe NFE', 'DeCe LC'];
