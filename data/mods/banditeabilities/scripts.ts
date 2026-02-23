750d36207244b088f6c3296086ec949062b3bda5
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2026-02-23 02:23:28 -0500

    Update scripts.ts

--- data/mods/banditeabilities/scripts.ts
+++ data/mods/banditeabilities/scripts.ts
@@ -3,7 +3,7 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 	gen: 9,
 	teambuilderConfig: {
 		// for micrometas to only show custom tiers
-		excludeStandardTiers: true,
+		// excludeStandardTiers: true,
 		// only to specify the order of custom tiers
 		customTiers: ['placeholder'],
 	},	
