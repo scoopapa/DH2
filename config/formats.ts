38c06d37a85841f9f6283e9e7b50b9686d57aa3f
Nintenbox0731 <nathanrodriguez448@gmail.com>
2026-03-17 20:21:26 -0400

    zama patch

--- config/formats.ts
+++ config/formats.ts
@@ -3422,7 +3422,7 @@ export const Formats: FormatList = [
 		mod: 'ponymonshowdown',
 		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Terastal Clause', 'Sleep Clause Mod', 'Data Mod'],
 		banlist: ['NDOU', 'UUBL', 'ND Uber', 'ND AG'],
-		unbanlist: ['Regieleki', 'Shedinja', 'Banette-Mega', 'Ceruledge', 'Clodsire', 'Corviknight', 'Dondozo', 'Garganacl', 'Glimmora', 'Gyarados', 'Iron Crown', 'Iron Hands', 'Latios', 'Pinsir-Mega', 'Tapu Lele', 'Ting-Lu' 'Thundurus-Therian', 'Zapdos-Galar'],
+		unbanlist: ['Regieleki', 'Shedinja', 'Banette-Mega', 'Ceruledge', 'Clodsire', 'Corviknight', 'Dondozo', 'Garganacl', 'Glimmora', 'Gyarados', 'Iron Crown', 'Iron Hands', 'Latios', 'Pinsir-Mega', 'Tapu Lele', 'Ting-Lu', 'Thundurus-Therian', 'Zapdos-Galar'],
 		teambuilderFormat: 'National Dex UU',
 	},
 	{
