1e8a74839f19fd0a90b63633e48a4904642b24de
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2026-05-12 00:22:01 -0700

    Update abilities.ts

--- data/mods/ironfist/abilities.ts
+++ data/mods/ironfist/abilities.ts
@@ -1902,17 +1902,21 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 			},
 			onDisableMove(pokemon) {
 				const hazardMoves = ['stealthrock', 'spikes', 'toxicspikes', 'stickyweb', 'gmaxsteelsurge', 'fertilesoil', 'stoneaxe', 'ceaselessedge'];
-				for (const move of hazardMoves) {
-					if (moveSlot.id === 'struggle') continue;
-					pokemon.disableMove(move, true);
+				for (const moveSlot of pokemon.moveSlots) {
+					for (const move of hazardMoves) {
+						if (moveSlot.id === 'struggle') continue;
+						pokemon.disableMove(move, true);
+					}
 				}
 				pokemon.maybeDisabled = true;
 			},
 			onFoeDisableMove(pokemon) {
 				const hazardMoves = ['stealthrock', 'spikes', 'toxicspikes', 'stickyweb', 'gmaxsteelsurge', 'fertilesoil', 'stoneaxe', 'ceaselessedge'];
-				for (const move of hazardMoves) {
-					if (moveSlot.id === 'struggle') continue;
-					pokemon.disableMove(move, true);
+				for (const moveSlot of pokemon.moveSlots) {
+					for (const move of hazardMoves) {
+						if (moveSlot.id === 'struggle') continue;
+						pokemon.disableMove(move, true);
+					}
 				}
 				pokemon.maybeDisabled = true;
 			},
