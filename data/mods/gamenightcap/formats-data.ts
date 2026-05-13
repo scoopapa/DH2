b7ea7b0f7a96c5943062ead6300f962b8e839975
klaasplank <kl8f0z5xd@relay.firefox.com>
2026-05-13 18:01:20 +0200

    gamenightcap important files
    added all the necessary files apart from sprites

---
+++ data/mods/gamenightcap/formats-data.ts
@@ -0,0 +1,266 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	breakerk: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	shimmship: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	naftleach: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	terrabyte: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	jerry: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	velocirador: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	knauty: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	entombis: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	bristan: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	bisonia: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	neuroptera: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	weedhut: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	bonevoyage: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	meeble: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	hemiluca: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	theoiler: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	rehetid: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	krakenwagen: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	teroreligiosa: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	coratack: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	catscan: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	murphy: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	pararaptor: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	jaxxit: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	glideon: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	listeria: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	flabber: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	phaux: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	rubbaduck: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	raidplank: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	cloread: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	slumptosh: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	truckstop: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	protent: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	slum: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	brokeller: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	tristan: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	ocellus: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	weggle: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	trenchbot: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	mindefull: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	stalswarm: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	silkypus: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	lastemy: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	snappybara: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	florasol: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	markseye: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	hopeeve: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	tikismiki: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	sandireign: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	ohwowadsdis: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	walkasong: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	trevore: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	bristletails: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	frozapup: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	pimpimpom: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	beatbeak: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	yolkster: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	catotoca: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	mamoss: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	knucklesmon: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	coldstan: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	obserk: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	durophelon: {
+		tier: "GNCOU",
+		doublesTier: "GNCDOU",
+	},
+	refrigid: {
+		tier: "GNCBonus",
+		doublesTier: "GNCBonus",
+	},
+	scrumptush: {
+		tier: "GNCBonus",
+		doublesTier: "GNCBonus",
+	},
+};
