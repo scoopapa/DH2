ceb44d92ba1f050c64d837d0e7f2f62591fc7cfa
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2026-05-24 16:24:29 -0400

    Create conditions.ts

---
+++ data/mods/regeneration/conditions.ts
@@ -0,0 +1,45 @@
+export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDataTable = {
+	sandstorm: {
+		name: 'Sandstorm',
+		effectType: 'Weather',
+		duration: 5,
+		durationCallback(source, effect) {
+			if (source?.hasItem('smoothrock')) {
+				return 8;
+			}
+			return 5;
+		},
+		// This should be applied directly to the stat before any of the other modifiers are chained
+		// So we give it increased priority.
+		onModifySpDPriority: 10,
+		onModifySpD(spd, pokemon) {
+			if (pokemon.hasType('Rock') && this.field.isWeather('sandstorm')) {
+				return this.modify(spd, 1.5);
+			}
+		},
+		onFieldStart(field, source, effect) {
+			if (effect?.effectType === 'Ability') {
+				if (this.gen <= 5) this.effectState.duration = 0;
+				this.add('-weather', 'Sandstorm', '[from] ability: ' + effect.name, '[of] ' + source);
+			} else {
+				this.add('-weather', 'Sandstorm');
+			}
+		},
+		onFieldResidualOrder: 1,
+		onFieldResidual() {
+			this.add('-weather', 'Sandstorm', '[upkeep]');
+			if (this.field.isWeather('sandstorm')) this.eachEvent('Weather');
+		},
+		onWeather(target) {
+			for (const pokemon of this.getAllActive()) {
+				if (pokemon.hasAbility('jurassicdust')) {
+          this.damage(target.baseMaxhp / 8);
+			  } else {
+          this.damage(target.baseMaxhp / 16);
+        }
+		},
+		onFieldEnd() {
+			this.add('-weather', 'none');
+		},
+	},
+};
