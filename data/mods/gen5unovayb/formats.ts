60603d778e1d88fb954ae6f4d8cc57237744ee62
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2026-05-25 01:35:00 -0400

    Update formats.ts

--- data/mods/gen5unovayb/formats.ts
+++ data/mods/gen5unovayb/formats.ts
@@ -7,10 +7,22 @@ export const Formats: FormatData[] = [
 			"<b>Best Wishes from YB</b>: A Gen 5 Solomod where are only Unovan Pokemon are allowed, with them getting many changes.",
 		],
 		threads: [
-			`&bullet; <a href="https://www.smogon.com/forums/threads/solomods-megathread.3711007/post-10590904">Post in Solomods Megathread</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3778759/">Thread on the Smogon Forums</a>`,
 		],
 		mod: 'gen5unovayb',
 		ruleset: ['Standard', 'Sleep Moves Clause', 'Swagger Clause', 'Data Mod'],
 		banlist: ['Uber', 'Shadow Tag', 'King\'s Rock', 'Razor Fang', 'Soul Dew', 'Assist', 'Hidden Power', 'Baton Pass'],
-	}
+	},
+	{
+		name: "[Gen 5] Best Wishes from YB Random Battle",
+		desc: [
+			"<b>Best Wishes from YB</b>: A Gen 5 Solomod where are only Unovan Pokemon are allowed, with them getting many changes.",
+		],
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3778759/">Thread on the Smogon Forums</a>`,
+		],
+		mod: 'gen5unovayb',
+		team: 'random',
+		ruleset: ['Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod', 'Data Mod'],
+	},
 ];
