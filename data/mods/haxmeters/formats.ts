d0c81df0821d0b1a92901e3bd1958c385bc503a0
Xufactor3 <xufactor3@gmail.com>
2026-06-01 16:13:57 -0400

    moved and renamed hax meters formats

--- data/mods/haxmeters/formats.ts
+++ data/mods/haxmeters/formats.ts
@@ -1,4 +1,28 @@
 import { FormatData } from '../../../sim/dex-formats';
 
 export const Formats: FormatData[] = [
+	{
+		name: "[Gen 9] Hax Meters OU",
+		mod: 'haxmeters',
+		ruleset: ['Standard', '!Sleep Clause Mod', 'Sleep Moves Clause', '!Evasion Items Clause', 'Hax Meter Rule'],
+		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Shadow Tag', 'Baton Pass', 'Last Respects', 'Shed Tail', 'Tera Blast'],
+	},
+	{
+		name: "[Gen 9] Hax Meters Randoms",
+		mod: 'haxmeters',
+		team: 'random',
+		ruleset: ['Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod', 'Hax Meter Rule'],
+	},
+	{
+		name: "[Gen 8] Hax Meters OU",
+		mod: 'gen8haxmeters',
+		ruleset: ['Standard', '!Evasion Items Clause', 'Dynamax Clause', 'Hax Meter Rule'],          
+		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'Baton Pass'],
+	},
+	{
+		name: "[Gen 8] Hax Meters Randoms",
+		mod: 'gen8haxmeters',
+		team: 'random',
+		ruleset: ['Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod', 'Hax Meter Rule'],      
+	}
 ];
