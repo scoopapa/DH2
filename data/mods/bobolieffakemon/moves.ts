f3f1408b4991b08197f7fce00d6719216a480080
boboonatree <103380682+boboonatree@users.noreply.github.com>
2026-05-31 21:34:28 -0500

    Update moves.ts
    aurora veil try again

--- data/mods/bobolieffakemon/moves.ts
+++ data/mods/bobolieffakemon/moves.ts
@@ -811,7 +811,6 @@ export const Moves: {[moveid: string]: MoveData} = {
 		type: "Ice",
 		zMove: {boost: {spe: 1}},
 		contestType: "Beautiful",
-		auroraveil: {
   inherit: true,
   onTry() {
             return this.field.isWeather(['hail', 'snowscape']);
