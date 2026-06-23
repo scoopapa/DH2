28224b25f6009372c35152a942eddfee29ee9ee7
Rasdanation <Rasdanation@gmail.com>
2026-06-23 14:26:48 -0400

    formatting

--- data/mods/umbremons/items.ts
+++ data/mods/umbremons/items.ts
@@ -3,21 +3,21 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 	healthpack: {
 		num: -1,
 		onSwitchInPriority: -2,
-        onStart(pokemon) {
-            for (const ally of pokemon.adjacentAllies()) {
-                if (!pokemon.ignoringItem() && this.runEvent('TryHeal', ally, null, this.effect, ally.baseMaxhp / 4)) {
-                    pokemon.useItem();
-                    this.heal(ally.baseMaxhp / 4, ally, pokemon);
-                    return;
+		onStart(pokemon) {
+			for (const ally of pokemon.adjacentAllies()) {
+				if (!pokemon.ignoringItem() && this.runEvent('TryHeal', ally, null, this.effect, ally.baseMaxhp / 4)) {
+					this.heal(ally.baseMaxhp / 4, ally, pokemon);					
+					pokemon.useItem();
+					return;
                 }
             }
-        },
+		},
 		fling: {
 			basePower: 30,
 			effect(target, source, move) {
 				this.heal(target.maxhp / 2, target, source);
+			}
 		},
-	},
 		shortDesc: "When the holder enters the field, consumes and restores 1/4 of its ally's HP",
 	},
 	// Item Adjustments
