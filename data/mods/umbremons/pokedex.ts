de948dfc1c88add44d40cfd9f07ea35b4817cf58
Rasdanation <Rasdanation@gmail.com>
2026-06-23 21:26:45 -0400

    Ability Distribution

--- data/mods/umbremons/pokedex.ts
+++ data/mods/umbremons/pokedex.ts
@@ -30,5 +30,98 @@ export const Pokedex: { [k: string]: ModdedSpeciesData; } = {
 	scolipedemega: {
 		inherit: true,
 		abilities: {0: "Stamina"},
-	}
+	},
+	weavile: {
+		inherit: true,
+		abilities: {0: "Pressure", 1: "Mold Breaker", H: "Pickpocket"},
+	},
+	klefki: {
+		inherit: true,
+		abilities: {0: "Prankster", 1: "Stalwart", H: "Magician"},
+	},
+	forretress: {
+		inherit: true,
+		abilities: {0: "Sturdy", 1: "Stalwart", H: "Overcoat"},
+	},
+	lucario: {
+		inherit: true,
+		abilities: {0: "Stalwart", 1: "Inner Focus", H: "Justified"},
+	},
+	snorlax: {
+		inherit: true,
+		abilities: {0: "Massive", 1: "Thick Fat", H: "Gluttony"},
+	},
+	steelix: {
+		inherit: true,
+		abilities: {0: "Massive", 1: "Sturdy", H: "Sheer Force"},
+	},
+	avalugg: {
+		inherit: true,
+		abilities: {0: "Massive", 1: "Ice Body", H: "Sturdy"},
+	},
+	// Waiting on confirmation of which ability Massive replaces for Avalugg Hisui
+	avalugghisui: {
+		inherit: true,
+		abilities: {0: "Strong Jaw", 1: "Ice Body", H: "Sturdy"},
+	},
+	rhyperior: {
+		inherit: true,
+		abilities: {0: "Massive", 1: "Solid Rock", H: "Reckless"},
+	},
+	bastiodon: {
+		inherit: true,
+		abilities: {0: "Sturdy", 1: "Massive", H: "Soundproof"},
+	},
+	hawluchamega: {
+		inherit: true,
+		abilities: {0: "Masquerade"}
+	},
+	roserade: {
+		inherit: true,
+		abilities: {0: "Natural Cure", 1: "Masquerade", H: "Technician"},
+	},
+	furfrou: {
+		inherit: true,
+		abilities: {0: "Fur Coat", H: "Masquerade"},
+	},
+	cofagrigus: {
+		inherit: true,
+		abilities: {0: "Mummy", H: "Masquerade"},
+	},
+	mrrime: {
+		inherit: true,
+		abilities: {0: "Tangled Feet", 1: "Screen Cleaner", H: "Masquerade"},
+	},
+	decidueye: {
+		inherit: true,
+		abilities: {0: "Overgrow", H: "Masquerade"},
+	},
+	beartic: {
+		inherit: true,
+		abilities: {0: "Thick Fat", 1: "Slush Rush", H: "Swift Swim"},
+	},
+	bellibolt: {
+		inherit: true,
+		abilities: {0: "Electromorphosis", 1: "Static", H: "Thick Fat"},
+	},
+	goodra: {
+		inherit: true,
+		abilities: {0: "Sap Sipper", 1: "Thick Fat", H: "Gooey"},
+	},
+	hippowdon: {
+		inherit: true,
+		abilities: {0: "Sand Stream", 1: "Thick Fat", H: "Sand Force"},
+	},
+	pangoro: {
+		inherit: true,
+		abilities: {0: "Thick Fat", 1: "Mold Breaker", H: "Scrappy"},
+	},
+	slurpuff: {
+		inherit: true,
+		abilities: {0: "Sweet Veil", 1: "Thick Fat", H: "Unburden"},
+	},
+	meganium: {
+		inherit: true,
+		abilities: {0: "Overgrow", H: "Healer"},
+	},
 };
