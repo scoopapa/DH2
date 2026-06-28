4a475e244e7a92786cbb1b33cfd4dc16eaf03ab1
Rasdanation <Rasdanation@gmail.com>
2026-06-27 22:05:20 -0400

    typo fix

--- data/mods/publicdomain/abilities.ts
+++ data/mods/publicdomain/abilities.ts
@@ -376,7 +376,7 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		onBeforeMove(pokemon, target, move) {
 			if(move.id === 'chillyreception') {
 				const reaction = this.dex.getActiveMove('futuresight');
-				this.actions.useMove(reaction, source, target);
+				this.actions.useMove(reaction, pokemon, target);
 			}
 		},
 		flags: {},
