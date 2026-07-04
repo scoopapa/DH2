8f1e18623a5d3f36c7a46dec16b9b1c576906079
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2026-07-03 21:28:57 -0600

    Remaining aesthetic improvements
    There are icons now! and I think this is a better way for the information to be organized

--- data/mods/umbremons/rulesets.ts
+++ data/mods/umbremons/rulesets.ts
@@ -10,7 +10,6 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		onTeamPreview() {
 			for (const side of this.sides) {
 				let showFakemon = false;
-				let extraLineBreak = false;
 				let hideBox = `raw|<div class="infobox" open><details class ="details"><summary>Modded Pokémon on ${side.name}'s team</summary>`;
 				for (const pokemon of side.pokemon) {
 					let species = this.dex.species.get(pokemon.species.name);
@@ -31,7 +30,8 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 					// Since Umbremons changes some in-battle forms (like Mega Evolutions), we also need to know if any of those are modded
 					let formDisplay = ``;
 					let formList = [];
-					if (species.otherFormes) for (const form of species.otherFormes) {
+					let lowestSpecies = (species.baseSpecies ? this.dex.species.get(species.baseSpecies) : species);
+					if (lowestSpecies.otherFormes) for (const form of lowestSpecies.otherFormes) {
 						formList.push(form); // even if the form isn't modded, we'll want to check later if their Abilities are modded
 						
 						let umbremonsForm = this.dex.species.get(form);
@@ -54,9 +54,9 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 							if (umbremonsForm.abilities['S']) abilities += ` // <i>(${umbremonsForm.abilities['S']})</i>`;
 							const baseStats = umbremonsForm.baseStats;
 							
-							formDisplay += `<div class="message"><ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + umbremonsForm.name + `</span> <span class="col typecol"><img src="http://play.pokemonshowdown.com/sprites/types/${umbremonsForm.types[0]}.png" alt="${umbremonsForm.types[0]}" height="14" width="32">`;
+							formDisplay += `<br><div class="message"><ul class="utilichart"><li class="result"><span class="col iconcol"><span class "picon"><img src="https://www.smogon.com/forums/media/minisprites/${(umbremonsForm.spriteid).replace(`mega`,`-mega`).replace(`--mega`,`-mega`)}.png" alt="${umbremonsForm.name}"></span></span><span class="col pokemonnamecol" style="white-space: nowrap">` + umbremonsForm.name + `</span> <span class="col typecol"><img src="http://play.pokemonshowdown.com/sprites/types/${umbremonsForm.types[0]}.png" alt="${umbremonsForm.types[0]}" height="14" width="32">`;
 							if (umbremonsForm.types[1]) formDisplay += `<img src="http://play.pokemonshowdown.com/sprites/types/${umbremonsForm.types[1]}.png" alt="${umbremonsForm.types[1]}" height="14" width="32">`;
-							formDisplay += `</span></li><br><li class="result"><span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span></li><br><li class="result"><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul></div><br>`;
+							formDisplay += `</span></li><br><li class="result"><span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span></li><br><li class="result"><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul></div>`;
 						}
 					}
 					
@@ -69,67 +69,66 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 					if (species && modded) {
 						showFakemon = true; // This lets us know that at least one modded Pokémon exists on the team; if not, we won't want to print anything!
 						
-						if (extraLineBreak) hideBox += `<br>`;
-						else extraLineBreak = true; // This gives us an extra linebreak before every Pokémon except the first
-						
 						let abilities = species.abilities[0];
 						if (species.abilities[1]) abilities += ` / ${species.abilities[1]}`;
 						if (species.abilities['H']) abilities += ` // ${species.abilities['H']}`;
 						if (species.abilities['S']) abilities += ` // <i>(${species.abilities['S']})</i>`;
 						const baseStats = species.baseStats;
-						hideBox += `<div class="message"><ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="http://play.pokemonshowdown.com/sprites/types/${species.types[0]}.png" alt="${species.types[0]}" height="14" width="32">`;
+						hideBox += `<br><div class="message"><ul class="utilichart"><li class="result"><span class="col iconcol"><span class="picon"><img src="https://www.smogon.com/forums/media/minisprites/${species.spriteid}.png" alt="${species.name}"></span></span><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="http://play.pokemonshowdown.com/sprites/types/${species.types[0]}.png" alt="${species.types[0]}" height="14" width="32">`;
 						if (species.types[1]) hideBox += `<img src="http://play.pokemonshowdown.com/sprites/types/${species.types[1]}.png" alt="${species.types[1]}" height="14" width="32">`;
 						hideBox += `</span></li><br><li class="result"><span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span></li><br><li class="result"><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul></div>`;
-						
-						let customGuide = `<br><div class="infobox" open><details class ="details"><summary>More details on ${species.name}</summary>`;
-
-						if (formDisplay) customGuide += formDisplay;
 						
 						// Movepool changes
 						// This section is slightly reworded from the Evo 2 version, since the modded Pokémon in Umbremons aren't Fakemon
 						if (species.movepoolAdditions) {
-							customGuide += `<div class="hint">${species.name} <strong>gained</strong> the move`;
-							if (species.movepoolAdditions.length > 1) customGuide += `s`;
+							hideBox += `<br><div class="hint">${species.name} gained the move`;
+							if (species.movepoolAdditions.length > 1) hideBox += `s`;
 							let order = 0;
 							for (const moveid of species.movepoolAdditions) {
 								order++;
 								let move = this.dex.moves.get(moveid);
 								if (order < species.movepoolAdditions.length) {
-									customGuide += ` ${move.name}`;
-									if (order + 1 < species.movepoolAdditions.length) customGuide += `,`;
+									hideBox += ` <strong>${move.name}</strong>`;
+									if (order + 1 < species.movepoolAdditions.length) hideBox += `,`;
 								}
 								else {
-									if (species.movepoolAdditions.length !== 1) customGuide += ` and`;
-									customGuide += ` ${move.name}`;
+									if (species.movepoolAdditions.length !== 1) hideBox += ` and`;
+									hideBox += ` <strong>${move.name}</strong>`;
 								}
 							}
 						}
 						if (species.movepoolDeletions) {
-							if (species.movepoolAdditions) customGuide += `,<br>but it <strong>lost</strong> the move`;
-							else customGuide += `<div class="hint">${species.name} <strong>lost</strong> the move`;
+							if (species.movepoolAdditions) hideBox += `,<br>but it <i>lost</i> the move`;
+							else hideBox += `<br><div class="hint">${species.name} <i>lost</i> the move`;
 							
-							if (species.movepoolDeletions.length > 1) customGuide += `s`;
+							if (species.movepoolDeletions.length > 1) hideBox += `s`;
 							let order = 0;
 							for (const moveid of species.movepoolDeletions) {
 								order++;
 								let move = this.dex.moves.get(moveid);
 								if (order < species.movepoolDeletions.length) {
-									customGuide += ` ${move.name}`;
-									if (order + 1 < species.movepoolDeletions.length) customGuide += `,`;
+									hideBox += ` <strong>${move.name}</strong>`;
+									if (order + 1 < species.movepoolDeletions.length) hideBox += `,`;
 								}
 								else {
-									if (species.movepoolDeletions.length !== 1) customGuide += ` and`;
-									customGuide += ` ${move.name}`;
+									if (species.movepoolDeletions.length !== 1) hideBox += ` and`;
+									hideBox += ` <strong>${move.name}</strong>`;
 								}
 							}
 						}
-						if (species.movepoolAdditions || species.movepoolDeletions) customGuide += `.</div>`;
-
+						if (species.movepoolAdditions || species.movepoolDeletions) hideBox += `.</div>`;
+						
+						if (formDisplay.length) hideBox += `<br><div class="infobox" open><details class ="details"><summary>Forms related to ${species.name}</summary>${formDisplay}<br></details></div>`;
+						
+						let customGuide = `<br><div class="infobox" open><details class ="details"><summary>Custom elements related to ${species.name}</summary>`;
+						let displayCustoms = false;
+						
 						let abilitiesCovered = [];
 						// custom Abilities
 						for (const num in [0, 1, 'H', 'S']) if (species.abilities[num]) {
 							let ability = this.dex.abilities.get(species.abilities[num]);
 							if (ability && !abilitiesCovered.includes(ability) && (!ability.num || ability.num < 0 || ability.modded)) { // report custom Abilities only
+								displayCustoms = true;
 								customGuide += `<br><div class="message"><li class="result"><span class="col namecol"><strong>${ability.name}</strong></span>`;
 								if (ability.desc) customGuide += `<br><font color="#686868">${ability.desc}</font>`;
 								else if (ability.shortDesc) customGuide += `<br><font color="#686868">${ability.shortDesc}</font>`;
@@ -140,6 +139,7 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 						if (formList.length) for (const form of formList) for (const num in [0, 1, 'H', 'S']) if (this.dex.species.get(form).abilities[num]) {
 							let ability = this.dex.abilities.get(this.dex.species.get(form).abilities[num]);
 							if (ability && !abilitiesCovered.includes(ability) && (!ability.num || ability.num < 0 || ability.modded)) { // report custom Abilities only
+								displayCustoms = true;
 								customGuide += `<br><div class="hint">${form}'s Ability, ${ability.name}, is modded:</div>`
 								customGuide += `<br><div class="message"><li class="result"><span class="col namecol"><strong>${ability.name}</strong></span>`;
 								if (ability.desc) customGuide += `<br><font color="#686868">${ability.desc}</font>`;
@@ -155,11 +155,12 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 							for (const moveid in this.dex.data.Learnsets[this.toID(pokemon.species)].learnset) {
 								let move = this.dex.moves.get(moveid);
 								if (move && (!move.num || move.num < 0 || move.modded)) { // report custom moves only
+									displayCustoms = true;
 									let power = move.basePower;
 									if (power < 2) power = "—";
 									let acc = move.accuracy;
 									if (acc === true) acc = "—";
-									customGuide += `<br><div class="message"><ul class="utilichart"><li class="result"><span class="col movenamecol"><strong>${move.name}</strong></span><span class="col typecol"><img src="https://play.pokemonshowdown.com/sprites/types/${move.type}.png" alt="${move.type} width="32" height="14"><img src="https://play.pokemonshowdown.com/sprites/categories/${move.category}.png" alt="${move.category}" width="32" height="14"></span><span class="col labelcol"><em>Power</em><br>${power}</span><span class="col widelabelcol"><em>Accuracy</em><br>${acc}</span><span class="col pplabelcol"><em>PP</em><br>${move.pp}</span></li></ul></div>`;
+									customGuide += `<br><div class="message"><ul class="utilichart"><li class="result"><span class="col movenamecol"><strong>${move.name}</strong></span><span class="col typecol"><img src="https://play.pokemonshowdown.com/sprites/types/${move.type}.png" alt="${move.type} width="32" height="14"><img src="https://play.pokemonshowdown.com/sprites/categories/${move.category}.png" alt="${move.category}" width="32" height="14"></span><span class="col labelcol"><em>Power</em><br>${power}</span><span class="col widelabelcol"><em>Accuracy</em><br>${acc}</span><span class="col pplabelcol"><em>PP</em><br>${move.noPPBoosts ? move.pp : Math.floor(move.pp * 8 / 5)}</span></li></ul></div>`;
 									if (move.desc) customGuide += `<br><font color="#686868">${move.desc}</font>`;
 									else if (move.shortDesc) customGuide += `<br><font color="#686868">${move.shortDesc}</font>`;
 								}
@@ -170,9 +171,10 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 						if (species.description) customGuide += `<br><div class="hint">${species.description}</div>`;
 						
 						customGuide += `<br></details></div>`;
-						hideBox += customGuide;
+						if (displayCustoms) hideBox += customGuide;
 					}
 				}
+				
 				hideBox += 	`</details></div>`;
 				if (showFakemon) this.add(`${hideBox}`);
 			}
