de0eb0d096abe03f626962d4f5d22e7dfe9eadef
KnivesTotality <olivesparkly@gmail.com>
2026-07-11 19:36:23 -0400

    Update scripts.ts

--- data/mods/monsterhunter/scripts.ts
+++ data/mods/monsterhunter/scripts.ts
@@ -84,7 +84,7 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 				}
 		
 				if (!ignoreImmunities && status.id &&
-						!(source?.hasAbility('corrosion') || source?.hasAbility('perforating')) && ['tox', 'psn'].includes(status.id))) {
+						!(source?.hasAbility('corrosion') || source?.hasAbility('perforating')) && ['tox', 'psn'].includes(status.id)) {
 					// the game currently never ignores immunities
 					if (!this.runStatusImmunity(status.id === 'tox' ? 'psn' : status.id)) {
 						this.battle.debug('immune to status');
