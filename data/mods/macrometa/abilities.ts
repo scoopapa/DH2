a9f6a08fff3e9744383da2b6b155a36c26b2238b
Scoopapa <ahoffman4422@gmail.com>
2026-07-21 00:28:50 -0400

    Create abilities.ts

---
+++ data/mods/macrometa/abilities.ts
@@ -0,0 +1,27 @@
+export const Abilities: { [abilityid: string]: ModdedAbilityData } = {
+	dragonize: {
+		inherit: true,
+		isNonstandard: null;
+	},
+	eelevate: {
+		inherit: true,
+		isNonstandard: null;
+	},
+	firemane: {
+		inherit: true,
+		isNonstandard: null;
+	},
+	spicyspray: {
+		inherit: true,
+		isNonstandard: null;
+	},
+	megasol: {
+		inherit: true,
+		isNonstandard: null;
+	},
+	piercingdrill: {
+		inherit: true,
+		isNonstandard: null;
+	},
+};
+
