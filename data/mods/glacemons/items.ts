bc0503281e4029249a8dd13d04e390a37cf4bd27
Spammernoob69 <ahhhhthunderblunder@gmail.com>
2026-08-04 00:11:00 -0400

    Dungeon Looplet bugfix (Glacemons)
    Fix bug where Looplet does not apply abilities before hazards.

--- data/mods/glacemons/items.ts
+++ data/mods/glacemons/items.ts
@@ -2055,16 +2055,16 @@ export const Items: { [k: string]: ModdedItemData; } = {
 		gen: 9,
 		shortDesc: "All abilities active at once.",
 		onTakeItem: false,
-		onStart(target) {
-			this.add('-item', target, 'Dungeon\'s Looplet');
-			this.add('-message', `${target.name} is holding a Dungeon's Looplet!`);
-			target.m.innates = Object.keys(target.species.abilities)
-					.map(key => this.toID(target.species.abilities[key as "0" | "1" | "H" | "S"]))
-					.filter(ability => ability !== target.ability);
-			if (target.m.innates) {
-				for (const innate of target.m.innates) {
-					if (target.hasAbility(innate)) continue;
-					target.addVolatile("ability:" + innate, target);
+		onSwitchIn(pokemon) {
+			this.add('-item', pokemon, 'Dungeon\'s Looplet');
+			this.add('-message', `${pokemon.name} is holding a Dungeon's Looplet!`);
+			pokemon.m.innates = Object.keys(pokemon.species.abilities)
+					.map(key => this.toID(pokemon.species.abilities[key as "0" | "1" | "H" | "S"]))
+					.filter(ability => ability !== pokemon.ability);
+			if (pokemon.m.innates) {
+				for (const innate of pokemon.m.innates) {
+					if (pokemon.hasAbility(innate)) continue;
+					pokemon.addVolatile("ability:" + innate, pokemon);
 				}
 			}
 		},
