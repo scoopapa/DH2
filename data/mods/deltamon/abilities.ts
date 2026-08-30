9ee6ee82dcf8e4590ee7428e2550725eff50fd5a
ToxicAriados <ian.wrobel05@gmail.com>
2026-08-29 19:00:42 -0500

    Plane nerfs

--- data/mods/deltamon/abilities.ts
+++ data/mods/deltamon/abilities.ts
@@ -655,6 +655,7 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 
 		onResidualOrder: 13,
 		onResidual(pokemon) {
+			if (pokemon.baseSpeciesname !== 'Normal NPC') return;
 			for (const target of pokemon.adjacentFoes()) {
 				if (target.baseSpecies.name !== 'Onionsan' || target.volatiles['substitute'] || target.isSemiInvulnerable()) return;
 				if (this.randomChance(2, 10)) {
