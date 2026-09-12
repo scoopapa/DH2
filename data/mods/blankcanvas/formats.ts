755d8c3e08ef540defea31448bc44edfb1fd23c7
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2026-09-12 11:49:52 -0400

    Update formats.ts

--- data/mods/blankcanvas/formats.ts
+++ data/mods/blankcanvas/formats.ts
@@ -100,6 +100,7 @@ export const Formats: FormatData[] = [
 			'Iron Mike', 'Brasspecter', 'Deadward', 'Ptoxidactyl', 'Craggon', 'Siltworm', 'Arsenstorm', 'Guttergar', 'Goodjur', 'Pinaturbo',
 			'Mediocjur', 'Plushimander', 'Orchrefait', 'Eighremophas', 'Snaellcaster', 'Marultan', 'Vertabrea', 'Flingangel', 'Nekolepsy',
 			'Honeydue', 'Drug', 'Aberrockin', 'Tartzacoatl', 'Iusperare', 'Wyrmware-N', 'Irdonerkeb', 'Kappaqua', 'Lactoxic', 'Horronimbus', 'Woolly Crawler',
+			'Brainstrom', 'Monster', 'Grizbearab',
 		],
 		/* onValidateTeam(team, format) {
 			let speciesTable = {};
