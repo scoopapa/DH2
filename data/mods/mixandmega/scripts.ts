771aee36d2b23f9cab31d2292181a7ddec90a861
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2026-09-16 23:39:24 -0400

    Update scripts.ts

--- data/mods/mixandmega/scripts.ts
+++ data/mods/mixandmega/scripts.ts
@@ -10,9 +10,9 @@ export const Scripts: ModdedBattleScriptsData = {
 			if (!item.megaStone && !item.onDrive && !(item.onPlate && !item.zMove) && !item.onMemory) continue;
 			this.modData('Items', i).onTakeItem = false;
 			if (item.isNonstandard === "Past") this.modData('Items', i).isNonstandard = null;
-			if (item.megaStone) {
+		/*	if (item.megaStone) {
 				this.modData('FormatsData', this.toID(item.megaStone)).isNonstandard = null;
-			}
+			} */
 		}
 	},
 	start() {
